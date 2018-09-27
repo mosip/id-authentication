a51538a07abe79ec0245087f2c873d85ab722a8a
M1037462 <M1037462@A2MD19331.mindtree.com>
2018-09-27 18:22:42 +0530

    Code for validation of PRID
    Former-commit-id: 9d66a7a8f016fc3769d88655288380d73a1c5c48
    Former-commit-id: bee61fb23ba98eacc994dcd1af4ea3d63bbeb5e4

---
+++ kernel/idvalidator/src/main/java/org/mosip/kernel/idvalidator/pridvalidator/constants/MosipPridExceptionConstants.java
@@ -0,0 +1,63 @@
+package org.mosip.kernel.idvalidator.pridvalidator.constants;
+
+/**
+ * 
+ * @author M1037462
+ *
+ *         since 1.0.0
+ */
+public enum MosipPridExceptionConstants {
+
+	PRID_VAL_INVALID_NULL("KER-IDV-101",
+			"Entered PRID should not be empty or null."), PRID_VAL_ILLEGAL_SEQUENCE_REPEATATIVE("KER-IDV-102",
+					"Entered PRID should not contain any sequential and repeated block of number for 2 or more than two digits"), PRID_VAL_ILLEGAL_LENGTH(
+							"KER-IDV-103",
+							"Entered PRID length should be 14 digit."), PRID_VAL_INVALID_DIGITS("KER-IDV-104",
+									"Entered PRID should not contain any alphanumeric characters."), PRID_VAL_ILLEGAL_CHECKSUM(
+											"KER-IDV-105",
+											"Entered PRID should match checksum."), PRID_VAL_INVALID_ZERO_ONE(
+													"KER-IDV-106",
+													"Entered PRID should not contain Zero or One as first Digit.");
+
+	/**
+	 * This variable holds the error code.
+	 */
+	private String errorCode;
+
+	/**
+	 * This variable holds the error message.
+	 */
+	private String errorMessage;
+
+	/**
+	 * Constructor for VIDErrorConstants Enum.
+	 * 
+	 * @param errorCode
+	 *            the error code.
+	 * @param errorMessage
+	 *            the error message.
+	 */
+	MosipPridExceptionConstants(String errorCode, String errorMessage) {
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+
+	/**
+	 * Getter for errorCode.
+	 * 
+	 * @return the error code.
+	 */
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	/**
+	 * Getter for errorMessage.
+	 * 
+	 * @return the error message.
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+}
