497b647072f5bb6ba8ffdfa6272e564bb4c971a4
Manoj SP <Manoj.SP@mindtree.com>
2018-09-27 13:17:39 +0530

    Updated swagger to ignore basic-error-controller
    Former-commit-id: 1e6257f6d404a1876889610621fc554428b2b234

--- authentication/service/src/main/java/org/mosip/auth/service/config/SwaggerConfig.java
+++ authentication/service/src/main/java/org/mosip/auth/service/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+				.paths(PathSelectors.regex("(?!/error.*).*")).build();
 	}
 
 }
