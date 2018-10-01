a9e4e6c45523b2ff8727197b3ab17e2ef40b1b77
M1046559 <M1046559@b2ml27198.mindtree.com>
2018-10-01 19:23:28 +0530

    Resolving Sonar issue in UserPasswordDAO
    Former-commit-id: 5d95b1615c67af27f7d36a915614f2811b13773a
    Former-commit-id: affa218dcdfce519b55c1cb83919a88b20aaf6a2

--- registration/registration-service/src/main/java/org/mosip/registration/dao/impl/RegistrationUserPasswordDAOImpl.java
+++ registration/registration-service/src/main/java/org/mosip/registration/dao/impl/RegistrationUserPasswordDAOImpl.java
@@ -1,7 +1,14 @@
 package org.mosip.registration.dao.impl;
 
+import static org.mosip.registration.constants.RegConstants.APPLICATION_ID;
+import static org.mosip.registration.constants.RegConstants.APPLICATION_NAME;
+import static org.mosip.registration.util.reader.PropertyFileReader.getPropertyValue;
+
 import java.util.List;
 
+import org.mosip.kernel.core.spi.logging.MosipLogger;
+import org.mosip.kernel.logger.appenders.MosipRollingFileAppender;
+import org.mosip.kernel.logger.factory.MosipLogfactory;
 import org.mosip.registration.dao.RegistrationUserPasswordDAO;
 import org.mosip.registration.entity.RegistrationUserPassword;
 import org.mosip.registration.entity.RegistrationUserPasswordID;
@@ -9,22 +16,55 @@ import org.mosip.registration.repositories.RegistrationUserPasswordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The implementation class of {@link RegistrationUserPasswordDAO}.
+ *
+ * @author Sravya Surampalli
+ * @since 1.0.0
+ */
 @Repository
 public class RegistrationUserPasswordDAOImpl  implements RegistrationUserPasswordDAO {
 	
+	/** The registrationUserPassword repository. */
 	@Autowired
 	private RegistrationUserPasswordRepository registrationUserPasswordRepository;
 	
+	/** Object for Logger. */
+	private static MosipLogger LOGGER;
+
+	/**
+	 * Initialize logger.
+	 *
+	 * @param mosipRollingFileAppender the mosip rolling file appender
+	 */
+	@Autowired
+	private void initializeLogger(MosipRollingFileAppender mosipRollingFileAppender) {
+		LOGGER = MosipLogfactory.getMosipDefaultRollingFileLogger(mosipRollingFileAppender, this.getClass());
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.mosip.registration.dao.RegistrationUserPasswordDAO#getPassword(java.lang.String,java.lang.String)
+	 */
 	public String getPassword(String userId, String hashPassword) {
+		
+		LOGGER.debug("REGISTRATION - USER_PASSWORD - REGISTRATION_USER_PASSWORD_DAO", 
+				getPropertyValue(APPLICATION_NAME), getPropertyValue(APPLICATION_ID), 
+				"Retrieving User Password");
+		
 		RegistrationUserPasswordID registrationUserPasswordID = new RegistrationUserPasswordID();
 		registrationUserPasswordID.setUsrId(userId);
 		registrationUserPasswordID.setPwd(hashPassword);
 		List<RegistrationUserPassword> registrationUserPassword = registrationUserPasswordRepository.findByRegistrationUserPasswordID(registrationUserPasswordID);
-		String password = "";
-		if(registrationUserPassword.size() > 0) {
-			password = registrationUserPassword.get(0).getRegistrationUserPasswordID().getPwd();
+		String userData = "";
+		if(!registrationUserPassword.isEmpty()) {
+			userData = registrationUserPassword.get(0).getRegistrationUserPasswordID().getPwd();
 		}
-		return password;
+		
+		LOGGER.debug("REGISTRATION - USER_PASSWORD - REGISTRATION_USER_PASSWORD_DAO", 
+				getPropertyValue(APPLICATION_NAME), getPropertyValue(APPLICATION_ID), 
+				"Retrieved User Password");
+		
+		return userData;
 	}
 
 }
