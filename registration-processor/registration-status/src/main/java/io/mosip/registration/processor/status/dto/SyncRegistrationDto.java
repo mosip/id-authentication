cb2b9c9dd62cf3ccd8f482352e7b0c683cc6d99e
M1047487 <M1047487@A2MD20783.mindtree.com>
2018-10-04 14:24:16 +0530

    Updated SyncRegistrationDto with updated columns in db script
    Former-commit-id: 204e9caf4baa969ec500e0d30686fda1166f7373
    Former-commit-id: ced833eea00c2e144bc8b25c7ec24a031133f744

--- registration-processor/registration-status/src/main/java/io/mosip/registration/processor/status/dto/SyncRegistrationDto.java
+++ registration-processor/registration-status/src/main/java/io/mosip/registration/processor/status/dto/SyncRegistrationDto.java
@@ -4,10 +4,12 @@
 package io.mosip.registration.processor.status.dto;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 import io.mosip.registration.processor.status.code.SyncStatus;
 import io.mosip.registration.processor.status.code.SyncType;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class SyncRegistrationDto.
  *
@@ -18,67 +20,292 @@ public class SyncRegistrationDto implements Serializable {
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -3922338139042373367L;
 
-	/** The id. */
-	private String id;
+	/** The Sync registration id. */
+	private String SyncRegistrationId;
 
-	/** The parent id. */
-	private String parentId;
+	/** The registration id. */
+	private String registrationId;
+	
+	/** The parent registration id. */
+	private String parentRegistrationId;
+	
+	/** The status code. */
+	private String statusCode;
+	
+	/** The status comment. */
+	private String statusComment;
+	
+	/** The lang code. */
+	private String langCode;
+	
+	/** The is active. */
+	private Boolean isActive;
+	
+	/** The created by. */
+	private String createdBy;
+	
+	/** The create date time. */
+	private LocalDateTime createDateTime;
+	
+	/** The updated by. */
+	private String updatedBy;
+	
+	/** The update date time. */
+	private LocalDateTime updateDateTime;
+	
+	/** The is deleted. */
+	private Boolean isDeleted;
+	
+	/** The deleted date time. */
+	private LocalDateTime deletedDateTime;
 
 	/**
 	 * Instantiates a new sync registration dto.
 	 *
-	 * @param id
-	 *            the id
-	 * @param parentId
-	 *            the parent id
-	 * @param syncType
-	 *            the sync type
-	 * @param synchStatus
-	 *            the synch status
+	 * @param SyncRegistrationId the sync registration id
+	 * @param parentId            the parent id
+	 * @param syncType            the sync type
+	 * @param synchStatus            the synch status
 	 */
-	public SyncRegistrationDto(String id, String parentId, SyncType syncType, SyncStatus synchStatus) {
+	public SyncRegistrationDto(String registrationId, String parentRegistrationId, SyncType syncType, SyncStatus synchStatus) {
 
-		this.id = id;
-		this.parentId = parentId;
+		this.registrationId = registrationId;
+		this.parentRegistrationId = parentRegistrationId;
 
 	}
 
 	/**
-	 * Gets the id.
+	 * Gets the sync registration id.
 	 *
-	 * @return the id
+	 * @return the sync registration id
 	 */
-	public String getId() {
-		return id;
+	public String getSyncRegistrationId() {
+		return SyncRegistrationId;
 	}
 
 	/**
-	 * Sets the id.
+	 * Sets the sync registration id.
 	 *
-	 * @param id
-	 *            the id to set
+	 * @param syncRegistrationId the new sync registration id
 	 */
-	public void setId(String id) {
-		this.id = id;
+	public void setSyncRegistrationId(String syncRegistrationId) {
+		SyncRegistrationId = syncRegistrationId;
 	}
 
 	/**
-	 * Gets the parent id.
+	 * Gets the registration id.
 	 *
-	 * @return the parentId
+	 * @return the registration id
 	 */
-	public String getParentId() {
-		return parentId;
+	public String getRegistrationId() {
+		return registrationId;
 	}
 
 	/**
-	 * Sets the parent id.
+	 * Sets the registration id.
 	 *
-	 * @param parentId
-	 *            the parentId to set
+	 * @param registrationId the new registration id
 	 */
-	public void setParentId(String parentId) {
-		this.parentId = parentId;
+	public void setRegistrationId(String registrationId) {
+		this.registrationId = registrationId;
+	}
+
+	/**
+	 * Gets the parent registration id.
+	 *
+	 * @return the parent registration id
+	 */
+	public String getParentRegistrationId() {
+		return parentRegistrationId;
+	}
+
+	/**
+	 * Sets the parent registration id.
+	 *
+	 * @param parentRegistrationId the new parent registration id
+	 */
+	public void setParentRegistrationId(String parentRegistrationId) {
+		this.parentRegistrationId = parentRegistrationId;
+	}
+
+	/**
+	 * Gets the status code.
+	 *
+	 * @return the status code
+	 */
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	/**
+	 * Sets the status code.
+	 *
+	 * @param statusCode the new status code
+	 */
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	/**
+	 * Gets the status comment.
+	 *
+	 * @return the status comment
+	 */
+	public String getStatusComment() {
+		return statusComment;
+	}
+
+	/**
+	 * Sets the status comment.
+	 *
+	 * @param statusComment the new status comment
+	 */
+	public void setStatusComment(String statusComment) {
+		this.statusComment = statusComment;
+	}
+
+	/**
+	 * Gets the lang code.
+	 *
+	 * @return the lang code
+	 */
+	public String getLangCode() {
+		return langCode;
+	}
+
+	/**
+	 * Sets the lang code.
+	 *
+	 * @param langCode the new lang code
+	 */
+	public void setLangCode(String langCode) {
+		this.langCode = langCode;
+	}
+
+	/**
+	 * Gets the checks if is active.
+	 *
+	 * @return the checks if is active
+	 */
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	/**
+	 * Sets the checks if is active.
+	 *
+	 * @param isActive the new checks if is active
+	 */
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	/**
+	 * Gets the created by.
+	 *
+	 * @return the created by
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	/**
+	 * Sets the created by.
+	 *
+	 * @param createdBy the new created by
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	/**
+	 * Gets the creates the date time.
+	 *
+	 * @return the creates the date time
+	 */
+	public LocalDateTime getCreateDateTime() {
+		return createDateTime;
+	}
+
+	/**
+	 * Sets the creates the date time.
+	 *
+	 * @param createDateTime the new creates the date time
+	 */
+	public void setCreateDateTime(LocalDateTime createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	/**
+	 * Gets the updated by.
+	 *
+	 * @return the updated by
+	 */
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	/**
+	 * Sets the updated by.
+	 *
+	 * @param updatedBy the new updated by
+	 */
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	/**
+	 * Gets the update date time.
+	 *
+	 * @return the update date time
+	 */
+	public LocalDateTime getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	/**
+	 * Sets the update date time.
+	 *
+	 * @param updateDateTime the new update date time
+	 */
+	public void setUpdateDateTime(LocalDateTime updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+
+	/**
+	 * Gets the checks if is deleted.
+	 *
+	 * @return the checks if is deleted
+	 */
+	public Boolean getIsDeleted() {
+		return isDeleted;
+	}
+
+	/**
+	 * Sets the checks if is deleted.
+	 *
+	 * @param isDeleted the new checks if is deleted
+	 */
+	public void setIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	/**
+	 * Gets the deleted date time.
+	 *
+	 * @return the deleted date time
+	 */
+	public LocalDateTime getDeletedDateTime() {
+		return deletedDateTime;
+	}
+
+	/**
+	 * Sets the deleted date time.
+	 *
+	 * @param deletedDateTime the new deleted date time
+	 */
+	public void setDeletedDateTime(LocalDateTime deletedDateTime) {
+		this.deletedDateTime = deletedDateTime;
 	}
 
 }
