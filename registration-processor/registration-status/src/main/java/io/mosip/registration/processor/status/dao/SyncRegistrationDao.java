3d52988a11a81ac71a17c99e7e639a7e475de3aa
Girish Yarru <M1048219@mindtree.com>
2018-10-05 12:06:36 +0530

    MOS-1030 : Added Dao class for Syncregistration
    Former-commit-id: c8c7bbae7d2bdbd016777a34267d1e7b2ed68a72
    Former-commit-id: 695553818d750c60750195d46e97b39813908b07

--- registration-processor/registration-status/src/main/java/io/mosip/registration/processor/status/dao/SyncRegistrationDao.java
+++ registration-processor/registration-status/src/main/java/io/mosip/registration/processor/status/dao/SyncRegistrationDao.java
@@ -1,6 +1,6 @@
 package io.mosip.registration.processor.status.dao;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,16 +8,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import io.mosip.registration.processor.status.entity.RegistrationStatusEntity;
 import io.mosip.registration.processor.status.entity.SyncRegistrationEntity;
-import io.mosip.registration.processor.status.repositary.RegistrationStatusRepositary;
 import io.mosip.registration.processor.status.repositary.SyncRegistrationRepository;
 
 
 /**
- * The Class RegistrationStatusDao.
+ * The Class SyncRegistrationDao.
  *
- * @author Horteppa
+ * @author Girish Yarru
  */
 @Component
 public class SyncRegistrationDao {
@@ -53,11 +51,11 @@ public class SyncRegistrationDao {
 	/** The Constant ISDELETED_COLON. */
 	public static final String ISDELETED_COLON = ".isDeleted=:";
 
+
 	/**
 	 * Save.
 	 *
-	 * @param syncRegistrationEntity
-	 *            the registration status entity
+	 * @param syncRegistrationEntity the sync registration entity
 	 * @return the sync registration entity
 	 */
 	public SyncRegistrationEntity save(SyncRegistrationEntity syncRegistrationEntity) {
@@ -65,11 +63,11 @@ public class SyncRegistrationDao {
 		return syncRegistrationRepository.save(syncRegistrationEntity);
 	}
 
+
 	/**
 	 * Update.
 	 *
-	 * @param syncRegistrationEntity
-	 *            the registration status entity
+	 * @param syncRegistrationEntity the sync registration entity
 	 * @return the sync registration entity
 	 */
 	public SyncRegistrationEntity update(SyncRegistrationEntity syncRegistrationEntity) {
@@ -78,15 +76,30 @@ public class SyncRegistrationDao {
 	}
 
 	/**
+	 * Adds the registrations.
+	 *
+	 * @param syncRegistrationEntityList the sync registration entity list
+	 * @return the list
+	 */
+	public List<SyncRegistrationEntity> addRegistrations(List<SyncRegistrationEntity> syncRegistrationEntityList) {
+		List<SyncRegistrationEntity> syncList = new ArrayList<>();
+		for(SyncRegistrationEntity synEntity: syncRegistrationEntityList) {
+			SyncRegistrationEntity syncRegistrationEntity = syncRegistrationRepository.save(synEntity);
+			syncList.add(syncRegistrationEntity);
+		}
+		return syncList;
+	}
+
+
+	/**
 	 * Find by id.
 	 *
-	 * @param registrationId
-	 *            the enrolment id
+	 * @param registrationId the registration id
 	 * @return the sync registration entity
 	 */
 	public SyncRegistrationEntity findById(String registrationId) {
 		Map<String, Object> params = new HashMap<>();
-		String className = RegistrationStatusEntity.class.getSimpleName();
+		String className = SyncRegistrationEntity.class.getSimpleName();
 
 		String alias = SyncRegistrationEntity.class.getName().toLowerCase().substring(0, 1);
 
@@ -104,81 +117,6 @@ public class SyncRegistrationDao {
 		return !syncRegistrationEntityList.isEmpty() ? syncRegistrationEntityList.get(0) : null;
 	}
 
-	/**
-	 * Findbyfiles by threshold.
-	 *
-	 * @param statusCode
-	 *            the status code
-	 * @param threshholdTime
-	 *            the threshhold time
-	 * @return the list
-	 */
-	public List<SyncRegistrationEntity> findbyfilesByThreshold(String statusCode, int threshholdTime) {
-		Map<String, Object> params = new HashMap<>();
-		String className = SyncRegistrationEntity.class.getSimpleName();
 
-		String alias = SyncRegistrationEntity.class.getName().toLowerCase().substring(0, 1);
-		LocalDateTime localDateTime = LocalDateTime.now();
-
-		LocalDateTime expiredDateTime = localDateTime.minusMinutes(threshholdTime);
-		String queryStr = SELECT_DISTINCT + alias + FROM + className + EMPTY_STRING + alias + WHERE + alias
-				+ ".statusCode=:statusCode" + EMPTY_STRING + AND + EMPTY_STRING + alias + ".updateDateTime < :date"
-				+ EMPTY_STRING + AND + EMPTY_STRING + alias + ISACTIVE_COLON + ISACTIVE + EMPTY_STRING + AND
-				+ EMPTY_STRING + alias + ISDELETED_COLON + ISDELETED;
-
-		params.put("statusCode", statusCode);
-		params.put("date", expiredDateTime);
-		params.put(ISACTIVE, Boolean.TRUE);
-		params.put(ISDELETED, Boolean.FALSE);
-
-		return syncRegistrationRepository.createQuerySelect(queryStr, params);
-	}
-
-	/**
-	 * Gets the registration sync status by status code.
-	 *
-	 * @param status
-	 *            the status
-	 * @return the registration sync status by status code
-	 */
-	public List<SyncRegistrationEntity> getEnrolmentStatusByStatusCode(String status) {
-
-		Map<String, Object> params = new HashMap<>();
-		String className = RegistrationStatusEntity.class.getSimpleName();
-
-		String alias = RegistrationStatusEntity.class.getName().toLowerCase().substring(0, 1);
-
-		String queryStr = SELECT_DISTINCT + alias + FROM + className + EMPTY_STRING + alias + WHERE + alias
-				+ ".statusCode=:statusCode" + EMPTY_STRING + AND + EMPTY_STRING + alias + ISACTIVE_COLON + ISACTIVE
-				+ EMPTY_STRING + AND + EMPTY_STRING + alias + ISDELETED_COLON + ISDELETED;
-
-		params.put("statusCode", status);
-		params.put(ISACTIVE, Boolean.TRUE);
-		params.put(ISDELETED, Boolean.FALSE);
-		return syncRegistrationRepository.createQuerySelect(queryStr, params);
-	}
-
-	/**
-	 * Gets the by ids.
-	 *
-	 * @param ids
-	 *            the ids
-	 * @return the by ids
-	 */
-	public List<SyncRegistrationEntity> getByIds(List<String> ids) {
-
-		Map<String, Object> params = new HashMap<>();
-		String className = SyncRegistrationEntity.class.getSimpleName();
-
-		String alias = SyncRegistrationEntity.class.getName().toLowerCase().substring(0, 1);
-		String queryStr = SELECT_DISTINCT + alias + FROM + className + EMPTY_STRING + alias + WHERE + alias
-				+ ".registrationId IN :ids" + EMPTY_STRING + AND + EMPTY_STRING + alias + ISACTIVE_COLON + ISACTIVE
-				+ EMPTY_STRING + AND + EMPTY_STRING + alias + ISDELETED_COLON + ISDELETED;
-		params.put("ids", ids);
-		params.put(ISACTIVE, Boolean.TRUE);
-		params.put(ISDELETED, Boolean.FALSE);
-
-		return syncRegistrationRepository.createQuerySelect(queryStr, params);
-	}
 
 }
