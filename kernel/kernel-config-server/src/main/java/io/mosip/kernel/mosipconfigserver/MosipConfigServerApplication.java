2de0e138572de23aafc97a234bc84bd5790150c9
SwatiRaj <swati.raj2@mindtree.com>
2018-10-09 14:31:03 +0530

    Added documentation for main class and changed package name.
    Former-commit-id: e36ec4b046060b554ebcad9e6fcc1bedec48bd23
    Former-commit-id: 7cee014e81e889fa424bedac3e37757b3e6c10ef

--- kernel/kernel-config-server/src/main/java/io/mosip/kernel/mosipconfigserver/MosipConfigServerApplication.java
+++ kernel/kernel-config-server/src/main/java/io/mosip/kernel/mosipconfigserver/MosipConfigServerApplication.java
@@ -1,4 +1,4 @@
-package io.mosip.mosipconfigserver;
+package io.mosip.kernel.mosipconfigserver;
 
 
 import org.springframework.boot.SpringApplication;
@@ -6,14 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
+ * Config Server application
  * 
  * @author Swati Raj
+ * @since 1.0.0
  *
  */
 @EnableConfigServer
 @SpringBootApplication
 public class MosipConfigServerApplication {
-
+  
+	/**
+	 * Main method to run spring boot application
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MosipConfigServerApplication.class, args);
 	}
