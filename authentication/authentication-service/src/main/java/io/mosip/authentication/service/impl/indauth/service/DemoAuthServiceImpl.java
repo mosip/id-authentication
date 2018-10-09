67840a12ceadc9949dd8b2a6c73b3f2374c1a567
Rakesh Roshan <Rakesh.Roshan@mindtree.com>
2018-10-09 11:09:16 +0530

    update import package DemoAuthServiceImpl
    Former-commit-id: 9b14366724c060415958f750479499da2b85312c
    Former-commit-id: 9c0334a0e45dbc2d920650edf802da1a0cace183

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/service/DemoAuthServiceImpl.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/service/DemoAuthServiceImpl.java
@@ -6,28 +6,28 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import io.mosip.authentication.core.dto.indauth.AuthRequestDTO;
-import io.mosip.authentication.core.dto.indauth.PersonalFullAddressDTO;
+import io.mosip.authentication.core.spi.idauth.demo.PersonalFullAddressDTO;
 import io.mosip.authentication.core.spi.indauth.service.DemoAuthService;
 import io.mosip.authentication.service.impl.indauth.service.demo.DemoMatchType;
 import io.mosip.authentication.service.impl.indauth.service.demo.MatchInput;
 
 @Service
 public class DemoAuthServiceImpl implements DemoAuthService {
-	
-	public List<MatchInput> constructMatchInput(AuthRequestDTO authRequestDTO){
-		
-		List<MatchInput> listMatchInput=new ArrayList<>();
-		PersonalFullAddressDTO fad=authRequestDTO.getPersonalDataDTO().getDemoDTO().getPersonalFullAddressDTO();
-		if(null!=fad) {
-			
-			MatchInput matchInput=new MatchInput
-					(DemoMatchType.ADDR_PRI,authRequestDTO.getPersonalDataDTO().getDemoDTO().getPersonalFullAddressDTO().getMsPri(),authRequestDTO.getPersonalDataDTO().getDemoDTO().getPersonalFullAddressDTO().getMtPri());
+
+	public List<MatchInput> constructMatchInput(AuthRequestDTO authRequestDTO) {
+
+		List<MatchInput> listMatchInput = new ArrayList<>();
+		PersonalFullAddressDTO fad = authRequestDTO.getPersonalDataDTO().getDemoDTO().getPersonalFullAddressDTO();
+		if (null != fad) {
+
+			MatchInput matchInput = new MatchInput(DemoMatchType.ADDR_PRI,
+					authRequestDTO.getPersonalDataDTO().getDemoDTO().getPersonalFullAddressDTO().getMsPri(),
+					authRequestDTO.getPersonalDataDTO().getDemoDTO().getPersonalFullAddressDTO().getMtPri());
 			listMatchInput.add(matchInput);
-			
-			//FIX it for secondary
+
+			// FIX it for secondary
 		}
-		return  listMatchInput;
-		
-		
+		return listMatchInput;
+
 	}
 }
