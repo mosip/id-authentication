661788b361b28c9036f09bd31d10d3d9b1c805e3
Manoj SP <Manoj.SP@mindtree.com>
2018-10-10 19:58:34 +0530

    Reverting Other Matcher
    Former-commit-id: de20cabb24ddf3f705fec326539e1eb82641b7e3
    Former-commit-id: cc1aa2e13776b6d6a8b336f5e648d6a2aaf0d89e

---
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/service/demo/EmailMatchingStrategy.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package io.mosip.authentication.service.impl.indauth.service.demo;
+
+import io.mosip.authentication.core.util.MatcherUtil;
+
+/**
+ * @author M1047395
+ *
+ */
+public enum EmailMatchingStrategy implements MatchingStrategy {
+	
+	EXACT(MatchStrategyType.EXACT, (reqInfo, entityInfo) -> {
+		if (reqInfo instanceof String && entityInfo instanceof String) {
+			return MatcherUtil.doExactMatch((String) reqInfo, (String) entityInfo);
+		} else {
+			return 0;
+		}
+	});
+	
+	private final MatchFunction matchFunction;
+
+	private final MatchStrategyType matchStrategyType;
+
+	/**
+	 * 
+	 * @param matchStrategyType
+	 * @param matchValue
+	 * @param matchFunction
+	 */
+	private EmailMatchingStrategy(MatchStrategyType matchStrategyType, MatchFunction matchFunction) {
+		this.matchFunction = matchFunction;
+		this.matchStrategyType = matchStrategyType;
+	}
+
+	@Override
+	public MatchStrategyType getType() {
+		return matchStrategyType;
+	}
+
+	@Override
+	public MatchFunction getMatchFunction() {
+		return matchFunction;
+	}
+
+}
