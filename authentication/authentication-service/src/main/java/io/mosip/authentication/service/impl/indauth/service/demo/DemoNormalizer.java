c7ffcf4ee865580ac157537c2909d71684a1d6b6
Rakesh Roshan <Rakesh.Roshan@mindtree.com>
2018-10-11 12:34:32 +0530

    DemoNormalizer
    Former-commit-id: d70741f0ee14dcac58060f1b076fc083a2b25d5e
    Former-commit-id: 5a5618e1fe840e64b5c04003b67cf70f9e33946e

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/service/demo/DemoNormalizer.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/service/demo/DemoNormalizer.java
@@ -1,16 +1,43 @@
 package io.mosip.authentication.service.impl.indauth.service.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+/**
+ * Generic class to normalize individual name, address.
+ *
+ * @author Rakesh Roshan
+ */
+@PropertySource(value = { "classpath:application-local.properties" })
 public final class DemoNormalizer {
+
+	@Autowired
+	static Environment env;
+
+	private static final String REGEX_SPECIAL_CHARACTERS = env.getProperty("Normalizer.special.characters");
+	private static final String REGEX_SALUTATION = env.getProperty("Normalizer.salutations");
+	private static final String REGEX_CARE_OF_LABLE = env.getProperty("Normalizer.careOfLables");
+	private static final String OTHER = env.getProperty("Normalizer.other");
+	private static final String REGEX_WHITE_SPACE = env.getProperty("Normalizer.whiteSpace");
 
 	private DemoNormalizer() {
 
 	}
 
 	public static String normalizeName(String name) {
+
+		name = name.replaceAll(REGEX_SALUTATION, "").replaceAll(REGEX_SPECIAL_CHARACTERS, " ")
+				.replaceAll(REGEX_WHITE_SPACE, " ").trim();
+
 		return name;
 	}
 
 	public static String normalizeAddress(String address) {
+
+		address = address.replace(REGEX_CARE_OF_LABLE, "").replaceAll(REGEX_SPECIAL_CHARACTERS, " ")
+				.replaceAll(OTHER, "").replaceAll(REGEX_WHITE_SPACE, " ").trim();
+
 		return address;
 	}
 
