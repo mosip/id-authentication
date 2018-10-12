7055b528659b506e68f71a84eb8e875d1f03c439
M1044542 <M1044542@A2ML27062.mindtree.com>
2018-10-12 18:24:09 +0530

    Excluded JpaAutoConfiguration
    Former-commit-id: dbc71bc12516df916fe56dc77be2a25ef51c861e
    Former-commit-id: f7eca683f505b2f83796418ed9c9e2fb70a32639

--- kernel/kernel-notification-email/src/main/java/io/mosip/kernel/emailnotifier/NotificationEmailApplication.java
+++ kernel/kernel-notification-email/src/main/java/io/mosip/kernel/emailnotifier/NotificationEmailApplication.java
@@ -1,10 +1,14 @@
 package io.mosip.kernel.emailnotifier;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @EnableAsync
 public class NotificationEmailApplication {
 	public static void main(String[] args) {
