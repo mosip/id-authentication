3edf966f46fdcb85aee17a8d901ee78c14377773
Dinesh Karuppiah <dineshkaruppiah.thiagarajan@mindtree.com>
2018-10-12 16:33:38 +0530

    Added DemoMatchTypeTest class
    Former-commit-id: 4207e81ec181d8cb49bb575ac82b39ad72db17ac
    Former-commit-id: 9115a97a0d0e59356325fa087b544f7fd2381778

---
+++ authentication/authentication-service/src/test/java/io/mosip/authentication/service/impl/indauth/service/demo/DemoMatchTypeTest.java
@@ -0,0 +1,376 @@
+package io.mosip.authentication.service.impl.indauth.service.demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import io.mosip.authentication.core.dto.indauth.AuthUsageDataBit;
+import io.mosip.authentication.core.dto.indauth.DemoDTO;
+import io.mosip.authentication.core.spi.idauth.demo.PersonalAddressDTO;
+import io.mosip.authentication.core.spi.idauth.demo.PersonalFullAddressDTO;
+import io.mosip.authentication.core.spi.idauth.demo.PersonalIdentityDTO;
+
+public class DemoMatchTypeTest {
+
+	@Test
+	public void TestPriAddressisNotNull() {
+		assertNotNull(DemoMatchType.ADDR_PRI.getAllowedMatchingStrategy(FullAddressMatchingStrategy.EXACT.getType()));
+		assertNotNull(DemoMatchType.ADDR_PRI.getMatchedBit());
+		assertNotNull(DemoMatchType.ADDR_PRI.getMatchedBit());
+	}
+
+	@Test
+	public void TestValidPriAddressExact() {
+		Optional<MatchingStrategy> matchStrategy = DemoMatchType.ADDR_PRI
+				.getAllowedMatchingStrategy(FullAddressMatchingStrategy.EXACT.getType());
+		assertEquals(matchStrategy.get(), FullAddressMatchingStrategy.EXACT);
+	}
+
+	@Test
+	public void TestAuthUsageDataBit() {
+		Map<AuthUsageDataBit, Long> bitsCountMap = Arrays.stream(DemoMatchType.values()).map(dmt -> dmt.getUsedBit())
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+//		assertEquals(AuthUsageDataBit.values().length, bitsCountMap.size());
+		assertTrue(bitsCountMap.values().stream().allMatch(c -> c == 1));
+	}
+
+	@Test
+	public void TestAuthUsageMatchedBit() {
+		Map<AuthUsageDataBit, Long> bitsCountMap = Arrays.stream(DemoMatchType.values()).map(dmt -> dmt.getMatchedBit())
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+//		assertEquals(AuthUsageDataBit.values().length, bitsCountMap.size());
+		assertTrue(bitsCountMap.values().stream().allMatch(c -> c == 1));
+	}
+
+	@Test
+	public void TestInvalidPriAddressExact() {
+		Optional<MatchingStrategy> matchStrategy = DemoMatchType.ADDR_PRI
+				.getAllowedMatchingStrategy(FullAddressMatchingStrategy.EXACT.getType());
+		assertNotEquals(matchStrategy.get(), FullAddressMatchingStrategy.PARTIAL);
+	}
+
+	@Test
+	public void TestPriAddressDemoInfoFetcherisNotNull() {
+		assertNotNull(DemoMatchType.ADDR_PRI);
+	}
+
+	@Test
+	public void TestPriAddressDemoInfoFetcher() {
+		String tmpAddress = "no 11 gandhi street";
+		DemoDTO demodto = new DemoDTO();
+		PersonalFullAddressDTO personalFullAddressDTO = new PersonalFullAddressDTO();
+		personalFullAddressDTO.setAddrPri("no 11 gandhi street");
+		demodto.setLangPri("english");
+		demodto.setPersonalFullAddressDTO(personalFullAddressDTO);
+		assertEquals(tmpAddress, DemoMatchType.ADDR_PRI.getDemoInfoFetcher().getInfo(demodto));
+		assertNotEquals("invalid", DemoMatchType.ADDR_PRI.getDemoInfoFetcher().getInfo(demodto));
+	}
+
+	@Test
+	public void TestFullAddress() {
+		String tmpAddress = "no 1 gandhi street chennai india 600111";
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setAddrLine1("no 1");
+		demoEntity.setAddrLine2("gandhi street");
+		demoEntity.setAddrLine3("chennai");
+		demoEntity.setNationalId("india");
+		demoEntity.setLocationCode("600111");
+		assertEquals(tmpAddress, DemoMatchType.ADDR_PRI.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("invalid address", DemoMatchType.ADDR_PRI.getEntityInfoFetcher().getInfo(demoEntity));
+	}
+
+	@Test
+	public void TestPriAddressPartialisNotNull() {
+		assertNotNull(DemoMatchType.ADDR_PRI.getAllowedMatchingStrategy(FullAddressMatchingStrategy.PARTIAL.getType()));
+	}
+
+	@Test
+	public void TestValidPriAddressPartial() {
+		Optional<MatchingStrategy> matchStrategy = DemoMatchType.ADDR_PRI
+				.getAllowedMatchingStrategy(FullAddressMatchingStrategy.PARTIAL.getType());
+		assertEquals(matchStrategy.get(), FullAddressMatchingStrategy.PARTIAL);
+
+	}
+
+	@Test
+	public void TestInvalidPriAddressPartial() {
+		Optional<MatchingStrategy> matchStrategy = DemoMatchType.ADDR_PRI
+				.getAllowedMatchingStrategy(FullAddressMatchingStrategy.PARTIAL.getType());
+		assertNotEquals(matchStrategy.get(), FullAddressMatchingStrategy.EXACT);
+	}
+
+	@Test
+	public void TestSecondaryAddressisNotNull() {
+		assertNotNull(DemoMatchType.ADDR_SEC);
+	}
+
+	@Test
+	public void TestSecondaryAddressStrategy() {
+		String tmpAddress = "no 1 gandhi street chennai india 600111";
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setAddrLine1("no 1");
+		demoEntity.setAddrLine2("gandhi street");
+		demoEntity.setAddrLine3("chennai");
+		demoEntity.setNationalId("india");
+		demoEntity.setLocationCode("600111");
+		assertEquals(tmpAddress, DemoMatchType.ADDR_SEC.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("invalid address", DemoMatchType.ADDR_SEC.getEntityInfoFetcher().getInfo(demoEntity));
+	}
+
+	@Test
+	public void TestSecondaryAddressStrategywithValid() {
+
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setAddrLine1("No 2");
+		demoEntity.setAddrLine2("gandhi");
+		demoEntity.setAddrLine3("street");
+		demoEntity.setNationalId("india");
+		demoEntity.setLocationCode("600111");
+		DemoDTO demoDTO = new DemoDTO();
+		PersonalFullAddressDTO personalFullAddressDTO = new PersonalFullAddressDTO();
+		personalFullAddressDTO.setAddrSec("No 2 gandhi street india 600111");
+		demoDTO.setPersonalFullAddressDTO(personalFullAddressDTO);
+		assertEquals(DemoMatchType.ADDR_SEC.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.ADDR_SEC.getEntityInfoFetcher().getInfo(demoEntity));
+
+		assertNotEquals("ganesh", DemoMatchType.ADDR_SEC.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestValidpriNameType() {
+		Optional<MatchingStrategy> matchStrategy = DemoMatchType.NAME_PRI
+				.getAllowedMatchingStrategy(NameMatchingStrategy.EXACT.getType());
+		assertEquals(matchStrategy.get(), NameMatchingStrategy.EXACT);
+	}
+
+	@Test
+	public void TestInValidpriNameType() {
+		Optional<MatchingStrategy> matchStrategy = DemoMatchType.NAME_PRI
+				.getAllowedMatchingStrategy(NameMatchingStrategy.EXACT.getType());
+		assertNotEquals(matchStrategy.get(), NameMatchingStrategy.PARTIAL);
+	}
+
+	@Test
+	public void TestpriName() {
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setFirstName("dinesh");
+		demoEntity.setMiddleName("karuppiah");
+		demoEntity.setLastName("thiagarajan");
+		DemoDTO demoDTO = new DemoDTO();
+		PersonalIdentityDTO personalIdentityDTO = new PersonalIdentityDTO();
+		personalIdentityDTO.setNamePri("dinesh karuppiah thiagarajan");
+		demoDTO.setPersonalIdentityDTO(personalIdentityDTO);
+		assertEquals(DemoMatchType.NAME_PRI.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.NAME_PRI.getEntityInfoFetcher().getInfo(demoEntity));
+
+		assertNotEquals("ganesh", DemoMatchType.NAME_PRI.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestSecondaryNameisNotNull() {
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setFirstName("dinesh");
+		demoEntity.setMiddleName("karuppiah");
+		demoEntity.setLastName("thiagarajan");
+		DemoDTO demoDTO = new DemoDTO();
+		PersonalIdentityDTO personalIdentityDTO = new PersonalIdentityDTO();
+		personalIdentityDTO.setNameSec("dinesh karuppiah thiagarajan");
+		demoDTO.setPersonalIdentityDTO(personalIdentityDTO);
+		assertEquals(DemoMatchType.NAME_SEC.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.NAME_SEC.getEntityInfoFetcher().getInfo(demoEntity));
+
+		assertNotEquals("ganesh", DemoMatchType.NAME_SEC.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestGenderMatchStrategyisNotNull() {
+		assertNotNull(GenderMatchingStrategy.EXACT);
+	}
+
+	@Test
+	public void TestGenderMatchStrategy() {
+		PersonalIdentityDTO personalIdentityDTO = new PersonalIdentityDTO();
+		personalIdentityDTO.setGender("male");
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalIdentityDTO(personalIdentityDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setGenderCode("male");
+		assertEquals(DemoMatchType.GENDER.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.GENDER.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("female", DemoMatchType.GENDER.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestAgeMatchStrategy() {
+		PersonalIdentityDTO personalIdentityDTO = new PersonalIdentityDTO();
+		personalIdentityDTO.setAge(20);
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalIdentityDTO(personalIdentityDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setAge(20);
+		assertEquals(DemoMatchType.AGE.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.AGE.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals(40, DemoMatchType.AGE.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestDOBMatchStrategy() throws ParseException {
+		LocalDateTime localDate = LocalDateTime.of(1998, 01, 21, 07, 22, 11);
+		long epochSecond = localDate.toEpochSecond(ZoneOffset.UTC);
+		Date reqDate = new Date(epochSecond);
+		LocalDateTime localDate2 = LocalDateTime.of(1998, 01, 21, 07, 22, 11);
+		long entitiyDateEpochSecond = localDate2.toEpochSecond(ZoneOffset.UTC);
+		Date entityDate = new Date(entitiyDateEpochSecond);
+		PersonalIdentityDTO personalIdentityDTO = new PersonalIdentityDTO();
+		personalIdentityDTO.setDob(reqDate.toString());
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalIdentityDTO(personalIdentityDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setDob(entityDate);
+		assertEquals(DemoMatchType.DOB.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.DOB.getEntityInfoFetcher().getInfo(demoEntity).toString());
+		assertNotEquals(localDate2, DemoMatchType.DOB.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestMobileStrategyisNull() {
+		assertNotNull(DemoMatchType.MOBILE);
+	}
+
+	@Test
+	public void TestMobileMatchStrategy() {
+		PersonalIdentityDTO personalIdentityDTO = new PersonalIdentityDTO();
+		personalIdentityDTO.setPhone("1234567890");
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalIdentityDTO(personalIdentityDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setMobile("1234567890");
+		assertEquals(DemoMatchType.MOBILE.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.MOBILE.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals(40, DemoMatchType.MOBILE.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestEmailStrategyisNotNull() {
+		assertNotNull(DemoMatchType.EMAIL);
+	}
+
+	@Test
+	public void TestEmailStrategy() {
+		PersonalIdentityDTO personalIdentityDTO = new PersonalIdentityDTO();
+		personalIdentityDTO.setEmail("test@test.com");
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalIdentityDTO(personalIdentityDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setEmail("test@test.com");
+		assertEquals(DemoMatchType.EMAIL.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.EMAIL.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("test@Test1.com", DemoMatchType.EMAIL.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestAddrLine1isNotnull() {
+		assertNotNull(DemoMatchType.ADDR_LINE1_PRI);
+	}
+
+	@Test
+	public void TestAddrLine1Strategy() {
+		PersonalAddressDTO personalAddressDTO = new PersonalAddressDTO();
+		personalAddressDTO.setAddrLine1Pri("no1 gandhi street");
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalAddressDTO(personalAddressDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setAddrLine1("no1 gandhi street");
+		assertEquals(DemoMatchType.ADDR_LINE1_PRI.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.ADDR_LINE1_PRI.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("invalid address", DemoMatchType.ADDR_LINE1_PRI.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestAddrLine2isNotnull() {
+		assertNotNull(DemoMatchType.ADDR_LINE2_PRI);
+	}
+
+	@Test
+	public void TestAddrLine2Strategy() {
+		PersonalAddressDTO personalAddressDTO = new PersonalAddressDTO();
+		personalAddressDTO.setAddrLine2Pri("kamarajapuram");
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalAddressDTO(personalAddressDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setAddrLine2("kamarajapuram");
+		assertEquals(DemoMatchType.ADDR_LINE2_PRI.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.ADDR_LINE2_PRI.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("invalid address", DemoMatchType.ADDR_LINE2_PRI.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestAddrLine3isNotnull() {
+		assertNotNull(DemoMatchType.ADDR_LINE3_PRI);
+	}
+
+	@Test
+	public void TestAddrLine3Strategy() {
+		PersonalAddressDTO personalAddressDTO = new PersonalAddressDTO();
+		personalAddressDTO.setAddrLine3Pri("chennai");
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalAddressDTO(personalAddressDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setAddrLine3("chennai");
+		assertEquals(DemoMatchType.ADDR_LINE3_PRI.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.ADDR_LINE3_PRI.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("invalid address", DemoMatchType.ADDR_LINE3_PRI.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestCountryStrategyisNotNull() {
+		assertNotNull(DemoMatchType.COUNTRY_PRI);
+	}
+
+	@Test
+	public void TestCountryStrategy() {
+		PersonalAddressDTO personalAddressDTO = new PersonalAddressDTO();
+		personalAddressDTO.setCountryPri("india");
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalAddressDTO(personalAddressDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setNationalId("india");
+		assertEquals(DemoMatchType.COUNTRY_PRI.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.COUNTRY_PRI.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("invalid address", DemoMatchType.COUNTRY_PRI.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+	@Test
+	public void TestPincodeStrategyisNotNull() {
+		assertNotNull(DemoMatchType.PINCODE_PRI);
+	}
+
+	@Test
+	public void TestPincodeStrategy() {
+		PersonalAddressDTO personalAddressDTO = new PersonalAddressDTO();
+		personalAddressDTO.setPinCodePri("600117");
+		DemoDTO demoDTO = new DemoDTO();
+		demoDTO.setPersonalAddressDTO(personalAddressDTO);
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setLocationCode("600117");
+		assertEquals(DemoMatchType.PINCODE_PRI.getDemoInfoFetcher().getInfo(demoDTO),
+				DemoMatchType.PINCODE_PRI.getEntityInfoFetcher().getInfo(demoEntity));
+		assertNotEquals("600000", DemoMatchType.PINCODE_PRI.getDemoInfoFetcher().getInfo(demoDTO));
+	}
+
+}
