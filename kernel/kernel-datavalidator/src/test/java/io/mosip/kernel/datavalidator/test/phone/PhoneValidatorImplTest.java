c185763e1a2a43342ad06d5eaa89c3bf6800abaa
Raj <M1026343@mindtree.com>
2018-10-12 14:59:53 +0530

    Merge branch 'DEV_SPRINT4_MEGHA_MOS-1029' into DEV
    Former-commit-id: 75997bc4f560968beecfdebf84be97dabdeb4bff
    Former-commit-id: 132107169d621fc88a44e1eba848fc7808ffe1d2

---
+++ kernel/kernel-datavalidator/src/test/java/io/mosip/kernel/datavalidator/test/phone/PhoneValidatorImplTest.java
@@ -0,0 +1,100 @@
+
+/**
+ * 
+ */
+package io.mosip.kernel.datavalidator.test.phone;
+
+/**
+ * 
+ */
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.mosip.kernel.datavalidator.phone.exception.InvalidPhoneNumberException;
+import io.mosip.kernel.datavalidator.phone.impl.PhoneValidatorImpl;
+
+/**
+ * Test class for testing PhoneValidator class
+ * 
+ * @author Megha Tanga
+ * @since 1.0.0
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhoneValidatorImplTest {
+
+	@Autowired
+	PhoneValidatorImpl phonevalidator;
+	
+	@Value("${mosip.kernel.phone.test.null}")
+	String phoneNull;
+
+	@Value("${mosip.kernel.phone.test.true}")
+	String phoneTrue;
+
+	@Value("${mosip.kernel.phone.test.empty}")
+	String phoneEmpty;
+
+	@Value("${mosip.kernel.phone.test.max-length}")
+	String phoneMaxLength;
+
+	@Value("${mosip.kernel.phone.test.min-length}")
+	String phoneMinLength;
+
+	@Value("${mosip.kernel.phone.test.alphanumaric}")
+	String phoneAlpanumaric;
+
+	@Value("${mosip.kernel.phone.test.specialchar}")
+	String phoneSpecialChar;
+
+	@Test
+	public void phoneValidatorTestTrue() {
+		assertEquals(true, phonevalidator.validatePhone(phoneTrue));
+	}
+
+	@Test(expected = InvalidPhoneNumberException.class)
+	public void testNull() throws InvalidPhoneNumberException {
+
+		phonevalidator.validatePhone(phoneNull);
+	}
+
+	@Test(expected = InvalidPhoneNumberException.class)
+	public void testEmpty() throws InvalidPhoneNumberException {
+
+		phonevalidator.validatePhone(phoneEmpty);
+	}
+
+	@Test(expected = InvalidPhoneNumberException.class)
+	public void testMaxLenght() throws InvalidPhoneNumberException {
+
+		phonevalidator.validatePhone(phoneMaxLength);
+	}
+
+	@Test(expected = InvalidPhoneNumberException.class)
+	public void testMinLenght() throws InvalidPhoneNumberException {
+
+		phonevalidator.validatePhone(phoneMinLength);
+	}
+
+	@Test(expected = InvalidPhoneNumberException.class)
+	public void testAlphanumeric() throws InvalidPhoneNumberException {
+
+		phonevalidator.validatePhone(phoneAlpanumaric);
+	}
+
+	@Test(expected = InvalidPhoneNumberException.class)
+	public void testSpecialChar() throws InvalidPhoneNumberException {
+
+		phonevalidator.validatePhone(phoneSpecialChar);
+	}
+
+}
