fee89b5e5018ce4022879b7e3fd51d0f3b49a33f
M1044542 <M1044542@A2ML27062.mindtree.com>
2018-10-15 12:34:44 +0530

    Added comment in main application
    Former-commit-id: 681197c1993f1b460a04f1ff662bb9cdb5023bbd
    Former-commit-id: 614b0a6c3dd7eb6366ddc226927620820c4e93cc

--- kernel/kernel-notification-email/src/main/java/io/mosip/kernel/emailnotifier/NotificationEmailApplication.java
+++ kernel/kernel-notification-email/src/main/java/io/mosip/kernel/emailnotifier/NotificationEmailApplication.java
@@ -7,10 +7,23 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * Mail notifier application
+ * 
+ * @author Sagar Mahapatra
+ * @since 1.0.0
+ *
+ */
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @EnableAsync
 public class NotificationEmailApplication {
+	
+	/**
+	 * Main method to run spring boot application
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationEmailApplication.class, args);
 	}
