56931989deecabd58f104b134bd66efeca949403
M1046559 <M1046559@b2ml27198.mindtree.com>
2018-10-16 15:39:50 +0530

    Entity for User machine mapping. Error Page.
    Former-commit-id: 4206f7a5c37f6ed60d1e1774eec806a9177e410c
    Former-commit-id: b53b7210509631e673deab86bc685b6b6555f09f

---
+++ registration/registration-service/src/main/java/io/mosip/registration/entity/UserMachineMapping.java
@@ -0,0 +1,95 @@
+package io.mosip.registration.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "reg", name = "reg_center_user_machine")
+public class UserMachineMapping extends RegistrationCommonFields implements Serializable {
+
+	/**
+	 * serial Version UID
+	 */
+	private static final long serialVersionUID = 8686723876595925323L;
+
+	@EmbeddedId
+	@Column(name = "pk_cntrum_usr_id")
+	private UserMachineMappingID userMachineMappingId;
+
+	@ManyToOne
+	@JoinColumn(name = "usr_id", nullable = false, insertable = false, updatable = false)
+	private RegistrationUserDetail registrationUserDetail;
+
+	@Column(name = "is_deleted")
+	private boolean isDeleted;
+
+	@Column(name = "del_dtimesz")
+	private String deletedDateTime;
+
+	/**
+	 * @return the userMachineMappingId
+	 */
+	public UserMachineMappingID getUserMachineMappingId() {
+		return userMachineMappingId;
+	}
+
+	/**
+	 * @param userMachineMappingId
+	 *            the userMachineMappingId to set
+	 */
+	public void setUserMachineMappingId(UserMachineMappingID userMachineMappingId) {
+		this.userMachineMappingId = userMachineMappingId;
+	}
+
+	/**
+	 * @return the registrationUserDetail
+	 */
+	public RegistrationUserDetail getRegistrationUserDetail() {
+		return registrationUserDetail;
+	}
+
+	/**
+	 * @param registrationUserDetail
+	 *            the registrationUserDetail to set
+	 */
+	public void setRegistrationUserDetail(RegistrationUserDetail registrationUserDetail) {
+		this.registrationUserDetail = registrationUserDetail;
+	}
+
+	/**
+	 * @return the isDeleted
+	 */
+	public boolean isDeleted() {
+		return isDeleted;
+	}
+
+	/**
+	 * @param isDeleted
+	 *            the isDeleted to set
+	 */
+	public void setDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	/**
+	 * @return the deletedDateTime
+	 */
+	public String getDeletedDateTime() {
+		return deletedDateTime;
+	}
+
+	/**
+	 * @param deletedDateTime
+	 *            the deletedDateTime to set
+	 */
+	public void setDeletedDateTime(String deletedDateTime) {
+		this.deletedDateTime = deletedDateTime;
+	}
+
+}
---
+++ registration/registration-service/src/main/java/io/mosip/registration/entity/UserMachineMappingID.java
@@ -0,0 +1,73 @@
+package io.mosip.registration.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class UserMachineMappingID implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1883492292190913762L;
+
+	@Column(name = "usr_id", length = 32, nullable = false)
+	private String userID;
+	@Column(name = "cntr_id", length = 64, nullable = false)
+	private String centreID;
+	@Column(name = "machm_id", length = 64, nullable = false)
+	private String machineID;
+
+	/**
+	 * @return the userID
+	 */
+	public String getUserID() {
+		return userID;
+	}
+
+	/**
+	 * @param userID
+	 *            the userID to set
+	 */
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	/**
+	 * @return the centreID
+	 */
+	public String getCentreID() {
+		return centreID;
+	}
+
+	/**
+	 * @param centreID
+	 *            the centreID to set
+	 */
+	public void setCentreID(String centreID) {
+		this.centreID = centreID;
+	}
+
+	/**
+	 * @return the machineID
+	 */
+	public String getMachineID() {
+		return machineID;
+	}
+
+	/**
+	 * @param machineID
+	 *            the machineID to set
+	 */
+	public void setMachineID(String machineID) {
+		this.machineID = machineID;
+	}
+
+	@Override
+	public String toString() {
+		return "UserMachineMappingID [userID=" + userID + ", centreID=" + centreID + ", machineID=" + machineID + "]";
+
+	}
+
+}
