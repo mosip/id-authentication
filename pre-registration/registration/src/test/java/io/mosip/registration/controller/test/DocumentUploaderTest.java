6832b5e3ce157905828f9064730236b2d8907594
Kishan Rathore <M1043008@mindtree.com>
2018-10-16 10:18:43 +0530

    test cases update
    Former-commit-id: 1a4fdd465beaa11032823d6d3676ca2201abe009
    Former-commit-id: 283ffaa6220735a0442b1fe03db7e7b6cfdf0617

--- pre-registration/registration/src/test/java/io/mosip/registration/controller/test/DocumentUploaderTest.java
+++ pre-registration/registration/src/test/java/io/mosip/registration/controller/test/DocumentUploaderTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -29,13 +30,13 @@ import io.mosip.registration.dto.DocumentDto;
 import io.mosip.registration.service.DocumentUploadService;
 
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
+@WebMvcTest(DocumentUploaderTest.class)
 public class DocumentUploaderTest {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
+	
 	@Autowired
-	private WebApplicationContext webApplicationContext;
+	private MockMvc mockMvc;
 	
 	private MockMultipartFile multipartFile, jsonMultiPart;
 	
@@ -63,23 +64,21 @@ public class DocumentUploaderTest {
 		}
 		try {
 			multipartFile = new MockMultipartFile("file", "file-name.data", "text/plain", "some other type".getBytes());
-			//jsonMultiPart=new MockMultipartFile("jsonFile", "jsonFile", "mixed/multipart",new FileInputStream(jsonFile));
 			jsonMultiPart = new MockMultipartFile( "documentString","",  "application/json",  mapper.writeValueAsString(documentDto).getBytes());
 		} catch (IOException e) {
 			logger.error(e.getMessage());
 		}
 		
 		
-		Mockito.when(service.uploadDoucment(multipartFile, documentDto)).thenReturn(true);
+		Mockito.when(service.uploadDoucment(this.multipartFile, documentDto)).thenReturn(true);
 	}
 
 	@Test
 	public void successTest() throws Exception {
 
-		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 		
-		mockMvc.perform(MockMvcRequestBuilders.multipart("/upload")
-				.file(this.jsonMultiPart).file(this.multipartFile)).andExpect(status().is(200));
+		this.mockMvc.perform(MockMvcRequestBuilders.multipart("/v0.1/pre-registration/registration/document/upload")
+				.file(this.jsonMultiPart).file(this.multipartFile)).andExpect(status().isOk());
 
 	}
 
