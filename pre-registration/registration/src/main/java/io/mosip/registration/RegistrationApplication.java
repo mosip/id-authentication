5e2cf10bdb3b6b5b3c5a37c7df93739a56a9f736
Kishan Rathore <M1043008@mindtree.com>
2018-10-17 14:41:26 +0530

    test changes
    Former-commit-id: 750ae5fb2f29654f66318686f4bccce947df5f8c
    Former-commit-id: 36dd3c475c79a461b72061bcf85bef6d5103c3dd

--- pre-registration/registration/src/main/java/io/mosip/registration/RegistrationApplication.java
+++ pre-registration/registration/src/main/java/io/mosip/registration/RegistrationApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "io.mosip.*")
-@PropertySource({ "classpath:registration-application.properties" })
-@PropertySource({ "classpath:bootstrap.properties" })
-@PropertySource({ "classpath:core-application.properties" })
+@SpringBootApplication(scanBasePackages="io.mosip.*")
+@PropertySource({"classpath:registration-application.properties"})
+//@PropertySource({"classpath:application.properties"})
+@PropertySource({"classpath:core-application.properties"})
 public class RegistrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationApplication.class, args);
--- pre-registration/registration/src/test/java/io/mosip/registration/controller/test/RegistrationControllerTest.java
+++ pre-registration/registration/src/test/java/io/mosip/registration/controller/test/RegistrationControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -40,6 +41,7 @@ import io.mosip.registration.service.RegistrationService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RegistrationController.class)
+@TestPropertySource({ "classpath:registration-application.test.properties" })
 public class RegistrationControllerTest {
 	
 	@Autowired
