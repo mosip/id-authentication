eae1733de5464dec91250fe97b3adcaeafc58d8c
M1047487 <M1047487@A2ML29918.mindtree.com>
2018-10-23 17:22:58 +0530

    MOS-8367 added audit builder constants and method in registration-core module
    Former-commit-id: 73ac069d7724aeeb499d71b911f7ef9ee663722d
    Former-commit-id: a334912c2ba819e908af5f9e7b3f5875d27b202a

---
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/code/AuditLogConstant.java
@@ -0,0 +1,21 @@
+package io.mosip.registration.processor.core.code;
+
+/**
+ * The Enum AuditLogConstant.
+ * 
+ * @author Ranjitha
+ */
+public enum AuditLogConstant {
+
+	/** The application id. */
+	MOSIP_4,
+
+	/** The application name. */
+	REGISTRATION_PROCESSOR,
+
+	/** The created by and session userId. */
+	SYSTEM,
+
+	/** The reference id type. */
+	REGISTRATION_ID,
+}
---
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/code/EventId.java
@@ -0,0 +1,24 @@
+package io.mosip.registration.processor.core.code;
+
+/**
+ * The Enum EventId.
+ * 
+ * @author Ranjitha
+ */
+public enum EventId {
+	
+	/** Event Id **/
+	
+	RPR_401,
+	
+	RPR_402,
+	
+	RPR_403,
+
+	RPR_404,
+	
+	RPR_405,
+
+	RPR_406,
+
+}
