be48beb4ec265ba5c141d45bd099f13947144f7d
M1044292 <M1044292@b2ml27210.mindtree.com>
2018-10-23 11:09:17 +0530

    added changes for login with otp and onboard user/header part
    Former-commit-id: 078f9c884111aaaababb230f431fe1ab5cde8574
    Former-commit-id: 3453c74a7abd9314829bb5f4a2f8d4b4bd8f76a7

--- registration/registration-service/src/main/java/io/mosip/registration/service/LoginServiceImpl.java
+++ registration/registration-service/src/main/java/io/mosip/registration/service/LoginServiceImpl.java
@@ -10,12 +10,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 import io.mosip.kernel.core.spi.logger.MosipLogger;
-import io.mosip.kernel.core.util.JsonUtils;
-import io.mosip.kernel.core.util.exception.MosipIOException;
-import io.mosip.kernel.core.util.exception.MosipJsonMappingException;
-import io.mosip.kernel.core.util.exception.MosipJsonParseException;
 import io.mosip.kernel.logger.appender.MosipRollingFileAppender;
 import io.mosip.kernel.logger.factory.MosipLogfactory;
 import io.mosip.registration.audit.AuditFactory;
@@ -110,8 +107,8 @@ public class LoginServiceImpl implements LoginService {
 	public Map<String, Object> getModesOfLogin() {
 		// Retrieve Login information
 
-		LOGGER.debug("REGISTRATION - LOGINMODES - LOGINSERVICE", APPLICATION_NAME,
-				APPLICATION_ID, "Fetching list of login modes");
+		LOGGER.debug("REGISTRATION - LOGINMODES - LOGINSERVICE", APPLICATION_NAME, APPLICATION_ID,
+				"Fetching list of login modes");
 
 		auditFactory.audit(AuditEventEnum.LOGIN_MODES_FETCH, AppModuleEnum.LOGIN_MODES, "Fetching list of login modes",
 				"refId", "refIdType");
@@ -129,8 +126,8 @@ public class LoginServiceImpl implements LoginService {
 	public boolean validateUserPassword(String userId, String hashPassword) {
 		// Validating Registration Officer Credentials
 
-		LOGGER.debug("REGISTRATION - VALIDATECREDENTIALS - LOGINSERVICE", APPLICATION_NAME,
-				APPLICATION_ID, "Validating User credentials");
+		LOGGER.debug("REGISTRATION - VALIDATECREDENTIALS - LOGINSERVICE", APPLICATION_NAME, APPLICATION_ID,
+				"Validating User credentials");
 
 		auditFactory.audit(AuditEventEnum.VALIDATE_USER_CRED, AppModuleEnum.VALIDATE_USER,
 				"Validating User credentials", "refId", "refIdType");
@@ -149,8 +146,8 @@ public class LoginServiceImpl implements LoginService {
 	public RegistrationUserDetail getUserDetail(String userId) {
 		// Retrieving Registration Officer details
 
-		LOGGER.debug("REGISTRATION - USERDETAIL - LOGINSERVICE", APPLICATION_NAME,
-				APPLICATION_ID, "Fetching User details");
+		LOGGER.debug("REGISTRATION - USERDETAIL - LOGINSERVICE", APPLICATION_NAME, APPLICATION_ID,
+				"Fetching User details");
 
 		auditFactory.audit(AuditEventEnum.FETCH_USR_DET, AppModuleEnum.USER_DETAIL, "Fetching User details", "refId",
 				"refIdType");
@@ -168,8 +165,8 @@ public class LoginServiceImpl implements LoginService {
 	public RegistrationCenterDetailDTO getRegistrationCenterDetails(String centerId) {
 		// Retrieving Registration Center details
 
-		LOGGER.debug("REGISTRATION - CENTERDETAILS - LOGINSERVICE", APPLICATION_NAME,
-				APPLICATION_ID, "Fetching Center details");
+		LOGGER.debug("REGISTRATION - CENTERDETAILS - LOGINSERVICE", APPLICATION_NAME, APPLICATION_ID,
+				"Fetching Center details");
 
 		auditFactory.audit(AuditEventEnum.FETCH_CNTR_DET, AppModuleEnum.CENTER_DETAIL, "Fetching Center details",
 				"refId", "refIdType");
@@ -187,8 +184,8 @@ public class LoginServiceImpl implements LoginService {
 	public AuthorizationDTO getScreenAuthorizationDetails(String roleCode) {
 		// Fetching screen authorization details
 
-		LOGGER.debug("REGISTRATION - SCREENAUTHORIZATION - LOGINSERVICE", APPLICATION_NAME,
-				APPLICATION_ID, "Fetching list of Screens to be Authorized");
+		LOGGER.debug("REGISTRATION - SCREENAUTHORIZATION - LOGINSERVICE", APPLICATION_NAME, APPLICATION_ID,
+				"Fetching list of Screens to be Authorized");
 
 		auditFactory.audit(AuditEventEnum.FETCH_SCR_AUTH, AppModuleEnum.SCREEN_AUTH,
 				"Fetching list of Screens to be Authorized", "refId", "refIdType");
@@ -196,101 +193,69 @@ public class LoginServiceImpl implements LoginService {
 		return registrationScreenAuthorizationDAO.getScreenAuthorizationDetails(roleCode);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.mosip.registration.service.LoginService#getOTP(java.lang.String)
+	
+
+	/* (non-Javadoc)
+	 * @see io.mosip.registration.service.LoginService#getOTP(java.lang.String)
 	 */
 	@Override
-	public ResponseDTO getOTP(final String key) {
-		LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME, APPLICATION_ID,
-				"Get OTP method called");
+    public ResponseDTO getOTP(final String key) {
+          LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME, APPLICATION_ID,
+                       "Get OTP method called");
 
-		// Create Response to return to UI layer
-		ResponseDTO response = new ResponseDTO();
-		OtpGeneratorRequestDto otpGeneratorRequestDto = new OtpGeneratorRequestDto();
-		OtpGeneratorResponseDto otpGeneratorResponseDto = new OtpGeneratorResponseDto();
+          // Create Response to return to UI layer
+          ResponseDTO response = new ResponseDTO();
+          OtpGeneratorRequestDto otpGeneratorRequestDto = new OtpGeneratorRequestDto();
+          OtpGeneratorResponseDto otpGeneratorResponseDto = new OtpGeneratorResponseDto();
 
-		SuccessResponseDTO successResponse = null;
+          SuccessResponseDTO successResponse = null;
 
-		// prepare otpGeneratorRequestDto with specified key(EO Username) obtained from
-		// UI
-		otpGeneratorRequestDto.setKey(key);
+          // prepare otpGeneratorRequestDto with specified key(EO Username) obtained from
+          otpGeneratorRequestDto.setKey(key);
 
-		try {
-			try {
+          try {
 
-				// obtain otpGeneratorResponseDto from serviceDelegateUtil
-				otpGeneratorResponseDto = (OtpGeneratorResponseDto) serviceDelegateUtil
-						.post(RegConstants.OTP_GENERATOR_SERVICE_NAME, otpGeneratorRequestDto);
-			} catch (RegBaseCheckedException e) {
+                 // obtain otpGeneratorResponseDto from serviceDelegateUtil
+                 otpGeneratorResponseDto = (OtpGeneratorResponseDto) serviceDelegateUtil
+                              .post(RegConstants.OTP_GENERATOR_SERVICE_NAME, otpGeneratorRequestDto);
+                 if (otpGeneratorResponseDto != null && otpGeneratorResponseDto.getOtp() != null) {
 
-				// create Error Response
-				response = getErrorResponse(response, RegConstants.OTP_GENERATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response created");
+                       // create Success Response
+                       successResponse = new SuccessResponseDTO();
+                        successResponse.setCode(RegConstants.ALERT_INFORMATION);
+                       successResponse
+                                     .setMessage(RegConstants.OTP_GENERATION_SUCCESS_MESSAGE + otpGeneratorResponseDto.getOtp());
 
-			}
-		} catch (HttpClientErrorException httpClientErrorException) {
-			try {
-				// obtain otpGeneratorResponseDto from JsonUtil
-				otpGeneratorResponseDto = (OtpGeneratorResponseDto) JsonUtils.jsonStringToJavaObject(
-						OtpGeneratorResponseDto.class, httpClientErrorException.getResponseBodyAsString());
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "JSON conversion completed");
+                       Map<String, Object> otherAttributes = new HashMap<String, Object>();
+                     otherAttributes.put(RegConstants.OTP_GENERATOR_RESPONSE_DTO, otpGeneratorResponseDto);
 
-			} catch (MosipJsonParseException e) {
-				// create Error Response
-				response = getErrorResponse(response, RegConstants.OTP_GENERATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response created");
+                        successResponse.setOtherAttributes(otherAttributes);
+                        response.setSuccessResponseDTO(successResponse);
+                       LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
+                                     APPLICATION_ID, "Success Response created");
 
-			} catch (MosipJsonMappingException e) {
-				// create Error Response
-				response = getErrorResponse(response, RegConstants.OTP_GENERATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response created");
+                 } else {
+                       // create Error Response
+                       response = getErrorResponse(response, RegConstants.OTP_GENERATION_ERROR_MESSAGE);
+                       LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
+                                     APPLICATION_ID, "Error Response called");
 
-			} catch (MosipIOException e) {
-				// create Error Response
-				response = getErrorResponse(response, RegConstants.OTP_GENERATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response created");
+                 }
 
-			}
-			// create Error Response
-			response = getErrorResponse(response, RegConstants.OTP_GENERATION_ERROR_MESSAGE);
-			LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-					APPLICATION_ID, "Error Response created");
+          } catch (RegBaseCheckedException | HttpClientErrorException | HttpServerErrorException exception) {
 
-		}
+                 // create Error Response
+                 response = getErrorResponse(response, RegConstants.OTP_GENERATION_ERROR_MESSAGE);
+                 LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
+                              APPLICATION_ID, "Error Response created");
 
-		if (otpGeneratorResponseDto != null) {
-			// create Success Response
-			successResponse = new SuccessResponseDTO();
-			successResponse.setCode(RegConstants.ALERT_INFORMATION);
-			successResponse.setMessage(RegConstants.OTP_GENERATION_SUCCESS_MESSAGE + otpGeneratorResponseDto.getOtp());
+          }
 
-			Map<String, Object> otherAttributes = new HashMap<String, Object>();
-			otherAttributes.put(RegConstants.OTP_GENERATOR_RESPONSE_DTO, otpGeneratorResponseDto);
+          LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME, APPLICATION_ID,
+                       "Get OTP method ended");
 
-			successResponse.setOtherAttributes(otherAttributes);
-			response.setSuccessResponseDTO(successResponse);
-			LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-					APPLICATION_ID, "Success Response created");
-
-		} else {
-			// create Error Response
-			response = getErrorResponse(response, RegConstants.OTP_GENERATION_ERROR_MESSAGE);
-			LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-					APPLICATION_ID, "Error Response created");
-
-		}
-		LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME, APPLICATION_ID,
-				"Get OTP method ended");
-
-		return response;
-	}
+          return response;
+    }
 
 	/*
 	 * (non-Javadoc)
@@ -300,113 +265,62 @@ public class LoginServiceImpl implements LoginService {
 	 * java.lang.String)
 	 */
 	@Override
-	public ResponseDTO validateOTP(final String key, final String otp) {
+    public ResponseDTO validateOTP(final String key, final String otp) {
 
-		LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME, APPLICATION_ID,
-				"Validation of OTP called");
+          LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME, APPLICATION_ID,
+                       "Validation of OTP called");
 
-		// Create Response to Return to UI layer
-		ResponseDTO response = new ResponseDTO();
-		SuccessResponseDTO successResponse;
-		OtpValidatorResponseDto otpValidatorResponseDto = null;
+          // Create Response to Return to UI layer
+          ResponseDTO response = new ResponseDTO();
+          SuccessResponseDTO successResponse;
+          OtpValidatorResponseDto otpValidatorResponseDto = null;
 
-		// Validator response service api creation
-		final String SERVICE_NAME = RegConstants.OTP_VALIDATOR_SERVICE_NAME;
+          // Validator response service api creation
+          final String SERVICE_NAME = RegConstants.OTP_VALIDATOR_SERVICE_NAME;
 
-		// prepare request params to pass through URI
-		Map<String, String> requestParamMap = new HashMap<String, String>();
-		requestParamMap.put(RegConstants.USERNAME_KEY, key);
-		requestParamMap.put(RegConstants.OTP_GENERATED, otp);
+          // prepare request params to pass through URI
+          Map<String, String> requestParamMap = new HashMap<String, String>();
+          requestParamMap.put(RegConstants.USERNAME_KEY, key);
+          requestParamMap.put(RegConstants.OTP_GENERATED, otp);
 
-		try {
-			try {
-				// Obtain otpValidatorResponseDto from service delegate util
-				otpValidatorResponseDto = (OtpValidatorResponseDto) serviceDelegateUtil.get(SERVICE_NAME,
-						requestParamMap);
-			} catch (RegBaseCheckedException e) {
-				// Create Error response
-				response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response Created");
+          try {
+                 // Obtain otpValidatorResponseDto from service delegate util
+                 otpValidatorResponseDto = (OtpValidatorResponseDto) serviceDelegateUtil.get(SERVICE_NAME, requestParamMap);
+                 if (otpValidatorResponseDto != null && otpValidatorResponseDto.getStatus() != null
+                              && otpValidatorResponseDto.getStatus().equalsIgnoreCase("true")) {
 
-			}
-		} catch (HttpClientErrorException httpClientErrorException) {
-			try {
-				// obtain otpValidatorResponseDto through JSON Util
-				otpValidatorResponseDto = (OtpValidatorResponseDto) JsonUtils.jsonStringToJavaObject(
-						OtpValidatorResponseDto.class, httpClientErrorException.getResponseBodyAsString());
-			} catch (MosipJsonParseException e) {
+                       // Create Success Response
+                       successResponse = new SuccessResponseDTO();
+                        successResponse.setCode(RegConstants.ALERT_INFORMATION);
+                 successResponse.setMessage(RegConstants.OTP_VALIDATION_SUCCESS_MESSAGE);
+                        response.setSuccessResponseDTO(successResponse);
+                       LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
+                                     APPLICATION_ID, "Success Response Created");
 
-				// Create Error response
-				response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response Created");
+                 } else {
 
-			} catch (MosipJsonMappingException e) {
-				// Create Error response
-				response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response Created");
+                       // Create Error response
+                       response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
+                       LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
+                                    APPLICATION_ID, "Error Response Created");
 
-			} catch (MosipIOException e) {
-				// Create Error response
-				response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response Created");
+                 }
 
-			}
-			// Create Error response
-			response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
-			LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-					APPLICATION_ID, "Error Response Created");
+          } catch (RegBaseCheckedException | HttpClientErrorException | HttpServerErrorException exception) {
+                 // Create Error response
+                 response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
+                 LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
+                              APPLICATION_ID, "Error Response Created");
 
-		}
-		if (otpValidatorResponseDto != null) {
-			if (otpValidatorResponseDto.getStatus() != null) {
-				if (otpValidatorResponseDto.getStatus().equalsIgnoreCase("true")) {
+          }
 
-					// Create Success Response
-					successResponse = new SuccessResponseDTO();
-					successResponse.setCode(RegConstants.ALERT_INFORMATION);
-					successResponse.setMessage(RegConstants.OTP_VALIDATION_SUCCESS_MESSAGE);
-					Map<String, Object> otherAttributes = new HashMap<String, Object>();
-					otherAttributes.put(RegConstants.OTP_VALIDATOR_RESPONSE_DTO, otpValidatorResponseDto);
-					successResponse.setOtherAttributes(otherAttributes);
-					response.setSuccessResponseDTO(successResponse);
-					LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-							APPLICATION_ID, "Success Response Created");
+          LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME, APPLICATION_ID,
+                       "Validation of OTP ended");
 
-				} else {
+          return response;
 
-					// Create Error response
-					response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
-					LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-							APPLICATION_ID, "Error Response Created");
+    }
 
-				}
-			} else {
-
-				// Create Error response
-				response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
-				LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-						APPLICATION_ID, "Error Response Created");
-
-			}
-		} else {
-
-			// Create Error response
-			response = getErrorResponse(response, RegConstants.OTP_VALIDATION_ERROR_MESSAGE);
-			LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME,
-					APPLICATION_ID, "Error Response Created");
-
-		}
-
-		LOGGER.debug("REGISTRATION - LOGIN - OTP", APPLICATION_NAME, APPLICATION_ID,
-				"Validation of OTP ended");
-
-		return response;
-
-	}
 
 	private ResponseDTO getErrorResponse(ResponseDTO response, final String message) {
 		// Create list of Error Response
