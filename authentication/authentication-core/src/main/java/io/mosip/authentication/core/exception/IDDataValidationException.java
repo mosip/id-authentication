662ca82c435c5facd8d73b9485da02158d287331
Sanjay Murali <sanjay.murali@mindtree.com>
2018-10-29 19:34:45 +0530

    String to date convertion
    Former-commit-id: e41f2e3f5b15ba6a9074badb73c8d5dc6b723427
    Former-commit-id: 731445f6890e04d9ccfa0a067a5305db0552a23d

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/exception/IDDataValidationException.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/exception/IDDataValidationException.java
@@ -1,6 +1,7 @@
 package io.mosip.authentication.core.exception;
 
 import io.mosip.authentication.core.constant.IdAuthenticationErrorConstants;
+import io.mosip.kernel.core.exception.BaseUncheckedException;
 
 /**
  * The Class IDDataValidationException - Thrown when any Data validation error occurs.
@@ -36,6 +37,29 @@ public class IDDataValidationException extends IdAuthenticationBusinessException
 	 */
 	public IDDataValidationException(IdAuthenticationErrorConstants exceptionConstant, Throwable rootCause) {
 		super(exceptionConstant, rootCause);
+	}
+	
+	/**
+	 * Constructs exception for the given error code and error message.
+	 *
+	 * @param errorCode the error code
+	 * @param errorMessage the error message
+	 * @see BaseUncheckedException#BaseUncheckedException(String, String)
+	 */
+	public IDDataValidationException(String errorCode, String errorMessage) {
+		super(errorCode, errorMessage);
+	}
+	
+	/**
+	 * Constructs exception for the given  error code, error message and {@code Throwable}.
+	 *
+	 * @param errorCode the error code
+	 * @param errorMessage the error message
+	 * @param cause the cause
+	 * @see BaseUncheckedException#BaseUncheckedException(String, String, Throwable)
+	 */
+	public IDDataValidationException(String errorCode, String errorMessage, Throwable cause) {
+		super(errorCode, errorMessage, cause);
 	}
 
 }
