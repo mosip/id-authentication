0db3e6d54557727053cd2cf0666237a438f7207d
M1022006 <sowmya.goudar@mindtree.com>
2018-11-02 12:43:39 +0530

    File system testcase updated
    Former-commit-id: 437f9349cb208766e699189faea14fd553b3ad62
    Former-commit-id: 2e6a58aee4b949eafa5426622c021cb9b2c5afc8

--- registration-processor/filesystem-adapter-ceph/src/test/java/io/mosip/registration/processor/filesystem/ceph/adapter/impl/FilesystemCephAdapterImplTest.java
+++ registration-processor/filesystem-adapter-ceph/src/test/java/io/mosip/registration/processor/filesystem/ceph/adapter/impl/FilesystemCephAdapterImplTest.java
@@ -1,14 +1,14 @@
-/*package io.mosip.registration.processor.filesystem.ceph.adapter.impl;
+package io.mosip.registration.processor.filesystem.ceph.adapter.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
-import java.util.Properties;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
@@ -17,10 +17,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.AnonymousAWSCredentials;
@@ -29,82 +31,86 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 import io.findify.s3mock.S3Mock;
-import io.mosip.registration.processor.core.spi.filesystem.adapter.FileSystemAdapter;
 import io.mosip.registration.processor.filesystem.ceph.adapter.impl.exception.ConnectionUnavailableException;
 import io.mosip.registration.processor.filesystem.ceph.adapter.impl.exception.InvalidConnectionParameters;
 import io.mosip.registration.processor.filesystem.ceph.adapter.impl.exception.PacketNotFoundException;
 import io.mosip.registration.processor.filesystem.ceph.adapter.impl.utils.ConnectionUtil;
 import io.mosip.registration.processor.filesystem.ceph.adapter.impl.utils.PacketFiles;
 
-*//**
+/**
  * This class tests the functionalities of DFSAdapterImpl.
  *
  * @author Pranav Kumar
  * @author Ranjitha
- *//*
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(ConnectionUtil.class)
-@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*" })
+ */
+@RefreshScope
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class FilesystemCephAdapterImplTest {
 
-	*//** The api. *//*
+	/** The api. */
 	private S3Mock api;
 
-	*//** The client. *//*
+	/** The client. */
 	private AmazonS3 client;
 
-	*//** The check enrolment id. *//*
+	/** The check enrolment id. */
 	private String checkEnrolmentId;
 
-	*//** The file extension. *//*
+	/** The file extension. */
 	private String fileExtension;
 
-	*//** The Constant CONFIG_FILE_NAME. *//*
+	/** The Constant CONFIG_FILE_NAME. */
 	private static final String CONFIG_FILE_NAME = "config.properties";
 
-	*//** The dfs adapter. *//*
-	private FileSystemAdapter<InputStream, Boolean> dfsAdapter;
+	/** The dfs adapter. */
 
-	*//** The Constant FAILURE_ENROLMENT_ID. *//*
+	private FilesystemCephAdapterImpl dfsAdapter;
+
+	@Autowired
+	private Environment env = mock(Environment.class);
+
+	/** The Constant FAILURE_ENROLMENT_ID. */
 	private static final String FAILURE_ENROLMENT_ID = "1234";
 
-	*//**
+	@Mock
+	private ConnectionUtil connectionUtil;
+
+	/**
 	 * This method sets up the required configuration before execution of test
 	 * cases.
 	 *
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
-	 *//*
+	 */
 	@Before
 	public void setup() throws IOException {
-		Properties properties = new Properties();
-		InputStream inputStream;
-		inputStream = ConnectionUtil.class.getClassLoader().getResourceAsStream(CONFIG_FILE_NAME);
-		properties.load(inputStream);
-		this.checkEnrolmentId = properties.getProperty("check.enrolment.id");
-		this.fileExtension = properties.getProperty("file.extension");
+
+		this.checkEnrolmentId = env.getProperty("registration.processor.check.enrolment.id");
+		this.fileExtension = env.getProperty("registration.processor.file.extension");
+
 		api = new S3Mock.Builder().withPort(8001).withInMemoryBackend().build();
 		api.start();
 		EndpointConfiguration endpoint = new EndpointConfiguration("http://localhost:8001", "us-west-2");
 		client = AmazonS3ClientBuilder.standard().withPathStyleAccessEnabled(true).withEndpointConfiguration(endpoint)
 				.withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials())).build();
-		PowerMockito.mockStatic(ConnectionUtil.class);
-		when(ConnectionUtil.getConnection()).thenReturn(client);
 
+		when(connectionUtil.getConnection()).thenReturn(client);
+		dfsAdapter = new FilesystemCephAdapterImpl(connectionUtil);
 		// Putting a file to mocked ceph instance
-		this.dfsAdapter = new FilesystemCephAdapterImpl();
+
 		ClassLoader classLoader = getClass().getClassLoader();
 		String filePath = classLoader.getResource(checkEnrolmentId + fileExtension).getFile();
 		File packet = new File(filePath);
 		dfsAdapter.storePacket(this.checkEnrolmentId, packet);
 	}
 
-	*//**
+	/**
 	 * This method tests uploading of a packet to DFS.
 	 *
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
-	 *//*
+	 */
 	@Test
 	public void testUploadPacketSuccess() throws IOException {
 		ClassLoader classLoader = getClass().getClassLoader();
@@ -114,13 +120,13 @@ public class FilesystemCephAdapterImplTest {
 		assertEquals("Successfully uploaded packet as filepath to DFS .", true, result);
 	}
 
-	*//**
+	/**
 	 * This method tests getting a packet successfully from DFS.
 	 *
 	 * @return the packet success test
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
-	 *//*
+	 */
 	@Test
 	public void testGetPacketSuccess() throws IOException {
 		ClassLoader classLoader = getClass().getClassLoader();
@@ -144,24 +150,24 @@ public class FilesystemCephAdapterImplTest {
 		actualZipFile.close();
 	}
 
-	*//**
+	/**
 	 * This method tests getting a packet which is not present in DFS.
 	 *
 	 * @return the packet failure test
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
-	 *//*
+	 */
 	@Test(expected = PacketNotFoundException.class)
 	public void testGetPacketFailure() throws IOException {
 		this.dfsAdapter.unpackPacket(FAILURE_ENROLMENT_ID);
 	}
 
-	*//**
+	/**
 	 * This method checks unpacking of a packet in DFS.
 	 *
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
-	 *//*
+	 */
 	@Test
 	public void testUnpackPacketFailure() throws IOException {
 		this.dfsAdapter.unpackPacket(checkEnrolmentId);
@@ -178,28 +184,28 @@ public class FilesystemCephAdapterImplTest {
 		assertEquals("Delete file by registration id .", true, result);
 	}
 
-	*//**
+	/**
 	 * This method checks fetching a file from a packet that is not present in DFS.
 	 *
 	 * @return the file failure test
-	 *//*
+	 */
 	@Test(expected = PacketNotFoundException.class)
 	public void testGetFileFailure() {
 		this.dfsAdapter.getFile(FAILURE_ENROLMENT_ID, PacketFiles.DEMOGRAPHIC.name());
 	}
 
-	*//**
+	/**
 	 * This method checks deleting a packet from DFS and later fetching it.
-	 *//*
+	 */
 	@Test(expected = PacketNotFoundException.class)
 	public void testDeletePacketFailure() {
 		this.dfsAdapter.deletePacket(checkEnrolmentId);
 		this.dfsAdapter.getPacket(checkEnrolmentId);
 	}
 
-	*//**
+	/**
 	 * Store packet input stream success test.
-	 *//*
+	 */
 	@Test
 	public void testStorePacketInputStreamSuccess() {
 		InputStream packet = dfsAdapter.getPacket(checkEnrolmentId);
@@ -207,17 +213,17 @@ public class FilesystemCephAdapterImplTest {
 		assertEquals("Successfully uploaded packet as inputstream to DFS.", true, result);
 	}
 
-	*//**
+	/**
 	 * Store packet connection unavailable exception test.
-	 *//*
+	 */
 	@Test(expected = ConnectionUnavailableException.class)
 	public void testStorePacketConnectionUnavailableException() {
 		EndpointConfiguration endpoint = new EndpointConfiguration("http://localhost:8001", "us-west-2");
 		AmazonS3 client1 = AmazonS3ClientBuilder.standard().withPathStyleAccessEnabled(true)
 				.withEndpointConfiguration(endpoint).withCredentials(null).build();
-		PowerMockito.mockStatic(ConnectionUtil.class);
-		when(ConnectionUtil.getConnection()).thenReturn(client1);
-		this.dfsAdapter = new FilesystemCephAdapterImpl();
+
+		when(connectionUtil.getConnection()).thenReturn(client1);
+		dfsAdapter = new FilesystemCephAdapterImpl(connectionUtil);
 		InputStream packet = new InputStream() {
 			@Override
 			public int read() throws IOException {
@@ -227,36 +233,36 @@ public class FilesystemCephAdapterImplTest {
 		this.dfsAdapter.storePacket(this.checkEnrolmentId, packet);
 	}
 
-	*//**
+	/**
 	 * Store packet invalid connection parameter exception test.
-	 *//*
+	 */
 	@SuppressWarnings("unchecked")
 	@Test(expected = InvalidConnectionParameters.class)
 	public void testStorePacketInvalidConnectionParameterException() {
-		PowerMockito.mockStatic(ConnectionUtil.class);
-		when(ConnectionUtil.getConnection()).thenThrow(InvalidConnectionParameters.class);
+
+		when(connectionUtil.getConnection()).thenThrow(InvalidConnectionParameters.class);
 		InputStream packet = new InputStream() {
 			@Override
 			public int read() throws IOException {
 				return 0;
 			}
 		};
-		this.dfsAdapter = new FilesystemCephAdapterImpl();
+		dfsAdapter = new FilesystemCephAdapterImpl(connectionUtil);
 		this.dfsAdapter.storePacket(this.checkEnrolmentId, packet);
 	}
-	
+
 	@Test
 	public void fileExistenceFailureTest() {
 		boolean result = this.dfsAdapter.checkFileExistence(checkEnrolmentId, PacketFiles.BIOMETRIC.name());
 		assertEquals(false, result);
 	}
 
-	*//**
+	/**
 	 * This method destroys the dummy DFS connection.
-	 *//*
+	 */
 	@After
 	public void destroy() {
 		api.stop();
 	}
 
-}*/
+}
