555cf97b63f8af1a68864f4adb140b1bf334ff3d
Monobikash Das <M1045447@mindtree.com>
2018-11-02 23:05:44 +0530

    "Code refactoring"
    Former-commit-id: caa9e865d200fe3653a73794ddf7c7bf01624743
    Former-commit-id: eb72624ccdfbdfae90a6a4690a3a8def0b26608d

--- registration-processor/registration-status-service/src/main/java/io/mosip/registration/processor/status/controller/RegistrationStatusController.java
+++ registration-processor/registration-status-service/src/main/java/io/mosip/registration/processor/status/controller/RegistrationStatusController.java
@@ -7,13 +7,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.registration.processor.status.code.RegistrationStatusCode;
 import io.mosip.registration.processor.status.dto.RegistrationStatusDto;
+import io.mosip.registration.processor.status.dto.SyncRegistrationDto;
 import io.mosip.registration.processor.status.service.RegistrationStatusService;
+import io.mosip.registration.processor.status.service.SyncRegistrationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -21,11 +25,14 @@ import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/v0.1/registration-processor/registration-status")
-@Api(tags = "Status Handler")
+@Api(tags = "Registration Status")
 public class RegistrationStatusController {
 
 	@Autowired
 	RegistrationStatusService<String, RegistrationStatusDto> registrationStatusService;
+	
+	@Autowired
+	SyncRegistrationService<SyncRegistrationDto> syncRegistrationService;
 
 	@GetMapping(path = "/registrationstatus", consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Get the registration entity", response = RegistrationStatusCode.class)
@@ -35,5 +42,22 @@ public class RegistrationStatusController {
 			@RequestParam(value = "registrationIds", required = true) String registrationIds) {
 		List<RegistrationStatusDto>registrations = registrationStatusService.getByIds(registrationIds);
 		return ResponseEntity.status(HttpStatus.OK).body(registrations);
+	}
+	
+	/**
+	 * Sync registration ids.
+	 *
+	 * @param syncRegistrationDto
+	 *            the sync registration dto
+	 * @return the response entity
+	 */
+	@PostMapping(path = "/sync", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "Get the synchronizing registration entity", response = RegistrationStatusCode.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Synchronizing Registration Entity successfully fetched") })
+	public ResponseEntity<List<SyncRegistrationDto>> syncRegistrationController(
+			@RequestBody(required = true) List<SyncRegistrationDto> syncRegistrationDto) {
+		List<SyncRegistrationDto> syncRegistrationDtoResponse = syncRegistrationService.sync(syncRegistrationDto);
+		return ResponseEntity.status(HttpStatus.OK).body(syncRegistrationDtoResponse);
 	}
 }
