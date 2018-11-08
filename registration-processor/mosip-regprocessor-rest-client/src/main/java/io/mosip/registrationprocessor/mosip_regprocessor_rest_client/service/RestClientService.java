39ce7c7f2a1d667f5bb6e1ff2c8259767eea5661
Alok Ranjan <Alok.Ranjan2@mindtree.com>
2018-11-08 12:59:14 +0530

    Added mosip regprocessor rest client structure
    Former-commit-id: f37d789adfbf7701f4fbfe5a94bc82bd305b2c12
    Former-commit-id: 6124baee91b1dab19a557f93bb88fab2bae16945

---
+++ registration-processor/mosip-regprocessor-rest-client/src/main/java/io/mosip/registrationprocessor/mosip_regprocessor_rest_client/service/RestClientService.java
@@ -0,0 +1,9 @@
+package io.mosip.registrationprocessor.mosip_regprocessor_rest_client.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface RestClientService {
+	
+
+}
