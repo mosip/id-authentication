643d2c182a771f0f00bbc706303c68647ede275a
M1049387 <M1049387@A2ML29516.mindtree.com>
2018-11-09 17:56:17 +0530

    Mos-241  commited rest client genric code
    Former-commit-id: 01b5d90b063b8ae9481f0eeaee98d81ec8701bc6
    Former-commit-id: 7004b46d8ac5300c55396180b501da8bee3637cc

--- registration-processor/mosip-regprocessor-rest-client/src/main/java/io/mosip/registrationprocessor/mosip_regprocessor_rest_client/utils/RegProcGenericRestClient.java
+++ registration-processor/mosip-regprocessor-rest-client/src/main/java/io/mosip/registrationprocessor/mosip_regprocessor_rest_client/utils/RegProcGenericRestClient.java
@@ -1,11 +1,17 @@
 package io.mosip.registrationprocessor.mosip_regprocessor_rest_client.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import io.mosip.registration.processor.status.dto.RegistrationStatusDto;
 import io.mosip.registrationprocessor.mosip_regprocessor_rest_client.dto.RequestDetails;
 
 /**
@@ -15,7 +21,7 @@ import io.mosip.registrationprocessor.mosip_regprocessor_rest_client.dto.Request
  * @param <V> the value type
  * @author Rishabh Keshari
  */
-public class RegProcGenericRestClient<T, V>  {
+public class RegProcGenericRestClient<T,V>  {
 	
 	/** The rest template. */
 	private RestTemplate restTemplate = new RestTemplate();
@@ -33,11 +39,41 @@ public class RegProcGenericRestClient<T, V>  {
 
 		restTemplate.setErrorHandler(errorHandler);
 		HttpHeaders headers = new HttpHeaders();
-		HttpEntity<T> entity = new HttpEntity<>(requestedData, headers);
+	//	UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(requestDetails.getRequestUrl()).queryParam("registrationIds", registrationIds);
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(requestDetails.getRequestUrl());
+		 
+
 		
-		ResponseEntity<V> response = restTemplate.exchange(requestDetails.getRequestUrl(), requestDetails.getRequestMethodType(), entity, reponseType);
+		HttpEntity<T> entity = new HttpEntity<>(requestedData, headers);
+		 ResponseEntity<V> response=restTemplate.exchange(builder.toUriString(), requestDetails.getRequestMethodType(), entity, reponseType);
+		 return response.getBody();
+	}
 	
-		return response.getBody();
-	}	
+	/*
+	@SuppressWarnings("unchecked")
+	public void run() {
+		RestTemplate template = new RestTemplate();
+		String url = "http://localhost:8080/v0.1/registration-processor/registration-status/registrationstatus?registrationIds=2018782130000224092018121229";
+		HttpHeaders headers = new HttpHeaders();
+		//headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		HttpEntity<List<RegistrationStatusDto>> entity = new HttpEntity<List<RegistrationStatusDto>>(headers);
+		List<RegistrationStatusDto> list = new ArrayList<>();
+		ResponseEntity<? extends List> response = template.exchange(url,HttpMethod.GET,entity,list.getClass());
+        System.out.println(response);
+	
+	} 
+	
+	public void generic(String url) {
+		RestTemplate template = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<R> entity = new HttpEntity<R>(headers);
+		List<T> list = new ArrayList<>();
+		ResponseEntity<? extends List> response = template.exchange(url,HttpMethod.GET,entity,list.getClass());
+
+        System.out.println(response);
+
+	}*/
+	
+	
 
 }
