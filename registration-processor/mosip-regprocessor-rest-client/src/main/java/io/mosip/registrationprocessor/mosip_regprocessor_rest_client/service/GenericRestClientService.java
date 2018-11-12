47b1197e2f6302ef6855e2398c343982b808d16e
M1049387 <M1049387@A2ML29516.mindtree.com>
2018-11-12 15:30:07 +0530

    MOS-241 rest clicnt service and service impl folder added
    Former-commit-id: cbfd8cb7e8a41f494da508ad279d0f6c7bdb2820
    Former-commit-id: f9c6033963ff50a6ffb0e475af91212bed368fde

---
+++ registration-processor/mosip-regprocessor-rest-client/src/main/java/io/mosip/registrationprocessor/mosip_regprocessor_rest_client/service/GenericRestClientService.java
@@ -0,0 +1,16 @@
+package io.mosip.registrationprocessor.mosip_regprocessor_rest_client.service;
+
+import org.springframework.stereotype.Service;
+
+
+/**
+ * The Interface GenericRestClientService.
+ * @author Rishabh Keshari
+ */
+
+@Service
+public interface GenericRestClientService{
+	
+	
+
+}
---
+++ registration-processor/mosip-regprocessor-rest-client/src/main/java/io/mosip/registrationprocessor/mosip_regprocessor_rest_client/service/impl/GenericRestClientServiceImpl.java
@@ -0,0 +1,20 @@
+package io.mosip.registrationprocessor.mosip_regprocessor_rest_client.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import io.mosip.registrationprocessor.mosip_regprocessor_rest_client.service.GenericRestClientService;
+
+
+/**
+ * The Class GenericRestClientServiceImpl.
+ * @author Rishabh Keshari
+ */
+
+@Component
+public class GenericRestClientServiceImpl implements GenericRestClientService{
+
+	/** The logger. */
+	private final Logger logger = LoggerFactory.getLogger(GenericRestClientServiceImpl.class);
+	
+}
