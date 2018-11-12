9cb31fd6d55835b0f1eb098bfd3a849ba61f93d6
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2018-11-12 12:34:47 +0530

    adding auth type predicate in ekyc auth type.
    Former-commit-id: 476971712d3fe9387ec8c32a1f2df972f8e0ecf6
    Former-commit-id: aece80e22b89f014f35ca3aa20548144276ba653

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/dto/indauth/EkycAuthType.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/dto/indauth/EkycAuthType.java
@@ -1,6 +1,7 @@
 package io.mosip.authentication.core.dto.indauth;
 
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import javax.xml.bind.annotation.XmlValue;
@@ -17,29 +18,31 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum EkycAuthType {
 		
 	/** For Fingerprints  */
-	FINGERPRINTS("F"),
+	FINGERPRINT("F", AuthTypeDTO::isFingerPrint),
 	
 	/** For Iris */
-	IRIS("I"),
+	IRIS("I", AuthTypeDTO::isIris),
 	
 	/** For Face */
-	FACE("A"),
+	FACE("A", AuthTypeDTO::isFace),
 	
 	/** For Pin */
-	PIN("P"),
+	PIN("P", AuthTypeDTO::isPin),
 	/** For OTP */
-	OTP("O");
+	OTP("O", AuthTypeDTO::isOtp);
 	
 	
 	String type;
+	private Predicate<AuthTypeDTO> checkAuthType;
 	
 	/**
 	 * Instantiates a new EkycAuthType.
 	 *
 	 * @param type the EkycAuthType
 	 */
-	private EkycAuthType(String type) {
+	private EkycAuthType(String type, Predicate<AuthTypeDTO> checkAuthType) {
 		this.type = type;
+		this.checkAuthType = checkAuthType;
 	}
 	
 	/**
@@ -62,6 +65,14 @@ public enum EkycAuthType {
 	public static Optional<EkycAuthType> getEkycAuthType(String type) {
 		return Stream.of(values()).filter(t -> t.getType().equals(type)).findAny();
 
+	}
+	
+	/**
+	 * Get the predicate to check the auth type
+	 * @return the predicate
+	 */
+	public Predicate<AuthTypeDTO> getCheckAuthType() {
+		return checkAuthType;
 	}
 	
 }
