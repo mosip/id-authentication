6bdc8c4acdb8c2549f71bbb01f7faaa8462fbe06
Girish Yarru <M1048219@mindtree.com>
2018-11-12 10:12:45 +0530

    MOS-241 : Generic Rest Client - GET,POST
    Former-commit-id: 9ab945d81fc21bf1ae0132369d08301b426f675c
    Former-commit-id: 9c9c660929b5eaed167656452fb79745f6774852

--- registration-processor/mosip-regprocessor-rest-client/src/main/java/io/mosip/registrationprocessor/mosip_regprocessor_rest_client/RestClientApplication.java
+++ registration-processor/mosip-regprocessor-rest-client/src/main/java/io/mosip/registrationprocessor/mosip_regprocessor_rest_client/RestClientApplication.java
@@ -1,6 +1,5 @@
 package io.mosip.registrationprocessor.mosip_regprocessor_rest_client;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,68 +7,61 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import io.mosip.registration.processor.status.dto.RegistrationStatusDto;
 import io.mosip.registration.processor.status.dto.SyncRegistrationDto;
 import io.mosip.registration.processor.status.dto.SyncStatusDto;
 import io.mosip.registration.processor.status.dto.SyncTypeDto;
-import io.mosip.registrationprocessor.mosip_regprocessor_rest_client.dto.RequestDetails;
-import io.mosip.registrationprocessor.mosip_regprocessor_rest_client.utils.RegProcGenericRestClient;
 
 @SpringBootApplication
 @PropertySource({ "classpath:rest-client-application.properties" })
-public class RestClientApplication
-{
-	
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(RestClientApplication.class, args);
-    }
+public class RestClientApplication implements CommandLineRunner {
 
-	/*@Override
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientApplication.class, args);
+	}
+
+	@Override
 	public void run(String... args) throws Exception {
-		RegProcGenericRestClient<RegistrationStatusDto,List<RegistrationStatusDto>> reg = new RegProcGenericRestClient();
-		
-		
-SyncRegistrationDto synreg=new SyncRegistrationDto();
-		
-		//synreg.setIsActive("");
-		//synreg.setIsDeleted(isDeleted);
-		synreg.setLangCode("en");
-		
-		synreg.setSyncStatus(SyncStatusDto.PRE_SYNC);
-		synreg.setParentRegistrationId("abc123");
-		synreg.setStatusComment("new reg");
-		synreg.setSyncType(SyncTypeDto.NEW_REGISTRATION);
-		synreg.setRegistrationId("abc1");
-		
-		List<SyncRegistrationDto> list = new ArrayList<>();
-		
-		list.add(synreg);
-		
-	ResponseErrorHandler responseHandler=new ResponseErrorHandler() {
-			
-			@Override
-			public boolean hasError(ClientHttpResponse arg0) throws IOException {
-			
-				return false;
-			}
-			
-			@Override
-			public void handleError(ClientHttpResponse arg0) throws IOException {
-			
-				
-			}
-		};
-		
-		String str=	new RegProcGenericRestClient<List, String>().execute(new RequestDetails("http://172.23.30.3:8080/v0.1/registration-processor/registration-status/registrationstatus", HttpMethod.GET),list, responseHandler,String.class);
-			
-	//String str=	new RegProcGenericRestClient<List<SyncRegistrationDto>, String>().execute(new RequestDetails("http://172.23.30.3:8080/v0.1/registration-processor/registration-status/sync", HttpMethod.POST),list, responseHandler,String.class);
-	System.out.println("as my output is :  "+str);		
-		//reg.generic("http://172.23.30.3:8080/v0.1/registration-processor/registration-status/registrationstatus?registrationIds=2018782130000224092018121229");
+		final String getURI = "http://localhost:8080/v0.1/registration-processor/registration-status/registrationstatus";
+		List<RegistrationStatusDto> list = new ArrayList<>();
+		// Generic GET Client
+		List<RegistrationStatusDto> getResult = genericGETClient(getURI, "registrationIds",
+				"2018782130000224092018121229", list);
+		System.out.println(getResult);
 
-	}*/
+		final String postURI = "http://localhost:8080/v0.1/registration-processor/registration-status/sync";
+		SyncRegistrationDto dto1 = new SyncRegistrationDto();
+		dto1.setIsActive(true);
+		dto1.setIsDeleted(false);
+		dto1.setLangCode("eng");
+		dto1.setParentRegistrationId("");
+		dto1.setRegistrationId("2018782130000224092018121229");
+		dto1.setStatusComment("Uploaded to Virus Scanner");
+		dto1.setSyncStatus(SyncStatusDto.PRE_SYNC);
+		dto1.setSyncType(SyncTypeDto.NEW_REGISTRATION);
+
+		List<SyncRegistrationDto> syncRegistrationDto = new ArrayList<>();
+		syncRegistrationDto.add(dto1);
+		// Generic POST Client
+		List<SyncRegistrationDto> postResult = genericPostClient(postURI, syncRegistrationDto);
+		System.out.println(postResult);
+	}
+
+	public <T> T genericGETClient(String getURI, String queryParam, String queryParamValue, T responseType) {
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(getURI).queryParam(queryParam,
+				queryParamValue);
+		RestTemplate restTemplate = new RestTemplate();
+		T result = (T) restTemplate.getForObject(builder.buildAndExpand().toUri(), responseType.getClass());
+		return result;
+	}
+
+	public <T> T genericPostClient(String uri, T responseType) {
+		RestTemplate restTemplate = new RestTemplate();
+		T result = (T) restTemplate.postForObject(uri, responseType, List.class);
+		return result;
+
+	}
 }
