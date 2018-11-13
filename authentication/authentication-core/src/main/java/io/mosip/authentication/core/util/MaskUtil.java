da8b66dc06eae39fcae9e7749698f965ca7a418c
Sanjay Murali <sanjay.murali@mindtree.com>
2018-11-13 11:50:59 +0530

    Masking value util
    Former-commit-id: e2c966cf86eade89255ad9723da5c196e7be8100
    Former-commit-id: 2b76c8cd392905e5ec529696d41dad764b253e7c

---
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/util/MaskUtil.java
@@ -0,0 +1,22 @@
+package io.mosip.authentication.core.util;
+
+/**
+ * Method to generate masking the values
+ * 
+ * @author Sanjay Murali
+ */
+public class MaskUtil {
+	
+	private MaskUtil() {
+		
+	}
+	
+	public static String generateMaskValue(String maskValue, int maskNo) {
+		char[] maskedDetail = maskValue.toCharArray();
+		for(int i=0; i<maskNo; i++) {
+			maskedDetail[i] = 'X';
+		}
+		return String.valueOf(maskedDetail);
+	}
+	
+}
