8230147d21a10acd41975599c01dc416238d8c71
Dharmesh Khandelwal <Dharmesh.Khandelwal@mindtree.com>
2018-11-13 14:05:36 +0530

    Updated javadoc for auditmanager
    Former-commit-id: 1dc3f55709bbb6f0e03c48f8f40c1a2cb53f75b3
    Former-commit-id: bfc5aa1196809680e0a4a9360da264d1b466b524

--- kernel/kernel-auditmanager-service/src/test/java/io/mosip/kernel/auditmanager/test/AuditServiceTest.java
+++ kernel/kernel-auditmanager-service/src/test/java/io/mosip/kernel/auditmanager/test/AuditServiceTest.java
@@ -2,30 +2,25 @@ package io.mosip.kernel.auditmanager.test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.mosip.kernel.auditmanager.dto.AuditResponseDto;
+import io.mosip.kernel.auditmanager.exception.AuditManagerException;
 import io.mosip.kernel.auditmanager.request.AuditRequestDto;
 import io.mosip.kernel.auditmanager.service.impl.AuditManagerServiceImpl;
-import io.mosip.kernel.core.auditmanager.spi.AuditHandler;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class AuditServiceTest {
-	@Mock
-	private AuditHandler<AuditRequestDto> handler;
-	@Mock
-	private ModelMapper modelMapper;
-	@InjectMocks
+
+	@Autowired
 	private AuditManagerServiceImpl service;
 
 	@Test
@@ -51,8 +46,32 @@ public class AuditServiceTest {
 
 		AuditResponseDto auditResponseDto = new AuditResponseDto();
 		auditResponseDto.setStatus(true);
-		when(handler.addAudit(ArgumentMatchers.any())).thenReturn(true);
+
 		assertThat(service.addAudit(auditRequestDto), is(auditResponseDto));
 	}
 
+	@Test(expected = AuditManagerException.class)
+	public void auditServiceVoilationTest() {
+
+		AuditRequestDto auditRequestDto = new AuditRequestDto();
+		auditRequestDto.setActionTimeStamp(LocalDateTime.now());
+		auditRequestDto.setApplicationId("applicationId");
+		auditRequestDto.setApplicationName("applicationName");
+		auditRequestDto.setDescription("description");
+		auditRequestDto.setEventId("eventId");
+		auditRequestDto.setEventName("eventName");
+		auditRequestDto.setEventType("eventType");
+		auditRequestDto.setHostIp("hostIp");
+		auditRequestDto.setHostName("hostName");
+		auditRequestDto.setId("id");
+		auditRequestDto.setIdType("idType");
+		auditRequestDto.setModuleId("moduleId");
+		auditRequestDto.setModuleName("moduleName");
+		auditRequestDto.setSessionUserId("sessionUserId");
+		auditRequestDto.setSessionUserName("sessionUserName");
+
+		AuditResponseDto auditResponseDto = new AuditResponseDto();
+		auditResponseDto.setStatus(true);
+		service.addAudit(auditRequestDto);
+	}
 }
