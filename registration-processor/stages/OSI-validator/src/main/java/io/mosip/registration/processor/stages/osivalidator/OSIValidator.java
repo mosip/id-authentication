73d0d68e501ffdb0eac7b29d0ae90ce6237d93b6
M1022006 <sowmya.goudar@mindtree.com>
2018-11-14 12:48:24 +0530

    Updated OSI validator
    Former-commit-id: 0db5560e835b758a37458e3f204305c75e3fa2a3
    Former-commit-id: 3b7c3a4c00e695b4720fe31f925e1d5b28b7fff3

--- registration-processor/stages/OSI-validator/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidator.java
+++ registration-processor/stages/OSI-validator/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidator.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.mosip.registration.processor.core.packet.dto.OsiData;
+import io.mosip.registration.processor.core.packet.dto.PacketInfo;
+import io.mosip.registration.processor.core.util.JsonUtil;
 import io.mosip.registration.processor.filesystem.ceph.adapter.impl.FilesystemCephAdapterImpl;
+import io.mosip.registration.processor.filesystem.ceph.adapter.impl.utils.PacketFiles;
 
 @Service
 public class OSIValidator {
@@ -16,10 +19,23 @@ public class OSIValidator {
 	@Autowired
 	FilesystemCephAdapterImpl adapter;
 
-	public boolean isValidOSI(String registrationId) {
+	public boolean isValidOSI(String registrationId) throws IOException {
 		// To do call packet info to get osi data then call each validation
+		boolean isValidOSI = false;
+		InputStream packetMetaInfoStream = adapter.getFile(registrationId, PacketFiles.PACKETMETAINFO.name());
+		PacketInfo packetInfo = (PacketInfo) JsonUtil.inputStreamtoJavaObject(packetMetaInfoStream, PacketInfo.class);
+		OsiData osiData = packetInfo.getOsiData();
+		isValidOSI = isValidOperator(osiData);
 
-		return false;
+		if (isValidOSI) {
+			isValidOSI = isValidSupervisor(osiData);
+			if (isValidOSI) {
+				isValidOSI = isValidIntroducer(osiData);
+			}
+
+		}
+
+		return isValidOSI;
 	}
 
 	private boolean isValidOperator(OsiData osiData) throws IOException {
--- registration-processor/stages/OSI-validator/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidatorStage.java
+++ registration-processor/stages/OSI-validator/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidatorStage.java
@@ -1,5 +1,8 @@
 package io.mosip.registration.processor.stages.osivalidator;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
@@ -8,16 +11,24 @@ import io.mosip.registration.processor.core.abstractverticle.MessageBusAddress;
 import io.mosip.registration.processor.core.abstractverticle.MessageDTO;
 import io.mosip.registration.processor.core.abstractverticle.MosipEventBus;
 import io.mosip.registration.processor.core.abstractverticle.MosipVerticleManager;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 @RefreshScope
 @Service
 public class OSIValidatorStage extends MosipVerticleManager {
+
+	/** The log. */
+	private static Logger log = LoggerFactory.getLogger(OSIValidatorStage.class);
 
 	@Value("${registration.processor.vertx.cluster.address}")
 	private String clusterAddress;
 
 	@Value("${registration.processor.vertx.localhost}")
 	private String localhost;
+
+	@Autowired
+	OSIValidator osiValidator;
 
 	/**
 	 * Deploy verticle.
@@ -29,8 +40,26 @@ public class OSIValidatorStage extends MosipVerticleManager {
 
 	@Override
 	public MessageDTO process(MessageDTO object) {
+		object.setMessageBusAddress(MessageBusAddress.OSI_BUS_IN);
+		object.setIsValid(Boolean.FALSE);
+		object.setInternalError(Boolean.FALSE);
 
-		return null;
+		String registrationId = object.getRid();
+		boolean isValidOSI = false;
+
+		try {
+			isValidOSI = osiValidator.isValidOSI(registrationId);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (isValidOSI) {
+
+		} else {
+
+		}
+		return object;
 	}
 
 }
