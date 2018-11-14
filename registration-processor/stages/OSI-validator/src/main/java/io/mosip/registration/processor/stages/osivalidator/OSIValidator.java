b6fdffa53abd9475ba83974cc45a784ab4c878d9
M1039285 <M1039285@a2ml16222.mindtree.com>
2018-11-14 11:38:48 +0530

    Validation
    Former-commit-id: 882be78482006213b282acea348f07f72741027c
    Former-commit-id: b127489d9bf0ebff33322e2025e0b911bbc9fb27

--- registration-processor/stages/OSI-validator/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidator.java
+++ registration-processor/stages/OSI-validator/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidator.java
@@ -1,11 +1,20 @@
 package io.mosip.registration.processor.stages.osivalidator;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.mosip.registration.processor.core.packet.dto.OsiData;
+import io.mosip.registration.processor.filesystem.ceph.adapter.impl.FilesystemCephAdapterImpl;
 
 @Service
 public class OSIValidator {
+
+	@Autowired
+	FilesystemCephAdapterImpl adapter;
 
 	public boolean isValidOSI(String registrationId) {
 		// To do call packet info to get osi data then call each validation
@@ -13,7 +22,23 @@ public class OSIValidator {
 		return false;
 	}
 
-	private boolean isValidOperator(OsiData osiData) {
+	private boolean isValidOperator(OsiData osiData) throws IOException {
+
+		boolean isOperatorValidatorFingerprint = false;
+		String Uin = osiData.getOperatorId();
+		String FingerPrint = osiData.getOperatorFingerprintImage();
+		InputStream FingerPrintFileName = adapter.getFile(Uin, FingerPrint);
+
+		if (validateUIN(Uin)) {
+			isOperatorValidatorFingerprint = adapter.checkFileExistence(Uin, FingerPrint);
+			if (!isOperatorValidatorFingerprint)
+				return false;
+			else {
+				byte[] FingerPrintByte = IOUtils.toByteArray(FingerPrintFileName);
+				return validateOperatorBiometric(Uin, FingerPrintByte);
+			}
+
+		}
 
 		return false;
 	}
@@ -26,5 +51,16 @@ public class OSIValidator {
 	private boolean isValidIntroducer(OsiData osiData) {
 
 		return false;
+	}
+
+	private boolean validateUIN(String input) {
+		// todo To call IAM rest API for UNI validation
+		return true;
+
+	}
+
+	private boolean validateOperatorBiometric(String input, byte[] hmacFileHashByte) {
+		return true;
+
 	}
 }
