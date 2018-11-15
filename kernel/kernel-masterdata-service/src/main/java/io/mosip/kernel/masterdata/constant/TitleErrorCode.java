622c9509f352504023916d7b086eee108e24fd4a
M1047717 <M1047717@A2ML11911.mindtree.com>
2018-11-15 13:35:44 +0530

    MOS-8264 added the Boolean
    Former-commit-id: 41771890e84f32ac85c0c406f24928aee750c08a
    Former-commit-id: 74ca260161121ca9d98496bc4627f73df2db434b

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/TitleErrorCode.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/TitleErrorCode.java
@@ -1,11 +1,16 @@
 package io.mosip.kernel.masterdata.constant;
 
+/**
+ * Enum for error codes for fetching titles from master data
+ * 
+ * @author Sidhant Agarwal
+ * @since 1.0.0
+ *
+ */
 public enum TitleErrorCode {
-	
-	TITLE_FETCH_EXCEPTION("KER-MSD-033",
-			"Error occured while fetching title"), TITLE_MAPPING_EXCEPTION("KER-MSD-034",
-					"Error occured while mapping title"), TITLE_NOT_FOUND("KER-MSD-035",
-							"No title found");
+
+	TITLE_FETCH_EXCEPTION("KER-MSD-033", "Error occured while fetching title"), TITLE_MAPPING_EXCEPTION("KER-MSD-034",
+			"Error occured while mapping title"), TITLE_NOT_FOUND("KER-MSD-035", "No title found");
 
 	private final String errorCode;
 	private final String errorMessage;
@@ -22,6 +27,5 @@ public enum TitleErrorCode {
 	public String getErrorMessage() {
 		return errorMessage;
 	}
-
 
 }
