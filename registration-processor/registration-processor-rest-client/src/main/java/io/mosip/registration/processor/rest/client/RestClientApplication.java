c3dfd922ff58a1e0f8a7487e69ca6464f8b5677b
M1022006 <sowmya.goudar@mindtree.com>
2018-11-20 17:09:09 +0530

    updated bootstrap file for rest client
    Former-commit-id: 5df9ecb2a31a160378c08790a5d7856b99428af4
    Former-commit-id: d2819e8c2e236060eacd3b05cf22e4aa6b4d43d4

--- registration-processor/registration-processor-rest-client/src/main/java/io/mosip/registration/processor/rest/client/RestClientApplication.java
+++ registration-processor/registration-processor-rest-client/src/main/java/io/mosip/registration/processor/rest/client/RestClientApplication.java
@@ -3,12 +3,10 @@ package io.mosip.registration.processor.rest.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(scanBasePackages = { "io.mosip.registration.processor.core",
-"io.mosip.registration.processor.rest.client" })
-@PropertySource({ "classpath:rest-client-application.properties" })
-public class RestClientApplication  {
+		"io.mosip.registration.processor.rest.client" })
+public class RestClientApplication {
 
 	@Autowired
 	public static void main(String[] args) {
