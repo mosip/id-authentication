80c7b709ded3133dde289f2cbc9cad2363addc33
M1044345 <M1044345@A2ML26966.mindtree.com>
2018-11-20 17:54:57 +0530

    Document type create method added
    Former-commit-id: ccc9f226c4a9afafeb4e3fdb418cc2e244fa3c0f
    Former-commit-id: 9f4681cead74a6aa08c32cc498a777cdd221b8b9

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/DocumentTypeService.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/DocumentTypeService.java
@@ -3,17 +3,22 @@ package io.mosip.kernel.masterdata.service;
 import java.util.List;
 
 import io.mosip.kernel.masterdata.dto.DocumentTypeDto;
+import io.mosip.kernel.masterdata.dto.DocumentTypeRequestDto;
+import io.mosip.kernel.masterdata.dto.PostResponseDto;
 
 /**
+ * This interface have methods to fetch list of valid document types and to
+ * create document types based on list provided.
  * 
  * @author Uday Kumar
+ * @author Ritesh Sinha
  * @since 1.0.0
  *
  */
 public interface DocumentTypeService {
 	/*
-	 * methods to fetch list of valid document types for given document category code and
-	 * language code
+	 * methods to fetch list of valid document types for given document category
+	 * code and language code
 	 * 
 	 * @param code
 	 * 
@@ -24,5 +29,14 @@ public interface DocumentTypeService {
 	 * 
 	 */
 	public List<DocumentTypeDto> getAllValidDocumentType(String code, String langCode);
+
+	/**
+	 * Method to create list of document types .
+	 * 
+	 * @param documentTypeDto
+	 *            dto with documents type list.
+	 * @return {@link PostResponseDto}.
+	 */
+	public PostResponseDto addDocumentTypes(DocumentTypeRequestDto documentTypeDto);
 
 }
