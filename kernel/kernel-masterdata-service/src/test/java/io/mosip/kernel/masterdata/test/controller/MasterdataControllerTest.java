d9863d65debdc6c37dfc0febb799a30e0d68fc4f
M1044345 <M1044345@A2ML26966.mindtree.com>
2018-11-20 18:41:26 +0530

    document type test updated
    Former-commit-id: f30142883eca984b3dfd9380b7a665c5d7981521
    Former-commit-id: 22afe747250ee929ad6f212f3279b8b38586976b

--- kernel/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/controller/MasterdataControllerTest.java
+++ kernel/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/controller/MasterdataControllerTest.java
@@ -128,7 +128,7 @@ public class MasterdataControllerTest {
 	@MockBean
 	private DocumentTypeService documentTypeService;
 
-	private final String DOCUMENT_TYPE_EXPECTED = "{ \"documents\": [ { \"code\": \"addhar\", \"name\": \"adhar card\", \"description\": \"Uid\", \"langCode\": \"eng\", \"isActive\": true }, { \"code\": \"residensial\", \"name\": \"residensial_prof\", \"description\": \"document for residential prof\", \"langCode\": \"eng\", \"isActive\": true } ] }";
+	private final String DOCUMENT_TYPE_EXPECTED = "{ \"documents\": [ { \"code\": \"addhar\", \"name\": \"adhar card\", \"description\": \"Uid\", \"langCode\": \"eng\"}, { \"code\": \"residensial\", \"name\": \"residensial_prof\", \"description\": \"document for residential prof\", \"langCode\": \"eng\" } ] }";
 
 	ValidDocumentTypeResponseDto validDocumentTypeResponseDto = null;
 
@@ -291,14 +291,14 @@ public class MasterdataControllerTest {
 		documentType.setName("adhar card");
 		documentType.setDescription("Uid");
 		documentType.setLangCode("eng");
-		documentType.setIsActive(true);
+		// documentType.setIsActive(true);
 		documentTypeDtos.add(documentType);
 		DocumentTypeDto documentType1 = new DocumentTypeDto();
 		documentType1.setCode("residensial");
 		documentType1.setName("residensial_prof");
 		documentType1.setDescription("document for residential prof");
 		documentType1.setLangCode("eng");
-		documentType1.setIsActive(true);
+		// documentType1.setIsActive(true);
 		documentTypeDtos.add(documentType1);
 		validDocumentTypeResponseDto = new ValidDocumentTypeResponseDto(documentTypeDtos);
 	}
