97658711b270f14d607b4712a9559afa39bb56b8
M1022006 <sowmya.goudar@mindtree.com>
2018-11-21 14:57:31 +0530

    Added audit request builder for OSI
    Former-commit-id: 5111a62ae72167d0a5d388537fb62cb745e1fa14
    Former-commit-id: d8881b64175d4c0d8c1df05c47196d4291220038

--- registration-processor/stages/OSI-validator/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidatorStage.java
+++ registration-processor/stages/OSI-validator/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidatorStage.java
@@ -11,8 +11,12 @@ import io.mosip.registration.processor.core.abstractverticle.MessageBusAddress;
 import io.mosip.registration.processor.core.abstractverticle.MessageDTO;
 import io.mosip.registration.processor.core.abstractverticle.MosipEventBus;
 import io.mosip.registration.processor.core.abstractverticle.MosipVerticleManager;
+import io.mosip.registration.processor.core.code.EventId;
+import io.mosip.registration.processor.core.code.EventName;
+import io.mosip.registration.processor.core.code.EventType;
 import io.mosip.registration.processor.core.spi.restclient.RegistrationProcessorRestClientService;
 import io.mosip.registration.processor.filesystem.ceph.adapter.impl.FilesystemCephAdapterImpl;
+import io.mosip.registration.processor.rest.client.audit.builder.AuditLogRequestBuilder;
 import io.mosip.registration.processor.stages.osivalidator.exception.utils.ExceptionMessages;
 import io.mosip.registration.processor.stages.osivalidator.utils.StatusMessage;
 import io.mosip.registration.processor.status.code.RegistrationStatusCode;
@@ -25,6 +29,8 @@ import io.vertx.core.logging.LoggerFactory;
 @RefreshScope
 @Service
 public class OSIValidatorStage extends MosipVerticleManager {
+
+	private static final String USER = "MOSIP_SYSTEM";
 
 	/** The log. */
 	private static Logger log = LoggerFactory.getLogger(OSIValidatorStage.class);
@@ -44,6 +50,9 @@ public class OSIValidatorStage extends MosipVerticleManager {
 	@Autowired
 	RegistrationProcessorRestClientService<Object> restClientService;
 
+	@Autowired
+	AuditLogRequestBuilder auditLogRequestBuilder;
+
 	/**
 	 * Deploy verticle.
 	 */
@@ -57,6 +66,8 @@ public class OSIValidatorStage extends MosipVerticleManager {
 		object.setMessageBusAddress(MessageBusAddress.OSI_BUS_IN);
 		object.setIsValid(Boolean.FALSE);
 		object.setInternalError(Boolean.FALSE);
+		String description = "";
+		boolean isTransactionSuccessful = false;
 
 		String registrationId = object.getRid();
 		boolean isValidOSI = false;
@@ -68,32 +79,52 @@ public class OSIValidatorStage extends MosipVerticleManager {
 		try {
 
 			isValidOSI = osiValidator.isValidOSI(registrationId);
+			if (isValidOSI) {
+				object.setIsValid(Boolean.TRUE);
+				registrationStatusDto.setStatusComment(StatusMessage.OSI_VALIDATION_SUCCESS);
+				registrationStatusDto.setStatusCode(RegistrationStatusCode.PACKET_OSI_VALIDATION_SUCCESSFUL.toString());
+				isTransactionSuccessful = true;
+				description = "OSI validation successful for registration id : " + registrationId;
+			} else {
+				object.setIsValid(Boolean.FALSE);
+				if (registrationStatusDto.getRetryCount() == null) {
+					registrationStatusDto.setRetryCount(0);
+				} else {
+					registrationStatusDto.setRetryCount(registrationStatusDto.getRetryCount() + 1);
+				}
+				registrationStatusDto.setStatusComment(osiValidator.registrationStatusDto.getStatusComment());
+				registrationStatusDto.setStatusCode(RegistrationStatusCode.PACKET_OSI_VALIDATION_FAILED.toString());
+
+				description = "OSI validation Failed for registration id : " + registrationId;
+			}
+			registrationStatusDto.setUpdatedBy(USER);
+			registrationStatusService.updateRegistrationStatus(registrationStatusDto);
 
 		} catch (IOException e) {
 			log.error(ExceptionMessages.OSI_VALIDATION_FAILED.name(), e);
 			object.setInternalError(Boolean.TRUE);
+			description = "Internal error occured while processing registration  id : " + registrationId;
 
 		} catch (Exception ex) {
 			log.error(ExceptionMessages.OSI_VALIDATION_FAILED.name(), ex);
 			object.setInternalError(Boolean.TRUE);
+			description = "Internal error occured while processing registration  id : " + registrationId;
+		} finally {
+
+			String eventId = "";
+			String eventName = "";
+			String eventType = "";
+			eventId = isTransactionSuccessful ? EventId.RPR_402.toString() : EventId.RPR_405.toString();
+			eventName = eventId.equalsIgnoreCase(EventId.RPR_402.toString()) ? EventName.UPDATE.toString()
+					: EventName.EXCEPTION.toString();
+			eventType = eventId.equalsIgnoreCase(EventId.RPR_402.toString()) ? EventType.BUSINESS.toString()
+					: EventType.SYSTEM.toString();
+
+			auditLogRequestBuilder.createAuditRequestBuilder(description, eventId, eventName, eventType,
+					registrationId);
 
 		}
 
-		if (isValidOSI) {
-			object.setIsValid(Boolean.TRUE);
-			registrationStatusDto.setStatusComment(StatusMessage.OSI_VALIDATION_SUCCESS);
-			registrationStatusDto.setStatusCode(RegistrationStatusCode.PACKET_OSI_VALIDATION_SUCCESSFUL.toString());
-		} else {
-			object.setIsValid(Boolean.FALSE);
-			if (registrationStatusDto.getRetryCount() == null) {
-				registrationStatusDto.setRetryCount(0);
-			} else {
-				registrationStatusDto.setRetryCount(registrationStatusDto.getRetryCount() + 1);
-			}
-			registrationStatusDto.setStatusComment(osiValidator.registrationStatusDto.getStatusComment());
-			registrationStatusDto.setStatusCode(RegistrationStatusCode.PACKET_OSI_VALIDATION_FAILED.toString());
-		}
-		registrationStatusService.updateRegistrationStatus(registrationStatusDto);
 		return object;
 	}
 
