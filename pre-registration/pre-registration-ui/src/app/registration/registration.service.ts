19fcf706ee39c57bd390d16b75a23150c5969ef0
Agnitra Banerjee <M1042928@mindtree.com>
2018-11-21 11:18:09 +0530

    local commit
    Former-commit-id: d158d89aa0ea45e58fcf9ed627e04b2fdc660757
    Former-commit-id: a0711bdb50c1058540e13d99a10ce123ecd1fb0d

--- pre-registration/pre-registration-ui/src/app/registration/registration.service.ts
+++ pre-registration/pre-registration-ui/src/app/registration/registration.service.ts
@@ -15,12 +15,13 @@ export class RegistrationService {
 
   SEND_FILE_URL =
     'http://preregistration-intgra.southindia.cloudapp.azure.com/int-demographic/v0.1/pre-registration/registration/documents';
+ // BASE_URL = 'http://A2ML29862:9092/v0.1/pre-registration/applications';
   BASE_URL =
     'http://preregistration-intgra.southindia.cloudapp.azure.com/int-demographic/v0.1/pre-registration/applications';
   // obj: JSON;  yyyy-MM-ddTHH:mm:ss.SSS+000
   // https://pre-reg-df354.firebaseio.com/applications.json
   getUsers(value) {
-    value = 'User1';
+    value = 'mosip.pre-registration.demographic.create';
     return this.httpClient.get<Applicant[]>(this.BASE_URL, {
       observe: 'body',
       responseType: 'json',
