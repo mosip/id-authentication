0e448674fd2ee86f65431e21a70c6b6f0f4a3701
Nasir Khan <M1001063@mindtree.com>
2018-11-21 10:30:06 +0530

    Uploaded the WIP data architecture document and moved the fk creation to new file
    Former-commit-id: f247dfdf7dc12f1bb448f843410e758afd9569e8
    Former-commit-id: 55203f34e4026ed07a09aa27e4cdcc93180f2eec

--- database-scripts/mosip_uin/ddl/uin-uin_detail.sql
+++ database-scripts/mosip_uin/ddl/uin-uin_detail.sql
@@ -32,13 +32,6 @@ COMMENT ON COLUMN uin.uin_detail.del_dtimes IS 'Record deleted datetime';
 ALTER TABLE uin.uin_detail OWNER TO appadmin;
 -- ddl-end --
 
--- object: uind_uin_fk | type: CONSTRAINT --
--- ALTER TABLE uin.uin_detail DROP CONSTRAINT IF EXISTS uind_uin_fk CASCADE;
-ALTER TABLE uin.uin_detail ADD CONSTRAINT uind_uin_fk FOREIGN KEY (uin_ref_id)
-REFERENCES uin.uin (uin_ref_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
 -- object: uind_uk | type: CONSTRAINT --
 -- ALTER TABLE uin.uin_detail DROP CONSTRAINT IF EXISTS uind_uk CASCADE;
 ALTER TABLE uin.uin_detail ADD CONSTRAINT uind_uk UNIQUE (uin_ref_id);
