c5ab081f52144f676fe84274a25fea493a856137
M1047717 <M1047717@A2ML11911.mindtree.com>
2018-11-22 18:53:04 +0530

    modified comments
    Former-commit-id: e7fa9a5ab886085999f36460b74dbc791be56222
    Former-commit-id: 513eec5ed70bdf3361e7fe8ccaa356cb1e78adff

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/DeviceTypeController.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/DeviceTypeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.mosip.kernel.masterdata.dto.DeviceSpecificationRequestDto;
 import io.mosip.kernel.masterdata.dto.DeviceTypeRequestDto;
 import io.mosip.kernel.masterdata.dto.PostResponseDto;
 import io.mosip.kernel.masterdata.service.DeviceTypeService;
@@ -15,10 +16,18 @@ import io.mosip.kernel.masterdata.service.DeviceTypeService;
 public class DeviceTypeController {
 
 	/**
-	 * Reference to MachineDetailService.
+	 * Reference to deviceTypeService.
 	 */
 	@Autowired
 	private DeviceTypeService deviceTypeService;
+	
+	/**
+	 * Save list of device Type details to the DB
+	 * 
+	 * @param DeviceTypeRequestDto
+	 *            
+	 * @return {@link PostResponseDto}
+	 */
 
 	@PostMapping(value = "/")
 	public PostResponseDto addDeviceTypes(@RequestBody DeviceTypeRequestDto deviceTypes) {
