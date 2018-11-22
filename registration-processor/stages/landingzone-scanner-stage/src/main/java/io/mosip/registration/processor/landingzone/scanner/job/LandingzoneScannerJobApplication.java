cb985121a9b8c3ff1b257c1a893034aa351fb3a7
M1022006 <sowmya.goudar@mindtree.com>
2018-11-22 13:19:41 +0530

    missed config files added
    Former-commit-id: 96a51c972d531867f1b978b83bd148e9ba194317
    Former-commit-id: ec578e88417ae5e7929ce0cb3c0e34c348c7cc4e

--- registration-processor/stages/landingzone-scanner-stage/src/main/java/io/mosip/registration/processor/landingzone/scanner/job/LandingzoneScannerJobApplication.java
+++ registration-processor/stages/landingzone-scanner-stage/src/main/java/io/mosip/registration/processor/landingzone/scanner/job/LandingzoneScannerJobApplication.java
@@ -5,15 +5,12 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 import io.mosip.registration.processor.landingzone.scanner.job.stage.LandingzoneScannerStage;
 
-@SpringBootApplication(scanBasePackages = {"io.mosip.registration.processor.landingzone.scanner.job",
-											"io.mosip.registration.processor.core",
-											"io.mosip.registration.processor.status", 
-											"io.mosip.registration.processor.packet.manager",
-											"io.mosip.registration.processor.auditmanager"})
+@SpringBootApplication(scanBasePackages = { "io.mosip.registration.processor.landingzone.scanner.job",
+		"io.mosip.registration.processor.core", "io.mosip.registration.processor.status",
+		"io.mosip.registration.processor.packet.manager", "io.mosip.registration.processor.rest.client" })
 public class LandingzoneScannerJobApplication {
 
 	@Autowired
@@ -21,7 +18,7 @@ public class LandingzoneScannerJobApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LandingzoneScannerJobApplication.class, args);
-	} 
+	}
 
 	@PostConstruct
 	public void deployVerticle() {
