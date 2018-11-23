6e0f6923f93ed1e43a9c17195316307bb7088de1
bantiz <Balvikash.Sharma@mindtree.com>
2018-11-23 15:12:46 +0530

    MOS-1075 include json property to ignore null values
    Former-commit-id: f5f7518d6ece46df73d495fab220243efc7a5b01
    Former-commit-id: 8af5644bd0d1e8be9fd1b075b5ecd536a7b70e22

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/LanguageRequestResponseDto.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/LanguageRequestResponseDto.java
@@ -2,6 +2,9 @@ package io.mosip.kernel.masterdata.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +19,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class LanguageRequestResponseDto {
 
 	/**
