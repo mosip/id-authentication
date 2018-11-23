c7f84f44c668cb749f42dfd87f70d177bdd76a32
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2018-11-23 16:23:37 +0530

    Fix in demo app.
    Former-commit-id: 1d6f1c2d635228985a630299d0576fca87bcd9f8
    Former-commit-id: 230f3793e1ef69d78483b8add92edfdff169173a

--- authentication/authentication-demo/src/main/java/io/mosip/demo/authentication/service/impl/indauth/controller/PDFGenerator.java
+++ authentication/authentication-demo/src/main/java/io/mosip/demo/authentication/service/impl/indauth/controller/PDFGenerator.java
@@ -1,11 +1,8 @@
 package io.mosip.demo.authentication.service.impl.indauth.controller;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.util.Base64;
-
-import javax.ws.rs.core.Response;
 
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -23,14 +20,13 @@ public class PDFGenerator {
 	@PostMapping(path = "/decodeToFile", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_PDF_VALUE)
 	public ResponseEntity<InputStreamResource> decode(@RequestBody String stringToDecode) throws IOException {
 		byte[] decode = Base64.getDecoder().decode(stringToDecode);
-		File file = new File(PDF_PATH);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
 		headers.add("Pragma", "no-cache");
-		headers.add("Content-Disposition", "attachment; filename=e-KYC.pdf");
+		headers.add("Content-Disposition", "attachment; filename=" + PDF_PATH);
 		headers.add("Expires", "0");
 		InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(decode));
-		return ResponseEntity.ok().headers(headers).contentLength(file.length())
+		return ResponseEntity.ok().headers(headers).contentLength(decode.length)
 				.contentType(MediaType.parseMediaType("application/octet-stream")).body(resource);
 	}
 }
