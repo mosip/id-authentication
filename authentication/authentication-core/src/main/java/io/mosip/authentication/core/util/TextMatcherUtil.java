87544126d5f10e9197f4efe1e803fbb8feea3c18
Rakesh Roshan <Rakesh.Roshan@mindtree.com>
2018-11-23 19:17:57 +0530

    Fixed sonar issue TextMatcherUtil
    Former-commit-id: 2746cfa0289f51d5853137e7ef6eb7a2e8536e8a
    Former-commit-id: 470db7e0c8175fa1ed0a0a895e7b97f6807f18aa

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/util/TextMatcherUtil.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/util/TextMatcherUtil.java
@@ -14,6 +14,8 @@ import org.apache.commons.codec.language.bm.PhoneticEngine;
 import org.apache.commons.codec.language.bm.RuleType;
 
 import ch.qos.logback.core.util.FileUtil;
+import io.mosip.authentication.core.logger.IdaLogger;
+import io.mosip.kernel.core.logger.spi.Logger;
 
 /**
  * The Class TextMatcherUtil is used to match two input strings and to provide
@@ -23,120 +25,111 @@ import ch.qos.logback.core.util.FileUtil;
  */
 public final class TextMatcherUtil {
 
-    /** The Constant MOSIP_PROPERTIES_FILE. */
-    private static final String MOSIP_PROPERTIES_FILE = "/mosip.properties";
+	/** The Constant MOSIP_PROPERTIES_FILE. */
+	private static final String MOSIP_PROPERTIES_FILE = "/mosip.properties";
 
-    /** The Constant THRESHOLD_PROPERTY. */
-    private static final String THRESHOLD_PROPERTY = "mosip.textmatcher.threshold";
+	/** The Constant THRESHOLD_PROPERTY. */
+	private static final String THRESHOLD_PROPERTY = "mosip.textmatcher.threshold";
 
-    /**
-     * Gets the threshold from property file.
-     * 
-     * TODO need to check whether to return null or error if threshold validation
-     * fails.
-     *
-     * @return the threshold
-     */
-    private static final Integer getThreshold() {
-	Integer threshold = null;
-	threshold = Integer.parseInt(getProperty(THRESHOLD_PROPERTY));
-	if (validateThreshold(threshold)) {
-	    return threshold;
-	} else {
-	    return null;
-	}
-    }
+	private static Logger mosipLogger = IdaLogger.getLogger(TextMatcherUtil.class);
 
-    /**
-     * Gets the properties from property file.
-     *
-     * TODO need to use {@link FileUtil}
-     *
-     * @param property
-     *            the property
-     * @return the property
-     */
-    private static final String getProperty(String property) {
-	InputStream propertiesFileStream = null;
+	private static final String SESSION_ID = "sessionId";
 
-	Properties textMatcherProp = new Properties();
-
-	try {
-	    propertiesFileStream = TextMatcherUtil.class.getClass().getResourceAsStream(MOSIP_PROPERTIES_FILE);
-	    textMatcherProp.load(propertiesFileStream);
-	} catch (IOException e) {
-	    System.err.println(e.getMessage());
+	private TextMatcherUtil() {
 	}
 
-	return textMatcherProp.getProperty(property);
-    }
-
-    /**
-     * Validates whether threshold is between 0 and 100 and is not null.
-     *
-     * TODO need to check whether to return false or error if threshold validation
-     * fails.
-     * 
-     * @param threshold
-     *            the threshold
-     * @return true, if successful
-     */
-    private static final boolean validateThreshold(Integer threshold) {
-	return threshold >= 0 && threshold <= 100;
-    }
-
-    /**
-     * This method gets two strings as input along with their language and gets
-     * their phonetic values using PhoneticEngine class. Then the phonetic matching
-     * probability is calculated using Soundex class and validated against user
-     * threshold or default configured threshold. If the matching probability of
-     * both phonetic strings are greater than or equal to the given threshold, then
-     * the result is true, else false.
-     *
-     * TODO 1. need to validate the language given is supported or not. 2. need to
-     * log exceptions using Logger
-     * 
-     * @param inputString
-     *            the input string
-     * @param storedString
-     *            the stored string
-     * @param threshold
-     *            the threshold
-     * @param language
-     *            the language
-     * @return true, if successful
-     */
-    public static final boolean phoneticMatch(String inputString, String storedString, Integer threshold,
-	    String language) {
-	if (threshold == null) {
-	    threshold = getThreshold();
+	/**
+	 * Gets the threshold from property file.
+	 *
+	 * @return the threshold
+	 */
+	private static final Integer getThreshold() {
+		Integer threshold = null;
+		threshold = Integer.parseInt(getProperty(THRESHOLD_PROPERTY));
+		if (validateThreshold(threshold)) {
+			return threshold;
+		} else {
+			return null;
+		}
 	}
-	if (validateThreshold(threshold)) {
-	    try {
-		Integer thresholdProbability = phoneticsMatch(inputString, storedString, language);
 
-		return (validateThreshold(thresholdProbability) && (thresholdProbability >= threshold));
-	    } catch (EncoderException e) {
-		System.err.println(e.getMessage());
-	    }
+	/**
+	 * Gets the properties from property file.
+	 *
+	 * @param property the property
+	 * @return the property
+	 */
+	private static final String getProperty(String property) {
+		InputStream propertiesFileStream = null;
+
+		Properties textMatcherProp = new Properties();
+
+		try {
+			propertiesFileStream = TextMatcherUtil.class.getClass().getResourceAsStream(MOSIP_PROPERTIES_FILE);
+			textMatcherProp.load(propertiesFileStream);
+		} catch (IOException e) {
+			mosipLogger.error(SESSION_ID, "file not found", "IOException", e.getMessage());
+		}
+
+		return textMatcherProp.getProperty(property);
 	}
-	return false;
-    }
 
-    public static Integer phoneticsMatch(String inputString, String storedString, String language)
-	    throws EncoderException {
-	PhoneticEngine phoneticEngine = new PhoneticEngine(NameType.GENERIC, RuleType.EXACT, true);
+	/**
+	 * Validates whether threshold is between 0 and 100 and is not null.
+	 * 
+	 * @param threshold the threshold
+	 * @return true, if successful
+	 */
+	private static final boolean validateThreshold(Integer threshold) {
+		return threshold >= 0 && threshold <= 100;
+	}
 
-	Soundex soundex = new Soundex();
+	/**
+	 * This method gets two strings as input along with their language and gets
+	 * their phonetic values using PhoneticEngine class. Then the phonetic matching
+	 * probability is calculated using Soundex class and validated against user
+	 * threshold or default configured threshold. If the matching probability of
+	 * both phonetic strings are greater than or equal to the given threshold, then
+	 * the result is true, else false.
+	 *
+	 * 
+	 * @param inputString  the input string
+	 * @param storedString the stored string
+	 * @param threshold    the threshold
+	 * @param language     the language
+	 * @return true, if successful
+	 */
+	public static final boolean phoneticMatch(String inputString, String storedString, Integer threshold,
+			String language) {
+		if (threshold == null) {
+			threshold = getThreshold();
+		}
+		if (validateThreshold(threshold)) {
+			try {
+				Integer thresholdProbability = phoneticsMatch(inputString, storedString, language);
 
-	Set<String> languageSet = new HashSet<>();
-	languageSet.add(language);
+				return (validateThreshold(thresholdProbability) && (thresholdProbability >= threshold));
+			} catch (EncoderException e) {
+				mosipLogger.error(SESSION_ID, "Encoding", "EncoderException", e.getMessage());
+			}
+		}
+		return false;
+	}
 
-	String encodedInputString = phoneticEngine.encode(inputString, Languages.LanguageSet.from(languageSet));
+	public static Integer phoneticsMatch(String inputString, String storedString, String language)
+			throws EncoderException {
+		PhoneticEngine phoneticEngine = new PhoneticEngine(NameType.GENERIC, RuleType.EXACT, true);
 
-	String encodedStoredString = phoneticEngine.encode(storedString, Languages.LanguageSet.from(languageSet));
+		Soundex soundex = new Soundex();
 
-	return (soundex.difference(encodedInputString, encodedStoredString) + 1) * 20;
-    }
+		Set<String> languageSet = new HashSet<>();
+		languageSet.add(language);
+
+		String encodedInputString = phoneticEngine.encode(inputString, Languages.LanguageSet.from(languageSet));
+
+		String encodedStoredString = phoneticEngine.encode(storedString, Languages.LanguageSet.from(languageSet));
+
+		return (soundex.difference(encodedInputString, encodedStoredString) + 1) * 20;
+	}
 
 }
