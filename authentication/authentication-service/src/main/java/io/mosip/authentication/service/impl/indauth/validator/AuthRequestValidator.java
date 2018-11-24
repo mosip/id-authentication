671e076d412eb67001f118d3bd4c536399f908b4
Rakesh Roshan <Rakesh.Roshan@mindtree.com>
2018-11-24 11:15:57 +0530

    Fixed primary language issue - AuthRequestValidator
    Former-commit-id: 74c64f1bd242f63fda98dcb10f900cbce42d3306
    Former-commit-id: e53b5addefdc2b80a3061c11e1fc8231051d6042

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/validator/AuthRequestValidator.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/validator/AuthRequestValidator.java
@@ -493,7 +493,7 @@ public class AuthRequestValidator extends BaseAuthRequestValidator {
 	boolean anyOtherLang = langCount.keySet().stream()
 		.anyMatch(lang -> lang != null && !lang.equals(priLangCode) && !lang.equals(secLangCode));
 
-	if (primaryLangCount == null || primaryLangCount > 1 || anyOtherLang) {
+	if (primaryLangCount != null && primaryLangCount > 1 || anyOtherLang) {
 	    mosipLogger.error(SESSION_ID, AUTH_REQUEST_VALIDATOR, INVALID_INPUT_PARAMETER,
 		    "Invalid or Multiple Primary language code");
 	    errors.rejectValue(REQUEST, IdAuthenticationErrorConstants.INVALID_INPUT_PARAMETER.getErrorCode(),
