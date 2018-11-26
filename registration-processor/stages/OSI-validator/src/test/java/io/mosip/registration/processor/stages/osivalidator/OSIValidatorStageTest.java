f7bc6564a8d6432891c53c72daf8f98967bfcf4f
M1039285 <M1039285@a2ml16222.mindtree.com>
2018-11-26 12:26:50 +0530

    OSI test case updated
    Former-commit-id: 9afc641a91ce0cf2f3993dc1f37f947d78500cfb
    Former-commit-id: a8582683f289dc11b20264e358a1ab9f3a2e7a8b

--- registration-processor/stages/OSI-validator/src/test/java/io/mosip/registration/processor/stages/osivalidator/OSIValidatorStageTest.java
+++ registration-processor/stages/OSI-validator/src/test/java/io/mosip/registration/processor/stages/osivalidator/OSIValidatorStageTest.java
@@ -29,7 +29,6 @@ import io.mosip.registration.processor.core.abstractverticle.MosipEventBus;
 import io.mosip.registration.processor.core.code.EventId;
 import io.mosip.registration.processor.core.code.EventName;
 import io.mosip.registration.processor.core.code.EventType;
-import io.mosip.registration.processor.core.exception.ApisResourceAccessException;
 import io.mosip.registration.processor.core.packet.dto.Identity;
 import io.mosip.registration.processor.core.packet.dto.RegOsiDto;
 import io.mosip.registration.processor.core.spi.packetmanager.PacketInfoManager;
@@ -101,26 +100,33 @@ public class OSIValidatorStageTest {
 		regOsiDto.setOfficerId("O1234");
 		regOsiDto.setOfficerFingerpImageName("fingerprint");
 		regOsiDto.setOfficerfingerType("RIGHTRING");
-		regOsiDto.setOfficerIrisImageName("officerIrisImageName");
+		regOsiDto.setOfficerIrisImageName(null);
 		regOsiDto.setOfficerIrisType("LEFTEYE");
-		regOsiDto.setOfficerPhotoName("officerPhotoName");
+		regOsiDto.setOfficerPhotoName(null);
 		regOsiDto.setOfficerHashedPin("officerHashedPin");
 
 		regOsiDto.setSupervisorId("S1234");
 		regOsiDto.setSupervisorFingerpImageName("supervisorFingerpImageName");
 		regOsiDto.setSupervisorFingerType("LEFTINDEX");
 		regOsiDto.setSupervisorIrisImageName("supervisorIrisImageName");
-		regOsiDto.setSupervisorIrisType("RIGHTEYE");
+		regOsiDto.setSupervisorIrisType("LEFTEYE");
 		regOsiDto.setSupervisorPhotoName("supervisorPhotoName");
 
 		regOsiDto.setIntroducerUin("I1234");
 		regOsiDto.setIntroducerFingerpImageName("introducerFingerpImageName");
 		regOsiDto.setIntroducerFingerpType("RIGHTRING");
-		regOsiDto.setIntroducerIrisImageName("introducerIrisImageName");
+		regOsiDto.setIntroducerIrisImageName("IntroducerIrisImageName");
 		regOsiDto.setIntroducerIrisType("RIGHTEYE");
 
 		Mockito.when(adapter.getFile(anyString(), anyString())).thenReturn(inputStream);
 		Mockito.when(adapter.checkFileExistence(anyString(), anyString())).thenReturn(true);
+
+		PowerMockito.mockStatic(IOUtils.class);
+		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
+
+		authResponseDTO.setStatus(true);
+		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
+				.thenReturn(authResponseDTO);
 
 		@SuppressWarnings("unchecked")
 		RegistrationProcessorRestClientService<Object> mockObj = Mockito
@@ -135,35 +141,29 @@ public class OSIValidatorStageTest {
 				EventType.BUSINESS.toString(), "1234testcase");
 
 		dto.setRid("reg1234");
-
 		registrationStatusDto.setRegistrationId("reg1234");
+		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
 
 	}
 
 	@Test
 	public void testisValidOSISuccess() throws Exception {
-		authResponseDTO.setStatus(true);
+
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
+
 		assertTrue(messageDto.getIsValid());
 
 	}
 
 	@Test
-	public void testOfficeIdNull() throws Exception {
+	public void testOfficeIntroIdNull() throws Exception {
 		regOsiDto.setOfficerId(null);
-		authResponseDTO.setStatus(true);
+		regOsiDto.setIntroducerUin(null);
+
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertTrue(messageDto.getIsValid());
 	}
@@ -171,29 +171,21 @@ public class OSIValidatorStageTest {
 	@Test
 	public void testSupervisorIdNull() throws Exception {
 		regOsiDto.setSupervisorId(null);
-		authResponseDTO.setStatus(true);
+
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenReturn(authResponseDTO);
 		MessageDTO messageDto = osiValidatorStage.process(dto);
+
 		assertFalse(messageDto.getIsValid());
 	}
 
 	@Test
-	public void testDetailsNull() throws Exception {
+	public void testSupervisorDetailsNull() throws Exception {
 		regOsiDto.setSupervisorFingerpImageName(null);
 		regOsiDto.setSupervisorIrisImageName(null);
 		regOsiDto.setSupervisorPhotoName(null);
-		authResponseDTO.setStatus(true);
+
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertFalse(messageDto.getIsValid());
 	}
@@ -201,17 +193,12 @@ public class OSIValidatorStageTest {
 	@Test
 	public void testOfficerDetailsNull() throws Exception {
 		regOsiDto.setOfficerFingerpImageName(null);
-
 		regOsiDto.setOfficerIrisImageName(null);
-
 		regOsiDto.setOfficerPhotoName(null);
 		regOsiDto.setOfficerHashedPin(null);
+
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertFalse(messageDto.getIsValid());
 	}
@@ -219,57 +206,34 @@ public class OSIValidatorStageTest {
 	@Test
 	public void testIntroducerDetailsNull() throws Exception {
 		regOsiDto.setIntroducerFingerpImageName(null);
-
 		regOsiDto.setIntroducerIrisImageName(null);
 
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertFalse(messageDto.getIsValid());
 	}
 
 	@Test
 	public void IOExceptionTest() throws Exception {
-		authResponseDTO.setStatus(true);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
+
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
 		PowerMockito.mockStatic(IOUtils.class);
 		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenThrow(new IOException());
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertEquals(true, messageDto.getInternalError());
 
 	}
 
-	@Test(expected = ApisResourceAccessException.class)
-	public void validateBiometricTestFailureTest() throws ApisResourceAccessException {
-
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenThrow(ApisResourceAccessException.class);
-		// Mockito.when(
-		// oSIValidator.isValidOSI(anyString()).thenThrow(Exception.class);
-		// osiValidator.isValidOSI(registrationId);
-		boolean messageDto = oSIValidator.validateBiometric("2018701130000410092018110735", "FINGER", "LEFTTHUMB",
-				data);
-	}
-
 	@Test
 	public void testisValidOSIFailure() throws Exception {
 		authResponseDTO.setStatus(false);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
-		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
 
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
+		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
 		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
 				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertFalse(messageDto.getIsValid());
 	}
@@ -278,13 +242,11 @@ public class OSIValidatorStageTest {
 	public void testisValidSupervisorFailure() throws Exception {
 		regOsiDto.setOfficerId(null);
 		authResponseDTO.setStatus(false);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
-		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
 
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
+		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
 		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
 				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertFalse(messageDto.getIsValid());
 	}
@@ -292,30 +254,26 @@ public class OSIValidatorStageTest {
 	@Test
 	public void testisValidOSIFailurewithRetry() throws Exception {
 		authResponseDTO.setStatus(false);
-		PowerMockito.mockStatic(IOUtils.class);
-		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(data);
+
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
 		InternalRegistrationStatusDto registrationStatusDto = new InternalRegistrationStatusDto();
 		registrationStatusDto.setRetryCount(1);
 		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
 		Mockito.doNothing().when(registrationStatusService).updateRegistrationStatus(registrationStatusDto);
-
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
 		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
 				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertFalse(messageDto.getIsValid());
 	}
 
 	@Test
 	public void testException() throws Exception {
-		authResponseDTO.setStatus(true);
+
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsiDto);
 		PowerMockito.mockStatic(IOUtils.class);
 		PowerMockito.when(IOUtils.class, "toByteArray", inputStream).thenReturn(null);
-		Mockito.when(registrationStatusService.getRegistrationStatus(anyString())).thenReturn(registrationStatusDto);
-		Mockito.when(restClientService.postApi(any(), anyString(), anyString(), anyString(), any()))
-				.thenReturn(authResponseDTO);
+
 		MessageDTO messageDto = osiValidatorStage.process(dto);
 		assertEquals(true, messageDto.getInternalError());
 
