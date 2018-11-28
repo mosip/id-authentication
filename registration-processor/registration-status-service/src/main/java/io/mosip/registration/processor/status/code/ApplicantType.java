f33cb442678069a5a82778dd31fd5b2b80f5d191
M1039285 <M1039285@a2ml16222.mindtree.com>
2018-11-28 15:24:10 +0530

    Applicant type
    Former-commit-id: 85a3140219e842c808f672d3436c1f243b001c61
    Former-commit-id: 58e8e09d39956d6b3cce8f55ae65056c73551397

---
+++ registration-processor/registration-status-service/src/main/java/io/mosip/registration/processor/status/code/ApplicantType.java
@@ -0,0 +1,13 @@
+package io.mosip.registration.processor.status.code;
+
+/**
+ * The Enum ApplicantType.
+ */
+public enum ApplicantType {
+
+	/** The child. */
+	CHILD,
+	/** The adult. */
+	ADULT
+
+}
