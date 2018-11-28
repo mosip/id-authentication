8acf41c2beef6dadbcc5a333320f8042b4b2c09d
Alok Ranjan <Alok.Ranjan2@mindtree.com>
2018-11-28 15:13:40 +0530

    MOS-1064 deployed demo dedupe vertical
    Former-commit-id: 678dd6478a556f0584be942da9c2b1b6fce8acc6
    Former-commit-id: 0c1679ea8295ea0c4f45fbdd6b904cd18e56b5e5

--- registration-processor/stages/Demo-dedupe-stage/src/main/java/io/mosip/registrationprocessor/stages/demodedupe/DemodedupeStage.java
+++ registration-processor/stages/Demo-dedupe-stage/src/main/java/io/mosip/registrationprocessor/stages/demodedupe/DemodedupeStage.java
@@ -1,5 +1,6 @@
 package io.mosip.registrationprocessor.stages.demodedupe;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
@@ -8,10 +9,43 @@ import io.mosip.registration.processor.core.abstractverticle.MessageBusAddress;
 import io.mosip.registration.processor.core.abstractverticle.MessageDTO;
 import io.mosip.registration.processor.core.abstractverticle.MosipEventBus;
 import io.mosip.registration.processor.core.abstractverticle.MosipVerticleManager;
+import io.mosip.registration.processor.core.packet.dto.Identity;
+import io.mosip.registration.processor.core.spi.packetmanager.PacketInfoManager;
+import io.mosip.registration.processor.packet.storage.dto.ApplicantInfoDto;
+import io.mosip.registration.processor.rest.client.audit.builder.AuditLogRequestBuilder;
+import io.mosip.registration.processor.status.dto.InternalRegistrationStatusDto;
+import io.mosip.registration.processor.status.dto.RegistrationStatusDto;
+import io.mosip.registration.processor.status.service.RegistrationStatusService;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+/**
+ * The Class PacketValidatorStage.
+ *
+ * @author M1048358 Alok Ranjan
+ * 		   Girish Yarru 
+ */
 
 @RefreshScope
 @Service
 public class DemodedupeStage extends MosipVerticleManager {
+	
+	/** The Constant FILE_SEPARATOR. */
+	public static final String FILE_SEPARATOR = "\\";
+
+	/** The log. */
+	private static Logger log = LoggerFactory.getLogger(DemodedupeStage.class);
+	
+	/** The Constant USER. */
+	private static final String USER = "MOSIP_SYSTEM";
+
+	/** The registration status service. */
+	@Autowired
+	RegistrationStatusService<String, InternalRegistrationStatusDto, RegistrationStatusDto> registrationStatusService;
+
+	/** The packet info manager. */
+	@Autowired
+	private PacketInfoManager<Identity, ApplicantInfoDto> packetInfoManager;
 	
 	@Value("${registration.processor.vertx.cluster.address}")
 	private String clusterAddress;
@@ -19,6 +53,13 @@ public class DemodedupeStage extends MosipVerticleManager {
 	@Value("${registration.processor.vertx.localhost}")
 	private String localhost;
 	
+	/** The core audit request builder. */
+	@Autowired
+	AuditLogRequestBuilder auditLogRequestBuilder;
+	
+	/**
+	 * Deploy verticle.
+	 */
 	public void deployVerticle() {
 		MosipEventBus mosipEventBus = this.getEventBus(this.getClass(), clusterAddress, localhost);
 		this.consumeAndSend(mosipEventBus, MessageBusAddress.DEMODEDUPE_BUS_IN, MessageBusAddress.DEMODEDUPE_BUS_IN);
