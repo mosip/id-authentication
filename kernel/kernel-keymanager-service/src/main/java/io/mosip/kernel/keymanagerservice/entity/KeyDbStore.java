e8ce4fb03cd27e2bca449dc17daedc415d0b36c9
Dharmesh Khandelwal <Dharmesh.Khandelwal@mindtree.com>
2018-11-29 20:07:30 +0530

    Added keystore entity and repo
    Former-commit-id: d8d393727a10c6d67d89f9175e014adac01393c7
    Former-commit-id: 80294a0d8b91ccf1d1a83fb9c0047cbad33ef118

---
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/entity/KeyDbStore.java
@@ -0,0 +1,36 @@
+package io.mosip.kernel.keymanagerservice.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Dharmesh Khandelwal
+ * @since 1.0.0
+ *
+ */
+@Entity
+@Table(name = "key_store", schema = "kernel")
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeyDbStore extends BaseEntity {
+
+	@Id
+	@Column(name = "id", nullable = false, length = 36)
+	private String alias;
+
+	@Column(name = "public_key")
+	private byte[] publicKey;
+
+	@Column(name = "private_key")
+	private byte[] privateKey;
+
+}
---
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/repository/KeyStoreRepository.java
@@ -0,0 +1,23 @@
+package io.mosip.kernel.keymanagerservice.repository;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
+import io.mosip.kernel.keymanagerservice.entity.KeyDbStore;
+
+/**
+ * This interface extends BaseRepository which provides with the methods for
+ * several CRUD operations.
+ * 
+ * @author Dharmesh Khandelwal
+ * @since 1.0.0
+ *
+ */
+@Repository
+public interface KeyStoreRepository extends BaseRepository<KeyDbStore, String> {
+
+	Optional<KeyDbStore> findByAlias(String alias);
+
+}
