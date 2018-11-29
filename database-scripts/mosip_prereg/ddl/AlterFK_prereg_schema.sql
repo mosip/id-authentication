5181a6bc9096a37b09925e0829a22cf327987d46
Sadanandegowda Dm <M1045015@mindtree.com>
2018-11-29 15:50:02 +0530

    Created registration available slots and appointment tables
    Former-commit-id: 823b7563461009406593c192166d4aa4da855442
    Former-commit-id: 4ed42efaa119a2f13451835f0944ffe257f27540

--- database-scripts/mosip_prereg/ddl/AlterFK_prereg_schema.sql
+++ database-scripts/mosip_prereg/ddl/AlterFK_prereg_schema.sql
@@ -4,3 +4,4 @@
 -- FOREIGN KEY CONSTRAINTS : mosip_prereg database/schema.
 
 alter table prereg.applicant_document add constraint fk_appldoc_appldem foreign key (prereg_id) references prereg.applicant_demographic(prereg_id) on delete no action on update no action ;
+alter table prereg.reg_appointment add constraint fk_rappmnt_id foreign key (prereg_id) references prereg.applicant_demographic(prereg_id) on delete no action on update no action ;
