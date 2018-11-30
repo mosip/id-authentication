5cd485b59b19896c99a3d57cc5035b449de26df8
Dharmesh Khandelwal <Dharmesh.Khandelwal@mindtree.com>
2018-11-30 20:07:50 +0530

    Changed utility methods
    Former-commit-id: 60919f2143f735de153c5f16e1f98327ed6e52a2
    Former-commit-id: aacd673f1033e57ce299ef41e7dd1c6222cbe1b8

--- kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/util/KeymanagerUtil.java
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/util/KeymanagerUtil.java
@@ -6,6 +6,7 @@ package io.mosip.kernel.keymanagerservice.util;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Component;
 
 import io.mosip.kernel.keymanagerservice.entity.KeyAlias;
@@ -16,7 +17,7 @@ import io.mosip.kernel.keymanagerservice.entity.KeyAlias;
  *
  */
 @Component
-public class MetadataUtil {
+public class KeymanagerUtil {
 
 	public KeyAlias setMetaData(KeyAlias entity) {
 		String contextUser = "defaultadmin@mosip.io";
@@ -25,6 +26,14 @@ public class MetadataUtil {
 		entity.setCreatedtimes(time);
 		entity.setIsDeleted(false);
 		return entity;
+	}
+
+	public String encodeBase64(byte[] binaryData) {
+		return Base64.encodeBase64URLSafeString(binaryData);
+	}
+
+	public byte[] decodeBase64(String base64String) {
+		return Base64.decodeBase64(base64String);
 	}
 
 }
