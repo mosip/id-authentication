707fe8100eaa80f07ac68b82acbdbf9fbbd515f1
Dinesh Ashokan <Dinesh.Ashokan@mindtree.com>
2018-12-04 14:36:23 +0530

    Photo capture design changes as per finger print
    Former-commit-id: 558c1a95f8e30cd6bad6f25ea45165b38131474c
    Former-commit-id: aa639308b964a573bea6b476eb6966fe100a3485

--- registration/registration-service/src/main/java/io/mosip/registration/util/biometric/MosipWebcamProvider.java
+++ registration/registration-service/src/main/java/io/mosip/registration/util/biometric/MosipWebcamProvider.java
@@ -1,4 +1,4 @@
-package io.mosip.registration.service.device;
+package io.mosip.registration.util.biometric;
 
 import java.awt.image.BufferedImage;
 
@@ -9,18 +9,16 @@ import com.github.sarxos.webcam.Webcam;
  *
  * @author Himaja Dhanyamraju
  */
-public interface PhotoCaptureService {
-	
+public interface MosipWebcamProvider {
+
 	/**
 	 * This method is to open the first on-boarded camera from the list of webcams
 	 * with specified resolution.
 	 * 
-	 * @param width
-	 *            Required width for the camera to be set-up.
-	 * @param height
-	 *            Required height for the camera to be set-up.
-	 * @return Webcam returns the webcam that is set-up and opened with the specified width and
-	 *         height.
+	 * @param width  Required width for the camera to be set-up.
+	 * @param height Required height for the camera to be set-up.
+	 * @return Webcam returns the webcam that is set-up and opened with the
+	 *         specified width and height.
 	 */
 	public Webcam connect(int width, int height);
 
@@ -28,8 +26,7 @@ public interface PhotoCaptureService {
 	 * This method captures the image from webcam and return it. Will return image
 	 * object or null if webcam is closed or has been already disposed by JVM.
 	 * 
-	 * @param webcam
-	 *            The on-boarded camera which is open.
+	 * @param webcam The on-boarded camera which is open.
 	 * @return BufferedImage returns the image object that is captured from the
 	 *         webcam.
 	 */
@@ -38,8 +35,7 @@ public interface PhotoCaptureService {
 	/**
 	 * This method is to close the webcam.
 	 * 
-	 * @param webcam
-	 *            The on-boarded camera which is open.
+	 * @param webcam The on-boarded camera which is open.
 	 */
 	public void close(Webcam webcam);
 
