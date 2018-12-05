cdf7b77a43d0de58c09fedc20e28de42285b7555
Ravi Chitragar Balaji <M1046462@mindtree.com>
2018-12-05 08:33:13 +0530

    Pre-Registration code updation Missed one file
    Former-commit-id: 692b836fe83049ea23567ad5060c00d13a5876a2
    Former-commit-id: 8090614c279f86546143dec39065401208213060

--- pre-registration/pre-registration-demographic-service/src/main/java/io/mosip/preregistration/application/exception/system/JsonValidationException.java
+++ pre-registration/pre-registration-demographic-service/src/main/java/io/mosip/preregistration/application/exception/system/JsonValidationException.java
@@ -2,12 +2,14 @@ package io.mosip.preregistration.application.exception.system;
 
 import io.mosip.kernel.core.exception.BaseUncheckedException;
 
-public class JsonValidationException extends BaseUncheckedException  {
+public class JsonValidationException extends BaseUncheckedException {
 
 	private static final long serialVersionUID = 1L;
+
 	public JsonValidationException(String msg) {
 		super("", msg);
 	}
+
 	public JsonValidationException(String errCode, String msg) {
 		super(errCode, msg);
 	}
@@ -15,5 +17,5 @@ public class JsonValidationException extends BaseUncheckedException  {
 	public JsonValidationException(String errCode, String msg, Throwable cause) {
 		super(errCode, msg, cause);
 	}
-	
+
 }
