517981b994ae6a9b6d9b92686c21145ea1d022f6
M1022006 <sowmya.goudar@mindtree.com>
2018-12-05 10:42:57 +0530

    Removed the testing code
    Former-commit-id: e83647d5f5ce75b3bafbd639c25d1ebb1ad6b654
    Former-commit-id: baf2b8ecac97f9303a53e22ca22a31de527f20b3

--- registration-processor/packet-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/PacketInfoManagerApplication.java
+++ registration-processor/packet-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/PacketInfoManagerApplication.java
@@ -1,20 +1,7 @@
 package io.mosip.registration.processor.packet.storage;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.mosip.registration.processor.core.packet.dto.FieldValue;
-import io.mosip.registration.processor.core.packet.dto.Identity;
-import io.mosip.registration.processor.core.spi.packetmanager.PacketInfoManager;
-import io.mosip.registration.processor.packet.storage.dto.ApplicantInfoDto;
 
 /**
  * The Class PacketInfoManagerApplication.
@@ -24,10 +11,7 @@ import io.mosip.registration.processor.packet.storage.dto.ApplicantInfoDto;
 		"io.mosip.registration.processor.auditmanager", "io.mosip.registration.processor.filesystem.ceph.adapter.impl",
 		"io.mosip.registration.processor.rest.client" })
 
-public class PacketInfoManagerApplication implements CommandLineRunner {
-
-	@Autowired
-	PacketInfoManager<Identity, ApplicantInfoDto> packetInfoManager;
+public class PacketInfoManagerApplication {
 
 	/**
 	 * The main method.
@@ -39,23 +23,4 @@ public class PacketInfoManagerApplication implements CommandLineRunner {
 		SpringApplication.run(PacketInfoManagerApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		List<FieldValue> metaDataList = new ArrayList<>();
-		FieldValue regId = new FieldValue();
-		regId.setLabel("registrationId");
-		regId.setValue("2018782130000103122018100224");
-
-		FieldValue preRegId = new FieldValue();
-		preRegId.setLabel("preRegistrationId");
-		preRegId.setValue("PEN1345T");
-
-		metaDataList.add(regId);
-		metaDataList.add(preRegId);
-
-		File jsonFile = new File("..\\packet-info-storage-service\\src\\main\\resources\\DemographicInfo.json");
-		InputStream demoJsonStream = new FileInputStream(jsonFile);
-		packetInfoManager.saveDemographicInfoJson(demoJsonStream, metaDataList);
-
-	}
 }
