0cdab8ae0c6406b855d381052020f022e19616fa
M1044345 <M1044345@A2ML26966.mindtree.com>
2018-12-06 17:34:59 +0530

    exception handler test methods added
    Former-commit-id: 3cc044d0257486d6b925bae033220acde5242cd7
    Former-commit-id: 5c17f8270e6db93473605814574a8751926a82aa

--- kernel/kernel-idgenerator-tsp/src/test/java/io/mosip/kernel/idgenerator/tsp/test/exception/TspIdExceptionTest.java
+++ kernel/kernel-idgenerator-tsp/src/test/java/io/mosip/kernel/idgenerator/tsp/test/exception/TspIdExceptionTest.java
@@ -1,12 +1,48 @@
 package io.mosip.kernel.idgenerator.tsp.test.exception;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import io.mosip.kernel.core.dataaccess.exception.DataAccessLayerException;
+import io.mosip.kernel.idgenerator.tsp.repository.TspRepository;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 public class TspIdExceptionTest {
 
-	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	TspRepository tspRepository;
+
+	@Test
+	public void TspIdServiceFetchExceptionTest() throws Exception {
+
+		when(tspRepository.findMaxTspId())
+				.thenThrow(new DataAccessLayerException("", "cannot execute statement", null));
+		mockMvc.perform(get("/idgenerator/tsp").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isInternalServerError());
+	}
+
+	@Test
+	public void TspIdServiceInsertExceptionTest() throws Exception {
+		when(tspRepository.save(Mockito.any()))
+				.thenThrow(new DataAccessLayerException("", "cannot execute statement", null));
+		mockMvc.perform(get("/idgenerator/tsp").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isInternalServerError());
+	}
 }
