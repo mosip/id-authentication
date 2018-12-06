c439fc0e50b72edae9274e5919a73525760ff41f
M1044402 <M1044402@b2ml27211.mindtree.com>
2018-12-06 17:07:40 +0530

    Modified constants
    Former-commit-id: c9b52f84fa3f96e1e3a4dcb156449346362c9db4
    Former-commit-id: 3897ae01c780d6cf8a16c5d146382bc463e40e63

--- registration/registration-service/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration/registration-service/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -585,7 +585,7 @@ public class RegistrationConstants {
 	public static final String GET_PRE_REGISTRATION = "get_pre_registration";
 	public static final String REGISTRATION_CLIENT_ID = "59276903416082";
 	public static final String PRE_REGISTRATION_DUMMY_ID = "mosip.pre-registration.datasync";
-	public static final String VER = "1.0";
+	public static final List<String> VER = Arrays.asList("1.0","1.1","1.2");
 	public static final String PRE_REG_TO_GET_ID_ERROR = "Unable to get Pre registartion id's";
 	public static final String PRE_REG_TO_GET_PACKET_ERROR = "Unable to get Pre registartion for given id";
 	public static final String PRE_REG_SUCCESS_MESSAGE = "Got Pre registartion ";
