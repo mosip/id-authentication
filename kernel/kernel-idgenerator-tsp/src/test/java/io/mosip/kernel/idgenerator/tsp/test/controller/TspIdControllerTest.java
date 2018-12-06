5256cd7b31fc3f434abec940d69634f4c5a91450
M1044345 <M1044345@A2ML26966.mindtree.com>
2018-12-06 11:09:36 +0530

    test classes added
    Former-commit-id: 63778a11282a4c03b27495e08f0ee5372dced1cf
    Former-commit-id: c49261d5a8ac7cc026aa677621e4ea3879e011d2

---
+++ kernel/kernel-idgenerator-tsp/src/test/java/io/mosip/kernel/idgenerator/tsp/test/controller/TspIdControllerTest.java
@@ -0,0 +1,42 @@
+package io.mosip.kernel.idgenerator.tsp.test.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import io.mosip.kernel.core.idgenerator.spi.TspIdGenerator;
+import io.mosip.kernel.idgenerator.tsp.dto.TspResponseDTO;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class TspIdControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private TspIdGenerator<TspResponseDTO> tspIdGeneratorService;
+	
+	@Test
+	public void generateIdTest() throws Exception {
+		TspResponseDTO dto=new TspResponseDTO();
+		dto.setTspId(1000);
+		when(tspIdGeneratorService.generateId()).thenReturn(dto);
+		mockMvc.perform(get("/idgenerator/tsp").contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk()).andExpect(jsonPath("$.tspId", is(1000)));
+	}
+	
+}
