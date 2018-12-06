7056429a70be9ec63f6e3b7e18a378ecaa1ee7a0
Dharmesh Khandelwal <Dharmesh.Khandelwal@mindtree.com>
2018-12-06 18:07:54 +0530

    Fixed format
    Former-commit-id: bd96498fc8cf4f9ba3c9ace7babd898ad8719bfc
    Former-commit-id: 8bbb06c204c8ed05f8b32cbdc105d1ceabb0be65

--- kernel/kernel-keymanager-softhsm/src/test/java/io/mosip/kernel/keymanager/softhsm/test/KeyStoreImplTest.java
+++ kernel/kernel-keymanager-softhsm/src/test/java/io/mosip/kernel/keymanager/softhsm/test/KeyStoreImplTest.java
@@ -31,21 +31,21 @@ import org.springframework.test.util.ReflectionTestUtils;
 import io.mosip.kernel.keymanager.softhsm.impl.KeyStoreImpl;
 import io.mosip.kernel.keymanager.softhsm.util.CertificateUtility;
 
-@RunWith(SpringRunner.class) 
+@RunWith(SpringRunner.class)
 
-public class KeyStoreImplTest{
+public class KeyStoreImplTest {
 
-	
 	private java.security.KeyStore keyStore;
-	
+
 	private KeyStoreImpl keyStoreImpl;
 	BouncyCastleProvider provider;
 	SecureRandom random;
-	
+
 	@Before
-	public void setUp() throws Exception  {
+	public void setUp() throws Exception {
 		KeyStoreSpi keyStoreSpiMock = mock(KeyStoreSpi.class);
-		keyStore = new java.security.KeyStore(keyStoreSpiMock, null, "test"){ };
+		keyStore = new java.security.KeyStore(keyStoreSpiMock, null, "test") {
+		};
 		keyStoreImpl = new KeyStoreImpl();
 		keyStoreImpl.setKeyStore(keyStore);
 		ReflectionTestUtils.setField(keyStoreImpl, "configPath", "configPath");
@@ -60,99 +60,100 @@ public class KeyStoreImplTest{
 		Security.addProvider(provider);
 		random = new SecureRandom();
 	}
-	
-	
-   @Test
-	public void testStoreAsymmetricKey() throws Exception{
-	   
-		
-	  KeyPairGenerator keyGenerator= KeyPairGenerator.getInstance("RSA", provider);
-	   keyGenerator.initialize(2048, random);
-	 
-		keyStoreImpl.storeAsymmetricKey(keyGenerator.generateKeyPair(), "alias", LocalDateTime.now(), LocalDateTime.now().plusDays(100));
-		  
+
+	@Test
+	public void testStoreAsymmetricKey() throws Exception {
+
+		KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA", provider);
+		keyGenerator.initialize(2048, random);
+
+		keyStoreImpl.storeAsymmetricKey(keyGenerator.generateKeyPair(), "alias", LocalDateTime.now(),
+				LocalDateTime.now().plusDays(100));
+
 	}
 
-   @Test
-	public void testStoreSymmetricKey() throws Exception{
-	   
-		
-	   javax.crypto.KeyGenerator keyGenerator=  javax.crypto.KeyGenerator.getInstance("AES",provider);
-	   keyGenerator.init(256, random);
-	 
+	@Test
+	public void testStoreSymmetricKey() throws Exception {
+
+		javax.crypto.KeyGenerator keyGenerator = javax.crypto.KeyGenerator.getInstance("AES", provider);
+		keyGenerator.init(256, random);
+
 		keyStoreImpl.storeSymmetricKey(keyGenerator.generateKey(), "alias");
-		  
+
 	}
-   
-   @Test
+
+	@Test
 	public void testDeleteKey() throws Exception {
-	  keyStoreImpl.deleteKey("alias");
+		keyStoreImpl.deleteKey("alias");
 	}
+
 	@Test
 	public void testGetPrivateKey() throws Exception {
-		KeyPairGenerator keyGenerator= KeyPairGenerator.getInstance("RSA", provider);
-		   keyGenerator.initialize(2048, random);
-		 KeyPair keyPair=keyGenerator.generateKeyPair();
+		KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA", provider);
+		keyGenerator.initialize(2048, random);
+		KeyPair keyPair = keyGenerator.generateKeyPair();
 		X509Certificate[] chain = new X509Certificate[1];
-		chain[0] = CertificateUtility.generateX509Certificate(keyPair, "commonName", "organizationalUnit", "organization",
-				"country", LocalDateTime.now(), LocalDateTime.now().plusDays(100));
-		 PrivateKeyEntry keyEntry=new PrivateKeyEntry(keyPair.getPrivate(), chain);
+		chain[0] = CertificateUtility.generateX509Certificate(keyPair, "commonName", "organizationalUnit",
+				"organization", "country", LocalDateTime.now(), LocalDateTime.now().plusDays(100));
+		PrivateKeyEntry keyEntry = new PrivateKeyEntry(keyPair.getPrivate(), chain);
 		when(keyStore.entryInstanceOf("alias", PrivateKeyEntry.class)).thenReturn(true);
-		  when(keyStore.getEntry(Mockito.anyString(), Mockito.any())).thenReturn(keyEntry);
-		  assertThat(keyStoreImpl.getPrivateKey("alias"),isA(PrivateKey.class));
+		when(keyStore.getEntry(Mockito.anyString(), Mockito.any())).thenReturn(keyEntry);
+		assertThat(keyStoreImpl.getPrivateKey("alias"), isA(PrivateKey.class));
 	}
 
 	@Test
 	public void testGetPublicKey() throws Exception {
-		KeyPairGenerator keyGenerator= KeyPairGenerator.getInstance("RSA", provider);
-		   keyGenerator.initialize(2048, random);
-		 KeyPair keyPair=keyGenerator.generateKeyPair();
+		KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA", provider);
+		keyGenerator.initialize(2048, random);
+		KeyPair keyPair = keyGenerator.generateKeyPair();
 		X509Certificate[] chain = new X509Certificate[1];
-		chain[0] = CertificateUtility.generateX509Certificate(keyPair, "commonName", "organizationalUnit", "organization",
-				"country", LocalDateTime.now(), LocalDateTime.now().plusDays(100));
-		 PrivateKeyEntry keyEntry=new PrivateKeyEntry(keyPair.getPrivate(), chain);
-			when(keyStore.entryInstanceOf("alias", PrivateKeyEntry.class)).thenReturn(true);
-			  when(keyStore.getEntry(Mockito.anyString(), Mockito.any())).thenReturn(keyEntry);
-			  assertThat(keyStoreImpl.getPublicKey("alias"),isA(PublicKey.class));
+		chain[0] = CertificateUtility.generateX509Certificate(keyPair, "commonName", "organizationalUnit",
+				"organization", "country", LocalDateTime.now(), LocalDateTime.now().plusDays(100));
+		PrivateKeyEntry keyEntry = new PrivateKeyEntry(keyPair.getPrivate(), chain);
+		when(keyStore.entryInstanceOf("alias", PrivateKeyEntry.class)).thenReturn(true);
+		when(keyStore.getEntry(Mockito.anyString(), Mockito.any())).thenReturn(keyEntry);
+		assertThat(keyStoreImpl.getPublicKey("alias"), isA(PublicKey.class));
 	}
 
 	@Test
 	public void testGetCertificate() throws Exception {
-		KeyPairGenerator keyGenerator= KeyPairGenerator.getInstance("RSA", provider);
-		   keyGenerator.initialize(2048, random);
-		 KeyPair keyPair=keyGenerator.generateKeyPair();
+		KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA", provider);
+		keyGenerator.initialize(2048, random);
+		KeyPair keyPair = keyGenerator.generateKeyPair();
 		X509Certificate[] chain = new X509Certificate[1];
-		chain[0] = CertificateUtility.generateX509Certificate(keyPair, "commonName", "organizationalUnit", "organization",
-				"country", LocalDateTime.now(), LocalDateTime.now().plusDays(100));
-		 PrivateKeyEntry keyEntry=new PrivateKeyEntry(keyPair.getPrivate(), chain);
-			when(keyStore.entryInstanceOf("alias", PrivateKeyEntry.class)).thenReturn(true);
-			  when(keyStore.getEntry(Mockito.anyString(), Mockito.any())).thenReturn(keyEntry);
-			  assertThat(keyStoreImpl.getCertificate("alias"),isA(X509Certificate.class));
+		chain[0] = CertificateUtility.generateX509Certificate(keyPair, "commonName", "organizationalUnit",
+				"organization", "country", LocalDateTime.now(), LocalDateTime.now().plusDays(100));
+		PrivateKeyEntry keyEntry = new PrivateKeyEntry(keyPair.getPrivate(), chain);
+		when(keyStore.entryInstanceOf("alias", PrivateKeyEntry.class)).thenReturn(true);
+		when(keyStore.getEntry(Mockito.anyString(), Mockito.any())).thenReturn(keyEntry);
+		assertThat(keyStoreImpl.getCertificate("alias"), isA(X509Certificate.class));
 	}
-	
+
 	@Test
 	public void testGetAllAlias() throws Exception {
-		Enumeration<String> enumeration= mock(Enumeration.class);
-	    when(keyStore.aliases()).thenReturn(enumeration);
-		assertThat(keyStoreImpl.getAllAlias(),isA(List.class));;
+		@SuppressWarnings("unchecked")
+		Enumeration<String> enumeration = mock(Enumeration.class);
+		when(keyStore.aliases()).thenReturn(enumeration);
+		assertThat(keyStoreImpl.getAllAlias(), isA(List.class));
+		;
 	}
-	
+
 	@Test
 	public void testGetKey() throws Exception {
-		Key key=mock(Key.class);
-	    when(keyStore.getKey(Mockito.anyString(),Mockito.any())).thenReturn(key);
-		assertThat(keyStoreImpl.getKey("alias"),isA(Key.class));;
+		Key key = mock(Key.class);
+		when(keyStore.getKey(Mockito.anyString(), Mockito.any())).thenReturn(key);
+		assertThat(keyStoreImpl.getKey("alias"), isA(Key.class));
+		;
 	}
-	
-	
+
 	@Test
 	public void testGetSymmetricKey() throws Exception {
-		  javax.crypto.KeyGenerator keyGenerator=  javax.crypto.KeyGenerator.getInstance("AES",provider);
-		   keyGenerator.init(256, random);
-		SecretKeyEntry secretKeyEntry= new SecretKeyEntry(keyGenerator.generateKey());
-	    when(keyStore.entryInstanceOf("alias", SecretKeyEntry.class)).thenReturn(true);
-	    when(keyStore.getEntry(Mockito.anyString(), Mockito.any())).thenReturn(secretKeyEntry);
-		assertThat(keyStoreImpl.getSymmetricKey("alias"),isA(Key.class));
+		javax.crypto.KeyGenerator keyGenerator = javax.crypto.KeyGenerator.getInstance("AES", provider);
+		keyGenerator.init(256, random);
+		SecretKeyEntry secretKeyEntry = new SecretKeyEntry(keyGenerator.generateKey());
+		when(keyStore.entryInstanceOf("alias", SecretKeyEntry.class)).thenReturn(true);
+		when(keyStore.getEntry(Mockito.anyString(), Mockito.any())).thenReturn(secretKeyEntry);
+		assertThat(keyStoreImpl.getSymmetricKey("alias"), isA(Key.class));
 	}
 
 }
