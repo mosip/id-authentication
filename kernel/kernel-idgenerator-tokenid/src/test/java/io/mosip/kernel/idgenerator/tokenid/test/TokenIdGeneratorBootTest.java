53f6adf40004411bdff28dbd02dacc4e4d7f83ad
M1046464 <M1046464@A2ML27485.mindtree.com>
2018-12-10 11:01:31 +0530

    changes in tokenIdGenerator
    Former-commit-id: 36c74d02b612d09700f6d00efaf96ece8a2447ff
    Former-commit-id: b9282465a43267e94a5ce2630e7dbf9c29152666

---
+++ kernel/kernel-idgenerator-tokenid/src/test/java/io/mosip/kernel/idgenerator/tokenid/test/TokenIdGeneratorBootTest.java
@@ -0,0 +1,11 @@
+package io.mosip.kernel.idgenerator.tokenid.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TokenIdGeneratorBootTest {
+
+}
