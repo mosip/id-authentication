7140a3c04c18e269785f23f48b16f52c8895dd51
M1046464 <M1046464@A2ML27485.mindtree.com>
2018-12-10 11:01:52 +0530

    changes in tokenIdGeneratorBootTest
    Former-commit-id: 6bcb251d6c46ab5723e84b64f98843c8bcdc86c9
    Former-commit-id: 26f3a4407033132f6590b2d7f4c5c98d8372cfa1

--- kernel/kernel-idgenerator-tokenid/src/test/java/io/mosip/kernel/idgenerator/tokenid/test/TokenIdGeneratorBootTest.java
+++ kernel/kernel-idgenerator-tokenid/src/test/java/io/mosip/kernel/idgenerator/tokenid/test/TokenIdGeneratorBootTest.java
@@ -1,11 +1,19 @@
 package io.mosip.kernel.idgenerator.tokenid.test;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import io.mosip.kernel.core.idgenerator.spi.TokenIdGenerator;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TokenIdGeneratorBootTest {
 
+	@Autowired
+	TokenIdGenerator<String> tokenIdGenerator;
+	
+	
 }
+
