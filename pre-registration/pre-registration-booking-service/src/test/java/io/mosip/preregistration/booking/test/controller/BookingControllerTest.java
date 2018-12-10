ca4aaec12d8e6abf211ea14f9246021dd68efce1
M1044479 <M1044479@A2ML27085.mindtree.com>
2018-12-10 15:32:37 +0530

    unit test for book appoinment
    Former-commit-id: 11c6b8783b3c010de97269a655e14726dbb59a5f
    Former-commit-id: bf7018125463a0a001f1a007c2588c3ebe22a913

--- pre-registration/pre-registration-booking-service/src/test/java/io/mosip/preregistration/booking/test/controller/BookingControllerTest.java
+++ pre-registration/pre-registration-booking-service/src/test/java/io/mosip/preregistration/booking/test/controller/BookingControllerTest.java
@@ -67,7 +67,6 @@ public class BookingControllerTest {
 				classLoader.getResource("booking.json").getFile().trim().replaceAll("\\u0020", "%20"));
 		File file = new File(dataSyncUri.getPath());
 		jsonObject = parser.parse(new FileReader(file));
-<<<<<<< HEAD
 
 		bookingRequestDTO.setPre_registration_id("23587986034785");
 		bookingRequestDTO.setNewBookingDetails(new BookingRegistrationDTO());
@@ -75,15 +74,7 @@ public class BookingControllerTest {
 //		bookingRequestDTOA.setSlotFromTime("09:00");
 //		bookingRequestDTOA.setSlotToTime("09:13");
 //		bookingRequestDTOA.setReg_date("2018-12-06");
-=======
-		bookingRequestDTO.setPre_registration_id("23587986034785");
-		oldBooking.setRegistration_center_id("1");
-		oldBooking.setSlotFromTime("09:00");
-		oldBooking.setSlotToTime("09:13");
-		oldBooking.setReg_date("2018-12-06");
->>>>>>> branch 'DEV_SPRINT6_TEAM_BRANCH' of https://github.com/mosip/mosip
 
-<<<<<<< HEAD
 
 		//bookingRequestDTOB.setPre_registration_id("31496715428069");
 //		bookingRequestDTOB.setRegistration_center_id("1");
@@ -91,12 +82,6 @@ public class BookingControllerTest {
 //		bookingRequestDTOB.setSlotToTime("09:13");
 //		bookingRequestDTOB.setReg_date("2018-12-06");
 
-=======
-		newBooking.setRegistration_center_id("1");
-		newBooking.setSlotFromTime("09:00");
-		newBooking.setSlotToTime("09:13");
-		newBooking.setReg_date("2018-12-06");
->>>>>>> branch 'DEV_SPRINT6_TEAM_BRANCH' of https://github.com/mosip/mosip
 
 		bookingDTO.setRequest(bookingList);
 
