5f69f9df012b680dc2728480a14c1658caae7259
Dharmesh Khandelwal <Dharmesh.Khandelwal@mindtree.com>
2018-12-11 13:20:30 +0530

    Added logger for keymanager
    Former-commit-id: 29dfd0b8f48912ca1b1d1c7d8e91e9f4e86a1033
    Former-commit-id: babe91c37e82ec6c1d20ef9b6302c3493edeb657

---
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/logger/KeymanagerLogger.java
@@ -0,0 +1,47 @@
+package io.mosip.kernel.keymanagerservice.logger;
+
+import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.kernel.logger.logback.appender.RollingFileAppender;
+import io.mosip.kernel.logger.logback.factory.Logfactory;
+
+/**
+ * Keymanager logger.
+ * 
+ * @author Dharmesh Khandelwal
+ * @since 1.0.0
+ *
+ */
+public final class KeymanagerLogger {
+
+	private static RollingFileAppender mosipRollingFileAppender;
+
+	static {
+		mosipRollingFileAppender = new RollingFileAppender();
+		mosipRollingFileAppender.setAppend(true);
+		mosipRollingFileAppender.setAppenderName("fileappender");
+		mosipRollingFileAppender.setFileName("logs/kernel-keymanager.log");
+		mosipRollingFileAppender.setFileNamePattern("logs/kernel-keymanager-%d{yyyy-MM-dd}-%i.log");
+		mosipRollingFileAppender.setImmediateFlush(true);
+		mosipRollingFileAppender.setMaxFileSize("1mb");
+		mosipRollingFileAppender.setMaxHistory(3);
+		mosipRollingFileAppender.setPrudent(false);
+		mosipRollingFileAppender.setTotalCap("10mb");
+	}
+
+	/**
+	 * Instantiates a new logger.
+	 */
+	private KeymanagerLogger() {
+	}
+
+	/**
+	 * Method to get the rolling file logger for the class provided.
+	 *
+	 * @param clazz
+	 *            the clazz
+	 * @return the logger
+	 */
+	public static Logger getLogger(Class<?> clazz) {
+		return Logfactory.getDefaultRollingFileLogger(mosipRollingFileAppender, clazz);
+	}
+}
