52f760aad564308de552a93292f973b9bda4947f
M1046540 <M1046540@b2ml26342.mindtree.com>
2018-12-11 11:30:01 +0530

    Pre Reg Sync : Document details added..
    Former-commit-id: 8d0cce867958dc01e52bd913e8d0be373d2efff3
    Former-commit-id: 746110ed4bc9076068ce490a3fc2c9d14b973fa1

--- registration/registration-service/src/main/java/io/mosip/registration/service/external/impl/PreRegZipHandlingServiceImpl.java
+++ registration/registration-service/src/main/java/io/mosip/registration/service/external/impl/PreRegZipHandlingServiceImpl.java
@@ -96,13 +96,18 @@ public class PreRegZipHandlingServiceImpl implements PreRegZipHandlingService {
 				if (zipEntry.getName().endsWith(".json")) {
 					registrationDTO = parseDemographicJson(zipInputStream, zipEntry, registrationDTO);
 				} else {
-
 					documentDetailsDTO = new DocumentDetailsDTO();
 					documentDetailsDTO.setDocumentName(zipEntry.getName());
 					documentDetailsDTO.setDocument(IOUtils.toByteArray(zipInputStream));
-
+					if (zipEntry.getName().contains("_")) {
+						documentDetailsDTO
+								.setDocumentCategory(zipEntry.getName().substring(0, zipEntry.getName().indexOf("_")));
+					}
+					if (zipEntry.getName().contains(".")) {
+						documentDetailsDTO
+								.setDocumentType(zipEntry.getName().substring(zipEntry.getName().lastIndexOf(".") + 1));
+					}
 					documentDetailsDTOs.add(documentDetailsDTO);
-					//System.out.println(zipEntry.getName());
 				}
 			}
 
