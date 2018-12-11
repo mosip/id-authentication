2346d6f03d28b28398bceb2644e0a391525704c4
Sanjay Murali <sanjay.murali@mindtree.com>
2018-12-11 11:36:23 +0530

    otp sign validation
    Former-commit-id: eff9e406ea8f5e5b15b58cc6a0f850e1358c5119
    Former-commit-id: 13e45b0418c8bc31e33c53fa181c9fcfbef13c1c

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/filter/OTPFilter.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/filter/OTPFilter.java
@@ -1,9 +1,18 @@
 package io.mosip.authentication.service.filter;
 
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
 import java.util.Map;
+import java.util.Optional;
 
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.lang.JoseException;
 import org.springframework.stereotype.Component;
 
+import io.mosip.authentication.core.constant.IdAuthenticationErrorConstants;
 import io.mosip.authentication.core.exception.IdAuthenticationAppException;
 
 /**
@@ -13,6 +22,15 @@ import io.mosip.authentication.core.exception.IdAuthenticationAppException;
  */
 @Component
 public class OTPFilter extends BaseAuthFilter {
+
+	/** The Constant X_509. */
+	private static final String X_509 = "X.509";
+	
+	/** The Constant PUBLIC_KEY_CERT. */
+	private static final String PUBLIC_KEY_CERT = "publicKeyCert";
+	
+	/** The Constant KEY. */
+	private static final String KEY = "key";
 
 	/*
 	 * (non-Javadoc)
@@ -54,6 +72,34 @@ public class OTPFilter extends BaseAuthFilter {
 			Map<String, Object> responseBody)
 			throws IdAuthenticationAppException {
 		return responseBody;
+	}
+
+	/* (non-Javadoc)
+	 * @see io.mosip.authentication.service.filter.BaseAuthFilter#validateSignature(java.util.Map, java.lang.String)
+	 */
+	@Override
+	protected boolean validateSignature(Map<String, Object> requestBody, String signature) throws IdAuthenticationAppException {
+		boolean isSigned = false;
+		Optional<String> map = Optional.ofNullable(requestBody.get(KEY))
+				.filter(obj -> obj instanceof Map)
+				.map(obj -> String.valueOf(((Map<String, Object>)obj).get(PUBLIC_KEY_CERT)));
+		if(map.isPresent()) {
+			byte[] cert = Base64.getDecoder().decode(map.get());
+			try {
+				X509Certificate certNew = (X509Certificate) CertificateFactory.getInstance(X_509)
+						.generateCertificate(new ByteArrayInputStream(cert));
+				JsonWebSignature jws = new JsonWebSignature();
+				jws.setCompactSerialization(signature);
+				jws.setKey(certNew.getPublicKey());
+				isSigned = jws.verifySignature();
+			} catch (CertificateException | JoseException e) {
+				throw new IdAuthenticationAppException(IdAuthenticationErrorConstants.INVALID_AUTH_REQUEST
+						.getErrorCode(),
+				IdAuthenticationErrorConstants.INVALID_AUTH_REQUEST
+						.getErrorMessage());
+			}
+		}
+		return isSigned;
 	}
 
 }
