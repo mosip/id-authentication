88e504b11afcf93cbd3aef517e3797eccfe5a603
urviljoshi <Urvil.Joshi2.mindtree.com>
2018-12-12 00:20:34 +0530

    test code formatted
    Former-commit-id: d5fb5ceea5c76757f38eda107921ed16a233d2c2
    Former-commit-id: d9a2afc1e3c6c9bd7c9261deddda64a6c290eca4

--- kernel/kernel-cryptomanager-service/src/test/java/io/mosip/kernel/cryptographic/service/KernelCryptographicServiceIntegrationExceptionTest.java
+++ kernel/kernel-cryptomanager-service/src/test/java/io/mosip/kernel/cryptographic/service/KernelCryptographicServiceIntegrationExceptionTest.java
@@ -33,77 +33,87 @@ import io.mosip.kernel.core.util.CryptoUtil;
 import io.mosip.kernel.cryptomanager.KernelCryptomanagerBootApplication;
 import io.mosip.kernel.cryptomanager.dto.KeymanagerPublicKeyResponseDto;
 
-@SpringBootTest(classes=KernelCryptomanagerBootApplication.class)
+@SpringBootTest(classes = KernelCryptomanagerBootApplication.class)
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 public class KernelCryptographicServiceIntegrationExceptionTest {
-   
+
 	@Autowired
 	private MockMvc mockMvc;
-	
-	
-	@Autowired 
+
+	@Autowired
 	private ObjectMapper objectMapper;
 
-	
-	
-	@Autowired 
+	@Autowired
 	private RestTemplate restTemplate;
-	
-	@MockBean 
+
+	@MockBean
 	Decryptor<PrivateKey, PublicKey, SecretKey> decryptor;
-	
-	
-	
+
 	private MockRestServiceServer server;
-	
+
 	@Before
 	public void setUp() {
 
-	server = MockRestServiceServer.bindTo(restTemplate).build();
+		server = MockRestServiceServer.bindTo(restTemplate).build();
 	}
 
-	
-@Test	
-public void testInvalidSpecEncrypt() throws Exception {
-	KeymanagerPublicKeyResponseDto keymanagerPublicKeyResponseDto= new KeymanagerPublicKeyResponseDto(CryptoUtil.encodeBase64("badprivatekey".getBytes()), LocalDateTime.now(), LocalDateTime.now().plusDays(100));
-    server.expect(requestTo("http://localhost:8088/keymanager/v1.0/publickey/REGISTRATION?timeStamp=2018-12-06T12:07:44.403&referenceId=ref123")).andRespond(withSuccess(objectMapper.writeValueAsString(keymanagerPublicKeyResponseDto), MediaType.APPLICATION_JSON));
-    String requestBody="{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
-	mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isInternalServerError());
-}
+	@Test
+	public void testInvalidSpecEncrypt() throws Exception {
+		KeymanagerPublicKeyResponseDto keymanagerPublicKeyResponseDto = new KeymanagerPublicKeyResponseDto(
+				CryptoUtil.encodeBase64("badprivatekey".getBytes()), LocalDateTime.now(),
+				LocalDateTime.now().plusDays(100));
+		server.expect(requestTo(
+				"http://localhost:8088/keymanager/v1.0/publickey/REGISTRATION?timeStamp=2018-12-06T12:07:44.403&referenceId=ref123"))
+				.andRespond(withSuccess(objectMapper.writeValueAsString(keymanagerPublicKeyResponseDto),
+						MediaType.APPLICATION_JSON));
+		String requestBody = "{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
+		mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+				.andExpect(status().isInternalServerError());
+	}
 
-@Test	
-public void testMethodArgumentNotValidException() throws Exception {
-	String requestBody="{\"applicationId\": \"\",\"data\": \"\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
-	mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest());
-}
-@Test	
-public void testHttpClientErrorException() throws Exception {
-    server.expect(requestTo("http://localhost:8088/keymanager/v1.0/publickey/REGISTRATION?timeStamp=2018-12-06T12:07:44.403&referenceId=ref123")).andRespond(withBadRequest());
-    String requestBody="{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
-	mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isInternalServerError());
-}
+	@Test
+	public void testMethodArgumentNotValidException() throws Exception {
+		String requestBody = "{\"applicationId\": \"\",\"data\": \"\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
+		mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+				.andExpect(status().isBadRequest());
+	}
 
-@Test	
-public void testHttpServerErrorException() throws Exception {
-    server.expect(requestTo("http://localhost:8088/keymanager/v1.0/publickey/REGISTRATION?timeStamp=2018-12-06T12:07:44.403&referenceId=ref123")).andRespond(withServerError());
-    String requestBody="{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
-	mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isInternalServerError());
-}
+	@Test
+	public void testHttpClientErrorException() throws Exception {
+		server.expect(requestTo(
+				"http://localhost:8088/keymanager/v1.0/publickey/REGISTRATION?timeStamp=2018-12-06T12:07:44.403&referenceId=ref123"))
+				.andRespond(withBadRequest());
+		String requestBody = "{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
+		mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+				.andExpect(status().isInternalServerError());
+	}
 
-@Test	
-public void testInvalidFormatException() throws Exception {
-	server.expect(requestTo("http://localhost:8088/keymanager/v1.0/publickey/REGISTRATION?timeStamp=2018-12-06T12:07:44.403&referenceId=ref123")).andRespond(withServerError());
-	String requestBody="{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-0\"}";
-	mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest());
-}
+	@Test
+	public void testHttpServerErrorException() throws Exception {
+		server.expect(requestTo(
+				"http://localhost:8088/keymanager/v1.0/publickey/REGISTRATION?timeStamp=2018-12-06T12:07:44.403&referenceId=ref123"))
+				.andRespond(withServerError());
+		String requestBody = "{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
+		mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+				.andExpect(status().isInternalServerError());
+	}
 
-@Test	
-public void testIllegalArgumentException() throws Exception {
-    String requestBody="{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
-	mockMvc.perform(post("/v1.0/decrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest());
-}
+	@Test
+	public void testInvalidFormatException() throws Exception {
+		server.expect(requestTo(
+				"http://localhost:8088/keymanager/v1.0/publickey/REGISTRATION?timeStamp=2018-12-06T12:07:44.403&referenceId=ref123"))
+				.andRespond(withServerError());
+		String requestBody = "{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-0\"}";
+		mockMvc.perform(post("/v1.0/encrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+				.andExpect(status().isBadRequest());
+	}
 
-	
-	
+	@Test
+	public void testIllegalArgumentException() throws Exception {
+		String requestBody = "{\"applicationId\": \"REGISTRATION\",\"data\": \"dXJ2aWw\",\"referenceId\": \"ref123\",\"timeStamp\": \"2018-12-06T12:07:44.403Z\"}";
+		mockMvc.perform(post("/v1.0/decrypt").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+				.andExpect(status().isBadRequest());
+	}
+
 }
