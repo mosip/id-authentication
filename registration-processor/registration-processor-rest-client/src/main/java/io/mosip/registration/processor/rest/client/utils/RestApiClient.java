1658647de2d9459a83f705b95c217ec36f8ddd5d
M1022006 <sowmya.goudar@mindtree.com>
2018-12-11 12:30:35 +0530

    updated rest client api
    Former-commit-id: c51caa4239d4e82dac338aa120b603e5e02759a3
    Former-commit-id: 266bedaa11608328299988b488f7799b94d6f5ed

--- registration-processor/registration-processor-rest-client/src/main/java/io/mosip/registration/processor/rest/client/utils/RestApiClient.java
+++ registration-processor/registration-processor-rest-client/src/main/java/io/mosip/registration/processor/rest/client/utils/RestApiClient.java
@@ -2,12 +2,19 @@ package io.mosip.registration.processor.rest.client.utils;
 
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.scheme.SchemeSocketFactory;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,87 +29,100 @@ import org.springframework.web.client.RestTemplate;
  * @author Rishabh Keshari
  */
 @Component
-public class RestApiClient{
+public class RestApiClient {
 
 	/** The logger. */
-	private final static Logger LOGGER = LoggerFactory.getLogger(RestApiClient.class);
+	private final Logger logger = LoggerFactory.getLogger(RestApiClient.class);
 
 	@Autowired
 	RestTemplateBuilder builder;
+
 	/**
 	 * Gets the api.
 	 *
-	 * @param <T> the generic type
-	 * @param getURI the get URI
-	 * @param responseType the response type
+	 * @param <T>
+	 *            the generic type
+	 * @param getURI
+	 *            the get URI
+	 * @param responseType
+	 *            the response type
 	 * @return the api
 	 */
-	@SuppressWarnings("unchecked")
-	public <T> T getApi(String getURI , Class<?> responseType) {
+	public <T> T getApi(String getURI, Class<?> responseType) {
 		RestTemplate restTemplate;
-		T result=null;
 		try {
 			restTemplate = getRestTemplate();
-			result = (T) restTemplate.getForObject(getURI, responseType);
-			
+			T result = (T) restTemplate.getForObject(getURI, responseType);
+
+			return result;
 		} catch (Exception e) {
-			LOGGER.error(e.getMessage());
+
+			logger.error(e.getMessage());
 		}
-		return result;
+		return null;
 	}
 
 	/**
 	 * Post api.
 	 *
-	 * @param <T> the generic type
-	 * @param uri the uri
-	 * @param requestType the request type
-	 * @param responseClass the response class
+	 * @param <T>
+	 *            the generic type
+	 * @param uri
+	 *            the uri
+	 * @param requestType
+	 *            the request type
+	 * @param responseClass
+	 *            the response class
 	 * @return the t
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	@SuppressWarnings("unchecked")
-	public <T> T postApi(String uri, T requestType,Class<?> responseClass)  {
+	public <T> T postApi(String uri, T requestType, Class<?> responseClass) {
 
 		RestTemplate restTemplate;
-		T result=null;
 		try {
+			System.out.println("Your URl is to change ::   " + uri);
 			restTemplate = getRestTemplate();
-			result = (T) restTemplate.postForObject(uri, requestType, responseClass);
-			
-		} catch (Exception e) {
-			LOGGER.error(e.getMessage());
-		}
-		return result;
-	}
 
+			T result = (T) restTemplate.postForObject(uri, requestType, responseClass);
+			return result;
+		} catch (Exception e) {
+
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
 
 	public static RestTemplate getRestTemplate() throws Exception {
 		SSLContext sslContext = SSLContext.getInstance("SSL");
 		// set up a TrustManager that trusts everything
-		sslContext.init(null, new TrustManager[] { new X509TrustManager
-				() {
+		sslContext.init(null, new TrustManager[] { new X509TrustManager() {
 			public X509Certificate[] getAcceptedIssuers() {
-				LOGGER.info("getAcceptedIssuers =============");
+
 				return null;
 			}
 
-			public void checkClientTrusted(X509Certificate[] certs,
-					String authType) {
-				LOGGER.info("checkClientTrusted =============");
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+
 			}
 
-			public void checkServerTrusted(X509Certificate[] certs,
-					String authType) {
-				LOGGER.info("checkServerTrusted =============");
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+
 			}
 		} }, new SecureRandom());
 
-		SSLConnectionSocketFactory sslcon=new SSLConnectionSocketFactory(sslContext);
-		CloseableHttpClient httpClient=HttpClients.custom().setSSLSocketFactory(sslcon).build();
+		SSLSocketFactory sf = new SSLSocketFactory(sslContext);
+
+		Scheme httpsScheme = new Scheme("https", 443, (SchemeSocketFactory) sf);
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(httpsScheme);
+
+		// apache HttpClient version >4.2 should use BasicClientConnectionManager
+		ClientConnectionManager cm = new SingleClientConnManager(schemeRegistry);
+		HttpClient httpClient = new DefaultHttpClient(cm);
 		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
 
 		requestFactory.setHttpClient(httpClient);
+
 		return new RestTemplate(requestFactory);
 	}
 
