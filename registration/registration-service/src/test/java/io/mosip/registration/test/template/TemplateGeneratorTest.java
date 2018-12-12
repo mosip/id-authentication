1733a4a1968d0e2c128021c0429849dcec66c835
M1044292 <M1044292@b2ml27210.mindtree.com>
2018-12-12 15:16:23 +0530

    TemplateGeneratorTest error fixed
    Former-commit-id: c797dc25b4fbfb44b395d1cca5331adab1e9aea3
    Former-commit-id: eb08c81071b4b1d9520856598be5388cc25dfe75

--- registration/registration-service/src/test/java/io/mosip/registration/test/template/TemplateGeneratorTest.java
+++ registration/registration-service/src/test/java/io/mosip/registration/test/template/TemplateGeneratorTest.java
@@ -1,23 +1,33 @@
 package io.mosip.registration.test.template;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URISyntaxException;
 
+import javax.imageio.ImageIO;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import io.mosip.kernel.templatemanager.velocity.builder.TemplateManagerBuilderImpl;
+import io.mosip.registration.constants.RegistrationConstants;
 import io.mosip.registration.dto.RegistrationDTO;
 import io.mosip.registration.exception.RegBaseCheckedException;
 import io.mosip.registration.test.util.datastub.DataProvider;
 import io.mosip.registration.util.acktemplate.TemplateGenerator;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ ImageIO.class })
 public class TemplateGeneratorTest {
 	TemplateManagerBuilderImpl template =  new TemplateManagerBuilderImpl();
 	
@@ -30,6 +40,9 @@ public class TemplateGeneratorTest {
 	@Test
 	public void generateTemplateTest() throws IOException, URISyntaxException, RegBaseCheckedException {
 		RegistrationDTO registrationDTO = DataProvider.getPacketDTO();
+		PowerMockito.mockStatic(ImageIO.class);
+		BufferedImage image = null;
+		when(ImageIO.read(templateGenerator.getClass().getResourceAsStream(RegistrationConstants.TEMPLATE_HANDS_IMAGE_PATH))).thenReturn(image);
 		Writer writer = templateGenerator.generateTemplate("sample text", registrationDTO, template);
 		assertNotNull(writer);
 	}
