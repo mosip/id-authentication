cef29b8dd5b33bb9d37aee7ac55e5cfcff39cb13
urviljoshi <Urvil.Joshi2.mindtree.com>
2018-12-12 11:41:20 +0530

    ssl verification skipped
    Former-commit-id: 9dd6df42e1a1bda85a811a41cad97b55ea6b3bf1
    Former-commit-id: b4d59765307ffe8c3974eacfb8b2e70c65413498

--- kernel/kernel-cryptomanager-service/src/main/java/io/mosip/kernel/cryptomanager/config/CryptomanagerConfig.java
+++ kernel/kernel-cryptomanager-service/src/main/java/io/mosip/kernel/cryptomanager/config/CryptomanagerConfig.java
@@ -1,7 +1,19 @@
 package io.mosip.kernel.cryptomanager.config;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -14,13 +26,23 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CryptomanagerConfig {
 
-	/**
-	 * {@link RestTemplate} Bean
-	 * 
-	 * @return {@link RestTemplate} instance
-	 */
 	@Bean
-	public RestTemplate restTemplateConfig() {
-		return new RestTemplate();
+	public RestTemplate restTemplateConfig()
+			throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+		{
+			TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+			SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+					.loadTrustMaterial(null, acceptingTrustStrategy).build();
+
+			SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+			CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+
+			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+			requestFactory.setHttpClient(httpClient);
+			return new RestTemplate(requestFactory);
+		}
 	}
 }
