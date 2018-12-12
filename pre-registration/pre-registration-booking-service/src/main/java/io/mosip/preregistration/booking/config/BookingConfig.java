bfec2f87cd84670083030612cb0273a3abadbb2a
Ravi Chitragar Balaji <M1046462@mindtree.com>
2018-12-12 19:58:05 +0530

    Pre Reg Booking Serivce Swagger config changes
    Former-commit-id: 6a0dfdceeb643652498d74940256e7d616e73d55
    Former-commit-id: 1ef53c5c98da3421fe04a9fde6311b6a93fe4223

--- pre-registration/pre-registration-booking-service/src/main/java/io/mosip/preregistration/booking/config/BookingConfig.java
+++ pre-registration/pre-registration-booking-service/src/main/java/io/mosip/preregistration/booking/config/BookingConfig.java
@@ -1,5 +1,11 @@
 package io.mosip.preregistration.booking.config;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,15 +25,58 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class BookingConfig {
 
+	@Value("${application.env.local:false}")
+	private Boolean localEnv;
+
+	@Value("${swagger.base-url:#{null}}")
+	private String swaggerBaseUrl;
+
+	@Value("${server.port:9095}")
+	private int serverPort;
+
+	String proto = "http";
+	String host = "localhost";
+	int port = -1;
+	String hostWithPort = "localhost:9095";
 	/**
 	 * @return docket
 	 */
+	
+	
 	@Bean
 	public Docket registrationStatusBean() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("Pre-Registration-Booking").select()
+		boolean swaggerBaseUrlSet = false;
+		if (!localEnv && swaggerBaseUrl != null && !swaggerBaseUrl.isEmpty()) {
+			try {
+				proto = new URL(swaggerBaseUrl).getProtocol();
+				host = new URL(swaggerBaseUrl).getHost();
+				port = new URL(swaggerBaseUrl).getPort();
+				if (port == -1) {
+					hostWithPort = host;
+				} else {
+					hostWithPort = host + ":" + port;
+				} 
+				swaggerBaseUrlSet = true;
+			} catch (MalformedURLException e) {
+				System.err.println("SwaggerUrlException: " + e);
+			}
+		}
+
+		Docket docket = new Docket(DocumentationType.SWAGGER_2).groupName("Pre-Registration-Booking").select()
 				.apis(RequestHandlerSelectors.basePackage("io.mosip.preregistration.booking.controller"))
 				.paths(PathSelectors.ant("/v0.1/pre-registration/booking/*")).build();
 
+		if (swaggerBaseUrlSet) {
+			docket.protocols(protocols()).host(hostWithPort);
+			System.out.println("\nSwagger Base URL: " + proto + "://" + hostWithPort + "\n");
+		}
+		return docket;
+	}
+	
+	private Set<String> protocols() {
+		Set<String> protocols = new HashSet<>();
+		protocols.add(proto);
+		return protocols;
 	}
 
 }
