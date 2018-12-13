617a2203e4acc9fe7da904acc6f39e5edfd3f557
M1046464 <M1046464@A2ML27485.mindtree.com>
2018-12-13 10:34:17 +0530

    MOS-996 changes made in mapper utils
    Former-commit-id: 606773c5d060641fba2e807587e6ede57316f7f3
    Former-commit-id: dd859f46ff3822dca4320a7b13713e188bb03023

--- kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/utils/MapperUtils.java
+++ kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/utils/MapperUtils.java
@@ -82,6 +82,7 @@ public class MapperUtils {
 			dto.setHolidayMonth(String.valueOf(date.getMonth().getValue()));
 			dto.setHolidayDay(String.valueOf(date.getDayOfWeek().getValue()));
 			dto.setIsActive(holiday.getIsActive());
+			dto.setLocationCode(holidayId.getLocationCode());
 			holidayDtos.add(dto);
 		});
 		return holidayDtos;
