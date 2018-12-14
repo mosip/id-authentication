7486b557096eff78e7ef5e3b6d1fc8538b0520a8
sinhaneha <neha.sinha@mindtree.com>
2018-12-14 13:55:01 +0530

    MOS 9622 Comments fixed
    Former-commit-id: e3ed434a52c62d9e4a28d712d6f8357b1fc4115f
    Former-commit-id: 12eadb7715efbbcc2682675a0be4977b90d5b727

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/DocumentCategoryData.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/DocumentCategoryData.java
@@ -1,15 +1,11 @@
 package io.mosip.kernel.masterdata.dto;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import lombok.Data;
 
 /**
  * Response dto for Document Category Detail
  * 
  * @author Neha
- * @author Ritesh Sinha
  * @since 1.0.0
  *
  */
@@ -19,32 +15,4 @@ import lombok.Data;
 public class DocumentCategoryData {
 	
 	private DocumentCategoryDto documentcategorytype;
-/*
-	*//**
-	 * Document category code.
-	 *//*
-	@NotNull
-	@Size(min = 1, max = 3)
-	private String code;
-
-	*//**
-	 * Document category name.
-	 *//*
-	private String name;
-
-	*//**
-	 * Document category description
-	 *//*
-	private String description;
-
-	*//**
-	 * The Language Code.
-	 *//*
-	private String langCode;
-
-	*//**
-	 * Is active or not.
-	 *//*
-	private Boolean isActive;
-*/
 }
