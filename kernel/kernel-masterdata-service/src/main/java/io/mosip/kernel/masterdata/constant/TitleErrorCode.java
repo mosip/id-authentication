2414089b4eb2e4f8f1a45b6c25e18a5a65f265d4
M1039257 <M1039257@A2ML23736.mindtree.com>
2018-12-18 15:27:45 +0530

    MOS-992 updated classes to update title data
    Former-commit-id: fae3bb4e845799855a5f4037b515b7440f400332
    Former-commit-id: a4fef23f13c6b0e6e6834e45004b0357bfcdca3e

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/TitleErrorCode.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/TitleErrorCode.java
@@ -10,7 +10,7 @@ package io.mosip.kernel.masterdata.constant;
 public enum TitleErrorCode {
 
 	TITLE_FETCH_EXCEPTION("KER-MSD-047", "Error occured while fetching Titles"), TITLE_NOT_FOUND("KER-MSD-048",
-			"Title not found"), TITLE_INSERT_EXCEPTION("KER-MSD-XXX", "Could not insert data");
+			"Title not found"), TITLE_INSERT_EXCEPTION("KER-MSD-XXX", "Could not insert data"), TITLE_UPDATE_EXCEPTION("","Could not update data");
 
 	private final String errorCode;
 	private final String errorMessage;
