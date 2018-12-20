c8439c5eb727652739b3e63efd8cfe65493f2561
urviljoshi <Urvil.Joshi2.mindtree.com>
2018-12-20 13:34:51 +0530

    MOS-988 #comment gender type update delete added
    Former-commit-id: 6a883452e6695462539b03204a344346c4399269
    Former-commit-id: 5a5eb97929c5146cdc217d1d29b51a6f78c74a95

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/GenderTypeErrorCode.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/GenderTypeErrorCode.java
@@ -5,13 +5,21 @@ package io.mosip.kernel.masterdata.constant;
  * gender data
  * 
  * @author Sidhant Agarwal
+ * @author Urvil Joshi
  * @since 1.0.0
  *
  */
 public enum GenderTypeErrorCode {
-	GENDER_TYPE_FETCH_EXCEPTION("KER-MSD-017", "Error occured while fetching gender types"), GENDER_TYPE_NOT_FOUND(
-			"KER-MSD-018",
-			"Gender Type not found"), GENDER_TYPE_INSERT_EXCEPTION("KER-MSD-XX", "Could not insert Gender Data");
+	
+	GENDER_TYPE_FETCH_EXCEPTION("KER-MSD-017", "Error occured while fetching gender types"), 
+	
+	GENDER_TYPE_NOT_FOUND("KER-MSD-018","Gender Type not found"), 
+	
+	GENDER_TYPE_INSERT_EXCEPTION("KER-MSD-XX", "Could not insert Gender Data"),
+	
+	GENDER_TYPE_UPDATE_EXCEPTION("xxx","Exception during update"),
+	
+	GENDER_TYPE_DELETE_EXCEPTION("xxx","Exception during deletion");
 
 	private final String errorCode;
 	private final String errorMessage;
