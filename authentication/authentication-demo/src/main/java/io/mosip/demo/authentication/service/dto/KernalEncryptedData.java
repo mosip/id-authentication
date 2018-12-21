36ccbf1c37fba0299d8b47037588bdbcb5c77276
Dinesh Karuppiah <dineshkaruppiah.thiagarajan@mindtree.com>
2018-12-21 17:18:42 +0530

    Updated DemoApp
    Former-commit-id: a08bcfd87f05e8aad2430a39a52171ec732beb45
    Former-commit-id: eb266ff636011264a04b53933ed412d6571477f3

---
+++ authentication/authentication-demo/src/main/java/io/mosip/demo/authentication/service/dto/KernalEncryptedData.java
@@ -0,0 +1,10 @@
+package io.mosip.demo.authentication.service.dto;
+
+import lombok.Data;
+
+@Data
+public class KernalEncryptedData {
+	String key;
+	String data;
+
+}
--- authentication/authentication-demo/src/main/java/io/mosip/demo/authentication/service/impl/indauth/controller/Encrypt.java
+++ authentication/authentication-demo/src/main/java/io/mosip/demo/authentication/service/impl/indauth/controller/Encrypt.java
@@ -1,19 +1,14 @@
 package io.mosip.demo.authentication.service.impl.indauth.controller;
 
 import java.io.IOException;
-import java.net.URI;
 import java.nio.charset.Charset;
-import java.security.KeyFactory;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
-import java.security.PublicKey;
 import java.security.cert.CertificateException;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
 import java.time.LocalDateTime;
-import java.util.Base64;
-import java.util.HashMap;
+
 import java.util.Map;
 
 import javax.crypto.SecretKey;
@@ -22,8 +17,11 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.Arrays;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
@@ -36,15 +34,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import io.mosip.demo.authentication.service.dto.CryptomanagerRequestDto;
+import io.mosip.demo.authentication.service.dto.CryptomanagerResponseDto;
 import io.mosip.demo.authentication.service.dto.EncryptionRequestDto;
 import io.mosip.demo.authentication.service.dto.EncryptionResponseDto;
+import io.mosip.demo.authentication.service.dto.KernalEncryptedData;
 import io.mosip.kernel.core.util.CryptoUtil;
 import io.mosip.kernel.crypto.jce.impl.EncryptorImpl;
 import io.mosip.kernel.keygenerator.bouncycastle.KeyGenerator;
@@ -83,40 +83,48 @@ public class Encrypt {
 
 	@PostMapping(path = "/identity/encrypt")
 	@ApiOperation(value = "Encrypt Identity with sessionKey and Encrypt Session Key with Public Key", response = EncryptionResponseDto.class)
-	public EncryptionResponseDto encrypt(@RequestBody EncryptionRequestDto encryptionRequestDto)
-			throws NoSuchAlgorithmException, InvalidKeySpecException, IOException, KeyManagementException {
+	public KernalEncryptedData encrypt(@RequestBody EncryptionRequestDto encryptionRequestDto)
+			throws NoSuchAlgorithmException, InvalidKeySpecException, IOException, KeyManagementException,
+			RestClientException, JSONException {
 		EncryptionResponseDto encryptionResponseDto = new EncryptionResponseDto();
 
-		SecretKey sessionKey = keyGenerator.getSymmetricKey();
-		ObjectMapper objMapper = new ObjectMapper();
-		// Encrypt data with session key
-		Map<String, Object> identityRequest = encryptionRequestDto.getIdentityRequest();
-		byte[] data = objMapper.writeValueAsBytes(identityRequest);
-		byte[] encryptedData = encryptor.symmetricEncrypt(sessionKey, data);
-		encryptionResponseDto.setEncryptedIdentity(Base64.getEncoder().encodeToString(encryptedData));
+//		SecretKey sessionKey = keyGenerator.getSymmetricKey();
+//		ObjectMapper objMapper = new ObjectMapper();
+//		// Encrypt data with session key
+//		Map<String, Object> identityRequest = encryptionRequestDto.getIdentityRequest();
+//		byte[] data = objMapper.writeValueAsBytes(identityRequest);
+//		byte[] encryptedData = encryptor.symmetricEncrypt(sessionKey, data);
+//		encryptionResponseDto.setEncryptedIdentity(Base64.getEncoder().encodeToString(encryptedData));
 
 //		KeyPair asymmetricKey = keyGenerator.getAsymmetricKey();
 //		PublicKey publicKey = asymmetricKey.getPublic();
 //		byte[] privateKey = asymmetricKey.getPrivate().getEncoded();
 //		storePrivateKey(privateKey, encryptionRequestDto.getTspID());
-		PublicKey publicKey = loadPublicKey();
+		String encryptedResponse = getEncryptedValue(encryptionRequestDto.getIdentityRequest().toString());
 
-		// Encrypt session Key with public Key
-		byte[] encryptedsessionKey = encryptor.asymmetricPublicEncrypt(publicKey, sessionKey.getEncoded());
-		encryptionResponseDto.setEncryptedSessionKey(Base64.getEncoder().encodeToString(encryptedsessionKey));
+		KernalEncryptedData value = split(encryptedResponse);
 
-		return encryptionResponseDto;
+//		// Encrypt session Key with public Key
+//		byte[] encryptedsessionKey = encryptor.asymmetricPublicEncrypt(publicKey, sessionKey.getEncoded());
+//		encryptionResponseDto.setEncryptedSessionKey(Base64.getEncoder().encodeToString(encryptedsessionKey));
+		return value;
+//		return encryptionResponseDto;
 	}
 
-	private PublicKey loadPublicKey()
-			throws InvalidKeySpecException, NoSuchAlgorithmException, IOException, KeyManagementException {
-		byte[] publicKeyBytes = getPublicKey();
-		PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publicKeyBytes));
+	private KernalEncryptedData split(String value) {
+		KernalEncryptedData kernalencryptedData = new KernalEncryptedData();
+		// byte[] encryptedHybridData = CryptoUtil.decodeBase64(value);
+		byte[] encryptedHybridData = Base64.decodeBase64(value);
+		int keyDemiliterIndex = 0;
+		String keySplitter = "#KEY_SPLITTER#";
+		keyDemiliterIndex = CryptoUtil.getSplitterIndex(encryptedHybridData, keyDemiliterIndex, keySplitter);
+		byte[] encryptedKey = Arrays.copyOfRange(encryptedHybridData, 0, keyDemiliterIndex);
+		byte[] encryptedData = Arrays.copyOfRange(encryptedHybridData, keyDemiliterIndex + keySplitter.length(),
+				encryptedHybridData.length);
 
-		// PublicKey publicKey =KeyFactory.getInstance("RSA").generatePublic(new
-		// RSAPublicKeySpec);
-
-		return publicKey;
+		kernalencryptedData.setKey(Base64.encodeBase64String(encryptedKey));
+		kernalencryptedData.setData(Base64.encodeBase64String(encryptedData));
+		return kernalencryptedData;
 	}
 
 //	private void storePrivateKey(byte[] encodedvalue, String tspId) {
@@ -136,34 +144,48 @@ public class Encrypt {
 //		}
 //	}
 
-	public byte[] getPublicKey() throws IOException, KeyManagementException, NoSuchAlgorithmException {
+	public String getEncryptedValue(String data)
+			throws IOException, KeyManagementException, NoSuchAlgorithmException, RestClientException, JSONException {
 
 		byte[] output = null;
 		JsonNode jsonNode = null;
 
 		turnOffSslChecking();
-		String url1 = "https://integ.mosip.io/keymanager/v1.0/publickey/{applicationId}";
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.setErrorHandler(new TestErrorHandler());
-		Map<String, Object> uriVariables = new HashMap<String, Object>();
-		uriVariables.put("applicationId", "IDA");
+		String fooResourceUrl = "https://integ.mosip.io/cryptomanager/v1.0/encrypt";
+		CryptomanagerRequestDto request = new CryptomanagerRequestDto();
+		request.setApplicationId("IDA");
+		request.setData(Base64.encodeBase64String(data.getBytes()));
+		request.setReferenceId("REF01");
+		request.setTimeStamp(LocalDateTime.now().toString());
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-		HttpEntity<Object> entity = new HttpEntity<Object>(headers);
+		ResponseEntity<CryptomanagerResponseDto> response = restTemplate.exchange(fooResourceUrl, HttpMethod.POST,
+				getHeaders(request), CryptomanagerResponseDto.class);
+		return response.getBody().getData();
 
-		URI uri = UriComponentsBuilder.fromUriString(url1).buildAndExpand(uriVariables).toUri();
-		uri = UriComponentsBuilder.fromUri(uri).queryParam("timeStamp", TIMESTAMP)
-				.queryParam("referenceId", REFERENCEID.toString()).build().toUri();
-
-		ResponseEntity<ObjectNode> exchange = restTemplate.exchange(uri, HttpMethod.GET, entity, ObjectNode.class);
-
-		if (exchange.getBody().has(PUBLICKEY)) {
-			return CryptoUtil.decodeBase64(exchange.getBody().get(PUBLICKEY).asText());
-		} else {
-			throw new IOException();
-		}
-
+//		String url1 = "https://integ.mosip.io/cryptomanager/v1.0/encrypt";
+//		RestTemplate restTemplate = new RestTemplate();
+//		restTemplate.setErrorHandler(new TestErrorHandler());
+//		Map<String, Object> uriVariables = new HashMap<String, Object>();
+//
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+//		HttpEntity<Object> entity = new HttpEntity<Object>(headers);
+//
+//		URI uri = UriComponentsBuilder.fromUriString(url1).buildAndExpand(uriVariables).toUri();
+//		uri = UriComponentsBuilder.fromUri(uri).queryParam("timeStamp", TIMESTAMP)
+//				.queryParam("referenceId", REFERENCEID.toString()).build().toUri();
+//
+//		ResponseEntity<ObjectNode> exchange = restTemplate.exchange(uri, HttpMethod.GET, entity, ObjectNode.class);
+//
+//		if (exchange.getBody().has(PUBLICKEY)) {
+//			String getEncodedValue = exchange.getBody().get(PUBLICKEY).toString();
+//			return getEncodedValue.getBytes();
+////			return Base64.getDecoder().decode((exchange.getBody().get(PUBLICKEY).asText()));
+//		} else {
+//			throw new IOException();
+//		}
 
 		// ObjectMapper mapper = new ObjectMapper();
 		// output = mapper.writeValueAsBytes(jsonNode);
@@ -179,6 +201,12 @@ public class Encrypt {
 		// } else {
 		// throw new IOException();
 		// }
+	}
+
+	private HttpEntity getHeaders(CryptomanagerRequestDto req) throws JSONException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		return new HttpEntity(req, headers);
 	}
 
 	private static final TrustManager[] UNQUESTIONING_TRUST_MANAGER = new TrustManager[] { new X509TrustManager() {
@@ -215,8 +243,4 @@ public class Encrypt {
 		}
 	}
 
-	public static void main(String[] args)
-			throws KeyManagementException, InvalidKeySpecException, NoSuchAlgorithmException, IOException {
-		new Encrypt().loadPublicKey();
-	}
 }
