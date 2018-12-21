6f1e0fd2ce13b262360e5f73eda7f8ffb89ddbd6
M1030448 <M1030448@A2ML23749.mindtree.com>
2018-12-21 10:31:16 +0530

    adding audit test case
    Former-commit-id: 19d9147e6913322d6d853c6f87dbea293e963f8e
    Former-commit-id: 36d5d9a2298296db30f03769771a99b4f8452d22

--- registration-processor/registration-processor-rest-client/src/test/java/io/mosip/registration/processor/rest/client/service/AuditLogRequestBuilderTest.java
+++ registration-processor/registration-processor-rest-client/src/test/java/io/mosip/registration/processor/rest/client/service/AuditLogRequestBuilderTest.java
@@ -2,16 +2,23 @@ package io.mosip.registration.processor.rest.client.service;
 
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.any;
+import static org.mockito.ArgumentMatchers.any;
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import io.mosip.registration.processor.core.exception.ApisResourceAccessException;
 import io.mosip.registration.processor.core.spi.restclient.RegistrationProcessorRestClientService;
 import io.mosip.registration.processor.rest.client.audit.builder.AuditLogRequestBuilder;
@@ -28,6 +35,9 @@ public class AuditLogRequestBuilderTest {
 	private RegistrationProcessorRestClientService<Object> registrationProcessorRestService;
 	
 	AuditResponseDto dto;
+	private Logger fooLogger;
+	
+	private ListAppender<ILoggingEvent> listAppender;	
 	
 	@Test
 	public void createAuditRequestBuilderTest() throws ApisResourceAccessException {
@@ -36,5 +46,20 @@ public class AuditLogRequestBuilderTest {
 		Mockito.when(registrationProcessorRestService.postApi(any(), any(), any(), any(), any())).thenReturn(dto);
 		assertTrue(auditLogRequestBuilder.createAuditRequestBuilder("abcde", "200", "ADD", "ADD", "123456789").isStatus());
 		
+	}
+	
+	@Test
+	public void createAuditRequestBuilderFailureTest() throws ApisResourceAccessException {
+		 fooLogger = (Logger) LoggerFactory.getLogger(AuditLogRequestBuilder.class);
+	     listAppender = new ListAppender<>();
+	     listAppender.start();
+	     fooLogger.addAppender(listAppender);
+		ApisResourceAccessException exp = new ApisResourceAccessException("errorMessage");
+		Mockito.when(registrationProcessorRestService.postApi(any(), any(), any(), any(), any())).thenThrow(exp);
+		
+		auditLogRequestBuilder.createAuditRequestBuilder("abcde", "200", "ADD", "ADD", "123456789");
+		Assertions.assertThat(listAppender.list)
+        .extracting( ILoggingEvent::getLevel, ILoggingEvent::getFormattedMessage)
+		.containsExactly(Tuple.tuple( Level.ERROR, "RPR-RCT-001 --> errorMessage")); 
 	}
 }
