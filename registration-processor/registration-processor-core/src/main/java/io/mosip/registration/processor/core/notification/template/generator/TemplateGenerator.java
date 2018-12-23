064e5d593f77578d40a00ce80fd846409f4e8909
Alok Ranjan <Alok.Ranjan2@mindtree.com>
2018-12-23 07:39:46 +0530

    MOS-1066 Added dtos in registration core
    Former-commit-id: cd1c7592577b370553a3d74160e0223ed9ad4f99
    Former-commit-id: 0668a795a8bf330fec9981157d4c67f311f43f79

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/notification/template/generator/TemplateGenerator.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/notification/template/generator/TemplateGenerator.java
@@ -6,45 +6,46 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectOutputStream;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import io.mosip.kernel.core.templatemanager.exception.TemplateParsingException;
 import io.mosip.kernel.core.templatemanager.exception.TemplateResourceNotFoundException;
 import io.mosip.kernel.core.templatemanager.spi.TemplateManager;
-import io.mosip.kernel.masterdata.dto.getresponse.TemplateResponseDto;
+import io.mosip.registration.processor.core.code.ApiName;
+import io.mosip.registration.processor.core.exception.ApisResourceAccessException;
 import io.mosip.registration.processor.core.exception.TemplateNotFoundException;
 import io.mosip.registration.processor.core.exception.TemplateProcessingFailureException;
 import io.mosip.registration.processor.core.exception.util.PlatformErrorMessages;
+import io.mosip.registration.processor.core.spi.restclient.RegistrationProcessorRestClientService;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
-@Component
 public class TemplateGenerator {
-	
 
-	/** The log. */
+	
 	private static Logger log = LoggerFactory.getLogger(TemplateGenerator.class);
 
 	@Autowired
 	private TemplateManager templateManager;
 
-	@Value("${kernal.masterdata.uri}")
-	private String kernalMasterDataUri;
+	
+	@Autowired
+	private RegistrationProcessorRestClientService<Object> restClientService;
 
 	public String templateGenerator(String templateTypeCode, Map<String, Object> attributes, String langCode)
 			throws IOException {
-		RestTemplate restTemplate = new RestTemplate();
-		String uri = kernalMasterDataUri + langCode + "/" + templateTypeCode;
 		String artifact = null;
 		try {
-			TemplateResponseDto template = restTemplate.getForObject(uri, TemplateResponseDto.class);
+			List<String> pathSegments = new ArrayList<>();
+			pathSegments.add(langCode);
+			pathSegments.add(templateTypeCode);
+			TemplateResponseDto template = (TemplateResponseDto) restClientService.getApi(ApiName.MASTER_DATA,
+					pathSegments, "", "", TemplateResponseDto.class);
 
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			ObjectOutputStream oos = new ObjectOutputStream(baos);
@@ -58,11 +59,11 @@ public class TemplateGenerator {
 
 			artifact = writer.toString();
 
-		} catch (RestClientException e) {
+		} catch (ApisResourceAccessException e) {
 			throw new TemplateNotFoundException(PlatformErrorMessages.RPR_TEM_NOT_FOUND.getCode());
 		} catch (TemplateResourceNotFoundException | TemplateParsingException e) {
 			throw new TemplateProcessingFailureException(PlatformErrorMessages.RPR_TEM_PROCESSING_FAILURE.getCode());
-		} catch( Exception e) {
+		} catch (Exception e) {
 			log.error("Template was not generated due to some internal error");
 		}
 
