22392053f5e0b335d08e3637784e9923a9af8a4f
M1046571 <M1046571@A2ML29161.mindtree.com>
2018-12-26 15:09:54 +0530

    modification in MasterdataIntegrationTest and java doc added in document type repository.
    Former-commit-id: 6941d5b136a0512138f147a221affe723dbb6cdb
    Former-commit-id: 71eebd6aef770cda1104379da56dba44f54c90e3

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/DocumentTypeRepository.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/DocumentTypeRepository.java
@@ -18,6 +18,16 @@ import io.mosip.kernel.masterdata.entity.id.CodeAndLanguageCodeID;
  */
 @Repository
 public interface DocumentTypeRepository extends BaseRepository<DocumentType, CodeAndLanguageCodeID> {
+
+	/**
+	 * Get Document Type by specific code and language code
+	 * 
+	 * @param code
+	 *            the document category code.
+	 * @param langCode
+	 *            the language code.
+	 * @return object of {@link DocumentType}.
+	 */
 	@Query(value = "select dt.code, dt.name, dt.descr , dt.lang_code , dt.is_active ,dt.cr_by ,dt.cr_dtimes ,dt.upd_by ,dt.upd_dtimes ,dt.is_deleted ,dt.del_dtimes from master.valid_document vd , master.doc_type dt , master.doc_category dc where vd.doctyp_code = dt.code and vd.doccat_code = dc.code and dc.code = ?1 and dc.lang_code = ?2 and (dt.is_deleted = false or dt.is_deleted is null)", nativeQuery = true)
 	List<DocumentType> findByCodeAndLangCodeAndIsDeletedFalse(String code, String langCode);
 
