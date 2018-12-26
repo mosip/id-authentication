6c68e85b10c0599299bd4525ff3c5c2433a6b814
urviljoshi <Urvil.Joshi2.mindtree.com>
2018-12-26 14:35:32 +0530

    MOS-1053 #comment java impl added
    Former-commit-id: 952845b51156d4ce802b87d7c9264f6f30b1c80a
    Former-commit-id: 0a8a7713d5c49ec2b9d151fca020c14be9c213d3

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/RegistrationCenterDeviceServiceImpl.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/RegistrationCenterDeviceServiceImpl.java
@@ -7,7 +7,6 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import io.mosip.kernel.core.dataaccess.exception.DataAccessLayerException;
 import io.mosip.kernel.masterdata.constant.RegistrationCenterDeviceErrorCode;
