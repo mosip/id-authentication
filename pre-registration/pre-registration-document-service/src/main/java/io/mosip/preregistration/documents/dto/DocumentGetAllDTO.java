fff04f99e8b59d3ec7bcb160f786023a1373005a
Rajath Kotebailu Raghuchandra <M1037717@mindtree.com>
2018-12-26 07:39:20 +0530

    added service test case
    Former-commit-id: 2b3fc21dab54b697373ee756a0996c83c4b58776
    Former-commit-id: 454eb2b9093f51ef79a0a357f446435e8cc565d5

--- pre-registration/pre-registration-document-service/src/main/java/io/mosip/preregistration/documents/dto/DocumentGetAllDTO.java
+++ pre-registration/pre-registration-document-service/src/main/java/io/mosip/preregistration/documents/dto/DocumentGetAllDTO.java
@@ -5,11 +5,13 @@ import java.io.Serializable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class DocumentGetAllDTO implements Serializable {
 
 	/**
--- pre-registration/pre-registration-document-service/src/main/java/io/mosip/preregistration/documents/exception/util/DocumentExceptionCatcher.java
+++ pre-registration/pre-registration-document-service/src/main/java/io/mosip/preregistration/documents/exception/util/DocumentExceptionCatcher.java
@@ -12,11 +12,17 @@ import io.mosip.kernel.core.exception.ParseException;
 import io.mosip.kernel.core.util.exception.JsonMappingException;
 import io.mosip.kernel.core.util.exception.JsonParseException;
 import io.mosip.preregistration.core.exception.InvalidRequestParameterException;
+import io.mosip.preregistration.documents.code.StatusCodes;
 import io.mosip.preregistration.documents.errorcodes.ErrorCodes;
 import io.mosip.preregistration.documents.errorcodes.ErrorMessages;
 import io.mosip.preregistration.documents.exception.ConnectionUnavailableException;
 import io.mosip.preregistration.documents.exception.DTOMappigException;
+import io.mosip.preregistration.documents.exception.DocumentFailedToCopyException;
 import io.mosip.preregistration.documents.exception.DocumentFailedToUploadException;
+import io.mosip.preregistration.documents.exception.DocumentNotFoundException;
+import io.mosip.preregistration.documents.exception.DocumentNotValidException;
+import io.mosip.preregistration.documents.exception.DocumentSizeExceedException;
+import io.mosip.preregistration.documents.exception.DocumentVirusScanException;
 import io.mosip.preregistration.documents.exception.FileNotFoundException;
 import io.mosip.preregistration.documents.exception.InvalidConnectionParameters;
 import io.mosip.preregistration.documents.exception.MandatoryFieldNotFoundException;
@@ -48,18 +54,36 @@ public class DocumentExceptionCatcher {
 		} else if (ex instanceof MandatoryFieldNotFoundException) {
 			throw new MandatoryFieldNotFoundException(ErrorCodes.PRG_PAM_DOC_014.toString(),
 					ErrorMessages.MANDATORY_FIELD_NOT_FOUND.toString());
-		}else if (ex instanceof AmazonS3Exception) {
-			if(((AmazonServiceException) ex).getStatusCode() == 403) {
+		} else if (ex instanceof AmazonS3Exception) {
+			if (((AmazonServiceException) ex).getStatusCode() == 403) {
 				throw new InvalidConnectionParameters(ErrorCodes.PRG_PAM_DOC_015.toString(),
 						ErrorMessages.INVALID_CEPH_CONNECTION.toString());
-			}
-			else if(((AmazonServiceException) ex).getStatusCode() == 404) {
+			} else if (((AmazonServiceException) ex).getStatusCode() == 404) {
 				throw new FileNotFoundException(ErrorCodes.PRG_PAM_DOC_005.toString(),
 						ErrorMessages.DOCUMENT_NOT_PRESENT.toString());
 			}
-		}else if (ex instanceof SdkClientException) {
+		} else if (ex instanceof SdkClientException) {
 			throw new ConnectionUnavailableException(ErrorCodes.PRG_PAM_DOC_017.toString(),
 					ErrorMessages.CONNECTION_UNAVAILABLE.toString());
+		} else if (ex instanceof DocumentNotValidException) {
+			throw new DocumentNotValidException(ErrorCodes.PRG_PAM_DOC_004.toString(),
+					ErrorMessages.DOCUMENT_INVALID_FORMAT.toString());
+
+		} else if (ex instanceof DocumentSizeExceedException) {
+			throw new DocumentSizeExceedException(ErrorCodes.PRG_PAM_DOC_007.toString(),
+					ErrorMessages.DOCUMENT_EXCEEDING_PREMITTED_SIZE.toString());
+		} else if (ex instanceof DocumentVirusScanException) {
+			throw new DocumentVirusScanException(ErrorCodes.PRG_PAM_DOC_010.toString(),
+					ErrorMessages.DOCUMENT_FAILED_IN_VIRUS_SCAN.toString());
+		} else if(ex instanceof DocumentNotFoundException) {
+			throw new DocumentNotFoundException(ErrorCodes.PRG_PAM_DOC_005.toString(),
+					StatusCodes.DOCUMENT_IS_MISSING.toString());
+		} else if(ex instanceof DocumentFailedToCopyException) {
+			throw new DocumentFailedToCopyException(ErrorCodes.PRG_PAM_DOC_011.toString(),
+					ErrorMessages.DOCUMENT_FAILED_TO_COPY.toString());
+		}else if (ex instanceof DocumentFailedToUploadException) {
+			throw new DocumentFailedToUploadException(ErrorCodes.PRG_PAM_DOC_009.toString(),
+					ErrorMessages.DOCUMENT_FAILED_TO_UPLOAD.toString());
 		}
 
 	}
