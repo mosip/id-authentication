90cf6b51e4f88a21b10a1864db2eece1ec022d2a
M1044292 <M1044292@b2ml27210.mindtree.com>
2018-12-27 12:27:21 +0530

    MOS-11678 - Test Case fixed for TemplateGenerator
    Former-commit-id: 196b1fb21921ae1c3208cc9e6d5a5c308e3469e5
    Former-commit-id: c90e10fb154719ea9de7b9309b61269965c17051

--- registration/registration-service/src/test/java/io/mosip/registration/test/template/TemplateGeneratorTest.java
+++ registration/registration-service/src/test/java/io/mosip/registration/test/template/TemplateGeneratorTest.java
@@ -8,7 +8,10 @@ import java.io.IOException;
 import java.io.Writer;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
 
@@ -16,6 +19,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.powermock.api.mockito.PowerMockito;
@@ -24,6 +29,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import io.mosip.kernel.templatemanager.velocity.builder.TemplateManagerBuilderImpl;
 import io.mosip.registration.constants.RegistrationConstants;
+import io.mosip.registration.context.ApplicationContext;
 import io.mosip.registration.dto.RegistrationDTO;
 import io.mosip.registration.dto.biometric.FingerprintDetailsDTO;
 import io.mosip.registration.exception.RegBaseCheckedException;
@@ -33,34 +39,53 @@ import io.mosip.registration.util.acktemplate.TemplateGenerator;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ ImageIO.class })
 public class TemplateGeneratorTest {
-	TemplateManagerBuilderImpl template =  new TemplateManagerBuilderImpl();
-	
+	TemplateManagerBuilderImpl template = new TemplateManagerBuilderImpl();
+
 	@InjectMocks
 	TemplateGenerator templateGenerator;
-	
+
 	@Rule
 	public MockitoRule mockitoRule = MockitoJUnit.rule();
-	
+
+	@Mock
+	ApplicationContext applicationContext;
+
+	ResourceBundle dummyResourceBundle = new ResourceBundle() {
+		@Override
+		protected Object handleGetObject(String key) {
+			return "fake_translated_value";
+		}
+
+		@Override
+		public Enumeration<String> getKeys() {
+			return Collections.emptyEnumeration();
+		}
+	};
+
 	@Test
 	public void generateTemplateTest() throws IOException, URISyntaxException, RegBaseCheckedException {
 		RegistrationDTO registrationDTO = DataProvider.getPacketDTO();
 		List<FingerprintDetailsDTO> segmentedFingerprints = new ArrayList<>();
 		segmentedFingerprints.add(new FingerprintDetailsDTO());
-		registrationDTO.getBiometricDTO().getApplicantBiometricDTO().getFingerprintDetailsDTO().forEach(fingerPrintDTO -> {
-			fingerPrintDTO.setSegmentedFingerprints(segmentedFingerprints);
-		}); 
+		registrationDTO.getBiometricDTO().getApplicantBiometricDTO().getFingerprintDetailsDTO()
+				.forEach(fingerPrintDTO -> {
+					fingerPrintDTO.setSegmentedFingerprints(segmentedFingerprints);
+				});
 		PowerMockito.mockStatic(ImageIO.class);
 		BufferedImage image = null;
-		when(ImageIO.read(templateGenerator.getClass().getResourceAsStream(RegistrationConstants.TEMPLATE_HANDS_IMAGE_PATH))).thenReturn(image);
+		when(ImageIO.read(
+				templateGenerator.getClass().getResourceAsStream(RegistrationConstants.TEMPLATE_HANDS_IMAGE_PATH)))
+						.thenReturn(image);
+		when(applicationContext.getLocalLanguageProperty()).thenReturn(dummyResourceBundle);
 		Writer writer = templateGenerator.generateTemplate("sample text", registrationDTO, template);
 		assertNotNull(writer);
 	}
-	
+
 	@Test
 	public void generateNotificationTemplateTest() throws IOException, URISyntaxException, RegBaseCheckedException {
 		RegistrationDTO registrationDTO = DataProvider.getPacketDTO();
 		Writer writer = templateGenerator.generateNotificationTemplate("sample text", registrationDTO, template);
 		assertNotNull(writer);
 	}
-	
+
 }
