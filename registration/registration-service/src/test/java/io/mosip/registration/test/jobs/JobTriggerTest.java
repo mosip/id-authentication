4551b9d0e4686b83df9e29b3f5d8d21ac53ace8d
M1044402 <M1044402@b2ml27211.mindtree.com>
2018-12-28 13:33:57 +0530

    MOS-8942 : Updated Job Trigger Listener and updated UT for specific update.
    Former-commit-id: 9295e98939643f66cfc348904ced0ce242aa0106
    Former-commit-id: dccc918a53670247eaade9e830202b8e0720b47c

--- registration/registration-service/src/test/java/io/mosip/registration/test/jobs/JobTriggerTest.java
+++ registration/registration-service/src/test/java/io/mosip/registration/test/jobs/JobTriggerTest.java
@@ -10,6 +10,7 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.quartz.JobExecutionContext;
 import org.quartz.Trigger;
+import org.quartz.TriggerKey;
 import org.quartz.Trigger.CompletedExecutionInstruction;
 
 import io.mosip.registration.entity.SyncTransaction;
@@ -19,74 +20,92 @@ import io.mosip.registration.jobs.JobTriggerListener;
 import io.mosip.registration.jobs.SyncManager;
 
 public class JobTriggerTest {
-	
+
 	@Mock
-	SyncManager transactionManager;	
+	SyncManager transactionManager;
 
 	@Mock
 	Trigger trigger;
-	
+
 	@Mock
 	JobManager jobManager;
-	
-	
+
 	@Rule
 	public MockitoRule mockitoRule = MockitoJUnit.rule();
-	
+
 	@Mock
 	JobExecutionContext jobExecutionContext;
-	
+
 	@Mock
 	SyncTransaction syncTransaction;
 	CompletedExecutionInstruction completedExecutionInstruction;
-	
+
 	@InjectMocks
 	JobTriggerListener jobTriggerListener;
-	
+
 	@Before
 	public void initiateTest() {
 		Mockito.when(jobManager.getJobId(jobExecutionContext)).thenReturn("1");
 	}
-	
-	
+
 	@Test
 	public void triggerMisFiredTest() {
-		Mockito.when(transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(syncTransaction);
+		TriggerKey tKey = new TriggerKey("ID0001");
+		
+		Mockito.when(trigger.getKey()).thenReturn(tKey);
+
+		Mockito.when(
+				transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+				.thenReturn(syncTransaction);
 		jobTriggerListener.triggerMisfired(trigger);
 
 	}
-	
+
 	@Test
 	public void triggerCompleteTest() {
-		Mockito.when(transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(syncTransaction);
+		Mockito.when(
+				transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+				.thenReturn(syncTransaction);
 		jobTriggerListener.triggerComplete(trigger, jobExecutionContext, completedExecutionInstruction);
 
 	}
-	
+
 	@Test
 	public void triggerFiredTest() {
-		Mockito.when(transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(syncTransaction);
+		Mockito.when(
+				transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+				.thenReturn(syncTransaction);
 		jobTriggerListener.triggerFired(trigger, jobExecutionContext);
 
 	}
-	
+
 	@Test
 	public void triggerMisFiredExceptionTest() {
-		Mockito.when(transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenThrow(RegBaseUncheckedException.class);
+		TriggerKey tKey = new TriggerKey("ID0001");
+
+		Mockito.when(trigger.getKey()).thenReturn(tKey);
+
+		Mockito.when(
+				transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+				.thenThrow(RegBaseUncheckedException.class);
 		jobTriggerListener.triggerMisfired(trigger);
 
 	}
-	
+
 	@Test
 	public void triggerCompleteExceptionTest() {
-		Mockito.when(transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenThrow(RegBaseUncheckedException.class);
+		Mockito.when(
+				transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+				.thenThrow(RegBaseUncheckedException.class);
 		jobTriggerListener.triggerComplete(trigger, jobExecutionContext, completedExecutionInstruction);
 
 	}
-	
+
 	@Test
 	public void triggerFiredExceptionTest() {
-		Mockito.when(transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenThrow(RegBaseUncheckedException.class);
+		Mockito.when(
+				transactionManager.createSyncTransaction(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+				.thenThrow(RegBaseUncheckedException.class);
 		jobTriggerListener.triggerFired(trigger, jobExecutionContext);
 
 	}
