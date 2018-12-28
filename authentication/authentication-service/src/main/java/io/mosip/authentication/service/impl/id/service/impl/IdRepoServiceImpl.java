56f2f7a45c0a225055e05e7b9a0a5cae20d13bef
Sanjay Murali <sanjay.murali@mindtree.com>
2018-12-28 18:38:13 +0530

    MOS-10911:Coding VID validation – now from VID table only Former-commit-id: d07036ae76cd5af4d95e599106744e0038858bcf Former-commit-id: 2e3db0f2bbe76dc64b96a0a04790a1558487b5ab

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/id/service/impl/IdRepoServiceImpl.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/id/service/impl/IdRepoServiceImpl.java
@@ -63,7 +63,6 @@ public class IdRepoServiceImpl implements IdRepoService {
 			buildRequest.setPathVariables(params);
 			response = restHelper.requestSync(buildRequest);
 			response.put("uin", uin);
-
 		} catch (RestServiceException e) {
 			logger.error(SESSION_ID, ID_REPO_SERVICE, e.getErrorCode(), e.getErrorText());
 			throw new IdAuthenticationBusinessException(IdAuthenticationErrorConstants.SERVER_ERROR,e);
