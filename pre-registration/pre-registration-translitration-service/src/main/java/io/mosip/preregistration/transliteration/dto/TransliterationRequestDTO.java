bc2622a4a4b55abd3620fafebdca64e659d4898b
Kishan Rathore <M1043008@mindtree.com>
2018-12-28 15:20:42 +0530

    Refracted the file names
    Former-commit-id: 46b172696e2ecb9db6a5f2f118d36a1dbe5ed2df
    Former-commit-id: 439af25547b5bf131e1c83d4f40d74054f38c03a

---
+++ pre-registration/pre-registration-translitration-service/src/main/java/io/mosip/preregistration/transliteration/dto/TransliterationRequestDTO.java
@@ -0,0 +1,19 @@
+package io.mosip.preregistration.transliteration.dto;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TransliterationRequestDTO<T> {
+	
+	String id;
+	String ver;
+    Date reqTime;
+    T request;
+
+}
