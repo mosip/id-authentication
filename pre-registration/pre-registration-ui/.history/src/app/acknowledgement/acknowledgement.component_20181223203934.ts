5a37947c68b8a722495d436f7d185bf575e53e63
Gaurav Sharan <M1030608@mindtree.com>
2018-12-28 11:24:51 +0530

    commiting acknowledgement component
    Former-commit-id: 1f5bf31f28de45b88129708e1286a96244e96446
    Former-commit-id: 67dbdc1ae2705f46974cbc75d3a9ecba2a192d82

---
+++ pre-registration/pre-registration-ui/.history/src/app/acknowledgement/acknowledgement.component_20181223203934.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acknowledgement',
+  templateUrl: './acknowledgement.component.html',
+  styleUrls: ['./acknowledgement.component.css']
+})
+export class AcknowledgementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
