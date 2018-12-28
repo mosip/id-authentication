31391ffe8a9ecaa5f6c0992368fb5938a29cd159
Manoj SP <Manoj.SP@mindtree.com>
2018-12-28 14:08:51 +0530

    MOS-11816 - Added Documents in request
    Former-commit-id: 3adca5d03e503547292001443cdadec0327f01a9
    Former-commit-id: 2b562295862ede3d1d8a9d3aae3c1cb26c00e277

---
+++ kernel/kernel-idrepo-service/src/main/java/io/mosip/kernel/idrepo/dto/Documents.java
@@ -0,0 +1,22 @@
+package io.mosip.kernel.idrepo.dto;
+
+import lombok.Data;
+
+/**
+ * The Class Documents.
+ *
+ * @author Manoj SP
+ */
+
+/**
+ * Instantiates a new documents.
+ */
+@Data
+public class Documents {
+
+	/** The doc cat. */
+	private String docCat;
+	
+	/** The doc value. */
+	private String docValue; 
+}
