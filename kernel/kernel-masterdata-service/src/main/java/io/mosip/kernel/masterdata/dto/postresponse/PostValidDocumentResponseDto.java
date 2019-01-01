f95d0d63bf48f26f74f1f81b7aaad1e58e76ff03
M1044345 <M1044345@A2ML26966.mindtree.com>
2019-01-01 15:20:39 +0530

    MOS-12373 Valid document delete methods added
    Former-commit-id: b2aaf75a0703a37277f456cb204816fda0f1da50
    Former-commit-id: efdd0cceabe17311880296e888cb1850fb931368

---
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/postresponse/PostValidDocumentResponseDto.java
@@ -0,0 +1,10 @@
+package io.mosip.kernel.masterdata.dto.postresponse;
+
+import lombok.Data;
+
+@Data
+public class PostValidDocumentResponseDto {
+
+	private String docCategoryCode;
+	private String docTypeCode;
+}
--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/ValidDocumentServiceImpl.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/ValidDocumentServiceImpl.java
@@ -1,29 +1,38 @@
 package io.mosip.kernel.masterdata.service.impl;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import io.mosip.kernel.core.dataaccess.exception.DataAccessLayerException;
 import io.mosip.kernel.masterdata.constant.ApplicationErrorCode;
+import io.mosip.kernel.masterdata.constant.ValidDocumentErrorCode;
 import io.mosip.kernel.masterdata.dto.RequestDto;
 import io.mosip.kernel.masterdata.dto.ValidDocumentDto;
+import io.mosip.kernel.masterdata.dto.postresponse.PostValidDocumentResponseDto;
 import io.mosip.kernel.masterdata.entity.ValidDocument;
 import io.mosip.kernel.masterdata.entity.id.ValidDocumentID;
 import io.mosip.kernel.masterdata.exception.MasterDataServiceException;
+import io.mosip.kernel.masterdata.exception.RequestException;
 import io.mosip.kernel.masterdata.repository.ValidDocumentRepository;
 import io.mosip.kernel.masterdata.service.ValidDocumentService;
 import io.mosip.kernel.masterdata.utils.ExceptionUtils;
 import io.mosip.kernel.masterdata.utils.MetaDataUtils;
 
 /**
- * This service class contains methods that create valid document in table.
+ * This service class contains methods that create and delete valid document.
  * 
  * @author Ritesh Sinha
  * @since 1.0.0
  *
  */
 @Service
+@Transactional
 public class ValidDocumentServiceImpl implements ValidDocumentService {
 
 	/**
@@ -45,7 +54,7 @@ public class ValidDocumentServiceImpl implements ValidDocumentService {
 		ValidDocument validDocument = MetaDataUtils.setCreateMetaData(document.getRequest(), ValidDocument.class);
 		try {
 			validDocument = documentRepository.create(validDocument);
-		} catch (DataAccessLayerException  | DataAccessException   e) {
+		} catch (DataAccessLayerException | DataAccessException e) {
 			throw new MasterDataServiceException(ApplicationErrorCode.APPLICATION_INSERT_EXCEPTION.getErrorCode(),
 					ExceptionUtils.parseException(e));
 		}
@@ -54,6 +63,35 @@ public class ValidDocumentServiceImpl implements ValidDocumentService {
 		validDocumentId.setDocCategoryCode(validDocument.getDocCategoryCode());
 		validDocumentId.setDocTypeCode(validDocument.getDocTypeCode());
 		return validDocumentId;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * io.mosip.kernel.masterdata.service.ValidDocumentService#deleteValidDocuemnt(
+	 * java.lang.String, java.lang.String)
+	 */
+	@Override
+	public PostValidDocumentResponseDto deleteValidDocuemnt(String docCatCode, String docTypeCode) {
+		try {
+			int updatedRows = documentRepository.deleteValidDocument(LocalDateTime.now(ZoneId.of("UTC")), docCatCode,
+					docTypeCode, MetaDataUtils.getContextUser());
+
+			if (updatedRows < 1) {
+
+				throw new RequestException(ValidDocumentErrorCode.VALID_DOCUMENT_NOT_FOUND_EXCEPTION.getErrorCode(),
+						ValidDocumentErrorCode.VALID_DOCUMENT_NOT_FOUND_EXCEPTION.getErrorMessage());
+			}
+
+		} catch (DataAccessLayerException | DataAccessException e) {
+			throw new MasterDataServiceException(ValidDocumentErrorCode.VALID_DOCUMENT_DELETE_EXCEPTION.getErrorCode(),
+					ValidDocumentErrorCode.VALID_DOCUMENT_DELETE_EXCEPTION.getErrorMessage());
+		}
+		PostValidDocumentResponseDto responseDto = new PostValidDocumentResponseDto();
+		responseDto.setDocCategoryCode(docCatCode);
+		responseDto.setDocTypeCode(docTypeCode);
+		return responseDto;
 	}
 
 }
