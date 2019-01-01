7b31c858b6a5348180cfd6222fda323861235066
M1046464 <M1046464@A2ML27485.mindtree.com>
2019-01-01 15:51:33 +0530

    MOS-11924 added service,impl,controller methods
    Former-commit-id: a69b692cb030f6b7d74b1122875fc1c7a196a179
    Former-commit-id: 2bd5fa59cc4b6ce8af320ea76dae24978d221d5b

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/RegistrationCenterController.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/RegistrationCenterController.java
@@ -15,6 +15,7 @@ import io.mosip.kernel.masterdata.dto.RegistrationCenterDto;
 import io.mosip.kernel.masterdata.dto.RegistrationCenterHolidayDto;
 import io.mosip.kernel.masterdata.dto.RequestDto;
 import io.mosip.kernel.masterdata.dto.getresponse.RegistrationCenterResponseDto;
+import io.mosip.kernel.masterdata.dto.getresponse.ResgistrationCenterStatusResponseDto;
 import io.mosip.kernel.masterdata.dto.postresponse.IdResponseDto;
 import io.mosip.kernel.masterdata.service.RegistrationCenterService;
 import io.swagger.annotations.Api;
@@ -144,6 +145,12 @@ public class RegistrationCenterController {
 				hierarchyLevel, text);
 
 	}
+	
+	@GetMapping("/v1.0/registrationcenters/validate/{id}/{timestamp}")
+	public ResgistrationCenterStatusResponseDto validateTimestamp(@PathVariable("id") String regId ,@PathVariable("timestamp") String timeStamp) {
+		return registrationCenterService.validateTimestampWithRegistrationCenter(regId, timeStamp);
+		
+	}
 
 	/**
 	 * This method creates registration center.
@@ -158,4 +165,6 @@ public class RegistrationCenterController {
 		return new ResponseEntity<>(registrationCenterService.createRegistrationCenter(registrationCenterDto),
 				HttpStatus.CREATED);
 	}
+	
+	
 }
