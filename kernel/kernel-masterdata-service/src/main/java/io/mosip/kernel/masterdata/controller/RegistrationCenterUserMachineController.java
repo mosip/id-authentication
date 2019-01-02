da3637a2055bf9e51fb10f5561d2af684d28d6e4
M1039257 <M1039257@A2ML23736.mindtree.com>
2019-01-02 11:49:29 +0530

    MOS-10589 Added methods to delete center-user-machine mapping in db
    Former-commit-id: 4f16b9830382529d8cb05286a103d3cb9ca3739d
    Former-commit-id: 074d823d34109cf045680fcf96f81a8e73e4e46c

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/RegistrationCenterUserMachineController.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/RegistrationCenterUserMachineController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.kernel.masterdata.dto.RegistrationCenterUserMachineMappingDto;
 import io.mosip.kernel.masterdata.dto.RequestDto;
+import io.mosip.kernel.masterdata.entity.id.RegistrationCenterDeviceID;
 import io.mosip.kernel.masterdata.entity.id.RegistrationCenterMachineUserID;
 import io.mosip.kernel.masterdata.service.RegistrationCenterMachineUserService;
 import io.swagger.annotations.Api;
@@ -52,6 +55,16 @@ public class RegistrationCenterUserMachineController {
 			@ApiParam("Registration center id,user id and ,machine id with metadata") @RequestBody @Valid RequestDto<RegistrationCenterUserMachineMappingDto> registrationCenterUserMachineMappingDto) {
 		return new ResponseEntity<>(registrationCenterMachineUserService.createRegistrationCentersMachineUserMapping(
 				registrationCenterUserMachineMappingDto), HttpStatus.CREATED);
+	}
+	
+	@ApiOperation(value = "Delete the mapping of registration center and user and machine", response = RegistrationCenterMachineUserID.class)
+	@DeleteMapping("/{regCenterId}/{machineId}/{userId}")
+	public ResponseEntity<RegistrationCenterMachineUserID> deleteRegistrationCenterUserMachineMapping(
+			@ApiParam("Registration center id to be deleted") @PathVariable String regCenterId,
+			@ApiParam("Machine id to be deleted") @PathVariable String machineId,@ApiParam("User id to be deleted") @PathVariable String userId) {
+		return new ResponseEntity<>(
+				registrationCenterMachineUserService.deleteRegistrationCentersMachineUserMapping(regCenterId, machineId, userId),
+				HttpStatus.OK);
 	}
 
 }
--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/RegistrationCenterMachineUserServiceImpl.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/RegistrationCenterMachineUserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import io.mosip.kernel.core.dataaccess.exception.DataAccessLayerException;
+import io.mosip.kernel.masterdata.constant.RegistrationCenterDeviceErrorCode;
 import io.mosip.kernel.masterdata.constant.RegistrationCenterMachineUserMappingErrorCode;
 import io.mosip.kernel.masterdata.dto.RegistrationCenterUserMachineMappingDto;
 import io.mosip.kernel.masterdata.dto.RequestDto;
@@ -14,6 +15,7 @@ import io.mosip.kernel.masterdata.entity.RegistrationCenterUserMachine;
 import io.mosip.kernel.masterdata.entity.RegistrationCenterUserMachineHistory;
 import io.mosip.kernel.masterdata.entity.id.RegistrationCenterMachineUserID;
 import io.mosip.kernel.masterdata.exception.MasterDataServiceException;
+import io.mosip.kernel.masterdata.exception.RequestException;
 import io.mosip.kernel.masterdata.repository.RegistrationCenterMachineUserRepository;
 import io.mosip.kernel.masterdata.repository.RegistrationCenterUserMachineHistoryRepository;
 import io.mosip.kernel.masterdata.service.RegistrationCenterMachineUserService;
@@ -36,38 +38,82 @@ public class RegistrationCenterMachineUserServiceImpl implements RegistrationCen
 	 * Instance of {@link RegistrationCenterMachineUserRepository}
 	 */
 	@Autowired
-	RegistrationCenterMachineUserRepository  registrationCenterMachineUserRepository;
-	
+	RegistrationCenterMachineUserRepository registrationCenterMachineUserRepository;
+
 	/**
 	 * Instance of {@link RegistrationCenterUserMachineHistoryRepository}
 	 */
 	@Autowired
 	RegistrationCenterUserMachineHistoryRepository registrationCenterUserMachineHistoryRepository;
-	
-	/* (non-Javadoc)
-	 * @see io.mosip.kernel.masterdata.service.RegistrationCenterMachineUserService#createRegistrationCentersMachineUserMapping(io.mosip.kernel.masterdata.dto.RequestDto)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.mosip.kernel.masterdata.service.RegistrationCenterMachineUserService#
+	 * createRegistrationCentersMachineUserMapping(io.mosip.kernel.masterdata.dto.
+	 * RequestDto)
 	 */
 	@Override
 	@Transactional
 	public RegistrationCenterMachineUserID createRegistrationCentersMachineUserMapping(
 			RequestDto<RegistrationCenterUserMachineMappingDto> registrationCenterUserMachineMappingDto) {
-        
-		RegistrationCenterUserMachine registrationCenterUserMachine=MetaDataUtils.setCreateMetaData(registrationCenterUserMachineMappingDto.getRequest(), RegistrationCenterUserMachine.class);
-		RegistrationCenterUserMachineHistory registrationCenterUserMachineHistory=MetaDataUtils.setCreateMetaData(registrationCenterUserMachineMappingDto.getRequest(), RegistrationCenterUserMachineHistory.class);
+
+		RegistrationCenterUserMachine registrationCenterUserMachine = MetaDataUtils.setCreateMetaData(
+				registrationCenterUserMachineMappingDto.getRequest(), RegistrationCenterUserMachine.class);
+		RegistrationCenterUserMachineHistory registrationCenterUserMachineHistory = MetaDataUtils.setCreateMetaData(
+				registrationCenterUserMachineMappingDto.getRequest(), RegistrationCenterUserMachineHistory.class);
 		registrationCenterUserMachineHistory.setEffectivetimes(registrationCenterUserMachine.getCreatedDateTime());
 		registrationCenterUserMachineHistory.setCreatedDateTime(registrationCenterUserMachine.getCreatedDateTime());
-		
+
 		try {
-			registrationCenterMachineUserRepository.create(registrationCenterUserMachine);	
+			registrationCenterMachineUserRepository.create(registrationCenterUserMachine);
 			registrationCenterUserMachineHistoryRepository.create(registrationCenterUserMachineHistory);
-		}catch(DataAccessLayerException | DataAccessException exception) {
-			throw new MasterDataServiceException(RegistrationCenterMachineUserMappingErrorCode.REGISTRATION_CENTER_USER_MACHINE_MAPPING_INSERT_EXCEPTION.getErrorCode(),
-					RegistrationCenterMachineUserMappingErrorCode.REGISTRATION_CENTER_USER_MACHINE_MAPPING_INSERT_EXCEPTION.getErrorMessage()
-							+ ExceptionUtils.parseException(exception));
+		} catch (DataAccessLayerException | DataAccessException exception) {
+			throw new MasterDataServiceException(
+					RegistrationCenterMachineUserMappingErrorCode.REGISTRATION_CENTER_USER_MACHINE_MAPPING_INSERT_EXCEPTION
+							.getErrorCode(),
+					RegistrationCenterMachineUserMappingErrorCode.REGISTRATION_CENTER_USER_MACHINE_MAPPING_INSERT_EXCEPTION
+							.getErrorMessage() + ExceptionUtils.parseException(exception));
 		}
 		return MapperUtils.map(registrationCenterUserMachine, RegistrationCenterMachineUserID.class);
 	}
 
+	@Override
+	public RegistrationCenterMachineUserID deleteRegistrationCentersMachineUserMapping(String regCenterId,
+			String machineId, String userId) {
+		RegistrationCenterMachineUserID registrationCenterMachineUserID = null;
+		try {
 
+			RegistrationCenterUserMachine registrationCenterUserMachine = registrationCenterMachineUserRepository
+					.findAllNondeletedMappings(regCenterId, machineId, userId);
+			if (registrationCenterUserMachine == null) {
+				throw new RequestException(
+						RegistrationCenterDeviceErrorCode.REGISTRATION_CENTER_DEVICE_DATA_NOT_FOUND.getErrorCode(),
+						RegistrationCenterDeviceErrorCode.REGISTRATION_CENTER_DEVICE_DATA_NOT_FOUND.getErrorMessage());
+			} else {
+				RegistrationCenterUserMachine centerUserMachine = registrationCenterUserMachine;
+				centerUserMachine = MetaDataUtils.setDeleteMetaData(centerUserMachine);
+				RegistrationCenterUserMachineHistory history = MapperUtils.map(centerUserMachine,
+						RegistrationCenterUserMachineHistory.class);
+				history.setCntrId(regCenterId);
+				history.setMachineId(machineId);
+				history.setUsrId(userId);
+				history.setEffectivetimes(centerUserMachine.getDeletedDateTime());
+				MapperUtils.setBaseFieldValue(centerUserMachine, history);
+				registrationCenterUserMachineHistoryRepository.create(history);
+				registrationCenterMachineUserRepository.update(centerUserMachine);
+				registrationCenterMachineUserID = new RegistrationCenterMachineUserID();
+				registrationCenterMachineUserID.setCntrId(regCenterId);
+				registrationCenterMachineUserID.setMachineId(machineId);
+				registrationCenterMachineUserID.setUsrId(userId);
+			}
+		} catch (DataAccessLayerException | DataAccessException e) {
+			throw new MasterDataServiceException(
+					RegistrationCenterDeviceErrorCode.REGISTRATION_CENTER_DEVICE_DELETE_EXCEPTION.getErrorCode(),
+					RegistrationCenterDeviceErrorCode.REGISTRATION_CENTER_DEVICE_DELETE_EXCEPTION.getErrorMessage()
+							+ ExceptionUtils.parseException(e));
+		}
+		return registrationCenterMachineUserID;
+	}
 
 }
