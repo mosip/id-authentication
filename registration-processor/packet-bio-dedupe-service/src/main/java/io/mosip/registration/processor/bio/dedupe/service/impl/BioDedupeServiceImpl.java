538b092122d9547831069b709ce82e0695fb9bfd
M1039285 <M1039285@a2ml16222.mindtree.com>
2019-01-02 17:34:21 +0530

    Bio dedupe class code update  MOS-1095
    Former-commit-id: 5ccb26ccafb62db758a4012c58d723b9b285173d
    Former-commit-id: 1d095363ad46f3ed2a667c3924205f1567a5138d

--- registration-processor/packet-bio-dedupe-service/src/main/java/io/mosip/registration/processor/bio/dedupe/service/impl/BioDedupeServiceImpl.java
+++ registration-processor/packet-bio-dedupe-service/src/main/java/io/mosip/registration/processor/bio/dedupe/service/impl/BioDedupeServiceImpl.java
@@ -3,36 +3,55 @@ package io.mosip.registration.processor.bio.dedupe.service.impl;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
+import io.mosip.registration.processor.abis.dto.AbisInsertRequestDto;
+import io.mosip.registration.processor.abis.dto.AbisInsertResponceDto;
+import io.mosip.registration.processor.core.code.ApiName;
+import io.mosip.registration.processor.core.exception.ApisResourceAccessException;
 import io.mosip.registration.processor.core.spi.biodedupe.BioDedupeService;
+import io.mosip.registration.processor.core.spi.restclient.RegistrationProcessorRestClientService;
 
 @RefreshScope
 @Service
 public class BioDedupeServiceImpl implements BioDedupeService {
+	AbisInsertRequestDto abisInsertRequestDto = new AbisInsertRequestDto();
+
+	/** The rest client service. */
+	@Autowired
+	RegistrationProcessorRestClientService<Object> restClientService;
 
 	@Override
-	public String insertBiometrics(String RegistrationId) {
+	public String insertBiometrics(String registrationId) throws ApisResourceAccessException {
 
-		String InsertStatus = "failure";
+		String requestId = uuidGenerator();
+		String referenceId = uuidGenerator();
 
-		// AbisInsertRequestDto abisInsertRequestDto = new AbisInsertRequestDto();
+		abisInsertRequestDto.setRequestId(requestId);
+		abisInsertRequestDto.setReferenceId(referenceId);
+		abisInsertRequestDto.setReferenceURL("https://mosip.io/biometric/" + registrationId);
 
-		String requestId = UUIDgenerator();
-		String referenceId = UUIDgenerator();
+		// add the reqId and RefId to DB
 
-		return InsertStatus;
+		AbisInsertResponceDto authResponseDTO = (AbisInsertResponceDto) restClientService.postApi(ApiName.AUTHINTERNAL,
+				"", "", abisInsertRequestDto, AbisInsertResponceDto.class);
+
+		return authResponseDTO.getReturnValue();
 
 	}
 
 	@Override
-	public List<String> performDedupe(String RegistrationId) {
-		// TODO Auto-generated method stub
+	public List<String> performDedupe(String registrationId) {
+		String requestId = uuidGenerator();
+		// Fetch the referenceId from Db
+		// String referenceId =
+
 		return null;
 	}
 
-	private String UUIDgenerator() {
+	private String uuidGenerator() {
 		return UUID.randomUUID().toString();
 	}
 
--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/spi/biodedupe/BioDedupeService.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/spi/biodedupe/BioDedupeService.java
@@ -2,9 +2,11 @@ package io.mosip.registration.processor.core.spi.biodedupe;
 
 import java.util.List;
 
+import io.mosip.registration.processor.core.exception.ApisResourceAccessException;
+
 public interface BioDedupeService {
 
-	public String insertBiometrics(String RegistrationId);
+	public String insertBiometrics(String RegistrationId) throws ApisResourceAccessException;
 
 	public List<String> performDedupe(String RegistrationId);
 
