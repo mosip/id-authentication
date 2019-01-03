5488e33fc4d3ed3877607d2a0de26ade543562d8
M1039285 <M1039285@a2ml16222.mindtree.com>
2019-01-03 16:58:35 +0530

    MOS-1095 code update
    Former-commit-id: f7a418be32ad89b0f76de68e97ab79a9027d1b45
    Former-commit-id: e3e17266692b39a10495e3ae2548699e26bbb1ea

---
+++ registration-processor/packet-bio-dedupe-service/src/test/java/io/mosip/registration/processor/bio/dedupe/service/test/BioDedupeServiceImplTest.java
@@ -0,0 +1,11 @@
+package io.mosip.registration.processor.bio.dedupe.service.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RefreshScope
+@RunWith(SpringRunner.class)
+public class BioDedupeServiceImplTest {
+
+}
