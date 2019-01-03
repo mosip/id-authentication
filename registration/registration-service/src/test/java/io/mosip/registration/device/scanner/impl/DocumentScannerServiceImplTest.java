3ff4c90b2a5242903abb1b06fa04279d5b7f448c
M1046540 <M1046540@b2ml26342.mindtree.com>
2019-01-03 15:18:14 +0530

    MOS-11612 - timeout added for scanner in test case
    Former-commit-id: 0cf5d63e906e7edfab679fc39ae05be88bb49a0d
    Former-commit-id: 91c8b9749e23e688b6872876ae6f3459fc418f98

--- registration/registration-service/src/test/java/io/mosip/registration/device/scanner/impl/DocumentScannerServiceImplTest.java
+++ registration/registration-service/src/test/java/io/mosip/registration/device/scanner/impl/DocumentScannerServiceImplTest.java
@@ -88,6 +88,8 @@ public class DocumentScannerServiceImplTest {
 		ReflectionTestUtils.setField(documentScannerServiceImpl, "scannerhost", "192.168.43.253");
 		ReflectionTestUtils.setField(documentScannerServiceImpl, "scannerPort", 6566);
 		ReflectionTestUtils.setField(documentScannerServiceImpl, "scannerImgType", "jpg");
+		ReflectionTestUtils.setField(documentScannerServiceImpl, "scannerTimeout", 2000);
+		
 	}
 
 }
