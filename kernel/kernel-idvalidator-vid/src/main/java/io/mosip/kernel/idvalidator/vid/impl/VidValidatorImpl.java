a6f0719e439c260c9a8c573cd6254b5978bd80c3
M1047717 <M1047717@A2ML30846.mindtree.com>
2019-01-03 11:20:05 +0530

    MOS-1070 added fileter validation for the VID validator
    Former-commit-id: 2e03f7f4e454159aefe484f4d101087a540eb1cc
    Former-commit-id: 9a3b702281a85362b450f4a28885c5cec9442ae9

--- kernel/kernel-idvalidator-vid/src/main/java/io/mosip/kernel/idvalidator/vid/impl/VidValidatorImpl.java
+++ kernel/kernel-idvalidator-vid/src/main/java/io/mosip/kernel/idvalidator/vid/impl/VidValidatorImpl.java
@@ -8,7 +8,6 @@ import java.util.stream.IntStream;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -18,7 +17,6 @@ import io.mosip.kernel.core.util.ChecksumUtils;
 
 import io.mosip.kernel.core.util.StringUtils;
 import io.mosip.kernel.idvalidator.vid.constant.VidExceptionConstant;
-import io.mosip.kernel.idvalidator.vid.util.VidFilterUtils;
 
 /**
  * Class to validate the VID
@@ -29,10 +27,7 @@ import io.mosip.kernel.idvalidator.vid.util.VidFilterUtils;
  */
 @Component
 public class VidValidatorImpl implements IdValidator<String> {
-	
-	@Autowired
-	VidFilterUtils vidFilterUtils;
-	
+
 	@Value("${mosip.kernel.vid.length}")
 	private int vidLength;
 
@@ -40,51 +35,51 @@ public class VidValidatorImpl implements IdValidator<String> {
 	 * This Field to hold regular expressions for checking UIN has only digits.
 	 */
 	private String numaricRegEx;
-	
-	//---------------------
-    /**
-    * Upper bound of number of digits in sequence allowed in id. For example if
-    * limit is 3, then 12 is allowed but 123 is not allowed in id (in both
-    * ascending and descending order)
-    */
-    @Value("${mosip.kernel.vid.length.sequence-limit}")
-    private int sequenceLimit;
 
-    /**
-    * Number of digits in repeating block allowed in id. For example if limit is 2,
-    * then 4xxx4 is allowed but 48xxx48 is not allowed in id (x is any digit)
-    */
-    @Value("${mosip.kernel.vid.length.repeating-block-limit}")
-    private int repeatingBlockLimit;
+	// ---------------------
+	/**
+	 * Upper bound of number of digits in sequence allowed in id. For example if
+	 * limit is 3, then 12 is allowed but 123 is not allowed in id (in both
+	 * ascending and descending order)
+	 */
+	@Value("${mosip.kernel.vid.length.sequence-limit}")
+	private int sequenceLimit;
 
-    /**
-    * Lower bound of number of digits allowed in between two repeating digits in
-    * id. For example if limit is 2, then 11 and 1x1 is not allowed in id (x is any
-    * digit)
-    */
-    @Value("${mosip.kernel.vid.length.repeating-limit}")
-    private int repeatingLimit;
+	/**
+	 * Number of digits in repeating block allowed in id. For example if limit is 2,
+	 * then 4xxx4 is allowed but 48xxx48 is not allowed in id (x is any digit)
+	 */
+	@Value("${mosip.kernel.vid.length.repeating-block-limit}")
+	private int repeatingBlockLimit;
 
-    /**
-    * Ascending digits which will be checked for sequence in id
-    */
-    private static final String SEQ_ASC = "0123456789";
+	/**
+	 * Lower bound of number of digits allowed in between two repeating digits in
+	 * id. For example if limit is 2, then 11 and 1x1 is not allowed in id (x is any
+	 * digit)
+	 */
+	@Value("${mosip.kernel.vid.length.repeating-limit}")
+	private int repeatingLimit;
 
-    /**
-    * Descending digits which will be checked for sequence in id
-    */
-    private static final String SEQ_DEC = "9876543210";
+	/**
+	 * Ascending digits which will be checked for sequence in id
+	 */
+	private static final String SEQ_ASC = "0123456789";
 
-    /**
-    * Compiled regex pattern of {@link #repeatingRegEx}
-    */
-    private Pattern repeatingPattern = null;
+	/**
+	 * Descending digits which will be checked for sequence in id
+	 */
+	private static final String SEQ_DEC = "9876543210";
 
-    /**
-    * Compiled regex pattern of {@link #repeatingBlockRegEx}
-    */
-    private Pattern repeatingBlockPattern = null;
-    //------------------------------------------
+	/**
+	 * Compiled regex pattern of {@link #repeatingRegEx}
+	 */
+	private Pattern repeatingPattern = null;
+
+	/**
+	 * Compiled regex pattern of {@link #repeatingBlockRegEx}
+	 */
+	private Pattern repeatingBlockPattern = null;
+	// ------------------------------------------
 
 	/**
 	 * Method to prepare regular expressions for checking UIN has only digits.
@@ -92,37 +87,37 @@ public class VidValidatorImpl implements IdValidator<String> {
 	@PostConstruct
 	private void VidValidatorImplPostConstruct() {
 		numaricRegEx = "\\d{" + vidLength + "}";
-		 /**
-         * Regex for matching repeating digits like 11, 1x1, 1xx1, 1xxx1, etc.<br/>
-         * If repeating digit limit is 2, then <b>Regex:</b> (\d)\d{0,2}\1<br/>
-         * <b>Explanation:</b><br/>
-         * <b>1st Capturing Group (\d)</b><br/>
-         * <b>\d</b> matches a digit (equal to [0-9])<br/>
-         * <b>{0,2} Quantifier</b> — Matches between 0 and 2 times, as many times as
-         * possible, giving back as needed (greedy)<br/>
-         * <b>\1</b> matches the same text as most recently matched by the 1st capturing
-         * group<br/>
-         */
-         String repeatingRegEx = "(\\d)\\d{0," + (repeatingLimit - 1) + "}\\1";
-         /**
-         * Regex for matching repeating block of digits like 482xx482, 4827xx4827 (x is
-         * any digit).<br/>
-         * If repeating block limit is 3, then <b>Regex:</b> (\d{3,}).*?\1<br/>
-         * <b>Explanation:</b><br/>
-         * <b>1st Capturing Group (\d{3,})</b><br/>
-         * <b>\d{3,}</b> matches a digit (equal to [0-9])<br/>
-         * <b>{3,}</b> Quantifier — Matches between 3 and unlimited times, as many times
-         * as possible, giving back as needed (greedy)<br/>
-         * <b>.*?</b> matches any character (except for line terminators)<br/>
-         * <b>*?</b> Quantifier — Matches between zero and unlimited times, as few times
-         * as possible, expanding as needed (lazy)<br/>
-         * <b>\1</b> matches the same text as most recently matched by the 1st capturing
-         * group<br/>
-         */
-         String repeatingBlockRegEx = "(\\d{" + repeatingBlockLimit + ",}).*?\\1";
+		/**
+		 * Regex for matching repeating digits like 11, 1x1, 1xx1, 1xxx1, etc.<br/>
+		 * If repeating digit limit is 2, then <b>Regex:</b> (\d)\d{0,2}\1<br/>
+		 * <b>Explanation:</b><br/>
+		 * <b>1st Capturing Group (\d)</b><br/>
+		 * <b>\d</b> matches a digit (equal to [0-9])<br/>
+		 * <b>{0,2} Quantifier</b> — Matches between 0 and 2 times, as many times as
+		 * possible, giving back as needed (greedy)<br/>
+		 * <b>\1</b> matches the same text as most recently matched by the 1st capturing
+		 * group<br/>
+		 */
+		String repeatingRegEx = "(\\d)\\d{0," + (repeatingLimit - 1) + "}\\1";
+		/**
+		 * Regex for matching repeating block of digits like 482xx482, 4827xx4827 (x is
+		 * any digit).<br/>
+		 * If repeating block limit is 3, then <b>Regex:</b> (\d{3,}).*?\1<br/>
+		 * <b>Explanation:</b><br/>
+		 * <b>1st Capturing Group (\d{3,})</b><br/>
+		 * <b>\d{3,}</b> matches a digit (equal to [0-9])<br/>
+		 * <b>{3,}</b> Quantifier — Matches between 3 and unlimited times, as many times
+		 * as possible, giving back as needed (greedy)<br/>
+		 * <b>.*?</b> matches any character (except for line terminators)<br/>
+		 * <b>*?</b> Quantifier — Matches between zero and unlimited times, as few times
+		 * as possible, expanding as needed (lazy)<br/>
+		 * <b>\1</b> matches the same text as most recently matched by the 1st capturing
+		 * group<br/>
+		 */
+		String repeatingBlockRegEx = "(\\d{" + repeatingBlockLimit + ",}).*?\\1";
 
-         repeatingPattern = Pattern.compile(repeatingRegEx);
-         repeatingBlockPattern = Pattern.compile(repeatingBlockRegEx);
+		repeatingPattern = Pattern.compile(repeatingRegEx);
+		repeatingBlockPattern = Pattern.compile(repeatingBlockRegEx);
 
 	}
 
@@ -217,9 +212,8 @@ public class VidValidatorImpl implements IdValidator<String> {
 		 * The VID should not have repeated block of numbers for more than 2 digits
 		 * 
 		 */
-		if (!vidFilterUtils.isValidId(id)) {
-			throw new InvalidIDException(
-					VidExceptionConstant.VID_VAL_ILLEGAL_SEQUENCE_REPEATATIVE.getErrorCode(),
+		if (!isValidId(id)) {
+			throw new InvalidIDException(VidExceptionConstant.VID_VAL_ILLEGAL_SEQUENCE_REPEATATIVE.getErrorCode(),
 					VidExceptionConstant.VID_VAL_ILLEGAL_SEQUENCE_REPEATATIVE.getErrorMessage());
 		}
 
@@ -243,47 +237,47 @@ public class VidValidatorImpl implements IdValidator<String> {
 		 */
 		return true;
 	}
-	
-	//--------------------------------------------------------------
-	 /**
-	    * Checks if the input id is valid by passing the id through
-	    * {@link #sequenceLimit} filter, {@link #repeatingLimit} filter and
-	    * {@link #repeatingBlockLimit} filters
-	    * 
-	     * @param id
-	    *            The input id to validate
-	    * @return true if the input id is valid
-	    */
-	    public boolean isValidId(String id) {
 
-	                   return !(sequenceFilter(id) || regexFilter(id, repeatingPattern) || regexFilter(id, repeatingBlockPattern));
-	    }
+	// --------------------------------------------------------------
+	/**
+	 * Checks if the input id is valid by passing the id through
+	 * {@link #sequenceLimit} filter, {@link #repeatingLimit} filter and
+	 * {@link #repeatingBlockLimit} filters
+	 * 
+	 * @param id
+	 *            The input id to validate
+	 * @return true if the input id is valid
+	 */
+	private boolean isValidId(String id) {
 
-	    /**
-	    * Checks the input id for {@link #SEQUENCE_LIMIT} filter
-	    * 
-	     * @param id
-	    *            The input id to validate
-	    * @return true if the id matches the filter
-	    */
-	    private boolean sequenceFilter(String id) {
-	                   return IntStream.rangeClosed(0, id.length() - sequenceLimit).parallel()
-	                                                .mapToObj(index -> id.subSequence(index, index + sequenceLimit))
-	                                                .anyMatch(idSubSequence -> SEQ_ASC.contains(idSubSequence) || SEQ_DEC.contains(idSubSequence));
-	    }
+		return !(sequenceFilter(id) || regexFilter(id, repeatingPattern) || regexFilter(id, repeatingBlockPattern));
+	}
 
-	    /**
-	    * Checks the input id if it matched the given regex pattern
-	    * ({@link #repeatingPattern}, {@link #repeatingBlockPattern})
-	    * 
-	     * @param id
-	    *            The input id to validate
-	    * @param pattern
-	    *            The input regex Pattern
-	    * @return true if the id matches the given regex pattern
-	    */
-	    private static boolean regexFilter(String id, Pattern pattern) {
-	                   return pattern.matcher(id).find();
-	    }
+	/**
+	 * Checks the input id for {@link #SEQUENCE_LIMIT} filter
+	 * 
+	 * @param id
+	 *            The input id to validate
+	 * @return true if the id matches the filter
+	 */
+	private boolean sequenceFilter(String id) {
+		return IntStream.rangeClosed(0, id.length() - sequenceLimit).parallel()
+				.mapToObj(index -> id.subSequence(index, index + sequenceLimit))
+				.anyMatch(idSubSequence -> SEQ_ASC.contains(idSubSequence) || SEQ_DEC.contains(idSubSequence));
+	}
+
+	/**
+	 * Checks the input id if it matched the given regex pattern
+	 * ({@link #repeatingPattern}, {@link #repeatingBlockPattern})
+	 * 
+	 * @param id
+	 *            The input id to validate
+	 * @param pattern
+	 *            The input regex Pattern
+	 * @return true if the id matches the given regex pattern
+	 */
+	private boolean regexFilter(String id, Pattern pattern) {
+		return pattern.matcher(id).find();
+	}
 
 }
