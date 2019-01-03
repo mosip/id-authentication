2836ed84d5b5079a2e87f58382b9f0f69f9fbee6
M1048860 <M1048860@mindtree.com>
2019-01-03 13:36:52 +0530

    MOS - 11897 Controller Created and exposed in swagger.bootstrapProperties set. swagger configured.
    Former-commit-id: 49b950ff90f1d94fe854ecb403443b2dd145454e
    Former-commit-id: 8cd7532a460874783f840549990ebc4d98245975

--- registration-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/controller/AbisController.java
+++ registration-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/controller/AbisController.java
@@ -1,5 +1,6 @@
 package io.mosip.registration.processor.abis.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,19 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.registration.processor.abis.dto.AbisInsertRequestDto;
 import io.mosip.registration.processor.abis.dto.AbisInsertResponceDto;
-import io.mosip.registration.processor.status.code.RegistrationStatusCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+@RefreshScope
 @RestController
 @RequestMapping("/v0.1/registration-processor/abis")
 @Api(tags = "Abis")
 public class AbisController {
 
-	@PostMapping(path = "/insert", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(value = "insert biometric data of an Individual", response = RegistrationStatusCode.class)
+	@PostMapping(path = "/insert", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "insert biometric data of an Individual", response = AbisInsertResponceDto.class)
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Biometric data inserted successfully"),
 			@ApiResponse(code = 400, message = "Uable to insert biometric data") })
 	public ResponseEntity<AbisInsertResponceDto> insert(@RequestBody(required = true) AbisInsertRequestDto abisInsertRequestDto) {
