d4dae0986d8b785fdfc6b15b2edb77f6cdca68b7
Pranav Kumar <pranav.kumar5@mindtree.com>
2019-01-07 12:26:50 +0530

    MOS-12568:added routes to config server
    Former-commit-id: 2ce4c6ea4da2d57fa113bd1e0add38c45d8ae2ea
    Former-commit-id: d5cd78b55ae25ff777d2adfb26539c5336c42105

--- registration-processor/registration-processor-camel-bridge/src/main/java/io/mosip/registration/processor/camel/bridge/MosipCamelBridge.java
+++ registration-processor/registration-processor-camel-bridge/src/main/java/io/mosip/registration/processor/camel/bridge/MosipCamelBridge.java
@@ -5,8 +5,14 @@ import java.io.InputStream;
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.RoutesDefinition;
+import org.apache.catalina.WebResourceRoot.ResourceSetType;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.registration.processor.camel.bridge.util.PropertyFileUtil;
 import io.mosip.registration.processor.core.abstractverticle.MessageBusAddress;
 import io.mosip.registration.processor.core.logger.RegProcessorLogger;
 import io.vertx.camel.CamelBridge;
@@ -49,8 +55,11 @@ public class MosipCamelBridge extends AbstractVerticle {
 						message.body().toString()));
 
 		CamelContext camelContext = new DefaultCamelContext();
-		InputStream is = MosipCamelBridge.class.getClassLoader().getResourceAsStream("camel-routes.xml");
-		RoutesDefinition routes = camelContext.loadRoutesDefinition(is);
+		RestTemplate restTemplate = new RestTemplate();
+		String url = PropertyFileUtil.getProperty(MosipCamelBridge.class, "bootstrap.properties",
+				"camel.routes.configuration");
+		ResponseEntity<Resource> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, Resource.class);
+		RoutesDefinition routes = camelContext.loadRoutesDefinition(responseEntity.getBody().getInputStream());
 		camelContext.addRouteDefinitions(routes.getRoutes());
 		camelContext.start();
 
