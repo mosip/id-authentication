d496aea57f5512532cb77fc390a969c76e239dfd
M1039285 <M1039285@a2ml16222.mindtree.com>
2019-01-08 15:05:27 +0530

    Added Java Doc MOS-1095
    Former-commit-id: e79c0238f4219205fbdc073e179562d6df442074
    Former-commit-id: 9cfed9826a060a1f5a2c25a26ad9cb03e696942a

--- registration-processor/packet-bio-dedupe-service/src/main/java/io/mosip/registration/processor/bio/dedupe/exception/ABISInternalError.java
+++ registration-processor/packet-bio-dedupe-service/src/main/java/io/mosip/registration/processor/bio/dedupe/exception/ABISInternalError.java
@@ -3,21 +3,39 @@ package io.mosip.registration.processor.bio.dedupe.exception;
 import io.mosip.kernel.core.exception.BaseUncheckedException;
 import io.mosip.registration.processor.core.exception.util.PlatformErrorMessages;
 
+/**
+ * The Class ABISInternalError.
+ */
 public class ABISInternalError extends BaseUncheckedException {
 
-	/**
-	 * 
-	 */
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Instantiates a new ABIS internal error.
+	 */
 	public ABISInternalError() {
 		super();
 	}
 
+	/**
+	 * Instantiates a new ABIS internal error.
+	 *
+	 * @param errorMessage
+	 *            the error message
+	 */
 	public ABISInternalError(String errorMessage) {
 		super(PlatformErrorMessages.RPR_BDD_ABIS_INTERNAL_ERROR.getCode() + EMPTY_SPACE, errorMessage);
 	}
 
+	/**
+	 * Instantiates a new ABIS internal error.
+	 *
+	 * @param message
+	 *            the message
+	 * @param cause
+	 *            the cause
+	 */
 	public ABISInternalError(String message, Throwable cause) {
 		super(PlatformErrorMessages.RPR_BDD_ABIS_INTERNAL_ERROR.getCode() + EMPTY_SPACE, message, cause);
 	}
