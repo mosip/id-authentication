b5c0a7df5d91eac0d8243230a8be1f26515046d5
M1039285 <M1039285@a2ml16222.mindtree.com>
2019-01-08 15:10:15 +0530

    Added Javadoc MOS-1095
    Former-commit-id: b11d909577019d4199ae5f1b8ea7921127619e7a
    Former-commit-id: 1d18fbe4bca755098c6cd144e0fda4f9945c4386

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/spi/biodedupe/BioDedupeService.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/spi/biodedupe/BioDedupeService.java
@@ -4,12 +4,40 @@ import java.util.List;
 
 import io.mosip.registration.processor.core.exception.ApisResourceAccessException;
 
+/**
+ * The Interface BioDedupeService.
+ */
 public interface BioDedupeService {
 
+	/**
+	 * Insert biometrics.
+	 *
+	 * @param registrationId
+	 *            the registration id
+	 * @return the string
+	 * @throws ApisResourceAccessException
+	 *             the apis resource access exception
+	 */
 	public String insertBiometrics(String registrationId) throws ApisResourceAccessException;
 
+	/**
+	 * Perform dedupe.
+	 *
+	 * @param registrationId
+	 *            the registration id
+	 * @return the list
+	 * @throws ApisResourceAccessException
+	 *             the apis resource access exception
+	 */
 	public List<String> performDedupe(String registrationId) throws ApisResourceAccessException;
 
+	/**
+	 * Gets the file.
+	 *
+	 * @param registrationId
+	 *            the registration id
+	 * @return the file
+	 */
 	public byte[] getFile(String registrationId);
 
 }
