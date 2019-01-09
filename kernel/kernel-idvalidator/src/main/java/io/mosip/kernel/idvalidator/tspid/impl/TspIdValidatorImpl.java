fb6e79d1e74ffa16107ffee41956f025d89b4e92
M1044345 <M1044345@A2ML26966.mindtree.com>
2019-01-09 10:21:07 +0530

    MOS-6606 tspid validator methods added
    Former-commit-id: f1243528ab8a298971b3d45eb2f8291b56c99390
    Former-commit-id: ff5ab5fb9a1049dba740bb4ce83e314bc3ac0368

---
+++ kernel/kernel-idvalidator/src/main/java/io/mosip/kernel/idvalidator/tspid/impl/TspIdValidatorImpl.java
@@ -0,0 +1,43 @@
+package io.mosip.kernel.idvalidator.tspid.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.mosip.kernel.core.idvalidator.exception.InvalidIDException;
+import io.mosip.kernel.core.idvalidator.spi.IdValidator;
+import io.mosip.kernel.idvalidator.tspid.constant.TspIdExceptionProperty;
+
+/**
+ * This class validate TSPID given in string format.
+ * 
+ * @author Ritesh Sinha
+ * @since 1.0.0
+ *
+ */
+@Component
+public class TspIdValidatorImpl implements IdValidator<String> {
+
+	/**
+	 * Length of TspId.
+	 */
+	@Value("${mosip.kernel.tspid.length}")
+	private int tspidLength;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * io.mosip.kernel.core.idvalidator.spi.IdValidator#validateId(java.lang.Object)
+	 */
+	@Override
+	public boolean validateId(String id) {
+
+		if (id.length() != tspidLength) {
+			throw new InvalidIDException(TspIdExceptionProperty.INVALID_TSPID_LENGTH.getErrorCode(),
+					TspIdExceptionProperty.INVALID_TSPID_LENGTH.getErrorMessage() + tspidLength);
+		}
+
+		return true;
+	}
+
+}
