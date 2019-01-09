28075fa7ab0c5a080842e37569d77849a960c23d
Nasir Khan <M1001063@mindtree.com>
2019-01-09 10:06:21 +0530

    modified the uin biometric table to biometric file type to capture multiple biometric for an UIN
    Former-commit-id: 4a726d4a7a5a84b231a1db006bfb1be50a78c73e
    Former-commit-id: 194efa411f45c1f224c62b0eee223697aafa0b71

--- database-scripts/mosip_idrepo/ddl/idrepo-uin_biometric.sql
+++ database-scripts/mosip_idrepo/ddl/idrepo-uin_biometric.sql
@@ -2,12 +2,10 @@
 -- DROP TABLE IF EXISTS idrepo.uin_biometric CASCADE;
 CREATE TABLE idrepo.uin_biometric(
 	uin_ref_id character varying(36) NOT NULL,
+	biometric_file_type character varying(36) NOT NULL,
 	bio_file_id character varying(128) NOT NULL,
 	biometric_file_name character varying(128),
 	biometric_file_hash character varying(64) NOT NULL,
-	parent_guardian_bio_file_id character varying(128),
-	parent_guardian_bio_file_name character varying(128),
-	parent_guardian_bio_file_hash character varying(64),
 	lang_code character varying(3) NOT NULL,
 	cr_by character varying(32) NOT NULL,
 	cr_dtimes timestamp NOT NULL,
@@ -15,24 +13,18 @@ CREATE TABLE idrepo.uin_biometric(
 	upd_dtimes timestamp,
 	is_deleted boolean,
 	del_dtimes timestamp,
-	CONSTRAINT uk_uinb UNIQUE (bio_file_id),
-	CONSTRAINT pk_uinb PRIMARY KEY (uin_ref_id)
-
+	CONSTRAINT pk_uinb PRIMARY KEY (uin_ref_id,biometric_file_type)
 );
 -- ddl-end --
 COMMENT ON TABLE idrepo.uin_biometric IS 'Table to store biometric reference and hash value of the biometric file. ';
+-- ddl-end --
+COMMENT ON COLUMN idrepo.uin_biometric.biometric_file_type IS 'Biometric file type defines the type of file being stored. for example: Individual''s biometric, parent / guardian''s biometric or introducer / supervisor''s biometric.';
 -- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric.bio_file_id IS 'file id of the biometric cbeff file of an individual';
 -- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric.biometric_file_name IS 'Name of the biometric cbeff file';
 -- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric.biometric_file_hash IS 'Hash value of the biometric dbeff file';
--- ddl-end --
-COMMENT ON COLUMN idrepo.uin_biometric.parent_guardian_bio_file_id IS 'file id of the biometric cbeff file of individual''s parent / guardian';
--- ddl-end --
-COMMENT ON COLUMN idrepo.uin_biometric.parent_guardian_bio_file_name IS 'Name of the biometric cbeff file of the parent / guardian';
--- ddl-end --
-COMMENT ON COLUMN idrepo.uin_biometric.parent_guardian_bio_file_hash IS 'Hash value of the biometric dbeff file of the parent / guardian';
 -- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric.lang_code IS 'Language code of the biometic file name.';
 -- ddl-end --
@@ -49,6 +41,11 @@ COMMENT ON COLUMN idrepo.uin_biometric.is_deleted IS 'Field to indicate whether 
 COMMENT ON COLUMN idrepo.uin_biometric.del_dtimes IS 'Record deleted datetime';
 -- ddl-end --
 ALTER TABLE idrepo.uin_biometric OWNER TO appadmin;
+-- ddl-end --
+
+-- object: uk_uinb | type: CONSTRAINT --
+-- ALTER TABLE idrepo.uin_biometric DROP CONSTRAINT IF EXISTS uk_uinb CASCADE;
+ALTER TABLE idrepo.uin_biometric ADD CONSTRAINT uk_uinb UNIQUE (uin_ref_id,bio_file_id);
 -- ddl-end --
 
 
--- database-scripts/mosip_idrepo/ddl/idrepo-uin_biometric_h.sql
+++ database-scripts/mosip_idrepo/ddl/idrepo-uin_biometric_h.sql
@@ -2,13 +2,11 @@
 -- DROP TABLE IF EXISTS idrepo.uin_biometric_h CASCADE;
 CREATE TABLE idrepo.uin_biometric_h(
 	uin_ref_id character varying(36) NOT NULL,
+	biometric_file_type character varying(36) NOT NULL,
 	eff_dtimes timestamp NOT NULL,
 	bio_file_id character varying(128) NOT NULL,
 	biometric_file_name character varying(128) NOT NULL,
 	biometric_file_hash character varying(64) NOT NULL,
-	parent_guardian_bio_file_id character varying(128),
-	parent_guardian_bio_file_name character varying(128),
-	parent_guardian_bio_file_hash character varying(64),
 	lang_code character varying(3) NOT NULL,
 	cr_by character varying(32) NOT NULL,
 	cr_dtimes timestamp NOT NULL,
@@ -16,8 +14,8 @@ CREATE TABLE idrepo.uin_biometric_h(
 	upd_dtimes timestamp,
 	is_deleted boolean,
 	del_dtimes timestamp,
-	CONSTRAINT pk_uinbh PRIMARY KEY (uin_ref_id,eff_dtimes),
-	CONSTRAINT uk_uinbh UNIQUE (bio_file_id,eff_dtimes)
+	CONSTRAINT pk_uinbh PRIMARY KEY (uin_ref_id,biometric_file_type,eff_dtimes),
+	CONSTRAINT uk_uinbh UNIQUE (uin_ref_id,bio_file_id,eff_dtimes)
 
 );
 -- ddl-end --
@@ -25,17 +23,13 @@ COMMENT ON TABLE idrepo.uin_biometric_h IS 'Table to store history of biometric 
 -- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric_h.uin_ref_id IS 'reference id of an uin, refers to uin_ref_id of idrep.uin table';
 -- ddl-end --
+COMMENT ON COLUMN idrepo.uin_biometric_h.biometric_file_type IS 'Biometric file type defines the type of file being stored. for example: Individual''s biometric, parent / guardian''s biometric or introducer / supervisor''s biometric.';
+-- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric_h.bio_file_id IS 'file id of the biometric cbeff file of an individual';
 -- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric_h.biometric_file_name IS 'Name of the biometric cbeff file';
 -- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric_h.biometric_file_hash IS 'Hash value of the biometric dbeff file';
--- ddl-end --
-COMMENT ON COLUMN idrepo.uin_biometric_h.parent_guardian_bio_file_id IS 'file id of the biometric cbeff file of individual''s parent / guardian';
--- ddl-end --
-COMMENT ON COLUMN idrepo.uin_biometric_h.parent_guardian_bio_file_name IS 'Name of the biometric cbeff file of parent / guardian';
--- ddl-end --
-COMMENT ON COLUMN idrepo.uin_biometric_h.parent_guardian_bio_file_hash IS 'Hash value of the biometric dbeff file of parent / guardian';
 -- ddl-end --
 COMMENT ON COLUMN idrepo.uin_biometric_h.lang_code IS 'Language code of the biometic file name.';
 -- ddl-end --
