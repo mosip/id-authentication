9c47dba5900db5404a52018afa9ac3f345b48e56
Kishan Rathore <M1043008@mindtree.com>
2019-01-09 17:44:26 +0530

    pregistration core changes
    Former-commit-id: dcafe02d1031dd9822b1eef1588d7dcf4cfc25a7
    Former-commit-id: 4b6acc8e102a1ebeb9bb61a87165049325972550

--- pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/exception/TableNotAccessibleException.java
+++ pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/exception/TableNotAccessibleException.java
@@ -7,27 +7,27 @@ import io.mosip.preregistration.core.exception.util.PreIssuanceExceptionCodes;
  * TablenotAccessibleException occurs when system is not able to access registration table.
  *
  */
-public class TablenotAccessibleException extends BaseUncheckedException {
+public class TableNotAccessibleException extends BaseUncheckedException {
 
 	private static final long serialVersionUID = 1L;
 
-	public TablenotAccessibleException() {
+	public TableNotAccessibleException() {
 		super();
 	}
 
-	public TablenotAccessibleException(String message) {
+	public TableNotAccessibleException(String message) {
 		super(PreIssuanceExceptionCodes.TABLE_NOT_FOUND_EXCEPTION, message);
 	}
 
-	public TablenotAccessibleException(String message, Throwable cause) {
+	public TableNotAccessibleException(String message, Throwable cause) {
 		super(PreIssuanceExceptionCodes.TABLE_NOT_FOUND_EXCEPTION+ EMPTY_SPACE, message, cause);
 	}
 	
-	public TablenotAccessibleException(String errorCode,String message) {
+	public TableNotAccessibleException(String errorCode,String message) {
 		super(errorCode, message);
 	}
 	
-	public TablenotAccessibleException(String errorCode,String message, Throwable cause) {
+	public TableNotAccessibleException(String errorCode,String message, Throwable cause) {
 		super(errorCode, message, cause);
 	}
 }
