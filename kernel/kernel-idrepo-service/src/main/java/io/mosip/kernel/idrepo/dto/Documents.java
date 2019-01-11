f3fe7ef3ff310aa04c6a480a70d7b5f403ace7bc
Manoj SP <Manoj.SP@mindtree.com>
2019-01-11 12:09:51 +0530

    resolved conflicts
    Former-commit-id: 534493e3f5336000027c793e1c47e2a5e57cf8e9
    Former-commit-id: d34aab9dd1e12e9e44cbfbcaf58e74feb3a86f48

--- kernel/kernel-idrepo-service/src/main/java/io/mosip/kernel/idrepo/dto/Documents.java
+++ kernel/kernel-idrepo-service/src/main/java/io/mosip/kernel/idrepo/dto/Documents.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package io.mosip.kernel.idrepo.dto;
 
 import lombok.AllArgsConstructor;
@@ -25,31 +24,3 @@ public class Documents {
 	/** The doc value. */
 	private String value; 
 }
-=======
-package io.mosip.kernel.idrepo.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-/**
- * The Class Documents.
- *
- * @author Manoj SP
- */
-
-/**
- * Instantiates a new documents.
- */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Documents {
-
-	/** The doc type. */
-	private String docType;
-	
-	/** The doc value. */
-	private String docValue; 
-}
->>>>>>> refs/remotes/origin/DEV
