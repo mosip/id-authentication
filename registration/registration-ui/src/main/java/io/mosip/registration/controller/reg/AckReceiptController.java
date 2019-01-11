ffd474f602dfd2fb8379186ea3f66fa0b85035f9
M1044292 <M1044292@b2ml27210.mindtree.com>
2019-01-11 15:48:22 +0530

    MOS-12175 : Incorporated Review Comments and changes in AckReceiptController
    Former-commit-id: b599aef63cabd8f8714529c1d4469de28a1dd902
    Former-commit-id: 2e2b8af259547d14e369e287bf584d7c5623c8b2

--- registration/registration-ui/src/main/java/io/mosip/registration/controller/reg/AckReceiptController.java
+++ registration/registration-ui/src/main/java/io/mosip/registration/controller/reg/AckReceiptController.java
@@ -48,6 +48,7 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.print.PrinterJob;
 import javafx.scene.control.Button;
 import javafx.scene.image.WritableImage;
 import javafx.scene.text.Text;
@@ -68,11 +69,13 @@ public class AckReceiptController extends BaseController implements Initializabl
 
 	@Autowired
 	private PacketHandlerController packetController;
+
 	@Autowired
 	private PacketHandlerService packetHandlerService;
 
 	@Autowired
 	private TemplateService templateService;
+
 	@Autowired
 	private NotificationService notificationService;
 
@@ -122,12 +125,36 @@ public class AckReceiptController extends BaseController implements Initializabl
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		try {
+		LOGGER.debug("REGISTRATION - UI - ACKRECEIPTCONTROLLER", APPLICATION_NAME, APPLICATION_ID,
+				"Page loading has been started");
+		
+		if (getRegistrationData().getSelectionListDTO() != null) {
+			registrationNavLabel.setText(RegistrationConstants.UIN_NAV_LABEL);
+			newRegistration.setVisible(false);
+		}
 
-			if (getRegistrationData().getSelectionListDTO() != null) {
-				registrationNavLabel.setText(RegistrationConstants.UIN_NAV_LABEL);
-				newRegistration.setVisible(false);
-			}
+		WebEngine engine = webView.getEngine();
+		// loads the generated HTML template content into webview
+		engine.loadContent(stringWriter.toString());
+		LOGGER.debug("REGISTRATION - UI - ACKRECEIPTCONTROLLER", APPLICATION_NAME, APPLICATION_ID,
+				"Acknowledgement template has been loaded to webview");
+		
+		// pauses the view for 3 seconds so that the webview will be loaded with the
+		// content and calls the method to create packet after 3 seconds
+		PauseTransition pause = new PauseTransition(Duration.seconds(3));
+		pause.setOnFinished(e -> saveRegistrationData());
+		pause.play();
+	}
+
+	/**
+	 * To generate email and SMS notification to the user after successful
+	 * registration
+	 */
+	private void generateEmailNotification() {
+		LOGGER.debug("REGISTRATION - UI - ACKRECEIPTCONTROLLER", RegistrationConstants.APPLICATION_NAME,
+				RegistrationConstants.APPLICATION_ID, "generating Email/SMS notification after packet creation");
+		
+		try {
 			// network availability check
 			if (RegistrationAppHealthCheckUtil.isNetworkAvailable()) {
 				// get the mode of communication
@@ -196,14 +223,17 @@ public class AckReceiptController extends BaseController implements Initializabl
 			LOGGER.error("REGISTRATION - ACK RECEIPT CONTROLLER ", APPLICATION_NAME, APPLICATION_ID,
 					regBaseUncheckedException.getMessage());
 		}
-		WebEngine engine = webView.getEngine();
-		engine.loadContent(stringWriter.toString());
-		PauseTransition pause = new PauseTransition(Duration.seconds(3));
-		pause.setOnFinished(e -> saveRegistrationData());
-		pause.play();
 	}
 
+	/**
+	 * To save the acknowledgement receipt along with the registration data and
+	 * create packet
+	 */
 	private void saveRegistrationData() {
+		LOGGER.debug("REGISTRATION - UI - ACKRECEIPTCONTROLLER", RegistrationConstants.APPLICATION_NAME,
+				RegistrationConstants.APPLICATION_ID, "packet creation has been started");
+		
+		// take a snapshot of the webview to save it along with the packet
 		WritableImage ackImage = webView.snapshot(null, null);
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		try {
@@ -221,10 +251,12 @@ public class AckReceiptController extends BaseController implements Initializabl
 					.setAcknowledgeReceiptName("RegistrationAcknowledgement." + RegistrationConstants.IMAGE_FORMAT);
 		}
 
+		// packet creation
 		ResponseDTO response = packetHandlerService.handle(registrationData);
+		
 		if (response.getSuccessResponseDTO() != null
 				&& response.getSuccessResponseDTO().getMessage().equals("Success")) {
-
+			generateEmailNotification();
 			try {
 				// Generate the file path for storing the Encrypted Packet and Acknowledgement
 				// Receipt
@@ -249,18 +281,13 @@ public class AckReceiptController extends BaseController implements Initializabl
 
 				Identity identity = registrationData.getDemographicDTO().getDemographicInfoDTO().getIdentity();
 				AddressDTO addressDTO = Builder.build(AddressDTO.class)
-						.with(address -> address
-								.setAddressLine1(identity.getAddressLine1().get(0).getValue()))
-						.with(address -> address
-								.setAddressLine2(identity.getAddressLine2().get(0).getValue()))
+						.with(address -> address.setAddressLine1(identity.getAddressLine1().get(0).getValue()))
+						.with(address -> address.setAddressLine2(identity.getAddressLine2().get(0).getValue()))
 						.with(address -> address.setLine3(identity.getAddressLine3().get(0).getValue()))
 						.with(address -> address.setLocationDTO(Builder.build(LocationDTO.class)
-								.with(location -> location
-										.setCity(identity.getCity().get(0).getValue()))
-								.with(location -> location
-										.setProvince(identity.getProvince().get(0).getValue()))
-								.with(location -> location
-										.setRegion(identity.getRegion().get(0).getValue()))
+								.with(location -> location.setCity(identity.getCity().get(0).getValue()))
+								.with(location -> location.setProvince(identity.getProvince().get(0).getValue()))
+								.with(location -> location.setRegion(identity.getRegion().get(0).getValue()))
 								.with(location -> location.setPostalCode(identity.getPostalCode())).get()))
 						.get();
 				Map<String, Object> addr = SessionContext.getInstance().getMapObject();
@@ -270,23 +297,36 @@ public class AckReceiptController extends BaseController implements Initializabl
 		}
 	}
 
+	/**
+	 * To print the acknowledgement receipt after packet creation when the user
+	 * clicks on print button.
+	 */
 	@FXML
-	public void saveReceipt(ActionEvent event) {
-		Button button = (Button) event.getSource();
-		/*
-		 * PrinterJob job = PrinterJob.createPrinterJob(); if (job != null) {
-		 * webView.getEngine().print(job); job.endJob(); }
-		 */
-		if (button.getId().equals(print.getId())) {
-			generateAlert(RegistrationConstants.SUCCESS_MSG, RegistrationUIConstants.PACKET_CREATED_SUCCESS);
-			goToHomePageFromRegistration();
+	public void printReceipt(ActionEvent event) {
+		LOGGER.debug("REGISTRATION - UI - ACKRECEIPTCONTROLLER", RegistrationConstants.APPLICATION_NAME,
+				RegistrationConstants.APPLICATION_ID, "Printing the Acknowledgement Receipt");
+		
+		PrinterJob job = PrinterJob.createPrinterJob();
+		if (job != null) {
+			webView.getEngine().print(job);
+			job.endJob();
 		}
-		if (button.getId().equals(newRegistration.getId())) {
-			clearRegistrationData();
-			packetController.createPacket();
-		}
+		generateAlert(RegistrationConstants.SUCCESS_MSG, RegistrationUIConstants.PACKET_CREATED_SUCCESS);
+		goToHomePageFromRegistration();
 	}
 
+	@FXML
+	public void goToNewRegistration(ActionEvent event) {
+		LOGGER.debug("REGISTRATION - UI - ACKRECEIPTCONTROLLER", RegistrationConstants.APPLICATION_NAME,
+				RegistrationConstants.APPLICATION_ID, "Going to New Registration Page after packet creation");
+		
+		clearRegistrationData();
+		packetController.createPacket();
+	}
+
+	/**
+	 * To generate alert if the email/sms notification is not sent
+	 */
 	private void generateNotificationAlert(String alertMessage) {
 		/* Generate Alert */
 		generateAlert(RegistrationConstants.ALERT_ERROR, alertMessage);
