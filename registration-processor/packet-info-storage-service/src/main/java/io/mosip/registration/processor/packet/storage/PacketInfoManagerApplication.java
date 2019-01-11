386c127662a3fa6e7fb3fb5f8a2a41ff44fe9243
rishabhjavad <Rishabh.Keshari@mindtree.com>
2019-01-11 12:35:13 +0530

    MOS-1067: PacketInfoManagerApplication has been commited
    Former-commit-id: 3f6d5731c7eecedd4c90acc4442b3144aceeb207
    Former-commit-id: 867d97c60b754c3c4ca66603c34754cc4be852c6

--- registration-processor/packet-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/PacketInfoManagerApplication.java
+++ registration-processor/packet-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/PacketInfoManagerApplication.java
@@ -1,23 +1,8 @@
 package io.mosip.registration.processor.packet.storage;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.mosip.registration.processor.core.packet.dto.FieldValue;
-import io.mosip.registration.processor.core.packet.dto.demographicinfo.IndividualDemographicDedupe;
-import io.mosip.registration.processor.core.packet.dto.demographicinfo.JsonValue;
 import io.mosip.registration.processor.packet.storage.service.impl.PacketInfoManagerImpl;
 
 /**
