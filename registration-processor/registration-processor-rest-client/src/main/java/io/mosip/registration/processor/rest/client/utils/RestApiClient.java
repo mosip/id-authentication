ef44c1cea917b79df8f385bd15ed6792bfe227b4
Pranav Kumar <pranav.kumar5@mindtree.com>
2019-01-17 17:05:07 +0530

    Modifier Rest-client
    Former-commit-id: 7089775d106b668634c171ae5ae71dfe7d1220be
    Former-commit-id: aa1e6b601ccfbc762abf095c0ac020784973a4dd

--- registration-processor/registration-processor-rest-client/src/main/java/io/mosip/registration/processor/rest/client/utils/RestApiClient.java
+++ registration-processor/registration-processor-rest-client/src/main/java/io/mosip/registration/processor/rest/client/utils/RestApiClient.java
@@ -1,28 +1,22 @@
 package io.mosip.registration.processor.rest.client.utils;
 
-import java.security.SecureRandom;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.scheme.SchemeSocketFactory;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.TrustStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -94,45 +88,23 @@ public class RestApiClient {
 		return result;
 	}
 
-	/**
-	 * Gets the rest template.
-	 *
-	 * @return the rest template
-	 * @throws Exception
-	 *             the exception
-	 */
-	public static RestTemplate getRestTemplate() throws Exception {
-		SSLContext sslContext = SSLContext.getInstance("SSL");
-		// set up a TrustManager that trusts everything
-		sslContext.init(null, new TrustManager[] { new X509TrustManager() {
-			public X509Certificate[] getAcceptedIssuers() {
+	public static RestTemplate getRestTemplate()
+			throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
 
-				return null;
-			}
+		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
 
-			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy)
+				.build();
 
-			}
+		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
 
-			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
 
-			}
-		} }, new SecureRandom());
-
-		SSLSocketFactory sf = new SSLSocketFactory(sslContext);
-
-		Scheme httpsScheme = new Scheme("https", 443, (SchemeSocketFactory) sf);
-		SchemeRegistry schemeRegistry = new SchemeRegistry();
-		schemeRegistry.register(httpsScheme);
-
-		// apache HttpClient version >4.2 should use BasicClientConnectionManager
-		ClientConnectionManager cm = new SingleClientConnManager(schemeRegistry);
-		HttpClient httpClient = new DefaultHttpClient(cm);
 		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
 
 		requestFactory.setHttpClient(httpClient);
-
 		return new RestTemplate(requestFactory);
+
 	}
 
 }
