f7c97f2f0862648f6f830650b2dabe6475ac5f56
Sadanandegowda Dm <M1045015@mindtree.com>
2019-01-18 14:26:34 +0530

    Added master data insert scripts
    Former-commit-id: a32b2ebaba231fba1bcdc3a77c69bdb34da0b2cb
    Former-commit-id: 4521d802a95570d5a16f2a69f9fff590b0ef7453

---
+++ database-scripts/mosip_master/dml/master-title.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO master.title(code,name,descr,lang_code,is_active,cr_by,cr_dtimes ) VALUES ('MIR','Mr','Male Title','eng',true,'superadmin',now())
+, ('MRS','Mrs','Female Title','eng',true,'superadmin',now())
+, ('MIS','Miss','Unmarried Female Title','eng',true,'superadmin',now())
+, ('MIR','أستاذ','العنوان الذكور','ara',true,'superadmin',now())
+, ('MRS','ست ','عنوان أنثى','ara',true,'superadmin',now())
+, ('MIS','آنسة ','العنوان الإناث غير المتزوجات','ara',true,'superadmin',now())
+, ('MIR','Monsieur','Titre masculin','fra',true,'superadmin',now())
+, ('MRS','Madame','Titre féminin','fra',true,'superadmin',now())
+, ('MIS','Mademoiselle','Titre de femme célibataire','fra',true,'superadmin',now());
