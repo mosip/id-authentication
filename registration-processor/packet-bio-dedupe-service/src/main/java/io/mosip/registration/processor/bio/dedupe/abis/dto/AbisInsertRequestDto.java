63021cddcf48b7273083cf83e9d60ed607444f4c
M1039285 <M1039285@a2ml16222.mindtree.com>
2019-01-21 16:29:25 +0530

    Fixed Test case in Service Impl and added Java DOC MOS-1095
    Former-commit-id: 35d74e906212797fa0143dcbd7d33c0b224b3d8d
    Former-commit-id: e95411fe53b0a3c7a2875257580348e46f42a2bc

--- registration-processor/packet-bio-dedupe-service/src/main/java/io/mosip/registration/processor/bio/dedupe/abis/dto/AbisInsertRequestDto.java
+++ registration-processor/packet-bio-dedupe-service/src/main/java/io/mosip/registration/processor/bio/dedupe/abis/dto/AbisInsertRequestDto.java
@@ -1,46 +1,139 @@
 package io.mosip.registration.processor.bio.dedupe.abis.dto;
 
+/**
+ * The Class AbisInsertRequestDto.
+ */
 public class AbisInsertRequestDto {
-private String id;
-private String ver;
-private String requestId;
-private String timestamp;
-private String referenceId;
-private String referenceURL;
-public String getId() {
-	return id;
-}
-public void setId(String id) {
-	this.id = id;
-}
-public String getVer() {
-	return ver;
-}
-public void setVer(String ver) {
-	this.ver = ver;
-}
-public String getRequestId() {
-	return requestId;
-}
-public void setRequestId(String requestId) {
-	this.requestId = requestId;
-}
-public String getTimestamp() {
-	return timestamp;
-}
-public void setTimestamp(String timestamp) {
-	this.timestamp = timestamp;
-}
-public String getReferenceId() {
-	return referenceId;
-}
-public void setReferenceId(String referenceId) {
-	this.referenceId = referenceId;
-}
-public String getReferenceURL() {
-	return referenceURL;
-}
-public void setReferenceURL(String referenceURL) {
-	this.referenceURL = referenceURL;
-}
+
+	/** The id. */
+	private String id;
+
+	/** The ver. */
+	private String ver;
+
+	/** The request id. */
+	private String requestId;
+
+	/** The timestamp. */
+	private String timestamp;
+
+	/** The reference id. */
+	private String referenceId;
+
+	/** The reference URL. */
+	private String referenceURL;
+
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id.
+	 *
+	 * @param id
+	 *            the new id
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the ver.
+	 *
+	 * @return the ver
+	 */
+	public String getVer() {
+		return ver;
+	}
+
+	/**
+	 * Sets the ver.
+	 *
+	 * @param ver
+	 *            the new ver
+	 */
+	public void setVer(String ver) {
+		this.ver = ver;
+	}
+
+	/**
+	 * Gets the request id.
+	 *
+	 * @return the request id
+	 */
+	public String getRequestId() {
+		return requestId;
+	}
+
+	/**
+	 * Sets the request id.
+	 *
+	 * @param requestId
+	 *            the new request id
+	 */
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	/**
+	 * Gets the timestamp.
+	 *
+	 * @return the timestamp
+	 */
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	/**
+	 * Sets the timestamp.
+	 *
+	 * @param timestamp
+	 *            the new timestamp
+	 */
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	/**
+	 * Gets the reference id.
+	 *
+	 * @return the reference id
+	 */
+	public String getReferenceId() {
+		return referenceId;
+	}
+
+	/**
+	 * Sets the reference id.
+	 *
+	 * @param referenceId
+	 *            the new reference id
+	 */
+	public void setReferenceId(String referenceId) {
+		this.referenceId = referenceId;
+	}
+
+	/**
+	 * Gets the reference URL.
+	 *
+	 * @return the reference URL
+	 */
+	public String getReferenceURL() {
+		return referenceURL;
+	}
+
+	/**
+	 * Sets the reference URL.
+	 *
+	 * @param referenceURL
+	 *            the new reference URL
+	 */
+	public void setReferenceURL(String referenceURL) {
+		this.referenceURL = referenceURL;
+	}
 }
--- registration-processor/packet-bio-dedupe-service/src/main/java/io/mosip/registration/processor/bio/dedupe/service/BioDedupeApp.java
+++ registration-processor/packet-bio-dedupe-service/src/main/java/io/mosip/registration/processor/bio/dedupe/service/BioDedupeApp.java
@@ -3,11 +3,20 @@ package io.mosip.registration.processor.bio.dedupe.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Class BioDedupeApp.
+ */
 @SpringBootApplication(scanBasePackages = { "io.mosip.registration.processor.bio.dedupe",
 		"io.mosip.registration.processor.rest.client", "io.mosip.registration.processor.packet.storage",
 		"io.mosip.registration.processor.core", "io.mosip.registration.processor.filesystem.ceph.adapter.impl" })
 public class BioDedupeApp {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BioDedupeApp.class, args);
 	}
