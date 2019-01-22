7229ac44a24d72bfe4a119cde2fabbaaad03307c
Omsaieswar <omsaieswar.mulakaluri@mindtree.com>
2019-01-22 18:44:53 +0530

    Packet related changes
    Former-commit-id: 56f30e42515c7eb2c3632533fcace5fe5d09eb09
    Former-commit-id: 8ce340bf1d801d31b3d2e9fc26f864850cef3ee6

--- registration/registration-service/src/main/java/io/mosip/registration/dto/OSIDataDTO.java
+++ registration/registration-service/src/main/java/io/mosip/registration/dto/OSIDataDTO.java
@@ -1,5 +1,8 @@
 package io.mosip.registration.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * This class contains the Registration Operator Specific Information
  * 
@@ -8,56 +11,17 @@ package io.mosip.registration.dto;
  * @since 1.0.0
  *
  */
+@Getter
+@Setter
 public class OSIDataDTO extends BaseDTO {
 
 	private String operatorID;
 	private String supervisorID;
 	// Below fields are used for Introducer or HOF or Parent
 	private String introducerType;
-
-	/**
-	 * @return the operatorID
-	 */
-	public String getOperatorID() {
-		return operatorID;
-	}
-
-	/**
-	 * @param operatorID
-	 *            the operatorID to set
-	 */
-	public void setOperatorID(String operatorID) {
-		this.operatorID = operatorID;
-	}
-
-	/**
-	 * @return the supervisorID
-	 */
-	public String getSupervisorID() {
-		return supervisorID;
-	}
-
-	/**
-	 * @param supervisorID
-	 *            the supervisorID to set
-	 */
-	public void setSupervisorID(String supervisorID) {
-		this.supervisorID = supervisorID;
-	}
-
-	/**
-	 * @return the introducerType
-	 */
-	public String getIntroducerType() {
-		return introducerType;
-	}
-
-	/**
-	 * @param introducerType
-	 *            the introducerType to set
-	 */
-	public void setIntroducerType(String introducerType) {
-		this.introducerType = introducerType;
-	}
+	private boolean isOperatorAuthenticatedByPassword;
+	private boolean isSuperviorAuthenticatedByPassword;
+	private boolean isOperatorAuthenticatedByPIN;
+	private boolean isSuperviorAuthenticatedByPIN;
 
 }
