3311f8c0d4730b9d46c6dd43dc7a5eac82cb1a86
M1044345 <M1044345@A2ML26966.mindtree.com>
2019-01-22 13:13:03 +0530

    Otp manager module modified
    Former-commit-id: f751e007001f397c1bfa6262c10f6decc2678ebc
    Former-commit-id: 36de31629a33cfa3868955eae2e8805a1b9ab73c

--- kernel/kernel-otpmanager-service/src/main/java/io/mosip/kernel/otpmanager/constant/OtpErrorConstants.java
+++ kernel/kernel-otpmanager-service/src/main/java/io/mosip/kernel/otpmanager/constant/OtpErrorConstants.java
@@ -14,7 +14,10 @@ public enum OtpErrorConstants {
 	OTP_VAL_ILLEGAL_KEY_INPUT("KER-OTV-002","Length of key should be in the range of 3-255."),
 	OTP_VAL_INVALID_OTP_INPUT("KER-OTV-003","OTP can't be empty or null."),
 	OTP_VAL_ILLEGAL_OTP_INPUT("KER-OTV-004","OTP consists of only numeric characters. No other characters is allowed."),
-	OTP_VAL_KEY_NOT_FOUND("KER-OTV-005","Validation can't be performed against this key. Generate OTP first.");
+	OTP_VAL_KEY_NOT_FOUND("KER-OTV-005","Validation can't be performed against this key. Generate OTP first."),
+	OTP_GEN_CRYPTO_FAILURE("KER-OTG-002","Crypto failure"),
+	OTP_GEN_ALGO_FAILURE("KER-OTG-003","Invalid key or algorithm found"),
+	OTP_GEN_IO_FAILURE("KER-OTG-004","IO exception found");
 	/**
 	 * The error code.
 	 */
