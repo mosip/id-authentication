d88a7fd4489aeafdee543ee3e27c7be4aa30d126
M1044345 <M1044345@A2ML26966.mindtree.com>
2019-01-22 15:01:02 +0530

    Otp generator error message modified
    Former-commit-id: 4b2c8c20df56cad182d2761220c8a7315dffa12c
    Former-commit-id: 56530c3a83cdc5c16ac39f3232f464c1c2acb485

--- kernel/kernel-otpmanager-service/src/main/java/io/mosip/kernel/otpmanager/constant/OtpErrorConstants.java
+++ kernel/kernel-otpmanager-service/src/main/java/io/mosip/kernel/otpmanager/constant/OtpErrorConstants.java
@@ -9,7 +9,7 @@ package io.mosip.kernel.otpmanager.constant;
  *
  */
 public enum OtpErrorConstants {
-	OTP_GEN_ILLEGAL_KEY_INPUT("KER-OTG-001","Key can't be empty, null, and length should be in the range of 3-255."),
+	OTP_GEN_ILLEGAL_KEY_INPUT("KER-OTG-001","Key can't be empty, null, and length should be in the range of 3-64."),
 	OTP_VAL_INVALID_KEY_INPUT("KER-OTV-001","Key can't be empty or null."),
 	OTP_VAL_ILLEGAL_KEY_INPUT("KER-OTV-002","Length of key should be in the range of 3-64."),
 	OTP_VAL_INVALID_OTP_INPUT("KER-OTV-003","OTP can't be empty or null."),
