f93e48637be874c0bcc9cd8273e114a1ceadacff
Kishan Rathore <M1043008@mindtree.com>
2019-01-24 16:16:14 +0530

    MOS-12385 unit test cases changes
    Former-commit-id: 6eec51305c5c82a67161f1f581d3ac3ced75ae19
    Former-commit-id: 5a66bbfd11d3c42bf9813aa211025caca89b40ba

--- pre-registration/pre-registration-batchjob-service/src/test/java/io/mosip/preregistration/batchjobservices/test/service/ConsumedStatusServiceTest.java
+++ pre-registration/pre-registration-batchjob-service/src/test/java/io/mosip/preregistration/batchjobservices/test/service/ConsumedStatusServiceTest.java
@@ -18,6 +18,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import io.mosip.preregistration.batchjobservices.entity.ApplicantDemographic;
 import io.mosip.preregistration.batchjobservices.entity.ProcessedPreRegEntity;
 import io.mosip.preregistration.batchjobservices.entity.RegistrationBookingEntity;
+import io.mosip.preregistration.batchjobservices.entity.RegistrationBookingPK;
+import io.mosip.preregistration.batchjobservices.repository.DemographicRepository;
+import io.mosip.preregistration.batchjobservices.repository.ProcessedPreIdRepository;
+import io.mosip.preregistration.batchjobservices.repository.RegAppointmentRepository;
 import io.mosip.preregistration.batchjobservices.repository.dao.BatchServiceDAO;
 import io.mosip.preregistration.batchjobservices.service.ConsumedStatusService;
 import io.mosip.preregistration.core.common.dto.MainResponseDTO;
@@ -25,41 +29,67 @@ import io.mosip.preregistration.core.common.dto.MainResponseDTO;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConsumedStatusServiceTest {
-	
-	@MockBean
+
+	@Autowired
 	private BatchServiceDAO batchServiceDAO;
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private ConsumedStatusService service;
 
-	List<ProcessedPreRegEntity> preRegList = new ArrayList<>();
-	ApplicantDemographic demographicEntity =new ApplicantDemographic();
-	RegistrationBookingEntity bookingEntity=new RegistrationBookingEntity();
+	@MockBean
+	private ProcessedPreIdRepository preIdRepository;
+
+	@MockBean
+	private DemographicRepository demographicRepository;
+
+	@MockBean
+	private RegAppointmentRepository regAppointmentRepository;
 	
+	private static final String STATUS_COMMENTS = "Processed by registration processor";
+
+	List<ProcessedPreRegEntity> preRegList = new ArrayList<>();
+	ApplicantDemographic demographicEntity = new ApplicantDemographic();
+	RegistrationBookingEntity bookingEntity = new RegistrationBookingEntity();
+	RegistrationBookingPK bookingPK = new RegistrationBookingPK();
+	ProcessedPreRegEntity processedEntity = new ProcessedPreRegEntity();
+
 	@Test
 	public void consumedAppointmentTest() {
 		MainResponseDTO<String> response = new MainResponseDTO<>();
-		
+
+		demographicEntity.setPreRegistrationId("12345678909876");
+
+		bookingPK.setPreregistrationId("12345678909876");
+		bookingEntity.setBookingPK(bookingPK);
+
+		processedEntity.setPreRegistrationId("12345678909876");
+		processedEntity.setStatusCode("Consumed");
+		processedEntity.setStatusComments(STATUS_COMMENTS);
+
+		preRegList.add(processedEntity);
+
 		logger.info("demographicEntity " + demographicEntity);
 		logger.info("bookingEntity " + bookingEntity);
-		
-		Mockito.when(batchServiceDAO.getAllConsumedPreIds(Mockito.anyString())).thenReturn(preRegList);
-		Mockito.when(batchServiceDAO.getApplicantDemographicDetails(Mockito.anyString())).thenReturn(demographicEntity);
+		Mockito.when(preIdRepository.findBystatusComments(STATUS_COMMENTS))
+	    		.thenReturn(preRegList);
+		Mockito.when(demographicRepository.findBypreRegistrationId(demographicEntity.getPreRegistrationId()))
+				.thenReturn(demographicEntity);
 		demographicEntity.setStatusCode("Consumed");
-		
-		Mockito.when(batchServiceDAO.updateApplicantDemographic(demographicEntity)).thenReturn(true);
-		Mockito.when(batchServiceDAO.getPreRegId(Mockito.anyString())).thenReturn(bookingEntity);
+		Mockito.when(demographicRepository.save(demographicEntity)).thenReturn(demographicEntity);
+		Mockito.when(regAppointmentRepository.getPreRegId("12345678909876")).thenReturn(bookingEntity);
 		bookingEntity.setStatusCode("Consumed");
-		
-		Mockito.when(batchServiceDAO.updateBooking(bookingEntity)).thenReturn(true);
-		//preRegList.get(0).setStatusCode("NEW_STATUS_COMMENT");
-		
-		//Mockito.when(batchServiceDAO.updateProcessedList(preRegList.)).thenReturn(true);
-		
-		response=service.demographicConsumedStatus();
+		Mockito.when(regAppointmentRepository.save(bookingEntity)).thenReturn(bookingEntity);
+
+		Mockito.when(preIdRepository.save(Mockito.any())).thenReturn(processedEntity);
+
+		// preRegList.get(0).setStatusCode("NEW_STATUS_COMMENT");
+
+		// Mockito.when(batchServiceDAO.updateProcessedList(preRegList.)).thenReturn(true);
+
+		response = service.demographicConsumedStatus();
 		assertEquals("Demographic status to consumed updated successfully", response.getResponse());
-		
+
 	}
 }
