b6bb008c2a5b3fc796533d5d4d608fc0771693dc
sinhaneha <neha.sinha@mindtree.com>
2019-01-24 17:48:54 +0530

    Comfig class updated
    Former-commit-id: 3edf48e5d55c44a6ceca8e3a6f14c017bcaaa9f9
    Former-commit-id: 149fde9bd8f6fab1dbc632952679941bc027b204

--- kernel/kernel-datamapper-orika/src/test/java/io/mosip/kernel/datamapper/orika/test/DataMapperImplTest.java
+++ kernel/kernel-datamapper-orika/src/test/java/io/mosip/kernel/datamapper/orika/test/DataMapperImplTest.java
@@ -3,6 +3,10 @@ package io.mosip.kernel.datamapper.orika.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -16,13 +20,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.mosip.kernel.core.datamapper.exception.DataMapperException;
-import io.mosip.kernel.core.datamapper.model.IncludeDataField;
 import io.mosip.kernel.core.datamapper.spi.DataMapper;
-import io.mosip.kernel.datamapper.orika.builder.DataMapperBuilderImpl;
 import io.mosip.kernel.datamapper.orika.test.model.DestinationModel;
 import io.mosip.kernel.datamapper.orika.test.model.Person;
 import io.mosip.kernel.datamapper.orika.test.model.PersonDestination;
 import io.mosip.kernel.datamapper.orika.test.model.PersonInterface;
+import io.mosip.kernel.datamapper.orika.test.model.PersonListConverter;
 import io.mosip.kernel.datamapper.orika.test.model.PersonNameList;
 import io.mosip.kernel.datamapper.orika.test.model.PersonNameMap;
 import io.mosip.kernel.datamapper.orika.test.model.PersonNameParts;
@@ -57,6 +60,47 @@ public class DataMapperImplTest {
 	@Autowired
 	DataMapper<Personne, PersonDestination> personneToPersonDestinationMapper6;
 
+	@Qualifier("personNameListToPersonNamePartsMapper")
+	@Autowired
+	DataMapper<PersonNameList, PersonNameParts> personNameListToPersonNamePartsMapper;
+	
+	@Qualifier("personNameMapToPersonNamePartsMapper")
+	@Autowired
+	DataMapper<PersonNameMap, PersonNameParts> personNameMapToPersonNamePartsMapper;
+	
+	@Qualifier("sourceModelToDestinationModelMapper")
+	@Autowired
+	DataMapper<SourceModel, DestinationModel> sourceModelToDestinationModelMapper;
+	
+	@Qualifier("sourceModelToDestinationModelMapper2")
+	@Autowired
+	DataMapper<SourceModel, DestinationModel> sourceModelToDestinationModelMapper2;
+	
+	@Qualifier("personneToPersonMapper")
+	@Autowired
+	DataMapper<Personne, Person> personneToPersonMapper;
+	
+	@Qualifier("personneToPersonInterface")
+	@Autowired
+	DataMapper<Personne, PersonInterface> personneToPersonInterface;
+	
+	@Qualifier("personListToPersonneListMapper")
+	@Autowired
+	DataMapper<List<Person>, List<Personne>> personListToPersonneListMapper;
+	
+	@Test
+	public void personConverterListTest() {
+		PersonListConverter personListConverter = new PersonListConverter();
+		Person person = new Person();
+		LocalDate dob = LocalDate.of(1994, Month.JANUARY, 1);
+		person.setDob(dob);
+		List<Person> personList = new ArrayList<>();
+		personList.add(person);
+		List<Personne> personneList = new ArrayList<Personne>();
+		personListToPersonneListMapper.map(personList, personneList, personListConverter);
+		assertEquals(Period.between(LocalDate.of(1994, Month.JANUARY, 1), LocalDate.now()).getYears(), personneList.get(0).getAge());
+	}
+	
 	@Test
 	public void givenSrcAndDest_whenCanExcludeField_thenCorrect() {
 		Personne french = new Personne("Claire", "cla", 2);
@@ -70,38 +114,24 @@ public class DataMapperImplTest {
 	@Test
 	public void givenSrcWithListAndDestWithPrimitiveAttributes_whenMaps_thenCorrect() {
 
-		List<IncludeDataField> includeField = Arrays.asList(new IncludeDataField("nameList[0]", "firstName", true),
-				new IncludeDataField("nameList[1]", "lastName", true));
-
 		List<String> nameList = Arrays.asList(new String[] { "Sylvester", "Stallone" });
-
 		PersonNameList src = new PersonNameList(nameList);
-
-		DataMapper<PersonNameList, PersonNameParts> dataMapperImpl_1 = new DataMapperBuilderImpl<>(PersonNameList.class,
-				PersonNameParts.class).includeFields(includeField).build();
-
-		PersonNameParts dest = dataMapperImpl_1.map(src);
+		
+		PersonNameParts dest = personNameListToPersonNamePartsMapper.map(src);
 
 		assertEquals("Sylvester", dest.getFirstName());
 		assertEquals("Stallone", dest.getLastName());
-
 	}
 
 	@Test
 	public void givenSrcWithMapAndDestWithPrimitiveAttributes_whenMaps_thenCorrect() {
 
-		List<IncludeDataField> includeField = Arrays.asList(new IncludeDataField("nameMap['first']", "firstName", true),
-				new IncludeDataField("nameMap[\"last\"]", "lastName", true));
-
 		Map<String, String> nameMap = new HashMap<>();
 		nameMap.put("first", "Leornado");
 		nameMap.put("last", "DiCaprio");
 
-		DataMapper<PersonNameMap, PersonNameParts> dataMapperImpl_2 = new DataMapperBuilderImpl<>(PersonNameMap.class,
-				PersonNameParts.class).includeFields(includeField).build();
-
 		PersonNameMap src = new PersonNameMap(nameMap);
-		PersonNameParts dest = dataMapperImpl_2.map(src);
+		PersonNameParts dest = personNameMapToPersonNamePartsMapper.map(src);
 
 		assertEquals("Leornado", dest.getFirstName());
 		assertEquals("DiCaprio", dest.getLastName());
@@ -110,11 +140,8 @@ public class DataMapperImplTest {
 	@Test
 	public void givenSrcWithNullField_whenMapsThenCorrect() {
 
-		DataMapper<SourceModel, DestinationModel> dataMapperImp_3 = new DataMapperBuilderImpl<>(SourceModel.class,
-				DestinationModel.class).build();
-
 		SourceModel src = new SourceModel(null, 10);
-		DestinationModel dest = dataMapperImp_3.map(src);
+		DestinationModel dest = sourceModelToDestinationModelMapper.map(src);
 
 		assertEquals(src.getAge(), dest.getAge());
 		assertEquals(src.getName(), dest.getName());
@@ -122,17 +149,13 @@ public class DataMapperImplTest {
 
 	@Test
 	public void givenSrcWithNullAndClassMapConfigNoNull_whenFailsToMap_thenCorrect() {
-		DataMapper<SourceModel, DestinationModel> dataMapperImpl_4 = new DataMapperBuilderImpl<>(SourceModel.class,
-				DestinationModel.class).build();
-
 		SourceModel src = new SourceModel(null, 10);
 		DestinationModel dest = new DestinationModel("Neha", 25);
 
-		dataMapperImpl_4.map(src, dest);
+		sourceModelToDestinationModelMapper.map(src, dest);
 
 		assertEquals(src.getAge(), dest.getAge());
 		assertNull(dest.getName());
-
 	}
 
 	@Test
@@ -141,10 +164,7 @@ public class DataMapperImplTest {
 		SourceModel src = new SourceModel(null, 10);
 		DestinationModel dest = new DestinationModel("Neha", 25);
 
-		DataMapper<SourceModel, DestinationModel> dataMapperImpl_5 = new DataMapperBuilderImpl<>(SourceModel.class,
-				DestinationModel.class).mapNulls(true).build();
-
-		dataMapperImpl_5.map(src, dest);
+		sourceModelToDestinationModelMapper2.map(src, dest);
 
 		assertEquals(src.getAge(), dest.getAge());
 		assertNull(dest.getName());
@@ -152,15 +172,8 @@ public class DataMapperImplTest {
 
 	@Test
 	public void givenSrcAndDestWithDiffFieldNames_whenMapsConfigNull_thenCorrect() {
-
-		/*List<IncludeDataField> includeField = Arrays.asList(new IncludeDataField("nom", "name", false),
-				new IncludeDataField("surnom", "nickName", true));*/
-
 		Personne french = new Personne(null, null, 2);
 		PersonDestination english = new PersonDestination("Mosip", "Project", 10);
-
-		/*DataMapper<Personne, Person2> dataMapperImpl_6 = personnePerson2Builder.includeFields(includeField).build();*/
-
 		personneToPersonDestinationMapper2.map(french, english);
 
 		assertEquals("Mosip", english.getName());
@@ -170,15 +183,8 @@ public class DataMapperImplTest {
 
 	@Test
 	public void givenSrcAndDestWithDiffFieldNames_whenMapsConfigNoNullIncludingField_thenCorrect() {
-
-		/*List<IncludeDataField> includeField = Arrays.asList(new IncludeDataField("nom", "name", true),
-				new IncludeDataField("surnom", "nickName", true));*/
-
 		Personne french = new Personne(null, "cla", 2);
 		PersonDestination english = new PersonDestination("Mosip", "Project", 10);
-
-		/*DataMapper<Personne, Person2> dataMapperImpl_7 = personnePerson2Builder.includeFields(includeField)
-				.mapNulls(false).build();*/
 
 		personneToPersonDestinationMapper3.map(french, english);
 
@@ -189,15 +195,8 @@ public class DataMapperImplTest {
 
 	@Test
 	public void givenSrcAndDestWithDiffFieldNames_whenMapsExlcudingField_thenCorrect() {
-
-		/*List<String> excludeField = Arrays.asList("nom");
-		List<IncludeDataField> includeField = Arrays.asList(new IncludeDataField("surnom", "nickName", true));*/
-
 		Personne french = new Personne(null, "cla", 2);
 		PersonDestination english = new PersonDestination("Mosip", "Project", 10);
-
-		/*DataMapper<Personne, Person2> dataMapperImpl_8 = personnePerson2Builder.includeFields(includeField)
-				.includeFields(includeField).excludeFields(excludeField).mapNulls(false).build();*/
 
 		personneToPersonDestinationMapper4.map(french, english);
 
@@ -208,14 +207,8 @@ public class DataMapperImplTest {
 
 	@Test
 	public void givenSrcAndDestWithDiffFieldNames_whenMapsNullExlcudingField_thenCorrect() {
-
-		/*List<IncludeDataField> includeField = Arrays.asList(new IncludeDataField("surnom", "nickName", false));
-		List<String> excludeField = Arrays.asList("nom");*/
 		Personne french = new Personne(null, "cla", 2);
 		PersonDestination english = new PersonDestination("Mosip", "Project", 10);
-
-		/*DataMapper<Personne, Person2> dataMapperImpl_9 = personnePerson2Builder.includeFields(includeField)
-				.includeFields(includeField).excludeFields(excludeField).build();*/
 
 		personneToPersonDestinationMapper5.map(french, english);
 
@@ -226,15 +219,8 @@ public class DataMapperImplTest {
 
 	@Test
 	public void givenSrcAsNullDestWithFieldNames_whenMapsExlcudingField_thenFails() {
-
-		/*List<String> excludeField = Arrays.asList("nom");
-		List<IncludeDataField> includeField = Arrays.asList(new IncludeDataField("surnom", "nickName", true));*/
-
 		Personne french = null;
 		PersonDestination english = new PersonDestination("Mosip", "Project", 10);
-
-	/*	DataMapper<Personne, Person2> dataMapperImpl_10 = personnePerson2Builder.includeFields(includeField)
-				.includeFields(includeField).excludeFields(excludeField).build();*/
 
 		personneToPersonDestinationMapper6.map(french, english);
 		assertEquals("Mosip", english.getName());
@@ -243,26 +229,16 @@ public class DataMapperImplTest {
 	@Test(expected = DataMapperException.class)
 	public void givenSrcWithFieldDestAsNull_whenMapsExlcudingField_thenFails() {
 
-		List<String> excludeField = Arrays.asList("nom");
-		List<IncludeDataField> includeField = Arrays.asList(new IncludeDataField("surnom", "nickName", true));
-
 		Personne french = new Personne(null, "cla", 2);
-		Person english = null;
-
-		DataMapper<Personne, Person> dataMapperImpl_11 = new DataMapperBuilderImpl<>(Personne.class, Person.class)
-				.includeFields(includeField).includeFields(includeField).excludeFields(excludeField).build();
-
-		dataMapperImpl_11.map(french, english);
+		Person english = null; 
+		personneToPersonMapper.map(french, english);
 		assertNull(english);
 	}
 
 	@Test(expected = DataMapperException.class)
 	public void givenSrcAndDestAsInterface_whenMaps_thenFails() {
-
 		Personne french = new Personne("Claire", "cla", 2);
-		DataMapper<Personne, PersonInterface> dataMapperImpl_12 = new DataMapperBuilderImpl<>(Personne.class,
-				PersonInterface.class).build();
-		dataMapperImpl_12.map(french);
+		personneToPersonInterface.map(french);
 	}
 
 }
