ab64fee92a22d1bfbb96c17efa74ae6593d33571
M1048860 <M1048860@mindtree.com>
2019-01-28 11:12:35 +0530

    Sonar issues fixed
    Former-commit-id: c3e2f939973fe4c183c9b312d332fdb86c2f6b4c
    Former-commit-id: 80d1dd0689e94024eaaf4788bb9ebba4a16659c9

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/notification/template/mapping/NotificationTemplate.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/notification/template/mapping/NotificationTemplate.java
@@ -1,5 +1,7 @@
 package io.mosip.registration.processor.core.notification.template.mapping;
 
+import java.util.Arrays;
+
 import io.mosip.registration.processor.core.packet.dto.demographicinfo.JsonValue;
 import lombok.Data;
 
@@ -7,6 +9,14 @@ import lombok.Data;
 public class NotificationTemplate {
 	
 	private JsonValue[] firstName;
+
+	public JsonValue[] getFirstName() {
+		return Arrays.copyOf(firstName, firstName.length);
+	}
+
+	public void setFirstName(JsonValue[] firstName) {
+		this.firstName = firstName!=null?firstName:null;
+	}
 
 	private String phoneNumber;
 
