18725d891e4a85509370a1ed521be9823c8417c9
Mukul Puspam <m1039303@mindtree.com>
2019-01-29 15:54:21 +0530

    test cases updated
    Former-commit-id: f363de8efbad0acedf48a0bd903e6a5f721231f2
    Former-commit-id: 938e84d47fcc9f53b68202946b7382ea8b7c8f9b

---
+++ registration-processor/stages/demo-dedupe-stage/src/test/java/io/mosip/registrationprocessor/demodedupe/config/ConfigTest.java
@@ -0,0 +1,55 @@
+package io.mosip.registrationprocessor.demodedupe.config;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+import io.mosip.registration.processor.stages.config.DemoDedupeConfig;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConfigTest {
+
+	@InjectMocks
+	DemoDedupeConfig config;
+
+	private Environment env;
+
+	@Before
+	public void setUp() {
+
+		System.setProperty("spring.cloud.config.uri", "http://104.211.212.28:51000");
+		System.setProperty("spring.profiles.active", "dev");
+		System.setProperty("spring.cloud.config.label", "DEV");
+		System.setProperty("spring.application.name", "registration-processor-packet-uploader");
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		env = context.getEnvironment();
+	}
+
+	@Test
+	public void getPropertySourcesPlaceholderConfigurerTest() throws IOException {
+		PropertySourcesPlaceholderConfigurer configurer = config.getPropertySourcesPlaceholderConfigurer(env);
+		assertNotNull(configurer);
+	}
+
+	@Test
+	public void getdemoDedupeStage() {
+		assertNotNull(config.demoDedupeStage());
+	}
+	
+	@Test
+	public void getdemoDedupeTest() {
+		assertNotNull(config.demoDedupe());
+	}
+
+}
