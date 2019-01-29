d3323d9865c9594e26fd3c30cc12d87c66ff8129
M1022006 <sowmya.goudar@mindtree.com>
2019-01-29 14:03:28 +0530

    added new dtos in core MOS-12831
    Former-commit-id: e49474ded0e4b636d1071784f55be388f73dea80
    Former-commit-id: 0a27d30f2b8483819d397305a3a0f6e25d484d9a

---
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/regcentermachine/DeviceHistoryResponseDto.java
@@ -0,0 +1,15 @@
+package io.mosip.registration.processor.core.packet.dto.regcentermachine;
+
+import java.util.List;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author Megha Tanga
+ * @version 1.0.0
+ */
+@Data
+public class DeviceHistoryResponseDto {
+	private List<DeviceHistoryDto> deviceHistoryDetails;
+}
---
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/regcentermachine/RegistrationCenterDeviceHistoryResponseDto.java
@@ -0,0 +1,15 @@
+
+package io.mosip.registration.processor.core.packet.dto.regcentermachine;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author Uday Kumar
+ * @version 1.0.0
+ */
+@Data
+
+public class RegistrationCenterDeviceHistoryResponseDto {
+	private RegistrationCenterDeviceHistoryDto registrationCenterDeviceHistoryDetails;
+}
