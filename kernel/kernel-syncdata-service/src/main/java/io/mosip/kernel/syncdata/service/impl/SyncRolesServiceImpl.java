be7199939c98202a2912a7a7c84ab3ac9933d9e8
srinivasanMT <srinivasan.lakshminarasimhan@mindtree.com>
2019-01-31 22:04:21 +0530

    MOS-12945 changes made in application properties
    Former-commit-id: 31e2e296463ebba840f7ba469f9c3c51e7bd9424
    Former-commit-id: d535d7136adb2127822e2344cc4b80907837fd6a

--- kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/service/impl/SyncRolesServiceImpl.java
+++ kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/service/impl/SyncRolesServiceImpl.java
@@ -1,6 +1,7 @@
 package io.mosip.kernel.syncdata.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -20,14 +21,22 @@ import io.mosip.kernel.syncdata.service.SyncRolesService;
 public class SyncRolesServiceImpl implements SyncRolesService {
 
 	@Autowired
-	RestTemplate restTemplate;
+	private RestTemplate restTemplate;
 
+	@Value("${https://integ.mosip.io/ldapmanager}")
+	private String authBaseUrl;
+	
+	@Value("${mosip.kernel.syncdata.auth-manager-roles}")
+	private String authServiceName;
+	
 	@Override
 	public RolesResponseDto getAllRoles() {
 		RolesResponseDto rolesDtos = null;
 		try {
 			// URI should be called from properties file
-			rolesDtos = restTemplate.getForObject("https://integ.mosip.io/ldapmanager/allroles",
+			StringBuilder uriBuilder=new StringBuilder();
+			uriBuilder.append(authBaseUrl).append(authServiceName);
+			rolesDtos = restTemplate.getForObject(uriBuilder.toString(),
 					RolesResponseDto.class);
 		} catch (RestClientException ex) {
 			throw new SyncDataServiceException(
