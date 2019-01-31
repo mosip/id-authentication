9e0cdb2e83d9c0ff4f875fbcf427136a564f1365
RanjithaSiddegowda <43336947+RanjithaSiddegowda@users.noreply.github.com>
2019-01-31 15:08:59 +0530

    MOS-12822 Added testcases for canterId and Timestamp validation
    Former-commit-id: 9fe627e1987d108c9730c8417a6a6eff1033f86f
    Former-commit-id: c414becab949f7f0e2b1d7a5a4d4d587a1724fe8

--- registration-processor/stages/osi-validator-stage/src/test/java/io/mosip/registration/processor/stages/umcvalidator/UMCValidatorTest.java
+++ registration-processor/stages/osi-validator-stage/src/test/java/io/mosip/registration/processor/stages/umcvalidator/UMCValidatorTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,12 +27,18 @@ import io.mosip.registration.processor.core.exception.ApisResourceAccessExceptio
 import io.mosip.registration.processor.core.packet.dto.Identity;
 import io.mosip.registration.processor.core.packet.dto.RegOsiDto;
 import io.mosip.registration.processor.core.packet.dto.RegistrationCenterMachineDto;
+import io.mosip.registration.processor.core.packet.dto.regcentermachine.DeviceHistoryDto;
+import io.mosip.registration.processor.core.packet.dto.regcentermachine.DeviceHistoryResponseDto;
 import io.mosip.registration.processor.core.packet.dto.regcentermachine.MachineHistoryDto;
 import io.mosip.registration.processor.core.packet.dto.regcentermachine.MachineHistoryResponseDto;
+import io.mosip.registration.processor.core.packet.dto.regcentermachine.RegistartionCenterTimestampResponseDto;
+import io.mosip.registration.processor.core.packet.dto.regcentermachine.RegistrationCenterDeviceHistoryDto;
+import io.mosip.registration.processor.core.packet.dto.regcentermachine.RegistrationCenterDeviceHistoryResponseDto;
 import io.mosip.registration.processor.core.packet.dto.regcentermachine.RegistrationCenterDto;
 import io.mosip.registration.processor.core.packet.dto.regcentermachine.RegistrationCenterResponseDto;
 import io.mosip.registration.processor.core.packet.dto.regcentermachine.RegistrationCenterUserMachineMappingHistoryDto;
 import io.mosip.registration.processor.core.packet.dto.regcentermachine.RegistrationCenterUserMachineMappingHistoryResponseDto;
+import io.mosip.registration.processor.core.spi.filesystem.adapter.FileSystemAdapter;
 import io.mosip.registration.processor.core.spi.packetmanager.PacketInfoManager;
 import io.mosip.registration.processor.core.spi.restclient.RegistrationProcessorRestClientService;
 import io.mosip.registration.processor.packet.storage.dto.ApplicantInfoDto;
@@ -42,14 +52,17 @@ public class UMCValidatorTest {
 
 	@Mock
 	PacketInfoManager<Identity, ApplicantInfoDto> packetInfoManager;
+	
+	@Mock
+	private FileSystemAdapter<InputStream, Boolean> adapter;
 
 	@Mock
 	private RegistrationProcessorRestClientService<Object> registrationProcessorRestService;
-	RegistrationCenterMachineDto rcmDto;
+	RegistrationCenterMachineDto rcmDto = new RegistrationCenterMachineDto();
 	RegOsiDto regOsi;
 
 	@Before
-	public void setUp() {
+	public void setUp() throws FileNotFoundException {
 		InternalRegistrationStatusDto registrationStatusDto = new InternalRegistrationStatusDto();
 		umcValidator.setRegistrationStatusDto(registrationStatusDto);
 		rcmDto = new RegistrationCenterMachineDto();
@@ -65,9 +78,19 @@ public class UMCValidatorTest {
 		regOsi.setOfficerId("O1234");
 
 		regOsi.setSupervisorId("S1234");
+		
+		
+		ClassLoader classLoader = getClass().getClassLoader();
+		File idJsonFile = new File(classLoader.getResource("packet_meta_info.json").getFile());
+		InputStream packetMetaInfoStream = new FileInputStream(idJsonFile);
+		
+		Mockito.when(adapter.getFile(any(),any())).thenReturn(packetMetaInfoStream);
 
 		Mockito.when(packetInfoManager.getOsi(anyString())).thenReturn(regOsi);
 		Mockito.when(packetInfoManager.getRegistrationCenterMachine(anyString())).thenReturn(rcmDto);
+		//Mockito.when(packetInfoManager.getCenterMachineDto(registrationId);)
+		
+		
 
 	}
 
@@ -87,7 +110,7 @@ public class UMCValidatorTest {
 
 		MachineHistoryDto mcdto = new MachineHistoryDto();
 		mcdto.setIsActive(true);
-		mcdto.setId("yyeqy26356");
+		mcdto.setId("12334");
 
 		List<MachineHistoryDto> mcdtos = new ArrayList<>();
 		mcdtos.add(mcdto);
@@ -105,9 +128,25 @@ public class UMCValidatorTest {
 
 		RegistrationCenterUserMachineMappingHistoryResponseDto offrepdto = new RegistrationCenterUserMachineMappingHistoryResponseDto(
 				officerucmdtos);
+		
+		RegistartionCenterTimestampResponseDto test = new RegistartionCenterTimestampResponseDto();
+		test.setStatus("Accepted");
+		
+		List<DeviceHistoryDto> deviceHistoryDetails = new ArrayList<>();
+		DeviceHistoryDto deviceHistoryDto = new DeviceHistoryDto();
+		deviceHistoryDto.setIsActive(true);
+		deviceHistoryDetails.add(deviceHistoryDto);
+		
+		DeviceHistoryResponseDto deviceHistoryResponsedto = new DeviceHistoryResponseDto();
+		deviceHistoryResponsedto.setDeviceHistoryDetails(deviceHistoryDetails);
 
+		RegistrationCenterDeviceHistoryResponseDto registrationCenterDeviceHistoryResponseDto = new RegistrationCenterDeviceHistoryResponseDto();
+		RegistrationCenterDeviceHistoryDto registrationCenterDeviceHistoryDetails = new RegistrationCenterDeviceHistoryDto();
+
+		registrationCenterDeviceHistoryDetails.setIsActive(true);
+		registrationCenterDeviceHistoryResponseDto.setRegistrationCenterDeviceHistoryDetails(registrationCenterDeviceHistoryDetails);
 		Mockito.when(registrationProcessorRestService.getApi(any(), any(), any(), any(), any())).thenReturn(regrepdto)
-				.thenReturn(mhrepdto).thenReturn(offrepdto);
+				.thenReturn(mhrepdto).thenReturn(offrepdto).thenReturn(offrepdto).thenReturn(test).thenReturn(deviceHistoryResponsedto).thenReturn(registrationCenterDeviceHistoryResponseDto);
 
 		assertTrue(umcValidator.isValidUMC("2018782130000121112018103016"));
 	}
@@ -129,7 +168,7 @@ public class UMCValidatorTest {
 		MachineHistoryDto mcdto = new MachineHistoryDto();
 		mcdto.setIsActive(true);
 		mcdto.setId("yyeqy26356");
-
+		mcdto.setLangCode("eng");
 		List<MachineHistoryDto> mcdtos = new ArrayList<>();
 		mcdtos.add(mcdto);
 		MachineHistoryResponseDto mhrepdto = new MachineHistoryResponseDto();
