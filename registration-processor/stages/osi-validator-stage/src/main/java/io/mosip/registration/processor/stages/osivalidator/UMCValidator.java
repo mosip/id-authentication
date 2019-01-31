915b1dc8cbfdf9aef2e7a862c5d6085db830af30
M1022006 <sowmya.goudar@mindtree.com>
2019-01-31 17:20:28 +0530

    updated test cases for MOS-12831
    Former-commit-id: 4932b5b3b3dfb68174f0ea083970f7684e9b6bc5
    Former-commit-id: cdfca982a519512ac1afdc0c17633c4774d2caff

--- registration-processor/stages/osi-validator-stage/src/main/java/io/mosip/registration/processor/stages/osivalidator/UMCValidator.java
+++ registration-processor/stages/osi-validator-stage/src/main/java/io/mosip/registration/processor/stages/osivalidator/UMCValidator.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -604,7 +603,7 @@ public class UMCValidator {
 	 * @throws ApisResourceAccessException
 	 *             the apis resource access exception
 	 * @throws UMCValidationException
-	 * @throws JSONException
+	 * 
 	 */
 	boolean validateCenterIdAndTimestamp(RegistrationCenterMachineDto rcmDto) throws ApisResourceAccessException {
 		boolean isValid = false;
