f7034e2a71546086f8b184ef99a26532d2f9b2d9
Dinesh Karuppiah <dineshkaruppiah.thiagarajan@mindtree.com>
2019-02-01 16:03:26 +0530

    MOS-1868 - Updated OtpMatchingStartegy
    Former-commit-id: 0f9762255ef0d0f04079703abac34f8152dc795c
    Former-commit-id: df96e2e6decceb29401429e43264f658300d27a3

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/service/demo/OtpMatchingStrategy.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/impl/indauth/service/demo/OtpMatchingStrategy.java
@@ -4,12 +4,18 @@ import java.util.Map;
 
 import io.mosip.authentication.core.constant.IdAuthenticationErrorConstants;
 import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
+import io.mosip.authentication.core.logger.IdaLogger;
 import io.mosip.authentication.core.spi.indauth.match.MatchFunction;
 import io.mosip.authentication.core.spi.indauth.match.MatchingStrategy;
 import io.mosip.authentication.core.spi.indauth.match.MatchingStrategyType;
 import io.mosip.authentication.core.spi.indauth.match.TextMatchingStrategy;
 import io.mosip.authentication.core.spi.indauth.match.ValidateOtpFunction;
+import io.mosip.kernel.core.logger.spi.Logger;
 
+/**
+ * @author M1047697
+ *
+ */
 public enum OtpMatchingStrategy implements TextMatchingStrategy {
 
 	EXACT(MatchingStrategyType.EXACT, (Object reqInfo, Object entityInfo, Map<String, Object> props) -> {
@@ -29,27 +35,58 @@ public enum OtpMatchingStrategy implements TextMatchingStrategy {
 		}
 	});
 
+	/**
+	 * Session id
+	 */
+	private final static String DEFAULT_SESSION_ID = "sessionId";
+
+	/**
+	 * Otp Matching Strategy
+	 */
+	private final static String TYPE = "OtpMatching Strategy";
+
 	/** The match function. */
 	private final MatchFunction matchFunction;
 
 	/** The match strategy type. */
 	private final MatchingStrategyType matchStrategyType;
 
+	/** The mosipLogger. */
+	private static Logger mosipLogger = IdaLogger.getLogger(OtpMatchingStrategy.class);
+
+	/*
+	 * Matching Strategy Type
+	 * 
+	 * @see
+	 * io.mosip.authentication.core.spi.indauth.match.MatchingStrategy#getType()
+	 */
 	@Override
 	public MatchingStrategyType getType() {
 		return matchStrategyType;
 	}
 
-	private static void logError(IdAuthenticationErrorConstants pinMismatch) {
-		// TODO Auto-generated method stub
-
+	private static void logError(IdAuthenticationErrorConstants errorConstants) {
+		mosipLogger.error(DEFAULT_SESSION_ID, TYPE,
+				"Inside OtpMatchingStrategy Strategy" + errorConstants.getErrorCode(),
+				errorConstants.getErrorMessage());
 	}
 
+	/*
+	 *
+	 * Get MatchFunction
+	 * 
+	 * @see io.mosip.authentication.core.spi.indauth.match.MatchingStrategy#
+	 * getMatchFunction()
+	 */
 	@Override
 	public MatchFunction getMatchFunction() {
 		return matchFunction;
 	}
 
+	/**
+	 * @param matchStrategyType
+	 * @param matchFunction
+	 */
 	OtpMatchingStrategy(MatchingStrategyType matchStrategyType, MatchFunction matchFunction) {
 		this.matchFunction = matchFunction;
 		this.matchStrategyType = matchStrategyType;
