62774c5eb7e6e75488e61de71ec7038f996f9d8b
Raj Kumar Jha <raj.jha.afara@gmail.com.com>
2019-02-04 15:08:33 +0530

    MOS-9743 Removed hard code digits in error msg https://mosipid.atlassian.net/browse/MOS-9743
    Former-commit-id: 1924fb661a5f25c109fc5f2451d48a411e321740
    Former-commit-id: dc8509532352a68c51f83b6d976332553f293c79

--- kernel/kernel-idvalidator-uin/src/main/java/io/mosip/kernel/idvalidator/uin/constant/UinExceptionConstant.java
+++ kernel/kernel-idvalidator-uin/src/main/java/io/mosip/kernel/idvalidator/uin/constant/UinExceptionConstant.java
@@ -13,13 +13,13 @@ package io.mosip.kernel.idvalidator.uin.constant;
 public enum UinExceptionConstant {
 	
 	UIN_VAL_INVALID_NULL("KER-IDV-201" , "UIN should not be Empty or Null."),
-	UIN_VAL_ILLEGAL_SEQUENCE_REPEATATIVE("KER-IDV-202", "UIN should not contain any sequential and repeated block of number for 2 or more than two digits and Admin ristricted numbers"), 
-	UIN_VAL_ILLEGAL_LENGTH("KER-IDV-203", "UIN length should be 12 digit."), 
+	UIN_VAL_ILLEGAL_SEQUENCE_REPEATATIVE("KER-IDV-202", "UIN should not contain any sequential and repeated block of number as per configured or more than that digits and Admin ristricted numbers"), 
+	UIN_VAL_ILLEGAL_LENGTH("KER-IDV-203", "UIN length should be as per configured digit."), 
 	UIN_VAL_INVALID_DIGITS("KER-IDV-204", "UIN should not contain any alphanumeric characters."), 
 	UIN_VAL_ILLEGAL_CHECKSUM("KER-IDV-205", "UIN should match checksum."),
-	UIN_VAL_INVALID_ZERO_ONE("KER-IDV-206", "UIN should not contain Zero or One as first digit."),
-	UIN_VAL_ILLEGAL_REVERSE("KER-IDV-xxx", "UIN First X digits should be different from the reverse of last X digits"),
-	UIN_VAL_ILLEGAL_EQUAL_LIMIT("KER-IDV-xxx", "UIN First X digits should be different from the last X digits");
+	UIN_VAL_INVALID_ZERO_ONE("KER-IDV-206", "UIN should not contain start with as per configured."),
+	UIN_VAL_ILLEGAL_REVERSE("KER-IDV-207", "UIN First configured no.of digits should be different from the reverse of last configured no. of digits"),
+	UIN_VAL_ILLEGAL_EQUAL_LIMIT("KER-IDV-208", "UIN First configured no.of digits should be different from the last configured no.of digits");
 	/**
 	 * This variable holds the error code.
 	 */
