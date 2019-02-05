7ffba5e05abc5677f781c7aededf822abd015d29
M1039285 <Nagalakshmi.Nithyanandan@mindtree.com>
2019-02-05 11:17:03 +0530

    MOS-11962 code update for main class
    Former-commit-id: 4aee65744934449c3a6de0fee02359f2c4250f7d
    Former-commit-id: bfbc2155e2d0f8d4d893734e8a56f5e7bd4261f5

--- registration-processor/registration-processor-message-sender/src/main/java/io/mosip/registration/processor/message/sender/MessageNotificationApplication.java
+++ registration-processor/registration-processor-message-sender/src/main/java/io/mosip/registration/processor/message/sender/MessageNotificationApplication.java
@@ -1,12 +1,7 @@
 package io.mosip.registration.processor.message.sender;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.mosip.registration.processor.message.sender.utility.NotificationTemplateType;
-import io.mosip.registration.processor.message.sender.utility.TriggerNotification;
 
 /**
  * The Class MessageNotificationApplication.
@@ -20,10 +15,7 @@ import io.mosip.registration.processor.message.sender.utility.TriggerNotificatio
 		"io.mosip.registration.processor.core", "io.mosip.registration.processor.packet.storage",
 		"io.mosip.registration.processor.rest.client", "io.mosip.registration.processor.filesystem.ceph.adapter.impl" })
 
-public class MessageNotificationApplication implements CommandLineRunner{
-	
-	@Autowired
-	TriggerNotification tr;
+public class MessageNotificationApplication {
 
 	/**
 	 * Main method to instantiate the spring boot application.
@@ -34,11 +26,6 @@ public class MessageNotificationApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessageNotificationApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		tr.triggerNotification("85425022110000120190117110505", NotificationTemplateType.DUPLICATE_UIN);
 	}
 
 }
