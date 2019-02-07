25c27c17a46392845cb453ff4bfde932db2f69e2
Omsaieswar <omsaieswar.mulakaluri@mindtree.com>
2019-02-07 11:05:44 +0530

    MOS-5771:Fixed the Sonar issues
    Former-commit-id: 9684050f2275d284a593e89973636d0f04066a64
    Former-commit-id: 47281315a143f2ea7ec4637c2b2ff4d03191d9b3

--- registration/registration-libs/src/main/test/io/mosip/registration/cipher/ClientJarEncryptionTest.java
+++ registration/registration-libs/src/main/test/io/mosip/registration/cipher/ClientJarEncryptionTest.java
@@ -6,8 +6,7 @@ public class ClientJarEncryptionTest {
 	
 	@Test
 	public void generateEncryptedJar() {
-		String args[] = new String[]{"exp.jar", null, "fdHPgbFn5LZjPE8fX5S0UQ==", "0.8.1"};
-		ClientJarEncryption.main(args);
+		ClientJarEncryption.main(null);
 	}
 
 }
