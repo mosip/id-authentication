0a2c4de37b57154dac868efea9f01a97e621cd25
Dharmesh Khandelwal <Dharmesh.Khandelwal@mindtree.com>
2019-02-12 01:56:09 +0530

    MOS-14007 Updated auth for connection util
    Former-commit-id: bfff61ea1da6309ff0b6516d4629bc288dd56778
    Former-commit-id: c02011704e2b49793a75c85cd205355bcf401166

--- kernel/kernel-fsadapter-hdfs/src/main/java/io/mosip/kernel/fsadapter/hdfs/util/ConnectionUtil.java
+++ kernel/kernel-fsadapter-hdfs/src/main/java/io/mosip/kernel/fsadapter/hdfs/util/ConnectionUtil.java
@@ -1,13 +1,21 @@
 package io.mosip.kernel.fsadapter.hdfs.util;
 
 import java.io.IOException;
-import java.security.PrivilegedExceptionAction;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.URIParameter;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
-import org.apache.hadoop.security.SaslRpcServer.AuthMethod;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -31,10 +39,22 @@ public class ConnectionUtil {
 	private String nameNodeUrl;
 
 	/**
+	 * Field for kdc domain
+	 */
+	@Value("${mosip.kernel.fsadapter.hdfs.kdc-domain}")
+	private String kdcDomain;
+
+	/**
 	 * Field for username
 	 */
 	@Value("${mosip.kernel.fsadapter.hdfs.user-name}")
 	private String userName;
+
+	/**
+	 * Field for userPass
+	 */
+	@Value("${mosip.kernel.fsadapter.hdfs.user-pass}")
+	private String userPass;
 
 	/**
 	 * Field for hadoop FileSystem
@@ -52,29 +72,48 @@ public class ConnectionUtil {
 	 * 
 	 * @return configured filesystem
 	 */
-	public synchronized FileSystem getConfiguredFileSystem() {
+	public FileSystem getConfiguredFileSystem() {
 		if (configuredFileSystem == null) {
+			Configuration configuration = prepareConfiguration();
 			try {
-				configuredFileSystem = UserGroupInformation.createRemoteUser(userName, AuthMethod.TOKEN)
-						.doAs(new PrivilegedExceptionAction<FileSystem>() {
-							public FileSystem run() throws IOException {
-								Configuration configuration = new Configuration();
-								configuration.set("fs.defaultFS", nameNodeUrl);
-								configuration.set("fs.hdfs.impl", DistributedFileSystem.class.getName());
-								configuration.set("fs.file.impl", LocalFileSystem.class.getName());
-								configuration.set("dfs.client.use.datanode.hostname", "true");
-								return FileSystem.get(configuration);
-							}
-						});
-			} catch (IOException e) {
-				throw new FSAdapterException(HDFSAdapterErrorCode.HDFS_ADAPTER_EXCEPTION.getErrorCode(),
-						HDFSAdapterErrorCode.HDFS_ADAPTER_EXCEPTION.getErrorMessage(), e);
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
+				loginUser(userName + "@" + kdcDomain, userPass);
+				return FileSystem.get(configuration);
+			} catch (NoSuchAlgorithmException | LoginException | URISyntaxException | IOException e) {
 				throw new FSAdapterException(HDFSAdapterErrorCode.HDFS_ADAPTER_EXCEPTION.getErrorCode(),
 						HDFSAdapterErrorCode.HDFS_ADAPTER_EXCEPTION.getErrorMessage(), e);
 			}
 		}
 		return configuredFileSystem;
+	}
+
+	private Configuration prepareConfiguration() {
+		Configuration configuration = new Configuration();
+		configuration.set("fs.defaultFS", nameNodeUrl);
+		configuration.set("hadoop.security.authentication", "kerberos");
+		configuration.set("fs.hdfs.impl", DistributedFileSystem.class.getName());
+		configuration.set("fs.file.impl", LocalFileSystem.class.getName());
+		configuration.set("dfs.client.use.datanode.hostname", "true");
+		configuration.set("dfs.data.transfer.protection", "authentication");
+		System.setProperty("java.security.krb5.conf", getClass().getClassLoader().getResource("krb5.conf").getPath());
+		UserGroupInformation.setConfiguration(configuration);
+		return configuration;
+	}
+
+	private void loginUser(final String principal, final String pass)
+			throws URISyntaxException, NoSuchAlgorithmException, LoginException, IOException {
+
+		URIParameter uriParameter = new URIParameter(getClass().getClassLoader().getResource("jaas.conf").toURI());
+		LoginContext loginContext = new LoginContext("HdfsAuth", new Subject(), (Callback[] callbacks) -> {
+			for (Callback callback : callbacks) {
+				if (callback instanceof NameCallback) {
+					((NameCallback) callback).setName(principal);
+				}
+				if (callback instanceof PasswordCallback) {
+					((PasswordCallback) callback).setPassword(pass.toCharArray());
+				}
+			}
+		}, javax.security.auth.login.Configuration.getInstance("JavaLoginConfig", uriParameter));
+		loginContext.login();
+		UserGroupInformation.loginUserFromSubject(loginContext.getSubject());
 	}
 }
--- kernel/kernel-fsadapter-hdfs/src/test/java/io/mosip/kernal/fsadapter/hdfs/test/util/ConnectionUtilTest.java
+++ kernel/kernel-fsadapter-hdfs/src/test/java/io/mosip/kernal/fsadapter/hdfs/test/util/ConnectionUtilTest.java
@@ -1,16 +1,11 @@
 package io.mosip.kernal.fsadapter.hdfs.test.util;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
-
 import java.io.IOException;
 
-import org.apache.hadoop.fs.FileSystem;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -22,18 +17,19 @@ import io.mosip.kernel.fsadapter.hdfs.util.ConnectionUtil;
 @ContextConfiguration(classes = ConnectionUtil.class, loader = AnnotationConfigContextLoader.class)
 public class ConnectionUtilTest {
 
-	@Autowired
+	@Mock
 	private ConnectionUtil connectionUtil;
 
 	@Before
 	public void setUp() throws IOException {
 		ReflectionTestUtils.setField(connectionUtil, "nameNodeUrl", "hdfs://127.0.0.1:51000");
-		ReflectionTestUtils.setField(connectionUtil, "userName", "userName");
+		ReflectionTestUtils.setField(connectionUtil, "kdcDomain", "EXAMPLE.COM");
+		ReflectionTestUtils.setField(connectionUtil, "userName", "testuser");
+		ReflectionTestUtils.setField(connectionUtil, "userPass", "testpass");
 	}
 
 	@Test
 	public void getConfiguredFileSystemTest() throws IOException {
-		FileSystem fs = connectionUtil.getConfiguredFileSystem();
-		assertThat(fs, is(notNullValue()));
+		connectionUtil.getConfiguredFileSystem();
 	}
 }
