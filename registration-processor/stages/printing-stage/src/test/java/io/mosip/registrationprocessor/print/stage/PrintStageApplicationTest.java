b449a344edace41c80d49bdd11abcbbbca4c4b69
M1048399 <Horteppa.Pujari@mindtree.com>
2019-02-12 11:54:47 +0530

    MOS-14596 creating print stage https://mosipid.atlassian.net/browse/MOS-14596
    Former-commit-id: 902d283e6ae3d5ad0d0c3806a56bfb87ffd09e80
    Former-commit-id: 88769e4d90b8ffc637a2167937f463578e76532a

---
+++ registration-processor/stages/printing-stage/src/test/java/io/mosip/registrationprocessor/print/stage/PrintStageApplicationTest.java
@@ -0,0 +1,38 @@
+package io.mosip.registrationprocessor.print.stage;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrintStageApplicationTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PrintStageApplicationTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PrintStageApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
