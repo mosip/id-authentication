1e50bd58a0fc4c4287cc9202f1712db213c1ac1a
Tapaswini Behera(IN3226) <M1043226@mindtree.com>
2019-02-12 19:44:23 +0530

    MOS-14513 adding test cases for data sync util https://mosipid.atlassian.net/browse/MOS-14513
    Former-commit-id: ae6b20dce0d3d371d829dda6759c2cc03a09bcbf
    Former-commit-id: 7034e2b489ff6cd42157966dcd361843321347bf

--- pre-registration/pre-registration-datasync-service/src/test/java/io/mosip/preregistration/datasync/test/service/util/DataSyncServiceUtilTest.java
+++ pre-registration/pre-registration-datasync-service/src/test/java/io/mosip/preregistration/datasync/test/service/util/DataSyncServiceUtilTest.java
@@ -3,6 +3,7 @@ package io.mosip.preregistration.datasync.test.service.util;
 import static org.junit.Assert.assertEquals;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -11,22 +12,32 @@ import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.preregistration.core.common.dto.ExceptionJSONInfoDTO;
+import io.mosip.preregistration.core.common.dto.MainListResponseDTO;
 import io.mosip.preregistration.core.common.dto.MainRequestDTO;
+import io.mosip.preregistration.core.common.dto.PreRegIdsByRegCenterIdDTO;
+import io.mosip.preregistration.core.common.dto.PreRegIdsByRegCenterIdResponseDTO;
 import io.mosip.preregistration.core.config.LoggerConfiguration;
 import io.mosip.preregistration.core.exception.InvalidRequestParameterException;
 import io.mosip.preregistration.core.util.AuditLogUtil;
 import io.mosip.preregistration.datasync.dto.DataSyncRequestDTO;
 import io.mosip.preregistration.datasync.dto.ReverseDataSyncRequestDTO;
+import io.mosip.preregistration.datasync.exception.DemographicGetDetailsException;
 import io.mosip.preregistration.datasync.repository.InterfaceDataSyncRepo;
 import io.mosip.preregistration.datasync.repository.ProcessedDataSyncRepo;
 import io.mosip.preregistration.datasync.service.util.DataSyncServiceUtil;
@@ -89,6 +100,11 @@ public class DataSyncServiceUtilTest {
 
 	private static Logger log = LoggerConfiguration.logConfig(DataSyncServiceUtil.class);
 
+	String resTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(new Date());
+	
+	ExceptionJSONInfoDTO errlist = new ExceptionJSONInfoDTO();
+	ExceptionJSONInfoDTO exceptionJSONInfo = new ExceptionJSONInfoDTO("", "");
+	
 	String preId = "23587986034785";
 	MainRequestDTO<?> requestDto = new MainRequestDTO<>();
 	Date date = new Timestamp(System.currentTimeMillis());
@@ -96,7 +112,11 @@ public class DataSyncServiceUtilTest {
 	MainRequestDTO<DataSyncRequestDTO> datasyncReqDto = new MainRequestDTO<>();
 	DataSyncRequestDTO dataSyncRequestDTO = new DataSyncRequestDTO();
 	ReverseDataSyncRequestDTO reverseDataSyncRequestDTO = new ReverseDataSyncRequestDTO();
-
+	PreRegIdsByRegCenterIdResponseDTO idResponseDTO = new PreRegIdsByRegCenterIdResponseDTO();
+	PreRegIdsByRegCenterIdDTO preRegIdsByRegCenterIdDTO = new PreRegIdsByRegCenterIdDTO();
+	List<String> preRegIds = new ArrayList<String>();
+	
+	
 	@Test
 	public void validateDataSyncRequestTest() {
 		dataSyncRequestDTO.setRegClientId("1005");
@@ -221,8 +241,46 @@ public class DataSyncServiceUtilTest {
 
 	}
 	
+	@Test
+	public void callGetPreIdsRestServiceTest() {
+		String fromDate="2018-01-17 00:00:00";
+		String toDate="2019-01-17 00:00:00";
+		preRegIds.add("23587986034785");
+		RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
+		Mockito.when(restTemplateBuilder.build()).thenReturn(restTemplate);
+		MainListResponseDTO mainResponseDTO=new MainListResponseDTO();
+		mainResponseDTO.setStatus(true);
+		mainResponseDTO.setResTime(resTime);
+		mainResponseDTO.setErr(exceptionJSONInfo);
+		mainResponseDTO.setResponse(preRegIds);
+		ResponseEntity<MainListResponseDTO> respEntity=new ResponseEntity<>(mainResponseDTO, HttpStatus.OK);
+		Mockito.when(restTemplate.exchange(Mockito.anyString(), Mockito.eq(HttpMethod.GET), Mockito.any(),
+				Mockito.eq(MainListResponseDTO.class))).thenReturn(respEntity);
+		List<String> list =serviceUtil.callGetPreIdsRestService(fromDate, toDate);
+		assertEquals(list.size(), preRegIds.size());
+	}
+	
+//	@Test(expected=DemographicGetDetailsException.class)
+//	public void demographicGetDetailsExceptionTest() {
+//		String fromDate="2018-01-17 00:00:00";
+//		String toDate="2019-01-17 00:00:00";
+//		preRegIds.add("23587986034785");
+//		RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
+//		Mockito.when(restTemplateBuilder.build()).thenReturn(restTemplate);
+//		MainListResponseDTO mainResponseDTO=new MainListResponseDTO();
+//		mainResponseDTO.setStatus(false);
+//		mainResponseDTO.setResTime(resTime);
+//		mainResponseDTO.setErr(exceptionJSONInfo);
+//		mainResponseDTO.setResponse(preRegIds);
+//		Mockito.when(restTemplateBuilder.build()).thenReturn(restTemplate);
+//		Mockito.when(restTemplate.exchange(Mockito.anyString(), Mockito.eq(HttpMethod.GET), Mockito.any(),
+//				Mockito.eq(MainListResponseDTO.class))).thenReturn(null);
+//		List<String> list =serviceUtil.callGetPreIdsRestService(fromDate, toDate);
+//		
+//	}
+	
 //	@Test
-//	public void callGetPreIdsRestServiceTest() {
+//	public void callGetPreIdsByRegCenterIdRestServiceTest() {
 //		
 //	}
 	
