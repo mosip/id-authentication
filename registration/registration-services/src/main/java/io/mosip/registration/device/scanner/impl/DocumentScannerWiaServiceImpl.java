fdaf4368f1d4c7d670d9394d74ac244cdf7e118f
BalamuruganRamamoorthy <Balamurugan.Ramamoorthy2@mindtree.com>
2019-02-13 14:39:21 +0530

    MOS-15312 : Document scanner fixes added
    Former-commit-id: 54117bb4d3e3eb785aec7e8ce47fd43786c84f09
    Former-commit-id: 33272b0a5f5bfef4a9395a6803306b77ef8beb5e

--- registration/registration-services/src/main/java/io/mosip/registration/device/scanner/impl/DocumentScannerWiaServiceImpl.java
+++ registration/registration-services/src/main/java/io/mosip/registration/device/scanner/impl/DocumentScannerWiaServiceImpl.java
@@ -5,7 +5,10 @@ import static io.mosip.registration.constants.RegistrationConstants.APPLICATION_
 import static io.mosip.registration.constants.RegistrationConstants.APPLICATION_NAME;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.stereotype.Service;
 
@@ -19,6 +22,13 @@ import io.mosip.registration.device.scanner.wia.WiaDeviceType;
 import io.mosip.registration.device.scanner.wia.WiaImageBias;
 import io.mosip.registration.device.scanner.wia.WiaImageIntent;
 
+/**
+ * This class is used to provide document scanner functionalities for windows
+ * platform through WIA service
+ * 
+ * @author balamurugan.ramamoorthy
+ *
+ */
 @Service
 public class DocumentScannerWiaServiceImpl extends DocumentScannerService {
 
@@ -36,26 +46,24 @@ public class DocumentScannerWiaServiceImpl extends DocumentScannerService {
 	public BufferedImage scan() {
 
 		BufferedImage bufferedImage = null;
-		ICommonDialog wiaObj = ClassFactory.createCommonDialog();
-		IImageFile iImageFile = wiaObj.showAcquireImage(WiaDeviceType.ScannerDeviceType, WiaImageIntent.GrayscaleIntent,
-				WiaImageBias.MinimizeSize, "{00000000-0000-0000-0000-000000000000}", false, false, false);
 		try {
-			bufferedImage = getBufferedImageFromBytes(scannedImageTobytes(iImageFile));
+			ICommonDialog wiaObj = ClassFactory.createCommonDialog();
+			IImageFile iImageFile = wiaObj.showAcquireImage(WiaDeviceType.ScannerDeviceType,
+					WiaImageIntent.GrayscaleIntent, WiaImageBias.MinimizeSize, "{00000000-0000-0000-0000-000000000000}",
+					false, false, false);
 
-		} catch (IOException e) {
+			String tempFilePath = File.createTempFile("tempImgFile", ".jpg").getAbsolutePath();
+			new File(tempFilePath).delete();
+
+			iImageFile.saveFile(tempFilePath);
+
+			bufferedImage = ImageIO.read(new File(tempFilePath));
+			new File(tempFilePath).delete();
+
+		} catch (IOException | RuntimeException e) {
 			LOGGER.error(LOG_REG_DOC_SCAN_CONTROLLER, APPLICATION_NAME, APPLICATION_ID, e.getMessage());
 		}
 		return bufferedImage;
-	}
-
-	private byte[] scannedImageTobytes(IImageFile iImageFile) {
-		IVector vector = iImageFile.fileData();
-		byte[] imageBytes = new byte[vector.count()];
-		for (int i = 1; i <= vector.count(); i++) {
-			Short sh = (Short) vector.item(i);
-			imageBytes[i - 1] = sh.byteValue();
-		}
-		return imageBytes;
 	}
 
 }
