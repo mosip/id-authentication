aef0f145cfbf5ee85e6ac0f5a945c4f9a9b35dcd
Sadanandegowda Dm <M1045015@mindtree.com>
2019-02-13 17:33:08 +0530

    Added updated on pre-reg tables and master seed data and data load scripts
    Former-commit-id: 73e57d184b48d066168a25c6eb06229d048141fe
    Former-commit-id: 586be866a8fd14364df4fcb495fc2d8c0312e704

--- scripts/database/mosip_prereg/ddl/prereg-applicant_demographic.sql
+++ scripts/database/mosip_prereg/ddl/prereg-applicant_demographic.sql
@@ -13,9 +13,9 @@ create schema if not exists prereg
 	create table prereg.applicant_demographic (
 			
 		prereg_id  			character varying(36) not null ,
+		group_id  			character varying(36) ,
 		
 		demog_detail   		bytea ,
-		demog_detail_hash 	character varying(64) not null ,
 		encrypted_dtimes 	timestamp not null ,
 
 		status_code 		character varying(36) not null ,	-- master.status_list.code
@@ -27,7 +27,9 @@ create schema if not exists prereg
 		cr_by 		character varying (32) not null,      
 		cr_dtimes 	timestamp not null ,
 		upd_by 		character varying (32) ,
-		upd_dtimes 	timestamp
+		upd_dtimes 	timestamp ,
+		is_deleted 	boolean ,
+		del_dtimes	timestamp
 	)
 ;
 
