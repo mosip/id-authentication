a3bfe6045f3dd8daf97ad1a74003c03158bf4215
Mukul Puspam <mukul.puspam2@mindtree.com>
2019-02-18 19:49:44 +0530

    MOS-14582 : test cases added https://mosipid.atlassian.net/browse/MOS-14582
    Former-commit-id: 8aa54c6f467146df6124f7b9ae1c6fdf194c6017
    Former-commit-id: 7f3b0ac81c8e3623dbb66ad48f1965a9904f1676

---
+++ registration-processor/registration-processor-core/src/test/java/io/mosip/registration/processor/queue/MosipActiveMqTest.java
@@ -0,0 +1,62 @@
+package io.mosip.registration.processor.queue;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.mosip.registration.processor.core.queue.factory.MosipActiveMq;
+import io.mosip.registration.processor.core.queue.factory.MosipQueue;
+import io.mosip.registration.processor.core.queue.factory.MosipQueueConnectionFactoryImpl;
+import io.mosip.registration.processor.core.queue.impl.MosipActiveMqImpl;
+import io.mosip.registration.processor.core.queue.impl.exception.InvalidConnectionException;
+import io.mosip.registration.processor.core.spi.queue.MosipQueueConnectionFactory;
+
+@RunWith(SpringRunner.class)
+public class MosipActiveMqTest {
+	
+	@InjectMocks
+	MosipActiveMqImpl mosipActiveMqImpl;
+	
+	@Mock
+	MosipActiveMq mosipActiveMq;
+	
+	@Mock
+	ActiveMQConnectionFactory factory;
+	
+	MosipQueue mosipQueue;
+	
+	@Test
+	public void testSendSuccess() throws JMSException {
+
+		mosipQueue = new MosipActiveMq("admin", "admin","vm://localhost?broker.persistent=false");
+		byte[] bytes = "message".getBytes();
+		when(mosipActiveMq.getActiveMQConnectionFactory()).thenReturn(factory);
+		assertTrue(mosipActiveMqImpl.send(mosipQueue, bytes, "address"));
+	}
+	
+	@Test(expected = InvalidConnectionException.class)
+	public void invalidConnectionExceptionTest() {
+		mosipQueue = new MosipActiveMq("admin", "admin","vm://localhost?broker.persistent=false") {
+			@Override
+			public ActiveMQConnectionFactory getActiveMQConnectionFactory() {
+				return null;
+			}
+		};
+		byte[] bytes = "message".getBytes();
+		when(mosipActiveMq.getActiveMQConnectionFactory()).thenReturn(factory);
+		assertTrue(mosipActiveMqImpl.send(mosipQueue, bytes, "address"));
+	}
+	
+}
