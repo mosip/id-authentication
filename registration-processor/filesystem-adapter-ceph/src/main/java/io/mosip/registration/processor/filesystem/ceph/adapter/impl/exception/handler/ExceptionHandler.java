de46245646153b0c12ebcb8a3d7eec3fa12e7551
Monobikash Das <M1045447@mindtree.com>
2019-02-19 15:28:36 +0530

    MOS-13136 : added filesystem adapter https://mosipid.atlassian.net/browse/MOS-13136
    Former-commit-id: d264ec19ae75c1af95ab711f16a13eacc4d892c2
    Former-commit-id: d8b51434c0ed61749e64e49a230630c081383d3b

---
+++ registration-processor/filesystem-adapter-ceph/src/main/java/io/mosip/registration/processor/filesystem/ceph/adapter/impl/exception/handler/ExceptionHandler.java
@@ -0,0 +1,48 @@
+package io.mosip.registration.processor.filesystem.ceph.adapter.impl.exception.handler;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+
+import io.mosip.registration.processor.core.exception.util.PlatformErrorMessages;
+import io.mosip.registration.processor.filesystem.ceph.adapter.impl.exception.ConnectionUnavailableException;
+import io.mosip.registration.processor.filesystem.ceph.adapter.impl.exception.InvalidConnectionParameters;
+import io.mosip.registration.processor.filesystem.ceph.adapter.impl.exception.PacketNotFoundException;
+
+/**
+ * Global Exception handler.
+ *
+ * @author Pranav Kumar
+ */
+public class ExceptionHandler {
+	
+	/**
+	 * Instantiates a new exception handler.
+	 */
+	private ExceptionHandler() {
+		
+	}
+	
+	/**
+	 * Exception handler.
+	 *
+	 * @param e the e
+	 */
+	public static void exceptionHandler(AmazonS3Exception e) {
+		if(e.getStatusCode() == 403) {
+			throw new InvalidConnectionParameters(PlatformErrorMessages.RPR_FAC_INVALID_CONNECTION_PARAMETERS.getMessage(),e);
+		}
+		else if(e.getStatusCode() == 404) {
+			throw new PacketNotFoundException(PlatformErrorMessages.RPR_FAC_PACKET_NOT_AVAILABLE.getMessage(),e);
+		}
+	}
+	
+	/**
+	 * Exception handler.
+	 *
+	 * @param e the e
+	 */
+	public static void exceptionHandler(SdkClientException e) {
+		throw new ConnectionUnavailableException(PlatformErrorMessages.RPR_FAC_CONNECTION_NOT_AVAILABLE.getMessage(),e);
+	}
+
+}
