96b590cb97b8a5f3153de32be067003d30c2a3c5
Chukka.Sreekar@mindtree.com <M1048290@b2ml24784.mindtree.com>
2019-02-22 17:58:02 +0530

    Encryption and Decryption algorithm fix
    Former-commit-id: f8a2adddaaffa3dbfda32b599adb75f8d05967df
    Former-commit-id: 4225c9030ef1b45407f236186e209cd7cb79ae19

--- registration/registration-libs/src/main/java/io/mosip/registration/cipher/CilentJarDecryption.java
+++ registration/registration-libs/src/main/java/io/mosip/registration/cipher/CilentJarDecryption.java
@@ -1,5 +1,6 @@
 package io.mosip.registration.cipher;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Base64;
@@ -8,6 +9,9 @@ import java.util.ResourceBundle;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.io.FileUtils;
+import org.bouncycastle.asn1.cmc.DecryptedPOP;
 
 import io.mosip.kernel.crypto.jce.constant.SecurityMethod;
 import io.mosip.kernel.crypto.jce.processor.SymmetricProcessor;
@@ -21,6 +25,7 @@ import io.mosip.kernel.crypto.jce.processor.SymmetricProcessor;
 public class CilentJarDecryption {
 	private static final String SLASH = "/";
 	private static final String AES_ALGORITHM = "AES";
+	private static final String REGISTRATION="registration";
 
 	/**
 	 * Decrypt the bytes
@@ -43,7 +48,20 @@ public class CilentJarDecryption {
 	 * @param args
 	 */
 	public static void main(String[] args) throws IOException {
+		CilentJarDecryption aesDecrypt = new CilentJarDecryption();
 		ResourceBundle resourceBundle = ResourceBundle.getBundle("reg-application");
-		//TODO: write the decryption logic and save the jar file to the temp directory.
+		File file = new File("D:\\decryption\\mosip-sw-0.8.10\\lib");
+		for (File files : file.listFiles()) {
+
+			if (files.getName().contains(REGISTRATION)) {
+				System.out.println("Decrypt File Name====>"+files.getName());
+				byte[] encryptedRegFileBytes = aesDecrypt.decrypt(FileUtils.readFileToByteArray(files),
+						Base64.getDecoder().decode( "fdHPgbFn5LZjPE8fX5S0UQ=="));
+				
+			}
+
+		}
+
+		// TODO: write the decryption logic and save the jar file to the temp directory.
 	}
 }
