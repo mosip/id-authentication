1c177fada5c3fec6e9966686ee14dcab9da97c43
bantiz <Balvikash.Sharma@mindtree.com>
2019-02-25 18:20:31 +0530

    MOS-16412 fetch list of doc category and type mapping
    Former-commit-id: f335a890029307729df6b27be23ad8015776c2b5
    Former-commit-id: e73b954f562bc37f80a39f9650b2164e47e394bb

---
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/IndividualTypeRepository.java
@@ -0,0 +1,13 @@
+package io.mosip.kernel.masterdata.repository;
+
+import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
+import io.mosip.kernel.masterdata.entity.IndividualType;
+import io.mosip.kernel.masterdata.entity.id.CodeAndLanguageCodeID;
+
+/**
+ * @author Bal Vikash Sharma
+ *
+ */
+public interface IndividualTypeRepository extends BaseRepository<IndividualType, CodeAndLanguageCodeID>{
+
+}
---
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/IndividualTypeService.java
@@ -0,0 +1,9 @@
+package io.mosip.kernel.masterdata.service;
+
+import io.mosip.kernel.masterdata.dto.getresponse.IndividualTypeResponseDto;
+
+public interface IndividualTypeService {
+
+	public IndividualTypeResponseDto getAllIndividualTypes();
+
+}
