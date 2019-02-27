0bbec942fd71d45617cb0ff997257a127db3d7e1
bantiz <Balvikash.Sharma@mindtree.com>
2019-02-27 19:45:33 +0530

    MOS-12319 get applicant type code
    Former-commit-id: 96c3ee06d5b7541cbf665ab7cc91335cecc091f6
    Former-commit-id: fd5912f489c59c14a1d77f2aa75b9f61c820650d

---
+++ kernel/kernel-applicanttype-service/src/main/java/io/mosip/kernel/applicanttype/controller/ApplicantTypeController.java
@@ -0,0 +1,52 @@
+package io.mosip.kernel.applicanttype.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.mosip.kernel.applicanttype.dto.KeyValues;
+import io.mosip.kernel.applicanttype.dto.request.RequestDTO;
+import io.mosip.kernel.applicanttype.dto.response.ResponseDTO;
+import io.mosip.kernel.applicanttype.service.ApplicantTypeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * 
+ * @author Bal Vikash Sharma
+ *
+ */
+@RestController
+@RequestMapping(value = "/v1.0/applicanttype")
+@Api(value = "This service provide operations on applicant type", tags = { "ApplicantType" })
+@Validated
+public class ApplicantTypeController {
+
+	@Autowired
+	private ApplicantTypeService applicantTypeService;
+
+	@GetMapping
+	@RequestMapping(value = "/getApplicantType")
+	@ApiOperation(value = "Get applicant type for provided queries", notes = "Get applicant type for matching queries", response = String.class)
+	public ResponseEntity<ResponseDTO> getApplicantType(@RequestParam("individualTypeCode") String individualTypeCode,
+			@RequestParam("genderCode") String genderCode, @RequestParam("dateofbirth") String dateofbirth,
+			@RequestParam("biometricAvailable") String biometricAvailable,
+			@RequestParam("languagecode") String languagecode) {
+		RequestDTO dto = new RequestDTO();
+		KeyValues request = new KeyValues();
+		request.getRequest().put("individualTypeCode", individualTypeCode);
+		request.getRequest().put("genderCode", genderCode);
+		request.getRequest().put("dateofbirth", dateofbirth);
+		request.getRequest().put("biometricAvailable", biometricAvailable);
+		request.getRequest().put("languagecode", languagecode);
+		dto.setRequest(request);
+		return new ResponseEntity<>(applicantTypeService.getApplicantType(dto), HttpStatus.OK);
+	}
+}
---
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/ApplicantTypeErrorCode.java
@@ -0,0 +1,29 @@
+package io.mosip.kernel.masterdata.constant;
+
+/**
+ * Error code constants
+ * 
+ * @author Bal Vikash Sharma
+ * @since 1.0.0
+ */
+public enum ApplicantTypeErrorCode {
+	APPLICANT_TYPE_FETCH_EXCEPTION("KER-MSD-XXXX",
+			"Error occurred while fetching Applicant type"), APPLICANT_TYPE_NOT_FOUND_EXCEPTION("KER-MSD-XXXX",
+					"No records found");
+
+	private final String errorCode;
+	private final String errorMessage;
+
+	private ApplicantTypeErrorCode(String errorCode, String errorMessage) {
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+
+	public String getErrorCode() {
+		return this.errorCode;
+	}
+
+	public String getErrorMessage() {
+		return this.errorMessage;
+	}
+}
