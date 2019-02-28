71397bdf701afd29d9ae1a642547d01e274d12c0
rishabhjavad <Rishabh.Keshari@mindtree.com>
2019-02-28 12:08:50 +0530

    MOS-15591 : changes for failed test cases and reponse format change for rest API's https://mosipid.atlassian.net/browse/MOS-15591
    Former-commit-id: 5814e25bc52b15e9d86454e7c356bd2302870553
    Former-commit-id: e701dc844f69daa424cf61da3e84e61dcd08c05d

--- registration-processor/stages/packet-receiver-stage/src/main/java/io/mosip/registration/processor/packet/receiver/dto/PacketReceiverResponseDTO.java
+++ registration-processor/stages/packet-receiver-stage/src/main/java/io/mosip/registration/processor/packet/receiver/dto/PacketReceiverResponseDTO.java
@@ -1,5 +1,7 @@
 package io.mosip.registration.processor.packet.receiver.dto;
 
+import java.util.List;
+
 import io.mosip.registration.processor.core.common.rest.dto.BaseRestResponseDTO;
 import io.mosip.registration.processor.core.common.rest.dto.ErrorDTO;
 import lombok.Data;
@@ -19,7 +21,7 @@ public class PacketReceiverResponseDTO extends BaseRestResponseDTO {
 	/** The response. */
 	private ResponseDTO response;
 	
-	/** The error. */
-	private ErrorDTO error;
+	/** The errors. */
+	private List<ErrorDTO> errors;
 	
 }
