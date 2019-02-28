f7df6d30aa0cf8f339c8bcc65fa4b58fd245afa1
bantiz <Balvikash.Sharma@mindtree.com>
2019-02-28 11:26:51 +0530

    error code updated
    Former-commit-id: e97c43f7c445125bce56a07f07159ef5531b332e
    Former-commit-id: 85f502f28cfa5ce7aac3da731c226d3d5d4a2a5f

--- kernel/kernel-applicanttype-service/src/main/java/io/mosip/kernel/applicanttype/constant/ApplicantTypeErrorCode.java
+++ kernel/kernel-applicanttype-service/src/main/java/io/mosip/kernel/applicanttype/constant/ApplicantTypeErrorCode.java
@@ -8,7 +8,7 @@ package io.mosip.kernel.applicanttype.constant;
  */
 public enum ApplicantTypeErrorCode {
 
-	NO_APPLICANT_FOUND_EXCEPTION("KER-MSD-XXXX", "Applicant type not found"), INVALID_REQUEST_EXCEPTION("KER-MSD-XXXX",
+	NO_APPLICANT_FOUND_EXCEPTION("KER-MSD-147", "Applicant type not found"), INVALID_REQUEST_EXCEPTION("KER-MSD-148",
 			"Invalid query passed to get applicant type");
 
 	private final String errorCode;
