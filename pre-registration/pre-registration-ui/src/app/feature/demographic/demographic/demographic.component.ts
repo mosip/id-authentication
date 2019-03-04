016030478bfc737b7fc7498f731258bfb8530268
Shashank0707 <shashank.agrawal@mindtree.com>
2019-03-04 13:03:03 +0530

    MOS-14523 : postal code and secondary langugae added https://mosipid.atlassian.net/browse/MOS-14523
    Former-commit-id: 53224662de4ce83b35168f8547c20f5cc704e0e1
    Former-commit-id: 97884b1f3d70449e5ea7570070b1e3247cdb8667

--- pre-registration/pre-registration-ui/src/app/feature/demographic/demographic/demographic.component.ts
+++ pre-registration/pre-registration-ui/src/app/feature/demographic/demographic/demographic.component.ts
@@ -658,12 +658,13 @@ export class DemographicComponent implements OnInit, OnDestroy {
       this.step,
       new UserModel(this.preRegId, request, this.regService.getUserFiles(this.step), this.codeValue)
     );
-    // this.sharedService.updateNameList(this.step, {
-    //   ...this.sharedService.getNameList(),
-    //   fullName: this.userForm.controls[this.formControlNames.fullName].value,
-    //   fullNameSecondaryLang: this.formControlValues.fullNameSecondary,
-    //   preRegId: this.preRegId
-    // });
+    this.sharedService.updateNameList(this.step, {
+      fullName: this.userForm.controls[this.formControlNames.fullName].value,
+      fullNameSecondaryLang: this.formControlValues.fullNameSecondary,
+      preRegId: this.preRegId,
+      postalCode: this.formControlValues.postalCode,
+      regDto: this.sharedService.getNameList()[0].regDto
+    });
 
     console.log('NMAE LIST ', this.sharedService.getNameList());
   }
@@ -674,7 +675,8 @@ export class DemographicComponent implements OnInit, OnDestroy {
     this.sharedService.addNameList({
       fullName: this.userForm.controls[this.formControlNames.fullName].value,
       fullNameSecondaryLang: this.formControlValues.fullNameSecondary,
-      preRegId: this.preRegId
+      preRegId: this.preRegId,
+      postalCode: this.formControlValues.postalCode
     });
   }
 
