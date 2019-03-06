22d28de4105f486e28e2ea5891c5503790517fc2
leonamarysusairaja <leonamary.susairaja@mindtree.com>
2019-03-06 18:26:25 +0530

    Updated the json file name in constant file and updated the test scripts MOS-17326
    Former-commit-id: a1488ef02613df14a06f68ec1e9b1a348d42745b
    Former-commit-id: 06149a81044eda4bb4a11a631a22213cce967524

--- registration/registration-services/src/test/java/io/mosip/registration/test/integrationtest/IntegrationTestConstants.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/integrationtest/IntegrationTestConstants.java
@@ -4,12 +4,12 @@ public class IntegrationTestConstants {
 
 	
 	
-public static final String FP_Path_10_FP_NotMatchWithDB="src/test/resources/testData/FingerPrintCaptureServiceData/10_FP_NotMatchWithDB.json";	
-public static final String FpPath_10_FP_Match_RightIndex_WithDB="src/test/resources/testData/FingerPrintCaptureServiceData/10_FP_Match_RightIndex_WithDB.json";
-public static final String FpPath_6_FP_NotMatchWithDB="src/test/resources/testData/FingerPrintCaptureServiceData/6_FP_NotMatchWithDB.json";	
-public static final String FpPath_6_FP_Match_RightIndex_WithDB="src/test/resources/testData/FingerPrintCaptureServiceData/6_FP_Match_RightIndex_WithDB.json";
-public static final String FpPath_Single_FP_Auth_Not_matchWithDB="src/test/resources/testData/FingerPrintCaptureServiceData/Single_FP_Auth_Not_matchWithDB.json";
-public static final String FpPath_Single_FP_Auth_Match_WithDB="src/test/resources/testData/FingerPrintCaptureServiceData/Single_FP_Auth_Match_WithDB.json";
+public static final String FP_Path_10_FP_NotMatchWithDB="src/test/resources/testData/FingerPrintCaptureServiceData/Resident_10_FP_NotMatchWithDB.json";	
+public static final String FpPath_10_FP_Match_RightIndex_WithDB="src/test/resources/testData/FingerPrintCaptureServiceData/Resident_10_FP_Match_RightIndex_WithDB.json";
+public static final String FpPath_6_FP_NotMatchWithDB="src/test/resources/testData/FingerPrintCaptureServiceData/Resident_6_FP_NotMatchWithDB.json";	
+public static final String FpPath_6_FP_Match_RightIndex_WithDB="src/test/resources/testData/FingerPrintCaptureServiceData/Resident_6_FP_Match_RightIndex_WithDB.json";
+public static final String FpPath_Single_FP_Auth_Not_matchWithDB="src/test/resources/testData/FingerPrintCaptureServiceData/OfficerOrSupervisor_Single_FP_Auth_Not_matchWithDB.json";
+public static final String FpPath_Single_FP_Auth_Match_WithDB="src/test/resources/testData/FingerPrintCaptureServiceData/OfficerOrSupervisor_Single_FP_Auth_Match_WithDB.json";
 
 public static String expectedCenterID=null;
 public static String expectedStatinID=null;
@@ -26,11 +26,11 @@ public static final String stationId_val="10011";
 public static final String USER_ON_BOARD_THRESHOLD_LIMIT="USER_ON_BOARD_THRESHOLD_LIMIT";
 public static final String USER_ON_BOARD_THRESHOLD_LIMIT_val="10";
 public static final String userOBErrormsg="Threshold for number of successful authentications not met.";
-public static final String UOB_Validate_null_path="src/test/resources/testData/UserOnboardServiceData/Validate_null.json";
-public static final String UOB_NO_IRIS_NoTHUMB_9="src/test/resources/testData/UserOnboardServiceData/NO_IRIS_NoTHUMB_9.json";
+public static final String UOB_Validate_null_path="src/test/resources/testData/UserOnboardServiceData/OfficerOrSupervisor_No_FP_No_IRIS_No_FACE.json";
+public static final String UOB_NO_IRIS_NoTHUMB_9="src/test/resources/testData/UserOnboardServiceData/OfficerOrSupervisor_NO_IRIS_NoTHUMB_9Count.json";
 public static final String UOB_success_msg="User on-boarded successfully.";
-public static final String IRIS_FP_NoLeftThumbs_path="src/test/resources/testData/UserOnboardServiceData/IRIS_FP_NoLeftThumbs.json";
-public static final String FP_NoIRIS_path="src/test/resources/testData/UserOnboardServiceData/FP_NoIRIS.json";
+public static final String IRIS_FP_NoLeftThumbs_path="src/test/resources/testData/UserOnboardServiceData/OfficerOrSupervisor_IRIS_FP_NoLeftThumbs.json";
+public static final String FP_NoIRIS_path="src/test/resources/testData/UserOnboardServiceData/OfficerOrSupervisor_FP_NoIRIS.json";
 
 public static final String NS_success_msg="Success";
 public static final String NS_msg="Testing SMS service";
