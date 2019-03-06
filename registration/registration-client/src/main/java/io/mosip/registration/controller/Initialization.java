dac4c3f4ce02bf1a6e45f69848da69b8bb43dcaa
Omsaieswar <omsaieswar.mulakaluri@mindtree.com>
2019-03-06 12:18:03 +0530

    MOS-17933:Fixed the issue for start application
    Former-commit-id: 64c01b0e92c9d661e054b9dc0ecca782696d5837
    Former-commit-id: f4eab3c462b5b5e0dd37e092e098770b78c1b56a

--- registration/registration-client/src/main/java/io/mosip/registration/controller/Initialization.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/Initialization.java
@@ -14,7 +14,6 @@ import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.registration.config.AppConfig;
 import io.mosip.registration.constants.RegistrationConstants;
 import io.mosip.registration.controller.auth.LoginController;
-import io.mosip.registration.exception.RegBaseCheckedException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -35,21 +34,27 @@ public class Initialization extends Application {
 
 	private static ApplicationContext applicationContext;
 
-	private static Stage primaryStage;
-
 	@Override
 	public void start(Stage primaryStage) {
-		LOGGER.info("REGISTRATION - LOGIN SCREEN INITILIZATION - REGISTRATIONAPPINITILIZATION", APPLICATION_NAME,
-				APPLICATION_ID, "Login screen initilization "
-						+ new SimpleDateFormat(RegistrationConstants.HH_MM_SS).format(System.currentTimeMillis()));
+		try {
+			LOGGER.info("REGISTRATION - LOGIN SCREEN INITILIZATION - REGISTRATIONAPPINITILIZATION", APPLICATION_NAME,
+					APPLICATION_ID, "Login screen initilization "
+							+ new SimpleDateFormat(RegistrationConstants.HH_MM_SS).format(System.currentTimeMillis()));
 
-		this.primaryStage = primaryStage;
-		LoginController loginController = applicationContext.getBean(LoginController.class);
-		loginController.loadInitialScreen(primaryStage);
+			getPrimaryStage(primaryStage);
+			LoginController loginController = applicationContext.getBean(LoginController.class);
+			loginController.loadInitialScreen(primaryStage);
 
-		LOGGER.info("REGISTRATION - LOGIN SCREEN INITILIZATION - REGISTRATIONAPPINITILIZATION", APPLICATION_NAME,
-				APPLICATION_ID, "Login screen loaded"
-						+ new SimpleDateFormat(RegistrationConstants.HH_MM_SS).format(System.currentTimeMillis()));
+			LOGGER.info("REGISTRATION - LOGIN SCREEN INITILIZATION - REGISTRATIONAPPINITILIZATION", APPLICATION_NAME,
+					APPLICATION_ID, "Login screen loaded"
+							+ new SimpleDateFormat(RegistrationConstants.HH_MM_SS).format(System.currentTimeMillis()));
+		} catch (Exception exception) {
+			LOGGER.error("REGISTRATION - APPLICATION INITILIZATION - REGISTRATIONAPPINITILIZATION", APPLICATION_NAME,
+					APPLICATION_ID,
+					"Application Initilization Error"
+							+ new SimpleDateFormat(RegistrationConstants.HH_MM_SS).format(System.currentTimeMillis())
+							+ ExceptionUtils.getStackTrace(exception));
+		}
 	}
 
 	public static void main(String[] args) {
@@ -94,7 +99,7 @@ public class Initialization extends Application {
 		Initialization.applicationContext = applicationContext;
 	}
 
-	public static Stage getPrimaryStage() {
+	public static Stage getPrimaryStage(Stage primaryStage) {
 		return primaryStage;
 	}
 
