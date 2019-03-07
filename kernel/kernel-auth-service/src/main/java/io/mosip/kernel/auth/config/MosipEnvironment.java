c1140e0bd3da96f4b6e6f98a7d12a5b72c41ec05
Ramadurai Pandian <Ramadurai.Pandian@mindtree.com>
2019-03-07 11:48:51 +0530

    MOS-1000 https://mosipid.atlassian.net/browse/MOS-1000
    Former-commit-id: 2c51d0960219c890b3f6b3d1f314365539211c29
    Former-commit-id: a2f62cab1d19bb48562192618b88cbd539e9fc69

--- kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/config/MosipEnvironment.java
+++ kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/config/MosipEnvironment.java
@@ -26,8 +26,7 @@ public class MosipEnvironment implements EnvironmentAware {
 	private final String generateOtpApi = "otp.manager.api.generate";
 	private final String verifyOtpUserApi = "otp.manager.api.verify";
 
-	private final String otpSenderEmailSvcUrl = "otp.sender.svc.url";
-	private final String otpSenderSmsSvcUrl = "otp.sender.svc.url";
+	private final String otpSenderSvcUrl = "otp.sender.svc.url";
 	private final String otpSenderEmailApi = "otp.sender.api.email.send";
 	private final String otpSenderSmsApi = "otp.sender.api.sms.send";
 
@@ -136,13 +135,10 @@ public class MosipEnvironment implements EnvironmentAware {
 		return environment.getProperty(verifyOtpUserApi);
 	}
 
-	public String getOtpSenderEmailSvcUrl() {
-		return environment.getProperty(otpSenderEmailSvcUrl);
+	public String getOtpSenderSvcUrl() {
+		return environment.getProperty(otpSenderSvcUrl);
 	}
 
-	public String getOtpSenderSmsSvcUrl() {
-		return environment.getProperty(otpSenderSmsSvcUrl);
-	}
 	public String getOtpSenderEmailApi() {
 		return environment.getProperty(otpSenderEmailApi);
 	}
