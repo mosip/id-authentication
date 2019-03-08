8c85248e2f4c005cf0b188cc8cfdc06da0007697
MOSIPRajath <rajath.raghuchandra@mindtree.com>
2019-03-08 11:49:02 +0530

    MOS-18060 : test cases https://mosipid.atlassian.net/browse/MOS-18060
    Former-commit-id: 57b304364b774a49a0e99b263d74dc5937e64e16
    Former-commit-id: 06f5179a65f6865e94fbacf8210f31da0ed34487

--- pre-registration/pre-registration-notification-service/src/main/java/io/mosip/preregistration/notification/service/NotificationService.java
+++ pre-registration/pre-registration-notification-service/src/main/java/io/mosip/preregistration/notification/service/NotificationService.java
@@ -1,26 +1,16 @@
 package io.mosip.preregistration.notification.service;
 
-
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import io.mosip.kernel.core.exception.FileNotFoundException;
 import io.mosip.kernel.core.qrcodegenerator.spi.QrCodeGenerator;
 import io.mosip.kernel.core.util.JsonUtils;
 import io.mosip.kernel.qrcode.generator.zxing.constant.QrVersion;
@@ -59,28 +49,16 @@ public class NotificationService {
 
 	@Autowired
 	private QrCodeGenerator<QrVersion> qrCodeGenerator;
-	
+
 	@Value("${global.config.file}")
 	private String globalFileName;
-	
+
 	@Value("${pre.reg.config.file}")
 	private String preRegFileName;
-	
-	
+
 	@Value("${ui.config.params}")
 	private String uiConfigParams;
-	/**
-	 * Environment instance
-	 */
-	@Autowired
-	private Environment env;
-	
-	/**
-	 * Autowired reference for {@link #RestTemplateBuilder}
-	 */
-	@Autowired
-	private RestTemplateBuilder restTemplateBuilder;
-	
+
 	/**
 	 * Method to send notification.
 	 * 
@@ -94,11 +72,11 @@ public class NotificationService {
 	 */
 	public MainResponseDTO<NotificationDTO> sendNotification(String jsonStirng, String langCode, MultipartFile file) {
 		MainResponseDTO<NotificationDTO> response = new MainResponseDTO<>();
-		
+
 		try {
 			NotificationDTO acknowledgementDTO = (NotificationDTO) JsonUtils
 					.jsonStringToJavaObject(NotificationDTO.class, jsonStirng);
-			
+
 			if (acknowledgementDTO.getMobNum() != null && !acknowledgementDTO.getMobNum().isEmpty()) {
 				notificationUtil.notify("sms", acknowledgementDTO, langCode, file);
 			}
@@ -119,94 +97,65 @@ public class NotificationService {
 		return response;
 	}
 
-	/**This method will generate qrcode
+	/**
+	 * This method will generate qrcode
+	 * 
 	 * @param data
 	 * @return
 	 */
 	public MainResponseDTO<QRCodeResponseDTO> generateQRCode(String data) {
-		byte[] qrCode=null;
-		QRCodeResponseDTO responsedto=new QRCodeResponseDTO();
-		MainResponseDTO<QRCodeResponseDTO> response=new MainResponseDTO<>();
+		byte[] qrCode = null;
+		QRCodeResponseDTO responsedto = new QRCodeResponseDTO();
+		MainResponseDTO<QRCodeResponseDTO> response = new MainResponseDTO<>();
 		try {
-		 qrCode=	qrCodeGenerator.generateQrCode(data, QrVersion.V25);
-		 
-		 responsedto.setQrcode(qrCode);
-		 
+			qrCode = qrCodeGenerator.generateQrCode(data, QrVersion.V25);
+
+			responsedto.setQrcode(qrCode);
+
 		} catch (Exception ex) {
-			
+
 			new NotificationExceptionCatcher().handle(ex);
-		} 
+		}
 		response.setResponse(responsedto);
 		response.setResTime(serviceUtil.getCurrentResponseTime());
 		response.setStatus(Boolean.TRUE);
-		
+
 		return response;
 	}
-	
+
 	/**
-	 * This will return UI related configurations
-	 * return
+	 * This will return UI related configurations return
 	 */
-	public MainResponseDTO<Map<String,String>> getConfig() {
-		MainResponseDTO<Map<String,String>> res= new MainResponseDTO<>();
-		RestTemplate restTemplate = restTemplateBuilder.build();
-		List<String> reqParams= new ArrayList<>();
-		String[] uiParams=uiConfigParams.split(",");
-		for(int i=0;i<uiParams.length;i++) {
-			reqParams.add(uiParams[i]);
-		}
-		Map<String,String> configParams= new HashMap<>();
-		String configServerUri = env.getProperty("spring.cloud.config.uri");
-		String configLabel = env.getProperty("spring.cloud.config.label");
-		String configProfile = env.getProperty("spring.profiles.active");
-		String configAppName = env.getProperty("spring.cloud.config.name");
-		JSONObject result = null;
-		StringBuilder globaluriBuilder = null;
-		StringBuilder preReguriBuilder = null;
+	public MainResponseDTO<Map<String, String>> getConfig() {
+		MainResponseDTO<Map<String, String>> res = new MainResponseDTO<>();
+		List<String> reqParams = new ArrayList<>();
+		Map<String, String> configParams = new HashMap<>();
 		try {
-		if (globalFileName != null  && preRegFileName !=null) {
-			globaluriBuilder = new StringBuilder();
-			preReguriBuilder = new StringBuilder();
-			globaluriBuilder.append(configServerUri + "/").append(configAppName + "/").append(configProfile + "/")
-					.append(configLabel + "/").append(globalFileName);
-			preReguriBuilder.append(configServerUri + "/").append(configAppName + "/").append(configProfile + "/")
-			.append(configLabel + "/").append(preRegFileName);
-		} else {
-			   throw new ConfigFileNotFoundException(ErrorCodes.PRG_ACK_007.name(), ErrorMessages.CONFIG_FILE_NOT_FOUND_EXCEPTION.name());
-		}
-			String globalParam = restTemplate.getForObject(globaluriBuilder.toString(), String.class);
-			String preregParam = restTemplate.getForObject(preReguriBuilder.toString(), String.class);
-			Properties prop1 = parsePropertiesString(globalParam);
-			Properties prop2 = parsePropertiesString(preregParam);
-			result = new JSONObject();
-			for (Entry<Object, Object> e : prop1.entrySet()) {
-				result.put(String.valueOf(e.getKey()), e.getValue());
-				if(reqParams.contains(String.valueOf(e.getKey()))){
-					System.out.println(String.valueOf(e.getKey())+" ----value--- "+ e.getValue());
-					configParams.put(String.valueOf(e.getKey()), e.getValue().toString());
-				}
-				
+			String[] uiParams = uiConfigParams.split(",");
+			for (int i = 0; i < uiParams.length; i++) {
+				reqParams.add(uiParams[i]);
 			}
-			for (Entry<Object, Object> e : prop2.entrySet()) {
-				result.put(String.valueOf(e.getKey()), e.getValue());
-				if(reqParams.contains(String.valueOf(e.getKey()))){
-					System.out.println(String.valueOf(e.getKey())+" ----value--- "+ e.getValue());
-					configParams.put(String.valueOf(e.getKey()), e.getValue().toString());
-				}
-				
+			if (globalFileName != null && preRegFileName != null) {
+				String globalParam = serviceUtil.configRestCall(globalFileName);
+				String preregParam = serviceUtil.configRestCall(preRegFileName);
+				Properties prop1 = serviceUtil.parsePropertiesString(globalParam);
+				Properties prop2 = serviceUtil.parsePropertiesString(preregParam);
+				serviceUtil.getConfigParams(prop1,configParams,reqParams);
+				serviceUtil.getConfigParams(prop2,configParams,reqParams);
+		
+			} else {
+				throw new ConfigFileNotFoundException(ErrorCodes.PRG_ACK_007.name(),
+						ErrorMessages.CONFIG_FILE_NOT_FOUND_EXCEPTION.name());
 			}
-		} catch (RestClientException | IOException e) {
+			
+		} catch (Exception e) {
 			new NotificationExceptionCatcher().handle(e);
 		}
-        res.setResponse(configParams);
-        res.setResTime(serviceUtil.getCurrentResponseTime());
+		res.setResponse(configParams);
+		res.setResTime(serviceUtil.getCurrentResponseTime());
 		res.setStatus(Boolean.TRUE);
 		return res;
 	}
+
 	
-	public Properties parsePropertiesString(String s) throws IOException {
-		final Properties p = new Properties();
-		p.load(new StringReader(s));
-		return p;
-	}
 }
