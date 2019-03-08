b253a4f3082d71874b5297b3c51450ca39786143
Ramadurai Pandian <Ramadurai.Pandian@mindtree.com>
2019-03-08 09:58:42 +0530

    MOS-1000 https://mosipid.atlassian.net/browse/MOS-1000
    Former-commit-id: be07172a0446be665f94aaa6e29971203a8f7473
    Former-commit-id: b67a87f62842735f94f22d71d6aee008c7c82e49

--- kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/service/impl/OTPServiceImpl.java
+++ kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/service/impl/OTPServiceImpl.java
@@ -3,14 +3,14 @@
  */
 package io.mosip.kernel.auth.service.impl;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -20,7 +20,6 @@ import io.mosip.kernel.auth.entities.AuthNResponseDto;
 import io.mosip.kernel.auth.entities.BasicTokenDto;
 import io.mosip.kernel.auth.entities.MosipUserDto;
 import io.mosip.kernel.auth.entities.MosipUserDtoToken;
-
 import io.mosip.kernel.auth.entities.otp.OtpEmailSendRequestDto;
 import io.mosip.kernel.auth.entities.otp.OtpEmailSendResponseDto;
 import io.mosip.kernel.auth.entities.otp.OtpGenerateRequestDto;
@@ -29,8 +28,6 @@ import io.mosip.kernel.auth.entities.otp.OtpSmsSendRequestDto;
 import io.mosip.kernel.auth.entities.otp.OtpSmsSendResponseDto;
 import io.mosip.kernel.auth.entities.otp.OtpTemplateDto;
 import io.mosip.kernel.auth.entities.otp.OtpTemplateResponseDto;
-import io.mosip.kernel.auth.entities.otp.OtpUser;
-import io.mosip.kernel.auth.entities.otp.OtpValidatorResponseDto;
 import io.mosip.kernel.auth.exception.AuthManagerException;
 import io.mosip.kernel.auth.jwtBuilder.TokenGenerator;
 import io.mosip.kernel.auth.service.OTPGenerateService;
@@ -93,7 +90,7 @@ public class OTPServiceImpl implements OTPService {
 		try {
 			String template = null;
 			final String url = mosipEnvironment.getMasterDataTemplateApi()
-					+ mosipEnvironment.getPrimaryLanguage() + mosipEnvironment.getMasterDataOtpTemplate();
+					+"/"+ mosipEnvironment.getPrimaryLanguage() + mosipEnvironment.getMasterDataOtpTemplate();
 
 			OtpTemplateResponseDto otpTemplateResponseDto = restTemplate.getForObject(url,
 					OtpTemplateResponseDto.class);
@@ -163,13 +160,22 @@ public class OTPServiceImpl implements OTPService {
 	public MosipUserDtoToken validateOTP(MosipUserDto mosipUser, String otp) {
 		String key = new OtpGenerateRequestDto(mosipUser).getKey();
 		MosipUserDtoToken mosipUserDtoToken = null;
+		ResponseEntity<String> response = null;
+		String responseBody = null;
 		final String url = mosipEnvironment.getVerifyOtpUserApi();
 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("key", key).queryParam("otp",
 				otp);
-		System.out.println(builder.toUriString());
+		//esponseEntity<OtpValidatorResponseDto> response1 = restTemplate.getForEntity(builder.toUriString(), OtpValidatorResponseDto.class);
 		//ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null,String.class);
-		ResponseEntity<String> response = restTemplate.getForEntity(builder.toUriString(), String.class);
-		String responseBody = response.getBody();
+		try
+		{
+		response = restTemplate.getForEntity(builder.toUriString(), String.class);
+		responseBody = response.getBody();
+		}catch (HttpClientErrorException | HttpServerErrorException e) {
+			String message = e.getResponseBodyAsString();
+			System.out.println(message);
+		
+		}
 		List<ServiceError> validationErrorsList=null;
 		try {
 			validationErrorsList = ExceptionUtils.getServiceErrorList(responseBody);
@@ -186,5 +192,32 @@ public class OTPServiceImpl implements OTPService {
 					basicToken.getRefreshToken(), basicToken.getExpiryTime(), null);
 		}
 		return mosipUserDtoToken;
+	}
+
+	@Override
+	public AuthNResponseDto sendOTPForUin(MosipUserDto mosipUserDto, List<String> otpChannel, String appId) {
+		AuthNResponseDto authNResponseDto = null;
+		OtpEmailSendResponseDto otpEmailSendResponseDto = null;
+		OtpSmsSendResponseDto otpSmsSendResponseDto = null;
+		String emailMessage = null,mobileMessage = null;
+		OtpGenerateResponseDto otpGenerateResponseDto = oTPGenerateService.generateOTP(mosipUserDto);
+		for(String channel:otpChannel)
+		{
+			switch(channel)
+			{
+			case AuthConstant.EMAIL:
+				emailMessage = getOtpEmailMessage(otpGenerateResponseDto, appId);
+				otpEmailSendResponseDto = sendOtpByEmail(emailMessage, mosipUserDto.getMail());
+			case AuthConstant.PHONE:
+				mobileMessage = getOtpSmsMessage(otpGenerateResponseDto, appId);
+				otpSmsSendResponseDto = sendOtpBySms(mobileMessage, mosipUserDto.getMobile());
+			}		
+		}
+		if(otpEmailSendResponseDto!=null && otpSmsSendResponseDto!=null)
+		{
+			AuthNResponseDto authResponseDto = new AuthNResponseDto();
+			authResponseDto.setMessage(AuthConstant.UIN_NOTIFICATION_MESSAGE);
+		}
+		return authNResponseDto;
 	}
 }
