61eaed413af4ee7e917fbe848cc04b75a521e3c8
srinivasanMT <srinivasan.lakshminarasimhan@mindtree.com>
2019-03-11 11:41:07 +0530

    MOS-18174 changes made in syncdata
    Former-commit-id: 6e7cf9daacdfa6b58bb7d53d93b68cd187fb4465
    Former-commit-id: f4c37ba4d6daa03a06bd8242332cbe1419816c11

--- kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/constant/MasterDataErrorCode.java
+++ kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/constant/MasterDataErrorCode.java
@@ -45,7 +45,9 @@ public enum MasterDataErrorCode {
 	REG_CENTER_MACHINE_HISTORY_FETCH_EXCEPTION("KER-SYNC-139","Error occurred while fetching Registration Center Machine History"),
 	REG_CENTER_MACHINE_DEVICE_HISTORY_FETCH_EXCEPTION("KER-SYNC-140","Error occurred while fetching Registration Center Machine Device History"),
 	APPLICANT_VALID_DOCUMENT_FETCH_EXCEPTION("KER-SNC-136","Error occurred while fetching ApplicantValidDocument"),
-	INDIVIDUAL_TYPE_FETCH_EXCEPTION("KER-SNC-137","Error occurred while fetching Individual types");
+	INDIVIDUAL_TYPE_FETCH_EXCEPTION("KER-SNC-137","Error occurred while fetching Individual types"),
+	INVALID_MAC_OR_SERIAL_NUMBER("KER-SNC-138","Mac-Address and/or Serial Number does not exist	"),
+	REG_CENTER_UPDATED("KER-SNC-139","Registration Center has been updated for the received Machine ID");
 	private final String errorCode;
 	private final String errorMessage;
 
