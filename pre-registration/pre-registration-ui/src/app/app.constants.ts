232613a06d3226c4ed9c7b24a1d3534133cbae36
Shashank0707 <shashank.agrawal@mindtree.com>
2019-03-13 16:23:18 +0530

    MOS-16119 : ID Defination https://mosipid.atlassian.net/browse/MOS-16119
    Former-commit-id: 535d8ec677e219aafab4d34e74da308969b88f76
    Former-commit-id: a7617e0292246cc50c749ff81801462696a699a5

--- pre-registration/pre-registration-ui/src/app/app.constants.ts
+++ pre-registration/pre-registration-ui/src/app/app.constants.ts
@@ -1,7 +1,7 @@
 export const NUMBER_PATTERN = '^[0-9]+[0-9]*$';
-export const MOBILE_PATTERN = '^([6-9]{1})([0-9]{9})$';
+// export const MOBILE_PATTERN = '^([6-9]{1})([0-9]{9})$';
 export const TEXT_PATTERN = '^[a-zA-Z ]*$';
-export const CNIE_PATTERN = '^([0-9]{10,30})$';
+// export const CNIE_PATTERN = '^([0-9]{10,30})$';
 export const COUNTRY_NAME = 'Morroco';
 export const COUNTRY_HIERARCHY = 'Country';
 export const VERSION = '1.0';
@@ -24,19 +24,19 @@ export const LANGUAGE_CODE = {
   secondaryKeyboardLang: 'ar'
 };
 
-export const APPEND_URL = {
-  location_metadata: 'v1.0/locations/locationhierarchy/',
-  location_immediate_children: 'v1.0/locations/immediatechildren/',
-  get_applicant: 'demographic/applications/details',
-  applicants: 'demographic/applications',
-  location: 'masterdata/',
-  gender: 'masterdata/v1.0/gendertypes',
-  transliteration: 'translitration/translitrate',
-  applicantType: 'applicanttype/v1.0/applicanttype/',
-  validDocument: 'v1.0/applicanttype/',
-  getApplicantType: 'getApplicantType'
-  }; 
-   
+export const APPEND_URL = {
+  location_metadata: 'v1.0/locations/locationhierarchy/',
+  location_immediate_children: 'v1.0/locations/immediatechildren/',
+  get_applicant: 'demographic/applications/details',
+  applicants: 'demographic/applications',
+  location: 'masterdata/',
+  gender: 'masterdata/v1.0/gendertypes',
+  transliteration: 'translitration/translitrate',
+  applicantType: 'applicanttype/v1.0/applicanttype/',
+  validDocument: 'v1.0/applicanttype/',
+  getApplicantType: 'getApplicantType'
+};
+
 export const PARAMS_KEYS = {
   getUsers: 'user_id',
   getUser: PRE_REGISTRATION_ID,
@@ -99,16 +99,16 @@ export const virtual_keyboard_languages = {
 };
 
 export const languageMapping = {
-  eng : {
+  eng: {
     langName: 'English'
   },
-  ara : {
+  ara: {
     langName: 'Arabic'
   },
-  fra : {
+  fra: {
     langName: 'French'
   }
-}
+};
 
 export const DOCUMENT_UPLOAD_REQUEST_DOCUMENT_KEY = 'file';
 export const DOCUMENT_UPLOAD_REQUEST_DTO_KEY = 'Document request';
