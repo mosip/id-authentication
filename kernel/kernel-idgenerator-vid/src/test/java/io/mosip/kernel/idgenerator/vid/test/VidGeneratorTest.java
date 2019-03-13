b44918eb2a4809a06fd46b6b31e8d7b645cc7c1e
Mosip-Ritesh <Ritesh.Sinha@mindtree.com>
2019-03-13 10:11:07 +0530

    MOS-18103 Vid generator unit test methods modified
    Former-commit-id: 53e8007f96acf0c2c8582a5e5e544242eed5f9f0
    Former-commit-id: f7f1ce74b26f934f73f7d8b2c042b393bd3cc591

--- kernel/kernel-idgenerator-vid/src/test/java/io/mosip/kernel/idgenerator/vid/test/VidGeneratorTest.java
+++ kernel/kernel-idgenerator-vid/src/test/java/io/mosip/kernel/idgenerator/vid/test/VidGeneratorTest.java
@@ -3,19 +3,32 @@ package io.mosip.kernel.idgenerator.vid.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.mosip.kernel.core.dataaccess.exception.DataAccessLayerException;
 import io.mosip.kernel.core.idgenerator.spi.VidGenerator;
 import io.mosip.kernel.core.idvalidator.spi.VidValidator;
+import io.mosip.kernel.idgenerator.vid.entity.Vid;
+import io.mosip.kernel.idgenerator.vid.exception.VidException;
+import io.mosip.kernel.idgenerator.vid.repository.VidRepository;
 
 /**
- * @author M1043226
+ * Test class for vid generator.
+ * 
+ * @author Ritesh Sinha
  * @since 1.0.0
  *
  */
@@ -26,27 +39,86 @@ public class VidGeneratorTest {
 	@Value("${mosip.kernel.vid.length}")
 	private int vidLength;
 
+	@Value("${mosip.kernel.vid.test.random-value-number}")
+	private String random;
+
+	@Value("${mosip.kernel.vid.test.random-counter-number}")
+	private String key;
+
 	@Autowired
 	VidGenerator<String> vidGenerator;
 
 	@Autowired
 	private VidValidator<String> vidValidator;
 
-	@Test
-	public void generateIdNullTest() {
-		String result = vidGenerator.generateId();
-		assertNotNull(result);
+	@MockBean
+	private VidRepository repository;
+
+	List<Vid> listOfEntity = null;
+
+	@Before
+	public void setUp() {
+
+		listOfEntity = new ArrayList<>();
+		Vid entity = new Vid();
+		entity.setRandomValue(random);
+		entity.setSequenceCounter(key);
+		listOfEntity.add(entity);
 	}
 
 	@Test
-	public void generateIdLengthTest() {
-		String result = vidGenerator.generateId();
-		assertEquals(vidLength, result.length());
+	public void notNullTest() {
+
+		when(repository.findRandomValues()).thenReturn(listOfEntity);
+
+		assertNotNull(vidGenerator.generateId());
 	}
 
-	//@Test
-	public void generateIdTest() {
-		String result = vidGenerator.generateId();
-		assertTrue(vidValidator.validateId(result));
+	@Test
+	public void vidLengthTest() {
+
+		when(repository.findRandomValues()).thenReturn(listOfEntity);
+
+		assertEquals(vidLength, vidGenerator.generateId().length());
 	}
+
+	@Test
+	public void vidValidationTest() {
+
+		when(repository.findRandomValues()).thenReturn(listOfEntity);
+
+		assertTrue(vidValidator.validateId(vidGenerator.generateId()));
+	}
+
+	@Test(expected = VidException.class)
+	public void randomValueFetchExceptionTest() {
+
+		when(repository.findRandomValues()).thenThrow(new DataAccessLayerException("errorCode", "errorMessage", null));
+
+		vidGenerator.generateId();
+	}
+
+	@Test(expected = VidException.class)
+	public void randomValuesUpdateExceptionTest() {
+
+		when(repository.findRandomValues()).thenReturn(listOfEntity);
+
+		when(repository.updateCounterValue(Mockito.any(), Mockito.any()))
+				.thenThrow(new DataAccessLayerException("errorCode", "errorMessage", null));
+
+		vidGenerator.generateId();
+	}
+
+	@Test(expected = VidException.class)
+	public void firstRandomNumberGenerationExceptionTest() {
+		listOfEntity.clear();
+
+		when(repository.findRandomValues()).thenReturn(listOfEntity);
+
+		when(repository.save(Mockito.any())).thenThrow(new DataAccessLayerException("errorCode", "errorMessage", null));
+
+		vidGenerator.generateId();
+
+	}
+
 }
