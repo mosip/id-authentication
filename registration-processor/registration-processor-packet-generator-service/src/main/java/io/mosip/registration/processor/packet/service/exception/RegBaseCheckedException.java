754b1f6dea940ac5627dba3e4273e6394a146fd5
RanjithaSiddegowda <43336947+RanjithaSiddegowda@users.noreply.github.com>
2019-03-18 10:32:23 +0530

    MOS-18182 Code merge https://mosipid.atlassian.net/browse/MOS-18182
    Former-commit-id: e62b5f90a900629ee653851d9983f30acf31e4eb
    Former-commit-id: b4aac7f325c8860731e8239d704853ae8f7fe4f0

--- registration-processor/registration-processor-packet-generator-service/src/main/java/io/mosip/registration/processor/packet/service/exception/RegBaseCheckedException.java
+++ registration-processor/registration-processor-packet-generator-service/src/main/java/io/mosip/registration/processor/packet/service/exception/RegBaseCheckedException.java
@@ -1,17 +1,12 @@
 package io.mosip.registration.processor.packet.service.exception;
 
-import static io.mosip.registration.processor.packet.service.constants.RegistrationConstants.APPLICATION_ID;
-import static io.mosip.registration.processor.packet.service.constants.RegistrationConstants.APPLICATION_NAME;
-
 import io.mosip.kernel.core.exception.BaseCheckedException;
-import io.mosip.kernel.core.exception.ExceptionUtils;
-import io.mosip.kernel.core.logger.spi.Logger;
-
+import io.mosip.registration.processor.core.exception.util.PlatformErrorMessages;
 
 /**
  * The class to handle all the checked exception in REG
  * 
- * @author Balaji Sridharan
+ * @author Sowmya
  * @since 1.0.0
  *
  */
@@ -21,10 +16,10 @@ public class RegBaseCheckedException extends BaseCheckedException {
 	 * Serializable Version Id
 	 */
 	private static final long serialVersionUID = 7381314129809012005L;
+
 	/**
 	 * Instance of {@link Logger}
 	 */
-	
 
 	/**
 	 * Constructs a new checked exception
@@ -33,33 +28,28 @@ public class RegBaseCheckedException extends BaseCheckedException {
 		super();
 	}
 
-	/**
-	 * Constructs a new checked exception with the specified detail message and
-	 * error code.
-	 * 
-	 * @param errorCode
-	 *            the error code
-	 * @param errorMessage
-	 *            the detail message.
-	 */
-	public RegBaseCheckedException(String errorCode, String errorMessage) {
-		super(errorCode, errorMessage);
-		
+	public RegBaseCheckedException(PlatformErrorMessages exceptionConstant, Throwable rootCause) {
+		super(exceptionConstant.getCode(), exceptionConstant.getMessage(), rootCause);
 	}
 
-	/**
-	 * Constructs a new checked exception with the specified detail message and
-	 * error code.
-	 * 
-	 * @param errorCode
-	 *            the error code
-	 * @param errorMessage
-	 *            the detail message
-	 * @param throwable
-	 *            the specified cause
-	 */
-	public RegBaseCheckedException(String errorCode, String errorMessage, Throwable throwable) {
-		super(errorCode, errorMessage, throwable);
-		
+	public RegBaseCheckedException(String errorCode, String errorMessage) {
+		super(errorCode, errorMessage);
 	}
+
+	public RegBaseCheckedException(PlatformErrorMessages exceptionConstant, String message, Throwable rootCause) {
+		super(exceptionConstant.getCode(), message, rootCause);
+	}
+
+	public RegBaseCheckedException(String errorCode, String errorMessage, Throwable rootCause) {
+		super(errorCode, errorMessage, rootCause);
+	}
+
+	public RegBaseCheckedException(String errorCode, String errorMessage, Throwable rootCause, String id) {
+		super(errorCode, errorMessage, rootCause);
+	}
+
+	public RegBaseCheckedException(PlatformErrorMessages exceptionConstant) {
+		this(exceptionConstant.getCode(), exceptionConstant.getMessage());
+	}
+
 }
