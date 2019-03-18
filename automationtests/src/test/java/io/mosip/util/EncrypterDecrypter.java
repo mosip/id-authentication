3e13071d51ed472b835df057fea9500c73d97bc3
Tabish Khan <M1047227@mindtree.com>
2019-03-18 09:59:58 +0530

    MOS-17700: Added logic to read from properties file and generate invalid packets https://mosipid.atlassian.net/browse/MOS-17700
    Former-commit-id: a6152ff1a404b940f07099cdb4eb02b89399d275
    Former-commit-id: b4dd5f9ce445805b413003bf3ebba8756b45482b

--- automationtests/src/test/java/io/mosip/util/EncrypterDecrypter.java
+++ automationtests/src/test/java/io/mosip/util/EncrypterDecrypter.java
@@ -5,6 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.commons.io.FileDeleteStrategy;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
 import io.mosip.service.ApplicationLibrary;
 import io.restassured.response.Response;
 import net.lingala.zip4j.core.ZipFile;
@@ -12,25 +16,28 @@ import net.lingala.zip4j.exception.ZipException;
 
 
 public class EncrypterDecrypter {
+	private static Logger logger = Logger.getLogger(EncrypterDecrypter.class);
 	static ApplicationLibrary applnMethods=new ApplicationLibrary();
 	private final String decrypterURL="http://104.211.220.190:9096/registrationprocessor-utility/v0.1/registration-processor/util/decryptPacket";
 	private final String encrypterURL="http://104.211.220.190:9096/registrationprocessor-utility/v0.1/registration-processor/util/encryptPacket";
 	
 	public File decryptFile(File f,String path) throws IOException, ZipException {
-		System.out.println(path);
+		logger.info(path);
 		Response response=applnMethods.putFile(f,decrypterURL);
-		System.out.println(response.contentType());
+		
 		if(response.contentType().equals("application/zip")) {
-		System.out.println(response.asByteArray());
+		
 		try (FileOutputStream fileOuputStream = new FileOutputStream(path)){
 			    fileOuputStream.write(response.asByteArray());
+			    fileOuputStream.close();
 			 }
 		 ZipFile zipFile = new ZipFile(path);
          zipFile.extractAll(path.substring(0, path.lastIndexOf('.')));
          File extractedFile=new File(path.substring(0, path.lastIndexOf('.')));
+         
 		return extractedFile;
 		} else {
-			System.out.println("Utility Service Is Not Running");
+			logger.info("Utility Service Is Not Running");
 		}
 		return null;
 	}
@@ -39,31 +46,62 @@ public class EncrypterDecrypter {
 	
 		File folder = new File(destinationPath);
 		folder.mkdirs();
-		System.out.println("File name is :: "+ sourcePath+"/"+f.getName());
-		System.out.println("Inside EncryptFile Method");
-		System.out.println("File Path Is :: "+destinationPath+"/"+f.getName()+".zip");
+		logger.info("File name is :: "+ sourcePath+"/"+f.getName());
+		logger.info("Inside EncryptFile Method");
+		logger.info("File Path Is :: "+destinationPath+"/"+f.getName()+".zip");
 		 org.zeroturnaround.zip.ZipUtil.pack(new File(sourcePath+"/"+f.getName()),new File(destinationPath+"/"+f.getName()+".zip"));
 		  File file1=new File(destinationPath+"/"+f.getName()+".zip");
 		  Response response=applnMethods.putDecryptedFile(file1,encrypterURL);
-		  System.out.println("Response from encrypter URL is  :: "+response.contentType());
+		 logger.info("Response from encrypter URL is  :: "+response.contentType());
 		  try (FileOutputStream fileOuputStream = new FileOutputStream(destinationPath+"/"+f.getName()+".zip")){
 			    fileOuputStream.write(response.asByteArray());
-			    System.out.println("Successfully Wrote The file");
+			    fileOuputStream.close();
+			    logger.info("Successfully Wrote The file");
 			 }
 		  
 	}
-	public void destroyFiles(String filePath) throws ZipException, FileNotFoundException, IOException {
-		System.out.println("Destroying Files");
-		File file = new File(filePath);
-		File zipFile=new File(filePath+".zip");
-		System.out.println(filePath);
-		System.out.println(filePath+".zip");
-		file.delete();
+	public void destroyFiles(File file) throws IOException {
+//		if(!file.canWrite()) {
+		logger.info("Destroying Files");
+
+		File[] listOfFiles=file.listFiles();
+		
+		for(File f: listOfFiles) {
+			if(f.isFile()) {
+				try {
+					FileDeleteStrategy.FORCE.delete(f);
+					logger.info("File Was Deleted");
+				} catch (Exception e) {
+					logger.info(f.getName()+" Was Not Deleted");
+					e.printStackTrace();
+				}
+			} else if(f.isDirectory()) {
+				try {
+					FileUtils.deleteDirectory(f);
+					logger.info("Folder Was Deleted");
+				} catch (Exception e) {
+					logger.info(f.getName()+"  Was Not Deleted");
+				}
+			}
+		}
+		try {
+			FileUtils.deleteDirectory(file);
+			logger.info("Decrypted File Was Deleted");
+		} catch (Exception e) {
+			logger.info("Decrypted File Has Some Files In It");
+		}
+		}/*else {
+			logger.info("File Is Being Used By"+file.setWritable(true));
+		}*/
+	
+	public void revertPacketToValid(String filePath) throws FileNotFoundException, IOException {
+		File zipFile = new File(filePath+".zip");
 		 Response response=applnMethods.putDecryptedFile(zipFile,encrypterURL);
-		  System.out.println("Response from encrypter URL is  :: "+response.contentType());
+		 logger.info("Response from encrypter URL is  :: "+response.contentType());
 		  try (FileOutputStream fileOuputStream = new FileOutputStream(filePath+".zip")){
 			    fileOuputStream.write(response.asByteArray());
-			    System.out.println("Successfully Wrote The file");
+			    fileOuputStream.close();
+			  logger.info("Successfully Wrote The file");
 			 }
 	}
 }
--- automationtests/src/test/java/io/mosip/util/TweakRegProcPackets.java
+++ automationtests/src/test/java/io/mosip/util/TweakRegProcPackets.java
@@ -1,10 +1,13 @@
 package io.mosip.util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -13,12 +16,13 @@ import org.json.simple.parser.ParseException;
 import net.lingala.zip4j.exception.ZipException;
 
 public class TweakRegProcPackets {
-	
+	private static Logger logger = Logger.getLogger(TweakRegProcPackets.class);
+	static String filesToBeDestroyed=null;
 	EncrypterDecrypter encryptDecrypt=new EncrypterDecrypter();
-	public  void tweakFile(String testCaseName,String parameterToBeChanged) throws IOException, ZipException {
+	public  void tweakFile(String testCaseName,String parameterToBeChanged,String parameterValue) throws IOException, ZipException {
 		File decryptedFile=null;
 		JSONObject metaInfo=null;
-		String filesToBeDestroyed=null;
+		
 		String configPath=System.getProperty("user.dir")+"/"+"src/test/resources/regProc/Packets/ValidPackets/";
 		String invalidPacketsPath=System.getProperty("user.dir")+"/"+"src/test/resources/regProc/Packets/InvalidPackets/"+testCaseName;
 		File file=new File(configPath);
@@ -32,73 +36,64 @@ public class TweakRegProcPackets {
 				
 				if(info.getName().toLowerCase().equals("packet_meta_info.json")) {
 					try {
-						metaInfo=(JSONObject) new JSONParser().parse(new FileReader(info.getPath()));
-						System.out.println("Inside Info Is  :: "+ metaInfo.toJSONString());
+						FileReader metaFileReader=new FileReader(info.getPath());
+						metaInfo=(JSONObject) new JSONParser().parse(metaFileReader);
+						metaFileReader.close();
 					} catch (IOException | ParseException e) {
 						e.printStackTrace();
 					}	
-					System.out.println("Initial Info is :: "+ metaInfo);
 					JSONObject identity= (JSONObject) metaInfo.get("identity");
 					JSONArray metaData=(JSONArray) identity.get("metaData");
-					JSONArray updatedData=tweakParameter(metaData,parameterToBeChanged);
+					JSONArray updatedData=tweakFile(metaData, parameterToBeChanged, parameterValue);
 					metaInfo.put("identity", identity);
+					//System.out.println("Inside info is :: "+ metaInfo);
 					try (FileWriter updatedFile = new FileWriter(info.getAbsolutePath())) 
 		            {
 		                try {
 							updatedFile.write(metaInfo.toString());
+							updatedFile.close();
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
-		                System.out.println("Successfully updated json object to file...!!");
+		               logger.info("Successfully updated json object to file...!!");
 		               
 		            } catch (IOException e1) {
 						
 						e1.printStackTrace();
 					}
 				}
-			
+				
 			}
 		}
 		}
 		encryptDecrypt.encryptFile(decryptedFile,configPath,invalidPacketsPath);
-		encryptDecrypt.destroyFiles(filesToBeDestroyed);
+		encryptDecrypt.revertPacketToValid(filesToBeDestroyed);
 	}
-	
-	public JSONArray tweakParameter(JSONArray metaData, String parameter) {
-		switch (parameter) {
-		case "registrationId":
-				return tweakRegID(metaData);
-		case "centerId":
-			return tweakCentreID(metaData);
-	
-		default:
-			break;
-		}
-		return metaData;
-		
-	}
-	public JSONArray tweakRegID(JSONArray metaData) {
-		
+	public JSONArray tweakFile(JSONArray metaData, String parameter, String value) {
 		for(int i=0;i<metaData.size();i++) {
 			JSONObject labels=(JSONObject) metaData.get(i);
-			if(labels.get("label").equals("registrationId")) {
-				labels.put("value", "12345");
-			}
+			if(labels.get("label").equals(parameter))
+				labels.put("value",value);
 		}
+		logger.info(metaData);
 		return metaData;
 	}
-	public  JSONArray tweakCentreID(JSONArray metaData) {
-		for(int i=0;i<metaData.size();i++) {
-			JSONObject labels=(JSONObject) metaData.get(i);
-			if(labels.get("label").equals("centerId")) {
-				labels.put("value", "ABCD");
-			}
-		}
-		return metaData;
-	}
-	
-	public static void main(String[] args) throws IOException, ZipException {
+	public void invalidPacketGenerator(String propertyFiles) throws FileNotFoundException, IOException, ZipException, InterruptedException {
+		EncrypterDecrypter encryptDecrypt=new EncrypterDecrypter();
 		TweakRegProcPackets e=new TweakRegProcPackets();
-		e.tweakFile("InvalidCentreID","centerId");
+		Properties prop=new Properties();
+		String propertyFilePath=System.getProperty("user.dir")+"/src/config/"+propertyFiles;
+		prop.load(new FileReader(new File(propertyFilePath)));
+		for(String property:prop.stringPropertyNames() ) {
+			logger.info("invalid"+property);
+			logger.info(prop.getProperty(property));
+		e.tweakFile("invalid"+property,property,prop.getProperty(property));
+		}
+		File decryptedPacket=new File(filesToBeDestroyed);
+		encryptDecrypt.destroyFiles(decryptedPacket);
+	}
+	public static void main(String[] args) throws IOException, ZipException, InterruptedException {
+		TweakRegProcPackets e=new TweakRegProcPackets();
+		e.invalidPacketGenerator("packetProperties.properties");
 	}
 }
