60226cb712e51a3c5165001173a7c679c0614649
Pranav Kumar (IN1740) <M1041740@mindtree.com>
2019-03-22 15:25:32 +0530

    MOS-15022 : Added poc for different camel flows
    Former-commit-id: 68352b801e33ba6c31f81af716a45b3d160971b7
    Former-commit-id: ede09c569f6780bfee13a71bdc7bb3b69ee6dd0b

---
+++ registration-processor/proof-of-concepts/camel-flow/camel-bridge/src/main/java/com/mindtree/camel_bridge/App.java
@@ -0,0 +1,13 @@
+package com.mindtree.camel_bridge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MosipBridgeFactory.getEventBus();
+    }
+}
