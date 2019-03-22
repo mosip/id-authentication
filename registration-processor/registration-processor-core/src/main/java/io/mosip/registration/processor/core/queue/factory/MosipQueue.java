943e0d2a00010b1c451cbf2e4a64c8279e2475d8
RanjithaSiddegowda <43336947+RanjithaSiddegowda@users.noreply.github.com>
2019-03-22 17:04:48 +0530

    MOS-20188 Added consume listener to ActiveMq implementation https://mosipid.atlassian.net/browse/MOS-20188
    Former-commit-id: f85bef8175d62c2fae790f9dded087d94439150d
    Former-commit-id: 1bc6e3283a49dbfe99c88b999ffe38ba456e6eb3

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/queue/factory/MosipQueue.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/queue/factory/MosipQueue.java
@@ -3,5 +3,7 @@ package io.mosip.registration.processor.core.queue.factory;
 public abstract class MosipQueue{
 	
 	public abstract void createConnection(String username, String password, String brokerUrl);
+	
+	public abstract String getQueueName();
 
 }
