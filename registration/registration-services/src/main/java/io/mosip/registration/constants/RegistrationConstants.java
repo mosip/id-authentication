ba536d40f9b4e901896a122876a8c9ceffd0cb67
Dinesh Ashokan <Dinesh.Ashokan@mindtree.com>
2019-03-25 11:40:16 +0530

    MOS-19869 - User onboard resource path changes
    Former-commit-id: 7ea992e37f527631b7d0c1d8543a0fc95963a827
    Former-commit-id: 855144f03a6b14cdd582a53fde3529b3981440f1

--- registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -830,13 +830,13 @@ public class RegistrationConstants {
 	public static final String LEFT_HAND = "Left hand ";
 	public static final String RIGHT_HAND = "Right hand ";
 	public static final String[] LEFTHAND_SEGMNTD_FILE_PATHS_USERONBOARD = new String[] {
-			"/fingerprints/UserOnboard/leftHand/leftIndex/", "/fingerprints/UserOnboard/leftHand/leftLittle/",
-			"/fingerprints/UserOnboard/leftHand/leftMiddle/", "/fingerprints/UserOnboard/leftHand/leftRing/" };
+			"/UserOnboard/leftHand/leftIndex/", "/UserOnboard/leftHand/leftLittle/",
+			"/UserOnboard/leftHand/leftMiddle/", "/UserOnboard/leftHand/leftRing/" };
 	public static final String[] RIGHTHAND_SEGMNTD_FILE_PATHS_USERONBOARD = new String[] {
-			"/fingerprints/UserOnboard/rightHand/rightIndex/", "/fingerprints/UserOnboard/rightHand/rightLittle/",
-			"/fingerprints/UserOnboard/rightHand/rightMiddle/", "/fingerprints/UserOnboard/rightHand/rightRing/" };
+			"/UserOnboard/rightHand/rightIndex/", "/UserOnboard/rightHand/rightLittle/",
+			"/UserOnboard/rightHand/rightMiddle/", "/UserOnboard/rightHand/rightRing/" };
 	public static final String[] THUMBS_SEGMNTD_FILE_PATHS_USERONBOARD = new String[] {
-			"/fingerprints/UserOnboard/thumb/leftThumb/", "/fingerprints/UserOnboard/thumb/rightThumb/" };
+			"/UserOnboard/thumb/leftThumb/", "/UserOnboard/thumb/rightThumb/" };
 
 	/** Exception codes **/
 	private static final String REG_SERVICE_CODE = "REG-SER-";
