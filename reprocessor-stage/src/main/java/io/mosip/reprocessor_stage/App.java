5c080ff465eb00502f38a5fde25a2096f4e00f54
Pranav Kumar (IN1740) <M1041740@mindtree.com>
2019-03-26 15:19:02 +0530

    MOS-20400
    Former-commit-id: e9f5a5d473d800eaf456563d99528a3b1f8949f8
    Former-commit-id: 3fa1ffdce6c45c50f88c3b8ac6312ebfacfdeb9c

---
+++ reprocessor-stage/src/main/java/io/mosip/reprocessor_stage/App.java
@@ -0,0 +1,13 @@
+package io.mosip.reprocessor_stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
