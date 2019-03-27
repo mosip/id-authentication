ef691c7445db8e90ce7fc2ab9eada922b0b60328
NagalakshmiNithyanandan <Nagalakshmi.Nithyanandan@mindtree.com>
2019-03-27 16:24:14 +0530

    MOS-21048
    Former-commit-id: f3098a3a692b285688d973352778252b9a5d618b
    Former-commit-id: d918ec64f0b6c1c0aca3199783fc53644af7ebaa

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/code/RegistrationExceptionTypeCode.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/code/RegistrationExceptionTypeCode.java
@@ -1,6 +1,6 @@
 package io.mosip.registration.processor.core.code;
 
-public enum ExceptionTypeCode {
+public enum RegistrationExceptionTypeCode {
 
 	TABLE_NOT_ACCESSIBLE_EXCEPTION, INTERNAL_SERVER_ERROR, FSADAPTER_EXCEPTION, PACKET_NOT_FOUND_EXCEPTION, DATA_ACCESS_EXCEPTION,
 
