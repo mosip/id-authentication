4ec4e13e3f7506d200226ee80f0d4e371363d1ff
Tabish Khan <M1047227@mindtree.com>
2019-03-27 19:04:10 +0530

    MOS-21041 : Updated Methods to encrypt the invalid packets https://mosipid.atlassian.net/browse/MOS-21041
    Former-commit-id: be95ab1ba894f10102b200d376ac6038c385d477
    Former-commit-id: f4b340d45393f1c51e81f362d307133f9d22f29d

--- automationtests/src/test/java/io/mosip/util/EncrypterDecrypter.java
+++ automationtests/src/test/java/io/mosip/util/EncrypterDecrypter.java
@@ -83,7 +83,7 @@ public class EncrypterDecrypter {
 		 org.zeroturnaround.zip.ZipUtil.pack(new File(sourcePath+"/"+f.getName()),new File(destinationPath+"/"+fileName+".zip"));
 		  File file1=new File(destinationPath+"/"+fileName+".zip");
 		  JSONObject decryptedFileBody=new JSONObject();
-		  decryptedFileBody=generateCryptographicData(file1);
+		  decryptedFileBody=generateCryptographicDataEncryption(file1);
 		  Response response=applnMethods.postRequestToDecrypt(decryptedFileBody, encrypterURL);
 		  try {
 
@@ -139,7 +139,7 @@ public class EncrypterDecrypter {
 	public void revertPacketToValid(String filePath) throws FileNotFoundException, IOException {
 		File zipFile = new File(filePath+".zip");
 		JSONObject cryptographicRequest=new JSONObject();
-		cryptographicRequest=generateCryptographicData(zipFile);
+		cryptographicRequest=generateCryptographicDataEncryption(zipFile);
 		 Response response=applnMethods.postRequestToDecrypt(cryptographicRequest, encrypterURL);
 		 try {
 				JSONObject data= (JSONObject) new JSONParser().parse(response.asString());
@@ -166,6 +166,50 @@ public class EncrypterDecrypter {
 			byte [] fileInBytes=FileUtils.readFileToByteArray(file);
 			//String encryptedPacketString= Base64.getEncoder().encodeToString(fileInBytes);
 			String encryptedPacketString = IOUtils.toString(encryptedPacket, "UTF-8");
+			encryptedPacketString=encryptedPacketString.replaceAll("\\s+","");
+			String registrationId=file.getName().substring(0,file.getName().lastIndexOf('.'));
+			String packetCreatedDateTime = registrationId.substring(registrationId.length() - 14);
+			int n = 100 + new Random().nextInt(900);
+			String milliseconds = String.valueOf(n);
+			encryptedPacket.close();
+			Date date = formatter.parse(packetCreatedDateTime.substring(0, 8) + "T"
+					+ packetCreatedDateTime.substring(packetCreatedDateTime.length() - 6)+milliseconds);
+			LocalDateTime ldt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+			decrypterDto.setApplicationId(applicationId);
+			decrypterDto.setReferenceId(centerId);
+			decrypterDto.setData(encryptedPacketString);
+			decrypterDto.setTimeStamp(ldt);
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.registerModule(new JavaTimeModule());
+			cryptographicRequest.put("applicationId", applicationId);
+			cryptographicRequest.put("data", encryptedPacketString);
+			cryptographicRequest.put("referenceId", centerId);
+			cryptographicRequest.put("timeStamp",decrypterDto.getTimeStamp().atOffset(ZoneOffset.UTC).toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cryptographicRequest;
+	}
+	
+	public JSONObject generateCryptographicDataEncryption(File file) {
+		JSONObject cryptographicRequest=new JSONObject();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd'T'HHmmssSSS");
+		InputStream encryptedPacket=null;
+		DecrypterDto decrypterDto=new DecrypterDto();
+		String centerId=file.getName().substring(0,5);
+		try {
+			encryptedPacket=new FileInputStream(file);
+			byte [] fileInBytes=FileUtils.readFileToByteArray(file);
+			String encryptedPacketString= Base64.getEncoder().encodeToString(fileInBytes);
+			//String encryptedPacketString = IOUtils.toString(encryptedPacket, "UTF-8");
 			encryptedPacketString=encryptedPacketString.replaceAll("\\s+","");
 			String registrationId=file.getName().substring(0,file.getName().lastIndexOf('.'));
 			String packetCreatedDateTime = registrationId.substring(registrationId.length() - 14);
