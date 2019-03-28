7581d18d22b04dfb1efdd727ebe85c7736abd11a
NagalakshmiNithyanandan <Nagalakshmi.Nithyanandan@mindtree.com>
2019-03-28 14:18:31 +0530

    MOS-21048 RegistrationStatusMapper updated
    Former-commit-id: d95724c3d99822a29940660296c6ae83545758ff
    Former-commit-id: 7e40fdc72e2a1715f4f9c4068d771be470e62446

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/util/RegistrationStatusMapperUtil.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/util/RegistrationStatusMapperUtil.java
@@ -5,7 +5,7 @@ import java.util.EnumMap;
 import java.util.Map;
 
 import io.mosip.registration.processor.core.code.RegistrationExceptionTypeCode;
-import io.mosip.registration.processor.status.code.RegistrationTransactionTypeCode;
+import io.mosip.registration.processor.core.code.RegistrationTransactionTypeCode;
 
 public class RegistrationStatusMapperUtil {
 	/** The status map. */
@@ -34,6 +34,59 @@ public class RegistrationStatusMapperUtil {
 				RegistrationTransactionTypeCode.REPROCESS);
 		statusMap.put(RegistrationExceptionTypeCode.DATA_ACCESS_EXCEPTION, RegistrationTransactionTypeCode.REPROCESS);
 		statusMap.put(RegistrationExceptionTypeCode.IOEXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.VIRUS_SCAN_FAILED_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.PACKET_DECRYPTION_FAILURE_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+
+		/////////////////
+
+		statusMap.put(RegistrationExceptionTypeCode.JSON_PARSE_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.JSON_MAPPING_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.JSON_PROCESSING_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.JSON_IO_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.JSON_SCHEMA_IO_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.FILE_IO_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.FILE_NOT_FOUND_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.FILE_NOT_FOUND_IN_DESTINATION_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.NO_SUCH_ALGORITHM_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.FILE_IO_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.PACKET_GENERATOR_VALIDATION_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.RUN_TIME_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.MOSIP_INVALID_DATA_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.MOSIP_INVALID_KEY_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.REG_BASE_CHECKED_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.REG_PRINT_APP_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.CLASS_NOT_FOUND_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.QR_CODE_GENERATION_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.UIN_NOT_FOUND_IN_DATABASE, RegistrationTransactionTypeCode.ERROR);
+
+		statusMap.put(RegistrationExceptionTypeCode.PDF_GENERATOR_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.REG_STATUS_APP_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.REG_STATUS_VALIDATION_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+
+		////////
+		statusMap.put(RegistrationExceptionTypeCode.EMAIL_ID_NOT_FOUND_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.PHONE_NUMBER_NOT_FOUND_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.TEMPLATE_GENERATION_FAILED_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+		//////
+		statusMap.put(RegistrationExceptionTypeCode.CONFIGURATION_NOT_FOUND_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.BASE_UNCHECKED_EXCEPTION, RegistrationTransactionTypeCode.ERROR);
+		statusMap.put(RegistrationExceptionTypeCode.IDENTITY_NOT_FOUND_EXCEPTION,
+				RegistrationTransactionTypeCode.ERROR);
+
+		statusMap.put(RegistrationExceptionTypeCode.QUEUE_CONNECTION_NOT_FOUND,
+				RegistrationTransactionTypeCode.REPROCESS);
+		statusMap.put(RegistrationExceptionTypeCode.CONNECTION_UNAVAILABLE_EXCEPTION,
+				RegistrationTransactionTypeCode.REPROCESS);
 
 		return unmodifiableMap;
 
