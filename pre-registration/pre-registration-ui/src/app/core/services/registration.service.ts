b455997081c9b228d268a4f0d14e0ea63c02f4e4
Shashank0707 <shashank.agrawal@mindtree.com>
2019-04-03 13:14:57 +0530

    MOS-20437 : Api mapping change https://mosipid.atlassian.net/browse/MOS-20437
    Former-commit-id: fb79d94e534030878136edbef89a5a53bcb07f0c
    Former-commit-id: f53bbdb3a57883f97ad3846a01710664e78d1bf7

--- pre-registration/pre-registration-ui/src/app/core/services/registration.service.ts
+++ pre-registration/pre-registration-ui/src/app/core/services/registration.service.ts
@@ -16,12 +16,9 @@ export class RegistrationService {
   sameAs = '';
   documentCategories: DocumentTypeModel[];
 
-
   getMessage() {
     return this.messageSource.asObservable();
   }
-
-
 
   changeMessage(message: Object) {
     this.messageSource.next(message);
---
+++ pre-registration/pre-registration-ui/src/app/shared/models/demographic-model/response.model.ts
@@ -0,0 +1,10 @@
+import { DemoIdentityModel } from './demo.identity.modal';
+
+export interface ResponseModel {
+  preRegistrationId: string;
+  createdBy: string;
+  createdDateTime: string;
+  updatedDateTime: string;
+  langCode: string;
+  demographicDetails: DemoIdentityModel;
+}
