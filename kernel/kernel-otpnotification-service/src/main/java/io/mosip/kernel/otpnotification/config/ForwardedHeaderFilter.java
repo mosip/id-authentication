ece80573c49ee59c75d3f92a508227d74475af27
Raj Jha <raj.jha@mindtree.com>
2019-04-05 13:53:09 +0530

    header filter removed
    Former-commit-id: 83ace331321d303c1b2f2b421340faf03ae27b12
    Former-commit-id: 0ff0a805e2756d4660ab1fcf0ddae05c7037f4c5

--- kernel/kernel-otpnotification-service/src/main/java/io/mosip/kernel/otpnotification/config/ForwardedHeaderFilter.java
+++ kernel/kernel-otpnotification-service/src/main/java/io/mosip/kernel/otpnotification/config/ForwardedHeaderFilter.java
@@ -164,9 +164,9 @@ public class ForwardedHeaderFilter extends OncePerRequestFilter {
 			Enumeration<String> names = request.getHeaderNames();
 			while (names.hasMoreElements()) {
 				String name = names.nextElement();
-				if (!FORWARDED_HEADER_NAMES.contains(name)) {
+				//if (!FORWARDED_HEADER_NAMES.contains(name)) {
 					headers.put(name, Collections.list(request.getHeaders(name)));
-				}
+				//}
 			}
 			return headers;
 		}
