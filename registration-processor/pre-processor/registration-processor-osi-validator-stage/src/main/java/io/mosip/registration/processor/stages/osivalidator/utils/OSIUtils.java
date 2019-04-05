f131ace77fa71947b0d4254ef5c521a53a2db5f2
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2019-04-05 22:43:58 +0530

    Revert "0.10.0 metadata idjson change"
    Former-commit-id: 5c3012de1bf13d5af5a806d4e01d293e9906ad24
    Former-commit-id: 7459e7fc7928cbff7927c58719b70e850f4d6135

--- registration-processor/pre-processor/registration-processor-osi-validator-stage/src/main/java/io/mosip/registration/processor/stages/osivalidator/utils/OSIUtils.java
+++ registration-processor/pre-processor/registration-processor-osi-validator-stage/src/main/java/io/mosip/registration/processor/stages/osivalidator/utils/OSIUtils.java
@@ -29,18 +29,35 @@ public class OSIUtils {
 	public RegOsiDto getOSIDetailsFromMetaInfo(String registrationId,Identity identity) throws UnsupportedEncodingException {
 		 
 		RegOsiDto regOsi = new RegOsiDto();
-		//regOsi.setIntroducerId("");// not found in json
+		//regOsi.setIntroducerFingerpImageName(identity.getBiometric().getIntroducer().getIntroducerFingerprint().getImageName());
+		regOsi.setIntroducerFingerpType(getMetaDataValue(JsonConstant.INTRODUCERFINGERPRINTTYPE,identity));
+		regOsi.setIntroducerId("");// not found in json
+		//regOsi.setIntroducerIrisImageName(identity.getBiometric().getIntroducer().getIntroducerIris().getImageName());
+		regOsi.setIntroducerIrisType(getMetaDataValue(JsonConstant.INTRODUCERIRISTYPE,identity));
+		//regOsi.setIntroducerPhotoName(identity.getBiometric().getIntroducer().getIntroducerImage().getImageName());
+		regOsi.setIntroducerRegId(getMetaDataValue(JsonConstant.INTRODUCERRID,identity));
 		regOsi.setIntroducerTyp(getMetaDataValue(JsonConstant.INTRODUCERTYPE,identity));
+		regOsi.setIntroducerUin(getMetaDataValue(JsonConstant.INTRODUCERUIN,identity));
+		regOsi.setOfficerFingerpImageName(getOsiDataValue(JsonConstant.OFFICERFINGERPRINTIMAGE,identity));
+		regOsi.setOfficerfingerType(getMetaDataValue(JsonConstant.OFFICERFINGERPRINTTYPE,identity));
 		regOsi.setOfficerHashedPin(getMetaDataValue(JsonConstant.OFFICERPIN,identity));
 		regOsi.setOfficerHashedPwd(getOsiDataValue(JsonConstant.OFFICERPWR,identity));
 		regOsi.setOfficerId(getOsiDataValue(JsonConstant.OFFICERID,identity));
+		//regOsi.setOfficerIrisImageName("");  // not found in json
+		regOsi.setOfficerIrisType(getMetaDataValue(JsonConstant.OFFICERIRISTYPE,identity));
+		regOsi.setOfficerPhotoName(getOsiDataValue(JsonConstant.OFFICERPHOTONAME,identity));
 		regOsi.setOfficerOTPAuthentication(getOsiDataValue(JsonConstant.OFFICEROTPAUTHENTICATION,identity));
 		regOsi.setPreregId(getMetaDataValue(JsonConstant.PREREGISTRATIONID,identity));
 		regOsi.setRegId(getMetaDataValue(JsonConstant.REGISTRATIONID,identity));
+		//regOsi.setSupervisorFingerpImageName(""); // not found in json
 		regOsi.setSupervisorBiometricFileName(getOsiDataValue(JsonConstant.SUPERVISORBIOMETRICFILENAME,identity));
+		regOsi.setSupervisorFingerType(getMetaDataValue(JsonConstant.SUPERVISORFINGERPRINTTYPE,identity));
 		regOsi.setSupervisorHashedPin(getOsiDataValue(JsonConstant.OFFICERPHOTONAME,identity));
 		regOsi.setSupervisorHashedPwd(getOsiDataValue(JsonConstant.SUPERVISORPWR,identity));
 		regOsi.setSupervisorId(getOsiDataValue(JsonConstant.SUPERVISORID,identity));
+		regOsi.setSupervisorIrisImageName(""); // not found in json
+		regOsi.setSupervisorIrisType(getOsiDataValue(JsonConstant.SUPERVISORIRISTYPE,identity));
+		regOsi.setSupervisorPhotoName("");  // not found in json
 		regOsi.setSupervisorOTPAuthentication(getOsiDataValue(JsonConstant.SUPERVISOROTPAUTHENTICATION,identity));
 		
 		return regOsi;
