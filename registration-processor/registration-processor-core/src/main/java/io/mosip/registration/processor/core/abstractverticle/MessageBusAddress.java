020fdac925a3a3ef5106c2dfd1646bdf9eb10799
Alok Ranjan <Alok.Ranjan2@mindtree.com>
2019-04-05 10:02:00 +0530

    MOS-20573 changed camel routes bus address https://mosipid.atlassian.net/browse/MOS-20573
    Former-commit-id: 34dd5d608aeb0c5364e6d6620bf5f1309cce00dc
    Former-commit-id: fd2a202e3167854ea5f52022272633579744ff62

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/abstractverticle/MessageBusAddress.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/abstractverticle/MessageBusAddress.java
@@ -131,8 +131,8 @@ public class MessageBusAddress implements Serializable {
 	/** The Constant PRINTING_BUS_RESEND. */
 	public static final MessageBusAddress PRINTING_BUS_RESEND = new MessageBusAddress("printing-bus-resend");
 
-	public static final MessageBusAddress EXTERNAL_STAGE_BUS_IN = new MessageBusAddress("external-stage-bus-in");
+	public static final MessageBusAddress EXTERNAL_STAGE_BUS_IN = new MessageBusAddress("external-bus-in");
 
-	public static final MessageBusAddress EXTERNAL_STAGE_BUS_OUT = new MessageBusAddress("external-stage-bus-out");
+	public static final MessageBusAddress EXTERNAL_STAGE_BUS_OUT = new MessageBusAddress("external-bus-out");
 
 }
