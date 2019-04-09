6e7f53a1b5121a08bb7fcd2b2edb2d72500c3429
Manoj SP <Manoj.SP@mindtree.com>
2019-04-09 12:18:38 +0530

    MOS-20222 - updated roles in auth
    Former-commit-id: e76665f681e8a987fcc66fab4b160874623315de
    Former-commit-id: 59cde544daa75c1421ec5d98f07ed05f4b4582bb

--- kernel/kernel-idrepo-service/src/main/java/io/mosip/kernel/idrepo/controller/IdRepoController.java
+++ kernel/kernel-idrepo-service/src/main/java/io/mosip/kernel/idrepo/controller/IdRepoController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
@@ -124,6 +125,7 @@ public class IdRepoController {
 	 * @throws IdRepoAppException
 	 *             the id repo app exception
 	 */
+	@PreAuthorize("hasAnyRole('REGISTRATION_PROCESSOR')") 
 	@PostMapping(path = "/identity/{uin}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<IdResponseDTO> addIdentity(@PathVariable String uin,
 			@Validated @RequestBody IdRequestDTO request, @ApiIgnore Errors errors) throws IdRepoAppException {
@@ -157,6 +159,7 @@ public class IdRepoController {
 	 * @throws IdRepoAppException
 	 *             the id repo app exception
 	 */
+	@PreAuthorize("hasAnyRole('ID_AUTHENTICATION', 'REGISTRATION_PROCESSOR')") 
 	@GetMapping(path = "/identity/{uin}", consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<IdResponseDTO> retrieveIdentity(@PathVariable String uin,
 			@RequestParam(name = TYPE, required = false) @Nullable String type) throws IdRepoAppException {
@@ -206,6 +209,7 @@ public class IdRepoController {
 	 * @throws IdRepoAppException
 	 *             the id repo app exception
 	 */
+	@PreAuthorize("hasAnyRole('REGISTRATION_PROCESSOR')")
 	@PatchMapping(path = "/identity/{uin}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<IdResponseDTO> updateIdentity(@PathVariable String uin,
 			@Validated @RequestBody IdRequestDTO request, @ApiIgnore Errors errors) throws IdRepoAppException {
