a34eaae2860ce62c0a607400f26229c754368131
Raj Jha <raj.jha@mindtree.com>
2019-04-11 15:24:35 +0530

    MOS-22488 RID authz added for RegProc role
    Former-commit-id: 55cc24b62c9a22b321e9772edaa1fc34249e9d65
    Former-commit-id: 4757c10969d74b31c4752add2cac756137a6cdc3

--- kernel/kernel-ridgenerator-service/src/test/java/io/mosip/kernel/ridgenerator/test/exception/RidGeneratorExceptionTest.java
+++ kernel/kernel-ridgenerator-service/src/test/java/io/mosip/kernel/ridgenerator/test/exception/RidGeneratorExceptionTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -31,6 +32,7 @@ public class RidGeneratorExceptionTest {
 	private RidGeneratorService<RidGeneratorResponseDto> ridGeneratorService;
 
 	@Test
+	@WithUserDetails("reg-processor")
 	public void invalidLengthCenterIdExceptionTest() throws Exception {
 		when(ridGeneratorService.generateRid(Mockito.any(), Mockito.any()))
 				.thenThrow(new InputLengthException("errorCode", "errorMessage"));
@@ -38,6 +40,7 @@ public class RidGeneratorExceptionTest {
 	}
 
 	@Test
+	@WithUserDetails("reg-processor")
 	public void emptyLengthCenterIdAndMachineIdExceptionTest() throws Exception {
 		when(ridGeneratorService.generateRid(Mockito.any(), Mockito.any()))
 				.thenThrow(new EmptyInputException("errorCode", "errorMessage"));
