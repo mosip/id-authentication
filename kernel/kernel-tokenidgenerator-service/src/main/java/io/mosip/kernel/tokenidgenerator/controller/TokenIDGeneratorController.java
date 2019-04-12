d1f1028fe7da3a706d9777b3a3363486334d1005
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-04-12 14:40:24 +0530

    merge
    Former-commit-id: fdf4cafcc71750afcf52a2f1be62c2bfa0dc2551
    Former-commit-id: 5dd6760a09022041190fc580a744923691eaa779

--- kernel/kernel-tokenidgenerator-service/src/main/java/io/mosip/kernel/tokenidgenerator/controller/TokenIDGeneratorController.java
+++ kernel/kernel-tokenidgenerator-service/src/main/java/io/mosip/kernel/tokenidgenerator/controller/TokenIDGeneratorController.java
@@ -1,6 +1,7 @@
 package io.mosip.kernel.tokenidgenerator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class TokenIDGeneratorController {
 	@Autowired
 	private TokenIDGeneratorService vidGeneratorService;
 
+	@PreAuthorize("hasRole('ID_AUTHENTICATION')")
 	@ResponseFilter
 	@GetMapping(value = "/{uin}/{partnercode}")
 	public ResponseWrapper<TokenIDResponseDto> generateTokenID(@ApiParam("uin of user") @PathVariable("uin") String uin,
--- kernel/kernel-tokenidgenerator-service/src/test/java/io/mosip/kernel/tokenidgenerator/test/integration/TokenIdGeneratorIntegrationTest.java
+++ kernel/kernel-tokenidgenerator-service/src/test/java/io/mosip/kernel/tokenidgenerator/test/integration/TokenIdGeneratorIntegrationTest.java
@@ -9,19 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import io.mosip.kernel.tokenidgenerator.dto.TokenIDResponseDto;
+import io.mosip.kernel.tokenidgenerator.test.TokenIdGeneratorTestBootApplication;
 
+@SpringBootTest(classes=TokenIdGeneratorTestBootApplication.class)
 @RunWith(SpringRunner.class)
-@SpringBootTest
 @AutoConfigureMockMvc
 public class TokenIdGeneratorIntegrationTest {
 
 	@Autowired
 	private MockMvc mockMvc;
 
+	@WithMockUser("id-auth")
 	@Test
 	public void generateTokenIDTest() throws Exception {
 		TokenIDResponseDto response = new TokenIDResponseDto();
@@ -30,6 +33,7 @@ public class TokenIdGeneratorIntegrationTest {
 
 	}
 
+	@WithMockUser("id-auth")
 	@Test
 	public void generateTokenIdExceptionTest() throws Exception {
 		mockMvc.perform(get("/    /   ").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
