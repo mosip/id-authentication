8b856dd195aa92401bda6ccebb150d173b1c523b
Ravikant-M1044571 <M1044571@b2ml27235.mindtree.com>
2019-04-17 17:19:05 +0530

    MOS-22916 : updated FetchDeviceSpec script and all the response body
    Former-commit-id: d27c892e60ffb2543c4f13ae5265fb51f2d6b62d
    Former-commit-id: a395702993efb991c2955d292e4da88e95463e44

--- automationtests/src/test/java/io/mosip/kernel/tests/FetchDeviceSpec.java
+++ automationtests/src/test/java/io/mosip/kernel/tests/FetchDeviceSpec.java
@@ -175,7 +175,7 @@ public class FetchDeviceSpec extends BaseTestCase implements ITest{
 			long obtainedObjectsCount = KernelMasterDataR.validateDBCount(query);
 
 			// fetching json object from response
-			JSONObject responseJson = (JSONObject) new JSONParser().parse(response.asString());
+			JSONObject responseJson = (JSONObject) ((JSONObject) new JSONParser().parse(response.asString())).get("response");
 			// fetching json array of objects from response
 			JSONArray deviceSpecFromGet = (JSONArray) responseJson.get("devicespecifications");
 			logger.info("===Dbcount===" + obtainedObjectsCount + "===Get-count===" + deviceSpecFromGet.size());
@@ -212,6 +212,7 @@ public class FetchDeviceSpec extends BaseTestCase implements ITest{
 		else {
 			// add parameters to remove in response before comparison like time stamp
 			ArrayList<String> listOfElementToRemove = new ArrayList<String>();
+			listOfElementToRemove.add("responsetime");
 			listOfElementToRemove.add("timestamp");
 			status = assertions.assertKernel(response, responseObject, listOfElementToRemove);
 		}
