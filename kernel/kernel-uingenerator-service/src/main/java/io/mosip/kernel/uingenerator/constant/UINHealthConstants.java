4d23f4853ce252d11f6375ffc456c7f1e8d32dcd
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-04-19 15:18:26 +0530

    MOS-23000 #comment constants added
    Former-commit-id: d74699cb04f14a0e7dfa2851f24a1272a5d8f2c9
    Former-commit-id: c3bc5a9819de99bece2ccbed9a21a43ccd1edc9b

---
+++ kernel/kernel-uingenerator-service/src/main/java/io/mosip/kernel/uingenerator/constant/UINHealthConstants.java
@@ -0,0 +1,73 @@
+package io.mosip.kernel.uingenerator.constant;
+
+public class UINHealthConstants {
+	/**
+	 * The string field current working directory
+	 */
+	public static final String ERROR = "error";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String ACTIVE = "active";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String NOT_ACTIVE = "not active";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String PING = "ping";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String DATABASE = "database";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String HELLO = "hello";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String TOTAL = "total";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String THRESHOLD = "threshold";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String FREE = "free";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String RESPONSE = "response";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String THRESHOLD_ERROR = "Free disk space below threshold. Available: %d bytes (threshold: %d bytes)";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String DETAILS = "details";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String DATA = "data";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String CHECKS = "checks";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String ID = "id";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String STATUS = "status";
+	/**
+	 * The string field current working directory
+	 */
+	public static final String OUTCOME = "outcome";
+
+}
