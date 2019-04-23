e49bc3017a79dcb64b7597b9acebb1586e76a6e8
Sanjay Murali <sanjay.murali@mindtree.com>
2019-04-23 19:03:18 +0530

    MOS-22941 : updating context properties
    Former-commit-id: bd83a207ad21cb9054c9b64b8af29e6d4e1e299a
    Former-commit-id: d8a17cd7c355e4f33501b7f888e2ad00241ab84b

--- authentication/kyc-service/src/main/java/io/mosip/authentication/kyc/service/config/KycFilterConfig.java
+++ authentication/kyc-service/src/main/java/io/mosip/authentication/kyc/service/config/KycFilterConfig.java
@@ -1,11 +1,9 @@
 package io.mosip.authentication.kyc.service.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.mosip.authentication.core.constant.IdAuthConfigKeyConstants;
 import io.mosip.authentication.kyc.service.filter.KycAuthFilter;
 
 /**
@@ -15,9 +13,6 @@ import io.mosip.authentication.kyc.service.filter.KycAuthFilter;
  */
 @Configuration
 public class KycFilterConfig {
-
-	@Value("${"+ IdAuthConfigKeyConstants.MOSIP_IDA_API_IDS + "}")
-	private String apiVersion;
 
 	/**
 	 * Gets the eKyc filter.
