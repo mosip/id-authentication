a1bb158542e570c19a7a65a195ddd72f333a04c0
TaleevAaalam <Taleev.Aalam@mindtree.com>
2019-04-26 18:06:03 +0530

    MOS-22835: added device capture controller and stubbed the data Former-commit-id: 7ad77fc19cea1329a24d23373b6d624ad44f6e6c Former-commit-id: 6f2f196e9a136ae027373eef3a7c3c5adb9f4326

---
+++ registration/registration-mdm-service/src/main/java/io/mosip/registration/mdm/dto/BioDevice.java
@@ -0,0 +1,68 @@
+package io.mosip.registration.mdm.dto;
+
+import java.util.Map;
+
+import io.mosio.registration.mdm.constants.MosipBioDeviceConstants;
+import io.mosip.registration.exception.RegBaseCheckedException;
+import io.mosip.registration.mdm.integrator.MosipBioDeviceIntegrator;
+import io.mosip.registration.service.mdm.util.MdmRequestResponseBuilder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Holds the Biometric Device details
+ * 
+ * @author balamurugan.ramamoorthy
+ *
+ */
+@Getter
+@Setter
+public class BioDevice {
+
+	private String deviceType;
+	private String deviceSubType;
+	private String deviceModality;
+	private int runningPort;
+	private String runningUrl;
+	private String status;
+	private String providerName;
+	private String providerId;
+	private String serialVersion;
+	private String certificationType;
+	private String callbackId;
+	private String deviceModel;
+	private String deviceMake;
+	private String firmWare;
+	private String deviceExpiry;
+
+	private MosipBioDeviceIntegrator mosipBioDeviceIntegrator;
+
+	public Map<String, byte[]> capture() throws RegBaseCheckedException {
+
+		String url = runningUrl + ":" + runningPort + "/" + MosipBioDeviceConstants.CAPTURE_ENDPOINT;
+
+		/* build the request object for capture */
+		MosipBioCaptureRequestDto mosipBioCaptureRequestDto = MdmRequestResponseBuilder
+				.buildMosipBioCaptureRequestDto();
+
+		return mosipBioDeviceIntegrator.capture(url, MosipBioDeviceConstants.CAPTURE_SERVICENAME,
+				mosipBioCaptureRequestDto, MosipBioCaptureResponseDto.class);
+
+	}
+
+	public byte[] forceCapture() {
+		return null;
+
+	}
+
+	public byte[] stream() {
+		return null;
+
+	}
+
+	public int deviceStatus() {
+		return 0;
+
+	}
+
+}
