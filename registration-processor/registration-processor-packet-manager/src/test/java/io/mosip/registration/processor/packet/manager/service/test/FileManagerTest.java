4d2418b302d60838bcc9d85b77aee89f5f390bb8
M1050450 <shekhar.suman@mindtree.com>
2019-04-26 11:43:34 +0530

    MOS-23095 : FilemanagerImpl test case update
    Former-commit-id: f99607f9c99a498d5b3c7a3816f97d39d7510295
    Former-commit-id: ed0ee3660e5bedd1b0b8d44e5e8df858f6e5fdb1

--- registration-processor/registration-processor-packet-manager/src/test/java/io/mosip/registration/processor/packet/manager/service/test/FileManagerTest.java
+++ registration-processor/registration-processor-packet-manager/src/test/java/io/mosip/registration/processor/packet/manager/service/test/FileManagerTest.java
@@ -11,9 +11,9 @@ import java.io.InputStream;
 
 import org.apache.commons.io.FilenameUtils;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,15 +24,19 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 
+import io.mosip.registration.processor.core.exception.JschConnectionException;
+import io.mosip.registration.processor.core.exception.SftpFileOperationException;
 import io.mosip.registration.processor.core.packet.dto.SftpJschConnectionDto;
 import io.mosip.registration.processor.core.spi.filesystem.manager.FileManager;
 import io.mosip.registration.processor.packet.manager.config.PacketManagerConfigTest;
 import io.mosip.registration.processor.packet.manager.dto.DirectoryPathDto;
+import io.mosip.registration.processor.packet.manager.service.impl.FileManagerImpl;
 
 /**
  * The Class FileManagerTest.
@@ -55,6 +59,9 @@ public class FileManagerTest {
 	/** The env. */
 	@MockBean
 	private Environment env;
+	
+	@Mock 
+	private Environment env1;
 
 	@Mock
 	private SftpJschConnectionDto sftpDto = new SftpJschConnectionDto();
@@ -66,18 +73,28 @@ public class FileManagerTest {
 	/** The virus scan dec. */
 	@Value("${VIRUS_SCAN_DEC}")
 	private String virusScanDec;
-	@Mock
-	private JSch jSch = new JSch();
+	//@InjectMocks
+	
 
 	@Mock
 	private ChannelSftp sftp = new ChannelSftp();
 
 	@Mock
 	private Session session;
+	
+	@InjectMocks
+	private FileManagerImpl impl=new FileManagerImpl() {
+		@Override
+		public ChannelSftp getSftpConnection(SftpJschConnectionDto sftpConnectionDto) throws JschConnectionException {
+			System.out.println("Hello");
+			return sftp;
+		
+	}};
 
 	@Mock
 	private InputStream is;
 
+	//SshServer sshd;
 	/**
 	 * Sets the up.
 	 *
@@ -87,17 +104,57 @@ public class FileManagerTest {
 	@Before
 	public void setUp() throws Exception {
 		ClassLoader classLoader = getClass().getClassLoader();
+		ReflectionTestUtils.setField(impl, "extention", ".zip");
 		file = new File(classLoader.getResource("1001.zip").getFile());
 		is = new FileInputStream(file);
 		when(env.getProperty(DirectoryPathDto.VIRUS_SCAN_ENC.toString())).thenReturn(virusScanEnc);
 		when(env.getProperty(DirectoryPathDto.VIRUS_SCAN_DEC.toString())).thenReturn(virusScanDec);
+		when(env1.getProperty(DirectoryPathDto.VIRUS_SCAN_ENC.toString())).thenReturn(virusScanEnc);
+		when(env1.getProperty(DirectoryPathDto.VIRUS_SCAN_DEC.toString())).thenReturn(virusScanDec);
+		//when(env.getProperty(Mockito.a))
 		sftpDto.setHost("localhost");
 		sftpDto.setPort(9700);
 		sftpDto.setPpkFileLocation("src/test/resources");
 		sftpDto.setProtocal("http");
 		sftpDto.setUser("System");
+		/*sshd = SshServer.setUpDefaultServer();
+		sshd.setPort(22999);
+		sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider("hostkey.ser"));
+		sshd.setPasswordAuthenticator(new PasswordAuthenticator() {
+			
+		public boolean authenticate(String username, String password, ServerSession session) {
+		// TODO Auto-generated method stub
+		return true;
+		}
+		});
+		CommandFactory myCommandFactory = new CommandFactory() {
+		public Command createCommand(String command) {
+		System.out.println("Command: " + command);
+		return null;
+		}
+		};
+		sshd.setCommandFactory(new ScpCommandFactory(myCommandFactory));
+		List<NamedFactory<Command>> namedFactoryList = new ArrayList<NamedFactory<Command>>(); 
+		namedFactoryList.add(new SftpSubsystem.Factory()); 
+		sshd.setSubsystemFactories(namedFactoryList); sshd.start(); 		
+		JSch jsch=new JSch();
+		Channel channel;
+		Hashtable<String,String> config = new Hashtable<>();
+		config.put("StrictHostKeyChecking", "no");
+		JSch.setConfig(config);
+		//jsch.addIdentity("src/test/resources/Mosip_Private_key.ppk");
+		session=jsch.getSession("remote-username", "localhost", 7878);
+        session.setPassword("remote-password");
+		
+		session.connect();
+		channel=session.openChannel("http");
+		channel.connect();
+		sftp=(ChannelSftp) channel;
+		sftp.connect();*/
 
 	}
+//@After
+//public void teardown() throws Exception { sshd.stop(); }
 
 	/**
 	 * Gets the put and if file exists and copy method check.
@@ -130,16 +187,32 @@ public class FileManagerTest {
 	}
 
 	@Test
-	@Ignore
+	//@Ignore
 	public void testGetFileByteArray() throws Exception {
 		File newFile = new File("Abc.zip");
 		String fileName = newFile.getName();
-		Mockito.when(jSch.getSession(Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(session);
+		String fileNameWithoutExtn = FilenameUtils.removeExtension(fileName);
+		//Mockito.when(jSch.getSession(Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(session);
+		Mockito.doNothing().when(session).connect();
 		Mockito.when(session.openChannel(Mockito.any())).thenReturn(sftp);
 		Mockito.doNothing().when(sftp).connect();
 		Mockito.when(sftp.get(Mockito.any())).thenReturn(is);
-		fileManager.getFile(DirectoryPathDto.VIRUS_SCAN_ENC, fileName, sftpDto);
+		impl.getFile(DirectoryPathDto.VIRUS_SCAN_ENC, fileNameWithoutExtn, sftpDto);
 
 	}
-
+	
+	@Test(expected=SftpFileOperationException.class)
+	public void testSftpExceptoin() throws Exception
+	{
+		File newFile = new File("Abc.zip");
+		String fileName = newFile.getName();
+		String fileNameWithoutExtn = FilenameUtils.removeExtension(fileName);
+		//Mockito.when(jSch.getSession(Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(session);
+		Mockito.doNothing().when(session).connect();
+		Mockito.when(session.openChannel(Mockito.any())).thenReturn(sftp);
+		Mockito.doNothing().when(sftp).connect();
+		Mockito.when(sftp.get(Mockito.any())).thenThrow(new SftpException(0, fileNameWithoutExtn));
+		impl.getFile(DirectoryPathDto.VIRUS_SCAN_ENC, fileNameWithoutExtn, sftpDto);
+		
+	}
 }
