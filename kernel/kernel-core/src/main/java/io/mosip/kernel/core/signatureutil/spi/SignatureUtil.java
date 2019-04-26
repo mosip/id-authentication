f904d43f9ac213073587c4a047655f41a2ad68df
srinivasanMT <srinivasan.lakshminarasimhan@mindtree.com>
2019-04-26 09:48:08 +0530

    MOS-23034 added validation methods in signature valdiation util
    Former-commit-id: 4f3fd8e04a10466da1728edf5f30ef42883931f0
    Former-commit-id: cfc7aaa303bf7584c43268e2613e17d2df022825

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/signatureutil/spi/SignatureUtil.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/signatureutil/spi/SignatureUtil.java
@@ -14,14 +14,43 @@ public interface SignatureUtil {
 	/**
 	 * Sign response.
 	 *
-	 * @param response the response
+	 * @param response
+	 *            the response
 	 * @return the string
 	 */
 	public String signResponse(String response);
 
+	/**
+	 * Validate with public key.
+	 *
+	 * @param responseSignature
+	 *            the response signature
+	 * @param responseBody
+	 *            the response body
+	 * @param publicKey
+	 *            the public key
+	 * @return true, if successful
+	 * @throws InvalidKeySpecException
+	 *             the invalid key spec exception
+	 * @throws NoSuchAlgorithmException
+	 *             the no such algorithm exception
+	 */
 	boolean validateWithPublicKey(String responseSignature, String responseBody, String publicKey)
 			throws InvalidKeySpecException, NoSuchAlgorithmException;
 
+	/**
+	 * Validate with public key.
+	 *
+	 * @param responseSignature
+	 *            the response signature
+	 * @param responseBody
+	 *            the response body
+	 * @return true, if successful
+	 * @throws InvalidKeySpecException
+	 *             the invalid key spec exception
+	 * @throws NoSuchAlgorithmException
+	 *             the no such algorithm exception
+	 */
 	boolean validateWithPublicKey(String responseSignature, String responseBody)
 			throws InvalidKeySpecException, NoSuchAlgorithmException;
 }
