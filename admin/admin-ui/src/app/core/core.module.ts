4e322a1b1ac176b39d5536e758a47323618c005c
Agnitra Banerjee <agneetra10@gmail.com>
2019-04-30 17:02:55 +0530

    MOS-0000 App routing in progress
    Former-commit-id: 6d1d85c578afd368e6f5cff883fc028387c9b156
    Former-commit-id: cd2d38f9ad24e4da3f6a432116c8877e6b4afd31

--- admin/admin-ui/src/app/core/core.module.ts
+++ admin/admin-ui/src/app/core/core.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   exports: [HeaderComponent, FooterComponent, SidebarComponent]
--- admin/admin-ui/src/app/navigation/navigation-routing.module.ts
+++ admin/admin-ui/src/app/navigation/navigation-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'authenticate', component: AuthenticationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
