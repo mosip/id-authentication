fab3765d4027234ef56fc1d8a736f8378419d07f
SaravanaKumar <Saravanakumar.Gnanaguru@mindtree.com>
2019-04-30 14:46:36 +0530

    MOS-23125 : Supervisor authentication issue fix
    Former-commit-id: 2964bd957c2a2528eda1f48f4c160c4cb605d01b
    Former-commit-id: 40ca2e87c22ef8f9884a3e07a45564ebc814f16f

--- registration/registration-client/src/main/java/io/mosip/registration/controller/auth/AuthenticationController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/auth/AuthenticationController.java
@@ -241,6 +241,13 @@ public class AuthenticationController extends BaseController implements Initiali
 			if (!username.getText().isEmpty()) {
 				if (fetchUserRole(username.getText())) {
 					status = validatePwd(username.getText(), password.getText());
+					if (RegistrationConstants.SUCCESS.equals(status)) {
+						userAuthenticationTypeListValidation.remove(0);
+						userNameField = username.getText();
+						loadNextScreen();
+					} else if (RegistrationConstants.FAILURE.equals(status)) {
+						generateAlert(RegistrationConstants.ERROR, RegistrationUIConstants.AUTHENTICATION_FAILURE);
+					}
 					if (!isEODAuthentication) {
 						getOSIData().setSupervisorID(userNameField);
 						getOSIData().setSuperviorAuthenticatedByPassword(true);
@@ -254,6 +261,13 @@ public class AuthenticationController extends BaseController implements Initiali
 		} else {
 			if (!username.getText().isEmpty()) {
 				status = validatePwd(username.getText(), password.getText());
+				if (RegistrationConstants.SUCCESS.equals(status)) {
+					userAuthenticationTypeListValidation.remove(0);
+					userNameField = username.getText();
+					loadNextScreen();
+				} else if (RegistrationConstants.FAILURE.equals(status)) {
+					generateAlert(RegistrationConstants.ERROR, RegistrationUIConstants.AUTHENTICATION_FAILURE);
+				}
 				if (!isEODAuthentication) {
 					getOSIData().setOperatorAuthenticatedByPassword(true);
 				}
@@ -262,13 +276,6 @@ public class AuthenticationController extends BaseController implements Initiali
 			}
 		}
 
-		if (RegistrationConstants.SUCCESS.equals(status)) {
-			userAuthenticationTypeListValidation.remove(0);
-			userNameField = username.getText();
-			loadNextScreen();
-		} else if (RegistrationConstants.FAILURE.equals(status)) {
-			generateAlert(RegistrationConstants.ERROR, RegistrationUIConstants.AUTHENTICATION_FAILURE);
-		}
 	}
 
 	/**
