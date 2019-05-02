2ca48525598945042c5078c72bbfb7d707a7bb02
Ajay J <M1044588@mindtree.com>
2019-05-02 18:30:37 +0530

    MOS-0000 login component added
    Former-commit-id: 7559e23b5060fd97b5e6d66609efe2405733c520
    Former-commit-id: 17cad22f923c2808b1425a9049ff5e86dbbedba0

--- admin/admin-ui/src/app/navigation/authentication/authentication.component.ts
+++ admin/admin-ui/src/app/navigation/authentication/authentication.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticationComponent implements OnInit {
 
-  constructor() { }
+  minutes: any;
+  seconds: any;
+  timer: any;
+  minuteString: any;
+  secondString: any;
+  constructor(private router: Router) {
+    this.startCountdown(10);
+   }
 
   ngOnInit() {
   }
-
+   startCountdown(timeLeft): void {
+    let counter = 0;
+    const interval = setInterval(() => {
+      console.log(counter);
+      document.getElementById('timer').innerHTML = this.convertSeconds(timeLeft - counter);
+      counter++;
+      if (counter < 0 ) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+    convertSeconds(timeLeft) {
+    this.minutes = Math.floor(timeLeft / 60);
+    this.seconds = Math.floor(timeLeft % 60);
+    // tslint:disable-next-line:max-line-length
+    return Number(this.minutes).toLocaleString('en-US', {minimumIntegerDigits: 2}) + ':' + Number(this.seconds).toLocaleString('en-US', {minimumIntegerDigits: 2});
+   }
+   onSubmit(){
+   this.router.navigateByUrl('admin/dashboard');
+   }
 }
--- admin/admin-ui/src/app/navigation/navigation.module.ts
+++ admin/admin-ui/src/app/navigation/navigation.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { NavigationRoutingModule } from './navigation-routing.module';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { MaterialModule } from '../material.module';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    NavigationRoutingModule
+    NavigationRoutingModule,
+    MaterialModule,
+    AppRoutingModule
   ],
   declarations: [LoginComponent, AuthenticationComponent]
 })
