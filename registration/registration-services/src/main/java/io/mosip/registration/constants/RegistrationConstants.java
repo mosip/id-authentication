b7a542d5f7f6f0247fa768159fe7e12ee8c45125
TaleevAaalam <Taleev.Aalam@mindtree.com>
2019-05-03 13:03:56 +0530

    MOS-22838: fixed the issue Former-commit-id: 27b96c53ee2c8d08e97039691080c1aded2a593c Former-commit-id: 7fa2d3a14ddcc91d622c661df4ff6d298493e95a

--- registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -1295,7 +1295,7 @@ public class RegistrationConstants {
 	public static final String ERRORS = "errors";
 	public static final String ERROR_MSG = "message";
 	public static final String OK_MSG = "ok";
-	public static final String NEW_LINE = "\\n";
+	public static final String NEW_LINE = "\n";
 
 	public static final String ATTR_INDIVIDUAL_TYPE = "individualTypeCode";
 	public static final String ATTR_DATE_OF_BIRTH = "dateofbirth";
