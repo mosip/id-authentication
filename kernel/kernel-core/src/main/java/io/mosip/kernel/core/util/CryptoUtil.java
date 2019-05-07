5c5d7234e362f3c488eefca24f568f5de5fbeeed
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-05-07 17:14:52 +0530

    MOS-23762 #comment sync data modified
    Former-commit-id: 2e223b7ca4c89900e0688636c894269fd10c7407
    Former-commit-id: c949b54872ffaf5be25a970f866bf299e8ab2f63

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/util/CryptoUtil.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/util/CryptoUtil.java
@@ -93,10 +93,20 @@ public class CryptoUtil {
 		return Base64.decodeBase64(data);
 	}
 
+	/**Compute Fingerprint of a key
+	 * @param data key data
+	 * @param metaData metadata related to key
+	 * @return fingerprint
+	 */
 	public String computeFingerPrint(String data, String metaData) {
 		return computeFingerPrint(data.getBytes(), metaData);
 	}
 
+	/** Compute Fingerprint of a key
+	 * @param data key data
+	 * @param metaData metadata related to key
+	 * @return fingerprint
+	 */
 	public String computeFingerPrint(byte[] data, String metaData) {
         byte[] combinedPlainTextBytes = null;
 		if (EmptyCheckUtils.isNullEmpty(metaData)) {
