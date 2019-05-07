ca68a9caf51caf7734f6da9c81eecb73d47c2ee8
kiranrajbs <kiranraj.s@mindtree.com>
2019-05-07 12:38:47 +0530

    MOS-23712 : Created Common Response Dto for AbisResponse Dto
    Former-commit-id: a862bde0c3fe31e91f42e9965fd53e0d32b97f01
    Former-commit-id: eb19535e8b627ee341621de343e0358b11b10449

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/abis/AbisIdentifyResponseDto.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/abis/AbisIdentifyResponseDto.java
@@ -1,100 +1,22 @@
 package io.mosip.registration.processor.core.packet.dto.abis;
 
+import java.io.Serializable;
+
 /**
  * The Class AbisIdentifyResponseDto.
  * @author M1048860 Kiran Raj
  */
-public class AbisIdentifyResponseDto {
+public class AbisIdentifyResponseDto extends AbisCommonResponseDto implements Serializable{
 
-	/** The id. */
-	private String id;
-	
-	/** The request id. */
-	private String requestId;
-	
-	/** The timestamp. */
-	private String timestamp;
-	
-	/** The return value. */
-	private Integer returnValue;
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1344142383925376038L;
+
 	/** The candidate list. */
 	private CandidateListDto candidateList;
 	
-	/** The failure reason. */
-	private Integer failureReason;
-
-	/**
-	 * Gets the id.
-	 *
-	 * @return the id
-	 */
-	public String getId() {
-		return id;
-	}
-
-	/**
-	 * Sets the id.
-	 *
-	 * @param id the new id
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	/**
-	 * Gets the request id.
-	 *
-	 * @return the request id
-	 */
-	public String getRequestId() {
-		return requestId;
-	}
-
-	/**
-	 * Sets the request id.
-	 *
-	 * @param requestId the new request id
-	 */
-	public void setRequestId(String requestId) {
-		this.requestId = requestId;
-	}
-
-	/**
-	 * Gets the timestamp.
-	 *
-	 * @return the timestamp
-	 */
-	public String getTimestamp() {
-		return timestamp;
-	}
-
-	/**
-	 * Sets the timestamp.
-	 *
-	 * @param timestamp the new timestamp
-	 */
-	public void setTimestamp(String timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	/**
-	 * Gets the return value.
-	 *
-	 * @return the return value
-	 */
-	public Integer getReturnValue() {
-		return returnValue;
-	}
-
-	/**
-	 * Sets the return value.
-	 *
-	 * @param returnValue the new return value
-	 */
-	public void setReturnValue(Integer returnValue) {
-		this.returnValue = returnValue;
-	}
+	
 
 	/**
 	 * Gets the candidate list.
@@ -114,22 +36,4 @@ public class AbisIdentifyResponseDto {
 		this.candidateList = candidateList;
 	}
 	
-	/**
-	 * Gets the failure reason.
-	 *
-	 * @return the failure reason
-	 */
-	public Integer getFailureReason() {
-		return failureReason;
-	}
-	
-	/**
-	 * Sets the failure reason.
-	 *
-	 * @param failureReason the new failure reason
-	 */
-	public void setFailureReason(Integer failureReason) {
-		this.failureReason = failureReason;
-	}
-
 }
