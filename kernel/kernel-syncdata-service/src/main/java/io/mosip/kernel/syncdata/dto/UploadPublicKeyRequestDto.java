82c336280897beafcc021c5f2f2e8bd1baf03644
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-05-07 14:09:24 +0530

    MOS-23762 #comment sync data modified
    Former-commit-id: d129155beafda66f07d3bce882d09c6f463fc9c8
    Former-commit-id: 0e6058c51e2daa8419b02acd4ce0a017311a9629

--- kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/dto/UploadPublicKeyRequestDto.java
+++ kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/dto/UploadPublicKeyRequestDto.java
@@ -1,10 +1,16 @@
 package io.mosip.kernel.syncdata.dto;
 
+import java.time.LocalDateTime;
+
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UploadPublicKeyRequestDto {
 	
 	@NotBlank
--- kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/dto/UploadPublicKeyResponseDto.java
+++ kernel/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/dto/UploadPublicKeyResponseDto.java
@@ -1,5 +1,14 @@
 package io.mosip.kernel.syncdata.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UploadPublicKeyResponseDto {
 	
 	private String keyIndex;
