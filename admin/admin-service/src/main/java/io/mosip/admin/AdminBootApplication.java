d62c35d07adbf60000d674bd50c34b25113a1658
abhishek kumar <abhishek.kumar14@mindtree.com>
2019-05-07 14:48:40 +0530

    added identity schema validator
    Former-commit-id: 83d105748a11603eebb8c31ff5c297a7411acdf7
    Former-commit-id: 832cd998b884ed4cf3a58bba026d711c79915468

--- admin/admin-service/src/main/java/io/mosip/admin/AdminBootApplication.java
+++ admin/admin-service/src/main/java/io/mosip/admin/AdminBootApplication.java
@@ -2,12 +2,14 @@ package io.mosip.admin;
 
 import java.util.concurrent.Executor;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import io.mosip.admin.iddefinition.JsonIdentitySchemaValidator;
 
 @SpringBootApplication(scanBasePackages = { "io.mosip.admin.*", "io.mosip.kernel.auth.*" })
 @EnableAsync
@@ -24,5 +26,14 @@ public class AdminBootApplication {
 		executor.setThreadNamePrefix("Admin-Async-Thread-");
 		executor.initialize();
 		return executor;
+	}
+
+	@Bean
+	public CommandLineRunner runner(JsonIdentitySchemaValidator validator) {
+		return args -> {
+			String schema = "{\"$id\":\"http://mosip.io/id_object/1.0/id_object.json\",\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"title\": \"MOSIP ID schema\",\"description\":\"TestIDschematoreferto\",\"type\":\"object\",\"additionalProperties\":false,\"properties\":{\"identity\":{\"title\":\"identity\",\"description\":\"ThisholdsalltheattributesofanIdentity\",\"type\":\"object\",\"additionalProperties\":false,\"properties\":{\"firstName\":{\"$ref\":\"#/definitions/values\"},\"middleName\":{\"$ref\":\"#/definitions/values\"},\"lastName\":{\"$ref\":\"#/definitions/values\"},\"dateOfBirth\":{\"$ref\":\"#/definitions/values\"},\"gender\":{\"$ref\":\"#/definitions/values\"},\"addressLine1\":{\"$ref\":\"#/definitions/values\"},\"addressLine2\":{\"$ref\":\"#/definitions/values\"},\"addressLine3\":{\"$ref\":\"#/definitions/values\"},\"region\":{\"$ref\":\"#/definitions/values\"},\"province\":{\"$ref\":\"#/definitions/values\"},\"city\":{\"$ref\":\"#/definitions/values\"},\"localAdministrativeAuthority\":{\"$ref\":\"#/definitions/values\"},\"mobileNumber\":{\"$ref\":\"#/definitions/values\"},\"emailId\":{\"$ref\":\"#/definitions/values\"},\"CNEOrPINNumber\":{\"$ref\":\"#/definitions/values\"},\"parentOrGuardianName\":{\"$ref\":\"#/definitions/values\"},\"parentOrGuardianRIDOrUIN\":{\"$ref\":\"#/definitions/values\"},\"leftEye\":{\"$ref\":\"#/definitions/values\"},\"rightEye\":{\"$ref\":\"#/definitions/values\"},\"biometricScan1\":{\"$ref\":\"#/definitions/values\"},\"biometricScan2\":{\"$ref\":\"#/definitions/values\"},\"biometricScan3\":{\"$ref\":\"#/definitions/values\"}}}},\"definitions\":{\"values\":{\"type\":\"array\",\"additionalItems\":false,\"uniqueItems\":true,\"items\":{\"type\":\"object\",\"required\":[\"language\",\"label\",\"value\"],\"additionalProperties\":false,\"properties\":{\"language\":{\"type\":\"string\"},\"label\":{\"type\":\"string\"},\"value\":{\"type\":\"string\"}}}}}}";
+			boolean result = validator.validateIdentitySchema(schema);
+			System.out.println(result);
+		};
 	}
 }
