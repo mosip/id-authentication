1f69d891da904a517d55638aa8d72f20d96bb87f
TaleevAaalam <Taleev.Aalam@mindtree.com>
2019-05-08 13:57:32 +0530

    MOS-23734 : fixed for defects
    Former-commit-id: 4777279c5778321ad7e6c0f8c65655f8093c6b5e
    Former-commit-id: a8f20376f87befc9a66f26dda31a369ce2078c64

--- registration/registration-client/src/main/java/io/mosip/registration/controller/reg/DateValidation.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/reg/DateValidation.java
@@ -53,18 +53,26 @@ public class DateValidation extends BaseController {
 	 *            the local field to be populated if input is valid.
 	 */
 	public void validateDate(Pane parentPane, TextField date, TextField month, TextField year, Validations validations,
-			FXUtils fxUtils, TextField localField,TextField ageField,TextField ageLocalField) {
+			FXUtils fxUtils, TextField localField, TextField ageField, TextField ageLocalField) {
 
 		try {
 			fxUtils.validateOnType(parentPane, date, validation, localField, false);
 			date.textProperty().addListener((obsValue, oldValue, newValue) -> {
-				yearValidator(date, month, year,ageField,ageLocalField);
+				if (!yearValidator(date, month, year, ageField, ageLocalField)
+						&& dateMonthYearNotNullOrEmpty(date, month, year)) {
+					date.setText(oldValue);
+				}
 			});
 		} catch (RuntimeException runTimeException) {
 			LOGGER.error(LoggerConstants.DATE_VALIDATION, APPLICATION_NAME, RegistrationConstants.APPLICATION_ID,
 					runTimeException.getMessage() + ExceptionUtils.getStackTrace(runTimeException));
 
 		}
+	}
+
+	private boolean dateMonthYearNotNullOrEmpty(TextField date, TextField month, TextField year) {
+		return (date != null && !date.getText().isEmpty() && month != null && !month.getText().isEmpty() && year != null
+				&& !year.getText().isEmpty());
 	}
 
 	/**
@@ -91,7 +99,10 @@ public class DateValidation extends BaseController {
 		try {
 			fxUtils.validateOnType(parentPane, month, validation, localField, false);
 			month.textProperty().addListener((obsValue, oldValue, newValue) -> {
-				yearValidator(date, month, year, ageField, ageLocalField);
+				if (!yearValidator(date, month, year, ageField, ageLocalField)
+						&& dateMonthYearNotNullOrEmpty(date, month, year)) {
+					month.setText(oldValue);
+				}
 			});
 		} catch (RuntimeException runTimeException) {
 			LOGGER.error(LoggerConstants.DATE_VALIDATION, APPLICATION_NAME, RegistrationConstants.APPLICATION_ID,
@@ -100,7 +111,6 @@ public class DateValidation extends BaseController {
 		}
 	}
 
-	
 	/**
 	 * Validates the date
 	 *
@@ -110,23 +120,21 @@ public class DateValidation extends BaseController {
 	 *            the month {@link TextField}
 	 * @param year
 	 *            the year {@link TextField}
-	 * @param ageLocalField 
-	 * @param ageField 
+	 * @param ageLocalField
+	 * @param ageField
 	 */
-	private void yearValidator(TextField date, TextField month, TextField year, TextField ageField,
+	private boolean yearValidator(TextField date, TextField month, TextField year, TextField ageField,
 			TextField ageLocalField) {
 		try {
-			int yearVal;
 			LocalDate localDate = LocalDate.now();
-			if (year != null && year.getText().matches(RegistrationConstants.FOUR_NUMBER_REGEX)) {
-				yearVal = Integer.parseInt(year.getText());
-				monthValidator(date, month, yearVal, localDate,ageField,ageLocalField);
+			if (year != null) {
+				return monthValidator(date, month, year, localDate, ageField, ageLocalField);
 			}
 		} catch (RuntimeException runTimeException) {
 			LOGGER.error(LoggerConstants.DATE_VALIDATION, APPLICATION_NAME, RegistrationConstants.APPLICATION_ID,
 					runTimeException.getMessage() + ExceptionUtils.getStackTrace(runTimeException));
-
 		}
+		return false;
 	}
 
 	/**
@@ -136,22 +144,30 @@ public class DateValidation extends BaseController {
 	 *            the date(dd) {@link TextField}
 	 * @param month
 	 *            the month {@link TextField}
-	 * @param ageLocalField 
-	 * @param ageField 
+	 * @param ageLocalField
+	 * @param ageField
 	 * @param year
 	 *            the year {@link TextField}
 	 * @param LocalDate
 	 *            the localDate {@link LocalDate}
 	 */
-	private void monthValidator(TextField date, TextField month, int yearVal, LocalDate localDate, TextField ageField,
-			TextField ageLocalField) {
+	private boolean monthValidator(TextField date, TextField month, TextField year, LocalDate localDate,
+			TextField ageField, TextField ageLocalField) {
+		int yearVal=0;
+		if(year.getText().matches(RegistrationConstants.FOUR_NUMBER_REGEX)) {
+			yearVal  = Integer.parseInt(year.getText());
+		}
+		if(yearVal==0)
+			return true;
 		if (month != null && yearVal == localDate.getYear()
 				&& month.getText().matches(RegistrationConstants.NUMBER_REGEX)
 				&& Integer.parseInt(month.getText()) > localDate.getMonth().getValue()) {
 			month.setText(RegistrationConstants.ONE);
 		} else {
-			dateValdidator(date, Integer.parseInt(month.getText()), yearVal, localDate, ageField, ageLocalField);
+			return dateValdidator(date, month, year, Integer.parseInt(month.getText()), yearVal, localDate, ageField,
+					ageLocalField);
 		}
+		return false;
 	}
 
 	/**
@@ -159,31 +175,34 @@ public class DateValidation extends BaseController {
 	 *
 	 * @param date
 	 *            the date(dd) {@link TextField}
-	 * @param ageLocalField 
-	 * @param ageField 
+	 * @param ageLocalField
+	 * @param ageField
 	 * @param month
 	 *            the month {@link TextField}
 	 * @param LocalDate
 	 *            the localDate {@link LocalDate}
 	 */
-	private void dateValdidator(TextField date, int monthVal, int year, LocalDate localDate, TextField ageField,
-			TextField ageLocalField) {
+	private boolean dateValdidator(TextField date, TextField month, TextField year, int monthVal, int yearValue,
+			LocalDate localDate, TextField ageField, TextField ageLocalField) {
 		if (date != null && monthVal == localDate.getMonth().getValue()
 				&& date.getText().matches(RegistrationConstants.NUMBER_REGEX)
 				&& Integer.parseInt(date.getText()) > localDate.getDayOfMonth()) {
 			date.setText(RegistrationConstants.ONE);
 		} else {
-			int dateValue = Integer.parseInt(date.getText());
-			int age = Period.between(LocalDate.of(year, monthVal, dateValue), LocalDate.now()).getYears();
-			if (age > 0) {
-				ageField.setText(RegistrationConstants.EMPTY + age);
-				ageLocalField.setText(RegistrationConstants.EMPTY + age);
-			} else {
-				ageField.setText("1");
-				ageLocalField.setText("1");
-			}
-
+				int dateValue = Integer.parseInt(date.getText());
+				int age = Period.between(LocalDate.of(yearValue, monthVal, dateValue), LocalDate.now()).getYears();
+				int maxAge = Integer.parseInt(getValueFromApplicationContext(RegistrationConstants.MAX_AGE));
+				if (age > maxAge || age == 0) {
+					ageField.setText("");
+					ageLocalField.setText("");
+					return false;
+				} else {
+					ageField.setText(RegistrationConstants.EMPTY + age);
+					ageLocalField.setText(RegistrationConstants.EMPTY + age);
+					return true;
+				}
 		}
+		return false;
 	}
 
 	/**
@@ -210,16 +229,10 @@ public class DateValidation extends BaseController {
 		try {
 			fxUtils.validateOnType(parentPane, year, validation, localField, false);
 			year.textProperty().addListener((obsValue, oldValue, newValue) -> {
-				if (year.getText().matches(RegistrationConstants.FOUR_NUMBER_REGEX)) {
-					int yearVal = Integer.parseInt(year.getText());
-					LocalDate localDate = LocalDate.now();
-					int minYear = localDate.getYear()
-							- Integer.parseInt(getValueFromApplicationContext(RegistrationConstants.MAX_AGE));
-					if (yearVal < minYear || yearVal > localDate.getYear()) {
-						year.setText(oldValue);
-					}
+				if (!yearValidator(date, month, year, ageField, ageLocalField)
+						&& dateMonthYearNotNullOrEmpty(date, month, year)) {
+					year.setText(oldValue);
 				}
-				yearValidator(date, month, year, ageField, ageLocalField);
 			});
 		} catch (RuntimeException runTimeException) {
 			LOGGER.error(LoggerConstants.DATE_VALIDATION, APPLICATION_NAME, RegistrationConstants.APPLICATION_ID,
