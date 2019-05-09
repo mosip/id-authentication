5d535339815edd5c9d2d436a4f67cb5886a019aa
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-05-09 15:56:18 +0530

    mos-0000 admin mofified
    Former-commit-id: b2799c45aa3a2765bdb9cdf3b51ee230f0efa3e0
    Former-commit-id: 1e86504e66ff48f9920a8be0c17aaad49df70d08

--- admin/admin-service/src/main/java/io/mosip/admin/usermgmt/service/impl/UserRegistrationServiceImpl.java
+++ admin/admin-service/src/main/java/io/mosip/admin/usermgmt/service/impl/UserRegistrationServiceImpl.java
@@ -1,7 +1,5 @@
 package io.mosip.admin.usermgmt.service.impl;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import io.mosip.admin.usermgmt.dto.UserRegistrationRequestDto;
 import io.mosip.admin.usermgmt.dto.UserRegistrationResponseDto;
 import io.mosip.admin.usermgmt.service.UserRegistrationService;
