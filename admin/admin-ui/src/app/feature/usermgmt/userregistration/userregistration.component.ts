1006117ddd32396931cadb61c44478a7d6518329
Ritesh Sinha <M1044345@mindtree.com>
2019-05-09 08:21:52 +0530

    MOS-000 User registration module form modified https://mosipid.atlassian.net/browse/MOS-000 U
    Former-commit-id: 9964eacb12d533ce74382a9322ee7362027f800e
    Former-commit-id: f3580e6c1a545126be34773253951f7cf2259cb9

--- admin/admin-ui/src/app/feature/usermgmt/userregistration/userregistration.component.ts
+++ admin/admin-ui/src/app/feature/usermgmt/userregistration/userregistration.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormGroup,FormBuilder,Validators } from '@angular/forms';
 @Component({
   selector: 'app-userregistration',
   templateUrl: './userregistration.component.html',
   styleUrls: ['./userregistration.component.css']
 })
 export class UserregistrationComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
+  submitted = false;
+  userRegistrationForm:FormGroup;
+  constructor(private router: Router,private formBuilder:FormBuilder) {
+    this.userRegistrationForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      contactNumber:['',[Validators.required,Validators.pattern("^[0-9]*$")]],
+      emailID:['',[Validators.required,Validators.email]],
+      DOB:['',Validators.required],
+      userName:['',Validators.required]
+    });
+   }
+ 
+ 
   ngOnInit() {
   }
   onSubmit(){
-
+    console.log("form submitted");
+this.submitted=true;
+if(this.userRegistrationForm.invalid)
+{
+  return;
+}
   }
   cancel(){
     this.router.navigateByUrl('');
+  }
+  get emailID(){
+    return this.userRegistrationForm.get('emailID');
   }
 }
