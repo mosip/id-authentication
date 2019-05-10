086a5cd943e6559b6bdf349d0a255d9cc53abf5e
Manoj SP <Manoj.SP@mindtree.com>
2019-05-10 12:43:46 +0530

    MOS-23423 - added vid policy provider
    Former-commit-id: bd05c794101876bf9d6ac13260b0a57c46176051
    Former-commit-id: 357d20948c6e63af69de431d77788eb2c84ec5c3

---
+++ id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/provider/VidPolicyProvider.java
@@ -0,0 +1,62 @@
+package io.mosip.idrepository.vid.provider;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+
+import io.mosip.idrepository.vid.dto.VidPolicy;
+
+/**
+ * @author Manoj SP
+ *
+ */
+@Component
+public class VidPolicyProvider {
+	
+	private static final Configuration READ_LIST_OPTIONS = Configuration.defaultConfiguration()
+			.addOptions(Option.SUPPRESS_EXCEPTIONS, Option.ALWAYS_RETURN_LIST);
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	private Map<String, VidPolicy> vidPolicies;
+	
+	@PostConstruct
+	public void policyDetails() throws IOException, URISyntaxException {
+		FileReader jsonFile = new FileReader(
+				new File(new URL(env.getProperty("mosip.idrepo.vid.policy-file-location")).toURI()));
+		JsonNode policyJson = mapper.readValue(jsonFile, JsonNode.class);
+		List<String> vidType = JsonPath.compile("vidPolicies.*.vidType").read(policyJson.toString(), READ_LIST_OPTIONS);
+		List<Object> vidPolicy = JsonPath.compile("vidPolicies.*.vidPolicy").read(policyJson.toString(),
+				READ_LIST_OPTIONS);
+		vidPolicies = IntStream.range(0, vidType.size()).parallel()
+			.boxed()
+			.collect(Collectors.toMap(i -> vidType.get(i),
+					i -> mapper.convertValue(vidPolicy.get(i), VidPolicy.class)));
+	}
+	
+	public VidPolicy getPolicy(String vidType) {
+		return vidPolicies.get(vidType);
+	}
+}
