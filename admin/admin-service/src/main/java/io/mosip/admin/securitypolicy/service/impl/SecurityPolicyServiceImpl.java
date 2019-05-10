f603b8e46c09aa04b9d2bd1cca097ffe424ca2e2
abhishek kumar <abhishek.kumar14@mindtree.com>
2019-05-10 18:35:24 +0530

    added authentication factor
    Former-commit-id: e71013ba643fc71aee1fc1aeeae71e0f2431ca37
    Former-commit-id: fbe4ae996437ece5f898bcbe10a46d7bdae31fe6

---
+++ admin/admin-service/src/main/java/io/mosip/admin/securitypolicy/service/impl/SecurityPolicyServiceImpl.java
@@ -0,0 +1,96 @@
+package io.mosip.admin.securitypolicy.service.impl;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.mosip.admin.securitypolicy.config.PolicyProperties;
+import io.mosip.admin.securitypolicy.dto.AuthFactorsDto;
+import io.mosip.admin.securitypolicy.dto.UserRoleDto;
+import io.mosip.admin.securitypolicy.dto.UserRoleResponseDto;
+import io.mosip.admin.securitypolicy.exception.SecurityPolicyException;
+import io.mosip.admin.securitypolicy.service.SecurityPolicyService;
+import io.mosip.kernel.core.exception.ServiceError;
+
+@Service
+public class SecurityPolicyServiceImpl implements SecurityPolicyService {
+
+	@Autowired
+	private PolicyProperties policyProps;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${mosip.admin.security.policy.userrole-auth-url}")
+	private String userRoleAuthServiceUrl;
+
+	@Value("${mosip.admin.app-id}")
+	private String appId;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * io.mosip.admin.securitypolicy.service.SecurityPolicyService#getSecurityPolicy
+	 * (java.lang.String)
+	 */
+	@Override
+	public AuthFactorsDto getSecurityPolicy(String username) {
+		AuthFactorsDto securityPolicyDto = null;
+		UserRoleDto userRole = null;
+		Set<String> policies = null;
+		Set<String> authTypes = null;
+		UserRoleResponseDto responseWrapper = null;
+
+		try {
+			responseWrapper = restTemplate.getForObject(userRoleAuthServiceUrl, UserRoleResponseDto.class, appId, username);
+		} catch (RestClientException e) {
+			throw new SecurityPolicyException("XX1", "XX", e);
+		}
+		if(responseWrapper.getErrors()!=null && !responseWrapper.getErrors().isEmpty()) {
+			ServiceError error = responseWrapper.getErrors().get(0);
+			throw new SecurityPolicyException(error.getErrorCode(), error.getMessage());
+		}
+		userRole = responseWrapper.getResponse();
+		Set<String> roles = Arrays.stream(userRole.getRole().split(",")).map(String::toUpperCase)
+				.collect(Collectors.toSet());
+		Map<String, String> rolePolicy = policyProps.getRolePolicyMapping();
+		Map<String, Set<String>> policyAuth = policyProps.getPolicyAuth();
+		policies = roles.stream().map(rolePolicy::get).filter(Objects::nonNull).collect(Collectors.toSet());
+		authTypes = new HashSet<>();
+		if (policies != null && !policies.isEmpty()) {
+			for (String policy : policies) {
+				String[] pArray = policy.split(",");
+				for (String p : pArray) {
+					Set<String> auth = policyAuth.get(p);
+					if (auth != null)
+						authTypes.addAll(auth);
+				}
+			}
+		} else {
+			throw new SecurityPolicyException("XX1", "No Policy Found for the role :" + roles);
+		}
+		securityPolicyDto = new AuthFactorsDto();
+		securityPolicyDto.setAuthTypes(authTypes);
+		return securityPolicyDto;
+	}
+	
+	public ObjectMapper mapper() {
+		return null;
+	}
+
+}
