54b2ecbbed8c3c12a5f83bc27eda465c2d8b7c74
kiranrajbs <kiranraj.s@mindtree.com>
2019-05-10 13:21:20 +0530

    MOS-23712 : Code Updated and config issue resolved
    Former-commit-id: 6185e7afa3c83593ac8d4359fac40b32eec84155
    Former-commit-id: b0cb8c2f8b0fe9048fbaead4333f4ffb932b86e1

--- registration-processor/core-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/RestAbisApplication.java
+++ registration-processor/core-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/RestAbisApplication.java
@@ -1,34 +1,24 @@
 package io.mosip.registration.processor.abis;
 
-import javax.annotation.PostConstruct;
+import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import io.mosip.registration.processor.abis.messagequeue.AbisMessageQueueImpl;
-import io.mosip.registration.processor.packet.storage.config.PacketStorageBeanConfig;
-import io.mosip.registration.processor.rest.client.config.RestConfigBean;
-import io.mosip.registration.processor.status.config.RegistrationStatusBeanConfig;
 
-@SpringBootApplication
-@ComponentScan(basePackages = { "io.mosip.registration.processor.*" }, excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, value = {
-        RegistrationStatusBeanConfig.class, RestConfigBean.class, PacketStorageBeanConfig.class, RestConfigBean.class }))
 public class RestAbisApplication {
 	
-	@Autowired
-	AbisMessageQueueImpl abisMessageQueueImpl;
-   
-	public static void main( String[] args )
+	public static void main( String[] args ) throws IOException
     {
-        SpringApplication.run(RestAbisApplication.class, args);
-    }
-    
-    @PostConstruct
-    public void runAbisQueue() {
-    	abisMessageQueueImpl.runAbisQueue();
-    }
+		AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
+		configApplicationContext.scan("io.mosip.registration.processor.abis.config","io.mosip.registration.processor.stages.config",
+				"io.mosip.registration.processor.status.config",
+				"io.mosip.registration.processor.packet.storage.config", "io.mosip.registration.processor.core.config",
+				"io.mosip.registration.processor.core.kernel.beans"
+				);
+		configApplicationContext.refresh();
+		AbisMessageQueueImpl abisMessageQueueImpl = configApplicationContext.getBean(AbisMessageQueueImpl.class);
+		abisMessageQueueImpl.runAbisQueue();
+    }  
 }
+
--- registration-processor/core-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/messagequeue/AbisMessageQueueImpl.java
+++ registration-processor/core-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/messagequeue/AbisMessageQueueImpl.java
@@ -1,5 +1,9 @@
 package io.mosip.registration.processor.abis.messagequeue;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.jms.Message;
 
 import org.apache.activemq.command.ActiveMQBytesMessage;
@@ -22,153 +26,148 @@ import io.mosip.registration.processor.core.packet.dto.abis.AbisInsertRequestDto
 import io.mosip.registration.processor.core.packet.dto.abis.AbisInsertResponseDto;
 import io.mosip.registration.processor.core.queue.factory.MosipQueue;
 import io.mosip.registration.processor.core.queue.factory.QueueListener;
-import io.mosip.registration.processor.core.spi.queue.MosipQueueConnectionFactory;
 import io.mosip.registration.processor.core.spi.queue.MosipQueueManager;
 import io.mosip.registration.processor.core.util.JsonUtil;
+import io.mosip.registration.processor.packet.storage.utils.Utilities;
 
 /**
- * The AbisMessageQueueImpl class
- *
+ * The AbisMessageQueueImpl class.
+ * @author jyoti-prakash
+ * @author Kiran Raj
  */
 @Component
 public class AbisMessageQueueImpl {
+	
+	/** The username. */
 	@Value("${registration.processor.queue.username}")
 	private String username;
 
+	/** The password. */
 	@Value("${registration.processor.queue.password}")
 	private String password;
 
+	/** The url. */
 	@Value("${registration.processor.queue.url}")
 	private String url;
 
+	/** The type of queue. */
 	@Value("${registration.processor.queue.typeOfQueue}")
 	private String typeOfQueue;
 
-	@Value("${registration.processor.abis.inbound.queue1}")
-	private String abis1;
-	
-	@Value("${registration.processor.abis.inbound.queue2}")
-	private String abis2;
-	
-	@Value("${registration.processor.abis.inbound.queue3}")
-	private String abis3;
-	
-	@Value("${registration.processor.abis.outbound.queue1}")
-	private String abismiddlewareaddress1;
-	
-	@Value("${registration.processor.abis.outbound.queue2}")
-	private String abismiddlewareaddress2;
-	
-	@Value("${registration.processor.abis.outbound.queue3}")
-	private String abismiddlewareaddress3;
-	
+	/** The utilities. */
+	@Autowired
+	Utilities utilities;
+
+	/** The abis service. */
 	@Autowired
 	AbisService abisService;
-	
+
 	/** The mosip queue manager. */
 	@Autowired
 	private MosipQueueManager<MosipQueue, byte[]> mosipQueueManager;
 
-	/** The mosip connection factory. */
-	@Autowired
-	private MosipQueueConnectionFactory<MosipQueue> mosipConnectionFactory;
-	
-	private MosipQueue queue;
-	
+	/** The Constant ABIS_INSERT. */
 	private static final String ABIS_INSERT = "mosip.abis.insert";
-	
+
+	/** The Constant ABIS_IDENTIFY. */
 	private static final String ABIS_IDENTIFY = "mosip.abis.identify";
-	
+
+	/** The Constant ID. */
 	private static final String ID = "id";
-	
+
+	/** The abis insert request dto. */
 	AbisInsertRequestDto abisInsertRequestDto;
-	
+
+	/** The identify request dto. */
 	AbisIdentifyRequestDto identifyRequestDto;
-	
+
 	/** The reg proc logger. */
 	private static Logger regProcLogger = RegProcessorLogger.getLogger(AbisMessageQueueImpl.class);
-	
-	boolean isConnection = false;
-	
-	private MosipQueue getQueueConnection() {
-		return mosipConnectionFactory.createConnection(typeOfQueue, username, password, url);
-	}
-	
-	public void runAbisQueue() {
 
-		queue = getQueueConnection();
-		if (queue != null) {
-			QueueListener listener1 = new QueueListener() {
-				@Override
-				public void setListener(Message message) {
-					consumeLogic(message,abismiddlewareaddress1);
-				}
-			};
-			
-			QueueListener listener2 = new QueueListener() {
-				@Override
-				public void setListener(Message message) {
-					consumeLogic(message,abismiddlewareaddress2);
-				}
-			};
-			
-			QueueListener listener3 = new QueueListener() {
-				@Override
-				public void setListener(Message message) {
-					consumeLogic(message,abismiddlewareaddress3);
-				}
-			};
-		
-			mosipQueueManager.consume(queue, abis1, listener1);
-			mosipQueueManager.consume(queue, abis2, listener2);
-			mosipQueueManager.consume(queue, abis3, listener3);
+	/** The is connection. */
+	boolean isConnection = false;
+
+	/**
+	 * Run abis queue.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public void runAbisQueue() throws IOException {
+
+		List<String> abisInboundAddresses;
+		List<String> abisOutboundAddresses;
+
+		List<List<String>> inboundOutBoundAddressList = utilities.getInboundOutBoundAddressList();
+		List<MosipQueue> mosipQueseList = utilities.getMosipQueuesForAbis();
+		if (mosipQueseList != null) {
+
+			abisInboundAddresses = inboundOutBoundAddressList.get(0);
+			abisOutboundAddresses = inboundOutBoundAddressList.get(1);
+			for (int i = 0; i < abisOutboundAddresses.size(); i++) {
+				String outBoundAddress = abisOutboundAddresses.get(i);
+				MosipQueue queue = mosipQueseList.get(i);
+				QueueListener listener = new QueueListener() {
+					@Override
+					public void setListener(Message message) {
+						consumeLogic(message, outBoundAddress,queue);
+					}
+				};
+				mosipQueueManager.consume(mosipQueseList.get(i), abisInboundAddresses.get(i), listener);
+			}
+
 			isConnection = true;
 		} else {
 			throw new QueueConnectionNotFound(PlatformErrorMessages.RPR_PRT_QUEUE_CONNECTION_NULL.getMessage());
 		}
 	}
 
+	/**
+	 * Consume logic.
+	 *
+	 * @param message the message
+	 * @param abismiddlewareaddress the abismiddlewareaddress
+	 * @param queue the queue
+	 * @return true, if successful
+	 */
 	@SuppressWarnings("unchecked")
-	public boolean consumeLogic(Message message, String abismiddlewareaddress) {
+	public boolean consumeLogic(Message message, String abismiddlewareaddress,MosipQueue queue) {
 		boolean isrequestAddedtoQueue = false;
-		String response=null;			
-		
+		String response = null;
+
 		String request = new String(((ActiveMQBytesMessage) message).getContent().data);
-		
-		regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
-				"","---received---"+request);
+
+		regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(), "",
+				"---received---" + request);
 		try {
-			JSONObject object=JsonUtil.objectMapperReadValue(request, JSONObject.class);
-			ObjectMapper obj = new ObjectMapper(); 
-			String id= (String)object.get(ID);
-			if(id.matches(ABIS_INSERT)) {
+			JSONObject object = JsonUtil.objectMapperReadValue(request, JSONObject.class);
+			ObjectMapper obj = new ObjectMapper();
+			String id = (String) object.get(ID);
+			if (id.matches(ABIS_INSERT)) {
 				abisInsertRequestDto = JsonUtil.objectMapperReadValue(request, AbisInsertRequestDto.class);
 				AbisInsertResponseDto abisInsertResponseDto = abisService.insert(abisInsertRequestDto);
-				response=obj.writeValueAsString(abisInsertResponseDto);
+				response = obj.writeValueAsString(abisInsertResponseDto);
 			}
-			
-			else if(id.matches(ABIS_IDENTIFY)) {
+
+			else if (id.matches(ABIS_IDENTIFY)) {
 				identifyRequestDto = JsonUtil.objectMapperReadValue(request, AbisIdentifyRequestDto.class);
 				AbisIdentifyResponseDto identifyResponseDto = abisService.performDedupe(identifyRequestDto);
-				response=obj.writeValueAsString(identifyResponseDto);
+				response = obj.writeValueAsString(identifyResponseDto);
 			}
-			
+
 			else {
 				object.put("respoQueueConnectionNotFoundnse", "invalid request");
-				response=obj.writeValueAsString(object);
+				response = obj.writeValueAsString(object);
 			}
-			
-			regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
-					"","---response---"+response);
 
-			isrequestAddedtoQueue = mosipQueueManager.send(queue,  response.getBytes("UTF-8"),
-						abismiddlewareaddress);
+			regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
+					"", "---response---" + response);
+
+			isrequestAddedtoQueue = mosipQueueManager.send(queue, response.getBytes("UTF-8"), abismiddlewareaddress);
 		} catch (Exception e) {
 			regProcLogger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
-					e.getMessage(),e.getStackTrace().toString());
+					e.getMessage(), Arrays.toString(e.getStackTrace()));
 		}
-		
-			
+
 		return isrequestAddedtoQueue;
 	}
 
