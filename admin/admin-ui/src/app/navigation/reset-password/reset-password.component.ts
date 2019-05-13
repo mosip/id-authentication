3d84cf47e9d2a9bff9ea1a2e6003041cf3f9a017
mosip-sagarmahapatra <Sagar.Mahapatra2@mindtree.com>
2019-05-14 01:08:38 +0530

    MOS-0000 Validations & Type Added
    Former-commit-id: 01c4bb9921886ff7423e28d926c099e43c4e8098
    Former-commit-id: 9a6150e5eab3b7ea9e3e453cdcc88dbe243593f6

--- admin/admin-ui/src/app/navigation/reset-password/reset-password.component.ts
+++ admin/admin-ui/src/app/navigation/reset-password/reset-password.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material';
+import {FormBuilder, Validators, FormGroup, AbstractControl} from '@angular/forms';
 import { Router } from '@angular/router';
+import { passwordValidator } from '../../shared/validators/password.validator';
 
 @Component({
   selector: 'app-reset-password',
@@ -10,18 +9,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
+  resetPasswordForm: FormGroup;
+  passwordPattern = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$';
 
-  constructor(private router: Router, private formBuilder: FormBuilder, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-      'thumbs-up',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/img/examples/thumbup-icon.svg'));
-  }
-  resetPasswordForm = this.formBuilder.group({
-    password: ['', [Validators.required]],
-    confirmPassword: ['', [Validators.required]]
-  });
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
   ngOnInit() {
+    this.resetPasswordForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.pattern(this.passwordPattern)]],
+      confirmPassword: ['', [Validators.required]]
+    }, {validator: [passwordValidator]});
   }
+
+   lengthValidator(control: AbstractControl): {[key: string]: boolean} | null {
+    const password = control.get('password');
+      return password.value.length >= 8 ? { 'passWordLength': true } : null;
+    }
+
   onSave() {
     this.router.navigate(['login']);
   }
