44a669286bc9ad24b14c8918f07c43d85497852f
Omsaieswar <omsaieswar.mulakaluri@mindtree.com>
2019-05-13 09:40:47 +0530

    MOS-23534:Added the reuqired logs
    Former-commit-id: f4a380e9a1471bfc2a4f5ead96aa93b43841cf17
    Former-commit-id: 163fcfa5f352a163498b6a3dac898a493d2ec73f

--- registration/registration-services/src/main/java/io/mosip/registration/config/DaoConfig.java
+++ registration/registration-services/src/main/java/io/mosip/registration/config/DaoConfig.java
@@ -1,5 +1,8 @@
 package io.mosip.registration.config;
 
+import static io.mosip.registration.constants.RegistrationConstants.APPLICATION_ID;
+import static io.mosip.registration.constants.RegistrationConstants.APPLICATION_NAME;
+
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -12,6 +15,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig;
 
 /**
@@ -22,26 +26,37 @@ import io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig;
  *
  */
 public class DaoConfig extends HibernateDaoConfig {
+	/**
+	 * Instance of LOGGER
+	 */
+	private static final Logger LOGGER = AppConfig.getLogger(DaoConfig.class);
+	
+	private static final String DRIVER_CLASS_NAME = "org.apache.derby.jdbc.EmbeddedDriver";
+	private static final String DB_PATH_VAR = "mosip.dbpath";
+	private static final String URL = "jdbc:derby:";
+	private static final String DB_AUTHENITICATION = ";bootPassword=mosip12345";
 
 	/**
 	 * instance of datasource
 	 */
 	private static DataSource dataSource;
-	
+
 	/**
 	 * connection of datasource
 	 */
 	static {
-		String dbPath = System.getProperty("mosip.dbpath");
 		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-		driverManagerDataSource.setDriverClassName("org.apache.derby.jdbc.EmbeddedDriver");
-		driverManagerDataSource.setUrl("jdbc:derby:"+dbPath+";bootPassword=mosip12345");
+		driverManagerDataSource.setDriverClassName(DRIVER_CLASS_NAME);
+		driverManagerDataSource.setUrl(URL +System.getProperty(DB_PATH_VAR) + DB_AUTHENITICATION);
 
 		dataSource = driverManagerDataSource;
 	}
 
-	/* (non-Javadoc)
-	 * @see io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig#dataSource()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig#dataSource()
 	 */
 	@Override
 	@Bean(name = "dataSource")
@@ -77,14 +92,13 @@ public class DaoConfig extends HibernateDaoConfig {
 	@Bean
 	@Lazy(false)
 	public static PropertyPlaceholderConfigurer properties() {
-		String profile = System.getProperty("spring.profiles.active") != null ? 
-				System.getProperty("spring.profiles.active") :
-			"integ";
-				
-		System.out.println("--------------------- Spring - " + profile + "---------------- properties loaded");
+		String profile = System.getProperty("spring.profiles.active");
+
+		LOGGER.info("REGISTRATION - DAO Config", APPLICATION_NAME, APPLICATION_ID, 
+				" spring profile loading with environment spring-" + profile + "loaded");
+		
 		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		Resource[] resources = new ClassPathResource[] { new ClassPathResource("spring.properties") , 
-				new ClassPathResource("spring-"+ profile + ".properties")};
+		Resource[] resources = new ClassPathResource[] {new ClassPathResource("spring-" + profile + ".properties") };
 		ppc.setLocations(resources);
 
 		Properties properties = new Properties();
