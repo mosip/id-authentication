a48fd22343e2fc245b9b19ac40d580a9fd3d3c51
Mosip-Ritesh <Ritesh.Sinha@mindtree.com>
2019-05-13 23:48:55 +0530

    MOS-000 user registration api modified
    Former-commit-id: 583471331180c80aa6ec1c3336d6097d1c9e2ecb
    Former-commit-id: 852424401464ba8760fbda57c4845a64e482bbfe

--- admin/admin-service/src/main/java/io/mosip/admin/usermgmt/service/impl/UserRegistrationServiceImpl.java
+++ admin/admin-service/src/main/java/io/mosip/admin/usermgmt/service/impl/UserRegistrationServiceImpl.java
@@ -1,6 +1,5 @@
 package io.mosip.admin.usermgmt.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +11,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.mosip.admin.usermgmt.dto.RidVerificationRequestDto;
 import io.mosip.admin.usermgmt.dto.UserRegistrationRequestDto;
 import io.mosip.admin.usermgmt.dto.UserRegistrationResponseDto;
+import io.mosip.admin.usermgmt.exception.AdminListOfServiceException;
 import io.mosip.admin.usermgmt.service.UserRegistrationService;
 import io.mosip.admin.usermgmt.util.UserMgmtUtil;
 import io.mosip.kernel.core.exception.ExceptionUtils;
+import io.mosip.kernel.core.exception.ServiceError;
 import io.mosip.kernel.core.http.RequestWrapper;
 
 @Service
@@ -33,49 +30,34 @@ public class UserRegistrationServiceImpl implements UserRegistrationService {
 
 	@Autowired
 	private UserMgmtUtil validateRidUtil;
-	
-	@Autowired
-	private ObjectMapper mapper;
 
 	@Override
 	public UserRegistrationResponseDto register(UserRegistrationRequestDto request) {
-		try {
-			String authUserRegistrationUrl = "http://localhost:8091/v1/authmanager/user";
-			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.APPLICATION_JSON);
-			RequestWrapper<UserRegistrationRequestDto> userRegistrationRequestDto = new RequestWrapper<>();
-			userRegistrationRequestDto.setRequest(request);
-			HttpEntity<RequestWrapper<UserRegistrationRequestDto>> userHttpEntity = new HttpEntity<>(
-					userRegistrationRequestDto, headers);
-			ResponseEntity<String> response = restTemplate.postForEntity(authUserRegistrationUrl, userHttpEntity,
-					String.class);
-			String responseBody=response.getBody();
-			System.out.println(ExceptionUtils.getServiceErrorList(responseBody));
-			JsonNode authResponse=mapper.readTree(responseBody);
-			authResponse=authResponse.get("response");
-			String responseStatus=authResponse.get("status").asText();
-			
-			if(responseStatus.equals("SUCCESS")) {
-				headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-				MultiValueMap<Object, Object> emailMap = new LinkedMultiValueMap<>();
-				emailMap.add("mailContent", "YOUR REGISTRATION ACTIVATION LINK IS " + request.getRidValidationUrl()
-						+ "?username=" + request.getUserName());
-				emailMap.add("mailSubject", "ADMIN USER REGISTRATION ALERT");
-				emailMap.add("mailTo", request.getEmailID());
-				HttpEntity<MultiValueMap<Object, Object>> httpEntity = new HttpEntity<>(emailMap, headers);
-
-				String url = "https://qa.mosip.io/v1/emailnotifier/email/send";
-				restTemplate.postForEntity(url, httpEntity, Object.class);
-			}else {
-				//exception
-			}
-		
-
-		} catch (Exception e) {
-			System.out.println(e);
+		String authUserRegistrationUrl = "http://localhost:8091/v1/authmanager/user";
+		String emailServiceApi = "https://qa.mosip.io/v1/emailnotifier/email/send";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		RequestWrapper<UserRegistrationRequestDto> userRegistrationRequestDto = new RequestWrapper<>();
+		userRegistrationRequestDto.setRequest(request);
+		HttpEntity<RequestWrapper<UserRegistrationRequestDto>> userHttpEntity = new HttpEntity<>(
+				userRegistrationRequestDto, headers);
+		ResponseEntity<String> response = restTemplate.postForEntity(authUserRegistrationUrl, userHttpEntity,
+				String.class);
+		String responseBody = response.getBody();
+		List<ServiceError> validationErrorList = ExceptionUtils.getServiceErrorList(responseBody);
+		if (!validationErrorList.isEmpty()) {
+			throw new AdminListOfServiceException(validationErrorList);
 		}
-		UserRegistrationResponseDto dto = new UserRegistrationResponseDto("SUCCESS");
-		return dto;
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		MultiValueMap<Object, Object> emailMap = new LinkedMultiValueMap<>();
+		emailMap.add("mailContent", "YOUR RID SUBMISSION LINK IS " + request.getRidValidationUrl()
+				+ "?username=" + request.getUserName());
+		emailMap.add("mailSubject", "ADMIN USER REGISTRATION ALERT");
+		emailMap.add("mailTo", request.getEmailID());
+		HttpEntity<MultiValueMap<Object, Object>> httpEntity = new HttpEntity<>(emailMap, headers);
+		restTemplate.postForEntity(emailServiceApi, httpEntity, Object.class);
+
+		return new UserRegistrationResponseDto("SUCCESS");
 	}
 
 	@Override
