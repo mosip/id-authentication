d3104e469c00aef199e77d6f6002e6f03f376655
mosip-sagarmahapatra <Sagar.Mahapatra2@mindtree.com>
2019-05-14 23:47:14 +0530

    MOS-0000 Service[Documentation(Done)]
    Former-commit-id: a651e86d7d5af753d172ffccefc719c86a73ab6e
    Former-commit-id: be0f53fb8acd58b8fb1b3e01825e17b82991527f

--- admin/admin-ui/src/app/shared/services/account-management.service.ts
+++ admin/admin-ui/src/app/shared/services/account-management.service.ts
@@ -4,57 +4,97 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
+/**
+ * @description This service provides the method for account management viz. Sending OTP, Getting userID, Password Management etc.
+ * @author Sagar Mahapatra
+ *
+ * @export
+ * @class AccountManagementService
+ */
 @Injectable()
 export class AccountManagementService {
 
-   forwardSlash = '/';
-   baseURL = 'https://dev.mosip.io/v1/admin/accountmanagement/';
-   baseAuthUrl = 'https://dev.mosip.io/v1/authmanager/authenticate/';
-// baseURL = 'https://localhost:8900/v1/admin/accountmanagement/';
+  forwardSlash = '/';
+  baseURL = 'https://dev.mosip.io/v1/admin/accountmanagement/';
+  baseAuthUrl = 'https://dev.mosip.io/v1/authmanager/authenticate/';
 
   constructor(private httpClient: HttpClient) { }
-  /*
-   * Method to get username from mobile number.
+  /**
+   * @description Method to get username from mobile number.
+   *
+   * @param phoneNumber phone Number for which the user name needs to be retrieved.
+   * @returns {Observable<any>} the observable response
+   * @memberof AccountManagementService
    */
   getUserNameFromPhoneNumber(phoneNumber: number): Observable<any> {
     return this.httpClient.get<any>(this.baseURL + 'username' + this.forwardSlash + phoneNumber)
     .catch(this.errorHandler);
   }
 
-  sendOTP(sendOTPRequest: any) {
+  /**
+   * @description Method to send OTP through different channels(SMS, E-mail).
+   *
+   * @param sendOTPRequest the request object.
+   * @returns {Observable<any>} the observable response
+   * @memberof AccountManagementService
+   */
+  sendOTP(sendOTPRequest: any): Observable<any> {
     return this.httpClient.post(this.baseAuthUrl + '/sendotp', sendOTPRequest).catch(this.errorHandler);
   }
 
-  validateOTP(validateOTPRequest: any) {
+  /**
+   * @description Method for OTP Authentication(Validation).
+   *
+   * @param validateOTPRequest the request object.
+   * @returns {Observable<any>} the observable response
+   * @memberof AccountManagementService
+   */
+  validateOTP(validateOTPRequest: any): Observable<any> {
     return this.httpClient.post(this.baseAuthUrl + 'useridOTP', validateOTPRequest).catch(this.errorHandler);
   }
 
-  /*
-   * Method to get username based on user ID.
+  /**
+   * @description Method to get username based on user ID.
+   *
+   * @param userId the user ID for which username needs to be retrieved.
+   * @returns {Observable<any>} the observable response
+   * @memberof AccountManagementService
    */
   forgotUserName(userId: string): Observable<any> {
     return this.httpClient.get<any>(this.baseURL + '?userId=' + userId);
 
   }
 
-  /*
-   * Method to change password.
+  /**
+   * @description Method to change password.
+   *
+   * @param changePasswordRequest the request object.
+   * @returns {Observable<any>} the observable response
+   * @memberof AccountManagementService
    */
   changePassword(changePasswordRequest: any): Observable<any> {
     return this.httpClient.post('', changePasswordRequest);
   }
 
-  /*
-   * Method to reset password.
+  /**
+   * @description Method to reset password.
+   *
+   * @param resetPasswordRequest the request object.
+   * @returns {Observable<any>} the observable response
+   * @memberof AccountManagementService
    */
   resetPassword(resetPasswordRequest: any): Observable<any> {
     return this.httpClient.post(this.baseURL + 'resetpassword', resetPasswordRequest);
   }
 
-  /*
-   * Method to handle HTTP Errors.
+  /**
+   * @description Method to handle HTTP Errors.
+   *
+   * @param error the error.
+   * @returns {Observable<any>} the observable error message else 'Server Error'.
+   * @memberof AccountManagementService
    */
-  errorHandler(error: HttpErrorResponse) {
+  errorHandler(error: HttpErrorResponse): Observable<any> {
     alert('HTTP_ERROR_IN_ACM_SERVICE');
     return Observable.throw(error.message || 'Server Error');
   }
