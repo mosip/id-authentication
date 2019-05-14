062fcb165be295a02fc17b937362ff8bef1cd720
mosip-sagarmahapatra <Sagar.Mahapatra2@mindtree.com>
2019-05-14 12:53:06 +0530

    MOS-0000 OTP timer added
    Former-commit-id: a36c17ae1fa8b23ca6f1f30678ddc2e5fab59981
    Former-commit-id: d92fd77c2b09794f2db0995a0cd03870f4940e05

--- admin/admin-ui/src/app/navigation/otp-authentication/otp-authentication.component.ts
+++ admin/admin-ui/src/app/navigation/otp-authentication/otp-authentication.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GetContactService } from '../../shared/services/get-contact.service';
 import {FormBuilder, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
@@ -8,16 +8,57 @@ import { Router } from '@angular/router';
   templateUrl: './otp-authentication.component.html',
   styleUrls: ['./otp-authentication.component.css']
 })
-export class OtpAuthenticationComponent implements OnInit {
+export class OtpAuthenticationComponent implements OnInit, OnDestroy {
+  otpExpiryTimeReached: boolean;
   userMobileNumber: number;
+  minutes: number;
+  seconds: number;
+  counter: number;
+  interval: any;
+
   constructor(private getContactService: GetContactService, private router: Router, private formBuilder: FormBuilder) { }
+
   otpAuthenticationForm = this.formBuilder.group({
     otp: ['', [Validators.required, Validators.pattern('[0-9]+')] ]
   });
+
   ngOnInit() {
+    this.otpExpiryTimeReached = false;
+    this.startCountdown(120);
     this.userMobileNumber = this.getContactService.getContactNumber();
   }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
   onVerify() {
     this.router.navigate(['resetpassword']);
   }
+
+  startCountdown(timeLeft: number): void {
+    this.counter = 0;
+    this.interval = setInterval(() => {
+      document.getElementById('timer').innerHTML = this.convertSeconds(timeLeft - this.counter);
+      this.counter++;
+      if (this.counter > timeLeft ) {
+        clearInterval(this.interval);
+        this.otpExpiryTimeReached = true;
+      }
+    }, 1000);
+  }
+
+  convertSeconds(timeLeft: number): string {
+    this.minutes = Math.floor(timeLeft / 60);
+    this.seconds = Math.floor(timeLeft % 60);
+    return Number(this.minutes)
+    .toLocaleString('en-US', {minimumIntegerDigits: 2}) + ':' + Number(this.seconds).toLocaleString('en-US', {minimumIntegerDigits: 2});
+   }
+
+   resendOTP() {
+    this.otpExpiryTimeReached = false;
+    clearInterval(this.interval);
+    this.otpAuthenticationForm.get('otp').reset();
+    this.startCountdown(120);
+   }
 }
