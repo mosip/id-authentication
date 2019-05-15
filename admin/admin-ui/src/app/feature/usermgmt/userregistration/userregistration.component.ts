7133155d6f720b2e53db3f8b8932ce9b7139e77b
Mosip-Ritesh <Ritesh.Sinha@mindtree.com>
2019-05-15 09:47:59 +0530

    MOS-000 otp validator component added https://mosipid.atlassian.net/browse/MOS-000 o
    Former-commit-id: 981fe7ae3341155e432050dd7e2c0cdab0b1b75f
    Former-commit-id: 54fc1c17e0973b7494d394ed97184d45ea9e76da

--- admin/admin-ui/src/app/feature/usermgmt/userregistration/userregistration.component.ts
+++ admin/admin-ui/src/app/feature/usermgmt/userregistration/userregistration.component.ts
@@ -20,12 +20,12 @@ export class UserregistrationComponent implements OnInit {
   originUrl: string;
   userRegistrationForm: FormGroup;
   genderResponseObject = [];
-  languageCode = "eng";
+  languageCode = 'eng';
   constructor(private router: Router, private formBuilder: FormBuilder, private service: UserregistrationService) {
     this.userRegistrationForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      contactNumber: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      contactNumber: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
       emailID: ['', [Validators.required, Validators.email]],
       DOB: ['', Validators.required],
       userName: ['', Validators.required],
@@ -44,34 +44,36 @@ export class UserregistrationComponent implements OnInit {
 
   onSubmit() {
     if (!this.userRegistrationForm.valid) {
-      alert("please fill all the requirements");
+      alert('please fill all the requirements');
       return;
     }
     this.requestObject.firstName = this.userRegistrationForm.get('firstName').value;
     this.requestObject.userName = this.userRegistrationForm.get('userName').value;
     this.requestObject.lastName = this.userRegistrationForm.get('lastName').value;
-    this.requestObject.contactNo = this.userRegistrationForm.get('contactNumber').value
+    this.requestObject.contactNo = this.userRegistrationForm.get('contactNumber').value;
     this.now = this.userRegistrationForm.get('DOB').value;
     this.requestObject.dateOfBirth = this.dateFormatter(this.now);
     this.requestObject.emailID = this.userRegistrationForm.get('emailID').value;
     this.requestObject.gender = this.userRegistrationForm.get('gender').value;
     this.requestObject.role = this.userRegistrationForm.get('roles').value;
-    this.requestObject.appId = "admin";
-    this.requestObject.ridValidationUrl = this.originUrl + "/#/admin/usermgmt/ridverification";
-    this.requestDTO = new RequestModel("id", "v1", this.requestObject, null);
+    this.requestObject.appId = 'admin';
+    this.requestObject.ridValidationUrl = this.originUrl + '/#/admin/usermgmt/ridverification';
+    this.requestDTO = new RequestModel('id', 'v1', this.requestObject, null);
+    console.log(this.requestDTO);
     this.service.registerUser(this.requestDTO).subscribe(data => {
-      alert("Detail submitted successfully");
+      alert('Detail submitted successfully');
+      console.log(data);
       this.router.navigateByUrl('admin/dashboard');
     }, error => {
       console.log(error);
-      alert("error occur");
+      alert('error occur');
     });
 
   }
 
   dateFormatter(date: Date) {
     const pipe = new DatePipe('en-US');
-    let formattedDate = pipe.transform(date, 'yyyy-MM-dd');
+    const formattedDate = pipe.transform(date, 'yyyy-MM-dd');
     return formattedDate;
   }
   cancel() {
@@ -82,7 +84,7 @@ export class UserregistrationComponent implements OnInit {
   }
 
   isValid(langCode: any) {
-    if (langCode == this.languageCode) {
+    if (langCode === this.languageCode) {
       return true;
     } else {
       return false;
