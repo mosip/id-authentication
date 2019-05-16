3119fcdfd8ceb7c251e2e6d29edf20b638fa4356
Manoj SP <Manoj.SP@mindtree.com>
2019-05-17 00:24:23 +0530

    MOS-23423 - updated policy provider to read policy from config server
    Former-commit-id: 23f1d5394a61cb079504d6497269cf2139de409e
    Former-commit-id: e7a85f5529b328fb0220ba1c8f94b626461115fc

--- id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/provider/VidPolicyProvider.java
+++ id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/provider/VidPolicyProvider.java
@@ -1,9 +1,6 @@
 package io.mosip.idrepository.vid.provider;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
@@ -14,9 +11,9 @@ import java.util.stream.IntStream;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,45 +21,78 @@ import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
 
+import io.mosip.idrepository.core.constant.IdRepoConstants;
 import io.mosip.idrepository.vid.dto.VidPolicy;
+import io.mosip.kernel.core.idobjectvalidator.exception.FileIOException;
+import io.mosip.kernel.core.idobjectvalidator.exception.IdObjectIOException;
+import io.mosip.kernel.core.idobjectvalidator.exception.IdObjectSchemaIOException;
+import io.mosip.kernel.core.idobjectvalidator.exception.IdObjectValidationProcessingException;
+import io.mosip.kernel.core.idobjectvalidator.spi.IdObjectValidator;
 
 /**
- * @author Manoj SP
+ * The Class VidPolicyProvider.
  *
+ * @author Manoj SP
  */
 @Component
 public class VidPolicyProvider {
-	
+
+	/** The Constant READ_LIST_OPTIONS. */
 	private static final Configuration READ_LIST_OPTIONS = Configuration.defaultConfiguration()
 			.addOptions(Option.SUPPRESS_EXCEPTIONS, Option.ALWAYS_RETURN_LIST);
-	
+
+	/** The env. */
 	@Autowired
 	private Environment env;
-	
+
+	/** The mapper. */
 	@Autowired
 	private ObjectMapper mapper;
 	
+	@Autowired
+	@Qualifier("schema")
+	private IdObjectValidator schemaValidator;
+
+	/** The vid policies. */
 	private Map<String, VidPolicy> vidPolicies;
-	
+
+	/**
+	 * Policy details.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws FileIOException 
+	 * @throws IdObjectSchemaIOException 
+	 * @throws IdObjectIOException 
+	 * @throws IdObjectValidationProcessingException 
+	 */
 	@PostConstruct
-	public void policyDetails() throws IOException, URISyntaxException {
-//		FileReader jsonFile = new FileReader(
-//				new File(new URL(env.getProperty("mosip.idrepo.vid.policy-file-location")).toURI()));
+	public void loadPolicyDetails() throws IOException, IdObjectValidationProcessingException, IdObjectIOException, IdObjectSchemaIOException, FileIOException {
 		JsonNode policyJson = mapper.readValue(
-				this.getClass().getClassLoader().getResource("vid_policy.json"), JsonNode.class);
-		List<String> vidType = JsonPath.compile("vidPolicies.*.vidType").read(policyJson.toString(), READ_LIST_OPTIONS);
-		List<Object> vidPolicy = JsonPath.compile("vidPolicies.*.vidPolicy").read(policyJson.toString(),
+				new URL(env.getProperty(IdRepoConstants.VID_POLICY_FILE_LOCATION.getValue())), JsonNode.class);
+		schemaValidator.validateIdObject(policyJson);
+		List<String> vidType = JsonPath.compile(IdRepoConstants.VID_TYPE_PATH.getValue()).read(policyJson.toString(),
 				READ_LIST_OPTIONS);
-		vidPolicies = IntStream.range(0, vidType.size()).parallel()
-			.boxed()
-			.collect(Collectors.toMap(i -> vidType.get(i),
-					i -> mapper.convertValue(vidPolicy.get(i), VidPolicy.class)));
+		List<Object> vidPolicy = JsonPath.compile(IdRepoConstants.VID_POLICY_PATH.getValue())
+				.read(policyJson.toString(), READ_LIST_OPTIONS);
+		vidPolicies = IntStream.range(0, vidType.size()).parallel().boxed()
+				.collect(Collectors.toMap(vidType::get, i -> mapper.convertValue(vidPolicy.get(i), VidPolicy.class)));
 	}
-	
+
+	/**
+	 * Gets the policy.
+	 *
+	 * @param vidType the vid type
+	 * @return the policy
+	 */
 	public VidPolicy getPolicy(String vidType) {
 		return vidPolicies.get(vidType);
 	}
-	
+
+	/**
+	 * Gets the all vid types.
+	 *
+	 * @return the all vid types
+	 */
 	public Set<String> getAllVidTypes() {
 		return vidPolicies.keySet();
 	}
