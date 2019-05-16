cfbd29eaf2038bd9edd3712cf60cd89b590dec66
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2019-05-16 19:29:34 +0530

    Adding validation to arguments.
    Former-commit-id: 2ad7f24c33ffe271bc986d4459db844b859a5eb5
    Former-commit-id: 4a89274c95afbaac71601a22b2d902b6b855e751

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/builder/AuthTransactionBuilder.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/builder/AuthTransactionBuilder.java
@@ -20,11 +20,24 @@ import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.core.util.DateUtils;
 import io.mosip.kernel.core.util.UUIDUtils;
 
+/**
+ * The builder to build {@code AutnTxn} instance.
+ * 
+ * @author Loganathan.Sekar
+ *
+ */
 public class AuthTransactionBuilder {
 	
+	/**
+	 * 
+	 */
 	private AuthTransactionBuilder() {
 	}
 	
+	/**
+	 *  Get new instance of {@code AuthTransactionBuilder}
+	 * @return new instance of AuthTransactionBuilder
+	 */
 	public static AuthTransactionBuilder newInstance() {
 		return new AuthTransactionBuilder();
 	}
@@ -38,68 +51,119 @@ public class AuthTransactionBuilder {
 	/** The mosipLogger. */
 	private Logger mosipLogger = IdaLogger.getLogger(AuditHelper.class);
 	
+	/**  */
 	private AuthRequestDTO authRequestDTO;
 
+	/**  */
 	private String uin;
 
+	/**  */
 	private RequestType requestType;
 
+	/**  */
 	private String staticTokenId;
 
+	/**  */
 	private boolean isStatus;
 	
+	/**
+	 * Set the AuthRequestDTO
+	 *
+	 * @param authRequestDTO 
+	 * @return {@code AuthTransactionBuilder} instance
+	 */
 	public AuthTransactionBuilder withAuthRequest(AuthRequestDTO authRequestDTO) {
 		this.authRequestDTO = authRequestDTO;
 		return this;
 	}
 
+	/**
+	 * Set the UIN
+	 *
+	 * @param uin 
+	 * @return  {@code AuthTransactionBuilder} instance
+	 */
 	public AuthTransactionBuilder withUin(String uin) {
 		this.uin = uin;
 		return this;
 	}
 
+	/**
+	 * Set the RequestType
+	 *
+	 * @param requestType 
+	 * @return {@code AuthTransactionBuilder} instance
+	 */
 	public AuthTransactionBuilder withRequestType(RequestType requestType) {
 		this.requestType = requestType;
 		return this;
 	}
 
+	/**
+	 * Set the static token
+	 *
+	 * @param staticTokenId 
+	 * @return {@code AuthTransactionBuilder} instance
+	 */
 	public AuthTransactionBuilder withStaticToken(String staticTokenId) {
 		this.staticTokenId = staticTokenId;
 		return this;
 	}
 
+	/**
+	 * 
+	 *
+	 * @param isStatus 
+	 * @return 
+	 */
 	public AuthTransactionBuilder withStatus(boolean isStatus) {
 		this.isStatus = isStatus;
 		return this;
 	}
 	
+	/**
+	 * Build {@code AutnTxn}
+	 *
+	 * @param idInfoFetcher 
+	 * @param env 
+	 * @return the instance of {@code AutnTxn}
+	 * @throws IdAuthenticationBusinessException 
+	 */
 	public AutnTxn build(IdInfoFetcher idInfoFetcher, Environment env) throws IdAuthenticationBusinessException {
 		try {
-			String status = isStatus ? SUCCESS_STATUS : FAILED;
-			String comment = isStatus ? requestType.getMessage() + " Success" : requestType.getMessage() + " Failed";
-			String idvId = idInfoFetcher.getUinOrVid(authRequestDTO).get();
-			String reqTime = authRequestDTO.getRequestTime();
-			String idvIdType = idInfoFetcher.getUinOrVidType(authRequestDTO).getType();
-			String txnID = authRequestDTO.getTransactionID();
-			AutnTxn autnTxn = new AutnTxn();
-			autnTxn.setRefId(idvId);
-			autnTxn.setRefIdType(idvIdType);
-			String id = createId(uin, env);
-			autnTxn.setId(id); // FIXME
-			autnTxn.setCrBy(env.getProperty(IdAuthConfigKeyConstants.APPLICATION_ID));
-			autnTxn.setStaticTknId(staticTokenId);
-			autnTxn.setCrDTimes(DateUtils.getUTCCurrentDateTime());
-			String strUTCDate = DateUtils.getUTCTimeFromDate(
-					DateUtils.parseToDate(reqTime, env.getProperty(IdAuthConfigKeyConstants.DATE_TIME_PATTERN)));
-			autnTxn.setRequestDTtimes(DateUtils.parseToLocalDateTime(strUTCDate));
-			autnTxn.setResponseDTimes(DateUtils.getUTCCurrentDateTime()); // TODO check this
-			autnTxn.setAuthTypeCode(requestType.getRequestType());
-			autnTxn.setRequestTrnId(txnID);
-			autnTxn.setStatusCode(status);
-			autnTxn.setStatusComment(comment);
-			// FIXME
-			autnTxn.setLangCode(env.getProperty(IdAuthConfigKeyConstants.MOSIP_PRIMARY_LANGUAGE));
-			return autnTxn;
+			if (authRequestDTO != null && uin != null && staticTokenId != null && requestType != null) {
+				String status = isStatus ? SUCCESS_STATUS : FAILED;
+				String comment = isStatus ? requestType.getMessage() + " Success"
+						: requestType.getMessage() + " Failed";
+				String idvId = idInfoFetcher.getUinOrVid(authRequestDTO).get();
+				String reqTime = authRequestDTO.getRequestTime();
+				String idvIdType = idInfoFetcher.getUinOrVidType(authRequestDTO).getType();
+				String txnID = authRequestDTO.getTransactionID();
+				AutnTxn autnTxn = new AutnTxn();
+				autnTxn.setRefId(idvId);
+				autnTxn.setRefIdType(idvIdType);
+				String id = createId(uin, env);
+				autnTxn.setId(id); // FIXME
+				autnTxn.setCrBy(env.getProperty(IdAuthConfigKeyConstants.APPLICATION_ID));
+				autnTxn.setStaticTknId(staticTokenId);
+				autnTxn.setCrDTimes(DateUtils.getUTCCurrentDateTime());
+				String strUTCDate = DateUtils.getUTCTimeFromDate(
+						DateUtils.parseToDate(reqTime, env.getProperty(IdAuthConfigKeyConstants.DATE_TIME_PATTERN)));
+				autnTxn.setRequestDTtimes(DateUtils.parseToLocalDateTime(strUTCDate));
+				autnTxn.setResponseDTimes(DateUtils.getUTCCurrentDateTime()); // TODO check this
+				autnTxn.setAuthTypeCode(requestType.getRequestType());
+				autnTxn.setRequestTrnId(txnID);
+				autnTxn.setStatusCode(status);
+				autnTxn.setStatusComment(comment);
+				// FIXME
+				autnTxn.setLangCode(env.getProperty(IdAuthConfigKeyConstants.MOSIP_PRIMARY_LANGUAGE));
+				return autnTxn;
+			} else {
+				mosipLogger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getName(), "Missing arguments to build for AutnTxn",
+						 this.toString());
+				throw new IdAuthenticationBusinessException(IdAuthenticationErrorConstants.DATA_VALIDATION_FAILED);
+			}
+
 		} catch (ParseException e) {
 			mosipLogger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getName(), e.getClass().getName(),
 					e.getMessage());
@@ -108,11 +172,11 @@ public class AuthTransactionBuilder {
 	}
 	
 	/**
-	 * Creates UUID
-	 * 
-	 * @param uin
+	 * Creates UUID.
+	 *
+	 * @param uin 
 	 * @param env 
-	 * @return
+	 * @return 
 	 */
 	private String createId(String uin, PropertyResolver env) {
 		String currentDate = DateUtils.formatDate(new Date(),
@@ -120,6 +184,14 @@ public class AuthTransactionBuilder {
 		String uinAndDate = uin + "-" + currentDate;
 		return UUIDUtils.getUUID(UUIDUtils.NAMESPACE_OID, uinAndDate).toString();
 	}
+
+	@Override
+	public String toString() {
+		return "AuthTransactionBuilder [authRequestDTO=" + authRequestDTO + ", uin=" + uin + ", requestType="
+				+ requestType + ", staticTokenId=" + staticTokenId + ", isStatus=" + isStatus + "]";
+	}
+	
+	
 
 	
 
