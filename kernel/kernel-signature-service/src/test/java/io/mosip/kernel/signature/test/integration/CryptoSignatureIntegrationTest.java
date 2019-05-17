88e9062b13db5e485d0103d27804335dc152736e
udaykumar222 <uday.kumar@mindtree.com>
2019-05-17 18:41:34 +0530

    MOS-24001 changes made testcases
    Former-commit-id: 361c425185f949f91b325077d2adc149b1b67fb1
    Former-commit-id: bba9e7c358425735c1d4a795df591430f1e9d469

--- kernel/kernel-signature-service/src/test/java/io/mosip/kernel/signature/test/integration/CryptoSignatureIntegrationTest.java
+++ kernel/kernel-signature-service/src/test/java/io/mosip/kernel/signature/test/integration/CryptoSignatureIntegrationTest.java
@@ -36,21 +36,14 @@ public class CryptoSignatureIntegrationTest {
 
 	@MockBean
 	private SignatureUtil signatureUtil;
-	
+
 	@MockBean
 	private RestTemplate restTemplate;
-	
 
 	private static final String SIGNRESPONSEREQUEST = "{ \"id\": \"string\", \"metadata\": {}, \"request\": { \"response\": \"admin\" }, \"requesttime\": \"2018-12-10T06:12:52.994Z\", \"version\": \"string\" }";
 	private static final String VALIDATEWITHPUBLICKEY = "{ \"id\": \"string\", \"metadata\": {}, \"request\": { \"publicKey\": \"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnoocJbIeMuAzqSzuJX9CvXmFFka3Fz3C-u9vz6c8RsJSKBCe_SAOi31IvL992kuy1qO4XTS-cUuirx-djuF0E7r5TbQFKlNa-FoPJu8QRIGw2rWVQsc2c0Aqd5cfhr9fgTsM3V3URl1jXY645v9EPE0Ih5E26ld6JQQQ90mpvoa6XlJEf5SUAOuzvr5ws5VoZgEQ6wjO05dZSaEL9vrA5npsNSwLb55FqZb7w9qLZfYbPOBVxUZ-HTddBLP6KvlIHWzsVapjvhUHPgSO0AZDYmx3kkKb7jFuWelPibNyKy619AAnlQX3VR39CKi-6sPLRABs4v-npsFLNz9Wd_VJHwIDAQAB\", \"responseBody\": \"admin\", \"responseSignature\": \"ZeNsCOsdgf0UgpXDMry82hrHS6b1ZKvS-tZ_3HBGQHleIu1fZA6LNTtx7XZPFeC8dxsyuYO_iN3mVExM4J2tPlebzsRtuxHigi9o7DI_2xGqFudzlgoH55CP_BBNUDmGm6m-lTMkRx6X61dKfKDNo2NipZdM-a_cHf6Z0aVAU4LdJhV4xWOOm8Pb8sYIc2Nf6kUJRiidEGrxonUCfXX1XlnjMAo75wu99pN8G0mc7JhOehUqbwuXwKo4sQ694ae4F_AYl70sepX24v-0k0ga9esXR4i9rKaoHbzhQFtt2hangQkxHajq9ZTrXWMhd4msTzjHCKdEPXQFsTbKrgKtDQ\" }, \"requesttime\": \"2018-12-10T06:12:52.994Z\", \"version\": \"string\" }";
 	StringBuilder builder;
 	SignatureResponse signResponse;
-
-	@Value("${mosip.kernel.syncdata.auth-manager-base-uri}")
-	private String authBaseUri;
-
-	@Value("${mosip.kernel.syncdata.auth-manager-roles}")
-	private String authAllRolesUri;
 
 	@Before
 	public void setup() {
@@ -72,8 +65,7 @@ public class CryptoSignatureIntegrationTest {
 	public void signResponseSuccessValidation() throws Exception {
 		when(signatureUtil.validateWithPublicKey(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
 				.thenReturn(true);
-		mockMvc.perform(
-				post("/validate").contentType(MediaType.APPLICATION_JSON).content(VALIDATEWITHPUBLICKEY))
+		mockMvc.perform(post("/validate").contentType(MediaType.APPLICATION_JSON).content(VALIDATEWITHPUBLICKEY))
 				.andExpect(status().isOk());
 	}
 
