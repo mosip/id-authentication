8ce3aabcf60904c70ea2d3d8e3aa0050b43e9972
Agnitra Banerjee <agneetra10@gmail.com>
2019-05-17 17:17:16 +0530

    MOS-0000 added mapping for all the relevant cards
    Former-commit-id: 5ca400db1697bbf0931a291a36c715a89f20b61b
    Former-commit-id: c5cc478a584bb323c811179d2cec25e4d13ca328

--- admin/admin-ui/src/app/app.constants.ts
+++ admin/admin-ui/src/app/app.constants.ts
@@ -6,13 +6,83 @@ export const menuItems = [
   'Configuration'
 ];
 export const base_url = 'dev.mosip.io';
+export const masterData_base_url = `https://${base_url}/v1/masterdata/`;
 export const code_url_mapping = {
-  centers: `https://${base_url}/v1/masterdata/registrationcenters`
+  'centers': {
+    appendURL: `registrationcenters`,
+    parameterName: 'registrationCenters'
+  },
+  'biometric-types': {
+    appendURL: `biometrictypes`,
+    parameterName: 'biometrictypes'
+  },
+  'individuals': {
+    appendURL: 'individualtypes',
+    parameterName: 'individualTypes'
+  },
+  'idtypes': {
+    appendURL: 'idtypes/eng',
+    parameterName: 'idtypes'
+  },
+  'holidays': {
+    appendURL: 'holidays',
+    parameterName: 'holidays'
+  },
+  'rejection-reason': {
+    appendURL: 'packetrejectionreasons',
+    parameterName: 'reasonCategories'
+  },
+  'devices': {
+    appendURL: 'devices/eng',
+    parameterName: 'devices'
+  },
+  'device-specs': {
+    appendURL: 'devicespecifications/eng',
+    parameterName: 'devicespecifications'
+  },
+  'templates': {
+    appendURL: 'templates',
+    parameterName: 'templates'
+  },
+  'genders': {
+    appendURL: 'gendertypes',
+    parameterName: 'genderType'
+  },
+  'titles': {
+    appendURL: 'title',
+    parameterName: ''
+  },
+  'document-category': {
+    appendURL: 'documentcategories',
+    parameterName: 'documentcategories'
+  },
+  'blacklisted': {
+    appendURL: 'blacklistedwords/eng',
+    parameterName: 'blacklistedwords'
+  },
+  'locations': {
+    appendURL: 'locations/eng',
+    parameterName: 'locations'
+  },
+  'machines': {
+    appendURL: 'machines',
+    parameterName: 'machines'
+  },
+  'applications': {
+    appendURL: 'applicationtypes',
+    parameterName: 'applicationtypes'
+  },
+  'valid-document': {
+    appendURL: 'validdocuments/eng',
+    parameterName: 'documentcategories'
+  }
 };
 
+export const admin_base_url = `https://${base_url}/v1/admin/`;
+
 export const loginURL = {
-  userRole: `https://${base_url}/v1/admin/security/authfactors/`,
-  userIdpasswd: `https://${base_url}/v1/admin/useridPwd`,
+  userRole: `${admin_base_url}security/authfactors/`,
+  userIdpasswd: `${admin_base_url}useridPwd`,
   sendOtp: `https://${base_url}/v1/authmanager/authenticate/sendotp`,
   verifyOtp: `https://${base_url}/v1/authmanager/authenticate/useridOTP`
 };
