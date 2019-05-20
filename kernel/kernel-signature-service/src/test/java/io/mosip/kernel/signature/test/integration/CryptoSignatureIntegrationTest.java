73c1d154aabc64ad4dae301c5891a464ecc5189f
udaykumar222 <uday.kumar@mindtree.com>
2019-05-20 14:59:25 +0530

    MOS-24001 Added test case for validation with timestamp for sign response.
    Former-commit-id: 5d741bdc17e0c0efe83fb98a1f8165700b33d196
    Former-commit-id: 5520ad6292e5cae9b023610fceac20b4cc8e2375

--- kernel/kernel-signature-service/src/test/java/io/mosip/kernel/signature/test/integration/CryptoSignatureIntegrationTest.java
+++ kernel/kernel-signature-service/src/test/java/io/mosip/kernel/signature/test/integration/CryptoSignatureIntegrationTest.java
@@ -41,6 +41,8 @@ public class CryptoSignatureIntegrationTest {
 
 	private static final String SIGNRESPONSEREQUEST = "{ \"id\": \"string\", \"metadata\": {}, \"request\": { \"data\": \"admin\" }, \"requesttime\": \"2018-12-10T06:12:52.994Z\", \"version\": \"string\" }";
 	private static final String VALIDATEWITHPUBLICKEY = "{ \"id\": \"string\", \"metadata\": {}, \"request\": { \"publickey\": \"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnoocJbIeMuAzqSzuJX9CvXmFFka3Fz3C-u9vz6c8RsJSKBCe_SAOi31IvL992kuy1qO4XTS-cUuirx-djuF0E7r5TbQFKlNa-FoPJu8QRIGw2rWVQsc2c0Aqd5cfhr9fgTsM3V3URl1jXY645v9EPE0Ih5E26ld6JQQQ90mpvoa6XlJEf5SUAOuzvr5ws5VoZgEQ6wjO05dZSaEL9vrA5npsNSwLb55FqZb7w9qLZfYbPOBVxUZ-HTddBLP6KvlIHWzsVapjvhUHPgSO0AZDYmx3kkKb7jFuWelPibNyKy619AAnlQX3VR39CKi-6sPLRABs4v-npsFLNz9Wd_VJHwIDAQAB\", \"data\": \"admin\", \"signature\": \"ZeNsCOsdgf0UgpXDMry82hrHS6b1ZKvS-tZ_3HBGQHleIu1fZA6LNTtx7XZPFeC8dxsyuYO_iN3mVExM4J2tPlebzsRtuxHigi9o7DI_2xGqFudzlgoH55CP_BBNUDmGm6m-lTMkRx6X61dKfKDNo2NipZdM-a_cHf6Z0aVAU4LdJhV4xWOOm8Pb8sYIc2Nf6kUJRiidEGrxonUCfXX1XlnjMAo75wu99pN8G0mc7JhOehUqbwuXwKo4sQ694ae4F_AYl70sepX24v-0k0ga9esXR4i9rKaoHbzhQFtt2hangQkxHajq9ZTrXWMhd4msTzjHCKdEPXQFsTbKrgKtDQ\" }, \"requesttime\": \"2018-12-10T06:12:52.994Z\", \"version\": \"string\" }";
+	private static final String VALIDATEWITHTIMESTAMP = "{ \"id\": \"string\", \"metadata\": {}, \"request\": { \"signature\": \"DrgkF2vm4WvBe04UNe-RePRcrg77uQpsH3GENRcglBsid-K0UDReeeZVKwimOdwV7Ht1j-_D1BFf2sCrM8ni7ztE5Xc_3TEaniOAnOgZDRSI0GG-uSqjH51AwTSl1PYdStfXtOn6HEfEU68JG7TdAliDI5C7thJ1YNmPnHusIsZzX6sW_VfvSpLeA_RzCqnUDH_VaEzZt_5zRYiQv9van4wt0P7HTfIBlQ5zaeO3wXOc3Pogct3ssKwqdaMmZdc7QTDOFqDZZVceMTIXKyiH-ZVs_u3QXRysiLVdXoz7d7yXHdWxQtzsfMjY7alMJNgbmu4X26LYNRemn65Mmn6ixA\", \"data\": \"test\", \"timestamp\": \"2019-05-20T07:28:04.269Z\" }, \"requesttime\": \"2018-12-10T06:12:52.994Z\", \"version\": \"string\" }";
+
 	StringBuilder builder;
 	SignatureResponse signResponse;
 
@@ -61,10 +63,35 @@ public class CryptoSignatureIntegrationTest {
 
 	@Test
 	@WithUserDetails("reg-processor")
-	public void signResponseSuccessValidation() throws Exception {
+	public void signResponsePublicKeyValidation() throws Exception {
 		when(signatureUtil.validateWithPublicKey(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
 				.thenReturn(true);
 		mockMvc.perform(post("/public/validate").contentType(MediaType.APPLICATION_JSON).content(VALIDATEWITHPUBLICKEY))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithUserDetails("reg-processor")
+	public void signResponsePublicKeyValidationInvalid() throws Exception {
+		when(signatureUtil.validateWithPublicKey(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
+				.thenReturn(false);
+		mockMvc.perform(post("/public/validate").contentType(MediaType.APPLICATION_JSON).content(VALIDATEWITHPUBLICKEY))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithUserDetails("reg-processor")
+	public void signResponseTimeStampValidation() throws Exception {
+		when(signatureUtil.validate(Mockito.anyString(), Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+		mockMvc.perform(post("/validate").contentType(MediaType.APPLICATION_JSON).content(VALIDATEWITHTIMESTAMP))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithUserDetails("reg-processor")
+	public void signResponseTimeStampValidationInvalid() throws Exception {
+		when(signatureUtil.validate(Mockito.anyString(), Mockito.anyString(), Mockito.anyString())).thenReturn(false);
+		mockMvc.perform(post("/validate").contentType(MediaType.APPLICATION_JSON).content(VALIDATEWITHTIMESTAMP))
 				.andExpect(status().isOk());
 	}
 
