f041ff55b8920a115bc5e58da9fa6799ac65fc5b
Alok Ranjan <Alok.Ranjan2@mindtree.com>
2019-05-20 14:57:50 +0530

    MOS-24048 fixed biodedupe service test case https://mosipid.atlassian.net/browse/MOS-24048
    Former-commit-id: a0c6273f173a01d466ea201161d8a5c9a3a9a18c
    Former-commit-id: 1ceca38a220a68b651a490b6ed3540185c1990e0

--- registration-processor/pre-processor/registration-processor-external-stage/src/main/java/io/mosip/registrationprocessor/externalstage/stage/ExternalStage.java
+++ registration-processor/pre-processor/registration-processor-external-stage/src/main/java/io/mosip/registrationprocessor/externalstage/stage/ExternalStage.java
@@ -127,6 +127,7 @@ public class ExternalStage extends MosipVerticleAPIManager {
 				registrationStatusDto.setStatusComment(StatusMessage.EXTERNAL_STAGE_SUCCESS);
 				registrationStatusDto.setStatusCode(RegistrationStatusCode.EXTERNAL_STAGE_SUCCESS.toString());
 				object.setIsValid(true);
+				object.setInternalError(false);
 				isTransactionSuccessful = true;
 				description = PlatformSuccessMessages.RPR_EXTERNAL_STAGE_SUCCESS.getMessage() + " -- " + registrationId;
 				code = PlatformSuccessMessages.RPR_EXTERNAL_STAGE_SUCCESS.getCode();
@@ -136,6 +137,7 @@ public class ExternalStage extends MosipVerticleAPIManager {
 				registrationStatusDto.setStatusComment(StatusMessage.EXTERNAL_STAGE_FAILURE);
 				registrationStatusDto.setStatusCode(RegistrationStatusCode.EXTERNAL_STAGE_FAILURE.toString());
 				object.setIsValid(false);
+				object.setInternalError(false);
 				description = PlatformErrorMessages.EXTERNAL_STAGE_FAILED.getMessage() + " -- " + description;
 				code = PlatformErrorMessages.EXTERNAL_STAGE_FAILED.getCode();
 			}
@@ -149,7 +151,7 @@ public class ExternalStage extends MosipVerticleAPIManager {
 			regProcLogger.debug("", "", "sent to next stage --> ", ExceptionUtils.getStackTrace(e));
 		} finally {
 
-			registrationStatusService.updateRegistrationStatus(registrationStatusDto);
+			
 			regProcLogger.info(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
 					registrationId, description);
 			if (object.getInternalError()) {
@@ -161,8 +163,9 @@ public class ExternalStage extends MosipVerticleAPIManager {
 				registrationStatusDto.setRetryCount(retryCount);
 				registrationStatusDto.setStatusComment(description);
 				registrationStatusDto.setStatusCode(RegistrationStatusCode.EXTERNAL_STAGE_FAILURE.toString());
-				registrationStatusService.updateRegistrationStatus(registrationStatusDto);
+				
 			}
+			registrationStatusService.updateRegistrationStatus(registrationStatusDto);
 			description = isTransactionSuccessful ? PlatformSuccessMessages.RPR_PKR_PACKET_VALIDATE.getMessage()
 					: description;
 			String eventId = isTransactionSuccessful ? EventId.RPR_402.toString() : EventId.RPR_405.toString();
