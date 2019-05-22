b6c9eae8c219914a0e31f916609001a33fb54706
MeghaUttamTanga <megha.tanga@mindtree.com>
2019-05-22 15:19:18 +0530

    MOS-24562  display UIN detail based on the given uin wrote rest call and test case
    Former-commit-id: c5dcafc1dfa580f9d0f202e501fa4c6c495e1fac
    Former-commit-id: 3e4ccf5d481f6eda0dddfdf1a3bf59693722a46e

--- admin/admin-service/src/main/java/io/mosip/admin/uinmgmt/constant/UinDetailErrorCode.java
+++ admin/admin-service/src/main/java/io/mosip/admin/uinmgmt/constant/UinDetailErrorCode.java
@@ -1,19 +1,20 @@
 package io.mosip.admin.uinmgmt.constant;
+
 /**
  * Constant for UINStatus
  * 
  * @autor Megha Tanga
  * 
  */
-public enum UinStatusErrorCode {
-	
-	UIN_STATUS_EXCEPTION("XXXX","Error occurred while fecthing the UIN Details from server"),
-	UIN_PROPERTY_NOT_FOUND("XXXXX", "Error occurred while fecthing UIN Properties from server");
-	
+public enum UinDetailErrorCode {
+
+	REST_SERVICE_EXCEPTION("ADM-UAT-001", "Error occurred while fecthing UIN Details from server"), 
+	INVAVIDE_UIN("ADM-UAT-002","UIN does not exist");
+
 	private final String errorCode;
 	private final String errorMessage;
 
-	private UinStatusErrorCode(String errorCode, String errorMessage) {
+	private UinDetailErrorCode(String errorCode, String errorMessage) {
 		this.errorCode = errorCode;
 		this.errorMessage = errorMessage;
 	}
