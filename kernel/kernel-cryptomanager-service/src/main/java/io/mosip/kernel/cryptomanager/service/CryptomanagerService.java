586fc4340fe41469d571bd0d4fd43658b9778f2f
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-05-23 14:07:54 +0530

    MOS-24055 endpoint removed
    Former-commit-id: 8f7db1a417c45162d5a46499f807a1f708107e75
    Former-commit-id: 82412399d06bcfef49849148e2b79525158338f6

--- kernel/kernel-cryptomanager-service/src/main/java/io/mosip/kernel/cryptomanager/service/CryptomanagerService.java
+++ kernel/kernel-cryptomanager-service/src/main/java/io/mosip/kernel/cryptomanager/service/CryptomanagerService.java
@@ -6,20 +6,12 @@
  */
 package io.mosip.kernel.cryptomanager.service;
 
-import java.util.Optional;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Service;
 
-import io.mosip.kernel.cryptomanager.dto.CryptoEncryptRequestDto;
-import io.mosip.kernel.cryptomanager.dto.CryptoEncryptResponseDto;
 import io.mosip.kernel.cryptomanager.dto.CryptomanagerRequestDto;
 import io.mosip.kernel.cryptomanager.dto.CryptomanagerResponseDto;
-import io.mosip.kernel.cryptomanager.dto.PublicKeyResponse;
-import io.mosip.kernel.cryptomanager.dto.SignatureRequestDto;
-import io.mosip.kernel.cryptomanager.dto.SignatureResponseDto;
-
 
 /**
  * This interface provides the methods which can be used for Encryption and
@@ -31,7 +23,7 @@ import io.mosip.kernel.cryptomanager.dto.SignatureResponseDto;
  */
 @Service
 public interface CryptomanagerService {
-	
+
 	/**
 	 * Encrypt the data requested with metadata.
 	 *
@@ -47,18 +39,5 @@ public interface CryptomanagerService {
 	 * @return decrypted data
 	 */
 	public CryptomanagerResponseDto decrypt(@Valid CryptomanagerRequestDto cryptoRequestDto);
-	
-	/**
-	 * Encrypt with private.
-	 *
-	 * @param {@link CryptoEncryptRequestDto}cryptoRequestDto the crypto request dto
-	 * @return the cryptomanager response dto {@link CryptoEncryptResponseDto}
-	 */
-	public CryptoEncryptResponseDto encryptWithPrivate(@Valid CryptoEncryptRequestDto cryptoRequestDto );
-
-	public SignatureResponseDto signaturePrivateEncrypt(SignatureRequestDto request);
-
-	public PublicKeyResponse getSignPublicKey(String applicationId, String timestamp, Optional<String> referenceId);
-
 
 }
