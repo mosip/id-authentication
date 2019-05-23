1829aa6008f1537b6e3e0dd565101cc559ff43ba
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-05-23 14:04:06 +0530

    MOS-24055 file removed
    Former-commit-id: 0b4ce23f390fdb3d19a9f17311d44c4bb67fbab7
    Former-commit-id: 6494444661693ee47bfbf051a3b4e632059eae77

--- kernel/kernel-signature-service/src/main/java/io/mosip/kernel/signature/service/SignatureService.java
+++ kernel/kernel-signature-service/src/main/java/io/mosip/kernel/signature/service/SignatureService.java
@@ -30,10 +30,10 @@ public interface SignatureService {
 	public ValidatorResponseDto validateWithPublicKey(PublicKeyRequestDto publicKeyRequestDto)
 			throws InvalidKeySpecException, NoSuchAlgorithmException;
 
-	public ValidatorResponseDto validate(TimestampRequestDto timestampRequestDto)
-			throws InvalidKeySpecException, NoSuchAlgorithmException;
+	/** Validate signature
+	 * @param timestampRequestDto {@link TimestampRequestDto}
+	 * @return {@link ValidatorResponseDto}
+	 */
+	public ValidatorResponseDto validate(TimestampRequestDto timestampRequestDto);
 
-	public SignatureResponse signCertificateResponse(SignRequestDto request);
-
-	public ValidatorResponseDto certificateValidate(TimestampRequestDto request);
 }
