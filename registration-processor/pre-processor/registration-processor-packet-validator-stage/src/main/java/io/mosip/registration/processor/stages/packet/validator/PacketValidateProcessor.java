d023b0438178c79e2e50e71a17d8429214c0cc05
NagalakshmiNithyanandan <Nagalakshmi.Nithyanandan@mindtree.com>
2019-05-23 16:22:15 +0530

    MOS-23580 status update in validator
    Former-commit-id: 06c8ebd239c8a99538866d8b6ce69c52ee901a1d
    Former-commit-id: c4f8b4d4921e7a48ed1a5b744c2302c02b9b87ca

--- registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/packet/validator/PacketValidateProcessor.java
+++ registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/packet/validator/PacketValidateProcessor.java
@@ -50,7 +50,6 @@ import io.mosip.registration.processor.core.packet.dto.Identity;
 import io.mosip.registration.processor.core.packet.dto.PacketMetaInfo;
 import io.mosip.registration.processor.core.packet.dto.applicantcategory.ApplicantTypeDocument;
 import io.mosip.registration.processor.core.packet.dto.demographicinfo.identify.RegistrationProcessorIdentity;
-import io.mosip.registration.processor.core.packet.dto.idjson.Document;
 import io.mosip.registration.processor.core.packet.dto.packetvalidator.MainRequestDTO;
 import io.mosip.registration.processor.core.packet.dto.packetvalidator.MainResponseDTO;
 import io.mosip.registration.processor.core.packet.dto.packetvalidator.ReverseDataSyncRequestDTO;
@@ -442,9 +441,10 @@ public class PacketValidateProcessor {
 	private boolean schemaValidation(String jsonString)
 			throws JsonValidationProcessingException, JsonIOException, JsonSchemaIOException, FileIOException {
 
-		if (env.getProperty(VALIDATESCHEMA).trim().equalsIgnoreCase(VALIDATIONFALSE))
-			return true;
-
+		if (env.getProperty(VALIDATESCHEMA).trim().equalsIgnoreCase(VALIDATIONFALSE)) {
+			isSchemaValidated = true;
+			return isSchemaValidated;
+		}
 		ValidationReport validationReport = jsonValidator.validateJson(jsonString);
 
 		if (validationReport.isValid())
@@ -455,8 +455,10 @@ public class PacketValidateProcessor {
 	}
 
 	private boolean fileValidation(Identity identity, InternalRegistrationStatusDto registrationStatusDto) {
-		if (env.getProperty(VALIDATEFILE).trim().equalsIgnoreCase(VALIDATIONFALSE))
-			return true;
+		if (env.getProperty(VALIDATEFILE).trim().equalsIgnoreCase(VALIDATIONFALSE)) {
+			isFilesValidated = true;
+			return isFilesValidated;
+		}
 		FilesValidation filesValidation = new FilesValidation(adapter, registrationStatusDto);
 		isFilesValidated = filesValidation.filesValidation(registrationId, identity);
 
@@ -466,8 +468,10 @@ public class PacketValidateProcessor {
 
 	private boolean checkSumValidation(Identity identity, InternalRegistrationStatusDto registrationStatusDto)
 			throws IOException {
-		if (env.getProperty(VALIDATECHECKSUM).trim().equalsIgnoreCase(VALIDATIONFALSE))
-			return true;
+		if (env.getProperty(VALIDATECHECKSUM).trim().equalsIgnoreCase(VALIDATIONFALSE)) {
+			isCheckSumValidated = true;
+			return isCheckSumValidated;
+		}
 		CheckSumValidation checkSumValidation = new CheckSumValidation(adapter, registrationStatusDto);
 		isCheckSumValidated = checkSumValidation.checksumvalidation(registrationId, identity);
 
@@ -477,9 +481,10 @@ public class PacketValidateProcessor {
 
 	private boolean applicantDocumentValidation(String jsonString)
 			throws IOException, ApisResourceAccessException, ParseException, org.json.simple.parser.ParseException {
-		if (env.getProperty(VALIDATEAPPLICANTDOCUMENT).trim().equalsIgnoreCase(VALIDATIONFALSE))
-			return true;
-
+		if (env.getProperty(VALIDATEAPPLICANTDOCUMENT).trim().equalsIgnoreCase(VALIDATIONFALSE)) {
+			isApplicantDocumentValidation = true;
+			return isApplicantDocumentValidation;
+		}
 		ApplicantDocumentValidation applicantDocumentValidation = new ApplicantDocumentValidation(utility, env,
 				applicantTypeDocument);
 		isApplicantDocumentValidation = applicantDocumentValidation.validateDocument(registrationId, jsonString);
@@ -490,9 +495,10 @@ public class PacketValidateProcessor {
 
 	private boolean masterDataValidation(String jsonString, InternalRegistrationStatusDto registrationStatusDto)
 			throws ApisResourceAccessException, IOException {
-		if (env.getProperty(VALIDATEMASTERDATA).trim().equalsIgnoreCase(VALIDATIONFALSE))
-			return true;
-
+		if (env.getProperty(VALIDATEMASTERDATA).trim().equalsIgnoreCase(VALIDATIONFALSE)) {
+			isMasterDataValidation = true;
+			return isMasterDataValidation;
+		}
 		MasterDataValidation masterDataValidation = new MasterDataValidation(env, registrationProcessorRestService,
 				utility);
 		isMasterDataValidation = masterDataValidation.validateMasterData(jsonString);
