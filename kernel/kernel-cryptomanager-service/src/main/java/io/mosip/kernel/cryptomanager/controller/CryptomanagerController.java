d25eb3bb05b073ef8365fbc30f76dba79fc00c67
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-05-23 14:07:59 +0530

    MOS-24055 endpoint removed
    Former-commit-id: be5c10cd0edbcea796ecec2fec67c1caa2b3a30f
    Former-commit-id: a026cd35b028c6fba398fd705361ee970e19d942

--- kernel/kernel-cryptomanager-service/src/main/java/io/mosip/kernel/cryptomanager/controller/CryptomanagerController.java
+++ kernel/kernel-cryptomanager-service/src/main/java/io/mosip/kernel/cryptomanager/controller/CryptomanagerController.java
@@ -6,33 +6,22 @@
  */
 package io.mosip.kernel.cryptomanager.controller;
 
-import java.util.Optional;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.kernel.core.http.RequestWrapper;
 import io.mosip.kernel.core.http.ResponseFilter;
 import io.mosip.kernel.core.http.ResponseWrapper;
-import io.mosip.kernel.cryptomanager.dto.CryptoEncryptRequestDto;
-import io.mosip.kernel.cryptomanager.dto.CryptoEncryptResponseDto;
 import io.mosip.kernel.cryptomanager.dto.CryptomanagerRequestDto;
 import io.mosip.kernel.cryptomanager.dto.CryptomanagerResponseDto;
-import io.mosip.kernel.cryptomanager.dto.PublicKeyResponse;
-import io.mosip.kernel.cryptomanager.dto.SignatureRequestDto;
-import io.mosip.kernel.cryptomanager.dto.SignatureResponseDto;
 import io.mosip.kernel.cryptomanager.service.CryptomanagerService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 /**
@@ -69,54 +58,6 @@ public class CryptomanagerController {
 		response.setResponse(cryptomanagerService.encrypt(cryptomanagerRequestDto.getRequest()));
 		return response;
 	}
-
-	/**
-	 * Encrypts data with private key
-	 * 
-	 * @param cryptomanagerRequestDto
-	 * @return {@link ResponseWrapper<CryptoEncryptResponseDto> }
-	 */
-	@ResponseFilter
-	@ApiOperation(value = "Encrypt the data with private key", response = CryptoEncryptResponseDto.class)
-	@PostMapping(value = "/private/encrypt", produces = "application/json")
-	public ResponseWrapper<CryptoEncryptResponseDto> encryptWithPrivate(
-			@ApiParam("Data to encrypt in BASE64 encoding") @RequestBody @Valid RequestWrapper<CryptoEncryptRequestDto> cryptomanagerRequestDto) {
-		ResponseWrapper<CryptoEncryptResponseDto> response = new ResponseWrapper<>();
-		response.setResponse(cryptomanagerService.encryptWithPrivate(cryptomanagerRequestDto.getRequest()));
-		return response;
-	}
-
-	
-	/**
-	 * Compute signature
-	 * 
-	 * @param cryptomanagerRequestDto
-	 * @return {@link ResponseWrapper<CryptoEncryptResponseDto> }
-	 */
-	@PreAuthorize("hasAnyRole('INDIVIDUAL','ID_AUTHENTICATION', 'REGISTRATION_ADMIN', 'REGISTRATION_SUPERVISOR', 'REGISTRATION_OFFICER', 'REGISTRATION_PROCESSOR')")
-	@ResponseFilter
-	@ApiOperation(value = "Sign Data Using Certificate")
-	@PostMapping("signature/private/encrypt")
-	public ResponseWrapper<SignatureResponseDto> signature(
-			@RequestBody RequestWrapper<SignatureRequestDto> signatureResponseDto) {
-		ResponseWrapper<SignatureResponseDto> response = new ResponseWrapper<>();
-		response.setResponse(cryptomanagerService.signaturePrivateEncrypt(signatureResponseDto.getRequest()));
-		return response;
-    }
-	
-	@PreAuthorize("hasAnyRole('INDIVIDUAL','ID_AUTHENTICATION', 'REGISTRATION_ADMIN', 'REGISTRATION_SUPERVISOR', 'REGISTRATION_OFFICER', 'REGISTRATION_PROCESSOR')")
-	@ResponseFilter
-	@ApiOperation(value = "Get Signature Public Key")
-	@GetMapping("signature/publickey/{applicationId}")
-	public ResponseWrapper<PublicKeyResponse> getSignaturePublicKey(
-			@ApiParam("Id of application") @PathVariable("applicationId") String applicationId,
-			@ApiParam("Timestamp as metadata") @RequestParam("timestamp") String timestamp,
-			@ApiParam("Refrence Id as metadata") @RequestParam("referenceId") Optional<String> referenceId) {
-		ResponseWrapper<PublicKeyResponse> response = new ResponseWrapper<>();
-		response.setResponse(cryptomanagerService.getSignPublicKey(applicationId,timestamp,referenceId));
-		return response;
-    }
-	
 	
 	/**
 	 * Controller for Decrypt the data
