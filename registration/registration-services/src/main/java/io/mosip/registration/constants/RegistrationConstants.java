be5cd781b8d3eb2d0cb2d1baf9c29a7689b983b1
Omsaieswar <omsaieswar.mulakaluri@mindtree.com>
2019-05-27 14:34:25 +0530

    MOS-24418:Modified the Sign key ref id from KER to SIGN
    Former-commit-id: fe187418914cc31390867b285c86a7fa75063f0b
    Former-commit-id: 3a70ba9908c5a5be6c5d84580a92b00045584c3b

--- registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -1332,7 +1332,7 @@ public class RegistrationConstants {
 	public static final String PUBLIC_KEY_EXPIRE_DATE = "expiryAt";
 	public static final String PUBLIC_KEY = "publicKey";
 	public static final String PUBLIC_KEY_REST = "public_key";
-	public static final String KER = "KER";
+	public static final String KER = "SIGN";
 	public static final String DTAE_MONTH_YEAR_REGEX = "dd|mm|yyyy|ddLocalLanguage|mmLocalLanguage|yyyyLocalLanguage|ageField";
 
 	public static final String UIN_UPDATE_PARENTGUARDIAN_DETAILS = "parentOrGuardianDetails";
