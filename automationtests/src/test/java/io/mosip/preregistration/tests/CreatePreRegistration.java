9fcd3dd5d53c54eabdae3560d81c54626519fb9f
Lavanya Ranganathan <M1048373@mindtree.com>
2019-05-28 14:55:23 +0530

    MOS-23993 https://mosipid.atlassian.net/browse/MOS-23993
    Former-commit-id: 26d6e84cd5ab4b3765cc79071edae40f41d8489a
    Former-commit-id: 76e6a8d311972293f6e76b240033267b24d00cf6

--- automationtests/src/test/java/io/mosip/preregistration/tests/CreatePreRegistration.java
+++ automationtests/src/test/java/io/mosip/preregistration/tests/CreatePreRegistration.java
@@ -188,7 +188,7 @@ public SoftAssert softAssert = new SoftAssert();
 		} catch (Exception e) {
 			Reporter.log("Exception : " + e.getMessage());
 		}
-		lib.logOut();
+		//lib.logOut();
 	}
 
 	@BeforeMethod(alwaysRun=true)
