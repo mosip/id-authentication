33040ac15d63dd09303ecdeef24031eb88896e2d
Raj Jha <raj.jha@mindtree.com>
2019-05-29 13:40:50 +0530

    Update SigningDataErrorCode.java
    Former-commit-id: 539e4222196d9efb50e1c881d83298596f8a507d
    Former-commit-id: 4127804b44140f129051584f9c58da7d38356cb8

--- kernel/kernel-crypto-signature/src/main/java/io/mosip/kernel/cryptosignature/constant/SigningDataErrorCode.java
+++ kernel/kernel-crypto-signature/src/main/java/io/mosip/kernel/cryptosignature/constant/SigningDataErrorCode.java
@@ -8,7 +8,7 @@ package io.mosip.kernel.cryptosignature.constant;
 public enum SigningDataErrorCode {
 
 	RESPONSE_PARSE_EXCEPTION("KER-SGN-100", "Error occured while parsing data"),
-	REST_CRYPTO_CLIENT_EXCEPTION("KER-SGN-101","Error occured while calling an Keymanger sign API"),
+	REST_CRYPTO_CLIENT_EXCEPTION("KER-SGN-101","Error occured while calling an Sign API"),
 	REST_KM_CLIENT_EXCEPTION("KER-SGN-102","Error occured while fetching Public Key");
 
 	private final String errorCode;
