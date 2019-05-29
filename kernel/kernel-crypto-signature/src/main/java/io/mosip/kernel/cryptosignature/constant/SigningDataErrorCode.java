503cf05e05950196eb8bdf481f6f947caf0f5d31
Raj Jha <raj.jha@mindtree.com>
2019-05-29 13:39:58 +0530

    sign error msg updated
    Former-commit-id: 0d9f4072efc2bf0f8027a213ef1baae0fd72911b
    Former-commit-id: e897dd1a3c707698061543f8883862e57ca21cff

--- kernel/kernel-crypto-signature/src/main/java/io/mosip/kernel/cryptosignature/constant/SigningDataErrorCode.java
+++ kernel/kernel-crypto-signature/src/main/java/io/mosip/kernel/cryptosignature/constant/SigningDataErrorCode.java
@@ -8,8 +8,8 @@ package io.mosip.kernel.cryptosignature.constant;
 public enum SigningDataErrorCode {
 
 	RESPONSE_PARSE_EXCEPTION("KER-SGN-100", "Error occured while parsing data"),
-	REST_CRYPTO_CLIENT_EXCEPTION("KER-SGN-101","Error occured while calling an CryptoManager API"),
-	REST_KM_CLIENT_EXCEPTION("KER-SGN-102","Error occured while calling an KeyManger API");
+	REST_CRYPTO_CLIENT_EXCEPTION("KER-SGN-101","Error occured while calling an Keymanger sign API"),
+	REST_KM_CLIENT_EXCEPTION("KER-SGN-102","Error occured while fetching Public Key");
 
 	private final String errorCode;
 	private final String errorMessage;
