20cf63f1d2d0a60af5c98943728be5bdfb8eb3e2
NagalakshmiNithyanandan <Nagalakshmi.Nithyanandan@mindtree.com>
2019-05-31 18:11:01 +0530

    MOS-24372 abis update
    Former-commit-id: f2c1c03d14e962fcf44c811c08e583ee9e7366d9
    Former-commit-id: 2041539e81b788b997f5324a28c3c0b15d314bba

--- registration-processor/core-processor/registration-processor-bio-dedupe-stage/src/main/java/io/mosip/registration/processor/biodedupe/stage/BioDedupeProcessor.java
+++ registration-processor/core-processor/registration-processor-bio-dedupe-stage/src/main/java/io/mosip/registration/processor/biodedupe/stage/BioDedupeProcessor.java
@@ -174,10 +174,15 @@ public class BioDedupeProcessor {
 
 			} else if (registrationType.equalsIgnoreCase(SyncTypeDto.LOST.toString())
 					&& isValidCbeff(registrationId, registrationType)) {
+				String packetStatus = abisHandlerUtil.getPacketStatus(registrationStatusDto);
 
-				List<String> matchedRegIds = abisHandlerUtil.getUniqueRegIds(registrationStatusDto.getRegistrationId(),
-						registrationType);
-				lostPacketValidation(registrationStatusDto, object, matchedRegIds);
+				if (packetStatus.equalsIgnoreCase(AbisConstant.PRE_ABIS_IDENTIFICATION)) {
+					lostPacketValidationPreAbisIdentification(registrationStatusDto, object);
+				} else if (packetStatus.equalsIgnoreCase(AbisConstant.POST_ABIS_IDENTIFICATION)) {
+					List<String> matchedRegIds = abisHandlerUtil
+							.getUniqueRegIds(registrationStatusDto.getRegistrationId(), registrationType);
+					lostPacketValidation(registrationStatusDto, object, matchedRegIds);
+				}
 
 			}
 
@@ -263,6 +268,16 @@ public class BioDedupeProcessor {
 					moduleName, registrationId);
 		}
 		return object;
+	}
+
+	private void lostPacketValidationPreAbisIdentification(InternalRegistrationStatusDto registrationStatusDto,
+			MessageDTO object) {
+
+		registrationStatusDto.setLatestTransactionStatusCode(RegistrationTransactionStatusCode.IN_PROGRESS.toString());
+		object.setMessageBusAddress(MessageBusAddress.ABIS_HANDLER_BUS_IN);
+		regProcLogger.info(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
+				registrationStatusDto.getRegistrationId(), "Lost Packet Pre abis identification");
+
 	}
 
 	/**
