42825ab48f5fd9656709578167f4e3d7d0597610
BrahmanandaReddy <brahmanandareddy.Chappidi@mindtree.com>
2019-05-31 14:52:15 +0530

    MOS-24201:FingerPrintValidatorTest.java is modified Former-commit-id: 614edc3c49c359eef730523e2b39524571f13e62 Former-commit-id: 1b0a5781ca30f6ef7debe29002e6d8e1f67fbe8c

--- registration/registration-services/src/test/java/io/mosip/registration/test/authentication/FingerprintValidatorTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/authentication/FingerprintValidatorTest.java
@@ -4,12 +4,16 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,9 +23,13 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import com.machinezoo.sourceafis.FingerprintTemplate;
+
 import io.mosip.kernel.bioapi.impl.BioApiImpl;
+import io.mosip.kernel.core.bioapi.model.KeyValuePair;
 import io.mosip.kernel.core.bioapi.model.Score;
 import io.mosip.kernel.core.cbeffutil.entity.BIR;
+import io.mosip.kernel.core.cbeffutil.entity.BIR.BIRBuilder;
 import io.mosip.kernel.core.cbeffutil.jaxbclasses.BIRType;
 import io.mosip.registration.context.ApplicationContext;
 import io.mosip.registration.dao.UserDetailDAO;
@@ -33,110 +41,117 @@ import io.mosip.registration.validator.FingerprintValidatorImpl;
 
 public class FingerprintValidatorTest {
 
-	@Rule
-	public MockitoRule mockitoRule = MockitoJUnit.rule();
+       @Rule
+       public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-	@InjectMocks
-	FingerprintValidatorImpl fingerprintValidator;
+       @InjectMocks
+       FingerprintValidatorImpl fingerprintValidator;
 
-	@Mock
-	private UserDetailDAO userDetailDAO;
-	
-	@Mock
-	private BioService bioService;
-	
-	@Mock
-	private BioApiImpl bioApiImpl;
+       @Mock
+       private UserDetailDAO userDetailDAO;
+       
+       @Mock
+       private BioService bioService;
+       
+       @Mock
+       private BioApiImpl bioApiImpl;
+       
 
-	AuthenticationValidatorDTO authenticationValidatorDTO = new AuthenticationValidatorDTO();
-	private ApplicationContext applicationContext = ApplicationContext.getInstance();
+       AuthenticationValidatorDTO authenticationValidatorDTO = new AuthenticationValidatorDTO();
+       private ApplicationContext applicationContext = ApplicationContext.getInstance();
 
-	@Before
-	public void initialize() {
-		
-		Map<String, Object> temp = new HashMap<String, Object>();
-		temp.put("mosip.registration.finger_print_score", "1");
-		applicationContext.setApplicationMap(temp);
-		
-		
-		authenticationValidatorDTO.setUserId("mosip");
-		authenticationValidatorDTO.setPassword("mosip");
-		authenticationValidatorDTO.setOtp("12345");
-		List<FingerprintDetailsDTO> fingerPrintDetails = new ArrayList<>();
-		FingerprintDetailsDTO fingerprintDetailsDTO = new FingerprintDetailsDTO();
+       @Before
+       public void initialize() throws IOException {
+             
+             Map<String, Object> temp = new HashMap<String, Object>();
+             temp.put("mosip.registration.finger_print_score", "1");
+             applicationContext.setApplicationMap(temp);
+             
+             
+             authenticationValidatorDTO.setUserId("mosip");
+             authenticationValidatorDTO.setPassword("mosip");
+             authenticationValidatorDTO.setOtp("12345");
+             List<FingerprintDetailsDTO> fingerPrintDetails = new ArrayList<>();
+             FingerprintDetailsDTO fingerprintDetailsDTO = new FingerprintDetailsDTO();
 
-		byte[] bytes = new byte[100];
-		Arrays.fill(bytes, (byte) 1);
+             byte[] bytes = new byte[100];
+             Arrays.fill(bytes, (byte) 1);
 
-		fingerprintDetailsDTO.setFingerPrint(bytes);
-		fingerprintDetailsDTO.setFingerprintImageName("fingerprintImageName");
-		fingerprintDetailsDTO.setFingerPrintISOImage("fingerPrintISOImage".getBytes());
-		fingerprintDetailsDTO.setFingerType("fingerType");
-		fingerprintDetailsDTO.setForceCaptured(false);
-		fingerprintDetailsDTO.setNumRetry(2);
-		fingerprintDetailsDTO.setQualityScore(90.1);
-		fingerPrintDetails.add(fingerprintDetailsDTO);
-		authenticationValidatorDTO.setFingerPrintDetails(fingerPrintDetails);
-	}
+       fingerprintDetailsDTO.setFingerPrint(IOUtils.toByteArray(this.getClass().getResourceAsStream("/ISOTemplate.iso")));
+       fingerprintDetailsDTO.setFingerprintImageName("fingerprintImageName");
+             fingerprintDetailsDTO.setFingerType("fingerType");
+             fingerprintDetailsDTO.setForceCaptured(false);
+             fingerprintDetailsDTO.setNumRetry(2);
+             fingerprintDetailsDTO.setQualityScore(90.1);
+             fingerPrintDetails.add(fingerprintDetailsDTO);
+       authenticationValidatorDTO.setFingerPrintDetails(fingerPrintDetails);
+       }
 
-	@Test
-	public void validateSingleTest() {
-		FingerprintDetailsDTO fingerprintDetailsDTO=new FingerprintDetailsDTO();
-		fingerprintDetailsDTO.setFingerType("right index");
-		UserBiometric userBiometric = new UserBiometric();
-		userBiometric.setQualityScore(91);
-		userBiometric.setBioMinutia("bioMinutia");
-		List<UserBiometric> userBiometrics = new ArrayList<>();
-		userBiometrics.add(userBiometric);
+       @Test
+       public void validateSingleTest() {
+//           FingerprintDetailsDTO fingerprintDetailsDTO=new FingerprintDetailsDTO();
+//           fingerprintDetailsDTO.setFingerType("right index");
+             UserBiometric userBiometric = new UserBiometric();
+             userBiometric.setQualityScore(91);
+             List<UserBiometric> userBiometrics = new ArrayList<>();
+             userBiometrics.add(userBiometric);
+             
+             FingerprintTemplate fingerprintTemplate = new FingerprintTemplate()
+                    .convert(authenticationValidatorDTO.getFingerPrintDetails().get(0).getFingerPrint());
+             String minutiae = fingerprintTemplate.serialize();
+             userBiometric.setBioMinutia(minutiae);
+             //capturedBir.setBDB(minutiae.getBytes());
+             
 
-		when(userDetailDAO.getUserSpecificBioDetail("mosip","FIN","fingerType")).thenReturn(userBiometric);
-		when(bioService.validateFP(authenticationValidatorDTO.getFingerPrintDetails().get(0), userBiometrics)).thenReturn(true);
-		authenticationValidatorDTO.setAuthValidationType("single");
-		BIRType birType=new BIRType();
-		birType.setBDB(authenticationValidatorDTO.getFingerPrintDetails().get(0).getFingerPrintISOImage());
-		BIRType biType[]=new BIRType[userBiometrics.size()];
-		BIRType b=new BIRType();
-		b.setBDB(userBiometrics.get(0).getBioMinutia().getBytes());
-		biType[0]=b;
-		Score score[]=new Score[1];
-		Score score2=new Score();
-		score2.setInternalScore(30);
-		score[0]=score2;
-		when(bioApiImpl.match(birType, biType, null)).thenReturn(score);
-		assertThat(fingerprintValidator.validate(authenticationValidatorDTO), is(true));
-	}
-	
-	@Test
-	public void validateMultipleTest() {
-		authenticationValidatorDTO.setAuthValidationType("multiple");
-		FingerprintDetailsDTO fingerprintDetailsDTO=new FingerprintDetailsDTO();
-		fingerprintDetailsDTO.setFingerType("right index");
-		UserBiometric userBiometric = new UserBiometric();
-		userBiometric.setQualityScore(91);
-		userBiometric.setBioMinutia("bioMinutia");
-		List<UserBiometric> userBiometrics = new ArrayList<>();
-		userBiometrics.add(userBiometric);
+       when(userDetailDAO.getUserSpecificBioDetails("mosip","FIN")).thenReturn(userBiometrics);
+       when(bioService.validateFP(authenticationValidatorDTO.getFingerPrintDetails().get(0), userBiometrics)).thenReturn(true);
+            authenticationValidatorDTO.setAuthValidationType("single");
+             BIR bir=new BIR(new BIRBuilder().withBdb(minutiae.getBytes()));
+             BIR biType[]=new BIR[userBiometrics.size()];
+             BIR b=new BIR(new BIRBuilder().withBdb(minutiae.getBytes()));
+             biType[0]=b;
+             Score score[]=new Score[1];
+             Score score2=new Score();
+             score2.setInternalScore(30);
+             score[0]=score2;
+             when(bioApiImpl.match(Mockito.any(), Mockito.any(), (KeyValuePair[]) Mockito.isNull())).thenReturn(score);
+       assertThat(fingerprintValidator.validate(authenticationValidatorDTO), is(true));
+       }
+       
+       @Test
+       public void validateMultipleTest() {
+          authenticationValidatorDTO.setAuthValidationType("multiple");
+             FingerprintDetailsDTO fingerprintDetailsDTO=new FingerprintDetailsDTO();
+             fingerprintDetailsDTO.setFingerType("right index");
+             UserBiometric userBiometric = new UserBiometric();
+             userBiometric.setQualityScore(91);
+             userBiometric.setBioMinutia("bioMinutia");
+             List<UserBiometric> userBiometrics = new ArrayList<>();
+             userBiometrics.add(userBiometric);
 
-		when(userDetailDAO.getUserSpecificBioDetail("mosip","FIN","fingerType")).thenReturn(userBiometric);
-		when(bioService.validateFP(authenticationValidatorDTO.getFingerPrintDetails().get(0), userBiometrics)).thenReturn(true);
-		authenticationValidatorDTO.setAuthValidationType("single");
-		BIRType birType=new BIRType();
-		birType.setBDB(authenticationValidatorDTO.getFingerPrintDetails().get(0).getFingerPrintISOImage());
-		BIRType biType[]=new BIRType[userBiometrics.size()];
-		BIRType b=new BIRType();
-		b.setBDB(userBiometrics.get(0).getBioMinutia().getBytes());
-		biType[0]=b;
-		Score score[]=new Score[1];
-		Score score2=new Score();
-		score2.setInternalScore(30);
-		score[0]=score2;
-		when(bioApiImpl.match(birType, biType, null)).thenReturn(score);
-		assertThat(fingerprintValidator.validate(authenticationValidatorDTO), is(true));
-	}
-	
-	@Test
-	public void validateTest() {
-		authenticationValidatorDTO.setAuthValidationType("");
-		assertThat(fingerprintValidator.validate(authenticationValidatorDTO), is(false));
-	}
+       when(userDetailDAO.getUserSpecificBioDetail("mosip","FIN","fingerType")).thenReturn(userBiometric);
+       when(bioService.validateFP(authenticationValidatorDTO.getFingerPrintDetails().get(0), userBiometrics)).thenReturn(true);
+            authenticationValidatorDTO.setAuthValidationType("single");
+             FingerprintTemplate fingerprintTemplate = new FingerprintTemplate()
+                    .convert(authenticationValidatorDTO.getFingerPrintDetails().get(0).getFingerPrint());
+             String minutiae = fingerprintTemplate.serialize();
+       
+             BIR bir=new BIR(new BIRBuilder().withBdb(minutiae.getBytes()));
+             BIR biType[]=new BIR[userBiometrics.size()];
+             BIR b=new BIR(new BIRBuilder().withBdb(minutiae.getBytes()));
+             biType[0]=b;
+             Score score[]=new Score[1];
+             Score score2=new Score();
+             score2.setInternalScore(30);
+             score[0]=score2;
+             when(bioApiImpl.match(Mockito.any(), Mockito.any(), (KeyValuePair[]) Mockito.isNull())).thenReturn(score);
+       assertThat(fingerprintValidator.validate(authenticationValidatorDTO), is(true));
+       }
+       
+       @Test
+       public void validateTest() {
+              authenticationValidatorDTO.setAuthValidationType("");
+       assertThat(fingerprintValidator.validate(authenticationValidatorDTO), is(false));
+       }
 }
+
