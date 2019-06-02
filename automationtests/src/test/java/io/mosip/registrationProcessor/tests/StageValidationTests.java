57a4596f957742f0b0e56745120f249a1f8313cc
Sayeri Mishra <sayeri.mishra2@mindtree.com>
2019-06-02 19:11:10 +0530

    MOS-24607 : method added for creating updated packet https://mosipid.atlassian.net/browse/MOS-24607
    Former-commit-id: 565c78d35d3fccac8c148b146a66ba802fdbbafa
    Former-commit-id: e723ff6a86b985d48968cef004fc41c871088599

--- automationtests/src/test/java/io/mosip/registrationProcessor/tests/StageValidationTests.java
+++ automationtests/src/test/java/io/mosip/registrationProcessor/tests/StageValidationTests.java
@@ -76,17 +76,18 @@ public class StageValidationTests extends BaseTestCase implements ITest {
 		}
 		String validPacketPath = property.getProperty("validPacketForPacketGeneration");
 		String invalidPacketFolderPath = property.getProperty("invalidPacketFolderPath");
-		/*e.packetValidatorPropertyFileReader("packetValidator.properties", validPacketPath, invalidPacketFolderPath);
+		e.packetValidatorPropertyFileReader("packetValidator.properties", validPacketPath, invalidPacketFolderPath);
 		for (int i = 0; i < 2; i++) {
 			e.demoDedupePropertyFileReader("IDjson.properties", validPacketPath, invalidPacketFolderPath);
 		}
 		e.osiValidatorPropertyFileReader("packetProperties.properties", validPacketPath, invalidPacketFolderPath);
+		e.updatePacketPropertyFileReader("updatePacketProperties.properties",validPacketPath,invalidPacketFolderPath);
 		try {
 			reader.close();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}*/
+		}
 	}
 
 	@DataProvider(name = "packetValidatorStage")
@@ -124,7 +125,7 @@ public class StageValidationTests extends BaseTestCase implements ITest {
 		 } 
 		return objArray;
 	}
-/*	@DataProvider(name="demoDedupeStage")
+	@DataProvider(name="demoDedupeStage")
 	public File[] getInvalidDemoDedupePackets() {
 		
 		File file = new File(invalidPacketPath + "/DemoDedupe");
@@ -141,7 +142,7 @@ public class StageValidationTests extends BaseTestCase implements ITest {
 		 } 
 		return objArray;
 	}
-	*/
+	
 	
 	@Test(dataProvider = "packetValidatorStage")
 	public void packetValidatorStage(File[] listOfInvpackets) {
@@ -225,7 +226,7 @@ public class StageValidationTests extends BaseTestCase implements ITest {
 	//	softAssert.assertAll();
 		regID="";
 	}
-	/*@Test(dataProvider = "demoDedupeStage")
+	@Test(dataProvider = "demoDedupeStage")
 	public void demoDedupeStage(File[] listOfInvpackets) {
 		List<String> statusCodes = new ArrayList<String>();
 		Properties prop = new Properties();
@@ -263,7 +264,7 @@ public class StageValidationTests extends BaseTestCase implements ITest {
 	//	cleanUp.prepareQueryList(regID);
 		softAssert.assertAll();
 		regID="";
-	}*/
+	}
 	
 
 
