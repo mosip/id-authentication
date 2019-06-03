b773d315638a9bf405f2825c88062550b4b4dcd6
NagalakshmiNithyanandan <Nagalakshmi.Nithyanandan@mindtree.com>
2019-06-03 17:43:02 +0530

    MOS-25392 fix for biometric auth
    Former-commit-id: 4eadb8e6e0b961d817c66582b15bd13877865d22
    Former-commit-id: 3eadb24de260ab9e98135b7222bb850922c21d49

--- registration-processor/core-processor/registration-processor-biometric-authentication-stage/src/main/java/io/mosip/registration/processor/biometric/authentication/BiometricAuthenticationApplication.java
+++ registration-processor/core-processor/registration-processor-biometric-authentication-stage/src/main/java/io/mosip/registration/processor/biometric/authentication/BiometricAuthenticationApplication.java
@@ -1,6 +1,5 @@
 package io.mosip.registration.processor.biometric.authentication;
 
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import io.mosip.registration.processor.biometric.authentication.stage.BiometricAuthenticationStage;
@@ -9,14 +8,14 @@ public class BiometricAuthenticationApplication {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-		ctx.scan("io.mosip.registration.processor.biometric.authentication.config", 
-				"io.mosip.registration.processor.status.config",
-				"io.mosip.registration.processor.rest.client.config",
-				"io.mosip.registration.processor.packet.storage.config", 
-				"io.mosip.registration.processor.core.config",
-				"io.mosip.registration.processor.core.kernel.beans");
+		ctx.scan("io.mosip.registration.processor.biometric.authentication.config",
+				"io.mosip.registration.processor.status.config", "io.mosip.registration.processor.rest.client.config",
+				"io.mosip.registration.processor.packet.storage.config", "io.mosip.registration.processor.core.config",
+				"io.mosip.registration.processor.core.kernel.beans",
+				"io.mosip.registration.processor.packet.manager.config");
 		ctx.refresh();
-		BiometricAuthenticationStage validatebean = (BiometricAuthenticationStage) ctx.getBean(BiometricAuthenticationStage.class);
+		BiometricAuthenticationStage validatebean = (BiometricAuthenticationStage) ctx
+				.getBean(BiometricAuthenticationStage.class);
 		validatebean.deployVerticle();
 	}
 
