2b33ca196bd8092cc254e82e1c4ab439a06b733f
Rishabh Keshari <Rishabh.Keshari@mindtree.com>
2019-06-03 17:31:48 +0530

    MOS-24949 : code review comments dopne https://mosipid.atlassian.net/browse/MOS-24949
    Former-commit-id: 4a87a3ebaeaabcc9a8e488f1e84c3ccd82ea80a2
    Former-commit-id: 6e24a7aca5897d3ac151acc9a05d9e1a1d9c43d3

--- registration-processor/core-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/RestAbisApplication.java
+++ registration-processor/core-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/RestAbisApplication.java
@@ -12,7 +12,7 @@ public class RestAbisApplication {
 		configApplicationContext.scan("io.mosip.registration.processor.abis.config",
 				"io.mosip.registration.processor.stages.config", "io.mosip.registration.processor.status.config",
 				"io.mosip.registration.processor.packet.storage.config", "io.mosip.registration.processor.core.config",
-				"io.mosip.registration.processor.core.kernel.beans", "io.mosip.kernel.auth.*");
+				"io.mosip.registration.processor.core.kernel.beans", "io.mosip.kernel.auth.*","io.mosip.registration.processor.packet.manager.config");
 		configApplicationContext.refresh();
 		AbisMessageQueueImpl abisMessageQueueImpl = configApplicationContext.getBean(AbisMessageQueueImpl.class);
 		abisMessageQueueImpl.runAbisQueue();
