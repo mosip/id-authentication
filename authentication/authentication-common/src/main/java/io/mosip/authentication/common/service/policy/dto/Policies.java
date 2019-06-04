eee7c05227996032b78b2e4a8ed3de0f715f236b
Arun Bose <arunbose.subashchandrabose@mindtree.com>
2019-06-04 16:41:34 +0530

    MOS-24937 : javadoc updated
    Former-commit-id: ed6148402758839bfa0b74ebf6f74100e0c402a2
    Former-commit-id: 45138cac4638008d517a849756aa3398bdf75f38

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/policy/dto/Policies.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/policy/dto/Policies.java
@@ -2,7 +2,13 @@ package io.mosip.authentication.common.service.policy.dto;
 
 import lombok.Data;
 
+/**
+ * Policies restricts the authorization type allowed,which is mapped with Auth Policy Json.
+ * @author Arun Bose S
+ */
 @Data
 public class Policies {
+	
+	/** The policies. */
 	private Policy policies;
 }
