7763b89efdc3962c39ad6bb7608291b42b4c094b
Ravikant-M1044571 <ravi.kant2@mindtree.com>
2019-06-06 10:06:13 +0530

    MOS-24926 : updated DB Validation for history validation
    Former-commit-id: e8b1a16755d57be108180b1ef4d97af6e2219214
    Former-commit-id: f8adc22478246aa428d5040abe982b03e1d3bcf3

--- automationtests/src/test/java/io/mosip/kernel/tests/FetchRegCentHistory.java
+++ automationtests/src/test/java/io/mosip/kernel/tests/FetchRegCentHistory.java
@@ -150,7 +150,7 @@ public class FetchRegCentHistory extends BaseTestCase implements ITest {
 			logger.info("===Dbcount===" + obtainedObjectsCount + "===Get-count===" + responseArrayFromGet.size());
 
 			// validating number of objects obtained form db and from get request
-			if (responseArrayFromGet.size() == obtainedObjectsCount) {
+			if (responseArrayFromGet.size() <= obtainedObjectsCount) {
 
 				// list to validate existance of attributes in response objects
 				List<String> attributesToValidateExistance = new ArrayList<String>();
