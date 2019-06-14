47626b3e6d56b46936f36d5b2a49f2b5fa46e491
M1050450 <shekhar.suman@mindtree.com>
2019-06-14 15:21:06 +0530

    MOS-0000 : Changes Retry Stage https://mosipid.atlassian.net/browse/MOS-0000
    Former-commit-id: e7395f35bb16ff61a395aa0ad4eb5a70657dbf6c
    Former-commit-id: d06d702f67985c1b2d04f6486cf8810e8e99d365

--- registration-processor/core-processor/registration-processor-retry-stage/src/main/java/io/mosip/registration/processor/retry/verticle/config/RetryStageConfig.java
+++ registration-processor/core-processor/registration-processor-retry-stage/src/main/java/io/mosip/registration/processor/retry/verticle/config/RetryStageConfig.java
@@ -7,14 +7,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.mosip.registration.processor.retry.verticle.stages.RetryStage;
 
+/**
+ * The Class RetryStageConfig.
+ */
 @Configuration
 public class RetryStageConfig {
 
+	/**
+	 * Gets the retry stage.
+	 *
+	 * @return the retry stage
+	 */
 	@Bean
 	public RetryStage getRetryStage() {
 		return new RetryStage();
 	}
 
+	/**
+	 * Gets the mapper.
+	 *
+	 * @return the mapper
+	 */
 	@Bean
 	public ObjectMapper getMapper() {
 		return new ObjectMapper();
