546dce0f5fd4adda920b52caa374e3eae2b3c447
yaswanths2 <yaswanth.s2@mindtree.com>
2019-06-17 15:51:09 +0530

    MOS-25792 : Added Javadocs for initial sync
    Former-commit-id: fae641a87b5f86d9ba51402fec4da0cf22436b75
    Former-commit-id: 09e2b1e90a933d96e64c321de19ea5a249cf0f93

--- registration/registration-services/src/main/java/io/mosip/registration/service/login/LoginService.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/login/LoginService.java
@@ -25,9 +25,11 @@ public interface LoginService {
 	 * authentication type and set of roles
 	 * 
 	 * @param authType
-	 *            {@code String} authentication type [Login auth, Packet auth, Exception auth, EOD auth, Onboard auth]
+	 *            {@code String} authentication type [Login auth, Packet auth,
+	 *            Exception auth, EOD auth, Onboard auth]
 	 * @param roleList
-	 *            {@code List} list of user roles[Registration Officer, Registration supervisor, Registration Admin]
+	 *            {@code List} list of user roles[Registration Officer, Registration
+	 *            supervisor, Registration Admin]
 	 * 
 	 * @return List of login modes
 	 */
@@ -38,8 +40,9 @@ public interface LoginService {
 	 * 
 	 * @param userId
 	 *            {@code String} user id entered by the operator
-	 *            
-	 * @return {@code UserDTO} UserDTO which will have roles, pword, center and machine information
+	 * 
+	 * @return {@code UserDTO} UserDTO which will have roles, pword, center and
+	 *         machine information
 	 */
 	UserDTO getUserDetail(String userId);
 
@@ -51,8 +54,9 @@ public interface LoginService {
 	 *            {@code String} centerId corresponding to entered userId
 	 * @param langCode
 	 *            {@code String} primary language code
-	 *            
-	 * @return {@code RegistrationCenterDetailDTO} which contains Registration center details
+	 * 
+	 * @return {@code RegistrationCenterDetailDTO} which contains Registration
+	 *         center details
 	 */
 	RegistrationCenterDetailDTO getRegistrationCenterDetails(String centerId, String langCode);
 
@@ -62,7 +66,7 @@ public interface LoginService {
 	 * 
 	 * @param roleCode
 	 *            {@code List} list of roles mapped to the user
-	 *            
+	 * 
 	 * @return {@code AuthorizationDTO} which contains screen authorization details
 	 *         based on which screen validation will happen
 	 */
@@ -71,59 +75,85 @@ public interface LoginService {
 	/**
 	 * This method will update login parameters for every login attempt
 	 * 
-	 * @param {@code UserDTO} user dto which contains the parameters that has to be updated
+	 * @param {@code UserDTO} user dto which contains the parameters that has to be
+	 *            updated
 	 */
 	void updateLoginParams(UserDTO userDTO);
-	
+
 	/**
-	 * Execute initial sync
+	 * Execute global param, master sync, user detail, user salt detail and key
+	 * policy sync's as a initial sync
 	 * 
-	 * @return List of sync results
+	 * @return {@code List} list of sync results
 	 */
 	List<String> initialSync();
-	
-	
+
 	/**
-	 * This method will Validate login attempts to check whether operator can login or not
+	 * This method will Validate login attempts to check whether operator can login
+	 * or not
 	 * 
-	 * <p>This will validate against invalid number of attempts and time and update the
-	 * table with the corresponding parameters in case of invalid or valid</p>
+	 * <p>
+	 * This will validate against invalid number of attempts and time and update the
+	 * table with the corresponding parameters in case of invalid or valid
+	 * </p>
 	 * 
 	 * @param userDTO
 	 *            {@code UserDTO} user details
 	 * @param errorMessage
 	 *            {@code String} error message for validation
 	 * @param invalidLoginCount
-	 *            {@code Integer} invalid login count against which validation will be done
+	 *            {@code Integer} invalid login count against which validation will
+	 *            be done
 	 * @param invalidLoginTime
-	 *            {@code Integer} invalid login time against which validation will be done 
-	 *            
+	 *            {@code Integer} invalid login time against which validation will
+	 *            be done
+	 * 
 	 * @return {@code String} returns whether validation successful or not
 	 */
 	String validateInvalidLogin(UserDTO userDTO, String errorMessage, int invalidLoginCount, int invalidLoginTime);
-	
+
 	/**
 	 * This method will Validate user id entered by operator
 	 * 
-	 * <p>If User exists :</p>
-	 * 		<p>Fetch the center id mapped to user id entered by operator</p>
-	 * 			<p>Fetch the roles mapped to user id</p>
-	 * 				<p>The success response DTO will be formed and returned from this method.</p>
+	 * <p>
+	 * If User exists :
+	 * </p>
+	 * <p>
+	 * Fetch the center id mapped to user id entered by operator
+	 * </p>
+	 * <p>
+	 * Fetch the roles mapped to user id
+	 * </p>
+	 * <p>
+	 * The success response DTO will be formed and returned from this method.
+	 * </p>
 	 * 
-	 * 			<p>If Roles not matches or exists</p>
-	 * 				<p>The failure response DTO will be formed and returned from this method.</p>
+	 * <p>
+	 * If Roles not matches or exists
+	 * </p>
+	 * <p>
+	 * The failure response DTO will be formed and returned from this method.
+	 * </p>
 	 * 
-	 * 		<p>If Center id not matches or exists</p>
-	 * 			<p>The failure response DTO will be formed and returned from this method.</p>
+	 * <p>
+	 * If Center id not matches or exists
+	 * </p>
+	 * <p>
+	 * The failure response DTO will be formed and returned from this method.
+	 * </p>
 	 * 
-	 * <p>If user not exists :</p>
-	 * 		<p>The failure response DTO will be formed and returned from this method.</p>
+	 * <p>
+	 * If user not exists :
+	 * </p>
+	 * <p>
+	 * The failure response DTO will be formed and returned from this method.
+	 * </p>
 	 * 
 	 * @param userId
 	 *            {@code String} user-id entered by operator
-	 *            
-	 * @return {@code ResponseDTO}
-	 * 			based on the result the response DTO will be formed and return to the caller.
+	 * 
+	 * @return {@code ResponseDTO} based on the result the response DTO will be
+	 *         formed and return to the caller.
 	 */
 	ResponseDTO validateUser(String userId);
 
