f3253d66123935b103b298c4109621f7f80fd0fd
Omsaieswar <omsaieswar.mulakaluri@mindtree.com>
2019-06-17 14:00:04 +0530

    MOS-24418:Added the java doc for the user detail service
    Former-commit-id: 4ee97a06d1039e1fd98a57852715312675a6a10d
    Former-commit-id: 946f3e6f8675a64cc7f2904b21d3878446d6e8d0

--- registration/registration-services/src/main/java/io/mosip/registration/service/operator/UserDetailService.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/operator/UserDetailService.java
@@ -7,11 +7,10 @@ import io.mosip.registration.dto.ResponseDTO;
  * information, while performing the user details sync(on-line) from server to
  * client. The registered user details WRT the center id will be sync as part of
  * this and same information will be persisted to the database. This class will
- * be invoked while performing automatic and manual sync operations.
+ * be invoked while performing the automatic[batch job] and manual sync operations.
  * 
  * @author Sreekar Chukka
- * @since 1.0.0
- *
+ * 
  */
 public interface UserDetailService {
 
@@ -19,7 +18,7 @@ public interface UserDetailService {
 	 * This method performs to invoke the user details sync operation based on the center-id, in synchronous way. 
 	 * 
 	 * <p>The center-id will be picked up based on the mac-id of the machine. While performing the call
-	 * the online connectivity check will be performed to connect to the server and sync the user details.</p>
+	 * the online connectivity check will be performed as preliminary step to connect to the server and then sync the user details.</p>
 	 * 
 	 * <p>If Online : </p>
 	 * 		<p>The server call performs and based on the result the return response will be formed.</p> 
