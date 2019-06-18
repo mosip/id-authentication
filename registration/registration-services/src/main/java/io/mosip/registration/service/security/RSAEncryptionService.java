45b171e3744c65b4debe9082fec054380961e420
yaswanths2 <yaswanth.s2@mindtree.com>
2019-06-18 14:49:05 +0530

    MOS-25792 : Added Javadocs and removed hardcoded job id
    Former-commit-id: c44cc8c6c0541ae248043af0da2367405c7fc01d
    Former-commit-id: 787247e688b23da5cc3d616ea4a1ef5c7184c6cc

--- registration/registration-services/src/main/java/io/mosip/registration/service/security/RSAEncryptionService.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/security/RSAEncryptionService.java
@@ -3,7 +3,8 @@ package io.mosip.registration.service.security;
 import io.mosip.registration.exception.RegBaseCheckedException;
 
 /**
- * Accepts AES session key as bytes and encrypt it by using RSA algorithm
+ * The {@code RSAEncryptionService} Accepts AES session key as bytes and encrypt
+ * it by using RSA algorithm
  * 
  * @author YASWANTH S
  * @author Balaji Sridharan
@@ -15,11 +16,11 @@ public interface RSAEncryptionService {
 	/**
 	 * Encrypts the AES Session Key using RSA encryption algorithm
 	 * 
-	 * @param sessionKey
-	 *            has to be encrypted as bytes
-	 * @return rsaEncryptedBytes has encrypted by rsa
-	 * @throws RegBaseCheckedException
-	 *             if any occurs while encrypting
+	 * @param {@code byte[]} sessionKey has to be encrypted as bytes
+	 * @return {@code byte[]} rsaEncryptedBytes has encrypted by rsa
+	 * @throws {@code
+	 *             RegBaseCheckedException} RegBaseCheckedException if any occurs
+	 *             while encrypting
 	 */
 	byte[] encrypt(final byte[] sessionKey) throws RegBaseCheckedException;
 }
