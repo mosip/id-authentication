35620dd5e23360a289005d125f397fd16f56b739
Omsaieswar <omsaieswar.mulakaluri@mindtree.com>
2019-06-18 09:52:37 +0530

    MOS-24418:Added the logs
    Former-commit-id: 1a44bff68a63c842d4259a024ef802d345e6796e
    Former-commit-id: 93ad539d00caaa37ac3220dcfc7864b541e1332d

--- registration/registration-services/src/main/java/io/mosip/registration/util/advice/RestClientAuthAdvice.java
+++ registration/registration-services/src/main/java/io/mosip/registration/util/advice/RestClientAuthAdvice.java
@@ -90,7 +90,7 @@ public class RestClientAuthAdvice {
 			Object response = joinPoint.proceed(joinPoint.getArgs());
 
 			LOGGER.info(LoggerConstants.AUTHZ_ADVICE, APPLICATION_ID, APPLICATION_NAME,
-					"Adding authZ token to web service request header if required completed");
+					"Adding authZ token to web service request header if required completed" + response);
 			
 			if (handleInvalidTokenFromResponse(response, joinPoint)) {
 				LOGGER.info(LoggerConstants.AUTHZ_ADVICE, APPLICATION_ID, APPLICATION_NAME,
