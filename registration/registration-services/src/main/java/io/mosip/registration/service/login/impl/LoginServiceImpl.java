1926016797ad387ec3363633e585b4fbdc3887e1
Mahesh Kumar Gattupalli <Mahesh.Gattupalli@mindtree.com>
2019-06-18 13:33:37 +0530

    MOS-25790 : Fix for code smells
    Former-commit-id: aa0212b01d2ce582df67e63022613e98e3822ee8
    Former-commit-id: 6be40e04c8a17a2324dce950edec9444cbeb5081

--- registration/registration-services/src/main/java/io/mosip/registration/service/login/impl/LoginServiceImpl.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/login/impl/LoginServiceImpl.java
@@ -232,6 +232,26 @@ public class LoginServiceImpl extends BaseService implements LoginService {
 			}
 		}
 
+		performingAllSyncOperations(val, keyIndex, isInitialSetUp);
+
+		LOGGER.info("REGISTRATION  - LOGINSERVICE", APPLICATION_NAME, APPLICATION_ID, "completed Initial sync");
+
+		return val;
+
+	}
+
+
+	/**
+	 * Performing all sync operations.
+	 *
+	 * @param val 
+	 * 				the List of values
+	 * @param keyIndex 
+	 * 				the key index
+	 * @param isInitialSetUp 
+	 * 				the isInitialSetUp flag
+	 */
+	private void performingAllSyncOperations(List<String> val, String keyIndex, final boolean isInitialSetUp) {
 		ResponseDTO publicKeySyncResponse = publicKeySyncImpl
 				.getPublicKey(RegistrationConstants.JOB_TRIGGER_POINT_USER);
 		ResponseDTO responseDTO = globalParamService.synchConfigData(false);
@@ -261,11 +281,6 @@ public class LoginServiceImpl extends BaseService implements LoginService {
 		} else {
 			val.add(RegistrationConstants.SUCCESS);
 		}
-
-		LOGGER.info("REGISTRATION  - LOGINSERVICE", APPLICATION_NAME, APPLICATION_ID, "completed Initial sync");
-
-		return val;
-
 	}
 
 	
