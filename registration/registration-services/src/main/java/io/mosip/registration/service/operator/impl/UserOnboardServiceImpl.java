c0844b0a7e855e515748555b7d8ace09d085c8a7
Chukka.Sreekar@mindtree.com <M1048290@b2ml24784.mindtree.com>
2019-06-19 14:41:37 +0530

    MOS-25781-UserOnBoard IDA integration
    Former-commit-id: 763b6c5e83a1972de8a4f58d53119469c2e8cc8f
    Former-commit-id: ae5cbc13ed50f6a200ab4dcea145a3b54d611d6f

--- registration/registration-services/src/main/java/io/mosip/registration/service/operator/impl/UserOnboardServiceImpl.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/operator/impl/UserOnboardServiceImpl.java
@@ -229,7 +229,7 @@ public class UserOnboardServiceImpl extends BaseService implements UserOnboardSe
 						LOGGER.info(LOG_REG_USER_ONBOARD, APPLICATION_NAME, APPLICATION_ID,
 								"User Onboarded authentication flag... :" + onboardAuthFlag);
 
-						if (true) {
+						if (onboardAuthFlag) {
 							responseDTO = save(biometricDTO);
 							LOGGER.info(LOG_REG_USER_ONBOARD, APPLICATION_NAME, APPLICATION_ID,
 									RegistrationConstants.USER_ON_BOARDING_SUCCESS_MSG);
