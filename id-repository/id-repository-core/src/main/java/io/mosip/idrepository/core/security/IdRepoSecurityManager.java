b65132b1e934514fb027546019619b2f05533c79
Manoj SP <Manoj.SP@mindtree.com>
2019-06-19 12:48:38 +0530

    MOS-25808 - added method to fetch user
    Former-commit-id: 719d1a99b68a0bf7bc9f1b053b7b892d2e0b8b73
    Former-commit-id: ae0b26babe95c1a5692216a1ff40afd4350238a8

--- id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/security/IdRepoSecurityManager.java
+++ id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/security/IdRepoSecurityManager.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,11 +20,14 @@ import io.mosip.idrepository.core.exception.IdRepoAppException;
 import io.mosip.idrepository.core.exception.RestServiceException;
 import io.mosip.idrepository.core.helper.RestHelper;
 import io.mosip.idrepository.core.logger.IdRepoLogger;
+import io.mosip.kernel.auth.adapter.model.AuthUserDetails;
+import io.mosip.kernel.core.http.RequestWrapper;
 import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.core.util.CryptoUtil;
 import io.mosip.kernel.core.util.DateUtils;
 import io.mosip.kernel.core.util.HMACUtils;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class IdRepoSecurityManager.
  *
@@ -35,6 +39,7 @@ public class IdRepoSecurityManager {
 	/** The mosip logger. */
 	private Logger mosipLogger = IdRepoLogger.getLogger(IdRepoSecurityManager.class);
 
+	/** The Constant ENCRYPT_DECRYPT_DATA. */
 	private static final String ENCRYPT_DECRYPT_DATA = "encryptDecryptData";
 
 	/** The Constant ID_REPO_SECURITY_MANAGER. */
@@ -74,8 +79,21 @@ public class IdRepoSecurityManager {
 	 * @return the string
 	 */
 	public String hashwithSalt(final byte[] data, final byte[] salt) {
-		// add salt in argument
-		return HMACUtils.digestAsPlainTextWithSalt(data,salt);
+		return HMACUtils.digestAsPlainTextWithSalt(data, salt);
+	}
+	
+	/**
+	 * Gets the user.
+	 *
+	 * @return the user
+	 */
+	public static String getUser() {
+		if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof AuthUserDetails) {
+			return ((AuthUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+					.getUserId();
+		} else {
+			return null;
+		}
 	}
 
 	/**
@@ -86,19 +104,17 @@ public class IdRepoSecurityManager {
 	 * @throws IdRepoAppException the id repo app exception
 	 */
 	public byte[] encrypt(final byte[] dataToEncrypt) throws IdRepoAppException {
-		// salt as input param
 		try {
-			ObjectNode baseRequest = new ObjectNode(mapper.getNodeFactory());
-			baseRequest.put("id", "string");
-			baseRequest.put("requesttime",
-					DateUtils.formatDate(new Date(), env.getProperty(IdRepoConstants.DATETIME_PATTERN.getValue())));
-			baseRequest.put("version", "1.0");
+			RequestWrapper<ObjectNode> baseRequest = new RequestWrapper<>();
+			baseRequest.setId("string");
+			baseRequest.setRequesttime(DateUtils.getUTCCurrentDateTime());
+			baseRequest.setVersion(env.getProperty(IdRepoConstants.APPLICATION_VERSION.getValue()));
 			ObjectNode request = new ObjectNode(mapper.getNodeFactory());
 			request.put("applicationId", env.getProperty(IdRepoConstants.APPLICATION_ID.getValue()));
 			request.put("timeStamp",
 					DateUtils.formatDate(new Date(), env.getProperty(IdRepoConstants.DATETIME_PATTERN.getValue())));
 			request.put("data", CryptoUtil.encodeBase64(dataToEncrypt));
-			baseRequest.set("request", request);
+			baseRequest.setRequest(request);
 			return encryptDecryptData(restBuilder.buildRequest(RestServicesConstants.CRYPTO_MANAGER_ENCRYPT,
 					baseRequest, ObjectNode.class));
 		} catch (IdRepoAppException e) {
@@ -111,24 +127,23 @@ public class IdRepoSecurityManager {
 	 * Encrypt.
 	 *
 	 * @param dataToEncrypt the data to encrypt
+	 * @param saltToEncrypt the salt to encrypt
 	 * @return the byte[]
 	 * @throws IdRepoAppException the id repo app exception
 	 */
 	public byte[] encryptWithSalt(final byte[] dataToEncrypt,final byte[] saltToEncrypt) throws IdRepoAppException {
-		// salt as input param
 		try {
-			ObjectNode baseRequest = new ObjectNode(mapper.getNodeFactory());
-			baseRequest.put("id", "string");
-			baseRequest.put("requesttime",
-					DateUtils.formatDate(new Date(), env.getProperty(IdRepoConstants.DATETIME_PATTERN.getValue())));
-			baseRequest.put("version", "1.0");
+			RequestWrapper<ObjectNode> baseRequest = new RequestWrapper<>();
+			baseRequest.setId("string");
+			baseRequest.setRequesttime(DateUtils.getUTCCurrentDateTime());
+			baseRequest.setVersion(env.getProperty(IdRepoConstants.APPLICATION_VERSION.getValue()));
 			ObjectNode request = new ObjectNode(mapper.getNodeFactory());
 			request.put("applicationId", env.getProperty(IdRepoConstants.APPLICATION_ID.getValue()));
 			request.put("timeStamp",
 					DateUtils.formatDate(new Date(), env.getProperty(IdRepoConstants.DATETIME_PATTERN.getValue())));
 			request.put("data", CryptoUtil.encodeBase64(dataToEncrypt));
 			request.put("salt", CryptoUtil.encodeBase64(saltToEncrypt));
-			baseRequest.set("request", request);
+			baseRequest.setRequest(request);
 			return encryptDecryptData(restBuilder.buildRequest(RestServicesConstants.CRYPTO_MANAGER_ENCRYPT,
 					baseRequest, ObjectNode.class));
 		} catch (IdRepoAppException e) {
@@ -146,17 +161,16 @@ public class IdRepoSecurityManager {
 	 */
 	public byte[] decrypt(final byte[] dataToDecrypt) throws IdRepoAppException {
 		try {
-			ObjectNode baseRequest = new ObjectNode(mapper.getNodeFactory());
-			baseRequest.put("id", "string");
-			baseRequest.put("requesttime",
-					DateUtils.formatDate(new Date(), env.getProperty(IdRepoConstants.DATETIME_PATTERN.getValue())));
-			baseRequest.put("version", "1.0");
+			RequestWrapper<ObjectNode> baseRequest = new RequestWrapper<>();
+			baseRequest.setId("string");
+			baseRequest.setRequesttime(DateUtils.getUTCCurrentDateTime());
+			baseRequest.setVersion(env.getProperty(IdRepoConstants.APPLICATION_VERSION.getValue()));
 			ObjectNode request = new ObjectNode(mapper.getNodeFactory());
 			request.put("applicationId", env.getProperty(IdRepoConstants.APPLICATION_ID.getValue()));
 			request.put("timeStamp",
 					DateUtils.formatDate(new Date(), env.getProperty(IdRepoConstants.DATETIME_PATTERN.getValue())));
 			request.put("data", new String(dataToDecrypt));
-			baseRequest.set("request", request);
+			baseRequest.setRequest(request);
 			return CryptoUtil.decodeBase64(new String(encryptDecryptData(restBuilder
 					.buildRequest(RestServicesConstants.CRYPTO_MANAGER_DECRYPT, baseRequest, ObjectNode.class))));
 		} catch (IdRepoAppException e) {
@@ -165,20 +179,27 @@ public class IdRepoSecurityManager {
 		}
 	}
 	
+	/**
+	 * Decrypt with salt.
+	 *
+	 * @param dataToDecrypt the data to decrypt
+	 * @param saltToDecrypt the salt to decrypt
+	 * @return the byte[]
+	 * @throws IdRepoAppException the id repo app exception
+	 */
 	public byte[] decryptWithSalt(final byte[] dataToDecrypt, final byte[] saltToDecrypt) throws IdRepoAppException {
 		try {
-			ObjectNode baseRequest = new ObjectNode(mapper.getNodeFactory());
-			baseRequest.put("id", "string");
-			baseRequest.put("requesttime",
-					DateUtils.formatDate(new Date(), env.getProperty(IdRepoConstants.DATETIME_PATTERN.getValue())));
-			baseRequest.put("version", "1.0");
+			RequestWrapper<ObjectNode> baseRequest = new RequestWrapper<>();
+			baseRequest.setId("string");
+			baseRequest.setRequesttime(DateUtils.getUTCCurrentDateTime());
+			baseRequest.setVersion(env.getProperty(IdRepoConstants.APPLICATION_VERSION.getValue()));
 			ObjectNode request = new ObjectNode(mapper.getNodeFactory());
 			request.put("applicationId", env.getProperty(IdRepoConstants.APPLICATION_ID.getValue()));
 			request.put("timeStamp",
 					DateUtils.formatDate(new Date(), env.getProperty(IdRepoConstants.DATETIME_PATTERN.getValue())));
 			request.put("data", CryptoUtil.encodeBase64(dataToDecrypt));
 			request.put("salt", CryptoUtil.encodeBase64(saltToDecrypt));
-			baseRequest.set("request", request);
+			baseRequest.setRequest(request);
 			return CryptoUtil.decodeBase64(new String(encryptDecryptData(restBuilder
 					.buildRequest(RestServicesConstants.CRYPTO_MANAGER_DECRYPT, baseRequest, ObjectNode.class))));
 		} catch (IdRepoAppException e) {
@@ -196,7 +217,6 @@ public class IdRepoSecurityManager {
 	 */
 	private byte[] encryptDecryptData(final RestRequestDTO restRequest) throws IdRepoAppException {
 		try {
-			restRequest.setTimeout(null);
 			ObjectNode response = restHelper.requestSync(restRequest);
 
 			if (response.has("response") && Objects.nonNull(response.get("response"))
