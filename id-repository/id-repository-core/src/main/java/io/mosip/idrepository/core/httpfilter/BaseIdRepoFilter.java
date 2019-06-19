b12845bffcc99ba92b79ab7d0d3745a3fd85ae48
Manoj SP <Manoj.SP@mindtree.com>
2019-06-20 00:03:53 +0530

    MOS-25815 - updated base filter
    Former-commit-id: 842bd16c6b3572ad9001f47cea70a1560134e7f0
    Former-commit-id: a3bbb841f6a5b673b1e00f29a0c197933e676119

--- id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/httpfilter/BaseIdRepoFilter.java
+++ id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/httpfilter/BaseIdRepoFilter.java
@@ -3,8 +3,11 @@ package io.mosip.idrepository.core.httpfilter;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
 
+import javax.annotation.Resource;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -14,21 +17,20 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 
 import io.mosip.idrepository.core.logger.IdRepoLogger;
 import io.mosip.idrepository.core.security.IdRepoSecurityManager;
 import io.mosip.kernel.core.logger.spi.Logger;
 
 /**
- * The Class BaseIdRepoFilter - base class that logs request time and time taken
- * to complete the request.
+ * The Class BaseIdRepoFilter.
  *
  * @author Manoj SP
  * @author Prem Kumar
  */
 @Component
 public abstract class BaseIdRepoFilter implements Filter  {
-	
 	/** The Constant ID_REPO_FILTER. */
 	private static final String ID_REPO_FILTER = "IdRepoFilter";
 
@@ -37,12 +39,46 @@ public abstract class BaseIdRepoFilter implements Filter  {
 
 	/** The mosip logger. */
 	Logger mosipLogger = IdRepoLogger.getLogger(BaseIdRepoFilter.class);
+
+	/** The path matcher. */
+	AntPathMatcher pathMatcher = new AntPathMatcher();
+
+	/** The id. */
+	@Resource
+	private Map<String, String> id;
+
+	/** The uin. */
+	String uin;
 	
 	/* (non-Javadoc)
 	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
 	 */
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+	
+	/**
+	 * Should not filter.
+	 *
+	 * @param request the request
+	 * @return true, if successful
+	 */
+	private boolean shouldNotFilter(HttpServletRequest request) {
+		if (Objects.nonNull(request.getPathInfo())) {
+			return Arrays.stream(allowedEndPoints()).anyMatch(p -> pathMatcher.match(p, request.getPathInfo()));
+		}
+		return true;
+	}
+
+	/**
+	 * Allowed end points.
+	 *
+	 * @return the string[]
+	 */
+	private String[] allowedEndPoints() {
+		return new String[] { "/assets/**", "/icons/**", "/screenshots/**", "/favicon**", "/**/favicon**", "/css/**",
+				"/js/**", "/**/error**", "/**/webjars/**", "/**/v2/api-docs", "/**/configuration/ui",
+				"/**/configuration/security", "/**/swagger-resources/**", "/**/swagger-ui.html" };
 	}
 	
 	/* (non-Javadoc)
@@ -56,11 +92,15 @@ public abstract class BaseIdRepoFilter implements Filter  {
 		mosipLogger.debug(IdRepoSecurityManager.getUser(), ID_REPO, ID_REPO_FILTER, "Request URL: " + ((HttpServletRequest) request).getRequestURL());
 
 		mosipLogger.debug(IdRepoSecurityManager.getUser(), ID_REPO, ID_REPO_FILTER, "Request received");
-		String responseString = buildResponse((HttpServletRequest) request);
-		if (Objects.isNull(responseString)) {
+		if (shouldNotFilter((HttpServletRequest) request)) {
 			chain.doFilter(request, response);
 		} else {
-			response.getWriter().write(responseString);
+			String responseString = buildResponse((HttpServletRequest) request);
+			if (Objects.isNull(responseString)) {
+				chain.doFilter(request, response);
+			} else {
+				response.getWriter().write(responseString);
+			}
 		}
 		Instant responseTime = Instant.now();
 		mosipLogger.debug(IdRepoSecurityManager.getUser(), ID_REPO, ID_REPO_FILTER, "Response sent at: " + responseTime);
