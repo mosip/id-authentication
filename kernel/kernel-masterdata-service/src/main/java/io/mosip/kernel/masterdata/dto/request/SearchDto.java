781a9365883608d98c8dcfb502a48b144284eba1
abhishek kumar <abhishek.kumar14@mindtree.com>
2019-06-21 12:24:44 +0530

    MOS-26155 added filter,serach,sort dto
    Former-commit-id: b26686fc6bb35ea8c86555a5935d82f85e8b5076
    Former-commit-id: bf59799f01875701880fc9565c7f266c73b8a2a1

---
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/request/SearchDto.java
@@ -0,0 +1,30 @@
+package io.mosip.kernel.masterdata.dto.request;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Dto to hold the search criteria.
+ * 
+ * @author Abhishek Kumar
+ * @since 1.0.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchDto {
+
+	@NotNull
+	private List<SearchFilter> filters;
+
+	@NotNull
+	private List<SearchSort> sort;
+
+	@NotNull
+	private Pagination pagination;
+}
---
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/request/SearchFilter.java
@@ -0,0 +1,38 @@
+package io.mosip.kernel.masterdata.dto.request;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Dtp to hold the search parameters
+ * 
+ * @author Abhishek Kumar
+ * @since 1.0.0
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchFilter {
+
+	@NotNull
+	private String columnName;
+
+	private String type;
+
+	private String value;
+
+	private String fromName;
+
+	private String fromValue;
+
+	private String toName;
+
+	private String toValue;
+
+	private String langCode;
+
+}
