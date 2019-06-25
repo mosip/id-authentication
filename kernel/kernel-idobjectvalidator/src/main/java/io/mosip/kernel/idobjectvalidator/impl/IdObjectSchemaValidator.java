fbe2b7249ba69b7461d4d3d0d5d7b5cc3d5b1b18
Manoj SP <Manoj.SP@mindtree.com>
2019-06-25 12:03:50 +0530

    Revert "MOS-26177 - updated DOB validations against calender and added logging"
    This reverts commit 8d4b092357ac57d277c899eec3a696a059eea577 [formerly 6b56fb68b968f6213926edb04eb991eec16392b1] [formerly b8364770817398a5d21ca071b6eeac7efff14018].
    
    
    Former-commit-id: baf52c315ce9306b110c7255e3cc0fc67fcd9ed8
    Former-commit-id: be280a506a5a9af5d97f0c25a175ef52ba377f2b

--- kernel/kernel-idobjectvalidator/src/main/java/io/mosip/kernel/idobjectvalidator/impl/IdObjectSchemaValidator.java
+++ kernel/kernel-idobjectvalidator/src/main/java/io/mosip/kernel/idobjectvalidator/impl/IdObjectSchemaValidator.java
@@ -41,7 +41,6 @@ import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
 
-import io.mosip.kernel.core.exception.ExceptionUtils;
 import io.mosip.kernel.core.exception.ServiceError;
 import io.mosip.kernel.core.idobjectvalidator.constant.IdObjectValidatorSupportedOperations;
 import io.mosip.kernel.core.idobjectvalidator.exception.IdObjectIOException;
@@ -113,7 +112,6 @@ public class IdObjectSchemaValidator implements IdObjectValidator {
 				schema = JsonLoader.fromURL(new URL(configServerFileStorageURL + schemaName));
 			}
 		} catch (IOException e) {
-			ExceptionUtils.logRootCause(e);
 			throw new IdObjectIOException(SCHEMA_IO_EXCEPTION, e);
 		}
 	}
@@ -172,10 +170,8 @@ public class IdObjectSchemaValidator implements IdObjectValidator {
 			}
 			return report.isSuccess();
 		} catch (IOException e) {
-			ExceptionUtils.logRootCause(e);
 			throw new IdObjectIOException(ID_OBJECT_PARSING_FAILED, e);
 		} catch (ProcessingException e) {
-			ExceptionUtils.logRootCause(e);
 			throw new IdObjectIOException(ID_OBJECT_VALIDATION_FAILED, e);
 		}
 	}
@@ -253,7 +249,6 @@ public class IdObjectSchemaValidator implements IdObjectValidator {
 				// creating a JsonSchema node against which the JSON object will be validated.
 				jsonSchemaNode = JsonLoader.fromURL(new URL(configServerFileStorageURL + schemaName));
 			} catch (IOException e) {
-				ExceptionUtils.logRootCause(e);
 				throw new IdObjectIOException(SCHEMA_IO_EXCEPTION, e);
 			}
 		}
@@ -263,7 +258,6 @@ public class IdObjectSchemaValidator implements IdObjectValidator {
 			try {
 				jsonSchemaNode = JsonLoader.fromResource(PATH_SEPERATOR.getValue() + schemaName);
 			} catch (IOException e) {
-				ExceptionUtils.logRootCause(e);
 				throw new IdObjectIOException(SCHEMA_IO_EXCEPTION.getErrorCode(), SCHEMA_IO_EXCEPTION.getMessage(),
 						e.getCause());
 			}
