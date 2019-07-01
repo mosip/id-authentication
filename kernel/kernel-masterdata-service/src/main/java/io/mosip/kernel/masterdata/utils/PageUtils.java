8c6325cc149ea6daeafedf8e90fc528b6bf763ca
abhishek kumar <abhishek.kumar14@mindtree.com>
2019-07-01 12:10:27 +0530

    MOS-26668 added pageUtil
    Former-commit-id: 7ca9c774381399fe2a7477794c826e45088e7597
    Former-commit-id: 3000646854c6eacea96020178176148e0e90e892

---
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/utils/PageUtils.java
@@ -0,0 +1,36 @@
+package io.mosip.kernel.masterdata.utils;
+
+import org.springframework.data.domain.Page;
+
+import io.mosip.kernel.masterdata.dto.response.PageResponseDto;
+
+/**
+ * Utility class to calculate the page details
+ * 
+ * @author Abhishek Kumar
+ *
+ */
+public class PageUtils {
+
+	/**
+	 * Method to create page metadat
+	 * 
+	 * @param page
+	 *            request to be
+	 * @return {@link PageResponseDto}
+	 */
+	public static <T> PageResponseDto<T> page(Page<T> page) {
+		PageResponseDto<T> pageResponse = null;
+		if (page != null) {
+			long totalItem = page.getTotalElements();
+			int pageNumber = page.getNumber();
+			int pageSize = page.getSize();
+			int start = (pageNumber * pageSize) - (pageSize - 1);
+			pageResponse = new PageResponseDto<>();
+			pageResponse.setFromRecord(start);
+			pageResponse.setToRecord(Math.min(start + pageSize - 1l, totalItem));
+			pageResponse.setTotalRecord(totalItem);
+		}
+		return pageResponse;
+	}
+}
