26435184463be985345fa405f145064b6ecb4e89
Dinesh Karuppiah <dineshkaruppiah.thiagarajan@mindtree.com>
2019-07-04 18:12:16 +0530

    MOS-24680 - Test cases for InternalAuthTxnController
    Former-commit-id: 5ef44d250d54ad717a8c55774031665b5e1bedb7
    Former-commit-id: cf9da679a0722410226a5ba749af9be193119ae1

---
+++ authentication/authentication-internal-service/src/test/java/io/mosip/authentication/internal/service/controller/InternalAuthTransactionTest.java
@@ -0,0 +1,105 @@
+package io.mosip.authentication.internal.service.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.context.WebApplicationContext;
+
+import io.mosip.authentication.common.service.entity.AutnTxn;
+import io.mosip.authentication.common.service.impl.IdServiceImpl;
+import io.mosip.authentication.common.service.repository.AutnTxnRepository;
+import io.mosip.authentication.core.constant.IdAuthenticationErrorConstants;
+import io.mosip.authentication.core.exception.IDDataValidationException;
+import io.mosip.authentication.core.exception.IdAuthenticationAppException;
+import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
+import io.mosip.authentication.core.indauth.dto.IdType;
+import io.mosip.authentication.internal.service.validator.AuthTxnValidator;
+import io.mosip.kernel.core.exception.BaseCheckedException;
+import io.mosip.kernel.idvalidator.uin.impl.UinValidatorImpl;
+import io.mosip.kernel.idvalidator.vid.impl.VidValidatorImpl;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+@ContextConfiguration(classes = { TestContext.class, WebApplicationContext.class })
+public class InternalAuthTransactionTest {
+
+	@InjectMocks
+	InternalAuthTxnController internalAuthTxnController;
+
+	@Mock
+	private AutnTxnRepository authtxnRepo;
+
+	@InjectMocks
+	private AuthTxnValidator authTxnValidator;
+
+	@Autowired
+	private Environment environment;
+
+	@Mock
+	WebDataBinder binder;
+
+	@Mock
+	private UinValidatorImpl uinValidator;
+
+	@Mock
+	private VidValidatorImpl vidValidator;
+
+	@Mock
+	private IdServiceImpl idService;
+
+	@Before
+	public void before() {
+		ReflectionTestUtils.invokeMethod(internalAuthTxnController, "initBinder", binder);
+		ReflectionTestUtils.setField(internalAuthTxnController, "authTxnValidator", authTxnValidator);
+		ReflectionTestUtils.setField(authTxnValidator, "env", environment);
+	}
+
+	@Test
+	public void TestgetTxnDetails() throws IdAuthenticationAppException, IdAuthenticationBusinessException {
+		Mockito.when(uinValidator.validateId(Mockito.anyString())).thenReturn(true);
+		Map<String, Object> value = new HashMap<>();
+		value.put("uin", "9172985031");
+		Mockito.when(idService.processIdType(Mockito.anyString(), Mockito.anyString(), Mockito.anyBoolean()))
+				.thenReturn(value);
+		List<AutnTxn> valueList = new ArrayList<>();
+		Mockito.when(authtxnRepo.findByPagableUinorVid(Mockito.anyString(), Mockito.any())).thenReturn(valueList);
+		internalAuthTxnController.getAuthTxnDetails(IdType.UIN.getType(), "9172985031", 1, 10);
+	}
+
+	@Test(expected = IdAuthenticationAppException.class)
+	public void TestIdAppException() throws IDDataValidationException, IdAuthenticationAppException {
+		Mockito.when(uinValidator.validateId(Mockito.anyString())).thenReturn(false);
+		internalAuthTxnController.getAuthTxnDetails(IdType.UIN.getType(), "", 1, 10);
+	}
+
+	@Test(expected = IdAuthenticationBusinessException.class)
+	public void TestBusinessException() throws IdAuthenticationAppException, IdAuthenticationBusinessException {
+		Mockito.when(idService.processIdType(Mockito.anyString(), Mockito.anyString(), Mockito.anyBoolean()))
+				.thenThrow(new IdAuthenticationBusinessException(
+						IdAuthenticationErrorConstants.ID_NOT_AVAILABLE.getErrorCode(),
+						IdAuthenticationErrorConstants.ID_NOT_AVAILABLE.getErrorMessage()));
+		try {
+			internalAuthTxnController.getAuthTxnDetails(IdType.UIN.getType(), "9172985031", 1, 10);
+		} catch (IdAuthenticationAppException e) {
+			throw new IdAuthenticationBusinessException(IdAuthenticationErrorConstants.ID_NOT_AVAILABLE, e);
+		}
+
+	}
+
+}
