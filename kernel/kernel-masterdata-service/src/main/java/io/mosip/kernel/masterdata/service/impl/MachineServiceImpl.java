6bdf9d0b4e967f1fb15f14254cc148430ef58827
Mosip-Ritesh <Ritesh.Sinha@mindtree.com>
2019-07-04 18:46:23 +0530

    MOS-26720 Machine filter api service method updated
    Former-commit-id: 6832856ec1d68f8fb6db601b312bdf33f78706b5
    Former-commit-id: 936b5bdc5a2fc6ea1bea604a54ea2b8a34d4cbe1

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/MachineServiceImpl.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/MachineServiceImpl.java
@@ -505,15 +505,12 @@ public class MachineServiceImpl implements MachineService {
 			for (FilterDto filterDto : filterValueDto.getFilters()) {
 				masterDataFilterHelper.filterValues(Machine.class, filterDto.getColumnName(), filterDto.getType(),
 						filterValueDto.getLanguageCode()).forEach(filterValue -> {
-							if (filterValue == null) {
-								throw new DataNotFoundException(
-										MachineErrorCode.NO_DATA_FOR_FILTER_VALUES.getErrorCode(),
-										MachineErrorCode.NO_DATA_FOR_FILTER_VALUES.getErrorMessage());
+							if (filterValue != null) {
+								ColumnValue columnValue = new ColumnValue();
+								columnValue.setFieldID(filterDto.getColumnName());
+								columnValue.setFieldValue(filterValue.toString());
+								columnValueList.add(columnValue);
 							}
-							ColumnValue columnValue = new ColumnValue();
-							columnValue.setFieldID(filterDto.getColumnName());
-							columnValue.setFieldValue(filterValue.toString());
-							columnValueList.add(columnValue);
 						});
 			}
 			filterResponseDto.setFilters(columnValueList);
