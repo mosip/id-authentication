73cade0bf386a5bddd89bfa5ff9a9bc82fe002ee
Mosip-Ritesh <Ritesh.Sinha@mindtree.com>
2019-07-05 14:52:31 +0530

    MOS-26720 Machine search api error code added
    Former-commit-id: aecd0b8968f8d0c7bb5e06a82bdc5c8a50b6e38f
    Former-commit-id: 29b118729f59a3dd2e7e703c3f66738082d7c681

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/MachineErrorCode.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/MachineErrorCode.java
@@ -14,15 +14,13 @@ public enum MachineErrorCode {
 	MACHINE_UPDATE_EXCEPTION("KER-MSD-087", "Error occurred while updating Machine details"),
 	MACHINE_DELETE_EXCEPTION("KER-MSD-088", "Error occurred while deleting Machine details"),
 	DEPENDENCY_EXCEPTION("KER-MSD-148", "Cannot delete as dependency found"),
-	REGISTRATION_CENTER_MACHINE_FETCH_EXCEPTION("KER-MSD-325",
+	REGISTRATION_CENTER_MACHINE_FETCH_EXCEPTION("KER-MSD-331",
 			"Error occurred while fetching a Machine details mapped with the given Registration Center"),
-	MACHINE_REGISTRATION_CENTER_NOT_FOUND_EXCEPTION("KER-MSD-XXX", "Registration Center and Machine Not Found"),
 	MAPPED_MACHINE_ID_NOT_FOUND_EXCEPTION("KER-MSD-326","No assigned Machines found"),
 	MACHINE_ID_ALREADY_MAPPED_EXCEPTION("KER-MSD-327","No unassigned Machines found"),
 	INVALID_MACHINE_FILTER_VALUE_EXCEPTION("KER-MSD-328","Invalid Filter value for Mapstatus column"),
 	MACHINE_ID_NOT_FOUND_FOR_NAME_EXCEPTION("KER-MSD-329","No Machine exist for Machine Type name:%s"),
-	NO_DATA_FOR_FILTER_VALUES("KER-MSD-330","No Data Found for the given filter column"),
-	MACHINE_SPECIFICATION_ID_NOT_FOUND_FOR_NAME_EXCEPTION("KER-MSD-331","No Machine Specification found for the Machine Type name:%s");
+	MACHINE_SPECIFICATION_ID_NOT_FOUND_FOR_NAME_EXCEPTION("KER-MSD-330","No Machine Specification found for the Machine Type name:%s");
 
 	private final String errorCode;
 	private final String errorMessage;
