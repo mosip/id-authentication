46c445a53995e0511e7a47ec76b9f411449bd2c2
RanjithaSiddegowda <43336947+RanjithaSiddegowda@users.noreply.github.com>
2019-07-23 17:47:29 +0530

    MOS-27735 Renamed the Application class https://mosipid.atlassian.net/browse/MOS-27735
    Former-commit-id: c9322fb6ff86097174fbe12d6e73accadbbc94c1
    Former-commit-id: 52cec6b8d34531844a37bb40ead639805ffd789a

--- registration-processor/init/registration-processor-request-handler-service/src/main/java/io/mosip/registration/processor/request/handler/RequestHandlerServiceApp.java
+++ registration-processor/init/registration-processor-request-handler-service/src/main/java/io/mosip/registration/processor/request/handler/RequestHandlerServiceApp.java
@@ -11,7 +11,7 @@ import io.mosip.registration.processor.rest.client.config.RestConfigBean;
 import io.mosip.registration.processor.status.config.RegistrationStatusBeanConfig;
 
 /**
- * The Class PacketGeneratorServiceApp.
+ * The Class RequestHandlerServiceApp.
  * 
  * @author Sowmya
  */
@@ -20,7 +20,7 @@ import io.mosip.registration.processor.status.config.RegistrationStatusBeanConfi
 		"io.mosip.registration.processor.request.handler.*,io.mosip.registration.processor.packet*,io.mosip.registration.processor.rest.client.*,io.mosip.registration.processor.core.*","io.mosip.kernel.auth.*"}, excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, value = {
 				RestConfigBean.class,PacketStorageBeanConfig.class,RegistrationStatusBeanConfig.class }))
 
-public class PacketGeneratorServiceApp {
+public class RequestHandlerServiceApp {
 
 	/**
 	 * The main method.
@@ -29,6 +29,6 @@ public class PacketGeneratorServiceApp {
 	 *            the arguments
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(PacketGeneratorServiceApp.class, args);
+		SpringApplication.run(RequestHandlerServiceApp.class, args);
 	}
 }
