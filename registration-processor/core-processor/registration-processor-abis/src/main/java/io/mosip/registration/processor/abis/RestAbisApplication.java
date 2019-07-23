843cf318623c6cc7d003c8df16ca165ccc4b65ad
M1048219 <girish.yarru@mindtree.com>
2019-07-23 11:36:17 +0530

    MOS-27380: ActiveMq connection fixes https://mosipid.atlassian.net/browse/MOS-27380
    Former-commit-id: 653930b812cbc5fc27d7dc415cc22297d279d0e3
    Former-commit-id: 3b4b2eda51cba7c21363899ca84ca3ae45ba5f89

--- registration-processor/core-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/RestAbisApplication.java
+++ registration-processor/core-processor/registration-processor-abis/src/main/java/io/mosip/registration/processor/abis/RestAbisApplication.java
@@ -1,20 +1,33 @@
 package io.mosip.registration.processor.abis;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
+import io.mosip.registration.processor.abis.config.RegistrationAbisConfig;
 import io.mosip.registration.processor.abis.messagequeue.AbisMessageQueueImpl;
 import io.mosip.registration.processor.core.exception.RegistrationProcessorCheckedException;
+import io.mosip.registration.processor.packet.manager.config.PacketManagerConfig;
+import io.mosip.registration.processor.packet.storage.config.PacketStorageBeanConfig;
+import io.mosip.registration.processor.rest.client.config.RestConfigBean;
+import io.mosip.registration.processor.status.config.RegistrationStatusBeanConfig;
 
-public class RestAbisApplication {
-
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan(basePackages = { "io.mosip.registration.processor.*",
+		"io.mosip.kernel.auth.*" }, excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, value = {
+				PacketManagerConfig.class, RestConfigBean.class, PacketManagerConfig.class,
+				RegistrationStatusBeanConfig.class, PacketStorageBeanConfig.class, RegistrationAbisConfig.class }))
+public class RestAbisApplication{
 	public static void main(String[] args) throws RegistrationProcessorCheckedException {
-		AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
-		configApplicationContext.scan("io.mosip.registration.processor.abis.config",
-				"io.mosip.registration.processor.stages.config", "io.mosip.registration.processor.status.config",
-				"io.mosip.registration.processor.packet.storage.config", "io.mosip.registration.processor.core.config",
-				"io.mosip.registration.processor.core.kernel.beans", "io.mosip.kernel.auth.*","io.mosip.registration.processor.packet.manager.config","io.mosip.registration.processor.rest.client.config");
-		configApplicationContext.refresh();
-		AbisMessageQueueImpl abisMessageQueueImpl = configApplicationContext.getBean(AbisMessageQueueImpl.class);
-		abisMessageQueueImpl.runAbisQueue();
+		ConfigurableApplicationContext configurableApplcnConetxt = SpringApplication.run(RestAbisApplication.class, args);
+		configurableApplcnConetxt.getBean(AbisMessageQueueImpl.class).runAbisQueue();
 	}
 }
