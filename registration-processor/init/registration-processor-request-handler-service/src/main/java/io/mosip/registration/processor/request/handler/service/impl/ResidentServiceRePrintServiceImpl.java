7c4f2737dc8bcd4e3228aeb7cebc647dcb3302d9
RanjithaSiddegowda <43336947+RanjithaSiddegowda@users.noreply.github.com>
2019-07-24 12:18:30 +0530

    MOS-27735 Created skeleton for Resident service reprint API https://mosipid.atlassian.net/browse/MOS-27735
    Former-commit-id: d7997f798cbcf669365459297586da17bd58c838
    Former-commit-id: 90e80a29c5155a1dc76316a5d5b2114448d69556

---
+++ registration-processor/init/registration-processor-request-handler-service/src/main/java/io/mosip/registration/processor/request/handler/service/impl/ResidentServiceRePrintServiceImpl.java
@@ -0,0 +1,8 @@
+package io.mosip.registration.processor.request.handler.service.impl;
+
+/**
+ * The Class ResidentServiceRePrintServiceImpl.
+ */
+public class ResidentServiceRePrintServiceImpl {
+
+}
