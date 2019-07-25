fffc9ed4e7ea32e604809d2a902d8e53d32d3365
jpnayak007 <jyotiprakashnayak103@gmail.com>
2019-07-25 15:28:07 +0530

    MOS-27726 : adding authorization code and error handler
    Former-commit-id: cceb1b63a2d97a991532adf0881cfb5f82103551
    Former-commit-id: 4ebab9dba576d0a29feb77dfe11792ae32750937

--- registration-processor/init/registration-processor-registration-status-service/src/main/java/io/mosip/registration/processor/status/api/controller/RegistrationTransactionController.java
+++ registration-processor/init/registration-processor-registration-status-service/src/main/java/io/mosip/registration/processor/status/api/controller/RegistrationTransactionController.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
-import org.springframework.http.ResponseEntity.HeadersBuilder;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.WebUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -32,7 +32,7 @@ import io.mosip.registration.processor.core.util.DigitalSignatureUtility;
 import io.mosip.registration.processor.status.dto.RegistrationTransactionDto;
 import io.mosip.registration.processor.status.dto.TransactionDto;
 import io.mosip.registration.processor.status.exception.RegStatusAppException;
-import io.mosip.registration.processor.status.exception.TransactionTableNotAccessibleException;
+import io.mosip.registration.processor.status.exception.TablenotAccessibleException;
 import io.mosip.registration.processor.status.service.TransactionService;
 import io.mosip.registration.processor.status.sync.response.dto.RegTransactionResponseDTO;
 import io.swagger.annotations.Api;
@@ -42,7 +42,7 @@ import io.swagger.annotations.ApiResponses;
 
 @RefreshScope
 @RestController
-@Api(tags = "Registration Transaction")
+@Api(tags = "Registration Status")
 public class RegistrationTransactionController {
 	
 	@Autowired
@@ -65,61 +65,44 @@ public class RegistrationTransactionController {
 	private static final String DATETIME_PATTERN = "mosip.registration.processor.datetime.pattern";
 	private static final String RESPONSE_SIGNATURE = "Response-Signature";
 	
-	@SuppressWarnings("unchecked")
-	@GetMapping(path = "/rid/{rid}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path = "/rid/{rid}")
 	@ApiOperation(value = "Get the transaction entity/entities")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Transaction Entity/Entities successfully fetched"),
 			@ApiResponse(code = 400, message = "Unable to fetch Transaction Entity/Entities") })
-	public ResponseEntity<RegTransactionResponseDTO> getTransactionsbyRid(@PathVariable String rid,@CookieValue(value = "Authorization") String token)
+	public ResponseEntity<RegTransactionResponseDTO> getTransactionsbyRid(@PathVariable String rid,HttpServletRequest request)
 			throws RegStatusAppException {
-		ResponseEntity<RegTransactionResponseDTO> response=new ResponseEntity<RegTransactionResponseDTO>(null);
 		List<RegistrationTransactionDto> dtoList=new ArrayList<>();
 		List<ErrorDTO> errors=new ArrayList<>();
 		HttpHeaders headers = new HttpHeaders();
+		Cookie token=WebUtils.getCookie( request,"Authorization");
+		tokenValidator.validate("Authorization=" + token.getValue(), "transaction");
 		try {	
-			tokenValidator.validate("Authorization=" + token, "transaction");
+			
 			dtoList = transactionService.getTransactionByRegId(rid);	
 			if(dtoList.isEmpty()) {
-				response= ResponseEntity.status(HttpStatus.NOT_FOUND)
-						.body(buildRegistrationTransactionResponse(dtoList,errors));
-			}else {
-			response= ResponseEntity.status(HttpStatus.OK)
+				ErrorDTO errorDTO=new ErrorDTO();
+				errorDTO.setErrorCode("RPR-RPN-001");
+				errorDTO.setMessage("RID Not Found");
+				errors.add(errorDTO);
+			}
+			if (isEnabled) {		 
+				headers.add(RESPONSE_SIGNATURE,
+						digitalSignatureUtility.getDigitalSignature(buildSignatureRegistrationTransactionResponse(
+								buildRegistrationTransactionResponse(dtoList,errors))));	
+				return ResponseEntity.status(HttpStatus.OK).headers(headers)
 						.body(buildRegistrationTransactionResponse(dtoList,errors));
 			}
-		} catch (TransactionTableNotAccessibleException e) {
-			ErrorDTO errorDTO=new ErrorDTO();
-			errorDTO.setErrorCode(e.getErrorCode());
-			errorDTO.setMessage(e.getMessage());
-			errors.add(errorDTO);
-			response=ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-					buildRegistrationTransactionResponse(dtoList,errors));
-		}
-		catch (InvalidTokenException e) {
-			ErrorDTO errorDTO=new ErrorDTO();
-			errorDTO.setErrorCode(e.getErrorCode());
-			errorDTO.setMessage(e.getMessage());
-			errors.add(errorDTO);
-			response=ResponseEntity.status(HttpStatus.FORBIDDEN).body(
-					buildRegistrationTransactionResponse(dtoList,errors));
-		}
-		catch (AccessDeniedException e) {
-			ErrorDTO errorDTO=new ErrorDTO();
-			errorDTO.setErrorCode(e.getErrorCode());
-			errorDTO.setMessage(e.getMessage());
-			errors.add(errorDTO);
-			response=ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-					buildRegistrationTransactionResponse(dtoList,errors));
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(buildRegistrationTransactionResponse(dtoList,errors));
 		}catch (Exception e) {
-			throw new RegStatusAppException(PlatformErrorMessages.RPR_RGS_UNKNOWN_EXCEPTION, e);
+			if(e instanceof TablenotAccessibleException | e instanceof InvalidTokenException |
+					e instanceof AccessDeniedException) {
+				throw e;
+			}
+			else {
+				throw new RegStatusAppException(PlatformErrorMessages.RPR_RGS_UNKNOWN_EXCEPTION, e);
+			}
 		}
-		if (isEnabled) {
-			 
-			headers.add(RESPONSE_SIGNATURE,
-					digitalSignatureUtility.getDigitalSignature(buildSignatureRegistrationTransactionResponse(
-							buildRegistrationTransactionResponse(dtoList,errors))));
-			((HeadersBuilder<BodyBuilder>) response).headers(headers);
-		}
-		return response;
 	}
 
 	private RegTransactionResponseDTO buildRegistrationTransactionResponse(List<RegistrationTransactionDto> dtoList,
@@ -130,12 +113,6 @@ public class RegistrationTransactionController {
 		}
 		regTransactionResponseDTO.setResponsetime(DateUtils.getUTCCurrentDateTimeString(env.getProperty(DATETIME_PATTERN)));
 		regTransactionResponseDTO.setVersion(env.getProperty(REG_TRANSACTION_APPLICATION_VERSION));
-		if(dtoList.isEmpty() && errors.isEmpty() ) {
-			ErrorDTO errorDTO=new ErrorDTO();
-			errorDTO.setErrorCode("RPR-RPN-001");
-			errorDTO.setMessage("RID Not Found");
-			errors.add(errorDTO);
-		}
 		regTransactionResponseDTO.setErrors(errors);
 		regTransactionResponseDTO.setResponse(dtoList);
 		return regTransactionResponseDTO;
