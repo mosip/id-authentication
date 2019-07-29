bb1c3134f9c69a840433c9f54c53fc2003dc271f
MOSIPRajath <rajath.raghuchandra@mindtree.com>
2019-07-29 14:52:46 +0530

    MOS-27929 : dto layer encryption
    Former-commit-id: 46be37dd85ac436490847f03cc7994a9f7b5a820
    Former-commit-id: bd090e0224e51241876a9d1ea8015bb1895b15ae

---
+++ kernel/kernel-dataaccess-hibernate/src/main/java/io/mosip/kernel/dataaccess/hibernate/config/EncryptionInterceptor.java
@@ -0,0 +1,66 @@
+package io.mosip.kernel.dataaccess.hibernate.config;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
+
+
+
+public class EncryptionInterceptor extends EmptyInterceptor {
+
+
+	private List<String> reqParams= new ArrayList<>();
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.hibernate.EmptyInterceptor#onSave(java.lang.Object,
+	 * java.io.Serializable, java.lang.Object[], java.lang.String[],
+	 * org.hibernate.type.Type[])
+	 */
+	@Override
+	public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+		return doSaveOrFlushAction(entity, state, propertyNames, types);
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.hibernate.EmptyInterceptor#onLoad(java.lang.Object,
+	 * java.io.Serializable, java.lang.Object[], java.lang.String[],
+	 * org.hibernate.type.Type[])
+	 */
+	@Override
+	public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+		return super.onLoad(entity, id, state, propertyNames, types);
+	}
+
+	private boolean doSaveOrFlushAction(Object entity, Object[] state, String[] propertyNames, Type[] types) {
+		try {
+				Field[] fields = entity.getClass().getDeclaredFields();
+				for (Field field : fields) {
+					if (field.isAnnotationPresent(Encrypted.class)) {
+						System.out.println("field name  " + field.getName());
+						reqParams.add(field.getName());
+
+					}
+				}
+				for (int i = 0; i < propertyNames.length; i++) {
+					if (reqParams.contains(propertyNames[i])) {
+						System.out.println("Value "+state[i]);
+					}
+				}
+				return true;
+
+		} catch (Exception e) {
+			// log error
+		}
+		return false;
+	}
+}
