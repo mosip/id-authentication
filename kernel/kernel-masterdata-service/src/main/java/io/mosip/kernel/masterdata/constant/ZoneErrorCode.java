9c94e49929cd96090e8bef643098b2e5253c0126
sagarmahapatra1996 <Sagar.Mahapatra2@mindtree.com>
2019-08-01 19:32:50 +0530

    MOS-27767 - API added to get zonename based on userID and langCode
    Former-commit-id: 5e7be7d91ae2660f600d29ddcd62e801788ef7ba
    Former-commit-id: 26e58189125df637d1b382dd01d54ec5935027c6

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/ZoneErrorCode.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/ZoneErrorCode.java
@@ -9,7 +9,10 @@ public enum ZoneErrorCode {
 
 	ZONE_FETCH_EXCEPTION("KER-MSD-337", "Error occured while fetching zone"),
 	USER_ZONE_UNAVAILABLE("KER-MSD-339","No zone found for the logged-in user %s"),
-	USER_ZONE_FETCH_EXCEPTION("KER-MSD-338","Error Occured while fetching zone of the user");
+	USER_ZONE_FETCH_EXCEPTION("KER-MSD-338","Error Occured while fetching zone of the user"),
+	ZONEUSER_ENTITY_NOT_FOUND("KER-MSD-___","Data Not Found"),
+	ZONE_ENTITY_NOT_FOUND("KER-MSD-___","Data Not Found"),
+	INTERNAL_SERVER_ERROR("KER-MSD-___","Internal Server Error");
 
 	private final String errorCode;
 	private final String errorMessage;
