255a1154051df0a29a3c0ac8000e6b83c45a0d9d
TaleevAalam <taleev.aalam@mindtree.com>
2019-08-02 21:30:52 +0530

    MOS-27743: fixed for Iris Former-commit-id: c4f711847eb498a5031188583ec5ece1fe5369f3 Former-commit-id: 6308d02f153236f6957b4cc02e87ba58b4d918e7

--- registration/registration-services/src/main/java/io/mosip/registration/service/packet/impl/PacketCreationServiceImpl.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/packet/impl/PacketCreationServiceImpl.java
@@ -359,16 +359,14 @@ public class PacketCreationServiceImpl implements PacketCreationService {
 				// Add Iris
 				if (isListNotEmpty(biometricInfoDTO.getIrisDetailsDTO())) {
 					for (IrisDetailsDTO iris : biometricInfoDTO.getIrisDetailsDTO()) {
-						for(IrisDetailsDTO irisDetailsDTO : iris.getIrises()) {
-						BIR bir = buildBIR(irisDetailsDTO.getIrisIso(), CbeffConstant.FORMAT_TYPE_IRIS,
-								(int) Math.round(irisDetailsDTO.getQualityScore()), Arrays.asList(SingleType.IRIS),
-								Arrays.asList(irisDetailsDTO.getIrisType().equalsIgnoreCase("lefteye")
+						BIR bir = buildBIR(iris.getIrisIso(), CbeffConstant.FORMAT_TYPE_IRIS,
+								(int) Math.round(iris.getQualityScore()), Arrays.asList(SingleType.IRIS),
+								Arrays.asList(iris.getIrisType().toLowerCase().contains("left")
 										? SingleAnySubtypeType.LEFT.value()
 										: SingleAnySubtypeType.RIGHT.value()));
 
 						birs.add(bir);
 						birUUIDs.put(personType.concat(iris.getIrisType()).toLowerCase(), bir.getBdbInfo().getIndex());
-						}
 					}
 				}
 
