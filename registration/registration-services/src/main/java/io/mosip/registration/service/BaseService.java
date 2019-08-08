2dd6081baded1d5ffba026402a19a16ad4329187
yaswanths2 <yaswanth.s2@mindtree.com>
2019-08-08 11:47:08 +0530

    MOS-28140 : Added Service Validation in BaseService
    Former-commit-id: a236fcb700829ecc37ae4731c83a7d0455a963d2
    Former-commit-id: 69e76b6b11a7912ce2f9bb99a2a6e8eca6b0742b

--- registration/registration-services/src/main/java/io/mosip/registration/service/BaseService.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/BaseService.java
@@ -200,13 +200,17 @@ public class BaseService {
 	 */
 	public String getStationId(String macAddress) {
 		String stationId = null;
-		try {
-			/* Get Station ID */
-			stationId = userOnboardDAO.getStationID(macAddress);
-		} catch (RegBaseCheckedException baseCheckedException) {
-			LOGGER.error("REGISTRATION_BASE_SERVICE", APPLICATION_NAME, APPLICATION_ID,
-					baseCheckedException.getMessage() + ExceptionUtils.getStackTrace(baseCheckedException));
+		if (macAddress != null) {
+			try {
 
+				/* Get Station ID */
+				stationId = userOnboardDAO.getStationID(macAddress);
+
+			} catch (RegBaseCheckedException baseCheckedException) {
+				LOGGER.error("REGISTRATION_BASE_SERVICE", APPLICATION_NAME, APPLICATION_ID,
+						baseCheckedException.getMessage() + ExceptionUtils.getStackTrace(baseCheckedException));
+
+			}
 		}
 		return stationId;
 	}
@@ -273,16 +277,20 @@ public class BaseService {
 	 */
 	public String getGlobalConfigValueOf(String key) {
 
-		ApplicationContext.getInstance();
-		// Check application map
-		if (ApplicationContext.map().isEmpty() || ApplicationContext.map().get(key) == null) {
+		String val = null;
+		if (key != null) {
+			ApplicationContext.getInstance();
+			// Check application map
+			if (ApplicationContext.map().isEmpty() || ApplicationContext.map().get(key) == null) {
 
-			// Load Global params if application map is empty
-			ApplicationContext.setApplicationMap(globalParamService.getGlobalParams());
+				// Load Global params if application map is empty
+				ApplicationContext.setApplicationMap(globalParamService.getGlobalParams());
+			}
+
+			// Get Value of global param
+			val = (String) ApplicationContext.map().get(key);
 		}
-
-		// Get Value of global param
-		return (String) ApplicationContext.map().get(key);
+		return val;
 	}
 
 	/**
