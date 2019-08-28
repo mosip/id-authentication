1eec011a4a50e4cf6b6fe86cd6b0166ff7044ad5
Manoj SP <Manoj.SP@mindtree.com>
2019-08-28 15:22:44 +0530

    MOS-28644 - updated test cases
    Former-commit-id: d14bb50f20cc403076536f0b6ad2e4cb7a10c121
    Former-commit-id: cbf11d12445a925fcc430d7ce35cd992d03405bd

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/builder/AuthTransactionBuilder.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/builder/AuthTransactionBuilder.java
@@ -20,7 +20,6 @@ import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
 import io.mosip.authentication.core.indauth.dto.AuthRequestDTO;
 import io.mosip.authentication.core.logger.IdaLogger;
 import io.mosip.authentication.core.otp.dto.OtpRequestDTO;
-import io.mosip.idrepository.core.constant.IdRepoConstants;
 import io.mosip.kernel.core.exception.ParseException;
 import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.core.util.DateUtils;
@@ -34,22 +33,22 @@ import io.mosip.kernel.core.util.UUIDUtils;
  *
  */
 public class AuthTransactionBuilder {
-	
 
 	/**
 	 * Instantiates a new auth transaction builder.
 	 */
 	private AuthTransactionBuilder() {
 	}
-	
+
 	/**
-	 *  Get new instance of {@code AuthTransactionBuilder}
+	 * Get new instance of {@code AuthTransactionBuilder}
+	 * 
 	 * @return new instance of AuthTransactionBuilder
 	 */
 	public static AuthTransactionBuilder newInstance() {
 		return new AuthTransactionBuilder();
 	}
-	
+
 	/** The Constant SUCCESS_STATUS. */
 	private static final String SUCCESS_STATUS = "Y";
 
@@ -58,7 +57,7 @@ public class AuthTransactionBuilder {
 
 	/** The mosipLogger. */
 	private Logger mosipLogger = IdaLogger.getLogger(AuditHelper.class);
-	
+
 	/** The auth request DTO. */
 	private AuthRequestDTO authRequestDTO;
 
@@ -76,24 +75,24 @@ public class AuthTransactionBuilder {
 
 	/** The otp request DTO. */
 	private OtpRequestDTO otpRequestDTO;
-	
-	
-	
+
 	/**
 	 * Set the AuthRequestDTO.
 	 *
-	 * @param authRequestDTO the auth request DTO
+	 * @param authRequestDTO
+	 *            the auth request DTO
 	 * @return {@code AuthTransactionBuilder} instance
 	 */
 	public AuthTransactionBuilder withAuthRequest(AuthRequestDTO authRequestDTO) {
 		this.authRequestDTO = authRequestDTO;
 		return this;
 	}
-	
+
 	/**
 	 * With otp request.
 	 *
-	 * @param otpRequestDTO the otp request DTO
+	 * @param otpRequestDTO
+	 *            the otp request DTO
 	 * @return the auth transaction builder
 	 */
 	public AuthTransactionBuilder withOtpRequest(OtpRequestDTO otpRequestDTO) {
@@ -104,8 +103,9 @@ public class AuthTransactionBuilder {
 	/**
 	 * Set the UIN.
 	 *
-	 * @param uin the uin
-	 * @return  {@code AuthTransactionBuilder} instance
+	 * @param uin
+	 *            the uin
+	 * @return {@code AuthTransactionBuilder} instance
 	 */
 	public AuthTransactionBuilder withUin(String uin) {
 		this.uin = uin;
@@ -115,7 +115,8 @@ public class AuthTransactionBuilder {
 	/**
 	 * Set the RequestType.
 	 *
-	 * @param requestType the request type
+	 * @param requestType
+	 *            the request type
 	 * @return {@code AuthTransactionBuilder} instance
 	 */
 	public AuthTransactionBuilder withRequestType(RequestType requestType) {
@@ -126,7 +127,8 @@ public class AuthTransactionBuilder {
 	/**
 	 * Set the static token.
 	 *
-	 * @param staticTokenId the static token id
+	 * @param staticTokenId
+	 *            the static token id
 	 * @return {@code AuthTransactionBuilder} instance
 	 */
 	public AuthTransactionBuilder withStaticToken(String staticTokenId) {
@@ -137,43 +139,47 @@ public class AuthTransactionBuilder {
 	/**
 	 * With status.
 	 *
-	 * @param isStatus the is status
+	 * @param isStatus
+	 *            the is status
 	 * @return the auth transaction builder
 	 */
 	public AuthTransactionBuilder withStatus(boolean isStatus) {
 		this.isStatus = isStatus;
 		return this;
 	}
-	
+
 	/**
 	 * Build {@code AutnTxn}.
 	 *
-	 * @param env the env
+	 * @param env
+	 *            the env
 	 * @return the instance of {@code AutnTxn}
-	 * @throws IdAuthenticationBusinessException the id authentication business exception
+	 * @throws IdAuthenticationBusinessException
+	 *             the id authentication business exception
 	 */
-	public AutnTxn build(Environment env,UinEncryptSaltRepo uinEncryptSaltRepo,UinHashSaltRepo uinHashSaltRepo,IdAuthTransactionManager transactionManager) throws IdAuthenticationBusinessException {
+	public AutnTxn build(Environment env, UinEncryptSaltRepo uinEncryptSaltRepo, UinHashSaltRepo uinHashSaltRepo,
+			IdAuthTransactionManager transactionManager) throws IdAuthenticationBusinessException {
 		try {
 			String idvId;
 			String reqTime;
 			String idvIdType;
 			String txnID;
-			if(authRequestDTO != null) {
+			if (authRequestDTO != null) {
 				idvId = authRequestDTO.getIndividualId();
 				reqTime = authRequestDTO.getRequestTime();
 				idvIdType = authRequestDTO.getIndividualIdType();
 				txnID = authRequestDTO.getTransactionID();
-			} else if(otpRequestDTO != null) {
+			} else if (otpRequestDTO != null) {
 				idvId = otpRequestDTO.getIndividualId();
 				reqTime = otpRequestDTO.getRequestTime();
-				idvIdType =otpRequestDTO.getIndividualIdType();
+				idvIdType = otpRequestDTO.getIndividualIdType();
 				txnID = otpRequestDTO.getTransactionID();
 			} else {
-				mosipLogger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getName(), "Missing arguments to build for AutnTxn",
-						this.toString());
+				mosipLogger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getName(),
+						"Missing arguments to build for AutnTxn", this.toString());
 				throw new IdAuthenticationBusinessException(IdAuthenticationErrorConstants.DATA_VALIDATION_FAILED);
 			}
-			
+
 			if (uin != null && requestType != null) {
 				String status = isStatus ? SUCCESS_STATUS : FAILED;
 				String comment = isStatus ? requestType.getMessage() + " Success"
@@ -196,26 +202,27 @@ public class AuthTransactionBuilder {
 				autnTxn.setStatusComment(comment);
 				// Setting primary code only
 				autnTxn.setLangCode(env.getProperty(IdAuthConfigKeyConstants.MOSIP_PRIMARY_LANGUAGE));
-				int SaltModuloConstant=Integer.valueOf(env.getProperty(IdAuthConfigKeyConstants.UIN_SALT_MODULO));
-				int uinModulo= (int)(Long.parseLong(uin)%SaltModuloConstant);
-				String hashSaltValue=uinHashSaltRepo.retrieveSaltById(uinModulo);
+				int saltModuloConstant = env.getProperty(IdAuthConfigKeyConstants.UIN_SALT_MODULO, Integer.class);
+				int uinModulo = (int) (Long.parseLong(uin) % saltModuloConstant);
+				String hashSaltValue = uinHashSaltRepo.retrieveSaltById(uinModulo);
 				autnTxn.setUinHash(HMACUtils.digestAsPlainTextWithSalt(uin.getBytes(), hashSaltValue.getBytes()));
-				String encryptSaltValue=uinEncryptSaltRepo.retrieveSaltById(uinModulo);
-				autnTxn.setUin(uinModulo+IdAuthCommonConstants.UIN_MODULO_SPLITTER+uin +IdAuthCommonConstants.UIN_MODULO_SPLITTER + encryptSaltValue);
-				Optional<String> clientId=Optional.of(transactionManager.getUser());
-				if(clientId.isPresent()) {
+				String encryptSaltValue = uinEncryptSaltRepo.retrieveSaltById(uinModulo);
+				autnTxn.setUin(uinModulo + IdAuthCommonConstants.UIN_MODULO_SPLITTER + uin
+						+ IdAuthCommonConstants.UIN_MODULO_SPLITTER + encryptSaltValue);
+				Optional<String> clientId = Optional.of(transactionManager.getUser());
+				if (clientId.isPresent()) {
 					autnTxn.setEntityName(clientId.get());
-					if(clientId.get().equalsIgnoreCase(env.getProperty(IdAuthConfigKeyConstants.MOSIP_IDA_AUTH_CLIENTID))) {
-					     autnTxn.setEntitytype(TransactionType.PARTNER.getType());
-					}
-					else {
+					if (clientId.get()
+							.equalsIgnoreCase(env.getProperty(IdAuthConfigKeyConstants.MOSIP_IDA_AUTH_CLIENTID))) {
+						autnTxn.setEntitytype(TransactionType.PARTNER.getType());
+					} else {
 						autnTxn.setEntitytype(TransactionType.INTERNAL.getType());
 					}
 				}
 				return autnTxn;
 			} else {
-				mosipLogger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getName(), "Missing arguments to build for AutnTxn",
-						this.toString());
+				mosipLogger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getName(),
+						"Missing arguments to build for AutnTxn", this.toString());
 				throw new IdAuthenticationBusinessException(IdAuthenticationErrorConstants.DATA_VALIDATION_FAILED);
 			}
 
@@ -229,8 +236,10 @@ public class AuthTransactionBuilder {
 	/**
 	 * Creates UUID.
 	 *
-	 * @param uin the uin
-	 * @param env the env
+	 * @param uin
+	 *            the uin
+	 * @param env
+	 *            the env
 	 * @return the string
 	 */
 	private String createId(String uin, PropertyResolver env) {
@@ -240,7 +249,9 @@ public class AuthTransactionBuilder {
 		return UUIDUtils.getUUID(UUIDUtils.NAMESPACE_OID, uinAndDate).toString();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
@@ -248,9 +259,5 @@ public class AuthTransactionBuilder {
 		return "AuthTransactionBuilder [authRequestDTO=" + authRequestDTO + ", uin=" + uin + ", requestType="
 				+ requestType + ", staticTokenId=" + staticTokenId + ", isStatus=" + isStatus + "]";
 	}
-	
-	
-
-	
 
 }
