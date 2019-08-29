65bb10e80d64b8d31dc76afed9e9d1516b3bf144
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-08-29 11:46:25 +0530

    MOS-28716 specs written correctly
    Former-commit-id: 354340cf064cdd6ece9d87cb06b799324cd3374c
    Former-commit-id: 40ac2af05f3c1f1cbbb0cadb91da06a911558e32

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/pdfgenerator/spi/PDFGenerator.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/pdfgenerator/spi/PDFGenerator.java
@@ -19,9 +19,10 @@ import java.io.OutputStream;
 import java.net.URL;
 import java.util.List;
 
-/** This interface is has specifications for PDF generation from different types.
+/**
+ * This interface is has specifications for PDF generation from different types.
  * 
- * The user of this interface will have 
+ * The user of this interface will have basic functionalities related to PDF. 
  * 
  * @author Urvil Joshi
  *
@@ -31,7 +32,7 @@ public interface PDFGenerator {
 	 * This method will convert InputStream to OutputStream
 	 * 
 	 * @param is the processedTemplate in the form of a {@link InputStream}
-	 * @throws IOException throws IOException
+	 * @throws IOException Signals that an I/O exception has occurred.
 	 * @return outpustStream
 	 */
 	public OutputStream generate(InputStream is) throws IOException;
@@ -41,7 +42,7 @@ public interface PDFGenerator {
 	 * 
 	 * @param template the processedTemplate in the form of a {@link String}
 	 * @return OutputStream
-	 * @throws IOException throws IOException
+	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
 	public OutputStream generate(String template) throws IOException;
 
@@ -52,7 +53,7 @@ public interface PDFGenerator {
 	 * @param templatePath   the processedTemplate in the form of a {@link String}
 	 * @param outputFilePath Output File Path
 	 * @param outputFileName Output File Name
-	 * @throws IOException throws IOException
+	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
 	public void generate(String templatePath, String outputFilePath, String outputFileName) throws IOException;
 
@@ -61,7 +62,7 @@ public interface PDFGenerator {
 	 * 
 	 * @param is          the processedTemplate in the form of a {@link InputStream}
 	 * @param resourceLoc resourceLoction {@link String}
-	 * @throws IOException throws IOException
+	 * @throws IOException Signals that an I/O exception has occurred.
 	 * @return outpustStream
 	 */
 	public OutputStream generate(InputStream is, String resourceLoc) throws IOException;
@@ -71,17 +72,30 @@ public interface PDFGenerator {
 	 * 
 	 * @param bufferedImages the input image to convert as PDF
 	 * @return byte array
-	 * @throws IOException throws IOException
+	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
 	public byte[] asPDF(List<BufferedImage> bufferedImages) throws IOException;
 
 	/**
-	 * This method will merge all the pdf files
+	 * This method will merge all the PDF files
 	 * 
-	 * @param pdfLists the url list of pdf files
-	 * @return the byte array of merged file
-	 * @throws IOException throws IOException
+	 * @param pdfLists the URL list of PDF files
+	 * @return the byte array comprising merged file
+	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
 	public byte[] mergePDF(List<URL> pdfLists) throws IOException;
+
+	/**
+	 * Converts data obtained from an {@link InputStream} to a password protected
+	 * PDF written to an {@link OutputStream}.
+	 * 
+	 * If password is null or empty, PDF will not be encrypted.
+	 * 
+	 * @param dataInputStream the processedTemplate in the form of a
+	 *                        {@link InputStream}
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @return Password Protected PDF Output Stream (PDF stream)
+	 */
+	public OutputStream generate(InputStream dataInputStream, byte[] password) throws IOException;
 
 }
