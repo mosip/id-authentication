04dd6318b55cc47d6f5dc2ea6886acccf3250e92
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2019-09-04 17:11:25 +0530

    MOS-28713 Added javadoc comment.
    Former-commit-id: 0e0bca50bc76324bc3dcf43157cc7eb3c82c57c5
    Former-commit-id: 38725b211170b5e80264b6d04b88d94109bfaccd

--- authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/validator/InternalOTPRequestValidator.java
+++ authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/validator/InternalOTPRequestValidator.java
@@ -5,8 +5,20 @@ import org.springframework.stereotype.Component;
 import io.mosip.authentication.common.service.validator.OTPRequestValidator;
 import io.mosip.authentication.core.constant.IdAuthConfigKeyConstants;
 
+/**
+ * Validator for internal OTP request
+ * 
+ * @author Loganathan Sekar
+ *
+ */
 @Component
 public class InternalOTPRequestValidator extends OTPRequestValidator {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.mosip.authentication.common.service.validator.IdAuthValidator#
+	 * getAllowedIdTypesConfigKey()
+	 */
 	@Override
 	protected String getAllowedIdTypesConfigKey() {
 		return IdAuthConfigKeyConstants.INTERNAL_AUTH_ALLOWED_IDTYPE;
