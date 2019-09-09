6d295d6d58d55faa05c04396703b065daadf7a32
MeghaUttamTanga <megha.tanga@mindtree.com>
2019-09-09 14:44:21 +0530

    MOS-29066 error code updated
    Former-commit-id: b6e691e91f2178768ad152cf2f1c5b790691519e
    Former-commit-id: 19f1f16ccad5b5ad5cf38d340cc6ea209a4362b4

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/RegistrationCenterUserErrorCode.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/RegistrationCenterUserErrorCode.java
@@ -10,7 +10,7 @@ public enum RegistrationCenterUserErrorCode {
 	REGISTRATION_CENTER_USER_ALREADY_UNMAPPED_EXCEPTION("KER-MSD-xxx", "User is already unmapped from the received Registration Center"),
 	USER_AND_REG_CENTER_MAPPING_NOT_FOUND_EXCEPTION("KER-MSD-xxx","User Id %s - Registration Center Id %s Mapping does not exist"),
 	INVALIDE_ZONE("KER-MSD-xxx", "User or Registration center does not belong to user"),
-	REGISTRATION_CENTER_USER_FETCH_EXCEPTION("KER-MSD-xxX","Error occurred while fetching Center Device details");
+	REGISTRATION_CENTER_USER_FETCH_EXCEPTION("KER-MSD-xxX","Error occurred while fetching Center User details");
 	
 	private final String errorCode;
 	private final String errorMessage;
