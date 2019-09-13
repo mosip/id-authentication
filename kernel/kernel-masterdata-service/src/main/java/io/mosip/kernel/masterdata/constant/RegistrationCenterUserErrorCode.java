5b4d7c66b290f664c4ed6045fd002862b1979275
MeghaUttamTanga <megha.tanga@mindtree.com>
2019-09-13 14:52:25 +0530

    MOS-28983 error code added device-regCenter un-mapping
    Former-commit-id: ad9423916780723c5a7b9dab7762c93c63ef6193
    Former-commit-id: 428e158cbf860fed9bf9ee067a7898a9c0793345

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/RegistrationCenterUserErrorCode.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/constant/RegistrationCenterUserErrorCode.java
@@ -11,7 +11,7 @@ public enum RegistrationCenterUserErrorCode {
 	USER_AND_REG_CENTER_MAPPING_NOT_FOUND_EXCEPTION("KER-MSD-423","User Id %s - Center Id %s mapping does not exist"),
 	REGISTRATION_CENTER_USER_MAPPING_EXCEPTION("KER-MSD-424","Error occurred while mapping User to Registration Center "),
 	REGISTRATION_CENTER_USER_UNMAPPING_EXCEPTION("KER-MSD-425","Error occurred while unmapping User to Registration Center "),
-	REGISTRATION_CENTER_USER_ALREADY_MAPPED_EXCEPTION("KER-MSD-426", " Registration Center-User mapping already exist"),
+	REGISTRATION_CENTER_USER_ALREADY_MAPPED_EXCEPTION("KER-MSD-426", "Registration Center-User mapping already exist"),
 	REGISTRATION_CENTER_USER_INSERT_EXCEPTION("KER-MSD-424","Error occurred while mapping User to Registration Center"),
 	REGISTRATION_CENTER_NOT_FOUND ("KER-MSD-428", "Registration Center is Decommission state or deoes not exist"),
 	INVALIDE_USER_ZONE("KER-MSD-429", "Admin not authorized to map/un-map this User"),
