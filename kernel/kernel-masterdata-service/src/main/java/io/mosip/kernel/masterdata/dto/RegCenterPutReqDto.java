f4363864bf147dc0141c6f70bf09fd77bac6a54d
Ramadurai Pandian <ramadurai.pandian@mindtree.com>
2019-10-02 21:31:20 +0530

    MOS-000 changes for automation test case
    Former-commit-id: 899f2e6e7ea9e8bf82feab63f0ad4c0594f893cb
    Former-commit-id: 6db8ec6a2d1f637efb9b8714bf559677f42bd66a

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/RegCenterPutReqDto.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/RegCenterPutReqDto.java
@@ -27,11 +27,11 @@ public class RegCenterPutReqDto
 	private String id;
 
 	@NotBlank
-	@Size(min = 1, max = 128)
+	@Size(min = 0, max = 128)
 	private String name;
 
 	@NotBlank
-	@Size(min = 1, max = 256)
+	@Size(min = 0, max = 256)
 	private String addressLine1;
 
 	@Size(min = 0, max = 256)
@@ -52,30 +52,30 @@ public class RegCenterPutReqDto
 	private Boolean isActive;
 	
 	@NotBlank
-	@Size(min = 1, max = 36)
+	@Size(min = 0, max = 36)
 	private String centerTypeCode;
 
 	@NotBlank
-	@Size(min = 1, max = 32)
+	@Size(min = 0, max = 32)
 	private String latitude;
 
 	@NotBlank
-	@Size(min = 1, max = 32)
+	@Size(min = 0, max = 32)
 	private String longitude;
 
 	@NotBlank
-	@Size(min = 1, max = 36)
+	@Size(min = 0, max = 36)
 	private String locationCode;
 
 	@NotBlank
-	@Size(min = 1, max = 36)
+	@Size(min = 0, max = 36)
 	private String holidayLocationCode;
 
 	@Size(min = 0, max = 16)
 	private String contactPhone;
 
 	@NotBlank
-	@Size(min = 1, max = 32)
+	@Size(min = 0, max = 32)
 	private String workingHours;
 
 	@NotNull
@@ -100,5 +100,7 @@ public class RegCenterPutReqDto
 	private String timeZone;
 
 	@NotNull
-	@Size(min = 1, max = 36)
-	private String zoneCode;}
+	@Size(min = 0, max = 36)
+	private String zoneCode;
+	
+}
