925b75ee6894fef9cf95351798f7ba26d5a7febd
Mukul Puspam <mukul.puspam2@mindtree.com>
2019-10-11 12:19:01 +0530

    token issue fixed for idObjectValidator
    Former-commit-id: 6f2e78c81f44d2ce6d47258cc486d735b124b5de
    Former-commit-id: 18e84d1ab8d1b865e3cd3a0b81b19e6b723431ae

--- registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/config/ValidatorConfig.java
+++ registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/config/ValidatorConfig.java
@@ -5,13 +5,22 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
+import io.mosip.kernel.core.idobjectvalidator.constant.IdObjectValidatorSupportedOperations;
+import io.mosip.kernel.core.idobjectvalidator.exception.IdObjectIOException;
+import io.mosip.kernel.core.idobjectvalidator.exception.IdObjectValidationFailedException;
 import io.mosip.kernel.core.idobjectvalidator.spi.IdObjectValidator;
-import io.mosip.kernel.idobjectvalidator.impl.IdObjectSchemaValidator;
+import io.mosip.kernel.core.util.StringUtils;
 import io.mosip.registration.processor.core.packet.dto.applicantcategory.ApplicantTypeDocument;
+import io.mosip.registration.processor.rest.client.utils.RestApiClient;
 import io.mosip.registration.processor.stages.packet.validator.PacketValidateProcessor;
 import io.mosip.registration.processor.stages.packet.validator.PacketValidatorStage;
 import io.mosip.registration.processor.stages.utils.DocumentUtility;
@@ -21,6 +30,14 @@ import io.mosip.registration.processor.stages.utils.RestTemplateInterceptor;
 @Configuration
 public class ValidatorConfig {
 
+	private final String IDOBJECT_PROVIDER = "idobject.validator.provider";
+	
+	@Autowired
+	private RestApiClient restApiClient;
+	
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public PacketValidatorStage getPacketValidatorStage() {
 		return new PacketValidatorStage();
@@ -40,20 +57,49 @@ public class ValidatorConfig {
 	public IdObjectsSchemaValidationOperationMapper getIdObjectsSchemaValidationOperationMapper() {
 		return new IdObjectsSchemaValidationOperationMapper();
 	}
-
+	
 	@Bean
-	public IdObjectValidator getIdObjectValidator() {
-		return new IdObjectSchemaValidator();
+	public RestApiClient getRestApiClient() {
+		return new RestApiClient();
 	}
-
+ 		
 	@Bean
 	public ApplicantTypeDocument getApplicantTypeDocument() {
 		return new ApplicantTypeDocument();
 	}
+	
 	@Bean
 	public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.setInterceptors(Collections.singletonList(new RestTemplateInterceptor()));
+		restTemplate.setInterceptors(Collections.singletonList(new RestTemplateInterceptor(restApiClient)));
 		return restTemplate;
+	}
+	
+	@PostConstruct
+	public void validateReferenceValidator() throws ClassNotFoundException {
+		if (StringUtils.isNotBlank(env.getProperty(IDOBJECT_PROVIDER))) {
+			Class.forName(env.getProperty(IDOBJECT_PROVIDER));
+		}
+	}
+
+	@Bean
+	@Lazy
+	public IdObjectValidator referenceValidator()
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		if (StringUtils.isNotBlank(env.getProperty(IDOBJECT_PROVIDER))) {
+			return (IdObjectValidator) Class
+					.forName(env.getProperty(IDOBJECT_PROVIDER)).newInstance();
+		} else {
+			
+			return new IdObjectValidator() {
+
+				@Override
+				public boolean validateIdObject(Object identityObject, IdObjectValidatorSupportedOperations operation)
+						throws IdObjectValidationFailedException, IdObjectIOException {
+					// TODO Auto-generated method stub
+					return true;
+				}
+			};
+		}
 	}
 }
