eb3b33f6b919646a25d3effd4345233d62813153
srinivasanMT <srinivasan.lakshminarasimhan@mindtree.com>
2019-10-14 14:23:36 +0530

    MOS-29625 added new testcases
    Former-commit-id: a921ba22d61e78501e72adda058167b04fca4ae7
    Former-commit-id: 91cb33f2b2475fed10318c1ebee6a77bd0793d4d

--- kernel/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/integration/DeviceProviderManagementIntegrationTest.java
+++ kernel/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/integration/DeviceProviderManagementIntegrationTest.java
@@ -1,18 +1,29 @@
 package io.mosip.kernel.masterdata.test.integration;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import io.mosip.kernel.masterdata.entity.DeviceProvider;
+import io.mosip.kernel.masterdata.entity.DeviceService;
+import io.mosip.kernel.masterdata.entity.RegisteredDevice;
 import io.mosip.kernel.masterdata.repository.DeviceProviderRepository;
 import io.mosip.kernel.masterdata.repository.DeviceServiceRepository;
+import io.mosip.kernel.masterdata.repository.RegisteredDeviceRepository;
 import io.mosip.kernel.masterdata.test.TestBootApplication;
 
 /**
@@ -26,17 +37,48 @@ import io.mosip.kernel.masterdata.test.TestBootApplication;
 @AutoConfigureMockMvc(print = MockMvcPrint.LOG_DEBUG, printOnlyOnFailure = false)
 public class DeviceProviderManagementIntegrationTest {
 
-	@Autowired
+	@MockBean
 	private DeviceServiceRepository deviceServiceRepository;
-	
-	@Autowired
+
+	@MockBean
 	private DeviceProviderRepository deviceProviderRepository;
-	
-	
-	
+
+	@MockBean
+	private RegisteredDeviceRepository regDeviceRepository;
+
+	@Autowired
+	private MockMvc mockBean;
+
+	private DeviceService deviceService;
+
+	private DeviceProvider deviceProvider;
+
+	private RegisteredDevice registeredDevice;
+
 	@Before
 	public void setUp() {
-		
-        when(deviceServiceRepository.findByIdAndDProviderId(Mockito.anyString(), Mockito.anyString())).thenReturn(value)
+		deviceService = new DeviceService();
+		deviceService.setId("1111");
+		deviceService.setDProviderId("10001");
+		deviceService.setSwVersion("0.1v");
+
+		deviceProvider = new DeviceProvider();
+		deviceProvider.setId("1111");
+
+		registeredDevice = new RegisteredDevice();
+		registeredDevice.setDeviceId("10001");
+
+		when(deviceServiceRepository.findByIdAndDProviderId(Mockito.anyString(), Mockito.anyString()))
+				.thenReturn(deviceService);
+		when(deviceServiceRepository.findByIdAndIsActiveIsTrue(Mockito.anyString())).thenReturn(deviceService);
+		when(regDeviceRepository.findByCodeAndIsActiveIsTrue(Mockito.anyString())).thenReturn(registeredDevice);
+	}
+
+	@Ignore
+	@WithUserDetails("zonal-admin")
+	@Test
+	public void validateDeviceProvider() throws Exception {
+
+		mockBean.perform(get("/deviceprovider/validate/10001/1111/10001/0.1v")).andExpect(status().isOk());
 	}
 }
