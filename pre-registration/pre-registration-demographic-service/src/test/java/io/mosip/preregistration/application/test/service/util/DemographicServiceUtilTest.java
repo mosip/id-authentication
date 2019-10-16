1c46332f62a88795ef74c911a92507d9f305de4b
Kishan Rathore <Kishan.Rathore2@mindtree.com>
2019-10-16 10:08:44 +0530

    MOS-29671 : demographic service unit test changes
    Former-commit-id: 7cba6a73d3e62709d857c4a0cd31f64ad0cf2d35
    Former-commit-id: 1acfd11e95b3aebfa3f9fa1dba2741d1a3a0e199

--- pre-registration/pre-registration-demographic-service/src/test/java/io/mosip/preregistration/application/test/service/util/DemographicServiceUtilTest.java
+++ pre-registration/pre-registration-demographic-service/src/test/java/io/mosip/preregistration/application/test/service/util/DemographicServiceUtilTest.java
@@ -18,15 +18,15 @@ import org.springframework.web.client.RestTemplate;
 
 import io.mosip.kernel.core.idobjectvalidator.spi.IdObjectValidator;
 import io.mosip.preregistration.application.DemographicTestApplication;
-import io.mosip.preregistration.application.dto.DemographicRequestDTO;
-import io.mosip.preregistration.application.exception.OperationNotAllowedException;
-import io.mosip.preregistration.application.exception.system.DateParseException;
-import io.mosip.preregistration.application.exception.system.JsonParseException;
-import io.mosip.preregistration.application.service.util.DemographicServiceUtil;
 import io.mosip.preregistration.core.code.StatusCodes;
 import io.mosip.preregistration.core.common.entity.DemographicEntity;
 import io.mosip.preregistration.core.util.AuditLogUtil;
 import io.mosip.preregistration.core.util.CryptoUtil;
+import io.mosip.preregistration.demographic.dto.DemographicRequestDTO;
+import io.mosip.preregistration.demographic.exception.OperationNotAllowedException;
+import io.mosip.preregistration.demographic.exception.system.DateParseException;
+import io.mosip.preregistration.demographic.exception.system.JsonParseException;
+import io.mosip.preregistration.demographic.service.util.DemographicServiceUtil;
 
 /**
  * Test class to test the PreRegistration Service util methods
