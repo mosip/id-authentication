d8d759a2815bfebbcf0a3df95d203d491aa4ab1e
urviljoshi <Urvil.Joshi2@mindtree.com>
2019-10-30 18:23:40 +0530

    MOS-29803 vid service added
    Former-commit-id: 5293cb53ac565b1c59c54d5bc3375793170e0205
    Former-commit-id: 98959b66f13a4c256fed639dcc62942f63fac4c7

--- kernel/kernel-uingenerator-service/src/main/java/io/mosip/kernel/uingenerator/repository/UinRepository.java
+++ kernel/kernel-uingenerator-service/src/main/java/io/mosip/kernel/uingenerator/repository/UinRepository.java
@@ -1,6 +1,7 @@
 package io.mosip.kernel.uingenerator.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import io.mosip.kernel.uingenerator.entity.UinEntity;
