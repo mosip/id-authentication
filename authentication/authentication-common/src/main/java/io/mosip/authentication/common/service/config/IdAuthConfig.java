4fa9cd09c0ab46fad6c25dc4d2d0281bc84411cd
Manoj SP <Manoj.SP@mindtree.com>
2019-11-08 12:07:24 +0530

    MOS-28644 - updated id auth config
    Former-commit-id: 9237f7ac5f85e44a0bc26380f6c97bbbb479af3a
    Former-commit-id: b41ad4ef1dc220a0fb045dcef21d84d583d2f4cb

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/config/IdAuthConfig.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/config/IdAuthConfig.java
@@ -31,31 +31,43 @@ import io.mosip.kernel.core.bioapi.spi.IBioApi;
 import io.mosip.kernel.core.exception.ExceptionUtils;
 import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig;
+
 /**
  * Class for defining configurations for the service.
  * 
  * @author Manoj SP
  *
  */
-public abstract class IdAuthConfig extends HibernateDaoConfig{
-	
+public abstract class IdAuthConfig extends HibernateDaoConfig {
+
+	/** The mosip logger. */
 	private static Logger mosipLogger = IdaLogger.getLogger(IdAuthConfig.class);
 
 	/** The environment. */
 	@Autowired
 	private Environment environment;
-	
+
 	/** The interceptor. */
 	@Autowired
 	private Interceptor interceptor;
-	
+
+	/** The bio api. */
+	private IBioApi bioApi;
+
+	/**
+	 * Initialize.
+	 */
 	@PostConstruct
 	public void initialize() {
 		IdType.initializeAliases(environment);
 	}
-	
-	/* (non-Javadoc)
-	 * @see io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig#jpaProperties()
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig#jpaProperties(
+	 * )
 	 */
 	@Override
 	public Map<String, Object> jpaProperties() {
@@ -68,25 +80,85 @@ public abstract class IdAuthConfig extends HibernateDaoConfig{
 	 * Finger provider.
 	 *
 	 * @return the i bio api
-	 * @throws IdAuthenticationAppException the id authentication app exception
+	 * @throws IdAuthenticationAppException
+	 *             the id authentication app exception
 	 */
 	@Bean("finger")
 	public IBioApi fingerProvider() throws IdAuthenticationAppException {
-		return getBiometricProvider(FINGERPRINT_PROVIDER, "finger", this::isFingerAuthEnabled);
+		if (Objects.nonNull(bioApi)
+				&& bioApi.getClass().getName().contentEquals(environment.getProperty(FINGERPRINT_PROVIDER))) {
+			return bioApi;
+		} else {
+			bioApi = getBiometricProvider(FINGERPRINT_PROVIDER, "finger", this::isFingerAuthEnabled);
+			return bioApi;
+		}
 	}
 
 	/**
 	 * Face provider.
 	 *
 	 * @return the i bio api
-	 * @throws IdAuthenticationAppException the id authentication app exception
+	 * @throws IdAuthenticationAppException
+	 *             the id authentication app exception
 	 */
 	@Bean("face")
 	public IBioApi faceProvider() throws IdAuthenticationAppException {
-		return getBiometricProvider(FACE_PROVIDER, "face", this::isFaceAuthEnabled);
+		if (Objects.nonNull(bioApi)
+				&& bioApi.getClass().getName().contentEquals(environment.getProperty(FACE_PROVIDER))) {
+			return bioApi;
+		} else {
+			bioApi = getBiometricProvider(FACE_PROVIDER, "face", this::isFaceAuthEnabled);
+			return bioApi;
+		}
 	}
 
-	private IBioApi getBiometricProvider(String property, String modalityName, Supplier<Boolean> enablementChecker) throws IdAuthenticationAppException {
+	/**
+	 * Iris provider.
+	 *
+	 * @return the i bio api
+	 * @throws IdAuthenticationAppException
+	 *             the id authentication app exception
+	 */
+	@Bean("iris")
+	public IBioApi irisProvider() throws IdAuthenticationAppException {
+		if (Objects.nonNull(bioApi)
+				&& bioApi.getClass().getName().contentEquals(environment.getProperty(IRIS_PROVIDER))) {
+			return bioApi;
+		} else {
+			bioApi = getBiometricProvider(IRIS_PROVIDER, "iris", this::isIrisAuthEnabled);
+			return bioApi;
+		}
+	}
+
+	/**
+	 * Composite biometric provider.
+	 *
+	 * @return the i bio api
+	 * @throws IdAuthenticationAppException the id authentication app exception
+	 */
+	@Bean("composite")
+	public IBioApi compositeBiometricProvider() throws IdAuthenticationAppException {
+		if (Objects.nonNull(bioApi)
+				&& bioApi.getClass().getName().contentEquals(environment.getProperty(COMPOSITE_BIO_PROVIDER))) {
+			return bioApi;
+		} else {
+			bioApi = getBiometricProvider(COMPOSITE_BIO_PROVIDER, "CompositBiometrics",
+					() -> this.isFingerAuthEnabled() || this.isIrisAuthEnabled() || this.isFaceAuthEnabled());
+			return bioApi;
+		}
+	}
+
+	/**
+	 * Gets the biometric provider.
+	 *
+	 * @param property the property
+	 * @param modalityName the modality name
+	 * @param enablementChecker the enablement checker
+	 * @return the biometric provider
+	 * @throws IdAuthenticationAppException the id authentication app exception
+	 */
+	private IBioApi getBiometricProvider(String property, String modalityName, Supplier<Boolean> enablementChecker)
+			throws IdAuthenticationAppException {
 		try {
 			if (Objects.nonNull(environment.getProperty(property)) && enablementChecker.get()) {
 				return (IBioApi) Class.forName(environment.getProperty(property)).newInstance();
@@ -98,23 +170,6 @@ public abstract class IdAuthConfig extends HibernateDaoConfig{
 					ExceptionUtils.getStackTrace(e));
 			throw new IdAuthenticationAppException("", "Unable to load " + modalityName + " provider", e);
 		}
-	}
-	
-	/**
-	 * Iris provider.
-	 *
-	 * @return the i bio api
-	 * @throws IdAuthenticationAppException the id authentication app exception
-	 */
-	@Bean("iris")
-	public IBioApi irisProvider() throws IdAuthenticationAppException {
-		return getBiometricProvider(IRIS_PROVIDER, "iris", this::isIrisAuthEnabled);
-	}
-	
-	@Bean("composite")
-	public IBioApi compositeBiometricProvider() throws IdAuthenticationAppException {
-		return getBiometricProvider(COMPOSITE_BIO_PROVIDER, "CompositBiometrics", 
-					() -> this.isFingerAuthEnabled() || this.isIrisAuthEnabled() || this.isFaceAuthEnabled());
 	}
 
 	/**
@@ -142,21 +197,21 @@ public abstract class IdAuthConfig extends HibernateDaoConfig{
 		source.addBasenames("errormessages", "actionmessages");
 		return source;
 	}
-	
+
 	/**
 	 * Checks if is finger auth enabled.
 	 *
 	 * @return true, if is finger auth enabled
 	 */
 	protected abstract boolean isFingerAuthEnabled();
-	
+
 	/**
 	 * Checks if is face auth enabled.
 	 *
 	 * @return true, if is face auth enabled
 	 */
 	protected abstract boolean isFaceAuthEnabled();
-	
+
 	/**
 	 * Checks if is iris auth enabled.
 	 *
