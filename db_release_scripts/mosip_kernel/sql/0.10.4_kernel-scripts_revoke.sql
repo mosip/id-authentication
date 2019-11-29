59dd55b62fe264da15ef35efdfd052bf3c057c8b
Sada <sadanandegowda.dm@mindtree.com>
2019-11-29 17:38:03 +0530

    Added VID pool related DB changes in core and delta release db-scripts
    Former-commit-id: bd713491690fab754a8f705b469a66288de36558
    Former-commit-id: 3d600593ed7d477180b39fcc213aaf9e57a3b5eb

--- db_release_scripts/mosip_kernel/sql/0.10.4_kernel-scripts_revoke.sql
+++ db_release_scripts/mosip_kernel/sql/0.10.4_kernel-scripts_revoke.sql
@@ -3,9 +3,13 @@
 -- Release Version 	: 0.10.4
 -- Purpose    		: Revoking Database Alter deployement done for release in Kernel DB.       
 -- Create By   		: Sadanandegowda
--- Created Date		: 23-Sep-2019
+-- Created Date		: 28-Nov-2019
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
 
 \c mosip_kernel sysadmin
+
+--------- VID POOL REQUIREMENT DB CHANGES REVOKE -----------
+
+DROP TABLE IF EXISTS kernel.vid;
