a00b987a056ec2638087b6db95a7ff562f3db24f
Urvil.Joshi2@mindtree.com <Urvil.Joshi2@mindtree.com>
2019-12-04 17:47:18 +0530

    MOS-000
    Former-commit-id: 6fb26afcdbf8fd0cbeee27b29b797179ff836aa7
    Former-commit-id: ecb995a068a91425a6920af32bca114b18a225a7

--- kernel/kernel-crypto-jce/src/test/java/io/mosip/kernel/crypto/jce/test/CryptoCoreNoSuchAlgorithmExceptionTest.java
+++ kernel/kernel-crypto-jce/src/test/java/io/mosip/kernel/crypto/jce/test/CryptoCoreNoSuchAlgorithmExceptionTest.java
@@ -19,6 +19,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -26,6 +28,7 @@ import io.mosip.kernel.core.crypto.spi.CryptoCoreSpec;
 import io.mosip.kernel.core.exception.NoSuchAlgorithmException;
 
 @RunWith(SpringRunner.class)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 @SpringBootTest
 public class CryptoCoreNoSuchAlgorithmExceptionTest {
 
