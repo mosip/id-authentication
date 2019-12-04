aef39091f9cb3508358221ec8804fc56df7d4bc6
TaleevAalam <taleev.aalam@mindtree.com>
2019-12-04 17:42:50 +0530

    MOS-30284 - local de duplication enable/disable
    Former-commit-id: 36bce4aa87eefeb45769312de50b8ca7fa977e73
    Former-commit-id: 0c1c4934cca7f493c4ce0e678665d18e8d9028c1

--- registration/registration-client/src/main/java/io/mosip/registration/controller/BaseController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/BaseController.java
@@ -1063,7 +1063,7 @@ public class BaseController {
 
 			String message = RegistrationUIConstants.REMAP_NO_ACCESS_MESSAGE;
 
-			if (isPacketsPendingForEOD()) {
+			if (isPacketsPendingForEODOrReRegister()) {
 				message += RegistrationConstants.NEW_LINE + RegistrationUIConstants.REMAP_EOD_PROCESS_MESSAGE;
 			}
 			message += RegistrationConstants.NEW_LINE + RegistrationUIConstants.REMAP_CLICK_OK;
@@ -1142,6 +1142,21 @@ public class BaseController {
 	protected boolean isPacketsPendingForEOD() {
 
 		return centerMachineReMapService.isPacketsPendingForEOD();
+	}
+
+	protected boolean isPacketsPendingForEODOrReRegister() {
+
+		return isPacketsPendingForEOD() || isPacketsPendingForReRegister();
+	}
+	
+	/**
+	 * Checks if is packets pending for ReRegister.
+	 *
+	 * @return true, if is packets pending for ReRegister
+	 */
+	protected boolean isPacketsPendingForReRegister() {
+
+		return centerMachineReMapService.isPacketsPendingForReRegister();
 	}
 
 	/**
