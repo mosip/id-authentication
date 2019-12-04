c84f4053109c25c47764342f850633e36deabfad
M1048219 <girish.yarru@mindtree.com>
2019-12-04 18:19:06 +0530

    MOS-30323:Utiity class and notification class
    Former-commit-id: 96ec1ad9758c937e192c3c18aa9e73e4ea8836c5
    Former-commit-id: 57e38e4cbb9069d7b9985445e9d93c4372029aa7

---
+++ resident/resident-service/src/main/java/io/mosip/resident/dto/NotificationResponseDTO.java
@@ -0,0 +1,9 @@
+package io.mosip.resident.dto;
+
+import lombok.Data;
+
+@Data
+public class NotificationResponseDTO {
+private String status;
+private  String message;
+}
---
+++ resident/resident-service/src/main/java/io/mosip/resident/dto/TemplateResponseDto.java
@@ -0,0 +1,17 @@
+package io.mosip.resident.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+
+
+public class TemplateResponseDto implements Serializable {
+	/**
+	 * Default serial version
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<TemplateDto> templates;
+}
