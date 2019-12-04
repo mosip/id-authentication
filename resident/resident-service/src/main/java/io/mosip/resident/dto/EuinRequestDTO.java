e3e18c8887e6b1d21a4aec3754caccf78dd7c9ea
jpnayak007 <jyotiprakashnayak103@gmail.com>
2019-12-04 18:22:38 +0530

    MOS-30307 : adding otp validation
    Former-commit-id: 40d019e2622f4e8c8eccea792df240c4ecc26c41
    Former-commit-id: f0085bb510ccf1c7a274a6c624095d4d3c5a605b

---
+++ resident/resident-service/src/main/java/io/mosip/resident/dto/EuinRequestDTO.java
@@ -0,0 +1,33 @@
+package io.mosip.resident.dto;
+
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EuinRequestDTO implements Serializable {
+	private static final long serialVersionUID = -4492209826585681216L;
+	@NotBlank(message = "transactionID should not be empty")
+	@NotNull(message = "transactionID should not be null")
+	private String transactionID;
+	@NotBlank(message = "individualId should not be empty")
+	@NotNull(message = "individualId should not be null")
+	private String individualId;
+	@NotBlank(message = "individualIdType should not be empty")
+	@NotNull(message = "individualIdType should not be null")
+	private String individualIdType;
+	@NotBlank(message = "cardType should not be empty")
+	@NotNull(message = "cardType should not be null")
+	private String cardType;
+	@NotBlank(message = "otp should not be empty")
+	@NotNull(message = "otp should not be null")
+	private String otp;
+}
