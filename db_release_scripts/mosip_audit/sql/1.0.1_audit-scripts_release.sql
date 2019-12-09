f348266969c531eab774ddae30d6de21a3e6d447
Sada <sadanandegowda.dm@mindtree.com>
2019-12-09 16:48:16 +0530

    Pushed 1.0.0 release DB changes
    Former-commit-id: 151a278680bf14d70f6b246071a07ffdb4d45907
    Former-commit-id: 466b31c5b01aa50f0688e1fd8060547611494399

---
+++ db_release_scripts/mosip_audit/sql/1.0.1_audit-scripts_release.sql
@@ -0,0 +1,11 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name: mosip_audit
+-- Release Version 	: 1.0.1
+-- Purpose    		: Database Alter scripts for the release for Audit DB.       
+-- Create By   		: Sadanandegowda
+-- Created Date		: 09-Dec-2019
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- -------------------------------------------------------------------------------------------------
+
+\c mosip_audit sysadmin
