777d99f122a34381c531cab6741db5c47682b2a8
jpnayak007 <jyotiprakashnayak103@gmail.com>
2019-12-10 11:40:32 +0530

    adding resident controller test
    Former-commit-id: f6739f8306ca64039c963dc725ea5c901a9dfae9
    Former-commit-id: cbac443d5c5f3119f362c3a3d869b58f283caba9

--- resident/resident-service/src/test/java/io/mosip/resident/controller/ResidentControllerTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/controller/ResidentControllerTest.java
@@ -1,10 +1,13 @@
 /**
  * 
- *//*
+ */
 package io.mosip.resident.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
@@ -14,40 +17,36 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import io.mosip.resident.ResidentTestBootApplication;
 import io.mosip.resident.dto.AuthLockRequestDto;
 import io.mosip.resident.dto.RequestWrapper;
 import io.mosip.resident.dto.ResponseDTO;
 import io.mosip.resident.service.ResidentService;
-import io.mosip.resident.testconfig.ResidentServiceConfigTest;
 import io.mosip.resident.validator.RequestValidator;
 
-*//**
- * @author M1022006
+/**
+ * @author Sowmya Ujjappa Banakar
+ * @author Jyoti Prakash Nayak
  *
- *//*
+ */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ResidentTestBootApplication.class)
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = ResidentServiceConfigTest.class)
-@ImportAutoConfiguration(RefreshAutoConfiguration.class)
+@TestPropertySource(locations = "classpath:application.properties")
 public class ResidentControllerTest {
 
-	@Autowired
-	private ObjectMapper objectMapper;
 	@MockBean
 	private ResidentService residentService;
 
@@ -59,11 +58,11 @@ public class ResidentControllerTest {
 
 	RequestWrapper<AuthLockRequestDto> authLockRequest;
 
-	*//** The array to json. *//*
+	/** The array to json. */
 	private String regStatusToJson;
-	Gson gson = new GsonBuilder().serializeNulls().create();
+	
 
-	*//** The mock mvc. *//*
+	/** The mock mvc. */
 	@Autowired
 	private MockMvc mockMvc;
 
@@ -79,11 +78,24 @@ public class ResidentControllerTest {
 	@Test
 	public void testRequestAuthLockSuccess() throws Exception {
 		ResponseDTO responseDto = new ResponseDTO();
+		responseDto.setStatus("success");
 		doNothing().when(validator).validateAuthLockRequest(authLockRequest);
 		Mockito.doReturn(responseDto).when(residentService).reqAauthLock(ArgumentMatchers.any());
 
 		this.mockMvc.perform(post("/req/auth-lock").contentType(MediaType.APPLICATION_JSON).content(regStatusToJson))
-				.andExpect(status().isOk());
+				.andExpect(status().isOk()).andExpect(jsonPath("$.response.status", is("success")));
 	}
+	
+	@Test
+	public void testRequestAuthLockBadRequest() throws Exception {
+		ResponseDTO responseDto = new ResponseDTO();
+		doNothing().when(validator).validateAuthLockRequest(authLockRequest);
+		Mockito.doReturn(responseDto).when(residentService).reqAauthLock(ArgumentMatchers.any());
+
+		MvcResult result=this.mockMvc.perform(post("/req/auth-lock").contentType(MediaType.APPLICATION_JSON).content(""))
+				.andExpect(status().isOk()).andReturn();
+		assertTrue(result.getResponse().getContentAsString().contains("RES-500"));
+	}
+	
+	
 }
-*/
