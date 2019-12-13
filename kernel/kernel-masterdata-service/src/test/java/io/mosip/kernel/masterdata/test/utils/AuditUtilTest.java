2c918b6ddf42aa9613d7ddc0a4e578cee09b98b0
srinivasanMT <srinivasan.lakshminarasimhan@mindtree.com>
2019-12-13 10:27:02 +0530

    MOS-29512 added unit testcases
    Former-commit-id: 6b23738c1c9ac975fbb8fc980393159210f47891
    Former-commit-id: 79204080bb0bda63144daada2e154ebdf520d18a

---
+++ kernel/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/utils/AuditUtilTest.java
@@ -0,0 +1,42 @@
+package io.mosip.kernel.masterdata.test.utils;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import io.mosip.kernel.masterdata.utils.AuditUtil;
+
+@RunWith(JUnit4.class)
+public class AuditUtilTest {
+
+	@Autowired
+	private AuditUtil auditUtil;
+	
+	@Value("${mosip.kernel.masterdata.audit-url}")
+	private String auditUrl;
+	
+	private MockRestServiceServer mockRestServiceServer;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Before
+	public void setUp() {
+		mockRestServiceServer=mockRestServiceServer.bindTo(restTemplate).build();
+	}
+	
+	@Test
+	public void testAuditUtil() {
+		mockRestServiceServer.expect(post(uri))
+	}
+}
+
+
+
