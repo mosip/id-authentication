b867b0a621d4a73dbcd1445ea1dc7f7e73889f3e
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-01-22 17:40:24 +0530

    MOS-30337_Removed code duplication in Demo Matching Strategy classes.

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/spi/indauth/match/TextMatchingStrategy.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/spi/indauth/match/TextMatchingStrategy.java
@@ -1,9 +1,13 @@
 package io.mosip.authentication.core.spi.indauth.match;
 
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
+import io.mosip.authentication.core.dto.DemoMatcherUtil;
 import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
+import io.mosip.authentication.core.spi.bioauth.util.DemoNormalizer;
 
 /**
  * The Interface MatchingStrategy.
@@ -16,5 +20,34 @@ public interface TextMatchingStrategy extends MatchingStrategy {
 		String entityInfo = entityValues.values().stream().collect(Collectors.joining(" "));
 		return  getMatchFunction().match(reqInfo, entityInfo, matchProperties);
 	}
+	
+	public static int normalizeAndMatch(Object reqInfo, 
+			Object entityInfo, 
+			Map<String, Object> props,
+			NormalizeFunction normalizeFunction, 
+			BiFunction<String, String, Integer> matchFunction) throws IdAuthenticationBusinessException {
+		if (reqInfo instanceof String && entityInfo instanceof String) {
+			Object demoNormalizerObject=  props.get("demoNormalizer");
+			Object langObject=props.get("langCode");
+			if(demoNormalizerObject instanceof  DemoNormalizer && langObject instanceof String) {
+				DemoNormalizer demoNormalizer=(DemoNormalizer)demoNormalizerObject;
+			    String langCode=(String)langObject;
+				String refInfoText = normalizeFunction.normalizeText(demoNormalizer, (String) reqInfo,langCode, props);
+				String entityInfoText = normalizeFunction.normalizeText(demoNormalizer, (String) reqInfo,langCode, props);
+				return matchFunction.apply(refInfoText, entityInfoText);
+			} else {
+				return 0;
+			}
+		} else {
+			return 0;
+		}
+	}
+	
+
+	@FunctionalInterface
+	public static interface NormalizeFunction {
+		String normalizeText(DemoNormalizer demoNormalizer, String inputText, String langCode, Map<String, Object> props) throws IdAuthenticationBusinessException;
+	}
+	
 	
 }
