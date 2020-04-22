4cf9326cac9c72a5a46143804646edc248f7f4d2
sadanandegowda <43214830+sadanandegowda@users.noreply.github.com>
2020-04-22 17:57:16 +0530

    [MOSIP-768] - DB Changes for Monolithic IDA Design (#33)

--- db_scripts/mosip_ida/mosip_ida_ddl_deploy.sql
+++ db_scripts/mosip_ida/mosip_ida_ddl_deploy.sql
@@ -6,6 +6,10 @@
 \ir ddl/ida-uin_auth_lock.sql
 \ir ddl/ida-uin_hash_salt.sql
 \ir ddl/ida-uin_encrypt_salt.sql
+\ir ddl/ida-key_alias.sql
+\ir ddl/ida-key_store.sql
+\ir ddl/ida-key_policy_def.sql
+\ir ddl/ida-key_policy_def_h.sql
 
 \ir ddl/ida-fk.sql
 
