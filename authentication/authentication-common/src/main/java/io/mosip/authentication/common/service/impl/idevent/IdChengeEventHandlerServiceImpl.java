c075ad7a136e7468ec5a43c82e291512ca1ce76b
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-05-09 19:41:36 +0530

    Minor refactoring

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/idevent/IdChengeEventHandlerServiceImpl.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/idevent/IdChengeEventHandlerServiceImpl.java
@@ -199,8 +199,7 @@ public class IdChengeEventHandlerServiceImpl implements IdChangeEventHandlerServ
 									.filter(Objects::nonNull)
 									.map(idService::getUinHash)
 									.collect(Collectors.toList());
-		List<IdentityEntity> vidEntities = findEntitys(ids);
-		return vidEntities;
+		return findEntitys(ids);
 	}
 	
 	private List<IdentityEntity> findEntitys(List<String> ids) {
