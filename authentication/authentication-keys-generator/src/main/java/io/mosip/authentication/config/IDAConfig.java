4524698d523ce48f9000a2c1488e1a474222ab5b
Nagarjuna K <nagarjunabtechece@gmail.com>
2020-05-29 12:52:13 +0530

    Key generator tool has been added (#66)
    * Keys generator tool has been added
    
    * Unwanted classes removed
    
    * bootstrap application name modified
    
    * application name updated in config
    
    * Update application-local.properties
    
    Co-authored-by: Nagarjuna <Nagarjuna@Techno-296.technoforte.co.in>

---
+++ authentication/authentication-keys-generator/src/main/java/io/mosip/authentication/config/IDAConfig.java
@@ -0,0 +1,53 @@
+package io.mosip.authentication.config;
+
+import java.security.Provider;
+import java.security.Security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import sun.security.pkcs11.SunPKCS11;
+
+@Configuration
+public class IDAConfig {
+
+	@Value("${mosip.kernel.keymanager.softhsm.config-path}")
+	private String configPath;
+	
+	@Value("${mosip.ida.database.url}")
+	private String dbUrl;
+	
+	@Value("${mosip.ida.database.user}")
+	private String dbUser;
+	
+	@Value("${mosip.ida.database.password}")
+	private String dbPassword;
+	
+	@Value("${mosip.ida.database.driverClassName}")
+	private String dbDriverClassName;
+	
+	@Value("${mosip.ida.database.schema}")
+	private String dbSchemaName;
+
+	@Bean
+	public DataSource buildDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource(dbUrl);
+		dataSource.setUsername(dbUser);
+		dataSource.setPassword(dbPassword);
+		dataSource.setDriverClassName(dbDriverClassName);
+		dataSource.setSchema(dbSchemaName);
+		return dataSource;
+	}
+	
+
+	@Bean
+    public Provider getProvider(){
+        Provider provider = new SunPKCS11(configPath);
+        Security.addProvider(provider);
+        return provider;
+    }
+}
---
+++ authentication/authentication-keys-generator/src/main/java/io/mosip/authentication/entity/DataEncryptKeystoreRepository.java
@@ -0,0 +1,16 @@
+package io.mosip.authentication.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DataEncryptKeystoreRepository extends JpaRepository<DataEncryptKeystore, Integer>{
+
+	@Query("SELECT d.key from DataEncryptKeystore d where d.id = :id")
+	String findKeyById(@Param("id") Integer id);
+	
+	@Query("SELECT MAX(d.id) from DataEncryptKeystore d")
+	long findMaxId();
+}
