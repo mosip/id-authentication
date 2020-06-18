b5c1a3a944e938d8790f06f214062c370e9ddda4
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-06-18 18:22:05 +0530

    correction in biosdk script

--- authentication/authentication-service/configure_biosdk.sh
+++ authentication/authentication-service/configure_biosdk.sh
@@ -3,21 +3,24 @@
 #installs the pkcs11 libraries.
 set -e
 
+mkdir -p /biosdk
+cd /biosdk
+
 echo "Download the softhsm client from $artifactory_url_env"
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xaa
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xab
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xac
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xad
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xae
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xaf
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xag
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xah
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xai
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xaj
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xak
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xal
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xam
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/xan
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xaa
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xab
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xac
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xad
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xae
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xaf
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xag
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xah
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xai
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xaj
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xak
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xal
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xam
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/xan
 
 cat x* > ./biosdk-tech5.zip
 
@@ -26,5 +29,8 @@ unzip biosdk-tech5.zip
 echo "Attempting to install softhsm client"
 cd ./biosdk-tech5 && /bin/bash ./install_biosdk.sh 
 echo "Installation of softhsm client complete"
+
+cd /
+rm -rf /biosdk
 
 exec "$@"
