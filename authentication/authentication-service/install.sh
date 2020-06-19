d522b6c4dc221e746bb8475a7cf508592ff65107
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-06-19 07:43:34 +0530

    fix in install script

--- authentication/authentication-service/install.sh
+++ authentication/authentication-service/install.sh
@@ -3,11 +3,9 @@
 #installs the pre-requisites.
 set -e
 
-current_module=$1
-
 echo "Downloading pre-requisites install scripts"
-wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/$git_user_env/$code_repo_env/$code_branch_env/authentication/$current_module/configure_biosdk.sh -O configure_biosdk.sh
-wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/$git_user_env/$code_repo_env/$code_branch_env/authentication/$current_module/configure_softhsm.sh -O configure_softhsm.sh
+wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/$git_user_env/$code_repo_env/$code_branch_env/authentication/$current_module_env/configure_biosdk.sh -O configure_biosdk.sh
+wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/$git_user_env/$code_repo_env/$code_branch_env/authentication/$current_module_env/configure_softhsm.sh -O configure_softhsm.sh
 
 echo "Installating pre-requisites.."
 cd /
