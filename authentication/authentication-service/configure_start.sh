5613843c068364695db9a2b18e6126dbae0f2e6d
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-06-23 11:18:17 +0530

    Correction to dockers

--- authentication/authentication-service/configure_start.sh
+++ authentication/authentication-service/configure_start.sh
@@ -4,8 +4,8 @@
 set -e
 
 echo "Downloading pre-requisites install scripts"
-wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/$git_user_env/$code_repo_env/$code_branch_env/authentication/$current_module_env/configure_biosdk.sh -O configure_biosdk.sh
-wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/$git_user_env/$code_repo_env/$code_branch_env/authentication/$current_module_env/configure_softhsm.sh -O configure_softhsm.sh
+wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/deployment/docker/id-authentication/configure_biosdk.sh -O configure_biosdk.sh
+wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/deployment/docker/id-authentication/configure_softhsm.sh -O configure_softhsm.sh
 
 echo "Installating pre-requisites.."
 cd /
--- docker/configure_biosdk.sh
+++ docker/configure_biosdk.sh
@@ -7,12 +7,12 @@ mkdir -p /biosdk
 cd /biosdk
 
 echo "Download the biosdk from $artifactory_url_env"
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/$bio_sdk_folder_env/biosdk.zip -O biosdk.zip
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/0.8/biosdk.zip
 echo "Downloaded $artifactory_url_env"
 
 unzip biosdk.zip
 echo "Attempting to install biosdk"
-source ./install.sh 
+/bin/bash ./install.sh 
 echo "Installation of biosdk complete"
 
 cd /
