4009961fc937b216fbf3ada0c96166145df5e5df
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-07-02 14:09:59 +0530

    Corrected docker file names

--- docker/configure_biosdk.sh
+++ docker/configure_biosdk.sh
@@ -7,8 +7,7 @@ mkdir -p /biosdk
 cd /biosdk
 
 echo "Download the biosdk from $artifactory_url_env"
-#wget $artifactory_url_env/artifactory/libs-release-local/biosdk/tech5/0.8/biosdk.zip
-wget $artifactory_url_env/artifactory/libs-release-local/biosdk/mock/0.9/biosdk.zip
+wget $artifactory_url_env/artifactory/libs-release-local/biosdk/$bio_sdk_folder_env/biosdk.zip -O biosdk.zip
 echo "Downloaded $artifactory_url_env"
 
 unzip biosdk.zip
