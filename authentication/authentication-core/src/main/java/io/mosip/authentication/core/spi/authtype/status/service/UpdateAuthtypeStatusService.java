c904c2e5552f2c81a942e71cdef574cd4e783768
Manoj SP <Manoj.SP@mindtree.com>
2020-09-03 12:43:00 +0530

    [MOSIP-8293] added javadoc

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/spi/authtype/status/service/UpdateAuthtypeStatusService.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/spi/authtype/status/service/UpdateAuthtypeStatusService.java
@@ -8,9 +8,19 @@ import org.springframework.stereotype.Service;
 import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
 import io.mosip.idrepository.core.dto.AuthtypeStatus;
 
+/**
+ * The Interface UpdateAuthtypeStatusService.
+ */
 @Service
 public interface UpdateAuthtypeStatusService {
 
+	/**
+	 * Update auth type status.
+	 *
+	 * @param tokenId the token id
+	 * @param authTypeStatusList the auth type status list
+	 * @throws IdAuthenticationBusinessException the id authentication business exception
+	 */
 	public void updateAuthTypeStatus(String tokenId, List<AuthtypeStatus> authTypeStatusList)
 			throws IdAuthenticationBusinessException;
 }
