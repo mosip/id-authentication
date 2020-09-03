8181183a900731f298a0cfdf5919b2065ee7ee25
sadanandegowda <43214830+sadanandegowda@users.noreply.github.com>
2020-09-03 16:32:42 +0530

    IDA DB changes added (#153)

--- db_release_scripts/mosip_ida/sql/1.1_ida-scripts_release.sql
+++ db_release_scripts/mosip_ida/sql/1.1_ida-scripts_release.sql
@@ -1,9 +1,9 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_ida
--- Release Version 	: 1.0.8
+-- Release Version 	: 1.1
 -- Purpose    		: Database Alter scripts for the release for ID Authentication DB.       
 -- Create By   		: Sadanandegowda DM
--- Created Date		: 22-Apr-2020
+-- Created Date		: May-2020
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
@@ -16,6 +16,9 @@
 \ir ../ddl/ida-key_store.sql
 \ir ../ddl/ida-key_policy_def.sql
 \ir ../ddl/ida-key_policy_def_h.sql
+
+\ir ../ddl/ida-identity_cache.sql
+\ir ../ddl/ida-data_encrypt_keystore.sql
 
 
 -------------- Level 1 data load scripts ------------------------
@@ -30,8 +33,6 @@ TRUNCATE TABLE ida.key_policy_def cascade ;
 TRUNCATE TABLE ida.key_policy_def_h cascade ;
 
 \COPY ida.key_policy_def_h (app_id,key_validity_duration,is_active,cr_by,cr_dtimes,eff_dtimes) FROM './dml/ida-key_policy_def_h.csv' delimiter ',' HEADER  csv;
-
----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 ----------------------------------------------------------------------------------------------------
