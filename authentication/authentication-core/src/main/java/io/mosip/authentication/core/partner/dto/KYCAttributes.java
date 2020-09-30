3557158546e3062850ff2c44bd9820327c5dcff1
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2020-09-30 17:05:01 +0530

    MOSIP-9575-Removed required field check in allowed kyc attributes (#210)

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/partner/dto/KYCAttributes.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/partner/dto/KYCAttributes.java
@@ -12,9 +12,6 @@ public class KYCAttributes {
 	/** The attribute name. */
 	private String attributeName;
 	
-	/** required is for the attribute name */
-	private boolean required;
-	
 	/**  masked is for the attribute  name*/
 	private boolean masked;
 
