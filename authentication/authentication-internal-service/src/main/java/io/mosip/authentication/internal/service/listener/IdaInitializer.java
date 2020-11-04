25da20a6757f006a97d67eebe3065fd4fa0593fa
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2020-11-04 18:46:34 +0530

    Ida starup fix 2 (#256)
    * fix in master data initial load. added option to disable websub resubscription
    
    * fix in master data initial load. added option to disable websub resubscription

--- authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/listener/IdaInitializer.java
+++ authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/listener/IdaInitializer.java
@@ -15,41 +15,53 @@ import io.mosip.authentication.core.logger.IdaLogger;
 import io.mosip.authentication.internal.service.integration.WebSubSubscriptionHelper;
 import io.mosip.kernel.core.logger.spi.Logger;
 
-//TODO To be removed. Added as a work-around for the bug: MOSIP-9496
+/**
+ * TODO : To be removed. The initializer to schedule subscription of topics
+ * which is done as a a work-around for the bug: MOSIP-9496
+ * 
+ * @author Loganathan Sekar
+ *
+ */
 @Component
-public class IdaInitializer implements ApplicationListener<ApplicationReadyEvent>{
-	
+public class IdaInitializer implements ApplicationListener<ApplicationReadyEvent> {
+
 	private static Logger logger = IdaLogger.getLogger(IdaInitializer.class);
-	
-	@Value("${retry-count:3}")
+
+	@Value("${ida-websub-resubscription-retry-count:3}")
 	private int retryCount;
 
-	@Value("${resubscription-delay-secs:7200}") // Default is 60 * 60 * 2 = 2 hours
+	/**
+	 * Default is Zero which will disable the scheduling.
+	 */
+	@Value("${ida-websub-resubscription-delay-secs:0}")
 	private int reSubscriptionDelaySecs;
-	
+
 	@Autowired
 	private WebSubSubscriptionHelper webSubSubscriptionHelper;
-	
+
 	@Autowired
 	private ThreadPoolTaskScheduler taskScheduler;
-	
+
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
-		logger.info(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(), "onApplicationEvent",  "Work around for web-sub notification issue after some time.");
+		logger.info(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(), "onApplicationEvent",
+				"Work around for web-sub notification issue after some time.");
 		scheduleRetrySubscriptions();
 	}
 
 	private void scheduleRetrySubscriptions() {
-		logger.info(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(), "scheduleRetrySubscriptions",  "Scheduling re-subscription every " + reSubscriptionDelaySecs + " seconds");
-		taskScheduler.scheduleAtFixedRate(this::retrySubscriptions,
-				Instant.now().plusSeconds(reSubscriptionDelaySecs), 
-				Duration.ofSeconds(reSubscriptionDelaySecs));
+		logger.info(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(), "scheduleRetrySubscriptions",
+				"Scheduling re-subscription every " + reSubscriptionDelaySecs + " seconds");
+		if (reSubscriptionDelaySecs > 0) {
+			taskScheduler.scheduleAtFixedRate(this::retrySubscriptions,
+					Instant.now().plusSeconds(reSubscriptionDelaySecs), Duration.ofSeconds(reSubscriptionDelaySecs));
+		}
 	}
 
 	private void retrySubscriptions() {
-		//Call Init Subscriptions for the count until no error in the subscription
-		for(int i = 0; i < retryCount; i++) {
-			if(initSubsriptions()) {
+		// Call Init Subscriptions for the count until no error in the subscription
+		for (int i = 0; i < retryCount; i++) {
+			if (initSubsriptions()) {
 				return;
 			}
 		}
@@ -57,15 +69,15 @@ public class IdaInitializer implements ApplicationListener<ApplicationReadyEvent
 
 	private boolean initSubsriptions() {
 		try {
-			logger.info(IdAuthCommonConstants.SESSION_ID, "initSubsriptions",  "", "Initializing subscribptions..");
+			logger.info(IdAuthCommonConstants.SESSION_ID, "initSubsriptions", "", "Initializing subscribptions..");
 			webSubSubscriptionHelper.initSubsriptions();
-			logger.info(IdAuthCommonConstants.SESSION_ID, "initSubsriptions",  "", "Initialized subscribptions.");
+			logger.info(IdAuthCommonConstants.SESSION_ID, "initSubsriptions", "", "Initialized subscribptions.");
 			return true;
 		} catch (Exception e) {
-			logger.error(IdAuthCommonConstants.SESSION_ID, "initSubsriptions",  "", "Initializing subscribptions failed: " + e.getMessage());
+			logger.error(IdAuthCommonConstants.SESSION_ID, "initSubsriptions", "",
+					"Initializing subscribptions failed: " + e.getMessage());
 			return false;
 		}
 	}
-	
 
 }
