4eda014ad15c70705a549e7b35889f44c16da961
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2020-11-06 13:27:46 +0530

    Hsm switch at runtime

--- docker/configure_softhsm.sh
+++ docker/configure_softhsm.sh
@@ -3,11 +3,24 @@
 #installs the pkcs11 libraries.
 set -e
 
-echo "Download the softhsm client from $artifactory_url_env"
-wget $artifactory_url_env/artifactory/libs-release-local/hsm/$softhsm_client_zip_env -O client.zip
-echo "Downloaded $artifactory_url_env"
-unzip client.zip
-echo "Attempting to install softhsm client"
-cd ./client && ./install.sh
-echo "Installation of softhsm client complete"
+DEFAULT_ZIP_PATH=artifactory/libs-release-local/hsm/client.zip
+[ -z "$zip_file_path" ] && zip_path="$DEFAULT_ZIP_PATH" || zip_path="$zip_file_path"
 
+echo "Download the client from $artifactory_url_env"
+echo "Zip File Path: $zip_path"
+
+FILE_NAME=${zip_path##*/}
+DIR_NAME=${FILE_NAME%%.*}
+
+echo "File names is: $FILE_NAME \n"
+echo "Directory name is: $DIR_NAME \n"
+
+wget "$artifactory_url_env/$zip_path"
+echo "Downloaded $artifactory_url_env/$zip_path"
+
+unzip $FILE_NAME
+echo "Attempting to install"
+cd ./$DIR_NAME && ./install.sh
+echo "Installation complete"
+
+exec "$@"
