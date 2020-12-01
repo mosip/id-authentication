4c8b4163e5b9002c6d3c8ed226d00cb20fedf36e
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-12-01 18:59:02 +0530

    adding logger

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/controller/PartnerServiceCallbackControllerDelegate.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/controller/PartnerServiceCallbackControllerDelegate.java
@@ -6,8 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.mosip.authentication.common.service.integration.PartnerServiceManager;
+import io.mosip.authentication.core.constant.IdAuthCommonConstants;
+import io.mosip.authentication.core.logger.IdaLogger;
 import io.mosip.idrepository.core.dto.EventModel;
+import io.mosip.kernel.core.logger.spi.Logger;
 
 /**
  * The callback delegate for handling partner service update
@@ -17,11 +23,26 @@ import io.mosip.idrepository.core.dto.EventModel;
  */
 @Component
 public class PartnerServiceCallbackControllerDelegate {
+	
+	private static final Logger logger = IdaLogger.getLogger(PartnerServiceManager.class);
 
 	@Autowired
 	private PartnerServiceManager partnerManager;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+
 
 	public void updatePartnerInfo(@RequestBody EventModel eventModel) {
+		
+		try {
+			logger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(), "handling event: \n",
+					mapper.writeValueAsString(eventModel));
+		} catch (JsonProcessingException e) {
+			// Skipping the error
+		}
+
 		Map<String, Object> data = eventModel.getEvent().getData();
 		if (data.containsKey("partnerId") && data.get("partnerId") instanceof String) {
 			partnerManager.evictPartnerBasedOnPartnerId((String) data.get("partnerId"));
