8d8194b58d3d58c2cb7b2beb75c2fa9c63790906
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2021-01-21 21:32:08 +0530

    Installing biosdk with env variables

--- authentication/authentication-internal-service/configure_start.sh
+++ authentication/authentication-internal-service/configure_start.sh
@@ -8,9 +8,11 @@ wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifac
 wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/deployment/docker/id-authentication/configure_hsmclient.sh -O configure_hsmclient.sh
 
 echo "Installating pre-requisites.."
-source ./configure_biosdk.sh
-cd ~
-source ./configure_hsmclient.sh
+chmod +x configure_biosdk.sh
+./configure_biosdk.sh
+
+chmod +x configure_hsmclient.sh
+./configure_hsmclient.sh
 
 echo "Installating pre-requisites completed."
 
--- authentication/authentication-otp-service/configure_start.sh
+++ authentication/authentication-otp-service/configure_start.sh
@@ -7,7 +7,9 @@ echo "Downloading pre-requisites install scripts"
 wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/deployment/docker/id-authentication/configure_hsmclient.sh -O configure_hsmclient.sh
 
 echo "Installating pre-requisites.."
-source ./configure_hsmclient.sh
+chmod +x configure_hsmclient.sh
+./configure_hsmclient.sh
+
 
 echo "Installating pre-requisites completed."
 
