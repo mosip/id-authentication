8fd9e81f22c751acc46e36b679eb2fe6a7bdc471
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2021-01-27 17:39:43 +0530

    Added directory for additional dependencies/jars.

--- docker/configure_biosdk.sh
+++ docker/configure_biosdk.sh
@@ -31,7 +31,7 @@ else
 fi
 
 echo "Attempting to install"
-cd ./$DIR_NAME && chmod +x install.sh && source install.sh
+cd ./$DIR_NAME && chmod +x install.sh && sudo ./install.sh
 echo "Installation complete"
 cd $work_dir
 
