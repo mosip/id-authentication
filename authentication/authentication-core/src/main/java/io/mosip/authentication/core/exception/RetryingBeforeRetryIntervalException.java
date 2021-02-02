43ae429d0e0bdd9d266e7e3bce547c05cf05b360
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-02-02 11:14:44 +0530

    added comment

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/exception/RetryingBeforeRetryIntervalException.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/exception/RetryingBeforeRetryIntervalException.java
@@ -1,5 +1,12 @@
 package io.mosip.authentication.core.exception;
 
+/**
+ * The Class RetryingBeforeRetryIntervalException - The exception to be thrown
+ * if try is performed before the retry interval. This is used to skip the job
+ * item with this exception.
+ * 
+ * @author Loganathan Sekar
+ */
 public class RetryingBeforeRetryIntervalException extends IdAuthenticationBaseException {
 
 	/**
