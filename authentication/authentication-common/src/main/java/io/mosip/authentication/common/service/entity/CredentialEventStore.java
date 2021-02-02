bbe512141030c6bb1eed5bf7c0230ddf45d322b4
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-02-02 10:59:37 +0530

    Added comments

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/entity/CredentialEventStore.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/entity/CredentialEventStore.java
@@ -12,60 +12,78 @@ import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Instantiates a new credential event store.
+ * @author Loganathan Sekar
+ */
 @NoArgsConstructor
 @Data
 @Table(name = "credential_event_store", schema = "ida")
 @Entity
 public class CredentialEventStore {
 	
+	/** The event id. */
 	@Id
 	@NotNull
 	@Column(name = "event_id")
 	private String eventId;
 	
+	/** The event topic. */
 	@NotNull
 	@Column(name = "event_topic")
 	private String eventTopic;
 	
+	/** The credential transaction id. */
 	@NotNull
 	@Column(name = "credential_transaction_id")
 	private String credentialTransactionId;
 	
+	/** The publisher. */
 	@Column(name = "publisher")
 	private String publisher;
 
+	/** The published on dtimes. */
 	@Column(name = "published_on_dtimes")
 	private LocalDateTime publishedOnDtimes;
 
+	/** The retry count. */
 	@Column(name = "retry_count")
 	private int retryCount;
 
+	/** The status code. */
 	@NotNull
 	@Size(max = 36)
 	@Column(name = "status_code")
 	private String statusCode;
 	
+	/** The event object. */
 	@NotNull
 	@Column(name = "event_object")
 	private String eventObject;
 
+	/** The cr by. */
 	@NotNull
 	@Column(name = "cr_by")
 	private String crBy;
 
+	/** The cr D times. */
 	@NotNull
 	@Column(name = "cr_dtimes")
 	private LocalDateTime crDTimes;
 
+	/** The upd by. */
 	@Column(name = "upd_by")
 	private String updBy;
 
+	/** The upd D times. */
 	@Column(name = "upd_dtimes")
 	private LocalDateTime updDTimes;
 
+	/** The is deleted. */
 	@Column(name = "is_deleted")
 	private boolean isDeleted;
 	
+	/** The del D times. */
 	@Column(name = "del_dtimes")
 	private LocalDateTime delDTimes;
 
--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/CredentialEventStoreRepository.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/CredentialEventStoreRepository.java
@@ -8,10 +8,20 @@ import org.springframework.stereotype.Repository;
 import io.mosip.authentication.common.service.entity.CredentialEventStore;
 import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 
+/**
+ * The Interface CredentialEventStoreRepository.
+ * @author Loganathan Sekar
+ */
 @Repository
 public interface CredentialEventStoreRepository extends BaseRepository<CredentialEventStore, String>
 {
 
+	/**
+	 * Find new or failed events.
+	 *
+	 * @param pageable the pageable
+	 * @return the page
+	 */
 	@Query(value = "SELECT * from credential_event_store where status_code in ('NEW', 'FAILED')", nativeQuery = true)
 	Page<CredentialEventStore> findNewOrFailedEvents(Pageable pageable);
 }
