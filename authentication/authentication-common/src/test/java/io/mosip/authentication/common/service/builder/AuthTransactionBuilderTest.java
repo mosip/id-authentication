c05edbc47dee861773d42c1f7c8b11a90083cebb
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-03-22 14:27:02 +0530

    Fixed unit test

--- authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/builder/AuthTransactionBuilderTest.java
+++ authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/builder/AuthTransactionBuilderTest.java
@@ -29,7 +29,7 @@ public class AuthTransactionBuilderTest {
 	
 	@Test(expected = IdAuthenticationBusinessException.class)
 	public void buildTest() throws IdAuthenticationBusinessException {
-		ReflectionTestUtils.setField(authTransactionBuilder, "otpRequestDTO", null);
+		ReflectionTestUtils.setField(authTransactionBuilder, "requestDTO", null);
 	authTransactionBuilder.build(env,null,null,null);
 	}
 	
