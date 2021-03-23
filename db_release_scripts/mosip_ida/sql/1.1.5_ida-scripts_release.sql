7a290bae095900aaca39a4248055847bbaa55219
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2021-03-23 12:32:54 +0530

    Merge 1.1.5 to develop 2 (#364)
    * Mosip 11734 fix auth txn for validation failures 1.1.5 (#350)
    
    * Initial commit
    
    * MOSIP-11734-fix-in-auth transaction for exceptions
    
    * Test case fix
    
    * Fix in storing auth txn for exception
    
    * Fix in storing auth txn for exception
    
    * Added comments
    
    * test fixes
    
    * Fixes in otp kyc auth txn store
    
    * Updated POM version
    
    * Fix in IDA for Partner validate response change (#353) (#354)
    
    * Apply 1.1.3.1 fixes 2 (#355) (#356)
    
    * updated versions to 1.1.3-SNAPSHOT (#268)
    
    * removed getpublic key api (#270)
    
    * adding new keymanager beans (#271)
    
    * adding new keymanager beans
    
    * adding new keymanager beans
    
    * change working dir after hsm client installation (#269)
    
    * [MOSIP-10252] fixed auth when cache is disabled (#276)
    
    * [MOSIP-10252] fixed auth when cache is disabled
    
    * {MOSIP-10152] updated test cases
    
    * remove set::env command (#277)
    
    * remove set::env command
    
    * fix allignment
    
    * remove all set-env
    
    * Encrypt KYC data using partner certificate (#278)
    
    * Encrypt KYC data using partner certificate
    
    * Refactored getErrors method
    
    * fixed test
    
    * fixed test
    
    * adding changes for release 1.1.3
    
    * updating for 1.1.3 release
    
    * setting raw level to the bdb info (#296)
    
    * [MOSIP-12030] Logger hotfix
    
    * [MOSIP-12030] updated logging
    
    * Corrections per review comments
    
    * Minor correction to logger
    
    * Organized import
    
    * setting raw level to the bdb info (#296)
    
    * corrected pom version
    
    * Removed duplicate methods of merge conflic
    
    * Removed duplicate methods of merge conflict
    
    * Organized imports
    
    * Fixed merge conflict
    
    * Corrected biomatcherutilfix
    
    Co-authored-by: Mandeep Dhiman <46880392+mandeepdhiman123@users.noreply.github.com>
    Co-authored-by: Manoj SP <43261486+manojsp12@users.noreply.github.com>
    Co-authored-by: Sasikumar Ganesan <gsasikumar@gmail.com>
    Co-authored-by: ckm007 <chandra.mishra@technoforte.co.in>
    Co-authored-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    Co-authored-by: Manoj SP <Manoj.SP@mindtree.com>
    
    Co-authored-by: Mandeep Dhiman <46880392+mandeepdhiman123@users.noreply.github.com>
    Co-authored-by: Manoj SP <43261486+manojsp12@users.noreply.github.com>
    Co-authored-by: Sasikumar Ganesan <gsasikumar@gmail.com>
    Co-authored-by: ckm007 <chandra.mishra@technoforte.co.in>
    Co-authored-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    Co-authored-by: Manoj SP <Manoj.SP@mindtree.com>
    
    * Mosip 11734 fix auth transaction history missing for validation (#361) (#362)
    
    * Fixed missing auth transaction for validation
    
    * Fixed unit test
    
    * Added missing file
    
    * MOSIP-12271 mosip_ida is_deleted not null changes reverted for 1.1.5 (#360)
    
    * MOSIP-12401 Set DML Flag=0 (#359)
    
    * Changed version to 1.2.0-SNAPSHOT
    
    Co-authored-by: Mandeep Dhiman <46880392+mandeepdhiman123@users.noreply.github.com>
    Co-authored-by: Manoj SP <43261486+manojsp12@users.noreply.github.com>
    Co-authored-by: Sasikumar Ganesan <gsasikumar@gmail.com>
    Co-authored-by: ckm007 <chandra.mishra@technoforte.co.in>
    Co-authored-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    Co-authored-by: Manoj SP <Manoj.SP@mindtree.com>
    Co-authored-by: rambhatt1591 <77657115+rambhatt1591@users.noreply.github.com>

--- db_release_scripts/mosip_ida/sql/1.1.5_ida-scripts_release.sql
+++ db_release_scripts/mosip_ida/sql/1.1.5_ida-scripts_release.sql
@@ -10,6 +10,7 @@
 -- Jan-2021		Ram Bhatt	    Set is_deleted flag to not null and default false
 -- Feb-2021		Ram Bhatt	    Added hotlist table in ida
 -- Feb-2021		Ram Bhatt	    Changed size of auth_type_code from 32 to 128
+-- Mar-2021		Ram Bhatt	    Reverting is_deleted not null changes
 ----------------------------------------------------------------------------------------------------
 \c mosip_ida sysadmin
 
@@ -29,29 +30,29 @@
 
 ----------------------------------------------------------------------------------------------------
 
---------- ------------ALTER TABLE SCRIPT DEPLOYMENT ------------------------------------------------
+--------- --------------ALTER TABLE SCRIPT DEPLOYMENT ------------------------------------------------
 
-ALTER TABLE ida.auth_transaction ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.identity_cache ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.key_policy_def_h ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.key_policy_def ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.key_store ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.key_alias ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.uin_auth_lock ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.otp_transaction ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.credential_event_store ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE ida.ca_cert_store ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.auth_transaction ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.identity_cache ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.key_policy_def_h ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.key_policy_def ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.key_store ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.key_alias ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.uin_auth_lock ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.otp_transaction ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.credential_event_store ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE ida.ca_cert_store ALTER COLUMN is_deleted SET NOT NULL;
 
-ALTER TABLE ida.auth_transaction ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.identity_cache ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.key_policy_def_h ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.key_policy_def ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.key_store ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.key_alias ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.uin_auth_lock ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.otp_transaction ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.credential_event_store ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE ida.ca_cert_store ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.auth_transaction ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.identity_cache ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.key_policy_def_h ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.key_policy_def ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.key_store ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.key_alias ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.uin_auth_lock ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.otp_transaction ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.credential_event_store ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE ida.ca_cert_store ALTER COLUMN is_deleted SET DEFAULT FALSE;
 -------------------------------------------------------------------------------------------------------
 --------------------------------------HOTLISTING TABLE DEPLOYMENT-----------------------------------------
 
--- db_scripts/mosip_ida/ddl/ida-credential_event_store.sql
+++ db_scripts/mosip_ida/ddl/ida-credential_event_store.sql
@@ -9,6 +9,7 @@
 -- Modified Date        Modified By         Comments / Remarks
 -- ------------------------------------------------------------------------------------------
 -- Jan-2021		Ram Bhatt	    Set is_deleted flag to not null and default false 
+-- Mar-2021		Ram Bhatt	    Reverting is_deleted not null changes
 -- ------------------------------------------------------------------------------------------
 
 -- object: ida.credential_event_store | type: TABLE --
@@ -26,7 +27,7 @@ CREATE TABLE ida.credential_event_store(
 	cr_dtimes timestamp NOT NULL,
 	upd_by character varying(256),
 	upd_dtimes timestamp,
-	is_deleted boolean NOT NULL DEFAULT FALSE,
+	is_deleted boolean DEFAULT FALSE,
 	del_dtimes timestamp,
 	CONSTRAINT pk_ces_id PRIMARY KEY (event_id)
 
