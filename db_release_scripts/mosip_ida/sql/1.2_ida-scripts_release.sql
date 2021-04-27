3f663a89f105aa6e371dedff3eaf6f28594bf0ea
rambhatt1591 <77657115+rambhatt1591@users.noreply.github.com>
2021-04-27 12:22:10 +0530

    MOSIP-13528 Added unlock_timestamp column in uin_auth_lock table (#382)
    * MOSIP-13528 Added unlock_timestamp column in uin_auth_lock table
    
    * MOSIP-13528 Changed unlock_timestamp to unlock_expiry_datetime column in uin_auth_lock table

---
+++ db_release_scripts/mosip_ida/sql/1.2_ida-scripts_release.sql
@@ -0,0 +1,18 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name	: mosip_ida
+-- Release Version 	: 1.2
+-- Purpose    		: Database Alter scripts for the release for ID Authentication DB.       
+-- Create By   		: Ram Bhatt
+-- Created Date		: Apr-2021
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- -------------------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------
+\c mosip_ida sysadmin
+
+ALTER TABLE ida.uin_auth_lock ADD COLUMN unlock_expiry_datetime timestamp;
+
+----------------------------------------------------------------------------------------------------------
+
+
