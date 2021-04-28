3d5361993fe008caeb13e674376563242d8f5d54
Manoj SP <Manoj.SP@mindtree.com>
2021-04-28 17:36:17 +0530

    [MOSIP-13502] updated test case

--- authentication/authentication-kyc-service/src/test/java/io/mosip/authentication/kyc/service/facade/KycFacadeImplTest.java
+++ authentication/authentication-kyc-service/src/test/java/io/mosip/authentication/kyc/service/facade/KycFacadeImplTest.java
@@ -47,7 +47,6 @@ import io.mosip.authentication.common.service.integration.TokenIdManager;
 import io.mosip.authentication.common.service.repository.UinEncryptSaltRepo;
 import io.mosip.authentication.common.service.repository.UinHashSaltRepo;
 import io.mosip.authentication.common.service.transaction.manager.IdAuthSecurityManager;
-import io.mosip.authentication.core.authtype.dto.AuthtypeStatus;
 import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
 import io.mosip.authentication.core.exception.IdAuthenticationDaoException;
 import io.mosip.authentication.core.indauth.dto.AuthRequestDTO;
@@ -69,6 +68,7 @@ import io.mosip.authentication.core.spi.indauth.match.IdInfoFetcher;
 import io.mosip.authentication.core.spi.indauth.service.KycService;
 import io.mosip.authentication.core.spi.notification.service.NotificationService;
 import io.mosip.authentication.core.spi.partner.service.PartnerService;
+import io.mosip.idrepository.core.dto.AuthtypeStatus;
 import io.mosip.kernel.templatemanager.velocity.builder.TemplateManagerBuilderImpl;
 
 /**
