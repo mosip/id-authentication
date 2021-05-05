2bd83e57bced1c4c5784e1b543e0a857243762ed
rambhatt1591 <77657115+rambhatt1591@users.noreply.github.com>
2021-05-05 12:53:19 +0530

    MOSIP-13600 Constraints change and new table (#402)

--- db_scripts/mosip_ida/ddl/ida-misp_license_data.sql
+++ db_scripts/mosip_ida/ddl/ida-misp_license_data.sql
@@ -1,30 +1,31 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_ida
--- Table Name 	: ida.partner_data
-
--- Purpose    	: 
+-- Table Name 	: ida.misp_license_data
+-- Purpose    	: misp_license_data : 
 --           
--- Create By   	: Ram Bhatt
+-- Created By   : Ram Bhatt
 -- Created Date	: Apr-2021
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- ------------------------------------------------------------------------------------------
--- 
+
 -- ------------------------------------------------------------------------------------------
--- object: ida.partner_data | type: TABLE --
--- DROP TABLE IF EXISTS ida.partner_data CASCADE;
-CREATE TABLE ida.partner_data (
-	partner_id character varying(36) NOT NULL,
-	partner_name character varying(128) NOT NULL,
-	certificate_data bytea ,
-	partner_status character varying(36) NOT NULL,
+
+-- DROP TABLE IF EXISTS ida.misp_license_data CASCADE;
+CREATE TABLE ida.misp_license_data (
+	misp_id character varying(36) NOT NULL,
+	license_key character varying(128) NOT NULL,
+	misp_commence_on timestamp NOT NULL,
+	misp_expires_on timestamp,
+	misp_status character varying(36) NOT NULL,
 	cr_by character varying(256) NOT NULL,
 	cr_dtimes timestamp NOT NULL,
 	upd_by character varying(256),
 	upd_dtimes timestamp,
-	is_deleted boolean DEFAULT false,
+	is_deleted bool DEFAULT false,
 	del_dtimes timestamp,
-	CONSTRAINT partner_data_pk PRIMARY KEY (partner_id)
+	CONSTRAINT misp_license_data_pk PRIMARY KEY (misp_id)
 
 );
 -- ddl-end --
+
