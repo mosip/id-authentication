115f91294d717e4015d3b57f9160680c7284cecd
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-05-10 19:21:52 +0530

    license_data release script fixed

--- db_release_scripts/mosip_ida/ddl/ida-misp_license_data.sql
+++ db_release_scripts/mosip_ida/ddl/ida-misp_license_data.sql
@@ -1,42 +1,31 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_ida
--- Table Name 	: ida.hotlisting_table
--- Purpose    	: Hotlisting Table : To store the list of blocked ids.
+-- Table Name 	: ida.misp_license_data
+-- Purpose    	: misp_license_data : 
 --           
 -- Created By   : Ram Bhatt
--- Created Date	: Feb-2021
+-- Created Date	: Apr-2021
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- ------------------------------------------------------------------------------------------
 
 -- ------------------------------------------------------------------------------------------
 
--- object: ida.hotlist_cache | type: TABLE --
-
--- DROP TABLE IF EXISTS ida.hotlist_cache CASCADE;
-CREATE TABLE ida.hotlist_cache (
-	id_hash character varying(128) NOT NULL,
-	id_type character varying(128) NOT NULL,
-	status character varying(64),
-	start_timestamp timestamp,
-	expiry_timestamp timestamp,
-	CONSTRAINT "pk_idHashidType" PRIMARY KEY (id_hash,id_type)
+-- DROP TABLE IF EXISTS ida.misp_license_data CASCADE;
+CREATE TABLE ida.misp_license_data (
+	misp_id character varying(36) NOT NULL,
+	license_key character varying(128) NOT NULL,
+	misp_commence_on timestamp NOT NULL,
+	misp_expires_on timestamp,
+	misp_status character varying(36) NOT NULL,
+	cr_by character varying(256) NOT NULL,
+	cr_dtimes timestamp NOT NULL,
+	upd_by character varying(256),
+	upd_dtimes timestamp,
+	is_deleted bool DEFAULT false,
+	del_dtimes timestamp,
+	CONSTRAINT misp_license_data_pk PRIMARY KEY (misp_id)
 
 );
 -- ddl-end --
-COMMENT ON TABLE ida.hotlist_cache IS E'UIN Authentication Lock: An individual is provided an option to lock or unlock any of the authentication types that are provided by the system. When an individual locks a particular type of authentication, any requests received by the system will be rejected. The details of the locked authentication types are stored in this table.';
--- ddl-end --
-COMMENT ON COLUMN ida.hotlist_cache.id_hash IS E'Vanilla hash of IdValue';
--- ddl-end --
-COMMENT ON COLUMN ida.hotlist_cache.id_type IS E'ID Type';
--- ddl-end --
-COMMENT ON COLUMN ida.hotlist_cache.status IS E'Status : Blocked/Unblocked';
--- ddl-end --
-COMMENT ON COLUMN ida.hotlist_cache.start_timestamp IS E'Start Timestamp';
--- ddl-end --
-COMMENT ON COLUMN ida.hotlist_cache.expiry_timestamp IS E'Lock End Datetime: End date and time when the UIN Authentication lock was released.';
--- ddl-end --
-ALTER TABLE ida.hotlist_cache OWNER TO sysadmin;
--- ddl-end --
 
-----------------------------------------------------------------------------------------------------------
