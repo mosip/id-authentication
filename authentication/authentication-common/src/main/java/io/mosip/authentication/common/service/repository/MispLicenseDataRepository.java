238ee92416106ce59c86130d2df4ddcc08325e62
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2021-05-13 19:12:23 +0530

    Fixed misp query (#415)

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/MispLicenseDataRepository.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/MispLicenseDataRepository.java
@@ -1,9 +1,13 @@
 package io.mosip.authentication.common.service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.mosip.authentication.common.service.entity.MispLicenseData;
 
 public interface MispLicenseDataRepository extends JpaRepository<MispLicenseData, String> {
+	
+	Optional<MispLicenseData> findByLicenseKey(String licenseKey);
 
 }
