879776f87876697d1cf90210cea8ad6d81fdf750
Manoj SP <43261486+manojsp12@users.noreply.github.com>
2021-06-28 20:53:21 +0530

    Mosip 14380 - Fixed auth type status temp unlock (#465)
    * [MOSIP-11882] updated partner event structure
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] removed temp event logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] updated partner service validation
    
    * [MOSIP-13847] updated partner service
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-14509] fixed validation of hotlist expiry time
    
    * [MOSIP-14509] updated method name
    
    * [MOSIP-14509] updated hotlist validation
    
    * [MOSIP-14509] fixed code for UrlEncodedUtils
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-13847] adde temp logging of event request for pmp events
    
    * [MOSIP-13847] reverted temp logging
    
    * [MOSIP-14509] resolved merge conflicts
    
    * [MOSIP-14509] updated hotlist validation
    
    * [MOSIP-14509] fixed code for UrlEncodedUtils
    
    * [MOSIP-14509] resolved merge conflict
    
    * [MOSIP-14509] fixed merge conflict
    
    * [MOSIP-14509] Fixed auto unlocking of hotlisted id and added cleanup job
    
    * [MOSIP-14509] updated hotlist cache repo
    
    * [MOSIP-14380] Fixed auth type status temp unlock

--- authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/impl/AuthtypeStatusImplTest.java
+++ authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/impl/AuthtypeStatusImplTest.java
@@ -1,11 +1,12 @@
 package io.mosip.authentication.common.service.impl;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -19,7 +20,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import io.mosip.authentication.common.service.impl.match.BioAuthType;
 import io.mosip.authentication.common.service.repository.AuthLockRepository;
-import io.mosip.authentication.common.service.transaction.manager.IdAuthSecurityManager;
 import io.mosip.authentication.core.authtype.dto.AuthtypeRequestDto;
 import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
 import io.mosip.authentication.core.indauth.dto.IdType;
@@ -32,50 +32,20 @@ public class AuthtypeStatusImplTest {
 
 	@InjectMocks
 	private AuthtypeStatusImpl authtypeStatusImpl;
-	
-	@Mock
-	private IdAuthSecurityManager securityManager;
-
-	@Mock
-	private IdServiceImpl idService;
 
 	@Mock
 	private AuthLockRepository authLockRepository;
-	
+
 	@Test
-	@Ignore
 	public void TestvalidfetchAuthtypeStatus() throws IdAuthenticationBusinessException {
 		AuthtypeRequestDto authtypeRequestDto = getAuthTypeRequestDto();
 		Map<String, Object> value = new HashMap<>();
 		value.put("uin", "9172985031");
-		Mockito.when(idService.processIdType(Mockito.anyString(), Mockito.anyString(), Mockito.anyBoolean(), Mockito.anyBoolean()))
-				.thenReturn(value);
-		Mockito.when(securityManager.hash(Mockito.anyString())).thenReturn("213213214325254326643");
-		
 		List<Object[]> valuelist = new ArrayList<>();
-		Object[] authtypeLockStatus = new Object[] {"bio-FMR", "y"};
+		Object[] authtypeLockStatus = new Object[] { "bio-FMR", "true", Timestamp.valueOf(LocalDateTime.now().plusMinutes(1)) };
 		valuelist.add(authtypeLockStatus);
 		Mockito.when(authLockRepository.findByToken(Mockito.anyString())).thenReturn(valuelist);
-//		List<AuthtypeStatus> authTypeStatus = authtypeStatusImpl.fetchAuthtypeStatus(authtypeRequestDto);
-	}
-
-	@Test
-	@Ignore
-	public void TestvalidfetchAuthtypeStatuswithParam() throws IdAuthenticationBusinessException {
-		Map<String, Object> value = new HashMap<>();
-		value.put("uin", "9172985031");
-		Mockito.when(idService.processIdType(Mockito.anyString(), Mockito.anyString(), Mockito.anyBoolean(), Mockito.anyBoolean()))
-				.thenReturn(value);
-		Mockito.when(securityManager.hash(Mockito.anyString())).thenReturn("213213214325254326643");
-
-		List<Object[]> valuelist = new ArrayList<>();
-		Object[] authtypeLockStatus = new Object[] {"bio-FMR", "y"};
-		valuelist.add(authtypeLockStatus);
-		authtypeLockStatus = new Object[] {"demo", "n"};
-		valuelist.add(authtypeLockStatus);
-		Mockito.when(authLockRepository.findByToken(Mockito.anyString())).thenReturn(valuelist);
-//		List<AuthtypeStatus> authTypeStatus = authtypeStatusImpl.fetchAuthtypeStatus("9172985031",
-//				IdType.UIN.getType());
+		List<AuthtypeStatus> authTypeStatus = authtypeStatusImpl.fetchAuthtypeStatus("");
 	}
 
 	private AuthtypeRequestDto getAuthTypeRequestDto() {
--- authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/impl/UpdateAuthtypeStatusServiceImpl.java
+++ authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/impl/UpdateAuthtypeStatusServiceImpl.java
@@ -1,4 +1,5 @@
 package io.mosip.authentication.internal.service.impl;
+
 import static io.mosip.authentication.core.constant.IdAuthConfigKeyConstants.AUTH_TYPE_STATUS_ACK_TOPIC;
 
 import java.time.LocalDateTime;
@@ -35,7 +36,7 @@ import io.mosip.kernel.core.util.DateUtils;
 @Component
 @Transactional
 public class UpdateAuthtypeStatusServiceImpl implements UpdateAuthtypeStatusService {
-	
+
 	private static Logger mosipLogger = IdaLogger.getLogger(UpdateAuthtypeStatusServiceImpl.class);
 
 	private static final String STAUTS_LOCKED = "LOCKED";
@@ -52,35 +53,36 @@ public class UpdateAuthtypeStatusServiceImpl implements UpdateAuthtypeStatusServ
 	/** The environment. */
 	@Autowired
 	private Environment environment;
-	
+
 	@Autowired
 	private AuthTypeStatusEventPublisher authTypeStatusEventPublisherManager;
 
 	/**
 	 * Update auth type status.
 	 *
-	 * @param tokenId the token id
+	 * @param tokenId            the token id
 	 * @param authTypeStatusList the auth type status list
-	 * @throws IdAuthenticationBusinessException the id authentication business exception
+	 * @throws IdAuthenticationBusinessException the id authentication business
+	 *                                           exception
 	 */
 	@Override
 	public void updateAuthTypeStatus(String tokenId, List<AuthtypeStatus> authTypeStatusList)
 			throws IdAuthenticationBusinessException {
 		List<Entry<String, AuthtypeLock>> entitiesForRequestId = authTypeStatusList.stream()
-				.map(authTypeStatus -> new SimpleEntry<>(authTypeStatus.getRequestId(), 
-						this.putAuthTypeStatus(authTypeStatus, tokenId) ))
+				.map(authTypeStatus -> new SimpleEntry<>(authTypeStatus.getRequestId(),
+						this.putAuthTypeStatus(authTypeStatus, tokenId)))
 				.collect(Collectors.toList());
 		List<AuthtypeLock> entities = entitiesForRequestId.stream().map(Entry::getValue).collect(Collectors.toList());
 		authLockRepository.saveAll(entities);
-		
+
 		entitiesForRequestId.stream().forEach(entry -> {
 			String requestId = entry.getKey();
-			if(requestId != null) {
+			if (requestId != null) {
 				AuthtypeLock authtypeLock = entry.getValue();
 				String status = Boolean.valueOf(authtypeLock.getStatuscode()) ? STAUTS_LOCKED : STATUS_UNLOCKED;
 				authTypeStatusEventPublisherManager.publishEvent(status, requestId, authtypeLock.getCrDTimes());
 			} else {
-				mosipLogger.error("requestId is null; Websub Notification for {} topic is not sent." , AUTH_TYPE_STATUS_ACK_TOPIC);
+				mosipLogger.error("requestId is null; Websub Notification for {} topic is not sent.", AUTH_TYPE_STATUS_ACK_TOPIC);
 			}
 		});
 	}
@@ -88,8 +90,8 @@ public class UpdateAuthtypeStatusServiceImpl implements UpdateAuthtypeStatusServ
 	/**
 	 * Put auth type status.
 	 *
-	 * @param authtypeStatus            the authtype status
-	 * @param token the token
+	 * @param authtypeStatus the authtype status
+	 * @param token          the token
 	 * @return the authtype lock
 	 */
 	private AuthtypeLock putAuthTypeStatus(AuthtypeStatus authtypeStatus, String token) {
@@ -104,9 +106,9 @@ public class UpdateAuthtypeStatusServiceImpl implements UpdateAuthtypeStatusServ
 		authtypeLock.setLockrequestDTtimes(currentDtime);
 		authtypeLock.setLockstartDTtimes(currentDtime);
 		if (Objects.nonNull(authtypeStatus.getMetadata())
-				&& authtypeStatus.getMetadata().containsKey(UNLOCK_EXP_TIMESTAMP)
-				&& authtypeStatus.getMetadata().get(UNLOCK_EXP_TIMESTAMP) instanceof LocalDateTime) {
-			authtypeLock.setUnlockExpiryDTtimes((LocalDateTime) authtypeStatus.getMetadata().get(UNLOCK_EXP_TIMESTAMP));
+				&& authtypeStatus.getMetadata().containsKey(UNLOCK_EXP_TIMESTAMP)) {
+			authtypeLock.setUnlockExpiryDTtimes(
+					DateUtils.parseUTCToLocalDateTime((String) authtypeStatus.getMetadata().get(UNLOCK_EXP_TIMESTAMP)));
 		}
 		authtypeLock.setStatuscode(Boolean.toString(authtypeStatus.getLocked()));
 		authtypeLock.setCreatedBy(environment.getProperty(IdAuthConfigKeyConstants.APPLICATION_ID));
@@ -114,6 +116,5 @@ public class UpdateAuthtypeStatusServiceImpl implements UpdateAuthtypeStatusServ
 		authtypeLock.setLangCode(environment.getProperty(IdAuthConfigKeyConstants.MOSIP_PRIMARY_LANGUAGE));
 		return authtypeLock;
 	}
-	
-	
+
 }
