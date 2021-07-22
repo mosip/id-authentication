bc998874d78c06071feed65d543ef31c3246c152
Manoj SP <Manoj.SP@mindtree.com>
2021-07-22 14:03:41 +0530

    [MOSIP-15641] added id change event websub init class

---
+++ authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/listener/InternalAuthIdChangeEventsWebSubInitializer.java
@@ -0,0 +1,38 @@
+package io.mosip.authentication.internal.service.listener;
+
+import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.mosip.authentication.common.service.websub.CacheUpdatingWebsubInitializer;
+import io.mosip.authentication.common.service.websub.impl.IdChangeEventsInitializer;
+
+/**
+ * The Class InternalAuthWebSubInitializer.
+ * 
+ * @author Manoj SP
+ */
+@Component
+public class InternalAuthIdChangeEventsWebSubInitializer extends CacheUpdatingWebsubInitializer {
+
+	/** The id change event initializer. */
+	@Autowired
+	private IdChangeEventsInitializer idChangeEventInitializer;
+
+	/**
+	 * Do init subscriptions.
+	 */
+	public int doInitSubscriptions() {
+		return webSubHelper.initSubscriber(idChangeEventInitializer);
+	}
+
+	/**
+	 * Do register topics.
+	 */
+	@Override
+	public int doRegisterTopics() {
+		webSubHelper.initRegistrar(idChangeEventInitializer);
+		return HttpStatus.SC_OK;
+	}
+
+}
