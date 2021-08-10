d8335886e149d238c92487350daaaaa62ff0aa85
Nagarjuna K <nagarjunabtechece@gmail.com>
2021-08-10 14:08:06 +0530

    OTP Service loading issue fixed

---
+++ authentication/authentication-otp-service/src/main/java/io/mosip/authentication/otp/service/config/OtpLangComparatorConfig.java
@@ -0,0 +1,15 @@
+package io.mosip.authentication.otp.service.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.mosip.authentication.common.service.config.LangComparatorConfig;
+
+/**
+ * Language comparator config.
+ * @author Nagarjuna
+ *
+ */
+@Configuration
+public class OtpLangComparatorConfig extends LangComparatorConfig {
+
+}
