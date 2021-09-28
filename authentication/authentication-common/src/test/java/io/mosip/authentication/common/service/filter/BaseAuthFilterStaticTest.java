142a8cb249137cc105b7e22f265bc843eb8b052b
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2021-09-28 20:47:34 +0530

    Mosip 17348 anonymous profile fix 2 (#550)
    * Setting year of birth in anonym prof
    
    * Minor refactoring
    
    * Mosip 16784 test without kernel core decoder 1 (#543)
    
    * WIP - using local encoder decoder implementation
    
    * Use native decoder
    
    * Removed test comment
    
    * Refactored method names
    
    * Removed unused method
    
    * Corrected jdoc
    
    Co-authored-by: Loganathan Sekar <m1047627@mindtree.com>
    # Conflicts:
    #	authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/filter/IdAuthFilter.java
    #	authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/IdInfoFetcherImpl.java
    #	authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/transaction/manager/IdAuthSecurityManager.java
    #	authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/filter/BaseAuthFilterStaticTest.java
    #	authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/util/BioMatcherUtilTest.java
    
    * Encoding decoding fix and anonymous profile fixes
    
    * DOB prop
    
    Co-authored-by: Loganathan S <loganathan001@gmail.com>

--- authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/filter/BaseAuthFilterStaticTest.java
+++ authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/filter/BaseAuthFilterStaticTest.java
@@ -8,22 +8,19 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import io.mosip.authentication.core.exception.IdAuthenticationAppException;
-import io.mosip.authentication.core.logger.IdaLogger;
-import io.mosip.kernel.core.logger.spi.Logger;
-import io.mosip.kernel.core.util.CryptoUtil;
-import io.mosip.kernel.logger.logback.factory.Logfactory;
+import io.mosip.authentication.core.util.CryptoUtil;
+
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ CryptoUtil.class, IdaLogger.class, Logfactory.class })
+@PrepareForTest(CryptoUtil.class)
 public class BaseAuthFilterStaticTest {
 
-	@Test(expected = IdAuthenticationAppException.class)
+	@Test(expected=IdAuthenticationAppException.class)
 	public void decodetest() throws IdAuthenticationAppException {
 		PowerMockito.mockStatic(CryptoUtil.class);
-		PowerMockito.mockStatic(IdaLogger.class);
-		String stringToDecode = "assad";
-		Mockito.when(CryptoUtil.decodeBase64(stringToDecode)).thenThrow(new IllegalArgumentException());
-		Mockito.when(IdaLogger.getLogger(Mockito.any())).thenReturn(Mockito.mock(Logger.class));
+		String stringToDecode = "YWJjZA==";
+		Mockito.when(CryptoUtil.decodeBase64Url(stringToDecode)).thenThrow(new IllegalArgumentException());
 		BaseAuthFilter.decode(stringToDecode);
 	}
 }
+
