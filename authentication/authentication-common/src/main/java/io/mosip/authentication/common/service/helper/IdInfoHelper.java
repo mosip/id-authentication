37edb0b969c59a0617d3490c27b8a5262c0e3f35
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2021-10-26 12:26:33 +0530

    Mosip 18119 auto allow new dynamic attributes for authentication (#598)
    * Fixes in demo auth dynamic attributes
    
    * Fixed error message for decoding failure
    
    * Fix in fetching postal code for auth anonym profile
    
    * WIP for dynamic attribute auto match
    
    * Allow new dynamic attrib for auth without mapping
    
    * Removed unwanted comment

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/helper/IdInfoHelper.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/helper/IdInfoHelper.java
@@ -22,16 +22,25 @@ import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.mosip.authentication.common.service.config.IDAMappingConfig;
+import io.mosip.authentication.common.service.impl.match.BioAuthType;
 import io.mosip.authentication.common.service.impl.match.DemoMatchType;
 import io.mosip.authentication.common.service.impl.match.IdaIdMapping;
+import io.mosip.authentication.common.service.util.AuthTypeUtil;
 import io.mosip.authentication.core.constant.IdAuthCommonConstants;
 import io.mosip.authentication.core.constant.IdAuthenticationErrorConstants;
 import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
 import io.mosip.authentication.core.indauth.dto.AuthRequestDTO;
+import io.mosip.authentication.core.indauth.dto.BioIdentityInfoDTO;
+import io.mosip.authentication.core.indauth.dto.DataDTO;
+import io.mosip.authentication.core.indauth.dto.IdentityDTO;
 import io.mosip.authentication.core.indauth.dto.IdentityInfoDTO;
+import io.mosip.authentication.core.indauth.dto.RequestDTO;
 import io.mosip.authentication.core.logger.IdaLogger;
 import io.mosip.authentication.core.spi.indauth.match.EntityValueFetcher;
 import io.mosip.authentication.core.spi.indauth.match.IdInfoFetcher;
@@ -42,8 +51,11 @@ import io.mosip.authentication.core.spi.indauth.match.MatchType;
 import io.mosip.authentication.core.spi.indauth.match.MatchType.Category;
 import io.mosip.authentication.core.spi.indauth.match.MatchingStrategy;
 import io.mosip.authentication.core.spi.indauth.match.MatchingStrategyType;
+import io.mosip.kernel.biometrics.constant.BiometricType;
+import io.mosip.kernel.biometrics.entities.SingleAnySubtypeType;
 import io.mosip.kernel.core.logger.spi.Logger;
 
+// TODO: Auto-generated Javadoc
 /**
  * Helper class to build Authentication request.
  *
@@ -64,8 +76,17 @@ public class IdInfoHelper {
 	/** The mosip logger. */
 	private static Logger mosipLogger = IdaLogger.getLogger(IdInfoHelper.class);
 	
+	/** The default identiy filter attributes. */
 	@Value("${" + IDA_DEFAULT_IDENTITY_FILTER_ATTRIBUTES + ":#{null}" + "}")
 	private String defaultIdentiyFilterAttributes;
+	
+	/** The object mapper. */
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	/** The env. */
+	@Autowired
+	private Environment env;
 
 	/**
 	 * Get Authrequest Info.
@@ -237,24 +258,22 @@ public class IdInfoHelper {
 	 */
 	public Map<String, String> getIdEntityInfoMap(MatchType matchType, Map<String, List<IdentityInfoDTO>> identityInfos,
 			String language, String idName) throws IdAuthenticationBusinessException {
-		List<String> propertyNames = getIdMappingValue(matchType.getIdMapping(), matchType);
-		List<String> filteredPropNames;
-		//If this is dynamic match type, filter it based on the id name
-		if (matchType.isDynamic()) {
-			filteredPropNames = getFilteredPropNamesForIdName(matchType, idName, propertyNames);
-		} else {
-			filteredPropNames = propertyNames;
-		}
-		
+		List<String> propertyNames = getPropertyNamesForMatchType(matchType, idName);
 		Map<String, String> identityValuesMap = new LinkedHashMap<>();
-		Map<String, String> identityValuesMapWithLang = getIdentityValuesMap(matchType, filteredPropNames, language, identityInfos);
-		Map<String, String> identityValuesMapWithoutLang = getIdentityValuesMap(matchType, filteredPropNames, null, identityInfos);
+		Map<String, String> identityValuesMapWithLang = getIdentityValuesMap(matchType, propertyNames, language, identityInfos);
+		Map<String, String> identityValuesMapWithoutLang = getIdentityValuesMap(matchType, propertyNames, null, identityInfos);
 		mergeNonNullValues(identityValuesMap, identityValuesMapWithLang);
 		mergeNonNullValues(identityValuesMap, identityValuesMapWithoutLang);
 		Map<String, Object> props = Map.of(IdInfoFetcher.class.getSimpleName(), idInfoFetcher);
 		return matchType.getEntityInfoMapper().apply(identityValuesMap, props);
 	}
 
+	/**
+	 * Merge non null values.
+	 *
+	 * @param identityValuesMap the identity values map
+	 * @param identityValuesMapWithoutLang the identity values map without lang
+	 */
 	private void mergeNonNullValues(Map<String, String> identityValuesMap, Map<String, String> identityValuesMapWithoutLang) {
 		identityValuesMapWithoutLang.entrySet()
 			.stream()
@@ -265,10 +284,11 @@ public class IdInfoHelper {
 	/**
 	 * This method returns the list of  data capture languages.
 	 * These are used to send the notifications in data capture languages.
-	 * @param matchType
-	 * @param identityInfos
-	 * @return
-	 * @throws IdAuthenticationBusinessException
+	 *
+	 * @param matchType the match type
+	 * @param identityInfos the identity infos
+	 * @return the data captured languages
+	 * @throws IdAuthenticationBusinessException the id authentication business exception
 	 */
 	public List<String> getDataCapturedLanguages(MatchType matchType, Map<String, List<IdentityInfoDTO>> identityInfos)
 			throws IdAuthenticationBusinessException {
@@ -281,12 +301,12 @@ public class IdInfoHelper {
 	
 	/**
 	 * Get the ID Entity Info for all languages for the given match type from the
-	 * identity infos map
-	 * 
-	 * @param matchType
-	 * @param identityInfos
-	 * @return
-	 * @throws IdAuthenticationBusinessException
+	 * identity infos map.
+	 *
+	 * @param matchType the match type
+	 * @param identityInfos the identity infos
+	 * @return the id entity info
+	 * @throws IdAuthenticationBusinessException the id authentication business exception
 	 */
 	public Map<String, List<IdentityInfoDTO>> getIdEntityInfo(MatchType matchType, Map<String, List<IdentityInfoDTO>> identityInfos)
 			throws IdAuthenticationBusinessException {
@@ -296,27 +316,6 @@ public class IdInfoHelper {
 		return propertyNames.stream().collect(Collectors.toMap(Function.identity(), prop -> mappedIdEntity.get(prop).getValue()));
 	}
 
-	/**
-	 * Gets the filtered prop names for id name.
-	 *
-	 * @param matchType the match type
-	 * @param idName the id name
-	 * @param propertyNames the property names
-	 * @return the filtered prop names for id name
-	 */
-	private List<String> getFilteredPropNamesForIdName(MatchType matchType, String idName, List<String> propertyNames) {
-		List<String> filteredPropNames = propertyNames;
-		if(idName != null) {
-			Optional<IdMapping> idMapping = IdMapping.getIdMapping(idName, IdaIdMapping.values(), idMappingConfig);
-			if(idMapping.isPresent()) {
-				List<String> idEntityNames = idMapping.get().getMappingFunction().apply(idMappingConfig, matchType);
-				if(!idEntityNames.isEmpty()) {
-					filteredPropNames = propertyNames.stream().filter(idEntityNames::contains).collect(Collectors.toList());
-				}
-			}
-		}
-		return filteredPropNames;
-	}
 
 	/**
 	 * Match id data.
@@ -546,7 +545,6 @@ public class IdInfoHelper {
 	/**
 	 * Gets the dynamic entity info for ID Name.
 	 *
-	 * @param matchType the match type
 	 * @param filteredIdentityInfo the filtered identity info
 	 * @param langCode the lang code
 	 * @param idName the id name
@@ -567,8 +565,9 @@ public class IdInfoHelper {
 	}
 	
 	/**
-	 * Gets the default identity filter attributes from configuration
-	 * @return
+	 * Gets the default identity filter attributes from configuration.
+	 *
+	 * @return the default filter attributes
 	 */
 	public Set<String> getDefaultFilterAttributes() {
 		return Optional.ofNullable(defaultIdentiyFilterAttributes).stream()
@@ -578,29 +577,221 @@ public class IdInfoHelper {
 	}
 	
 	/**
-	 * Method will bring matching id values for given input demo attributes
-	 * @param defaultFilterAttributes
-	 * @return
-	 * @throws IdAuthenticationBusinessException
+	 * Builds the demo attribute filters.
+	 *
+	 * @param authRequestDTO the auth request DTO
+	 * @return the sets the
+	 * @throws IdAuthenticationBusinessException the id authentication business exception
 	 */
-	public Set<String> getAttributesFromMatchInput(Set<MatchInput> defaultFilterAttributes) throws IdAuthenticationBusinessException {
-		Set<String> demoFilterAttributes = new HashSet<String>();
-		for (MatchInput matchInput : defaultFilterAttributes) {
-			try {
-				List<String> propertyNames = getIdMappingValue(matchInput.getMatchType().getIdMapping(),
-						matchInput.getMatchType());
-				if (!propertyNames.isEmpty()) {
-					demoFilterAttributes.addAll(propertyNames);
+	@SuppressWarnings("unchecked")
+	public Set<String> buildDemoAttributeFilters(AuthRequestDTO authRequestDTO)
+			throws IdAuthenticationBusinessException {
+		Set<String> defaultFilterAttributes = getDefaultFilterAttributes();
+		Set<String> demoFilterAttributes = new HashSet<>(defaultFilterAttributes);
+		if (AuthTypeUtil.isDemo(authRequestDTO)) {
+			IdentityDTO demographics = authRequestDTO.getRequest().getDemographics();
+			Set<String> demoAttributesFromReq = new HashSet<>();
+			//Add mapped attributes
+			Map<String, Object> demoMap = objectMapper.convertValue(demographics, Map.class);
+			List<String> inputMappedAttributes = demoMap.entrySet()
+														.stream()
+														.filter(entry -> entry.getValue() != null)
+														.map(Entry::getKey)
+														.collect(Collectors.toList());
+			for (String attrib : inputMappedAttributes) {
+				if(!attrib.equals(IdAuthCommonConstants.METADATA)) {
+					demoAttributesFromReq.addAll(getPropertyNamesForIdName(attrib, false));
 				}
-			} catch (IdAuthenticationBusinessException e) {
-				mosipLogger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(),
-						IdAuthCommonConstants.VALIDATE,
-						"IdMapping config is Invalid for Type -" + matchInput.getMatchType().getCategory().getType());
-				throw new IdAuthenticationBusinessException(
-						IdAuthenticationErrorConstants.UNABLE_TO_PROCESS.getErrorCode(),
-						IdAuthenticationErrorConstants.UNABLE_TO_PROCESS.getErrorMessage());
 			}
+			
+			//Add dynamic attributes
+			Map<String, Object> dynamicAttributes = demographics.getMetadata();
+			if(dynamicAttributes != null && !dynamicAttributes.isEmpty()) {
+				Set<String> inputUnmappedAttributes = dynamicAttributes.keySet();
+				for (String attrib : inputUnmappedAttributes) {
+					if(dynamicAttributes.get(attrib) != null) {
+						demoAttributesFromReq.addAll(getPropertyNamesForIdName(attrib, true));
+					}
+				}
+			}
+			
+			demoFilterAttributes.addAll(demoAttributesFromReq);
 		}
 		return demoFilterAttributes;
+	}
+	
+	/**
+	 * To build the bio filters. 
+	 * These are used to decrypt only required bio attributes
+	 *
+	 * @param authRequestDTO the auth request DTO
+	 * @return the sets the
+	 */
+	public Set<String> buildBioFilters(AuthRequestDTO authRequestDTO) {
+		Set<String> bioFilters = new HashSet<String>();
+		if (AuthTypeUtil.isBio(authRequestDTO)) {
+			if (AuthTransactionHelper.isFingerAuth(authRequestDTO, env)) {
+				List<BioIdentityInfoDTO> bioFingerInfo = getBioIds(authRequestDTO, BioAuthType.FGR_IMG.getType());
+				if (!bioFingerInfo.isEmpty()) {
+					List<DataDTO> bioFingerData = bioFingerInfo.stream().map(BioIdentityInfoDTO::getData)
+							.collect(Collectors.toList());
+					// for UNKNOWN getting all the subtypes
+					if(bioFingerData.stream().anyMatch(bio->bio.getBioSubType().equals(IdAuthCommonConstants.UNKNOWN_BIO))) {
+						bioFilters.addAll(getBioSubTypes(BiometricType.FINGER));
+					}else {
+						bioFilters.addAll(
+								bioFingerData.stream().map(bio -> (bio.getBioType() + "_" + bio.getBioSubType()))
+										.collect(Collectors.toList()));
+					}
+				}
+			}
+
+			if (AuthTransactionHelper.isIrisAuth(authRequestDTO, env)) {
+				List<BioIdentityInfoDTO> bioIrisInfo = getBioIds(authRequestDTO, BioAuthType.IRIS_IMG.getType());
+				if (!bioIrisInfo.isEmpty()) {
+					List<DataDTO> bioIrisData = bioIrisInfo.stream().map(BioIdentityInfoDTO::getData)
+							.collect(Collectors.toList());
+					// for UNKNOWN getting all the subtypes
+					if(bioIrisData.stream().anyMatch(bio->bio.getBioSubType().equals(IdAuthCommonConstants.UNKNOWN_BIO))) {
+						bioFilters.addAll(getBioSubTypes(BiometricType.IRIS));
+					}else {
+						bioFilters.addAll(
+								bioIrisData.stream().map(bio -> (bio.getBioType() + "_" + bio.getBioSubType()))
+										.collect(Collectors.toList()));
+					}
+				}
+			}
+			if (AuthTransactionHelper.isFaceAuth(authRequestDTO, env)) {
+				List<BioIdentityInfoDTO> bioFaceInfo = getBioIds(authRequestDTO, BioAuthType.FACE_IMG.getType());
+				List<DataDTO> bioFaceData = bioFaceInfo.stream().map(BioIdentityInfoDTO::getData)
+						.collect(Collectors.toList());
+				if (!bioFaceData.isEmpty()) {
+					bioFilters.addAll(bioFaceData.stream().map(bio -> (bio.getBioType()))
+							.collect(Collectors.toList()));
+				}
+			}
+			return bioFilters;
+		}
+		return Collections.emptySet();
+	}
+
+	/**
+	 * Gets the bio ids.
+	 *
+	 * @param authRequestDTO the auth request DTO
+	 * @param type the type
+	 * @return the bio ids
+	 */
+	private List<BioIdentityInfoDTO> getBioIds(AuthRequestDTO authRequestDTO, String type) {
+		List<BioIdentityInfoDTO> identity = Optional.ofNullable(authRequestDTO.getRequest())
+				.map(RequestDTO::getBiometrics).orElseGet(Collections::emptyList);
+		if (!identity.isEmpty()) {
+			return identity.stream().filter(Objects::nonNull)
+					.filter(bioId -> bioId.getData().getBioType().equalsIgnoreCase(type)).collect(Collectors.toList());
+		}
+		return Collections.emptyList();
+	}
+	
+	/**
+	 * to get all bio subtypes for given type.
+	 *
+	 * @param type the type
+	 * @return the bio sub types
+	 */
+	public List<String> getBioSubTypes(BiometricType type) {
+		switch (type) {
+		case FINGER:
+			return getFingerSubTypes(type);
+		case IRIS:
+			return getIrisSubTypes(type);
+		default:
+			return Collections.emptyList();
+		}
+	}
+	
+	/**
+	 * Construct and returns finger type along with all the sub types.
+	 *
+	 * @param type the type
+	 * @return the finger sub types
+	 */
+	private List<String> getFingerSubTypes(BiometricType type){
+		return List.of(type.value() + "_" + SingleAnySubtypeType.LEFT.value() + " " + SingleAnySubtypeType.THUMB.value(),
+				type.value() + "_" + SingleAnySubtypeType.LEFT.value() + " " + SingleAnySubtypeType.INDEX_FINGER.value(),
+				type.value() + "_" + SingleAnySubtypeType.LEFT.value() + " " + SingleAnySubtypeType.MIDDLE_FINGER.value(),
+				type.value() + "_" + SingleAnySubtypeType.LEFT.value() + " " + SingleAnySubtypeType.RING_FINGER.value(),
+				type.value() + "_" + SingleAnySubtypeType.LEFT.value() + " " + SingleAnySubtypeType.LITTLE_FINGER.value(),
+				type.value() + "_" + SingleAnySubtypeType.RIGHT.value() + " " + SingleAnySubtypeType.THUMB.value(),
+				type.value() + "_" + SingleAnySubtypeType.RIGHT.value() + " " + SingleAnySubtypeType.INDEX_FINGER.value(),
+				type.value() + "_" + SingleAnySubtypeType.RIGHT.value() + " " + SingleAnySubtypeType.MIDDLE_FINGER.value(),
+				type.value() + "_" + SingleAnySubtypeType.RIGHT.value() + " " + SingleAnySubtypeType.RING_FINGER.value(),
+				type.value() + "_" + SingleAnySubtypeType.RIGHT.value() + " " + SingleAnySubtypeType.LITTLE_FINGER.value());
+	}
+	
+	/**
+	 * Construct and returns finger type along with all the sub types.
+	 *
+	 * @param type the type
+	 * @return the iris sub types
+	 */
+	private List<String> getIrisSubTypes(BiometricType type){
+		return List.of(type.value() + "_" + SingleAnySubtypeType.LEFT.value(),
+				type.value() + "_" + SingleAnySubtypeType.RIGHT.value());
+	}
+	
+	/**
+	 * Gets the property names for match type.
+	 *
+	 * @param matchType the match type
+	 * @param idName the id name
+	 * @return the property names for match type
+	 */
+	public List<String> getPropertyNamesForMatchType(MatchType matchType, String idName) {
+		String propertyName = idName != null ? idName : matchType.getIdMapping().getIdname();
+		List<String> propertyNames;
+		if (!matchType.isDynamic()) {
+			if(matchType.getIdMapping().getIdname().equals(idName)) {
+				try {
+					propertyNames = getIdMappingValue(matchType.getIdMapping(), matchType);
+				} catch (IdAuthenticationBusinessException e) {
+					mosipLogger.debug(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(),
+							IdAuthCommonConstants.VALIDATE, "Ignoring : IdMapping config is Invalid for Type -" + matchType);
+					propertyNames = List.of();
+				}
+			} else {
+				propertyNames = List.of();
+			}
+
+		} else {
+			if (idMappingConfig.getDynamicAttributes().containsKey(propertyName)) {
+				propertyNames = idMappingConfig.getDynamicAttributes().get(propertyName);
+			} else {
+				propertyNames = List.of(idName);
+			}
+		}
+		return propertyNames;
+	}
+	
+	/**
+	 * Gets the property names for id name.
+	 *
+	 * @param idName the id name
+	 * @param isDynamic the is dynamic
+	 * @return the property names for id name
+	 * @throws IdAuthenticationBusinessException the id authentication business exception
+	 */
+	public List<String> getPropertyNamesForIdName(String idName, boolean isDynamic)
+			throws IdAuthenticationBusinessException {
+		DemoMatchType[] demoMatchTypes = DemoMatchType.values();
+		List<String> propNames = new ArrayList<>();
+		for (DemoMatchType demoMatchType : demoMatchTypes) {
+			if(isDynamic == demoMatchType.isDynamic()) {
+				List<String> propertyNamesForMatchType = this.getPropertyNamesForMatchType(demoMatchType, idName);
+				if(!propertyNamesForMatchType.isEmpty()) {
+					propNames.addAll(propertyNamesForMatchType);
+				}
+			}
+		}
+		return propNames;
 	}
 }
