cedacbce2a32a1353745c2e7d0f699291cdc21f6
Loganathan Sekar <loganathan.sekar@mindtree.com>
2021-11-16 16:43:20 +0530

    added jdoc

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/spi/indauth/match/MatchInput.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/spi/indauth/match/MatchInput.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.Objects;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -26,10 +25,44 @@ import lombok.ToString;
  * @param matchProperties the match properties
  * @param language the language
  */
+
+/**
+ * Gets the language.
+ *
+ * @return the language
+ */
 @Getter 
+
+/**
+ * Sets the language.
+ *
+ * @param language the new language
+ */
 @Setter 
+
+/**
+ * Instantiates a new match input.
+ */
 @RequiredArgsConstructor 
+
+/**
+ * To string.
+ *
+ * @return the java.lang. string
+ */
 @ToString 
+
+/**
+ * Instantiates a new match input.
+ *
+ * @param authType the auth type
+ * @param idName the id name
+ * @param matchType the match type
+ * @param matchStrategyType the match strategy type
+ * @param matchValue the match value
+ * @param matchProperties the match properties
+ * @param language the language
+ */
 @AllArgsConstructor
 public class MatchInput {
 
@@ -54,11 +87,22 @@ public class MatchInput {
 	/** The language. */
 	private String language;
 
+	/**
+	 * Hash code.
+	 *
+	 * @return the int
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(authType, idName, language, matchType);
 	}
 
+	/**
+	 * Equals.
+	 *
+	 * @param obj the obj
+	 * @return true, if successful
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
