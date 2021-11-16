d726d6548677a90514c9245def10f71ebf9d5120
Vipul Dhurve <vipuldhurve.13@gmail.com>
2021-11-16 18:52:42 +0530

    CredentialServiceImplTest update

--- authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/impl/idevent/IdChangeEventHandlerServiceImplTest.java
+++ authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/impl/idevent/IdChangeEventHandlerServiceImplTest.java
@@ -13,7 +13,6 @@ import org.springframework.web.context.WebApplicationContext;
 @WebMvcTest
 @ContextConfiguration(classes = { TestContext.class, WebApplicationContext.class })
 public class IdChangeEventHandlerServiceImplTest {
-//	
 //	@Autowired
 //	private Environment env;
 //	
