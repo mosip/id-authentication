62cd75b42f045612d892930d7b0da4215576ae5d
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2021-11-16 21:08:22 +0530

    Removed unnecessary db call (#645)

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/OtpTxnRepository.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/OtpTxnRepository.java
@@ -1,16 +1,24 @@
 package io.mosip.authentication.common.service.repository;
 
+import java.util.Optional;
+
 import io.mosip.authentication.common.service.entity.OtpTransaction;
 import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 
 /**
- * @author Manoj SP
+ * The Interface OtpTxnRepository.
  *
+ * @author Manoj SP
  */
 public interface OtpTxnRepository extends BaseRepository<OtpTransaction, String> {
 	
-	Boolean existsByOtpHashAndStatusCode(String otpHash, String statusCode);
-	
-	OtpTransaction findByOtpHashAndStatusCode(String otpHash, String statusCode);
+	/**
+	 * Find by otp hash and status code.
+	 *
+	 * @param otpHash the otp hash
+	 * @param statusCode the status code
+	 * @return the optional
+	 */
+	Optional<OtpTransaction> findByOtpHashAndStatusCode(String otpHash, String statusCode);
 	
 }
