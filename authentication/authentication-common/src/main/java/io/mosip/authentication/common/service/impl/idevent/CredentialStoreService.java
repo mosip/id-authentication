6b741d743fc661e621b6f77573ca7be3f2211462
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2021-11-26 15:20:14 +0530

    Fixed salt id type

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/idevent/CredentialStoreService.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/idevent/CredentialStoreService.java
@@ -402,7 +402,7 @@ public class CredentialStoreService {
 	 * @param salt   the salt
 	 */
 	private void saveSalt(String modulo, String salt) {
-		Long saltModulo = Long.valueOf(modulo);
+		Integer saltModulo = Integer.valueOf(modulo);
 		if (!uinHashSaltRepo.existsById(saltModulo)) {
 			UinHashSalt saltEntity = new UinHashSalt();
 			saltEntity.setId(saltModulo);
