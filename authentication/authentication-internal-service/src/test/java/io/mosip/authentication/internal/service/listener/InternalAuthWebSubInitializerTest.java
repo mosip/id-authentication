f676666cf107ab87fbddc7a310fbad89cf0dff3f
Vipul Dhurve <vipuldhurve.13@gmail.com>
2022-01-19 12:25:30 +0530

    Created Test class for InternalAuthWebSubInitializer => InternalAuthWebSubInitializerTest

--- authentication/authentication-internal-service/src/test/java/io/mosip/authentication/internal/service/listener/InternalAuthWebSubInitializerTest.java
+++ authentication/authentication-internal-service/src/test/java/io/mosip/authentication/internal/service/listener/InternalAuthWebSubInitializerTest.java
@@ -2,6 +2,7 @@ package io.mosip.authentication.internal.service.listener;
 
 import io.mosip.authentication.common.service.helper.WebSubHelper;
 import io.mosip.authentication.common.service.websub.impl.IdAuthFraudAnalysisEventPublisher;
+import org.apache.commons.lang3.builder.ReflectionDiffBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.context.WebApplicationContext;
 
 @WebMvcTest
@@ -26,13 +28,22 @@ public class InternalAuthWebSubInitializerTest {
     @Mock
     private IdAuthFraudAnalysisEventPublisher fraudEventPublisher;
 
+    /**
+     * This class tests the doInitSubscriptions method
+     */
     @Test
     public void doInitSubscriptionsTest(){
         internalAuthWebSubInitializer.doInitSubscriptions();
     }
 
+    /**
+     * This class tests the doRegisterTopics method
+     */
     @Test
     public void doRegisterTopicsTest(){
+        internalAuthWebSubInitializer.doRegisterTopics();
+//        when fraudEventPublisher is null
+        ReflectionTestUtils.setField(internalAuthWebSubInitializer, "fraudEventPublisher", null);
         internalAuthWebSubInitializer.doRegisterTopics();
     }
 }
