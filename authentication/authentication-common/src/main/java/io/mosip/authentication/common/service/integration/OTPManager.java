4ced2ec85b19c9905657f6801252bc3e40bd6bde
Vipul Dhurve <vipuldhurve.13@gmail.com>
2022-02-17 15:40:09 +0530

    - Fixed Null pointer exception response can be null in line 135

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/integration/OTPManager.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/integration/OTPManager.java
@@ -132,11 +132,12 @@ public class OTPManager {
 			RestRequestDTO restRequest = restRequestFactory.buildRequest(RestServicesConstants.OTP_GENERATE_SERVICE,
 					reqWrapper, ResponseWrapper.class);
 			ResponseWrapper<Map<String, String>> response = restHelper.requestSync(restRequest);
-			if ( Objects.nonNull(response) && response.getResponse().get("status").equals(USER_BLOCKED)) {
+			if ( response!=null && response.getResponse()!=null && response.getResponse().get("status").equals(USER_BLOCKED)) {
 				logger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(),
 						IdAuthenticationErrorConstants.BLOCKED_OTP_VALIDATE.getErrorCode(), USER_BLOCKED);
 				throw new IdAuthUncheckedException(IdAuthenticationErrorConstants.BLOCKED_OTP_VALIDATE);
 			}
+
 			return response.getResponse().get("otp");
 		} catch (IDDataValidationException e) {
 			logger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(), "generateOTP",
