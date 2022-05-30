3664541ddce3c5869a3fef75cc39e7f4f01f411c
kameshsr <47484458+kameshsr@users.noreply.github.com>
2022-05-30 10:53:39 +0530

    MOSIP-20406 Added partnerId in event. (#891)
    * MOSIP-20406 Added partnerId in event.
    
    * MOSIP-20406 Added partnerId in event.

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/websub/impl/AuthTypeStatusEventPublisher.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/websub/impl/AuthTypeStatusEventPublisher.java
@@ -3,6 +3,7 @@ package io.mosip.authentication.common.service.websub.impl;
 import static io.mosip.authentication.core.constant.IdAuthConfigKeyConstants.AUTH_TYPE_STATUS_ACK_TOPIC;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -23,10 +24,14 @@ public class AuthTypeStatusEventPublisher extends BaseWebSubEventsInitializer {
 
 	/** The Constant logger. */
 	private static final Logger logger = IdaLogger.getLogger(AuthTypeStatusEventPublisher.class);
+	private static final String OLV_PARTNER_ID = "olv_partner_id";
 
 	/** The credential status update topic. */
 	@Value("${" + AUTH_TYPE_STATUS_ACK_TOPIC + "}")
 	private String authTypeStatusAcknlowedgeTopic;
+
+	@Value("${ida-auth-partner-id}")
+	private String partnerId;
 	
 	/**
 	 * Do subscribe.
@@ -63,9 +68,14 @@ public class AuthTypeStatusEventPublisher extends BaseWebSubEventsInitializer {
 	public void publishEvent(String status, String requestId, LocalDateTime updatedDTimes) {
 		AuthTypeStatusUpdateAckEvent credentialStatusUpdateEvent = createEvent(requestId, status, updatedDTimes);
 		EventModel<AuthTypeStatusUpdateAckEvent> eventModel = webSubHelper.createEventModel(getTopic(), credentialStatusUpdateEvent);
+		if(eventModel != null) {
+			Map<String, Object> partnerIdMap = new java.util.HashMap<>();
+			partnerIdMap.put(OLV_PARTNER_ID, partnerId);
+			eventModel.getEvent().setData(partnerIdMap);
+		}
 		webSubHelper.publishEvent(getTopic(), eventModel);
 	}
-	
+
 	/**
 	 * Creates the credential status update event.
 	 *
