004a50520cdbd882f8dc8a6a8ecade4ee827ec31
kameshsr <kameshsr1338@gmail.com>
2022-08-18 18:16:38 +0530

    release file name changes.
    Signed-off-by: Deepesh Gurung <deepesh.gurung@technoforte.co.in>

--- db_release_scripts/mosip_ida/sql/1.2.0_revoke.sql
+++ db_release_scripts/mosip_ida/sql/1.2.0_revoke.sql
@@ -1,13 +1,30 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_ida
--- Release Version 	: 1.1.2
+-- Release Version 	: 1.1.5
 -- Purpose    		: Revoking Database Alter deployement done for release in ID Authentication DB.       
--- Create By   		: Sadanandegowda DM
--- Created Date		: Sep-2020
+-- Create By   		: Ram Bhatt
+-- Created Date		: Apr-2021
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
+-- Apr-2021		Ram Bhatt	    create tables to store partner details
+-- Sep-2021		Loganathan Sekar	    create anonymous_profile table
+-------------------------------------------------------------------------------------------------
 
 \c mosip_ida sysadmin
 
+ALTER TABLE ida.uin_auth_lock DROP COLUMN unlock_expiry_datetime;
+
 -- -------------------------------------------------------------------------------------------------
+
+
+DROP TABLE IF EXISTS ida.partner_data;
+DROP TABLE IF EXISTS ida.policy_data;
+DROP TABLE IF EXISTS ida.api_key_data;
+DROP TABLE IF EXISTS ida.partner_mapping;
+
+DROP TABLE IF EXISTS ida.misp_license_data;
+
+-----------------------------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS ida.anonymous_profile;
