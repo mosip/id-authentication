045a656140c9cb170ef3e3104c4fc67c2de0676c
Loganathan Sekar <loganathan.sekar@mindtree.com>
2022-08-18 20:53:30 +0530

    minor import correcion

--- authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/facade/AuthFacadeImplTest.java
+++ authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/facade/AuthFacadeImplTest.java
@@ -27,11 +27,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContext;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.context.TestPropertySource;;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
