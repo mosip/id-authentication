2d933c3500f1f4fe269efaaf52e3c6ec6a923c00
ckm007 <chandrakeshavmishra@gmail.com>
2022-10-14 22:22:58 +0530

    [MOSIP-23833] corrected release sql and added error handling

--- db_release_scripts/mosip_ida/sql/1.2.0_upgrade.sql
+++ db_release_scripts/mosip_ida/sql/1.2.0_upgrade.sql
@@ -22,25 +22,7 @@ ALTER TABLE ida.uin_auth_lock ADD COLUMN unlock_expiry_datetime timestamp;
 ALTER TABLE ida.identity_cache ADD COLUMN identity_expiry timestamp;
 --------------------------------------------------------------------------------------------------------------
 
-CREATE INDEX ind_akd_apkeyid ON ida.api_key_data (api_key_id);
-CREATE INDEX ind_pm_pid ON ida.partner_mapping (partner_id);
-CREATE INDEX ind_pd_pid ON ida.partner_data (partner_id);
-CREATE INDEX ind_mld_lk ON ida.misp_license_data (license_key);
-CREATE INDEX ind_pd_pyid ON ida.policy_data (policy_id);
-CREATE INDEX ind_reqtrnid_dtimes_tknid ON ida.auth_transaction (request_trn_id, request_dtimes, token_id, cr_dtimes, auth_type_code);
-CREATE INDEX ind_ces_id ON ida.credential_event_store (cr_dtimes);
-CREATE INDEX ind_hc_idhsh_etp ON ida.hotlist_cache (id_hash, expiry_timestamp);
-CREATE INDEX ind_id ON ida.identity_cache (id);
-CREATE INDEX ind_otphsh ON ida.otp_transaction (otp_hash,status_code);
-CREATE INDEX ind_ual_id ON ida.uin_auth_lock (token_id);
-CREATE INDEX ind_uhs_id ON ida.uin_hash_salt (id);
-
-
------------------------------------------------------------------------------------------------------------
-ALTER TABLE ida.key_alias ADD COLUMN cert_thumbprint character varying(100);
 ALTER TABLE ida.ca_cert_store ADD CONSTRAINT cert_thumbprint_unique UNIQUE (cert_thumbprint,partner_domain);
-
-
 
 --------------------------------------------------------------------------------------------------------------
 ALTER TABLE ida.key_alias ADD COLUMN uni_ident character varying(50);
@@ -55,7 +37,7 @@ ALTER TABLE ida.key_policy_def_h ADD COLUMN access_allowed character varying(102
 
 ALTER TABLE ida.uin_auth_lock ALTER COLUMN is_deleted SET DEFAULT FALSE;
 
-
 update ida.key_policy_def set pre_expire_days=90, access_allowed='NA' where app_id='ROOT';
 update ida.key_policy_def set pre_expire_days=30, access_allowed='NA' where app_id='BASE';
 update ida.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='IDA';
+
