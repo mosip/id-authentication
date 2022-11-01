e0a07b48a25afcd39ae6b657ba8f15ffe54b0a09
kameshsr <kameshsr1338@gmail.com>
2022-11-01 11:15:05 +0530

    MOSIP-21399 Added logger debug statement.

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/websub/impl/AuthTypeStatusEventPublisher.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/websub/impl/AuthTypeStatusEventPublisher.java
@@ -85,22 +85,29 @@ public class AuthTypeStatusEventPublisher extends BaseWebSubEventsInitializer {
 	}
 
 	public void publishEvent(List<AuthtypeStatus> authTypeStatusList){
+		logger.debug("List of Auth type status List- "+ authTypeStatusList);
 		AuthTypeStatusUpdateAckEvent credentialStatusUpdateEvent = new AuthTypeStatusUpdateAckEvent();
 		String requestId="";
 		if(!authTypeStatusList.isEmpty()){
 			requestId = authTypeStatusList.get(0).getRequestId();
 		}
+		logger.debug("RequestId- "+requestId);
 		credentialStatusUpdateEvent.setRequestId(requestId);
 		Map<String, Object> authTypes = new HashMap<>();
 		authTypes.put(AUTH_TYPES, authTypeStatusList);
+		logger.debug("AUTH TYPES- "+authTypes);
 		credentialStatusUpdateEvent.setData(authTypes);
-
+		logger.debug("credentialStatusUpdateEvent- "+credentialStatusUpdateEvent);
+		logger.debug("credentialStatusUpdateEvent Data-"+credentialStatusUpdateEvent.getData());
 		EventModel<AuthTypeStatusUpdateAckEvent> eventModel = webSubHelper.createEventModel(getTopic(), credentialStatusUpdateEvent);
+		logger.debug("EventModel- "+ eventModel);
 		if(eventModel != null) {
 			Map<String, Object> partnerIdMap = new java.util.HashMap<>();
 			partnerIdMap.put(OLV_PARTNER_ID, partnerId);
 			eventModel.getEvent().setData(partnerIdMap);
+			logger.debug("Event Model Data- "+eventModel.getEvent().getData());
 		}
+		logger.debug("Event Model- "+eventModel);
 		webSubHelper.publishEvent(getTopic(), eventModel);
 	}
 
