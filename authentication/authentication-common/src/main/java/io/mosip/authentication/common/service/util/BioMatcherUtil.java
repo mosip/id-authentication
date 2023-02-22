678ca48210d08a246cebf97a4dd3dea94ae2e01b
Neha2365 <110969715+Neha2365@users.noreply.github.com>
2023-02-22 16:45:25 +0530

    Changes in biomatcher Util for unknown data (#971)
    Co-authored-by: Neha Farheen <m1092365@mindtree.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/util/BioMatcherUtil.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/util/BioMatcherUtil.java
@@ -246,6 +246,7 @@ public class BioMatcherUtil {
 		}
 		String[] subTypes = Arrays.stream(idName.split(" "))
 				.filter(str -> !str.isEmpty())
+				.map(str -> str.replaceAll("\\d", ""))
 				.toArray(s -> new String[s]);
 		return new BioInfo(String.valueOf(type), singleType, subTypes);
 	}
