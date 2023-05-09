6335b0a1c1133871984b3fd9e38deef02bc5158c
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-05-09 08:09:34 +0530

    Update upgrade.sh

--- db_upgrade_scripts/mosip_ida/upgrade.sh
+++ db_upgrade_scripts/mosip_ida/upgrade.sh
@@ -1,60 +1,51 @@
-## Properties file
+#!/bin/bash
+
 set -e
 properties_file="$1"
-upgrade_version="$3"
-current_version="$2"
-     echo "Properties File Name - $properties_file"
-     echo "DB Upgrade Version - $upgrade_version"
-     echo "DB current version - $current_version"
+echo `date "+%m/%d/%Y %H:%M:%S"` ": $properties_file"
 if [ -f "$properties_file" ]
 then
-     echo "Property file \"$properties_file\" found."
+     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file \"$properties_file\" found."
     while IFS='=' read -r key value
     do
         key=$(echo $key | tr '.' '_')
          eval ${key}=\${value}
-   done < "$properties_file"
+    done < "$properties_file"
 else
-     echo "Property file not found, Pass property file name as argument."
-     exit 0
+     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file not found, Pass property file name as argument."
 fi
 
-if [ $# -ge 2 ] 
-then
-     echo "DB current version \"$current_version\" found."
-else
-     echo "DB current version not found, Pass current version as argument."
-     exit 0
-fi
-if [ $# -ge 3 ] 
-then
-     echo "DB upgrade version \"$upgrade_version\" found."
-else
-     echo "DB upgrade version not found, Pass upgrade version as argument."
-     exit 0
-fi
+echo "Current version: $CURRENT_VERSION"
+echo "UPGRADE version: $UPGRADE_VERSION"
+echo "Action: $ACTION"
 
-
-## Terminate existing connections
-echo "Terminating active connections" 
+# Terminate existing connections
+echo "Terminating active connections"
 CONN=$(PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -t -c "SELECT count(pg_terminate_backend(pg_stat_activity.pid)) FROM pg_stat_activity WHERE datname = '$MOSIP_DB_NAME' AND pid <> pg_backend_pid()";exit;)
 echo "Terminated connections"
 
-## Executing DB Upgrade scripts
-echo "Alter scripts deployment on $MOSIP_DB_NAME database from $current_version to $upgrade_version  started...."
-ALTER_SCRIPT_FILE="sql/${current_version}_to_${upgrade_version}_${ALTER_SCRIPT_FILENAME}"
-
-echo "Upgrade script considered for release deployment - $ALTER_SCRIPT_FILE"
-
-## Checking If Alter scripts are present
-echo "Checking if script $ALTER_SCRIPT_FILE is present"
-if [ -f "$ALTER_SCRIPT_FILE" ]
-then
-     echo "SQL file "$ALTER_SCRIPT_FILE" found."
+# Execute upgrade or rollback
+if [ "$ACTION" == "upgrade" ]; then
+  echo "Upgrading database from $CURRENT_VERSION to $UPGRADE_VERSION"
+  UPGRADE_SCRIPT_FILE="sql/${CURRENT_VERSION}_to_${UPGRADE_VERSION}_upgrade.sql"
+  if [ -f "$UPGRADE_SCRIPT_FILE" ]; then
+    echo "Executing upgrade script $UPGRADE_SCRIPT_FILE"
+    PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -a -b -f $UPGRADE_SCRIPT_FILE
+  else
+    echo "Upgrade script not found, exiting."
+    exit 1
+  fi
+elif [ "$ACTION" == "rollback" ]; then
+  echo "Rolling back database for $CURRENT_VERSION to $UPGRADE_VERSION"
+  REVOKE_SCRIPT_FILE="sql/${CURRENT_VERSION}_to_${UPGRADE_VERSION}_rollback.sql"
+  if [ -f "$REVOKE_SCRIPT_FILE" ]; then
+    echo "Executing rollback script $REVOKE_SCRIPT_FILE"
+    PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -a -b -f $REVOKE_SCRIPT_FILE
+  else
+    echo "rollback script not found, exiting."
+    exit 1
+  fi
 else
-     echo "SQL file not found, Since no SQL file present for \"$upgrade_version\"  hence exiting."
-     exit 0
+  echo "Unknown action: $ACTION, must be 'upgrade' or 'rollback'."
+  exit 1
 fi
-echo Applying upgrade changes
-
-PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -a -b -f $ALTER_SCRIPT_FILE
