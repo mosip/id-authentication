2b8e831bd721c096101948e86c0fa6af3261cb97
Neha Farheen <m1092365@mindtree.com>
2023-07-03 16:41:51 +0530

    condition changed

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/IdaUinHashSaltRepo.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/IdaUinHashSaltRepo.java
@@ -1,7 +1,6 @@
 package io.mosip.authentication.common.service.repository;
 import static io.mosip.authentication.core.constant.IdAuthCommonConstants.UIN_HASH_SALT;
 
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -25,7 +24,7 @@ public interface IdaUinHashSaltRepo extends JpaRepository<IdaUinHashSalt, Intege
 	 * @return String salt
 	 */
 
-	@Cacheable(value = UIN_HASH_SALT, key = "id", unless = "#result != null")
+	@Cacheable(value = UIN_HASH_SALT, key = "#id", unless = "#result != null")
 	@Query("select salt from IdaUinHashSalt where id = :id")
 	public String retrieveSaltById(@Param("id") Integer id);
 }
