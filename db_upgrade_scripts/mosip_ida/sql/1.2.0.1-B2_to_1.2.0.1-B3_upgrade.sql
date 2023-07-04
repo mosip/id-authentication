f5027c489272a56e4d4064a8f809c773b106f14e
Loganathan Sekar <m1047627@mindtree.com>
2023-07-04 06:05:36 +0530

    Removed sysadmin

--- db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B2_to_1.2.0.1-B3_upgrade.sql
+++ db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B2_to_1.2.0.1-B3_upgrade.sql
@@ -8,8 +8,6 @@
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
-\c mosip_ida sysadmin
-
 ALTER TABLE ida.kyc_token_store ADD request_trn_id character varying(64);
 
 CREATE TABLE ida.ident_binding_cert_store (
