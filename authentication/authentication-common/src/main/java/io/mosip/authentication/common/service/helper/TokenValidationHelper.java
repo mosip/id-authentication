83d24c1d5625dd3694bcb71be45900929394a90d
Mahammed Taheer <mohd.taheer@gmail.com>
2023-10-03 14:49:40 +0530

    [ES-313] Fixed blank attribute issue & language not available issue.

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/helper/TokenValidationHelper.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/helper/TokenValidationHelper.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import io.mosip.authentication.common.service.entity.KycTokenData;
 import io.mosip.authentication.common.service.entity.OIDCClientData;
@@ -31,14 +32,11 @@ import io.mosip.kernel.core.logger.spi.Logger;
  * @author Mahammed Taheer
  */
 
+@Component
 public class TokenValidationHelper {
     
     	/** The mosip logger. */
 	private static Logger mosipLogger = IdaLogger.getLogger(TokenValidationHelper.class);
-
-    @Value("${ida.idp.consented.individual_id.attribute.name:individual_id}")
-	private String consentedIndividualIdAttributeName;
-
 
     /** The Kyc Service */
 	@Autowired
@@ -46,13 +44,6 @@ public class TokenValidationHelper {
 
     @Autowired
 	private KycTokenDataRepository kycTokenDataRepo;
-
-	@Autowired
-	private IdInfoHelper idInfoHelper;
-
-    @Autowired
-	private OIDCClientDataRepository oidcClientDataRepo; 
-
 
     public KycTokenData findAndValidateIssuedToken(String tokenData, String oidcClientId, String reqTransactionId, 
         String idvidHash) throws IdAuthenticationBusinessException {
@@ -130,50 +121,5 @@ public class TokenValidationHelper {
 						IdAuthenticationErrorConstants.KYC_TOKEN_EXPIRED.getErrorCode(),
 						IdAuthenticationErrorConstants.KYC_TOKEN_EXPIRED.getErrorMessage());
 		}
-	}
-
-	public void mapConsentedAttributesToIdSchemaAttributes(List<String> consentAttributes, Set<String> filterAttributes, 
-			List<String> policyAllowedKycAttribs) throws IdAuthenticationBusinessException {
-
-		if(consentAttributes != null && !consentAttributes.isEmpty()) {
-			for (String attrib : consentAttributes) {
-				Collection<? extends String> idSchemaAttribute = idInfoHelper.getIdentityAttributesForIdName(attrib);
-				filterAttributes.addAll(idSchemaAttribute);
-			}
-			// removing individual id from consent if the claim is not allowed in policy.
-			if (!policyAllowedKycAttribs.contains(consentedIndividualIdAttributeName)) {
-				consentAttributes.remove(consentedIndividualIdAttributeName);
-			}
-		}
-	} 
-
-	public Set<String> filterByPolicyAllowedAttributes(Set<String> filterAttributes, List<String> policyAllowedKycAttribs) {
-		return policyAllowedKycAttribs.stream()
-							.filter(attribute -> filterAttributes.contains(attribute))
-							.collect(Collectors.toSet());
-	}
-
-	public String getKycExchangeResponseTime(BaseRequestDTO authRequestDTO) {
-		String dateTimePattern = EnvUtil.getDateTimePattern();
-		return IdaRequestResponsConsumerUtil.getResponseTime(authRequestDTO.getRequestTime(), dateTimePattern);
-	}
-
-	public List<String> filterAllowedUserClaims(String oidcClientId, List<String> consentAttributes) {
-		mosipLogger.info(IdAuthCommonConstants.IDA, this.getClass().getSimpleName(), "filterAllowedUserClaims", 
-					"Checking for OIDC client allowed userclaims");
-		Optional<OIDCClientData> oidcClientData = oidcClientDataRepo.findByClientId(oidcClientId);
-
-		List<String> oidcClientAllowedUserClaims = List.of(oidcClientData.get().getUserClaims())
-													   .stream()
-													   .map(String::toLowerCase)
-													   .collect(Collectors.toList());
-		if (consentAttributes.isEmpty()) {
-			return oidcClientAllowedUserClaims;
-		}
-
-		return consentAttributes.stream()
-							    .filter(claim -> oidcClientAllowedUserClaims.contains(claim.toLowerCase()))
-								.collect(Collectors.toList());
-
 	}
 }
