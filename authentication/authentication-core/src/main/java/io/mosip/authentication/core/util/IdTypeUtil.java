9b5c7596312a2bc94ae75b3ef0867660a291d57d
Anusha Sunkada <sunkadaeanusha@gmail.com>
2023-12-13 22:43:17 +0530

    ES-421 Updated credential_transaction_id column length in credential_event_store table (#1144)
    * ES-421 Update ida-credential_event_store.sql
    
    Signed-off-by: Anusha Sunkada <sunkadaeanusha@gmail.com>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * ES-421 renamed file and added alter stmt
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * ES-421
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * ES-421
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * Fixed review comment
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    ---------
    
    Signed-off-by: Anusha Sunkada <sunkadaeanusha@gmail.com>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/util/IdTypeUtil.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/util/IdTypeUtil.java
@@ -1,8 +1,14 @@
 package io.mosip.authentication.core.util;
 
+import java.util.Map;
 import java.util.Objects;
 
+import io.mosip.authentication.core.logger.IdaLogger;
+import io.mosip.kernel.core.exception.BaseUncheckedException;
+import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.kernel.core.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.mosip.authentication.core.constant.IdAuthCommonConstants;
@@ -10,6 +16,8 @@ import io.mosip.authentication.core.constant.IdAuthenticationErrorConstants;
 import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
 import io.mosip.authentication.core.indauth.dto.IdType;
 import io.mosip.kernel.core.idvalidator.exception.InvalidIDException;
+
+import static io.mosip.authentication.core.constant.IdAuthCommonConstants.SESSION_ID;
 
 /**
  * @author Manoj SP
@@ -19,8 +27,13 @@ import io.mosip.kernel.core.idvalidator.exception.InvalidIDException;
 @Component
 public class IdTypeUtil {
 
+	private static Logger mosipLogger = IdaLogger.getLogger(IdTypeUtil.class);
+
 	@Autowired
 	IdValidationUtil idValidator;
+
+	@Value("#{${mosip.ida.handle-types.regex}}")
+	private Map<String, String> handleTypesRegex;
 
 	public boolean validateUin(String uin) {
 		try {
@@ -44,11 +57,36 @@ public class IdTypeUtil {
 		}
 	}
 
+	public boolean validateHandle(String handle) {
+		try {
+			if(Objects.nonNull(handleTypesRegex)) {
+				if(StringUtils.isEmpty(handle))
+					return false;
+
+				int index = handle.lastIndexOf("@");
+				if(index <= 0)
+					return false;
+
+				String handleType = handle.substring(index);
+				if(!handleTypesRegex.containsKey(handleType))
+					return false;
+
+				return handle.matches(handleTypesRegex.get(handleType));
+			}
+		} catch (BaseUncheckedException e) {
+			mosipLogger.error(SESSION_ID, this.getClass().getSimpleName(), "VALIDATE_HANDLE",
+					"Failed to validate handle >> "+ e.getMessage());
+		}
+		return false;
+	}
+
 	public IdType getIdType(String id) throws IdAuthenticationBusinessException {
 		if (this.validateUin(id))
 			return IdType.UIN;
 		if (this.validateVid(id))
 			return IdType.VID;
+		if (this.validateHandle(id))
+			return IdType.HANDLE;
 		throw new IdAuthenticationBusinessException(
 				IdAuthenticationErrorConstants.INVALID_INPUT_PARAMETER.getErrorCode(),
 				String.format(IdAuthenticationErrorConstants.INVALID_INPUT_PARAMETER.getErrorMessage(),
--- db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B4_to_1.2.0.1_upgrade.sql
+++
@@ -1,1 +0,0 @@
-\echo 'Upgrade Queries not required for transition from 1.2.0.1-B4 to 1.2.0.1'
