03d93bf5b83f121005a2d56a5d09b66efd2f2d66
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2024-02-07 15:34:20 +0530

    Updated db scripts to index refid but not otphash
    Signed-off-by: Loganathan Sekar <Loganathan.Sekar@mindtree.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/OtpTxnRepository.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/OtpTxnRepository.java
@@ -13,14 +13,11 @@ import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 public interface OtpTxnRepository extends BaseRepository<OtpTransaction, String> {
 	
 	/**
-	 * Find by otp hash and status code.
+	 * Find first element by ref_id ordered by generated_dtimes in descending order.
 	 *
-	 * @param otpHash the otp hash
-	 * @param statusCode the status code
+	 * @param refIdHash the ref id hash
 	 * @return the optional
 	 */
-	Optional<OtpTransaction> findByOtpHashAndStatusCode(String otpHash, String statusCode);
-
 	Optional<OtpTransaction> findFirstByRefIdOrderByGeneratedDtimesDesc(String refIdHash);
 	
 }
