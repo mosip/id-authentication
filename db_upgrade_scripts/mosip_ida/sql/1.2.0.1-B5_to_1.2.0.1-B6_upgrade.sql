da3482cee5d3febc0c96cd9d9ee28d377f958fde
Neha2365 <110969715+Neha2365@users.noreply.github.com>
2024-02-28 12:50:48 +0530

    MOSIP-31759 renamed and change the version of file (#1209)
    Signed-off-by: Neha Farheen <neha.61092365@ltimindtree.com>
    Co-authored-by: Neha Farheen <neha.61092365@ltimindtree.com>

--- db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B5_to_1.2.0.1-B6_upgrade.sql
+++ db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B5_to_1.2.0.1-B6_upgrade.sql
@@ -1,4 +1,3 @@
-\echo 'Upgrade Queries not required for transition from 1.2.0.1-B5 to 1.2.0.1'
 
 ALTER TABLE ida.credential_event_store ALTER COLUMN credential_transaction_id type character varying(64);
 
