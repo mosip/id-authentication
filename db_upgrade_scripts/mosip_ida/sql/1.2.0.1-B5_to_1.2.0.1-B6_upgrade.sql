07528cdda950448fb9ecb90bc3dc32b996fc3ea3
Mohan E <mohanraj1715@gmail.com>
2024-03-13 19:19:14 +0530

    [MOSIP-31950] Updated 1.2.0.1-B5_to_1.2.0.1-B6_upgrade.sql and 1.2.0.1-B4_to_1.2.0.1-B5_upgrade.sql file. (#1216)
    * [MOSIP-31950] Updated 1.2.0.1-B5_to_1.2.0.1-B6_upgrade.sql
    
    Signed-off-by: Mohanraj209 <mohanraj1715@gmail.com>
    
    * [MOSIP-31950] Updated 1.2.0.1-B4_to_1.2.0.1-B5_upgrade.sql
    
    Signed-off-by: Mohanraj209 <mohanraj1715@gmail.com>
    
    ---------
    
    Signed-off-by: Mohanraj209 <mohanraj1715@gmail.com>

--- db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B5_to_1.2.0.1-B6_upgrade.sql
+++ db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B5_to_1.2.0.1-B6_upgrade.sql
@@ -1,7 +1,7 @@
+\c mosip_ida
 
 ALTER TABLE ida.credential_event_store ALTER COLUMN credential_transaction_id type character varying(64);
 
 DROP INDEX IF EXISTS ida.ind_otphsh;
 
 CREATE INDEX ind_refid ON ida.otp_transaction (ref_id,status_code);
-
