596dfcaaaa560fb14a7df949ff4eb991d05d89b3
Neha Farheen <neha.61092365@ltimindtree.com>
2024-03-15 14:09:39 +0530

    Ondemand changes move from develop to 1.2.0.2
    Signed-off-by: Neha Farheen <neha.61092365@ltimindtree.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/transaction/manager/IdAuthSecurityManager.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/transaction/manager/IdAuthSecurityManager.java
@@ -513,12 +513,12 @@ public class IdAuthSecurityManager {
 		return Tuples.of(CryptoUtil.encodeBase64Url(encryptedData.getT1()), CryptoUtil.encodeBase64Url(encryptedData.getT2()), digestAsPlainText(certificateThumbprint));
 	}
 
-	public byte[] asymmetricEncryption(byte[] dataToEncrypt, String partnerCertificate)
+	public String asymmetricEncryption(byte[] dataToEncrypt, String partnerCertificate)
 			throws IdAuthenticationBusinessException {
 		X509Certificate x509Certificate = getX509Certificate(partnerCertificate);
 		PublicKey publicKey = x509Certificate.getPublicKey();
 		byte[] encryptedData = cryptoCore.asymmetricEncrypt(publicKey, dataToEncrypt);
-		return encryptedData;
+		return CryptoUtil.encodeBase64(encryptedData);
 	}
 
 	/**
