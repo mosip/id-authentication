93975839b24abc9728006fd47f27cd7d83f9a11d
Neha Farheen <neha.61092365@ltimindtree.com>
2024-03-15 14:57:59 +0530

    Ondemand changes move from develop to 1.2.0.2
    Signed-off-by: Neha Farheen <neha.61092365@ltimindtree.com>

--- authentication/authentication-service/src/test/java/io/mosip/authentication/service/kyc/validator/KycAuthRequestValidatorTest.java
+++ authentication/authentication-service/src/test/java/io/mosip/authentication/service/kyc/validator/KycAuthRequestValidatorTest.java
@@ -22,6 +22,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.validation.BeanPropertyBindingResult;
@@ -34,10 +35,10 @@ import io.mosip.authentication.common.service.impl.match.DemoMatchType;
 import io.mosip.authentication.common.service.integration.MasterDataManager;
 import io.mosip.authentication.common.service.util.EnvUtil;
 import io.mosip.authentication.common.service.validator.AuthRequestValidator;
+import io.mosip.authentication.core.indauth.dto.EkycAuthRequestDTO;
 import io.mosip.authentication.core.indauth.dto.IdType;
 import io.mosip.authentication.core.indauth.dto.IdentityDTO;
 import io.mosip.authentication.core.indauth.dto.IdentityInfoDTO;
-import io.mosip.authentication.core.indauth.dto.EkycAuthRequestDTO;
 import io.mosip.authentication.core.indauth.dto.RequestDTO;
 import io.mosip.kernel.logger.logback.appender.RollingFileAppender;
 import io.mosip.kernel.pinvalidator.impl.PinValidatorImpl;
@@ -51,6 +52,7 @@ import io.mosip.kernel.pinvalidator.impl.PinValidatorImpl;
 @WebMvcTest
 @ContextConfiguration(classes = { TestContext.class, WebApplicationContext.class })
 @Import(EnvUtil.class)
+@TestPropertySource(locations="classpath:application.properties")
 public class KycAuthRequestValidatorTest {
 
 	@Mock
