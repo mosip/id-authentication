b464e7018f9f15ac7a8c404ca6631cdd7241a8f6
Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
2024-04-16 16:28:26 +0530

    changes done
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>

--- db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B5_to_1.2.0.1-B6_upgrade.sql
+++ db_upgrade_scripts/mosip_ida/sql/1.2.0.1-B5_to_1.2.0.1-B6_upgrade.sql
@@ -1,8 +1,7 @@
-\echo 'Upgrade Queries not required for transition from 1.2.0.1-B5 to 1.2.0.1'
+\c mosip_ida
 
 ALTER TABLE ida.credential_event_store ALTER COLUMN credential_transaction_id type character varying(64);
 
 DROP INDEX IF EXISTS ida.ind_otphsh;
 
 CREATE INDEX ind_refid ON ida.otp_transaction (ref_id,status_code);
-
