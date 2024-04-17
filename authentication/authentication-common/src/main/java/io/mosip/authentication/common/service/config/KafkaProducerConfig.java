3657ca67ce13fbcc75d677198fafcfd023c78faa
Neha Farheen <neha.61092365@ltimindtree.com>
2024-04-17 18:39:33 +0530

    kafka bean enable and disable
    Signed-off-by: Neha Farheen <neha.61092365@ltimindtree.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/config/KafkaProducerConfig.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/config/KafkaProducerConfig.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -23,6 +24,7 @@ import io.mosip.kernel.core.logger.spi.Logger;
  */
 
 @Configuration
+@ConditionalOnProperty(value = "mosip.ida.authentication.error.eventing.enabled", havingValue = "true", matchIfMissing = false)
 public class KafkaProducerConfig {
 	private static final Logger logger = IdaLogger.getLogger(KafkaProducerConfig.class);
 
