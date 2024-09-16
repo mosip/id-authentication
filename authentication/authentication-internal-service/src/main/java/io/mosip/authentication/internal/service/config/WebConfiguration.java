8c80c93ee85434f601ae7248a184d3bba1987fa6
kameshsr <kameshsr1338@gmail.com>
2024-09-16 17:28:08 +0530

    MOSIP-35176 Added custom Websubconfig
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

---
+++ authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/config/WebConfiguration.java
@@ -0,0 +1,15 @@
+package io.mosip.authentication.internal.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
+
+}
