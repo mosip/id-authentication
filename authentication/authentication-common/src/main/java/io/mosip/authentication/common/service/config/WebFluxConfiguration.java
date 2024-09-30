a08debae47c432d775b5d751edf9fbd5d7de589c
kameshsr <kameshsr1338@gmail.com>
2024-09-30 10:18:32 +0530

    MOSIP-35176 Added webflux class
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

---
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/config/WebFluxConfiguration.java
@@ -0,0 +1,13 @@
+package io.mosip.authentication.common.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfiguration implements WebFluxConfigurer {
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().maxInMemorySize(500 * 1024);
+    }
+}
