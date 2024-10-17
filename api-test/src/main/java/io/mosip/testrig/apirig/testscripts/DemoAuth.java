8e372bdd073925c5e116461ee77166fe2bd05a66
Sohan Kumar Dey <72375959+Sohandey@users.noreply.github.com>
2024-10-17 18:58:51 +0530

    DEMO AUTH TIMESTAMP Related fixes
    Signed-off-by: Sohan Kumar Dey <72375959+Sohandey@users.noreply.github.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/testscripts/DemoAuth.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/testscripts/DemoAuth.java
@@ -135,13 +135,19 @@ public class DemoAuth extends AdminTestUtil implements ITest {
 		String authRequest = getJsonFromTemplate(request.toString(), testCaseDTO.getInputTemplate());
 		logger.info("************* Modification of bio auth request ******************");
 		Reporter.log("<b><u>Modification of demo auth request</u></b>");
-		authRequest = modifyRequest(authRequest, demoAuthTempMap, getResourcePath()+props.getProperty("idaMappingPath"));
+		
 		JSONObject authRequestTemp = new JSONObject(authRequest);
+		String originalRequestTime = authRequestTemp.getString("requestTime");
+		authRequest = modifyRequest(authRequest, demoAuthTempMap, getResourcePath()+props.getProperty("idaMappingPath"));
+		
+		
+		authRequestTemp = new JSONObject(authRequest);
+		authRequestTemp.put("requestTime", originalRequestTime);
 		authRequestTemp.remove("env");
 		authRequestTemp.put("env", "Staging");
 		authRequest = authRequestTemp.toString();
 		testCaseDTO.setInput(authRequest);
-		testCaseDTO.setInput(authRequest);
+		
 				
 		logger.info("******Post request Json to EndPointUrl: " + ApplnURI + testCaseDTO.getEndPoint() + " *******");		
 		
