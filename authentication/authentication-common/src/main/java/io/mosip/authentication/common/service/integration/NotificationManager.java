6c62f0a29361002c84a792edbcab76e2469f5f7b
kameshsr <kameshsr1338@gmail.com>
2024-11-07 15:18:15 +0530

    Removed un-necessary changes
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/integration/NotificationManager.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/integration/NotificationManager.java
@@ -56,8 +56,8 @@ public class NotificationManager {
 			RestRequestDTO restRequestDTO = null;
 			restRequestDTO = restRequestFactory.buildRequest(RestServicesConstants.SMS_NOTIFICATION_SERVICE,
 					RestRequestFactory.createRequest(smsRequestDto), String.class);
-			restHelper.requestAsync(restRequestDTO);
-		} catch (IDDataValidationException e) {
+			restHelper.requestSync(restRequestDTO);
+		} catch (IDDataValidationException | RestServiceException e) {
 			logger.error(IdAuthCommonConstants.SESSION_ID, "Inside SMS Notification >>>>>", e.getErrorCode(), e.getErrorText());
 			throw new IdAuthenticationBusinessException(IdAuthenticationErrorConstants.DATA_VALIDATION_FAILED, e);
 		}
@@ -82,8 +82,8 @@ public class NotificationManager {
 			mailRequestDto.add("mailTo", emailId);
 			restRequestDTO = restRequestFactory.buildRequest(RestServicesConstants.MAIL_NOTIFICATION_SERVICE,
 					mailRequestDto, String.class);
-			restHelper.requestAsync(restRequestDTO);
-		} catch (IDDataValidationException e) {
+			restHelper.requestSync(restRequestDTO);
+		} catch (IDDataValidationException | RestServiceException e) {
 			// FIXME change error code
 			logger.error(IdAuthCommonConstants.SESSION_ID, "Inside Mail Notification >>>>>", e.getErrorCode(), e.getErrorText());
 			throw new IdAuthenticationBusinessException(IdAuthenticationErrorConstants.DATA_VALIDATION_FAILED, e);
