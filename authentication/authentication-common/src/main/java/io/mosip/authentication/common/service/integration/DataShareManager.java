4fb99de0e6eb827101fa8268157b24e78e00b996
kameshsr <kameshsr1338@gmail.com>
2024-11-19 17:32:38 +0530

    MOSIP-31576 Fixed token issue
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/integration/DataShareManager.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/integration/DataShareManager.java
@@ -54,7 +54,7 @@ public class DataShareManager {
 	public <R> R downloadObject(String dataShareUrl, Class<R> clazz, boolean decryptionRequired) throws  RestServiceException, IdAuthenticationBusinessException {
 		RestRequestDTO request = restRequestFactory.buildRequest(RestServicesConstants.DATA_SHARE_GET, null, String.class);
 		request.setUri(dataShareUrl);
-		String responseStr = restTemplate.getForObject(dataShareUrl, String.class);
+		String responseStr = restHelper.requestSync(request);
 		if (responseStr == null) {
 			throw new IdAuthUncheckedException(IdAuthenticationErrorConstants.UNABLE_TO_PROCESS.getErrorCode(), IdAuthenticationErrorConstants.UNABLE_TO_PROCESS.getErrorMessage());
 		}
