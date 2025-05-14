dafb9c8f1e91eeb731de2870db79f01e6133b076
Md Tarique Azeez <tarique.azeez@technoforte.co.in>
2025-05-14 15:10:44 +0530

    remove unwanted comment
    Signed-off-by: Md Tarique Azeez <tarique.azeez@technoforte.co.in>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/IdServiceImpl.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/IdServiceImpl.java
@@ -219,8 +219,7 @@ public class IdServiceImpl implements IdService<AutnTxn> {
 				entity.setTransactionLimit(Objects.nonNull(data[3]) ? Integer.parseInt(String.valueOf(data[3])) : null);
 				entity.setToken(String.valueOf(data[4]));
 			}
-//HERE CHECKING UIN/VID IS DEACTIVATE OR BLOCKED BASED ON EXPIRY TIMESTAMP IF EXPIRY TIMESTAMP IS IN PAST MEANS
-			//expired/deactivated/revoked/blocked AND THROW EXCEPTION
+
 			if (Objects.nonNull(entity.getExpiryTimestamp())
 					&& DateUtils.before(entity.getExpiryTimestamp(), DateUtils.getUTCCurrentDateTime())) {
 				logger.error(IdAuthCommonConstants.SESSION_ID, this.getClass().getSimpleName(), "getIdentity",
