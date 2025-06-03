bce79a08e17e4dcb34e0b5e54257f579852cd53f
TRIALBLAZERS <84778104+trialblazerseee@users.noreply.github.com>
2025-06-03 18:19:18 +0530

    [MOSIP-41323] IDA Internal Pod Replica Issue Causing Duplicate Processing Due to Unlocked Batch Reads (#1480)
    * IDA Internal Credential Process modified from step to job.
    
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
    
    * IDA Internal Credential Process modified from step to job.
    
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
    
    * IDA Internal Credential Process modified from step to job.
    
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
    
    * [MOSIP-41323] IDA Internal Pod Replica Issue Causing Duplicate Processing Due to Unlocked Batch Reads
    
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
    
    * [MOSIP-41323] IDA Internal Pod Replica Issue Causing Duplicate Processing Due to Unlocked Batch Reads
    
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
    Signed-off-by: tarique-azeez <mdtarique2703@gmail.com>
    Signed-off-by: Ashok Kumar Sharma <ashok@mosip.io>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/idevent/CredentialStoreServiceImpl.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/idevent/CredentialStoreServiceImpl.java
@@ -266,8 +266,6 @@ public class CredentialStoreServiceImpl implements CredentialStoreService {
 		
 		credentialEventStore.setUpdBy(IDA);
 		credentialEventStore.setUpdDTimes(DateUtils.getUTCCurrentDateTime());
-		
-		credentialEventRepo.save(credentialEventStore);
 	}
 
 	/**
@@ -324,7 +322,7 @@ public class CredentialStoreServiceImpl implements CredentialStoreService {
 		
 		String eventObjectStr = credentialEventStore.getEventObject();
 		try {
-			mosipLogger.info(IdAuthCommonConstants.SESSION_ID, this.getClass().getName(), "processCredentialStoreEvent",
+			mosipLogger.debug(IdAuthCommonConstants.SESSION_ID, this.getClass().getName(), "processCredentialStoreEvent",
 					"Processing credential store event: " + objectMapper.writeValueAsString(credentialEventStore));
 			
 			EventModel eventModel = objectMapper.readValue(eventObjectStr.getBytes(), EventModel.class);
@@ -425,11 +423,11 @@ public class CredentialStoreServiceImpl implements CredentialStoreService {
 	/**
 	 * Store identity entity.
 	 *
-	 * @param idEntities the id entities
+	 * @param identityEntity the id entity
 	 */
 	@Override
-	public void storeIdentityEntity(List<? extends IdentityEntity> idEntities) {
-		identityCacheRepo.saveAll(idEntities);
+	public void storeIdentityEntity(IdentityEntity identityEntity) {
+		identityCacheRepo.save(identityEntity);
 	}
 
 	/**
