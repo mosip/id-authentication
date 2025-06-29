3687f91eb857bb8bdf582bb772dcb1a1897005a7
ase-101 <sunkadaeanusha@gmail.com>
2025-06-29 19:01:56 +0530

    Added log to debug (#1510)
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/kyc/controller/KycAuthController.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/kyc/controller/KycAuthController.java
@@ -445,6 +445,7 @@ public class KycAuthController {
 						metadata != null && 
 								metadata.get(IdAuthCommonConstants.IDENTITY_DATA) != null &&
 										metadata.get(IdAuthCommonConstants.IDENTITY_INFO) != null) {
+					mosipLogger.info("processKycAuthV2 >> {}", authRequestDTO.getRequest().isClaimsMetadataRequired());
 					kycAuthResponseDtoV2 = kycFacade.processKycAuthV2(authRequestDTO, authResponseDTO, partnerId, oidcClientId, metadata);
 				}
 				return kycAuthResponseDtoV2;
