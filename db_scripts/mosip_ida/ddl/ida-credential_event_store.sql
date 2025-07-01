f10f0d8cc528c6e2ed5810ee5aa04f491d31b5a5
tarique-azeez <mdtarique2703@gmail.com>
2025-07-01 21:32:21 +0530

    update pom version and add db_upgrade_script
    Signed-off-by: tarique-azeez <mdtarique2703@gmail.com>

--- db_scripts/mosip_ida/ddl/ida-credential_event_store.sql
+++ db_scripts/mosip_ida/ddl/ida-credential_event_store.sql
@@ -37,6 +37,9 @@ CREATE TABLE ida.credential_event_store(
 --index section starts----
 CREATE INDEX ind_ces_id ON ida.credential_event_store (cr_dtimes);
 --index section ends------
+--index section starts----
+CREATE INDEX cred_event_store_status_cr_dtimes ON ida.credential_event_store USING btree (status_code desc, retry_count, cr_dtimes) WHERE status_code in ('NEW','FAILED');
+--index section ends------
 COMMENT ON TABLE ida.credential_event_store IS 'Credential Event Store: Store all credential request in IDA and their status, Retry request incase of failure';
 -- ddl-end --
 COMMENT ON COLUMN ida.credential_event_store.event_id IS 'Event ID: Event id of the credential request';
