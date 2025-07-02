5e11dafe387a5d7269157b98fca77219bd2e25a2
Nandhukumar <nandhukumare@gmail.com>
2025-07-02 17:25:33 +0530

    MOSIP-41781 - Fixed the automation failures and updated the branch for the release with 0 failures (#1497)
    * MOSIP-41781
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>
    
    * MOSIP-41781
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>
    
    * MOSIP-41704
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>
    
    * MOSIP-41704
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>
    
    * MOSIP-41781
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>
    
    ---------
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/auth/testrunner/MosipTestRunner.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/auth/testrunner/MosipTestRunner.java
@@ -47,6 +47,7 @@ import io.mosip.testrig.apirig.utils.MispPartnerAndLicenseKeyGeneration;
 import io.mosip.testrig.apirig.utils.OutputValidationUtil;
 import io.mosip.testrig.apirig.utils.PartnerRegistration;
 import io.mosip.testrig.apirig.utils.SkipTestCaseHandler;
+import io.mosip.testrig.apirig.utils.Watchdog;
 
 /**
  * Class to initiate mosip api test execution
@@ -68,6 +69,9 @@ public class MosipTestRunner {
 	 * @param arg
 	 */
 	public static void main(String[] arg) {
+		// Set execution elapse timeout to 1.5 hour
+		Watchdog watchdog = new Watchdog(90 * 60 * 1000L);
+		watchdog.start();
 
 		try {
 			LOGGER.info("** ------------- API Test Rig Run Started --------------------------------------------- **");
@@ -129,6 +133,9 @@ public class MosipTestRunner {
 		OTPListener.bTerminate = true;
 		
 		HealthChecker.bTerminate = true;
+
+		// Stop watchdog since task completed successfully
+		watchdog.stop();
 
 		System.exit(0);
 
