1c8d74ea63be1cfc45aa6db77cc5a2c975403c5e
nagendra0721 <nagendra0718@gmail.com>
2025-07-09 19:11:37 +0530

    MOSIP:42058: null pointer exception
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/IdInfoFetcherImpl.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/IdInfoFetcherImpl.java
@@ -333,10 +333,13 @@ public class IdInfoFetcherImpl implements IdInfoFetcher {
 			List<BIR> birDataFromXMLType = cbeffUtil.getBIRDataFromXMLType(biometricCbeff.getBytes(), type.getName());
 			Function<? super BIR, ? extends String> keyFunction = bir -> {
 				BDBInfo bdbInfo = bir.getBdbInfo();
-				return ((bdbInfo.getType() == null || bdbInfo.getType().isEmpty()) ? "" : bdbInfo.getType().get(0).toString())+ "_"
-						+ ((bdbInfo.getSubtype() == null || bdbInfo.getSubtype().isEmpty()) ? "" : bdbInfo.getSubtype().get(0))
-						+ ((bdbInfo.getSubtype() != null && bdbInfo.getSubtype().size() > 1) ?  " "  + bdbInfo.getSubtype().get(1) : "") + "_"
-						+ ((bdbInfo.getFormat() == null || bdbInfo.getFormat().getType() == null) ? "" : bdbInfo.getFormat().getType());
+				String modality = bdbInfo.getType().get(0).toString();
+				if (modality.equalsIgnoreCase("face")) {
+					return modality + "_" + bdbInfo.getFormat().getType();
+				}
+				return modality + "_" + (bdbInfo.getSubtype() == null || bdbInfo.getSubtype().isEmpty() ? "" : bdbInfo.getSubtype().get(0))
+						+ (bdbInfo.getSubtype() != null && bdbInfo.getSubtype().size() > 1 ? " " + bdbInfo.getSubtype().get(1) : "")
+				+ "_" + bdbInfo.getFormat().getType();
 			};
 			if(birDataFromXMLType.size() == 1) {
 				//This is the segmented cbeff
