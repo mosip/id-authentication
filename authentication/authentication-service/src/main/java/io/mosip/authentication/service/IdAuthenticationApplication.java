3598182cb748801a6eeb8856d9dc7d298ed004e8
kameshsr <kameshsr1338@gmail.com>
2025-08-04 20:34:02 +0530

    MOSIP-42357 added comment
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/IdAuthenticationApplication.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/IdAuthenticationApplication.java
@@ -82,7 +82,7 @@ import io.mosip.kernel.zkcryptoservice.service.impl.ZKCryptoManagerServiceImpl;
 import io.mosip.kernel.keymanager.hsm.health.HSMHealthCheck;
 import io.mosip.kernel.websub.api.config.publisher.RestTemplateHelper;
 import io.mosip.kernel.keymanagerservice.helper.SubjectAlternativeNamesHelper;
-
+//test
 /**
  * Spring-boot class for ID Authentication Application.
  *
