6eb94e91c456a28812ffdd34ac95b44fe9bde772
kameshsr <kameshsr1338@gmail.com>
2025-08-06 17:17:23 +0530

    MOSIP-42357 added auth type util
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/util/AuthTypeUtil.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/util/AuthTypeUtil.java
@@ -2,6 +2,7 @@ package io.mosip.authentication.common.service.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 import io.mosip.authentication.common.service.helper.AuthTransactionHelper;
@@ -16,21 +17,21 @@ import io.mosip.authentication.core.indauth.dto.EkycAuthRequestDTO;
 import io.mosip.authentication.core.spi.indauth.match.AuthType;
 
 public final class AuthTypeUtil {
-	
+
 	private AuthTypeUtil() {}
-	
+
 	public static boolean isBio(AuthRequestDTO authReqDto) {
 		return isAuthTypeInfoAvailable(authReqDto, BioAuthType.values());
 	}
-	
+
 	public static boolean isDemo(AuthRequestDTO authReqDto) {
 		return isAuthTypeInfoAvailable(authReqDto, DemoAuthType.values());
 	}
-	
+
 	public static boolean isOtp(AuthRequestDTO authReqDto) {
 		return isAuthTypeInfoAvailable(authReqDto, new AuthType[] {PinAuthType.OTP});
 	}
-	
+
 	public static boolean isPin(AuthRequestDTO authReqDto) {
 		return isAuthTypeInfoAvailable(authReqDto, new AuthType[] {PinAuthType.SPIN});
 	}
@@ -44,31 +45,42 @@ public final class AuthTypeUtil {
 	}
 
 	private static boolean isAuthTypeInfoAvailable(AuthRequestDTO authReqDto, AuthType[] values) {
-		return Stream.of(values).anyMatch(authType -> authType.isAuthTypeInfoAvailable(authReqDto));
+		for (AuthType authType : values) {
+			if (authType.isAuthTypeInfoAvailable(authReqDto)) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
+
 	public static List<RequestType> findAutRequestTypes(AuthRequestDTO authRequestDTO, EnvUtil env) {
-		List<RequestType> requestTypes = new ArrayList<>();
+		Objects.requireNonNull(authRequestDTO, "AuthRequestDTO cannot be null");
+
+		List<RequestType> requestTypes = new ArrayList<>(6);
+		boolean hasBio = AuthTypeUtil.isBio(authRequestDTO);
+
 		if(AuthTypeUtil.isOtp(authRequestDTO)) {
 			requestTypes.add(RequestType.OTP_AUTH);
 		}
+
 		if(AuthTypeUtil.isDemo(authRequestDTO)) {
 			requestTypes.add(RequestType.DEMO_AUTH);
 		}
-		if(AuthTypeUtil.isBio(authRequestDTO)) {
-			if (AuthTransactionHelper.isFingerAuth(authRequestDTO, env)) {
-				requestTypes.add(RequestType.FINGER_AUTH);
-			}
-			if (AuthTransactionHelper.isIrisAuth(authRequestDTO, env)) {
-				requestTypes.add(RequestType.IRIS_AUTH);
-			}
-			if (AuthTransactionHelper.isFaceAuth(authRequestDTO, env)) {
-				requestTypes.add(RequestType.FACE_AUTH);
-			}
+
+		if(hasBio) {
+			boolean finger = AuthTransactionHelper.isFingerAuth(authRequestDTO, env);
+			boolean iris   = AuthTransactionHelper.isIrisAuth(authRequestDTO, env);
+			boolean face   = AuthTransactionHelper.isFaceAuth(authRequestDTO, env);
+
+			if (finger) requestTypes.add(RequestType.FINGER_AUTH);
+			if (iris)   requestTypes.add(RequestType.IRIS_AUTH);
+			if (face)   requestTypes.add(RequestType.FACE_AUTH);
 		}
+
 		if(AuthTypeUtil.isKeyBindedToken(authRequestDTO)) {
 			requestTypes.add(RequestType.TOKEN_AUTH);
 		}
+
 		if(authRequestDTO instanceof EkycAuthRequestDTO) {
 			requestTypes.add(RequestType.EKYC_AUTH_REQUEST);
 		}
@@ -76,8 +88,7 @@ public final class AuthTypeUtil {
 		if (AuthTypeUtil.isPassword(authRequestDTO)) {
 			requestTypes.add(RequestType.PASSWORD_AUTH);
 		}
-		
+
 		return requestTypes;
 	}
-
 }
