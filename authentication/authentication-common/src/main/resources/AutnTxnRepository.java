57367a441b104662f6b99f5e54cd240ef95cce68
kameshsr <kameshsr1338@gmail.com>
2025-08-06 20:49:44 +0530

    MOSIP-42357 corrected
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-common/src/main/resources/AutnTxnRepository.java
+++ authentication/authentication-common/src/main/resources/AutnTxnRepository.java
@@ -14,57 +14,57 @@ import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 
 /**
  * This is a repository class for entity {@link AutnTxn}.
- * 
+ *
  * @author Rakesh Roshan
  */
 @Repository
 public interface AutnTxnRepository extends BaseRepository<AutnTxn, Integer> {
 
-	/**
-	 * Obtain all Authentication Transaction for particular TxnId and UIN.
-	 *
-	 * @param txnId        the txn id
-	 * @param pagaeable    the pagaeable
-	 * @param authtypecode the authtypecode
-	 * @return the list
-	 */
-	@Query(value = "Select new AutnTxn(token, refIdType, entityId) from AutnTxn where requestTrnId=:txnId AND authTypeCode=:authtypecode ORDER BY crDTimes DESC")
-	public List<AutnTxn> findByTxnId(@Param("txnId") String txnId, Pageable pagaeable,
-			@Param("authtypecode") String authtypecode);
+    /**
+     * Obtain all Authentication Transaction for particular TxnId and UIN.
+     *
+     * @param txnId        the txn id
+     * @param pagaeable    the pagaeable
+     * @param authtypecode the authtypecode
+     * @return the list
+     */
+    @Query(value = "Select new AutnTxn(token, refIdType, entityId) from AutnTxn where requestTrnId=:txnId AND authTypeCode=:authtypecode ORDER BY crDTimes DESC")
+    public List<AutnTxn> findByTxnId(@Param("txnId") String txnId, Pageable pagaeable,
+                                     @Param("authtypecode") String authtypecode);
 
-	@Query(value = "Select new AutnTxn( requestTrnId, requestDTimes, authTypeCode, statusCode, statusComment, refId, refIdType, entityName, requestSignature, responseSignature ) from AutnTxn where token=:token ORDER BY crDTimes DESC")
-	public List<AutnTxn> findByToken(@Param("token") String token, Pageable pagaeable);
+    @Query(value = "Select new AutnTxn( requestTrnId, requestDTimes, authTypeCode, statusCode, statusComment, refId, refIdType, entityName, requestSignature, responseSignature ) from AutnTxn where token=:token ORDER BY crDTimes DESC")
+    public List<AutnTxn> findByToken(@Param("token") String token, Pageable pagaeable);
 
-	/**
-	 * Obtain the number of count of request_dTtimes for particular UIN(uniqueId)
-	 * with within the otpRequestDTime and oneMinuteBeforeTime.
-	 *
-	 * @param otpRequestDTime     the otp request D time
-	 * @param oneMinuteBeforeTime the one minute before time
-	 * @param token               the token
-	 * @return the int
-	 */
-	@Query("Select count(1) from AutnTxn  where requestDTimes <= :otpRequestDTime and "
-			+ "requestDTimes >= :oneMinuteBeforeTime and token=:token")
-	public int countRequestDTime(@Param("otpRequestDTime") LocalDateTime otpRequestDTime,
-			@Param("oneMinuteBeforeTime") LocalDateTime oneMinuteBeforeTime, @Param("token") String token);
+    /**
+     * Obtain the number of count of request_dTtimes for particular UIN(uniqueId)
+     * with within the otpRequestDTime and oneMinuteBeforeTime.
+     *
+     * @param otpRequestDTime     the otp request D time
+     * @param oneMinuteBeforeTime the one minute before time
+     * @param token               the token
+     * @return the int
+     */
+    @Query("Select count(1) from AutnTxn  where requestDTimes <= :otpRequestDTime and "
+            + "requestDTimes >= :oneMinuteBeforeTime and token=:token")
+    public int countRequestDTime(@Param("otpRequestDTime") LocalDateTime otpRequestDTime,
+                                 @Param("oneMinuteBeforeTime") LocalDateTime oneMinuteBeforeTime, @Param("token") String token);
 
-	@Query("""
+    @Query("""
             SELECT COUNT(a) FROM AutnTxn a
             WHERE a.refId = :refId
               AND a.requestDTimes > :afterRequestTime
             """)
-	Long countByRefIdAndRequestDTimesAfter(String refId, LocalDateTime afterRequestTime);
-	
-	@Query("""
+    Long countByRefIdAndRequestDTimesAfter(String refId, LocalDateTime afterRequestTime);
+
+    @Query("""
             SELECT COUNT(a) FROM AutnTxn a
             WHERE a.entityId = :entityId
               AND a.requestDTimes > :afterRequestTime
             """)
-	Long countByEntityIdAndRequestDTimesAfter(@Param("entityId") String entityId,
-											  @Param("afterRequestTime") LocalDateTime afterRequestTime);
+    Long countByEntityIdAndRequestDTimesAfter(@Param("entityId") String entityId,
+                                              @Param("afterRequestTime") LocalDateTime afterRequestTime);
 
-	@Query(value = """
+    @Query(value = """
     SELECT CASE WHEN COUNT(1) >= :requestCountForFlooding
            THEN TRUE ELSE FALSE END
     FROM (
@@ -75,12 +75,10 @@ public interface AutnTxnRepository extends BaseRepository<AutnTxn, Integer> {
         LIMIT :requestCountForFlooding
     ) AS sub
 """, nativeQuery = true)
-	boolean hasFloodingByRefId(@Param("refId") String refId,
-							   @Param("afterRequestTime") LocalDateTime afterRequestTime,
-							   @Param("requestCountForFlooding") long requestCountForFlooding);
-	
-	
-	@Query(value = """
+    boolean hasFloodingByRefId(@Param("refId") String refId,
+                               @Param("afterRequestTime") LocalDateTime afterRequestTime,
+                               @Param("requestCountForFlooding") long requestCountForFlooding);
+    @Query(value = """
     SELECT CASE WHEN COUNT(1) >= :requestCountForFlooding
                 THEN TRUE ELSE FALSE END
     FROM (
@@ -91,8 +89,8 @@ public interface AutnTxnRepository extends BaseRepository<AutnTxn, Integer> {
         LIMIT :requestCountForFlooding
     ) AS sub
 """, nativeQuery = true)
-	boolean hasFloodingByEntityId(@Param("entityId") String entityId,
-								  @Param("afterRequestTime") LocalDateTime afterRequestTime,
-								  @Param("requestCountForFlooding") int requestCountForFlooding);
+    boolean hasFloodingByEntityId(@Param("entityId") String entityId,
+                                  @Param("afterRequestTime") LocalDateTime afterRequestTime,
+                                  @Param("requestCountForFlooding") int requestCountForFlooding);
 
 }
