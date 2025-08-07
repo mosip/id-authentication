75364cc14a3593ae4910f2308c63ad96398dc71f
kameshsr <kameshsr1338@gmail.com>
2025-08-07 19:41:24 +0530

    MOSIP-42357 corrected repo
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/IdentityCacheRepository.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/IdentityCacheRepository.java
@@ -1,6 +1,6 @@
 package io.mosip.authentication.common.service.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,20 +9,27 @@ import org.springframework.stereotype.Repository;
 import io.mosip.authentication.common.service.entity.IdentityEntity;
 import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 
-/**
- * Repository class for Identity Cache table
- * 
- * @author Loganathan Sekar
- *
- */
 @Repository
 public interface IdentityCacheRepository extends BaseRepository<IdentityEntity, String> {
 
-	@Query("SELECT i.id, i.demographicData, i.expiryTimestamp, i.transactionLimit, i.token, i.crBy, i.crDTimes, "
-			+ "i.updBy, i.updDTimes, i.isDeleted, i.delDTimes FROM IdentityEntity i where i.id = :id")
-	List<Object[]> findDemoDataById(@Param("id") String id);
+	/**
+	 * Fetch only required fields for demo data by Id.
+	 * Avoids fetching unnecessary large objects.
+	 */
+	@Query("SELECT i.id, i.demographicData, i.expiryTimestamp, i.transactionLimit, i.token, " +
+			"i.crBy, i.crDTimes, i.updBy, i.updDTimes, i.isDeleted, i.delDTimes " +
+			"FROM IdentityEntity i WHERE i.id = :id")
+	Optional<Object[]> findDemoDataById(@Param("id") String id);
 
-	@Query("SELECT i.id, i.expiryTimestamp, i.transactionLimit "
-			+ " FROM IdentityEntity i where i.id = :id")
-	List<Object[]> findTransactionLimitById(@Param("id") String id);
+	/**
+	 * Fetch only transaction limit by Id.
+	 */
+	@Query("SELECT i.id, i.expiryTimestamp, i.transactionLimit FROM IdentityEntity i WHERE i.id = :id")
+	Optional<Object[]> findTransactionLimitById(@Param("id") String id);
+
+	/**
+	 * Existence check optimized for boolean return to avoid unnecessary object hydration.
+	 */
+	@Query("SELECT 1 FROM IdentityEntity i WHERE i.id = :id")
+	boolean existsById(String id);
 }
