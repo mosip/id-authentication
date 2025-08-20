8b5498aaecfbafa4a6e38582129ce5025252c26c
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2025-08-20 12:32:38 +0530

    Added DB name (#1562) (#1563)
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- db_upgrade_scripts/mosip_ida/sql/1.2.1.1_to_1.2.1.2_upgrade.sql
+++ db_upgrade_scripts/mosip_ida/sql/1.2.1.1_to_1.2.1.2_upgrade.sql
@@ -1,1 +1,3 @@
+\c mosip_ida
+
 CREATE INDEX IF NOT EXISTS cred_event_store_status_cr_dtimes ON ida.credential_event_store USING btree (status_code desc, retry_count, cr_dtimes) WHERE status_code in ('NEW','FAILED');
