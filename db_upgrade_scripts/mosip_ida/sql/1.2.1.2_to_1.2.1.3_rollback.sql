1d85253985305e308c4a1706d11679889b258649
Mahammed Taheer <mohd.taheer@gmail.com>
2025-09-29 14:39:04 +0530

    [MOSIP-41856] updated the pom version to SNAPSHOT.
    Signed-off-by: Mahammed Taheer <mohd.taheer@gmail.com>

--- db_upgrade_scripts/mosip_ida/sql/1.2.1.2_to_1.2.1.3_rollback.sql
+++
@@ -1,3 +0,0 @@
-\c mosip_ida
-
-DROP INDEX IF EXISTS cred_event_store_status_cr_dtimes;
--- db_upgrade_scripts/mosip_ida/sql/1.2.1.2_to_1.2.1.3_upgrade.sql
+++
@@ -1,3 +0,0 @@
-\c mosip_ida
-
-CREATE INDEX IF NOT EXISTS cred_event_store_status_cr_dtimes ON ida.credential_event_store USING btree (status_code desc, retry_count, cr_dtimes) WHERE status_code in ('NEW','FAILED');
