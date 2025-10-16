29e33676ab6c247c6e5c45526633fbba430c6a08
Anuranjan14 <anuranjan.kumar@technoforte.co.in>
2025-10-16 14:18:41 +0530

    Mosip-41455: Updated java files with upstream code
    Signed-off-by: Anuranjan14 <anuranjan.kumar@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/auth/testscripts/MultiFactorAuthNew.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/auth/testscripts/MultiFactorAuthNew.java
@@ -84,12 +84,12 @@ public class MultiFactorAuthNew extends IdAuthenticationUtil implements ITest {
 			
 		{
 			testCaseDTO.setEndPoint(testCaseDTO.getEndPoint().replace("$partnerKeyURL$", partnerKeyUrl));
-			PartnerRegistration.appendEkycOrRp = "rp-";
+			PartnerRegistration.appendEkycOrRp.set("rp-");
 		}
 		if(testCaseDTO.getEndPoint().contains("$ekycPartnerKeyURL$"))
 		{
 			testCaseDTO.setEndPoint(testCaseDTO.getEndPoint().replace("$ekycPartnerKeyURL$", ekycPartnerKeyURL));
-			PartnerRegistration.appendEkycOrRp = "ekyc-";
+			PartnerRegistration.appendEkycOrRp.set("ekyc-");
 		}
 		if (testCaseDTO.getEndPoint().contains("$UpdatedPartnerKeyURL$")) {
 			testCaseDTO.setEndPoint(testCaseDTO.getEndPoint().replace("$UpdatedPartnerKeyURL$",
