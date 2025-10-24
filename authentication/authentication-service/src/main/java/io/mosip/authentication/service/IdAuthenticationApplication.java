4cdef24a0a53a937515026408e71056403d184de
kameshsr <kameshsr1338@gmail.com>
2025-10-24 15:02:54 +0530

    MOSIP-42334 fixed crashloop
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/IdAuthenticationApplication.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/IdAuthenticationApplication.java
@@ -5,6 +5,7 @@ import io.mosip.authentication.common.service.integration.*;
 import io.mosip.authentication.common.service.util.*;
 import io.mosip.kernel.keymanagerservice.helper.SubjectAlternativeNamesHelper;
 import io.mosip.kernel.keymanagerservice.validator.ECKeyPairGenRequestValidator;
+import io.mosip.kernel.signature.util.SignatureUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -119,7 +120,7 @@ import io.mosip.kernel.websub.api.config.publisher.RestTemplateHelper;
 		PDFGeneratorImpl.class, RestTemplateHelper.class, LanguageUtil.class, IdentityAttributesForMatchTypeHelper.class
 , TypeForIdNameHelper.class
 		, ValidateOtpHelper.class, RequireOtpNotFrozenHelper.class, MatchIdentityDataHelper.class, MatchTypeHelper.class
-		, SeparatorHelper.class, ECKeyPairGenRequestValidator.class, SubjectAlternativeNamesHelper.class, SignatureServiceImpl.class})
+		, SeparatorHelper.class, ECKeyPairGenRequestValidator.class, SubjectAlternativeNamesHelper.class, SignatureServiceImpl.class, SignatureUtil.class})
 @ComponentScan(basePackages = { "io.mosip.authentication.service.*", "io.mosip.kernel.core.logger.config",
 		"io.mosip.authentication.common.service.config","io.mosip.authentication.common.service.util",
 		"io.mosip.kernel.websub.api.config",
