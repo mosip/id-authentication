7281eb4221e0354d4b714d048e62340510bee948
kameshsr <kameshsr1338@gmail.com>
2025-10-29 18:04:54 +0530

    MOSIP-42334 corrected repository
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-authtypelockfilter-impl/src/main/java/io/mosip/authentication/authtypelockfilter/impl/AuthTypeLockFilterImpl.java
+++ authentication/authentication-authtypelockfilter-impl/src/main/java/io/mosip/authentication/authtypelockfilter/impl/AuthTypeLockFilterImpl.java
@@ -72,71 +72,107 @@ public class AuthTypeLockFilterImpl implements IMosipAuthFilter {
 		}
 	}
 
-	private void validateAuthTypeStatus(AuthRequestDTO authRequestDTO, AuthtypeStatus authTypeStatus,List<AuthtypeStatus> authtypeStatusList)
-			throws IdAuthenticationFilterException {
-		if (authTypeStatus.getLocked()) {
-			if (AuthTypeUtil.isDemo(authRequestDTO)
-					&& authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.DEMO.getType())) {
-				throw new IdAuthenticationFilterException(
-						IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
-						String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
-								MatchType.Category.DEMO.getType()));
-			}
+    private void validateAuthTypeStatus(AuthRequestDTO authRequestDTO, AuthtypeStatus authTypeStatus,
+                                        List<AuthtypeStatus> authtypeStatusList)
+            throws IdAuthenticationFilterException {
 
-			else if (AuthTypeUtil.isBio(authRequestDTO)
-					&& authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.BIO.getType())) {
-				for (AuthType authType : BioAuthType.getSingleBioAuthTypes().toArray(s -> new AuthType[s])) {
-					if (authType.getType().equalsIgnoreCase(authTypeStatus.getAuthSubType())) {
-						if (authType.isAuthTypeEnabled(authRequestDTO, idInfoFetcher)) {
-							throw new IdAuthenticationFilterException(
-									IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
-									String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
-											MatchType.Category.BIO.getType() + "-" + authType.getType()));
-						} else {
-							break;
-						}
-					}
-				}
-			}
+        System.out.println("---- Entered validateAuthTypeStatus ----");
+        System.out.println("authTypeStatus = " + authTypeStatus);
+        System.out.println("authTypeStatus.getAuthType() = " + authTypeStatus.getAuthType());
+        System.out.println("authTypeStatus.getAuthSubType() = " + authTypeStatus.getAuthSubType());
+        System.out.println("authTypeStatus.getLocked() = " + authTypeStatus.getLocked());
+        System.out.println("AuthRequestDTO = " + authRequestDTO);
 
-			else if (AuthTypeUtil.isOtp(authRequestDTO))
-				if (authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.OTP.getType())
-						&& (authTypeStatus.getAuthSubType() == null || authTypeStatus.getAuthSubType().isEmpty())) {
-					throw new IdAuthenticationFilterException(
-							IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
-							String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
-									MatchType.Category.OTP.getType()));
-				} else {
-					if (authTypeStatus.getAuthSubType() != null && !authTypeStatus.getAuthSubType().isEmpty()
-							&& (authTypeStatus.getAuthSubType().equalsIgnoreCase(IdAuthCommonConstants.PHONE_NUMBER)
-									|| authTypeStatus.getAuthSubType().equalsIgnoreCase(IdAuthCommonConstants.EMAIL))
-							&& authTypeStatus.getLocked().equals(true)) {
-						Optional<AuthtypeStatus> otherSubOtpTypeLocked = authtypeStatusList.stream()
-								.filter(authTypeStatus1 -> authTypeStatus1.getAuthType() != null
-										&& authTypeStatus1.getAuthType()
-												.equalsIgnoreCase(MatchType.Category.OTP.getType())
-										&& authTypeStatus1.getAuthSubType() != null
-										&& !authTypeStatus1.getAuthSubType()
-												.equalsIgnoreCase(authTypeStatus.getAuthSubType())
-										&& authTypeStatus1.getLocked())
-								.findAny();
-						if (otherSubOtpTypeLocked.isPresent()) {
-							throw new IdAuthenticationFilterException(
-									IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
-									String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
-											MatchType.Category.OTP.getType()));
-						}
+        if (authTypeStatus.getLocked()) {
+            System.out.println("Auth type is locked, proceeding to validation...");
 
-					}
-				}
+            // DEMO
+            if (AuthTypeUtil.isDemo(authRequestDTO)
+                    && authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.DEMO.getType())) {
+                System.out.println("DEMO auth type locked condition matched.");
+                throw new IdAuthenticationFilterException(
+                        IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
+                        String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
+                                MatchType.Category.DEMO.getType()));
+            }
 
-			else if (AuthTypeUtil.isPin(authRequestDTO)
-					&& authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.SPIN.getType())) {
-				throw new IdAuthenticationFilterException(
-						IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
-						String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
-								MatchType.Category.SPIN.getType()));
-			}
-		}
-	}
+            // BIO
+            else if (AuthTypeUtil.isBio(authRequestDTO)
+                    && authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.BIO.getType())) {
+                System.out.println("BIO auth type locked condition matched.");
+                for (AuthType authType : BioAuthType.getSingleBioAuthTypes().toArray(s -> new AuthType[s])) {
+                    System.out.println("Checking BIO sub-type: " + authType.getType());
+                    if (authType.getType().equalsIgnoreCase(authTypeStatus.getAuthSubType())) {
+                        System.out.println("Matched BIO subtype: " + authTypeStatus.getAuthSubType());
+                        if (authType.isAuthTypeEnabled(authRequestDTO, idInfoFetcher)) {
+                            System.out.println("BIO auth type enabled and locked. Throwing exception.");
+                            throw new IdAuthenticationFilterException(
+                                    IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
+                                    String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
+                                            MatchType.Category.BIO.getType() + "-" + authType.getType()));
+                        } else {
+                            System.out.println("BIO subtype found but not enabled. Breaking loop.");
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // OTP
+            else if (AuthTypeUtil.isOtp(authRequestDTO)) {
+                System.out.println("OTP authentication request detected.");
+                if (authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.OTP.getType())
+                        && (authTypeStatus.getAuthSubType() == null || authTypeStatus.getAuthSubType().isEmpty())) {
+                    System.out.println("OTP locked without subtype. Throwing exception.");
+                    throw new IdAuthenticationFilterException(
+                            IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
+                            String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
+                                    MatchType.Category.OTP.getType()));
+                } else {
+                    System.out.println("OTP subtype = " + authTypeStatus.getAuthSubType());
+                    if (authTypeStatus.getAuthSubType() != null && !authTypeStatus.getAuthSubType().isEmpty()
+                            && (authTypeStatus.getAuthSubType().equalsIgnoreCase(IdAuthCommonConstants.PHONE_NUMBER)
+                            || authTypeStatus.getAuthSubType().equalsIgnoreCase(IdAuthCommonConstants.EMAIL))
+                            && authTypeStatus.getLocked().equals(true)) {
+
+                        System.out.println("Checking if other OTP subtype is also locked...");
+                        Optional<AuthtypeStatus> otherSubOtpTypeLocked = authtypeStatusList.stream()
+                                .filter(authTypeStatus1 -> authTypeStatus1.getAuthType() != null
+                                        && authTypeStatus1.getAuthType().equalsIgnoreCase(MatchType.Category.OTP.getType())
+                                        && authTypeStatus1.getAuthSubType() != null
+                                        && !authTypeStatus1.getAuthSubType()
+                                        .equalsIgnoreCase(authTypeStatus.getAuthSubType())
+                                        && authTypeStatus1.getLocked())
+                                .findAny();
+
+                        if (otherSubOtpTypeLocked.isPresent()) {
+                            System.out.println("Both OTP subtypes locked. Throwing exception.");
+                            throw new IdAuthenticationFilterException(
+                                    IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
+                                    String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
+                                            MatchType.Category.OTP.getType()));
+                        } else {
+                            System.out.println("Other OTP subtype not locked.");
+                        }
+                    }
+                }
+            }
+
+            // SPIN
+            else if (AuthTypeUtil.isPin(authRequestDTO)
+                    && authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.SPIN.getType())) {
+                System.out.println("SPIN auth type locked condition matched. Throwing exception.");
+                throw new IdAuthenticationFilterException(
+                        IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
+                        String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
+                                MatchType.Category.SPIN.getType()));
+            }
+
+        } else {
+            System.out.println("Auth type is not locked, skipping validation.");
+        }
+
+        System.out.println("---- Exiting validateAuthTypeStatus ----");
+    }
+
 }
