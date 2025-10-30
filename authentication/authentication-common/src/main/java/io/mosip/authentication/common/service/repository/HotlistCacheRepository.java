192f127e5a65f3868d0ae6e7fae7697d8e8457cf
kameshsr <kameshsr1338@gmail.com>
2025-10-30 17:44:17 +0530

    MOSIP-42334 removed cache
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/HotlistCacheRepository.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/repository/HotlistCacheRepository.java
@@ -26,8 +26,8 @@ public interface HotlistCacheRepository extends BaseRepository<HotlistCache, Str
      * Fetch a hotlist record by IdHash and IdType.
      * Cached to reduce DB hits for frequently requested values.
      */
-    @Cacheable(value = "hotlistCache", key = "#idHash + ':' + #idType", unless = "#result == null")
-    @QueryHints(@QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_READ_ONLY, value = "true"))
+//    @Cacheable(value = "hotlistCache", key = "#idHash + ':' + #idType", unless = "#result == null")
+//    @QueryHints(@QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_READ_ONLY, value = "true"))
     @Query("SELECT h FROM HotlistCache h WHERE h.idHash = :idHash AND h.idType = :idType")
     Optional<HotlistCache> findByIdHashAndIdType(@Param("idHash") String idHash, @Param("idType") String idType);
     
