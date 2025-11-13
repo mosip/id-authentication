7c3ef863f0cbf14f2c71683d66806edfb88c08ef
kameshsr <kameshsr1338@gmail.com>
2025-11-13 16:01:56 +0530

    MOSIP-42357 reverted BioAuth
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/auth/testscripts/BioAuth.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/auth/testscripts/BioAuth.java
@@ -121,13 +121,13 @@ public class BioAuth extends IdAuthenticationUtil implements ITest {
 		}
 		logger.info(identityRequest);
 		String encryptedIdentityReq = null;
-        try {
-            encryptedIdentityReq = bioDataUtil.constractBioIdentityRequest(identityRequest,
-                    getResourcePath() + props.getProperty("bioValueEncryptionTemplate"), testCaseName, isInternal);
-            System.out.println("encryptedIdentityReq = " + encryptedIdentityReq);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		try {
+			encryptedIdentityReq = bioDataUtil.constractBioIdentityRequest(identityRequest,
+					getResourcePath() + props.getProperty("bioValueEncryptionTemplate"), testCaseName, isInternal);
+			System.out.println("encryptedIdentityReq = " + encryptedIdentityReq);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		Map<String, String> bioAuthTempMap = (isInternal)
 				? encryptDecryptUtil.getInternalEncryptSessionKeyValue(encryptedIdentityReq)
