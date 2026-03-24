fd452178c48089ab994a4bacc2f034284877c9e1
kameshsr <kameshsr1338@gmail.com>
2026-03-24 13:25:11 +0530

    MOSIP-44691 Added SubjectAlternativeNamesHelper
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/IdAuthenticationApplication.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/IdAuthenticationApplication.java
@@ -3,6 +3,7 @@ package io.mosip.authentication.service;
 import io.mosip.authentication.common.service.helper.*;
 import io.mosip.authentication.common.service.integration.*;
 import io.mosip.authentication.common.service.util.*;
+import io.mosip.kernel.core.pdfgenerator.spi.PDFGenerator;
 import io.mosip.kernel.keymanagerservice.helper.SubjectAlternativeNamesHelper;
 import io.mosip.kernel.keymanagerservice.validator.ECKeyPairGenRequestValidator;
 import org.springframework.boot.SpringApplication;
@@ -118,7 +119,7 @@ import io.mosip.kernel.websub.api.config.publisher.RestTemplateHelper;
 		RestTemplateHelper.class, LanguageUtil.class, IdentityAttributesForMatchTypeHelper.class
 , TypeForIdNameHelper.class
 		, ValidateOtpHelper.class, RequireOtpNotFrozenHelper.class, MatchIdentityDataHelper.class, MatchTypeHelper.class
-		, SeparatorHelper.class, ECKeyPairGenRequestValidator.class, SubjectAlternativeNamesHelper.class})
+		, SeparatorHelper.class, ECKeyPairGenRequestValidator.class, SubjectAlternativeNamesHelper.class, PDFGenerator.class})
 @ComponentScan(basePackages = { "io.mosip.authentication.service.*", "io.mosip.kernel.core.logger.config",
 		"io.mosip.authentication.common.service.config","io.mosip.authentication.common.service.util",
 		"io.mosip.kernel.websub.api.config",
