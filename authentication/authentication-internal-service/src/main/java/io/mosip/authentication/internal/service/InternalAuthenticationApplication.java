e4959584b9ca95042103b80e78ff5620cf6b8dfd
kameshsr <kameshsr1338@gmail.com>
2026-03-24 13:44:10 +0530

    MOSIP-44691 corrected SubjectAlternativeNamesHelper
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/InternalAuthenticationApplication.java
+++ authentication/authentication-internal-service/src/main/java/io/mosip/authentication/internal/service/InternalAuthenticationApplication.java
@@ -6,6 +6,7 @@ import io.mosip.authentication.common.service.util.*;
 import io.mosip.kernel.keymanagerservice.helper.SubjectAlternativeNamesHelper;
 import io.mosip.kernel.keymanagerservice.validator.ECKeyPairGenRequestValidator;
 import io.mosip.kernel.pdfgenerator.impl.PDFGeneratorImpl;
+import io.mosip.kernel.signature.util.SignatureUtil;
 import io.mosip.kernel.websub.api.client.SubscriberClientImpl;
 import io.mosip.kernel.websub.api.config.WebSubClientConfig;
 import io.mosip.kernel.websub.api.config.publisher.RestTemplateHelper;
@@ -139,7 +140,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		RestTemplateHelper.class, LanguageUtil.class, TypeForIdNameHelper.class
 		, ValidateOtpHelper.class, RequireOtpNotFrozenHelper.class, MatchIdentityDataHelper.class, MatchTypeHelper.class
 		, SeparatorHelper.class, IdentityAttributesForMatchTypeHelper.class, WebSubClientConfig.class, SubscriberClientImpl.class
-		, ECKeyPairGenRequestValidator.class, WebSubPublisherClientConfig.class, SubjectAlternativeNamesHelper.class
+		, ECKeyPairGenRequestValidator.class, WebSubPublisherClientConfig.class, SubjectAlternativeNamesHelper.class, SignatureUtil.class
 })
 @ComponentScan(basePackages = { "io.mosip.authentication.internal.service.*",
 		"io.mosip.kernel.core.logger.config",
--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/IdAuthenticationApplication.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/IdAuthenticationApplication.java
@@ -6,6 +6,7 @@ import io.mosip.authentication.common.service.util.*;
 import io.mosip.kernel.keymanagerservice.helper.SubjectAlternativeNamesHelper;
 import io.mosip.kernel.keymanagerservice.validator.ECKeyPairGenRequestValidator;
 import io.mosip.kernel.pdfgenerator.impl.PDFGeneratorImpl;
+import io.mosip.kernel.signature.util.SignatureUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -119,7 +120,7 @@ import io.mosip.kernel.websub.api.config.publisher.RestTemplateHelper;
 		RestTemplateHelper.class, LanguageUtil.class, IdentityAttributesForMatchTypeHelper.class
 , TypeForIdNameHelper.class
 		, ValidateOtpHelper.class, RequireOtpNotFrozenHelper.class, MatchIdentityDataHelper.class, MatchTypeHelper.class
-		, SeparatorHelper.class, ECKeyPairGenRequestValidator.class, SubjectAlternativeNamesHelper.class, PDFGeneratorImpl.class})
+		, SeparatorHelper.class, ECKeyPairGenRequestValidator.class, SubjectAlternativeNamesHelper.class, PDFGeneratorImpl.class, SignatureUtil.class})
 @ComponentScan(basePackages = { "io.mosip.authentication.service.*", "io.mosip.kernel.core.logger.config",
 		"io.mosip.authentication.common.service.config","io.mosip.authentication.common.service.util",
 		"io.mosip.kernel.websub.api.config",
