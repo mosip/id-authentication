346ee0f0911798d504132b6cfafd6cf3931c17a6
Sradha Mohanty <mohantysradha10@gmail.com>
2026-04-01 12:12:14 +0530

    MOSIP-41353: Remove unused config files and properties in AUTH module. (#1689)
    * MOSIP-41353
    
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>
    
    * MOSIP-41353
    
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>
    
    * MOSIP-41353
    
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>
    
    * MOSIP-41353
    
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>
    
    * MOSIP-41353
    
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>
    
    * MOSIP-41353
    
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>
    
    ---------
    
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>
    Signed-off-by: Jayesh Kharode <jayesh.kharode@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/auth/testscripts/SimplePostForAutoGenId.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/auth/testscripts/SimplePostForAutoGenId.java
@@ -1,6 +1,5 @@
 package io.mosip.testrig.apirig.auth.testscripts;
 
-import java.lang.reflect.Field;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +17,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.internal.BaseTestMethod;
-import org.testng.internal.TestResult;
 
 import io.mosip.testrig.apirig.auth.utils.IdAuthConfigManager;
 import io.mosip.testrig.apirig.auth.utils.IdAuthenticationUtil;
