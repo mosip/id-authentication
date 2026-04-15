f1c8117d9ab2fef8a2546e7939ab2a8068378c4c
likhitharl <91238496+likhitharl@users.noreply.github.com>
2026-04-15 15:06:59 +0530

    [Mosip-44166]  - Delegate APIs Tetscases(bio/demo/otp ) (#1686)
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * MOSIP-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * MOSIP-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * MOSIP-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * MOSIP-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * Mosip-44166
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    ---------
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    Co-authored-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    Signed-off-by: Jayesh Kharode <jayesh.kharode@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/auth/utils/IdAuthenticationUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/auth/utils/IdAuthenticationUtil.java
@@ -74,6 +74,8 @@ public class IdAuthenticationUtil extends AdminTestUtil {
 						|| testCaseName.contains("_EkycDemo_"))
 				&& (!isElementPresent(globalRequiredFields, individualBiometrics))) {
 			throw new SkipException(GlobalConstants.FEATURE_NOT_SUPPORTED_MESSAGE);
+		} else if (testCaseName.startsWith("auth_") && testCaseName.contains("_DemoAuthDelegated") || testCaseName.contains("_DemoAuthKycExchange")) {
+			throw new SkipException(GlobalConstants.FEATURE_NOT_SUPPORTED_MESSAGE);
 		} else if (testCaseName.startsWith("auth_")
 				&& ((testCaseName.contains("_DeactivateUINs_")) || (testCaseName.contains("PublishDraft_")))
 				&& (!BaseTestCase.getSupportedIdTypesValueFromActuator().contains("VID")
